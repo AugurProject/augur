a6a9ef759858babaf5f8345e93c9e02d8c74df26
Scott Leonard <scott@mixwe.com>
2015-04-12 11:22:14 -0700

    - added skeleton contract flux components - added contract addresses to contants

---
+++ app/static/actions/ContractActions.js
@@ -0,0 +1,19 @@
+var abi = require('../abi');
+var constants = require('../constants');
+
+var ContractActions = {
+
+  updateContracts: function () {
+
+  },
+
+  loadContract: function () {
+
+  },
+
+  updateIsDemo: function (isDemo) {
+
+  }
+};
+
+module.exports = ContractActions;
---
+++ app/static/stores/ContractStore.js
@@ -0,0 +1,42 @@
+var Fluxxor = require('fluxxor');
+var constants = require('../constants');
+
+var state = {
+  contracts: null,
+  updateFailed: false
+}
+
+var ConfigStore = Fluxxor.createStore({
+  initialize: function () {
+    this.bindActions(
+      constants.config.UPDATE_SUCCESS, this.handleUpdateSuccess,
+      constants.config.UPDATE_FAILED, this.handleUpdateFailed,
+      constants.config.UPDATE_IS_DEMO, this.handleUpdateIsDemo
+    );
+  },
+
+  getState: function () {
+    return state;
+  },
+
+  handleUpdateSuccess: function (payload) {
+    state.contracts = payload.contracts;
+    state.updateFailed = false;
+    this.emit(constants.CHANGE_EVENT);
+  },
+
+  handleUpdateFailed: function (payload) {
+    state.contracts = null;
+    state.updateFailed = true;
+    this.emit(constants.CHANGE_EVENT);
+  },
+
+  handleUpdateIsDemo: function (payload) {
+    state.isDemo = payload.isDemo;
+    state.contracts = payload.contracts;
+    state.updateFailed = false;
+    this.emit(constants.CHANGE_EVENT);
+  }
+});
+
+module.exports = ConfigStore;
