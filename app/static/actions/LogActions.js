a01b6283539f9ce93402973853b37f5031f779bc
Scott Leonard <scott@mixwe.com>
2015-04-13 14:19:16 -0700

    - adding new files

---
+++ app/static/actions/LogActions.js
@@ -0,0 +1,12 @@
+var constants = require('../constants');
+var utilities = require('../utilities');
+
+var LogActions = {
+
+  updateLog: function (message, type) {
+
+    // add message and type to log
+  }
+};
+
+module.exports = LogActions;
---
+++ app/static/stores/LogStore.js
@@ -0,0 +1,30 @@
+var Fluxxor = require('fluxxor');
+var constants = require('../constants');
+
+var state = {
+  log: []
+}
+
+var LogStore = Fluxxor.createStore({
+  
+  initialize: function () {
+    this.bindActions(
+      constants.log.UPDATE_LOG, this.handleUpdateLog
+    );
+  },
+
+  getState: function () {
+    return state;
+  },
+
+  handleUpdateLog: function (payload) {
+    var entry = {
+      message: payload.message,
+      type: payload.type || 'info'
+    }
+    this.state.log.push(entry)
+    this.emit(constants.CHANGE_EVENT);
+  }
+});
+
+module.exports = AssetStore;
