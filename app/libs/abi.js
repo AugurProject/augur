e648068db86e5866e9e16dd31ebfdcb5359066f2
Niran Babalola <niran@niran.org>
2015-04-15 02:34:09 -0500

    Add current ABI from augur-serpent

--- app/libs/abi.js
+++ app/libs/abi.js
@@ -1,2 +1,10 @@
 module.exports = {
-}
+  branches: require('./abi/branches.js'),
+  cash: require('./abi/cash.js'),
+  events: require('./abi/events.js'),
+  expiringEvents: require('./abi/expiringEvents.js'),
+  info: require('./abi/info.js'),
+  markets: require('./abi/markets.js'),
+  reporting: require('./abi/reporting.js'),
+  whitelist: require('./abi/whitelist.js')
+};
