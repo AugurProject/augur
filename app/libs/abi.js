2d450750f820e0f04245977d04366c8858e6605c
Niran Babalola <niran@niran.org>
2015-04-15 02:54:23 -0500

    Add ABIs from function files in augur-serpent

--- app/libs/abi.js
+++ app/libs/abi.js
@@ -1,10 +1,19 @@
 module.exports = {
   branches: require('./abi/branches.js'),
+  buyAndSellShares: require('./abi/buyAndSellShares.js'),
   cash: require('./abi/cash.js'),
+  checkQuorum: require('./abi/checkQuorum.js'),
+  createBranch: require('./abi/createBranch.js'),
+  createEvent: require('./abi/createEvent.js'),
+  createMarket: require('./abi/createMarket.js'),
   events: require('./abi/events.js'),
   expiringEvents: require('./abi/expiringEvents.js'),
   info: require('./abi/info.js'),
+  makeReports: require('./abi/makeReports.js'),
   markets: require('./abi/markets.js'),
+  p2pWagers: require('./abi/p2pWagers.js'),
   reporting: require('./abi/reporting.js'),
+  sendReputation: require('./abi/sendReputation.js'),
+  transferShares: require('./abi/transferShares.js'),
   whitelist: require('./abi/whitelist.js')
 };
