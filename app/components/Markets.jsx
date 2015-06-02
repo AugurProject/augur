3f131d0ae069d97c97946c085f30f1ad70a77e40
Scott Leonard <scott@mixwe.com>
2015-06-02 15:36:38 -0700

    - improved treatment for long titles in market panes

--- app/components/Markets.jsx
+++ app/components/Markets.jsx
@@ -57,7 +57,7 @@ var MarketPane = React.createClass({
 
     var body = (
       <div>
-        <h5>{ market.description }</h5>
+        <h5>{ market.description }<div className='overlay' /></h5>
         <div className='summary clearfix'>
           <span>{ percent }</span>
           <i className='pull-right'>{ status }</i>
