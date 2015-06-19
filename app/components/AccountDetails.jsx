b2068897ce532b8e0843da67f9c24dd7ab3c555d
Jack Peterson <jack@tinybike.net>
2015-06-19 15:28:44 -0700

    Disable send buttons on demo.augur.net

--- app/components/AccountDetails.jsx
+++ app/components/AccountDetails.jsx
@@ -110,56 +110,109 @@ var AccountDetails = React.createClass({
       });
     }, this);
 
-    return (
-      <div id="account">
-        <h3>Account<span className='subheading pull-right'>{ this.state.primaryAccount }</span></h3>
-        <div className='subheading row'>
-          <div className='col-sm-4 cash-balance'>
-            { cashBalance } <span className='unit'>cash</span>
-            <ButtonGroup>
-              <SendCashTrigger text='send' />
-              <Button bsSize='xsmall' bsStyle='default' onClick={ this.onCashFaucet }>Faucet<i className='fa fa-tint'></i></Button>
-            </ButtonGroup>
+    var rendered;
+    if (document.domain === "demo.augur.net") {
+      rendered = (
+        <div id="account">
+          <h3>Account<span className='subheading pull-right'>{ this.state.primaryAccount }</span></h3>
+          <div className='subheading row'>
+            <div className='col-sm-4 cash-balance'>
+              { cashBalance } <span className='unit'>cash</span>
+              <ButtonGroup>
+                <Button bsSize='xsmall' bsStyle='default' onClick={ this.onCashFaucet }>Faucet<i className='fa fa-tint'></i></Button>
+              </ButtonGroup>
+            </div>
+            <div className='col-sm-4 rep-balance'>
+              { repBalance } <span className='unit'>rep</span>
+              <ButtonGroup>
+                <Button bsSize='xsmall' bsStyle='default' onClick={ this.onRepFaucet }>Faucet<i className='fa fa-tint'></i></Button>
+              </ButtonGroup>
+            </div>
+            <div className='col-sm-4 ether-balance'>
+              { utilities.formatEther(this.state.asset.ether).value } <span className='unit'>{ utilities.formatEther(this.state.asset.ether).unit }</span>
+            </div>
           </div>
-          <div className='col-sm-4 rep-balance'>
-            { repBalance } <span className='unit'>rep</span>
-            <ButtonGroup>
-              <SendRepTrigger text='send' />
-              <Button bsSize='xsmall' bsStyle='default' onClick={ this.onRepFaucet }>Faucet<i className='fa fa-tint'></i></Button>
-            </ButtonGroup>
-          </div>
-          <div className='col-sm-4 ether-balance'>
-            { utilities.formatEther(this.state.asset.ether).value } <span className='unit'>{ utilities.formatEther(this.state.asset.ether).unit }</span>
-            <SendEtherTrigger text='send' />
+          <div className='row'>
+            <div className="col-xs-12">
+              <h4>Holdings</h4>
+              <Table responsive striped hover>
+                <thead>
+                  <tr>
+                    <th>Description</th>
+                    <th>Shares Held</th>
+                    <th>Price</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  { holdings }
+                </tbody>
+              </Table>
+              <h4>Authored Markets</h4>
+              <div className='authored-markets row'>
+                <Markets 
+                  markets={ this.state.authoredMarkets }
+                  votePeriod={ this.state.votePeriod }
+                  classNameWrapper='col-sm-4' />
+              </div>        
+            </div>
           </div>
         </div>
-        <div className='row'>
-          <div className="col-xs-12">
-            <h4>Holdings</h4>
-            <Table responsive striped hover>
-              <thead>
-                <tr>
-                  <th>Description</th>
-                  <th>Shares Held</th>
-                  <th>Price</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                { holdings }
-              </tbody>
-            </Table>
-            <h4>Authored Markets</h4>
-            <div className='authored-markets row'>
-              <Markets 
-                markets={ this.state.authoredMarkets }
-                votePeriod={ this.state.votePeriod }
-                classNameWrapper='col-sm-4' />
-            </div>        
+      );
+    } else {
+      rendered = (
+        <div id="account">
+          <h3>Account<span className='subheading pull-right'>{ this.state.primaryAccount }</span></h3>
+          <div className='subheading row'>
+            <div className='col-sm-4 cash-balance'>
+              { cashBalance } <span className='unit'>cash</span>
+              <ButtonGroup>
+                <SendCashTrigger text='send' />
+                <Button bsSize='xsmall' bsStyle='default' onClick={ this.onCashFaucet }>Faucet<i className='fa fa-tint'></i></Button>
+              </ButtonGroup>
+            </div>
+            <div className='col-sm-4 rep-balance'>
+              { repBalance } <span className='unit'>rep</span>
+              <ButtonGroup>
+                <SendRepTrigger text='send' />
+                <Button bsSize='xsmall' bsStyle='default' onClick={ this.onRepFaucet }>Faucet<i className='fa fa-tint'></i></Button>
+              </ButtonGroup>
+            </div>
+            <div className='col-sm-4 ether-balance'>
+              { utilities.formatEther(this.state.asset.ether).value } <span className='unit'>{ utilities.formatEther(this.state.asset.ether).unit }</span>
+              <SendEtherTrigger text='send' />
+            </div>
+          </div>
+          <div className='row'>
+            <div className="col-xs-12">
+              <h4>Holdings</h4>
+              <Table responsive striped hover>
+                <thead>
+                  <tr>
+                    <th>Description</th>
+                    <th>Shares Held</th>
+                    <th>Price</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  { holdings }
+                </tbody>
+              </Table>
+              <h4>Authored Markets</h4>
+              <div className='authored-markets row'>
+                <Markets 
+                  markets={ this.state.authoredMarkets }
+                  votePeriod={ this.state.votePeriod }
+                  classNameWrapper='col-sm-4' />
+              </div>        
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
+
+    return rendered;
   }
 });
 
