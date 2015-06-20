0e6e60eb7ec9fa0e7adb7fa84e83e2aa9db9bca3
Jack Peterson <jack@tinybike.net>
2015-06-19 17:43:02 -0700

    Updated private chain contracts

--- test/test_createEvent.js
+++ test/test_createEvent.js
@@ -20,7 +20,7 @@ var TIMEOUT = 120000;
 var minValue = 0;
 var maxValue = 1;
 var numOutcomes = 2;
-var num_events = 8;
+var num_events = 13;
 // var num_reporters = 14;
 
 var branch = Augur.branches.dev;
