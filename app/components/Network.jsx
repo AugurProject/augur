57c35acacc187db5c3d1937282ae921f9b4e45bb
Scott Leonard <scott@mixwe.com>
2015-07-07 12:00:01 -0700

    - added client version/platform to ethereum network component.

--- app/components/Network.jsx
+++ app/components/Network.jsx
@@ -40,6 +40,9 @@ var Network = React.createClass({
           <span className="pull-left">Ethereum</span>
         </div>
         <div className="panel-body">
+          <p className="client">
+            CLIENT<span className="pull-right">{this.state.network.clientVersion}</span>
+          </p>
           <p className="host">
             HOST<span className="pull-right">{this.state.host}</span>
           </p>
