426d6483b8153b3ac5ee829488945deeaaac7a8e
Jack Peterson <jack@tinybike.net>
2015-07-24 17:35:48 -0700

    Moved documentation from README to augur.js-docs

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -28,8 +28,8 @@ mocha test/multicast.js $1
 mocha test/createEvent.js $1
 mocha test/buyAndSellShares.js $1
 # mocha test/ballot.js $1
-mocha test/makeReports.js $1
-mocha test/checkQuorum.js $1
+# mocha test/makeReports.js $1
+# mocha test/checkQuorum.js $1
 # mocha test/closeMarket.js $1
 # mocha test/dispatch.js $1
 # mocha test/interpolate.js $1
