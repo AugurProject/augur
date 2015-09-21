927850d2203f971decf250a89100577c0872502f
Jack Peterson <jack@tinybike.net>
2015-09-20 17:49:26 -0700

    Faucet test CI check

--- test/core/faucets.js
+++ test/core/faucets.js
@@ -39,64 +39,68 @@ describe("Faucets", function () {
         );
     });
 
-    it("cashFaucet", function (done) {
-        this.timeout(constants.TIMEOUT*4);
+    if (!process.env.CONTINUOUS_INTEGRATION) {
 
-        function faucet() {
-            augur.cashFaucet(
-                function (r) {
-                    // sent
-                    assert(r.callReturn === "1" || r.callReturn === "-1");
-                    assert(abi.bignum(r.txHash).toNumber());
-                },
-                function (r) {
-                    // success
-                    assert(r.callReturn === "1" || r.callReturn === "-1");
-                    assert.notStrictEqual(abi.bignum(r.blockHash).toNumber(), 0);
-                    var rep_balance = augur.getRepBalance(augur.branches.dev, augur.coinbase);
-                    var cash_balance = augur.getCashBalance(augur.coinbase);
-                    if (r.callReturn === "1") {
-                        assert.strictEqual(cash_balance, "10000");
-                    } else {
-                        assert.isAbove(abi.bignum(cash_balance).toNumber(), 5);
+        it("cashFaucet", function (done) {
+            this.timeout(constants.TIMEOUT*4);
+
+            function faucet() {
+                augur.cashFaucet(
+                    function (r) {
+                        // sent
+                        assert(r.callReturn === "1" || r.callReturn === "-1");
+                        assert(abi.bignum(r.txHash).toNumber());
+                    },
+                    function (r) {
+                        // success
+                        assert(r.callReturn === "1" || r.callReturn === "-1");
+                        assert.notStrictEqual(abi.bignum(r.blockHash).toNumber(), 0);
+                        var rep_balance = augur.getRepBalance(augur.branches.dev, augur.coinbase);
+                        var cash_balance = augur.getCashBalance(augur.coinbase);
+                        if (r.callReturn === "1") {
+                            assert.strictEqual(cash_balance, "10000");
+                        } else {
+                            assert.isAbove(abi.bignum(cash_balance).toNumber(), 5);
+                        }
+                        done();
+                    },
+                    function (r) {
+                        // failed
+                        done(r);
                     }
-                    done();
-                },
-                function (r) {
-                    // failed
-                    done(r);
-                }
-            );
-        }
+                );
+            }
 
-        var augur = utils.setup(require("../../src"), process.argv.slice(2));
-        var cash_balance = augur.getCashBalance(augur.coinbase);
+            var augur = utils.setup(require("../../src"), process.argv.slice(2));
+            var cash_balance = augur.getCashBalance(augur.coinbase);
 
-        if (abi.number(cash_balance) >= 5) {
-            var start_balance = abi.bignum(cash_balance);
-            augur.sendCash({
-                to: "0x405be667f1a6b2d5149a61057040cade5aada366",
-                value: start_balance.sub(new BigNumber(1)),
-                onSent: function (r) {
-                    // log(r);
-                    assert.property(r, "txHash");
-                    assert.property(r, "callReturn");
-                },
-                onSuccess: function (r) {
-                    var final_balance = abi.bignum(
-                        augur.getCashBalance(augur.coinbase)
-                    );
-                    assert.strictEqual(final_balance.toFixed(), "1");
-                    faucet();
-                },
-                onFailed: function (r) {
-                    throw r;
-                }
-            });
+            if (abi.number(cash_balance) >= 5) {
+                var start_balance = abi.bignum(cash_balance);
+                augur.sendCash({
+                    to: "0x405be667f1a6b2d5149a61057040cade5aada366",
+                    value: start_balance.sub(new BigNumber(1)),
+                    onSent: function (r) {
+                        // log(r);
+                        assert.property(r, "txHash");
+                        assert.property(r, "callReturn");
+                    },
+                    onSuccess: function (r) {
+                        var final_balance = abi.bignum(
+                            augur.getCashBalance(augur.coinbase)
+                        );
+                        assert.strictEqual(final_balance.toFixed(), "1");
+                        faucet();
+                    },
+                    onFailed: function (r) {
+                        throw r;
+                    }
+                });
 
-        } else {
-            faucet();
-        }
-    });
+            } else {
+                faucet();
+            }
+        });
+
+    }
 
 });
