1a0a202ceb7c5dbba7f61cff6806ae61df53c63a
Jack Peterson <jack@tinybike.net>
2015-11-30 01:19:55 -0800

    Fixed gulpfile, API from CLI; separated build and minify tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,39 @@
 "use strict";
 
-var path = require("path");
-var cp = require("child_process");
-var nodeUtil = require("util");
 var gulp = require("gulp");
 var del = require("del");
+var babelify = require("babelify");
+var browserify = require("browserify");
+var buffer = require("vinyl-buffer");
+var source = require("vinyl-source-stream");
+var uglify = require("gulp-uglify");
 
-var gulp_log = require("fs").createWriteStream(
-    require("path").join(__dirname, "data", "gulp.log"),
-    {flags : 'w'}
-);
+function bundle() {
+    return browserify({
+        entries: "src/index.js",
+        insertGlobals: true,
+        fullPaths: true,
+        debug: true
+    }).transform(babelify, {
+        presets: ["es2015"],
+        compact: false,
+        global: true
+    }).bundle();
+}
 
 gulp.task("clean", function (callback) {
     del(["dist/*"], callback);
 });
 
-gulp.task("lint", function (callback) {
-    var runtests = cp.spawn("npm", ["run", "lint"]);
-    runtests.stdout.on("data", function (data) {
-        gulp_log.write(nodeUtil.format(data.toString()));
-    });
-    runtests.stderr.on("data", function (data) {
-        process.stdout.write(data);
-    });
-    runtests.on("close", callback);
+gulp.task("build", function () {
+    bundle().pipe(source("augur.js")).pipe(gulp.dest("dist"));
 });
 
-gulp.task("build", function (callback) {
-    var browserify = "./node_modules/browserify/bin/cmd.js ./exports.js > ./dist/augur-es6.js";
-    var babelify = "./node_modules/babel-cli/bin/babel.js ./dist/augur.js --presets es2015 --compact=false -o ./dist/augur-es5.js";
-    var minify = "./node_modules/uglify-js/bin/uglifyjs ./dist/augur.js -o ./dist/augur.min.js";
-    cp.exec(browserify, function (err, stdout) {
-        if (err) throw err;
-        if (stdout) process.stdout.write(stdout);
-        cp.exec(babelify, function (err, stdout) {
-            if (err) throw err;
-            if (stdout) process.stdout.write(stdout);
-            cp.exec(minify, function (err, stdout) {
-                if (err) throw err;
-                if (stdout) process.stdout.write(stdout);
-                callback();
-            });
-        });
-    });
+gulp.task("minify", function () {
+    bundle().pipe(source("augur.min.js"))
+            .pipe(buffer())
+            .pipe(uglify())
+            .pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", ["lint", "build"]);
+gulp.task("default", ["build", "minify"]);
