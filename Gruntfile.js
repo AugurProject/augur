9c61d9cfbab9615415aa769a318fffd3417b0818
priecint <tp-dev@seznam.cz>
2016-01-26 19:41:02 +0100

    Watch copied files during development

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,6 +72,10 @@ module.exports = function (grunt) {
       less: {
         files: ['app/less/**/*.less'],
         tasks: ['less:build']
+      },
+      copy: {
+        files: ['app/index.html', 'app/iamges/*', 'app/css/*'],
+        tasks: ['newer:copy:main']
       }
     },
     copy: {
@@ -125,7 +129,8 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-newer');
 
   grunt.registerTask('default', ['jshint', 'browserify:build', 'less:build', 'copy']);
-  grunt.registerTask('watchify', ['browserify:debug', 'watch:less']);
+  grunt.registerTask('watch', ['browserify:debug', 'watch']);
 };
