f7eb45e31c5a158963488024ddc5f461c2ae1d3e
Baz <bk@thinkloop.com>
2016-02-26 19:07:18 +0000

    improved icon implementation

--- app/components/markets-page/MarketRow.jsx
+++ app/components/markets-page/MarketRow.jsx
@@ -242,15 +242,18 @@ let MarketRow = React.createClass({
                     { rowAction }
                 </div>
                 <div className="subtitle clearfix">
-                    <div className="fa fa-square-o labelValue subtitle-group">
+                    <div className="labelValue subtitle-group">
+                        <i className="fa fa-square-o" />
                         <span className="labelValue-label trading-fee-label">Trading Fee: </span>
                         <span className="labelValue-value trading-fee">{market.tradingFee ? market.tradingFee.times(100).toFixed(1) + '%' : '-'}</span>
                     </div>
-                    <div className="fa fa-clock-o labelValue subtitle-group">
+                    <div className="labelValue subtitle-group">
+                        <i className="fa fa-clock-o" />
                         <span className="labelValue-label end-date-label">{endDateLabel}: </span>
                         <span className="labelValue-value end-date">{endDateFormatted}</span>
                     </div>
-                    <div className="fa fa-bookmark-o labelValue subtitle-group">
+                    <div className="labelValue subtitle-group">
+                        <i className="fa fa-bookmark-o" />
                         <span className="labelValue-label">
                             {utilities.getMarketTypeName(market)}
                             { " " }
