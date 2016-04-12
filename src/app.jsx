aa8a149c21492fb91f0dba04a87efe02ffb10bb5
Baz <bk@thinkloop.com>
2016-04-12 03:23:24 +0000

    1.0.0

--- src/app.jsx
+++ src/app.jsx
@@ -10,45 +10,47 @@ import AuthPage from './modules/auth/components/auth-page';
 import PositionsPage from './modules/positions/components/positions-page';
 import TransactionsPage from './modules/transactions/components/transactions-page';
 
-module.exports = function(props) {
-    var p = props.selectors;
-    switch(p.activePage) {
-    	case REGISTER:
-        case LOGIN:
-        case LOGOUT:
-    		return <AuthPage
-    					siteHeader={ p.siteHeader }
-    					authForm={ p.authForm } />;
+module.exports = React.createClass({
+    render: function() {
+        var p = this.props.selectors;
+        switch(p.activePage) {
+        	case REGISTER:
+            case LOGIN:
+            case LOGOUT:
+        		return <AuthPage
+        					siteHeader={ p.siteHeader }
+        					authForm={ p.authForm } />;
 
-    	case MAKE:
-    		return <CreateMarketPage
-    					siteHeader={ p.siteHeader }
-    					createMarketForm={ p.createMarketForm } />;
+        	case MAKE:
+        		return <CreateMarketPage
+        					siteHeader={ p.siteHeader }
+        					createMarketForm={ p.createMarketForm } />;
 
-    	case POSITIONS:
-    		return <PositionsPage
-    					siteHeader={ p.siteHeader }
-    					positions={ p.positions }
-    					positionsSummary={ p.positionsSummary } />;
+        	case POSITIONS:
+        		return <PositionsPage
+        					siteHeader={ p.siteHeader }
+        					positions={ p.positions }
+        					positionsSummary={ p.positionsSummary } />;
 
-    	case TRANSACTIONS:
-    		return <TransactionsPage
-    					siteHeader={ p.siteHeader }
-    					transactions={ p.transactions }
-    					transactionsTotals={ p.transactionsTotals } />;
+        	case TRANSACTIONS:
+        		return <TransactionsPage
+        					siteHeader={ p.siteHeader }
+        					transactions={ p.transactions }
+        					transactionsTotals={ p.transactionsTotals } />;
 
-    	case M:
-    		return <MarketPage selectors={ p } />;
+        	case M:
+        		return <MarketPage selectors={ p } />;
 
-    	default:
-    		return <MarketsPage
-    					siteHeader={ p.siteHeader }
-    					createMarketLink={ (p.links || {}).createMarketLink }
-    					onChangeKeywords={ p.keywordsChangeHandler }
+        	default:
+        		return <MarketsPage
+        					siteHeader={ p.siteHeader }
+        					createMarketLink={ (p.links || {}).createMarketLink }
+        					onChangeKeywords={ p.keywordsChangeHandler }
 
-    					markets={ p.markets }
-    					favoriteMarkets={ p.favoriteMarkets }
-    					marketsHeader={ p.marketsHeader }
-    					filtersProps={ p.filtersProps } />;
+        					markets={ p.markets }
+        					favoriteMarkets={ p.favoriteMarkets }
+        					marketsHeader={ p.marketsHeader }
+        					filtersProps={ p.filtersProps } />;
+        }
     }
-};
+});
