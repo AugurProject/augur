1e5adacef1f0b1a48d405a82d6958190aa4eb713
Baz <bk@thinkloop.com>
2016-04-19 10:01:02 +0000

    fancied sort labels

--- src/modules/markets/selectors/search-sort.js
+++ src/modules/markets/selectors/search-sort.js
@@ -8,20 +8,38 @@ export default function() {
 	var { selectedSort } = store.getState();
 	return {
 		selectedSort,
-		sortOptions: selectSortOptions(),
+		sortOptions: selectSortOptions(selectedSort),
 		onChangeSort: selectOnChangeSort(store.dispatch),
 		onChangeKeywords: selectOnChangeKeywords(store.dispatch)
 	};
 }
 
-export const selectSortOptions = memoizerific(1)(function() {
-	return [
-		{ label: 'Creation Date', value: 'creationSortOrder' },
-		{ label: 'End Date', value: 'endBlock' },
-		{ label: 'Volume', value: 'volume' },
-		{ label: 'Fee', value: 'tradingFeePercent' },
-		{ label: 'Description', value: 'description' }
-	];
+export const selectSortOptions = memoizerific(10)(function(selectedSort = {}) {
+	var creationSortOrder = { label: 'Newest Market', value: 'creationSortOrder', isDesc: true },
+		endBlock = { label: 'Soonest Expiry', value: 'endBlock', isDesc: false },
+		volume = { label: 'Most Volume', value: 'volume', isDesc: true },
+		tradingFeePercent = { label: 'Lowest Fee', value: 'tradingFeePercent', isDesc: false };
+
+	switch (selectedSort.prop) {
+		case creationSortOrder.value:
+			creationSortOrder.label = selectedSort.isDesc ? 'Newest Market' : 'Oldest Market';
+			creationSortOrder.isDesc = selectedSort.isDesc;
+			break;
+		case endBlock.value:
+			endBlock.label = selectedSort.isDesc ? 'Furthest Expiry' : 'Soonest Expiry';
+			endBlock.isDesc = selectedSort.isDesc;
+			break;
+		case volume.value:
+			volume.label = selectedSort.isDesc ? 'Most Volume' : 'Least Volume';
+			volume.isDesc = selectedSort.isDesc;
+			break;
+		case tradingFeePercent.value:
+			tradingFeePercent.label = selectedSort.isDesc ? 'Highest Fee' : 'Lowest Fee';
+			tradingFeePercent.isDesc = selectedSort.isDesc;
+			break;
+	}
+
+	return [creationSortOrder, endBlock, volume, tradingFeePercent];
 });
 
 export const selectOnChangeSort = memoizerific(1)(function(dispatch) {
@@ -33,6 +51,7 @@ export const selectOnChangeSort = memoizerific(1)(function(dispatch) {
         if (isDesc || isDesc === false) {
             o.isDesc = isDesc;
         }
+
         dispatch(MarketsActions.updateSelectedSort(o));
     };
 });
