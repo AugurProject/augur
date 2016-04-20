4f39dd484ca87aeedddeba897c37c38ef9d6aeb5
John Danz <jdanz@capturepoint.com>
2016-04-20 17:31:31 -0500

    testing advanced actions is going to be difficult because some use getState from store.js and won't use test data...

--- test/trade/actions/trade-actions-test.js
+++ test/trade/actions/trade-actions-test.js
@@ -42,4 +42,18 @@ describe('src/modules/trade/actions/trade-actions.js', () => {
       assert.deepEqual(actions.clearTradeInProgress('test'), expectedOutput);
   });
 
+  it('should be able to handle more advanced actions...', () => {
+    /*
+    this is going to be an tough one because the action im attempting to call
+    calls other actions that take from store which will be empty, it wont be
+    using the test store i have. This is going to require some rethinking...
+    */
+    const store = mockStore(testState);
+
+    // return store.dispatch(actions.placeTrade('test'))
+    //   .then(() => { // return of async actions
+    //     console.log(store.getActions())
+    //   });
+
+  });
 });
