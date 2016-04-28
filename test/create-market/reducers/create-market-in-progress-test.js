d445f867b0bfab6f6aac981e8cee5f841171982c
John Danz <John.Danz87@gmail.com>
2016-04-28 17:27:53 -0500

    implemented more reducer tests

--- test/create-market/reducers/create-market-in-progress-test.js
+++ test/create-market/reducers/create-market-in-progress-test.js
@@ -1,6 +1,39 @@
-import { assert } from 'chai';
+import {
+  assert
+} from 'chai';
+import {
+  UPDATE_MAKE_IN_PROGRESS,
+  CLEAR_MAKE_IN_PROGRESS
+} from '../../../src/modules/create-market/actions/update-make-in-progress';
+import reducer from '../../../src/modules/create-market/reducers/create-market-in-progress';
+import testState from '../../testState';
 
 describe(`modules/create-market/reducers/create-market-in-progress.js`, () => {
-  it(`should be able to update a make in progress`);
-  it(`should be able to clear a make in progress`);
+  let action, expectedOutput, test;
+  let thisTestState = Object.assign({}, testState);
+
+  it(`should be able to update a make in progress`, () => {
+    action = {
+      type: UPDATE_MAKE_IN_PROGRESS,
+      data: {
+        example: 'test'
+      }
+    };
+    expectedOutput = {
+      example: 'test'
+    };
+    test = reducer(thisTestState.createMarketInProgress, action);
+
+    assert.deepEqual(test, expectedOutput, `Didn't update the make in progress state`);
+  });
+
+  it(`should be able to clear a make in progress`, () => {
+    action = {
+      type: CLEAR_MAKE_IN_PROGRESS
+    };
+    expectedOutput = {};
+    test = reducer(thisTestState.createMarketInProgress, action);
+
+    assert.deepEqual(test, expectedOutput, `Didn't clear a make in progress`);
+  });
 });
