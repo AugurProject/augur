d84f9a21a4e8f0d89864fca6773afe51987da4f4
John Danz <John.Danz87@gmail.com>
2016-05-06 17:10:02 -0500

    unit tests for transactions/actions and trade/selectors/trade-orders-test abandoned for now because the unit it's testing isn't in use

--- test/trade/selectors/trade-orders-test.js
+++ test/trade/selectors/trade-orders-test.js
@@ -1,24 +1,38 @@
 import {
   assert
 } from 'chai';
-import proxyquire from 'proxyquire';
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import testState from '../../testState';
-import * as selector from '../../../src/modules/trade/selectors/trade-orders';
+// import {
+//   formatShares,
+//   formatEther,
+//   formatNumber
+// } from '../../../src/utils/format-number';
+// import * as selector from '../../../src/modules/trade/selectors/trade-orders';
 
 describe(`modules/trade/selectors/trade-orders.js`, () => {
-  proxyquire.noPreserveCache();
-  const middlewares = [thunk];
-  const mockStore = configureMockStore(middlewares);
-  let state = Object.assign({}, testState);
-  let store = mockStore(state);
-  let out;
-  // This area looks like it's still Under Contruction, will revisit tests
-  it(`should select trade orders correctly`);
+  let out, test;
+  // let market = {
+  //   outcomes: [{
+  //     id: 'test1',
+  //     name: 'testOutcome1'
+  //   }, {
+  //     id: 'test2',
+  //     name: 'testOutcome2'
+  //   }]
+  // };
+  // let makeTradeInProgress = {
+  //   test1: {
+  //     numShares: 10,
+  //     totalCost: 100,
+  //   },
+  //   test2: {
+  //     numShares: 5,
+  //     totalCost: 50,
+  //   }
+  // };
+  // let dispatch = (item) => item;
 
-  it(`should select outcome trade orders correctly`);
+  // test = selector.selectTradeOrders(market, makeTradeInProgress, dispatch);
+  // console.log(test);
 
-  it(`should select outcome transactions correctly`);
-
+  it(`[Not in use?] should select the trade orders`);
 });
