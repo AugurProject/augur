8e1524c640ecab9c432a63fac459a28ff9a57b9a
John Danz <John.Danz87@gmail.com>
2016-05-07 09:37:23 -0500

    added process-transaction test

--- test/transactions/actions/process-transactions-test.js
+++ test/transactions/actions/process-transactions-test.js
@@ -1,5 +1,42 @@
-import {assert} from 'chai';
+import {
+  assert
+} from 'chai';
+import proxyquire from 'proxyquire';
+import sinon from 'sinon';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import testState from '../../testState';
 
 describe(`modules/transactions/actions/process-transactions.js`, () => {
-  it(`should process pending transactions`);
+  proxyquire.noPreserveCache();
+  const middlewares = [thunk];
+  const mockStore = configureMockStore(middlewares);
+  let store, action;
+  let state = Object.assign({}, testState);
+  store = mockStore(state);
+  let trans = { transactions: [{
+    id: 'test1',
+    action: sinon.stub(),
+    status: 'pending'
+  }, {
+    id: 'test2',
+    action: sinon.stub(),
+    status: 'pending'
+  }, {
+    id: 'test3',
+    action: sinon.stub(),
+    status: 'pending'
+  }]};
+
+  action = proxyquire('../../../src/modules/transactions/actions/process-transactions', {
+    '../../../selectors': trans
+  });
+
+  it(`should process pending transactions`, () => {
+    store.dispatch(action.processTransactions());
+
+    assert(trans.transactions[0].action.calledOnce, `mockTrans1 wasn't called once`);
+    assert(trans.transactions[1].action.calledOnce, `MockTrans2 wasn't called once`);
+    assert(trans.transactions[2].action.calledOnce, `MockTrans3 wasn't called once`);
+  });
 });
