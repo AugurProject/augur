3ef5407d57e0ffc42547fa9920da300940e0bf96
Jack Peterson <jack@tinybike.net>
2016-05-08 03:03:12 -0700

    Revert "Changed path constants to relative paths"
    This reverts commit 5f511e6524f6ab97594402f15a1f60d73363affa.

--- src/modules/link/constants/paths.js
+++ src/modules/link/constants/paths.js
@@ -2,18 +2,18 @@ import { MARKETS, MAKE, POSITIONS, TRANSACTIONS, M } from '../../app/constants/p
 import { REGISTER, LOGIN } from '../../auth/constants/auth-types';
 
 export const PATHS_PAGES = {
-    '': MARKETS,
-    'make': MAKE,
-    'positions': POSITIONS,
-    'transactions': TRANSACTIONS,
-    'register': REGISTER,
-    'login': LOGIN,
-    'm': M
+    '/': MARKETS,
+    '/make': MAKE,
+    '/positions': POSITIONS,
+    '/transactions': TRANSACTIONS,
+    '/register': REGISTER,
+    '/login': LOGIN,
+    '/m': M
 };
 export const PAGES_PATHS = Object.keys(PATHS_PAGES).reduce((finalObj, key) => { finalObj[PATHS_PAGES[key]] = key; return finalObj }, {});
 
 export const PATHS_AUTH = {
-    'register': REGISTER,
-    'login': LOGIN
+    '/register': REGISTER,
+    '/login': LOGIN
 };
 export const AUTH_PATHS = Object.keys(PATHS_AUTH).reduce((finalObj, key) => { finalObj[PATHS_AUTH[key]] = key; return finalObj }, {});
