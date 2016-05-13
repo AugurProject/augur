4254276d191ab7aee3ea370a00d7d5149ba1f721
John Danz <John.Danz87@gmail.com>
2016-05-13 12:25:01 -0500

    added step 2-4 tests

--- test/create-market/selectors/form-steps/step-2-test.js
+++ test/create-market/selectors/form-steps/step-2-test.js
@@ -1,9 +1,144 @@
 import {
   assert
 } from 'chai';
+import {
+  BINARY,
+  CATEGORICAL,
+  SCALAR,
+  COMBINATORIAL
+} from '../../../../src/modules/markets/constants/market-types';
+import * as selector from '../../../../src/modules/create-market/selectors/form-steps/step-2';
 
 describe(`modules/create-market/selectors/form-steps/step-2.js`, () => {
-  it('should handle selecting type of market and return correct data');
-  it(`should handle validation of step 2`);
-  it(`should handle errors in step 2`);
+  let formState1, formState2, formState3, formState4, out1, out2, out3, out4;
+
+  it('should handle selecting type of market and return correct data', () => {
+    formState1 = {
+      type: BINARY
+    };
+    formState2 = {
+      type: CATEGORICAL
+    };
+    formState3 = {
+      type: SCALAR,
+      scalarSmallNum: 10,
+      scalarBigNum: 100
+    };
+    formState4 = {
+      type: COMBINATORIAL
+    };
+    out1 = {
+      descriptionPlaceholder: 'Will "Batman v Superman: Dawn of Justice" take more than $150 million box in office receipts opening weekend?',
+      descriptionMinLength: 1,
+      descriptionMaxLength: 256
+    };
+    out2 = {
+      descriptionPlaceholder: 'Who will win the Four Nations Rugby Championship in 2016?',
+      descriptionMinLength: 1,
+      descriptionMaxLength: 256,
+      categoricalOutcomesMinNum: 2,
+      categoricalOutcomesMaxNum: 8,
+      categoricalOutcomeMaxLength: 250
+    };
+    out3 = {
+      descriptionPlaceholder: 'What will the temperature (in degrees Fahrenheit) be in San Francisco, California, on July 1, 2016?',
+      descriptionMinLength: 1,
+      descriptionMaxLength: 256,
+      scalarSmallNum: 10,
+      scalarBigNum: 100
+    };
+    out4 = {
+      descriptionPlaceholder: 'Combinatorial',
+      descriptionMinLength: 1,
+      descriptionMaxLength: 256
+    };
+
+
+    assert.deepEqual(selector.select(formState1), out1, `Didn't produce the expected object`);
+    assert.deepEqual(selector.select(formState2), out2, `Didn't produce the expected object`);
+    assert.deepEqual(selector.select(formState3), out3, `Didn't produce the expected object`);
+    assert.deepEqual(selector.select(formState4), out4, `Didn't produce the expected object`);
+  });
+
+  it(`should handle validation of step 2`, () => {
+    formState1 = {
+      type: BINARY,
+      descriptionPlaceholder: 'Will "Batman v Superman: Dawn of Justice" take more than $150 million box in office receipts opening weekend?',
+      descriptionMinLength: 1,
+      descriptionMaxLength: 256
+    };
+
+    formState2 = {
+      descriptionPlaceholder: 'Who will win the Four Nations Rugby Championship in 2016?',
+      descriptionMinLength: 1,
+      descriptionMaxLength: 256,
+      categoricalOutcomesMinNum: 2,
+      categoricalOutcomesMaxNum: 8,
+      categoricalOutcomeMaxLength: 250
+    };
+
+    formState3 = {
+      descriptionPlaceholder: 'What will the temperature (in degrees Fahrenheit) be in San Francisco, California, on July 1, 2016?',
+      descriptionMinLength: 1,
+      descriptionMaxLength: 256,
+      scalarSmallNum: 10,
+      scalarBigNum: 100
+    };
+
+    formState4 = {
+      descriptionPlaceholder: 'Combinatorial',
+      descriptionMinLength: 1,
+      descriptionMaxLength: 256
+    };
+
+    assert(!selector.isValid(formState1), `Didn't properly invalidate a formstate without description`);
+    formState1.description = 'test1234 this is a description';
+    assert(!selector.isValid(formState1), `Didn't properly invalidate a formState without a valid endDate`);
+    formState1.endDate = Date('01/01/3000');
+    assert(selector.isValid(formState1), `Didn't properly validate a BINARY form state`);
+
+    formState2.description = 'test1234 this is a description';
+    formState2.endDate = Date('01/01/3000');
+    formState3.description = 'test1234 this is a description';
+    formState3.endDate = Date('01/01/3000');
+    formState4.description = 'test1234 this is a description';
+    formState4.endDate = Date('01/01/3000');
+
+    assert(selector.isValid(formState2), `Didn't properly validate a CATEGORICAL form state`);
+    assert(selector.isValid(formState3), `Didn't properly validate a SCALAR form state`);
+    assert(selector.isValid(formState4), `Didn't properly validate a COMBINATORIAL form state`);
+  });
+
+  it(`should handle errors in step 2`, () => {
+    formState1 = {
+      type: BINARY,
+      description: '',
+      endDate: 'notadate'
+    };
+    formState2 = {
+      type: CATEGORICAL,
+      categoricalOutcomes: [1]
+    };
+    formState3 = {
+      type: SCALAR,
+      scalarSmallNum: 200,
+      scalarBigNum: 1
+    };
+    out1 = {
+      description: 'Please enter your question',
+      endDate: null
+    };
+    out2 = {
+      categoricalOutcomes: ['Answer cannot be blank']
+    };
+    out3 = {
+      scalarSmallNum: 'Minimum must be less than maximum',
+      scalarBigNum: 'Maximum must be greater than minimum'
+    };
+
+    assert.deepEqual(selector.errors(formState1), out1, `Didn't catch the expected errors for BINARY forms`);
+    assert.deepEqual(selector.errors(formState2), out2, `Didn't catch the expected errors for SCALAR forms`);
+    assert.deepEqual(selector.errors(formState3), out3, `Didn't catch the expected errors for CATEGORICAL forms`);
+
+  });
 });
--- test/create-market/selectors/form-steps/step-4-test.js
+++ test/create-market/selectors/form-steps/step-4-test.js
@@ -1,9 +1,108 @@
 import {
   assert
 } from 'chai';
+import {
+  TRADING_FEE_MIN,
+  TRADING_FEE_MAX,
+  INITIAL_LIQUIDITY_MIN,
+  TRADING_FEE_DEFAULT,
+  INITIAL_LIQUIDITY_DEFAULT
+} from '../../../../src/modules/create-market/constants/market-values-constraints';
+import * as selector from '../../../../src/modules/create-market/selectors/form-steps/step-4';
 
 describe(`modules/create-market/selectors/form-steps/step-4.js`, () => {
-  it('should handle returning correct data shape');
-  it(`should handle validation of step 4`);
-  it(`should handle errors in step 4`);
+  let formState, out;
+
+  it('should handle returning correct data shape', () => {
+    out = {
+      tradingFeePercent: TRADING_FEE_DEFAULT,
+      initialLiquidity: INITIAL_LIQUIDITY_DEFAULT
+    };
+    assert.deepEqual(selector.select({}), out, `Didn't produce the expected return object for select()`);
+  });
+
+  it(`should handle validation of step 4`, () => {
+    formState = {
+      tradingFeePercent: '',
+      initialLiquidity: ''
+    };
+    assert(!selector.isValid(formState), `Didn't invalidate a blank tradingFeePercent`);
+    formState.tradingFeePercent = 'testNonNumeric';
+    assert(!selector.isValid(formState), `Didn't invalidate a tradingFeePercent that wasn't a number`);
+    formState.tradingFeePercent = (TRADING_FEE_MIN - 1);
+    assert(!selector.isValid(formState), `Didn't invalidate a tradingFeePercent that is below the Trading Fee min`);
+    formState.tradingFeePercent = (TRADING_FEE_MAX + 1);
+    assert(!selector.isValid(formState), `Didn't invalidate a tradingFeePercent that is above the Trading Fee max`);
+    formState.tradingFeePercent = TRADING_FEE_DEFAULT;
+    assert(!selector.isValid(formState), `Didn't invalidate a initialLiquidity of empty string`);
+    formState.initialLiquidity = 'testNonNumeric';
+    assert(!selector.isValid(formState), `Didn't invalidate a non numeric initialLiquidity`);
+    formState.initialLiquidity = (INITIAL_LIQUIDITY_MIN - 1);
+    assert(!selector.isValid(formState), `Didn't invalidate a initialLiquidity that was below the minumum`);
+    formState.initialLiquidity = (INITIAL_LIQUIDITY_MIN + 10);
+    assert(selector.isValid(formState), `Didn't validate a valid formState`);
+
+  });
+
+  it(`should handle errors in step 4`, () => {
+    formState = {
+      tradingFeePercent: '',
+      initialLiquidity: ''
+    };
+    out = {
+      tradingFeePercent: 'Please specify a trading fee %',
+      initialLiquidity: 'Please provide some initial liquidity'
+    };
+    assert.deepEqual(selector.errors(formState), out, `Didn't error on a blank tradingFeePercent`);
+
+    formState.tradingFeePercent = 'testNonNumeric';
+    out = {
+      tradingFeePercent: 'Trading fee must be a number',
+      initialLiquidity: 'Please provide some initial liquidity'
+    };
+    assert.deepEqual(selector.errors(formState), out, `Didn't error on a tradingFeePercent that wasn't a number`);
+
+    formState.tradingFeePercent = (TRADING_FEE_MIN - 1);
+    out = {
+      tradingFeePercent: 'Please specify a trading fee %',
+      initialLiquidity: 'Please provide some initial liquidity'
+    };
+    assert.deepEqual(selector.errors(formState), out, `Didn't error on a tradingFeePercent that is below the Trading Fee min`);
+
+    formState.tradingFeePercent = (TRADING_FEE_MAX + 1);
+    out = {
+      tradingFeePercent: 'Trading fee must be between +1.0% and +12.5%',
+      initialLiquidity: 'Please provide some initial liquidity'
+    };
+    assert.deepEqual(selector.errors(formState), out, `Didn't error on a tradingFeePercent that is above the Trading Fee max`);
+
+    formState.tradingFeePercent = TRADING_FEE_DEFAULT;
+    out = {
+      tradingFeePercent: undefined,
+      initialLiquidity: 'Please provide some initial liquidity'
+    };
+    assert.deepEqual(selector.errors(formState), out, `Didn't error on a initialLiquidity of empty string`);
+
+    formState.initialLiquidity = 'testNonNumeric';
+    out = {
+      tradingFeePercent: undefined,
+      initialLiquidity: 'Initial liquidity must be numeric'
+    };
+    assert.deepEqual(selector.errors(formState), out, `Didn't error on a non numeric initialLiquidity`);
+
+    formState.initialLiquidity = (INITIAL_LIQUIDITY_MIN - 1);
+    out = {
+      tradingFeePercent: undefined,
+      initialLiquidity: 'Initial liquidity must be at least +50.00Eth'
+    };
+    assert.deepEqual(selector.errors(formState), out, `Didn't error on a initialLiquidity that was below the minumum`);
+
+    formState.initialLiquidity = (INITIAL_LIQUIDITY_MIN + 10);
+    out = {
+      tradingFeePercent: undefined,
+      initialLiquidity: undefined
+    };
+    assert.deepEqual(selector.errors(formState), out, `Didn't return no errors for a valid formState`);
+
+  });
 });
