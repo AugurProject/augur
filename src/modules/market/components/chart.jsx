e8e5eb11b60bd1627078dff5704dc5c0867d6d56
priecint <tp-dev@seznam.cz>
2016-05-16 12:42:56 +0200

    Use default shouldComponentUpdate

--- src/modules/market/components/chart.jsx
+++ src/modules/market/components/chart.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import shouldComponentUpdate from '../../../utils/should-component-update-pure';
+
 import ReactHighcharts from 'react-highcharts';
 
 module.exports = React.createClass({
@@ -7,40 +9,7 @@ module.exports = React.createClass({
         series: React.PropTypes.array
     },
 
-    /**
-     * Update when series are not of same length or if last values are not the same.
-     * Inspired by code from old develop branch
-     *
-     * @param {Object} nextProps
-     * @param {Object} nextState
-     * @return {boolean}
-     */
-    shouldComponentUpdate(nextProps, nextState) {
-        var thisSeries = this.props.series;
-        var nextSeries = nextProps.series;
-
-        if (thisSeries === nextSeries) {
-            return false;
-        }
-
-        if (thisSeries && nextSeries) {
-            if (nextSeries.length !== thisSeries.length) return true;
-            var thisDataLength, nextDataLength;
-            for (var i = 0, len = thisSeries.length; i < len; ++i) {
-                thisDataLength = thisSeries[i].data.length;
-                nextDataLength = nextSeries[i].data.length;
-                if (thisDataLength !== nextDataLength) {
-                    return true;
-                }
-
-                if (thisSeries[i].data[thisDataLength-1].price !== nextSeries[i].data[nextDataLength-1].price) {
-                    return true;
-                }
-            }
-            return false;
-        }
-        return true;
-    },
+    shouldComponentUpdate: shouldComponentUpdate,
 
     render: function() {
         var p = this.props;
