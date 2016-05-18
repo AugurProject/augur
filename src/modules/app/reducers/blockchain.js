39dfc156105819ef07a0d3e47c6cd1094633f4c9
John Danz <John.Danz87@gmail.com>
2016-05-18 17:40:18 -0500

    installed eslint and air-bnb config as a starting point to testing if we want to try the eslint route. eslint marks a consideribly larger amount of errors currently. something in the ballpark of a few hundred for jshint and almost 3 thousand for eslint.

--- src/modules/app/reducers/blockchain.js
+++ src/modules/app/reducers/blockchain.js
@@ -1,14 +1,14 @@
-import { UPDATE_BLOCKCHAIN } from '../../app/actions/update-blockchain';
+import {UPDATE_BLOCKCHAIN} from '../../app/actions/update-blockchain';
 
 export default function(blockchain = {}, action) {
-    switch (action.type) {
-        case UPDATE_BLOCKCHAIN:
-            return {
-            	...blockchain,
-            	...action.data
-            };
+	switch (action.type) {
+		case UPDATE_BLOCKCHAIN:
+			return {
+				...blockchain,
+				...action.data
+			};
 
-        default:
-            return blockchain;
-    }
+		default:
+			return blockchain;
+	}
 }
