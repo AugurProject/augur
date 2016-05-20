0d8c4fcfce39dd1de8b4345e43a8383bd498bc9e
John Danz <John.Danz87@gmail.com>
2016-05-20 13:35:50 -0500

    conformed as much of broken bids-asks section to eslint

--- src/modules/bids-asks/actions/load-bids-asks.js
+++ src/modules/bids-asks/actions/load-bids-asks.js
@@ -1,51 +1,51 @@
-import * as AugurJS from '../../../services/augurjs';
+// import * as AugurJS from '../../../services/augurjs';
 
 export function loadBidsAsks(marketID) {
-    return (dispatch, getState) => {
-        var s = getState(),
-            outcomeKeys = Object.keys(s.outcomes[marketID]);
+	return (dispatch, getState) => {
+		const s = getState();
+		const outcomeKeys = Object.keys(s.outcomes[marketID]);
 
-        if (!s.bidsAsks[marketID]) {
-            makeTimedBunch(15, 1000);
-            makeTimedBunch(5, 5000);
-            makeTimedBunch(5, 10000);
-            makeTimedBunch(5, 15000);
-            makeTimedBunch(5, 20000);
-        }
+		function randomInt(min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
 
-        function makeTimedBunch(num, millis) {
-            num = num || randomInt(1, 10);
-            millis = millis || randomInt(500, 10000);
+		function makeBidAsk() {
+			return {
+				id: Math.random(),
+				marketID,
+				outcomeID: outcomeKeys[randomInt(0, outcomeKeys.length - 1)],
+				accountID: Math.random(),
+				action: Math.random() >= 0.7 && 'placed' || 'canceled',
+				bidOrAsk: Math.random() >= 0.7 && BID || ASK,
+				numShares: Math.round(Math.random() * 10000) / 100,
+				limitPrice: Math.round(Math.random() * 100) / 100,
+			};
+		}
 
-            setTimeout(() => {
-                dispatch(updateBidsAsks(makeBunch(num)));
-                //makeTimedBunch();
-            }, millis);
-        }
+		function makeBunch(num) {
+			const o = {};
+			for (let i = 0; i < num; i++) {
+				o[Math.random()] = makeBidAsk();
+			}
+			return o;
+		}
 
-        function makeBunch(num) {
-            var o = {};
-            for (var i = 0; i < num; i++) {
-                o[Math.random()] = makeBidAsk();
-            }
-            return o;
-        }
+		function makeTimedBunch(num = randomInt(1, 10), millis = randomInt(500, 10000)) {
+			// num = num || ;
+			// millis = millis || ;
 
-        function makeBidAsk() {
-            return {
-                id: Math.random(),
-                marketID: marketID,
-                outcomeID: outcomeKeys[randomInt(0, outcomeKeys.length - 1)],
-                accountID: Math.random(),
-                action: Math.random() >= 0.7 && 'placed' || 'canceled',
-                bidOrAsk: Math.random() >= 0.7 && BID || ASK,
-                numShares: Math.round(Math.random() * 10000) / 100,
-                limitPrice: Math.round(Math.random() * 100) / 100,
-            };
-        }
+			setTimeout(() => {
+				dispatch(updateBidsAsks(makeBunch(num)));
+				// makeTimedBunch();
+			}, millis);
+		}
 
-        function randomInt(min, max) {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-    };
+		if (!s.bidsAsks[marketID]) {
+			makeTimedBunch(15, 1000);
+			makeTimedBunch(5, 5000);
+			makeTimedBunch(5, 10000);
+			makeTimedBunch(5, 15000);
+			makeTimedBunch(5, 20000);
+		}
+	};
 }
