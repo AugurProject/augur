20b5e5aea055c1bc2f62e5f89b02bc17add846f8
John Danz <John.Danz87@gmail.com>
2016-05-20 20:43:31 -0500

    almost done wiht reports, issues with actions/xxx.js which im not sure is even used right now

--- src/modules/reports/actions/close-markets.js
+++ src/modules/reports/actions/close-markets.js
@@ -1,22 +1,22 @@
 import * as AugurJS from '../../../services/augurjs';
-
 import { BRANCH_ID } from '../../app/constants/network';
-
 import { isMarketDataPreviousReportPeriod } from '../../../utils/is-market-data-open';
-
 import { updateMarketData } from '../../markets/actions/update-markets-data';
 
 export function closeMarkets(marketsData) {
-	return function (dispatch, getState) {
-		var { loginAccount, blockchain, branch } = getState(),
-			unparsedMarkets;
+	return (dispatch, getState) => {
+		const { loginAccount, blockchain, branch } = getState();
 
 		if (blockchain.isReportConfirmationPhase || !loginAccount.ether) {
 			return;
 		}
 
-		unparsedMarkets = Object.keys(marketsData)
-			.filter(marketID => isMarketDataPreviousReportPeriod(marketsData[marketID], blockchain.currentPeriod, branch.periodLength))
+		const unparsedMarkets = Object.keys(marketsData)
+			.filter(marketID =>
+			isMarketDataPreviousReportPeriod(
+				marketsData[marketID],
+				blockchain.currentPeriod,
+				branch.periodLength))
 			.map(marketID => marketsData[marketID]);
 
 		if (!unparsedMarkets || !unparsedMarkets.length) {
@@ -24,11 +24,16 @@ export function closeMarkets(marketsData) {
 		}
 
 		(function process() {
-			var unparsedMarket = unparsedMarkets.pop();
+			function next() {
+				if (unparsedMarkets.length) {
+					setTimeout(process, 2000);
+				}
+			}
+
+			const unparsedMarket = unparsedMarkets.pop();
 
 			// check if this market has been determined and closed
 			AugurJS.getOutcome(unparsedMarket.eventID, reportedOutcome => {
-
 				// if a reported outcome exists, that means market has been determined and closed
 				if (reportedOutcome !== '0') {
 					dispatch(updateMarketData({ id: unparsedMarket._id, isClosed: true }));
@@ -41,16 +46,10 @@ export function closeMarkets(marketsData) {
 						return next();
 					}
 					dispatch(updateMarketData({ id: unparsedMarket._id, isClosed: true }));
-console.log('------> closed market', res);
+					console.log('------> closed market', res);
 					return next();
 				});
 			});
-
-			function next() {
-				if (unparsedMarkets.length) {
-					setTimeout(process, 2000);
-				}
-			}
-		})();
+		}());
 	};
 }
--- src/modules/reports/actions/penalize-wrong-reports.js
+++ src/modules/reports/actions/penalize-wrong-reports.js
@@ -1,40 +1,41 @@
 import * as AugurJS from '../../../services/augurjs';
 import { isMarketDataPreviousReportPeriod } from '../../../utils/is-market-data-open';
-
 import { BRANCH_ID } from '../../app/constants/network';
 
 export function penalizeWrongReports(marketsData) {
-	return function (dispatch, getState) {
-		var { blockchain, loginAccount } = getState(),
-			branchID = BRANCH_ID,
-			prevPeriod = blockchain.reportPeriod - 1,
-			eventIDs;
+	return (dispatch, getState) => {
+		const { blockchain, loginAccount } = getState();
+		const branchID = BRANCH_ID;
+		const prevPeriod = blockchain.reportPeriod - 1;
 
 		if (blockchain.isReportConfirmationPhase || !loginAccount.rep) {
 			return;
 		}
 
-		eventIDs = Object.keys(marketsData)
-			.filter(marketID => marketsData[marketID].eventID && !isMarketDataPreviousReportPeriod(marketsData[marketID], blockchain.currentBlockNumber))
+		const eventIDs = Object.keys(marketsData)
+			.filter(marketID => marketsData[marketID].eventID &&
+			!isMarketDataPreviousReportPeriod(
+				marketsData[marketID],
+				blockchain.currentBlockNumber
+			))
 			.map(marketID => marketsData[marketID].eventID);
 
 		(function process() {
-			var eventID = eventIDs.pop();
-			AugurJS.penalizeWrong(branchID, prevPeriod, eventID, (err, res) => {
-				if (err) {
-					console.log('ERROR penalizeWrong()', err);
-					return next();
-				}
-console.log('------> penalizeWrong', res);
-				return next();
-			});
-
 			// if there are more event ids, continue
 			function next() {
 				if (eventIDs.length) {
 					setTimeout(process, 1000);
 				}
 			}
-		})();
+			const eventID = eventIDs.pop();
+			AugurJS.penalizeWrong(branchID, prevPeriod, eventID, (err, res) => {
+				if (err) {
+					console.log('ERROR penalizeWrong()', err);
+					return next();
+				}
+				console.log('------> penalizeWrong', res);
+				return next();
+			});
+		}());
 	};
 }
