c532a2f7df0cb14e80ee472b0d459e9f1999ef3f
Stephen Sprinkle <git@sprinkle.io>
2016-05-24 14:15:36 -0700

    expose Disqus via shim

--- src/modules/common/components/disqus.jsx
+++ src/modules/common/components/disqus.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import DISQUS from 'disqus'
 
 module.exports = React.createClass({
     propTypes: {
@@ -7,42 +8,27 @@ module.exports = React.createClass({
     },
 
     componentDidMount(){
-        this.loadDisqus()
+        this.configureDisqus()
     },
 
     componentDidUpdate(){
-        this.loadDisqus()
+        this.configureDisqus()
     },
 
     render: function(){
         return <div id="disqus_thread" className="disqus-container"/>
     },
 
-    loadDisqus(){
+    configureDisqus(){
         let p = this.props
 
-        if(typeof DISQUS !== 'undefined'){ // Disqus already added, need to reset
-            DISQUS.reset({
-                reload: true,
-                config: function(){
-                    this.page.identifier = p.identifier
-                    this.page.title = p.title
-                    this.page.url = window.location.href
-                }
-            })
-        }else{
-            window['disqus_identifier'] = p.identifier
-            window['disqus_title'] = p.title
-            window['disqus_url'] = window.location.href
-
-            // Append Disqus Script
-            const SCRIPT = document.createElement('script')
-
-            SCRIPT.async = true
-            SCRIPT.type = 'text/javascript'
-            SCRIPT.src = '//augur.disqus.com/embed.js'
-
-            document.body.appendChild(SCRIPT)
-        }
+        DISQUS.reset({
+            reload: true,
+            config: function(){
+                this.page.identifier = p.identifier
+                this.page.title = p.title
+                this.page.url = window.location.href
+            }
+        })
     }
 });
