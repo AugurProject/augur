302925781c987082385afe88d8f5cf30946d1881
John Danz <John.Danz87@gmail.com>
2016-05-31 18:06:02 -0500

    commented out ui-tests due to new approach, new approach can be seen in test/app/selectors/active-page-test and test/auth/selectors/login-account-test

--- test/auth/selectors/login-account-test.js
+++ test/auth/selectors/login-account-test.js
@@ -5,13 +5,14 @@ import proxyquire from 'proxyquire';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import testState from '../../testState';
+import loginAccountAssertion from '../../../node_modules/augur-ui-react-components/test/assertions/loginAccount';
 
-let loginAccount;
+// let loginAccount;
 describe(`modules/auth/selectors/login-account.js`, () => {
 	proxyquire.noPreserveCache().noCallThru();
 	const middlewares = [thunk];
 	const mockStore = configureMockStore(middlewares);
-	let out, selector, state, store;
+	let actual, selector, state, store;
 	state = Object.assign({}, testState, {
 		loginAccount: {
 			address: '0xtest123',
@@ -26,55 +27,57 @@ describe(`modules/auth/selectors/login-account.js`, () => {
 		}
 	});
 	store = mockStore(state);
-
-	out = {
-		address: '0xtest123',
-		id: '0xtest123',
-		handle: 'testTesterson',
-		ether: {
-			value: 50,
-			formattedValue: 50,
-			formatted: '+50.00',
-			roundedValue: 50,
-			rounded: '+50',
-			minimized: '+50',
-			denomination: 'Eth',
-			full: '+50.00Eth'
-		},
-		realEther: {
-			value: 10,
-			formattedValue: 10,
-			formatted: '+10.00',
-			roundedValue: 10,
-			rounded: '+10',
-			minimized: '+10',
-			denomination: 'Eth',
-			full: '+10.00Eth'
-		},
-		rep: {
-			value: 105,
-			formattedValue: 105,
-			formatted: '+105',
-			roundedValue: 105,
-			rounded: '+105',
-			minimized: '+105',
-			denomination: 'Rep',
-			full: '+105Rep'
-		},
-		keystore: {
-			id: '0xtest123'
-		}
-	};
+	//
+	// out = {
+	// 	address: '0xtest123',
+	// 	id: '0xtest123',
+	// 	handle: 'testTesterson',
+	// 	ether: {
+	// 		value: 50,
+	// 		formattedValue: 50,
+	// 		formatted: '+50.00',
+	// 		roundedValue: 50,
+	// 		rounded: '+50',
+	// 		minimized: '+50',
+	// 		denomination: 'Eth',
+	// 		full: '+50.00Eth'
+	// 	},
+	// 	realEther: {
+	// 		value: 10,
+	// 		formattedValue: 10,
+	// 		formatted: '+10.00',
+	// 		roundedValue: 10,
+	// 		rounded: '+10',
+	// 		minimized: '+10',
+	// 		denomination: 'Eth',
+	// 		full: '+10.00Eth'
+	// 	},
+	// 	rep: {
+	// 		value: 105,
+	// 		formattedValue: 105,
+	// 		formatted: '+105',
+	// 		roundedValue: 105,
+	// 		rounded: '+105',
+	// 		minimized: '+105',
+	// 		denomination: 'Rep',
+	// 		full: '+105Rep'
+	// 	},
+	// 	keystore: {
+	// 		id: '0xtest123'
+	// 	}
+	// };
 
 	selector = proxyquire('../../../src/modules/auth/selectors/login-account', {
 		'../../../store': store
 	});
 
-	loginAccount = selector.default;
+	// loginAccount = selector.default;
 	it(`should login an account`, () => {
-		
-		assert.deepEqual(selector.default(), out, `Didn't properly update account information`);
+		actual = selector.default();
+		loginAccountAssertion(actual);
+		//
+		// assert.deepEqual(selector.default(), out, `Didn't properly update account information`);
 	});
 });
 
-export default loginAccount;
+// export default loginAccount;
