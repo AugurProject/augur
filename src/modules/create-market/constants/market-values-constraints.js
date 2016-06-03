0bb8a47dbc9bc4266ce11cbb4f7f25eab99993de
Stephen Sprinkle <git@sprinkle.io>
2016-06-03 11:46:30 -0700

    advanced constants + object inclusion

--- src/modules/create-market/constants/market-values-constraints.js
+++ src/modules/create-market/constants/market-values-constraints.js
@@ -14,6 +14,9 @@ export const RESOURCES_MAX_LENGTH = 1250;
 export const EXPIRY_SOURCE_GENERIC = 'generic';
 export const EXPIRY_SOURCE_SPECIFIC = 'specific';
 
+export const INITIAL_LIQUIDITY_MIN = 50;
+export const INITIAL_LIQUIDITY_DEFAULT = 500;
+
 export const TRADING_FEE_DEFAULT = 2;
 export const TRADING_FEE_MIN = 1;
 export const TRADING_FEE_MAX = 12.5;
@@ -22,23 +25,9 @@ export const MAKER_FEE_DEFAULT = 0.5;
 export const MAKER_FEE_MIN = 0;
 export const MAKER_FEE_MAX = 100;
 
-export const MAKER_FEES_DEFAULT = 0.5;
-export const MAKER_FEES_MIN = 0;
-export const MAKER_FEES_MAX = 100;
-
 // Advanced Market Creation Defaults
 export const INITIAL_FAIR_PRICE_DEFAULT = 46.5;
-export const INITIAL_FAIR_PRICE_MIN = 0.1;
-
-export const STARTING_QUANTITY_DEFAULT = 25;
-export const STARTING_QUANTITY_MIN = 0.1;
-
-export const BEST_STARTING_QUANTITY_DEFAULT = 35;
-export const BEST_STARTING_QUANTITY_MIN = 0.1;
-
+export const SHARES_PER_ORDER_DEFAULT = 25;
+export const SIZE_OF_BEST_DEFAULT = 35;
 export const PRICE_WIDTH_DEFAULT = 4;
-export const PRICE_WIDTH_MIN =  0.1;
-
-export const PRICE_DEPTH_DEFAULT = 0.1;
-
-export const IS_SIMULATION = false;
+export const SEPARATION_DEFAULT = 0.5;
