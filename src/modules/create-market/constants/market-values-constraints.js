ed358ddb520450eff9cedadb6cf09b9fa516ad57
Stephen Sprinkle <git@sprinkle.io>
2016-06-03 11:34:51 -0700

    updated market creation constants for congruence to augur repo

--- src/modules/create-market/constants/market-values-constraints.js
+++ src/modules/create-market/constants/market-values-constraints.js
@@ -14,11 +14,16 @@ export const RESOURCES_MAX_LENGTH = 1250;
 export const EXPIRY_SOURCE_GENERIC = 'generic';
 export const EXPIRY_SOURCE_SPECIFIC = 'specific';
 
+export const INITIAL_LIQUIDITY_MIN = 50;
+export const INITIAL_LIQUIDITY_DEFAULT = 500;
+
+export const TRADING_FEE_DEFAULT = 2;
 export const TRADING_FEE_MIN = 1;
 export const TRADING_FEE_MAX = 12.5;
 
-export const TRADING_FEE_DEFAULT = 2;
 export const MAKER_FEE_DEFAULT = 0.5;
+export const MAKER_FEE_MIN = 0;
+export const MAKER_FEE_MAX = 100;
 
 // Advanced Market Creation Defaults
 export const INITIAL_FAIR_PRICE_DEFAULT = 46.5;
@@ -26,6 +31,3 @@ export const SHARES_PER_ORDER_DEFAULT = 25;
 export const SIZE_OF_BEST_DEFAULT = 35;
 export const PRICE_WIDTH_DEFAULT = 4;
 export const SEPARATION_DEFAULT = 0.5;
-
-export const INITIAL_LIQUIDITY_MIN = 50;
-export const INITIAL_LIQUIDITY_DEFAULT = 500;
