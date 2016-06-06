16e199ca2f37861685a1e62d0c0c77abbfcf4f5e
Stephen Sprinkle <git@sprinkle.io>
2016-06-06 11:25:28 -0700

    improved validation for `initialLiquidity`

--- src/modules/create-market/selectors/form-steps/step-4.js
+++ src/modules/create-market/selectors/form-steps/step-4.js
@@ -82,7 +82,7 @@ export const validateTradingFee = (tradingFeePercent) => {
 
 	if (!tradingFeePercent)
 		return 'Please specify a trading fee %';
-	if (Number.isNaN(parsed) && Number.isFinite(parsed))
+	if (Number.isNaN(parsed) && !Number.isFinite(parsed))
 		return 'Trading fee must be a number';
 	if (parsed < TRADING_FEE_MIN || parsed > TRADING_FEE_MAX)
 		return `Trading fee must be between ${ 
@@ -108,12 +108,13 @@ export const validateMakerFee = (makerFeePercent) => {
 
 export const validateInitialLiquidity = (initialLiquidity) => {
 	const parsed = parseFloat(initialLiquidity);
-	if (!initialLiquidity) {
+
+	console.log('number check -- ', parsed, Number.isNaN(parsed), Number.isFinite(parsed))
+
+	if (!initialLiquidity)
 		return 'Please provide some initial liquidity';
-	}
-	if (parsed !== initialLiquidity) {
+	if (Number.isNaN(parsed) && !Number.isFinite(parsed))
 		return 'Initial liquidity must be numeric';
-	}
 	if (parsed < INITIAL_LIQUIDITY_MIN) {
 		return `Initial liquidity must be at least ${
 			formatEther(INITIAL_LIQUIDITY_MIN).full
