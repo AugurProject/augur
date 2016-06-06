c0d0dbd54e443884d880c9d85b90ccfbb3c49227
John Danz <John.Danz87@gmail.com>
2016-06-06 10:14:13 -0500

    added more detailed marketsTotal assertions

--- test/assertions/marketsTotals.js
+++ test/assertions/marketsTotals.js
@@ -1,82 +1,75 @@
 var assert = require('chai').assert;
 // marketsTotals:
-//  { positionsSummary:
-//     { numPositions: [Object],
-//       totalValue: [Object],
-//       gainPercent: [Object] },
-//    numPendingReports: 19 },
+// {
+// 	numAll: 1,
+// 	numFavorites: 0,
+// 	numFiltered: 0,
+// 	numPendingReports: 19,
+// 	numUnpaginated: 0,
+// 	positionsSummary:
+// 	{
+// 		gainPercent: [Object],
+// 		netChange: [Object],
+// 		numPositions: [Object],
+// 		positions: undefined/[Object],
+// 		purchasePrice: [Object],
+// 		qtyShares: [Object],
+// 		shareChange: [Object],
+// 		totalCost: [Object],
+// 		totalValue: [Object],
+// 	}
+// },
+function checkDefinedAndNumber(obj, name) {
+	assert.isDefined(obj, `marketsTotals.${name} isn't defined`);
+	assert.isNumber(obj, `marketsTotals.${name} isn't a number`);
+}
+
+function checkPositionSummaryObject(obj, name) {
+	assert.isDefined(obj, `positionsSummary.${name} isn't defined`);
+	assert.isObject(obj, `positionsSummary.${name} isn't a object`);
+	assert.isDefined(obj.value, `${name}.value isn't defined`);
+	assert.isNumber(obj.value, `${name}.value isn't a number`);
+	assert.isDefined(obj.formattedValue, `${name}.formattedValue isn't defined`);
+	assert.isNumber(obj.formattedValue, `${name}.formattedValue isn't a number`);
+	assert.isDefined(obj.formatted, `${name}.formatted isn't defined`);
+	assert.isString(obj.formatted, `${name}.formatted isn't a string`);
+	assert.isDefined(obj.roundedValue, `${name}.roundedValue isn't defined`);
+	assert.isNumber(obj.roundedValue, `${name}.roundedValue isn't a number`);
+	assert.isDefined(obj.rounded, `${name}.rounded isn't defined`);
+	assert.isString(obj.rounded, `${name}.rounded isn't a string`);
+	assert.isDefined(obj.minimized, `${name}.minimized isn't defined`);
+	assert.isString(obj.minimized, `${name}.minimized isn't a string`);
+	assert.isDefined(obj.denomination, `${name}.denomination isn't defined`);
+	assert.isString(obj.denomination, `${name}.denomination isn't a string`);
+	assert.isDefined(obj.full, `${name}.full isn't defined`);
+	assert.isString(obj.full, `${name}.full isn't a string`);
+}
+
 function marketsTotalsAssertion(actual) {
 	assert.isDefined(actual, `marketsTotals isn't defined`);
 	assert.isObject(actual, `marketsTotals isn't an object`);
+
+	checkDefinedAndNumber(actual.numAll, `numAll`);
+	checkDefinedAndNumber(actual.numFavorites, `numFavorites`);
+	checkDefinedAndNumber(actual.numFiltered, `numFiltered`);
+	checkDefinedAndNumber(actual.numPendingReports, `numPendingReports`);
+	checkDefinedAndNumber(actual.numUnpaginated, `numUnpaginated`);
+
 	assert.isDefined(actual.positionsSummary, `marketsTotals.positionsSummary isn't defined`);
 	assert.isObject(actual.positionsSummary, `marketsTotals.positionsSummary isn't an object as expected`);
-	assert.isDefined(actual.numPendingReports, `marketsTotals.numPendingReports isn't defined`);
-	assert.isNumber(actual.numPendingReports, `marketsTotals.numPendingReports isn't a number as expected.`)
 }
 
 function positionsSummaryAssertion(actual) {
 	assert.isDefined(actual, `positionsSummary isn't defined`);
 	assert.isObject(actual, `positionsSummary isn't an object`);
-
-	var numPos = actual.numPositions;
-	assert.isDefined(numPos, `positionsSummary.numPositions isn't defined`);
-	assert.isObject(numPos, `positionsSummary.numPositions isn't a object`);
-	assert.isDefined(numPos.value, `numPositions.value isn't defined`);
-	assert.isNumber(numPos.value, `numPositions.value isn't a number`);
-	assert.isDefined(numPos.formattedValue, `numPositions.formattedValue isn't defined`);
-	assert.isNumber(numPos.formattedValue, `numPositions.formattedValue isn't a number`);
-	assert.isDefined(numPos.formatted, `numPositions.formatted isn't defined`);
-	assert.isString(numPos.formatted, `numPositions.formatted isn't a string`);
-	assert.isDefined(numPos.roundedValue, `numPositions.roundedValue isn't defined`);
-	assert.isNumber(numPos.roundedValue, `numPositions.roundedValue isn't a number`);
-	assert.isDefined(numPos.rounded, `numPositions.rounded isn't defined`);
-	assert.isString(numPos.rounded, `numPositions.rounded isn't a string`);
-	assert.isDefined(numPos.minimized, `numPositions.minimized isn't defined`);
-	assert.isString(numPos.minimized, `numPositions.minimized isn't a string`);
-	assert.isDefined(numPos.denomination, `numPositions.denomination isn't defined`);
-	assert.isString(numPos.denomination, `numPositions.denomination isn't a string`);
-	assert.isDefined(numPos.full, `numPositions.full isn't defined`);
-	assert.isString(numPos.full, `numPositions.full isn't a string`);
-
-	var totVal = actual.totalValue;
-	assert.isDefined(totVal, `positionsSummary.totalValue isn't defined`);
-	assert.isObject(totVal, `positionsSummary.totalValue isn't a object`);
-	assert.isDefined(totVal.value, `totalValue.value isn't defined`);
-	assert.isNumber(totVal.value, `totalValue.value isn't a number`);
-	assert.isDefined(totVal.formattedValue, `totalValue.formattedValue isn't defined`);
-	assert.isNumber(totVal.formattedValue, `totalValue.formattedValue isn't a number`);
-	assert.isDefined(totVal.formatted, `totalValue.formatted isn't defined`);
-	assert.isString(totVal.formatted, `totalValue.formatted isn't a string`);
-	assert.isDefined(totVal.roundedValue, `totalValue.roundedValue isn't defined`);
-	assert.isNumber(totVal.roundedValue, `totalValue.roundedValue isn't a number`);
-	assert.isDefined(totVal.rounded, `totalValue.rounded isn't defined`);
-	assert.isString(totVal.rounded, `totalValue.rounded isn't a string`);
-	assert.isDefined(totVal.minimized, `totalValue.minimized isn't defined`);
-	assert.isString(totVal.minimized, `totalValue.minimized isn't a string`);
-	assert.isDefined(totVal.denomination, `totalValue.denomination isn't defined`);
-	assert.isString(totVal.denomination, `totalValue.denomination isn't a string`);
-	assert.isDefined(totVal.full, `totalValue.full isn't defined`);
-	assert.isString(totVal.full, `totalValue.full isn't a string`);
-
-	var gain = actual.gainPercent;
-	assert.isDefined(gain, `positionsSummary.gainPercent isn't defined`);
-	assert.isObject(gain, `positionsSummary.gainPercent isn't a object`);
-	assert.isDefined(gain.value, `gainPercent.value isn't defined`);
-	assert.isNumber(gain.value, `gainPercent.value isn't a number`);
-	assert.isDefined(gain.formattedValue, `gainPercent.formattedValue isn't defined`);
-	assert.isNumber(gain.formattedValue, `gainPercent.formattedValue isn't a number`);
-	assert.isDefined(gain.formatted, `gainPercent.formatted isn't defined`);
-	assert.isString(gain.formatted, `gainPercent.formatted isn't a string`);
-	assert.isDefined(gain.roundedValue, `gainPercent.roundedValue isn't defined`);
-	assert.isNumber(gain.roundedValue, `gainPercent.roundedValue isn't a number`);
-	assert.isDefined(gain.rounded, `gainPercent.rounded isn't defined`);
-	assert.isString(gain.rounded, `gainPercent.rounded isn't a string`);
-	assert.isDefined(gain.minimized, `gainPercent.minimized isn't defined`);
-	assert.isString(gain.minimized, `gainPercent.minimized isn't a string`);
-	assert.isDefined(gain.denomination, `gainPercent.denomination isn't defined`);
-	assert.isString(gain.denomination, `gainPercent.denomination isn't a string`);
-	assert.isDefined(gain.full, `gainPercent.full isn't defined`);
-	assert.isString(gain.full, `gainPercent.full isn't a string`);
+	checkPositionSummaryObject(actual.gainPercent, 'gainPercent');
+	checkPositionSummaryObject(actual.netChange, 'netChange');
+	checkPositionSummaryObject(actual.numPositions, 'numPositions');
+	checkPositionSummaryObject(actual.purchasePrice, 'purchasePrice');
+	checkPositionSummaryObject(actual.qtyShares, 'qtyShares');
+	checkPositionSummaryObject(actual.shareChange, 'shareChange');
+	checkPositionSummaryObject(actual.totalCost, 'totalCost');
+	checkPositionSummaryObject(actual.totalValue, 'totalValue');
 }
 
 module.exports = {
