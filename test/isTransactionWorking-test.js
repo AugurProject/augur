d127bd89d2b24c839af787be4c005f9aa6c28039
John Danz <John.Danz87@gmail.com>
2016-06-06 16:29:25 -0500

    fixed searchSort assertion, started favoriteMarkets assertion, but doesnt appear to be in use...

--- test/isTransactionWorking-test.js
+++ test/isTransactionWorking-test.js
@@ -1,10 +1,10 @@
 import selectors from '../src/selectors';
-import isTransactionWorkingAssertion from './assertions/isTransactionWorking';
+import isTransactionsWorkingAssertion from './assertions/isTransactionsWorking';
 
 describe(`selectors.isTransactionsWorking tests:`, () => {
 	// isTransactionsWorking: Boolean,
 	it(`should contain a isTransactionsWorking boolean`, () => {
 		let actual = selectors.isTransactionsWorking;
-		isTransactionWorkingAssertion(actual);
+		isTransactionsWorkingAssertion(actual);
 	});
 });
