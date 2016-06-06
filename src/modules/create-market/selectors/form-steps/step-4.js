b0661ae764ad1f856862e1ed61285c562647a9ee
Stephen Sprinkle <git@sprinkle.io>
2016-06-06 11:19:29 -0700

    augmented validation for liquidity to more clearly relate to input name

--- src/modules/create-market/selectors/form-steps/step-4.js
+++ src/modules/create-market/selectors/form-steps/step-4.js
@@ -107,7 +107,7 @@ export const validateMakerFee = (makerFeePercent) => {
 			formatPercent(MAKER_FEE_MAX, true).full}`
 };
 
-export const validateMarketInvestment = (initialLiquidity) => {
+export const validateInitialLiquidity = (initialLiquidity) => {
 	const parsed = parseFloat(initialLiquidity);
 
 	if (!initialLiquidity)
@@ -154,7 +154,7 @@ export const validatePriceWidth = (priceWidth) => {
 export const isValid = (formState) => {
 	if(	validateTradingFee(formState.tradingFeePercent) 				||
 		validateMakerFee(formState.makerFeePercent) 					||
-		validateMarketInvestment(formState.initialLiquidity)			||
+		validateInitialLiquidity(formState.initialLiquidity)			||
 		validateStartingQuantity(formState.startingQuantity)			||
 		validateBestStartingQuantity(formState.bestStartingQuantity)	||
 		validatePriceWidth(formState.priceWidth))
@@ -170,7 +170,7 @@ export const errors = (formState) => {
 		errs.tradingFeePercent = validateTradingFee(formState.tradingFeePercent);
 
 	if (formState.initialLiquidity !== undefined)
-		errs.initialLiquidity = validateMarketInvestment(formState.initialLiquidity);
+		errs.initialLiquidity = validateInitialLiquidity(formState.initialLiquidity);
 
 	return errs;
 };
