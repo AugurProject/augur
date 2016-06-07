80ec06a5eb7491620aedae3edd9b0e61b0b92eb3
Stephen Sprinkle <git@sprinkle.io>
2016-06-06 17:13:33 -0700

    update to the createSingleEventMarket submitted object

--- src/services/augurjs.js
+++ src/services/augurjs.js
@@ -343,8 +343,6 @@ ex.loadPriceHistory = function loadPriceHistory(marketID, cb) {
 };
 
 ex.createMarket = function createMarket(branchId, newMarket, cb) {
-	console.log('AugurJS -- createMarket -- ', newMarket);
-
 	augur.createSingleEventMarket({
 		description: newMarket.description,
 		expDate: newMarket.endDate.value.getTime() / 1000,
