4c84b9ce562f67236735990b11ba95692ed69e1d
Baz <bk@thinkloop.com>
2016-06-08 10:23:21 +0000

    clarified format-number

--- src/modules/auth/selectors/login-account.js
+++ src/modules/auth/selectors/login-account.js
@@ -5,8 +5,8 @@ export default function () {
 	const { loginAccount } = store.getState();
 	return {
 		...loginAccount,
-		rep: formatRep(loginAccount.rep || 0, { zero: true, decimalsRounded: 0 }),
-		ether: formatEther(loginAccount.ether || 0, { zero: true, decimalsRounded: 0 }),
-		realEther: formatEther(loginAccount.realEther || 0, { zero: true, decimalsRounded: 0 })
+		rep: formatRep(loginAccount.rep, { zeroStyled: false, decimalsRounded: 0 }),
+		ether: formatEther(loginAccount.ether, { zeroStyled: false, decimalsRounded: 0 }),
+		realEther: formatEther(loginAccount.realEther, { zeroStyled: false, decimalsRounded: 0 })
 	};
 }
--- src/utils/format-number.js
+++ src/utils/format-number.js
@@ -1,5 +1,98 @@
-// import memoizerific from 'memoizerific';
 import { MILLIS_PER_BLOCK } from '../modules/app/constants/network';
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	Produces a formatted number object used for display and possibly calculations
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+The main function is `formatNumber`, however there are top-level functions that wrap for common cases like `formatEther`, `formatShares`, etc.
+
+A formatted number generally has three parts: the sign (+ or -), the stylized number, and a denomination (Eth, Rep, %, etc.)
+
+The formatted number object that is returned looks something like this:
+	{
+		value: the parsed number in numerical form, 0 if a bad input was passed in, can be used in calculations
+
+		formattedValue: the value in numerical form, possibly rounded, can be used in calculations
+		formatted: the value in string form with possibly additional formatting, like comma separator, used for display
+
+		o.roundedValue: the value in numerical form, with extra rounding, can be used in calculations
+		o.rounded: the value in string form, with extra rounding and possibly additional formatting, like comma separator, used for display
+
+		o.minimized: the value in string form, with trailing 0 decimals omitted, for example if the `formatted` value is 1.00, this minimized value would be 1
+	}
+
+The reason the number object has multiple states of rounding simultaneously,
+is because the ui can use it for multiple purposes. For example, when showing ether,
+we generally like to show it with 2 decimals, however when used in totals,
+maximum precision is not necessary, and we can opt to show the `rounded` display, which is only 1 decimal.
+Similar logic applies for `minimized`, sometimes we don't need to be consistent with the decimals
+and just show the prettiest, smallest representation of the value.
+
+The options object that is passed into `formatNumber` that enables all of this looks like:
+	{
+		decimals: the number of decimals for the precise case, can be 0-infinity
+		decimalsRounded: the number of decimals for the prettier case, can be 0-infinity
+		denomination: the string denomination of the number (ex. Eth, Rep, %), can be blank
+		positiveSign: boolean whether to include a plus sign at the beginning of positive numbers
+		zeroStyled: boolean, if true, when the value is 0, it formates it as a dash (-) instead
+	}
+*/
+
+export function formatRep(num, opts) {
+	return formatNumber(
+		num,
+		{
+			decimals: 0,
+			decimalsRounded: 0,
+			denomination: 'Rep',
+			positiveSign: true,
+			zeroStyled: false,
+			...opts
+		}
+	);
+}
+
+export function formatEther(num, opts) {
+	return formatNumber(
+		num,
+		{
+			decimals: 2,
+			decimalsRounded: 1,
+			denomination: 'Eth',
+			positiveSign: true,
+			zeroStyled: false,
+			...opts
+		}
+	);
+}
+
+export function formatShares(num, opts) {
+	return formatNumber(
+		num,
+		{
+			decimals: 2,
+			decimalsRounded: 0,
+			denomination: 'Shares',
+			minimized: true,
+			zeroStyled: false,
+			...opts
+		}
+	);
+}
+
+export function formatPercent(num, opts) {
+	return formatNumber(
+		num,
+		{
+			decimals: 1,
+			decimalsRounded: 0,
+			denomination: '%',
+			positiveSign: true,
+			zeroStyled: false,
+			...opts
+		}
+	);
+}
 
 export function formatNone() {
 	return {
@@ -14,50 +107,28 @@ export function formatNone() {
 	};
 }
 
-export function formatNumber(
-num,
-opts = {
-	decimals: 0,
-	decimalsRounded: 0,
-	denomination: '',
-	positiveSign: false,
-	zero: true,
-	minimized: false
-}
-) {
+export function formatNumber(num, opts = { decimals: 0, decimalsRounded: 0, denomination: '', positiveSign: false, zeroStyled: true, minimized: false }) {
 	const { minimized } = opts;
 	const o = {};
-	let { decimals, decimalsRounded, denomination, positiveSign, zero } = opts;
+	let { value, decimals, decimalsRounded, denomination, positiveSign, zeroStyled } = opts;
 
 	decimals = decimals || 0;
 	decimalsRounded = decimalsRounded || 0;
 	denomination = denomination || '';
 	positiveSign = !!positiveSign;
-	zero = zero !== false;
+	zeroStyled = zeroStyled !== false;
+	value = parseFloat(num) || 0;
 
-	if (!num && !zero) {
+	if (!value && zeroStyled) {
 		return formatNone();
 	}
 
-	o.value = parseFloat(num) || 0;
-
-	o.formattedValue = Math.round(num * Math.pow(10, decimals)) / Math.pow(10, decimals);
-	o.formatted = o.formattedValue.toLocaleString(
-		undefined,
-		{ minimumFractionDigits: decimals, maximumFractionDigits: decimals }
-	);
-
-	o.roundedValue =
-	Math.round(num * Math.pow(10, decimalsRounded)) / Math.pow(10, decimalsRounded);
-	o.rounded = o.roundedValue.toLocaleString(
-		undefined,
-		{ minimumFractionDigits: decimalsRounded, maximumFractionDigits: decimalsRounded }
-	);
-
-	o.minimized = o.formattedValue.toLocaleString(
-		undefined,
-		{ minimumFractionDigits: 0, maximumFractionDigits: decimals }
-	);
+	o.value = value;
+	o.formattedValue = Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals);
+	o.formatted = o.formattedValue.toLocaleString(undefined, { minimumFractionDigits: decimals, maximumFractionDigits: decimals });
+	o.roundedValue = Math.round(value * Math.pow(10, decimalsRounded)) / Math.pow(10, decimalsRounded);
+	o.rounded = o.roundedValue.toLocaleString(undefined, { minimumFractionDigits: decimalsRounded, maximumFractionDigits: decimalsRounded });
+	o.minimized = o.formattedValue.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: decimals });
 
 	if (positiveSign) {
 		if (o.formattedValue > 0) {
@@ -79,61 +150,9 @@ opts = {
 	return o;
 }
 
-export function formatRep(num, opts) {
-	return formatNumber(
-		num,
-		{
-			decimals: 0,
-			decimalsRounded: 0,
-			denomination: 'Rep',
-			positiveSign: true,
-			...opts
-		}
-	);
-}
-
-export function formatEther(num, opts) {
-	return formatNumber(
-		num,
-		{
-			decimals: 2,
-			decimalsRounded: 1,
-			denomination: 'Eth',
-			positiveSign: true,
-			...opts
-		}
-	);
-}
-
-export function formatShares(num, opts) {
-	return formatNumber(
-		num,
-		{
-			decimals: 2,
-			decimalsRounded: 0,
-			denomination: 'Shares',
-			minimized: true,
-			...opts
-		}
-	);
-}
-
-export function formatPercent(num, opts) {
-	return formatNumber(
-		num,
-		{
-			decimals: 1,
-			decimalsRounded: 0,
-			denomination: '%',
-			positiveSign: true,
-			...opts
-		}
-	);
-}
-
 export function makeDateFromBlock(currentBlock, startBlock, startBlockMillisSinceEpoch) {
 	const millis = (currentBlock - startBlock) * MILLIS_PER_BLOCK;
-	const	currentMillisSinceEpoch = startBlockMillisSinceEpoch + millis;
+	const currentMillisSinceEpoch = startBlockMillisSinceEpoch + millis;
 	return new Date(currentMillisSinceEpoch);
 }
 
