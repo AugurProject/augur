665de8b84ab1afa14d8066e2d5ff349fedc02b7a
John Danz <John.Danz87@gmail.com>
2016-06-10 12:11:12 -0500

    removed ui-tests and selectors test, we are going to sidestep the large selector for now because we need more discussion to make a better plan. new style is updated for auth-form-test.js

--- test/auth/selectors/auth-form-test.js
+++ test/auth/selectors/auth-form-test.js
@@ -1,26 +1,130 @@
-// import {
-// 	assert
-// } from 'chai';
 import proxyquire from 'proxyquire';
-// import configureMockStore from 'redux-mock-store';
-// import thunk from 'redux-thunk';
-// import testState from '../../testState';
 import * as mockStore from '../../mockStore';
-// import {
-// 	INVALID_USERNAME_OR_PASSWORD,
-// 	USERNAME_REQUIRED,
-// 	PASSWORDS_DO_NOT_MATCH,
-// 	PASSWORD_TOO_SHORT,
-// 	USERNAME_TAKEN
-// } from '../../../src/modules/auth/constants/form-errors';
-// import authFormAssertion from '../../../node_modules/augur-ui-react-components/test/assertions/authForm';
+import {
+	INVALID_USERNAME_OR_PASSWORD,
+	USERNAME_REQUIRED,
+	PASSWORDS_DO_NOT_MATCH,
+	PASSWORD_TOO_SHORT,
+	USERNAME_TAKEN
+} from '../../../src/modules/auth/constants/form-errors';
+import {
+	assertions
+} from 'augur-ui-react-components';
 
-let { store, state } = mockStore.default;
-proxyquire.noPreserveCache().noCallThru();
-const authForm = proxyquire('../../../src/modules/auth/selectors/auth-form', {
+describe('authForm selectors test: ', () => {
+	proxyquire.noPreserveCache().noCallThru();
+	let {
+		store,
+		state
+	} = mockStore.default;
+	const authForm = proxyquire('../../../src/modules/auth/selectors/auth-form', {
 		'../../../store': store
 	});
-export default { authForm, state };
+
+	beforeEach(() => {
+		store.clearActions();
+	});
+	afterEach(() => {
+		store.clearActions();
+	});
+
+	it(`should handle a register form`, () => {
+		state.auth = {
+			err: null,
+			selectedAuthType: 'register'
+		};
+		// console.log(authForm.default());
+		assertions.authForm(authForm.default());
+	});
+
+	it(`should handle a login form`, () => {
+		state.auth = {
+			err: null,
+			selectedAuthType: 'login'
+		};
+		// console.log(authForm.default());
+		assertions.authForm(authForm.default());
+	});
+
+	it(`should handle a unrecognized authType`, () => {
+		state.auth = {
+			err: null,
+			selectedAuthType: 'some incorrect value'
+		};
+		// console.log(authForm.default());
+		assertions.authForm(authForm.default());
+	});
+
+	it(`should handle invalid user name or password`, () => {
+		state.auth = {
+			err: {
+				code: INVALID_USERNAME_OR_PASSWORD
+			},
+			selectedAuthType: 'login'
+		};
+		// console.log(authForm.default());
+		assertions.authForm(authForm.default());
+	});
+
+	it(`should handle username missing`, () => {
+		state.auth = {
+			err: {
+				code: USERNAME_REQUIRED
+			},
+			selectedAuthType: 'login'
+		};
+		// console.log(authForm.default());
+		assertions.authForm(authForm.default());
+	});
+
+	it(`should handle passwords not matching`, () => {
+		state.auth = {
+			err: {
+				code: PASSWORDS_DO_NOT_MATCH
+			},
+			selectedAuthType: 'login'
+		};
+		// console.log(authForm.default());
+		assertions.authForm(authForm.default());
+	});
+
+	it(`should handle passwords that are too short`, () => {
+		state.auth = {
+			err: {
+				code: PASSWORD_TOO_SHORT,
+				message: 'password is too short!'
+			},
+			selectedAuthType: 'login'
+		};
+		// console.log(authForm.default());
+		assertions.authForm(authForm.default());
+	});
+
+	it(`should handle taken usernames`, () => {
+		state.auth = {
+			err: {
+				code: USERNAME_TAKEN
+			},
+			selectedAuthType: 'login'
+		};
+		// console.log(authForm.default());
+		assertions.authForm(authForm.default());
+	});
+
+	it(`should handle unrecognized errors gracefully`, () => {
+		state.auth = {
+			err: {
+				code: 'unrecognized error',
+				message: 'some error message'
+			},
+			selectedAuthType: 'login'
+		};
+		// console.log(authForm.default());
+		assertions.authForm(authForm.default());
+	});
+});
+
+// export default { authForm, state };
 // describe(`modules/auth/selectors/auth-form.js`, () => {
 // 	proxyquire.noPreserveCache().noCallThru();
 // 	// const middlewares = [thunk];
