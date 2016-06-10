a356e19cbe9724671282e2edbedb434a5b334a8b
Stephen Sprinkle <git@sprinkle.io>
2016-06-10 09:32:04 -0700

    improved number validation for scalar values

--- src/modules/market/validators/validate-scalar-big-num.js
+++ src/modules/market/validators/validate-scalar-big-num.js
@@ -1,9 +1,10 @@
 export default function (scalarSmallNum, scalarBigNum) {
 	const parsedBig = parseFloat(scalarBigNum);
+
 	if (!scalarBigNum) {
 		return 'Please provide a maximum value';
 	}
-	if (parsedBig !== scalarBigNum) {
+	if (Number.isNaN(parsedBig) && !Number.isFinite(parsedBig)) {
 		return 'Maximum value must be a number';
 	}
 	if (parseFloat(scalarSmallNum) === scalarSmallNum && parsedBig <= parseFloat(scalarSmallNum)) {
