d63a0174c2cdb385d4554dc22e622ef99f96ede3
Stephen Sprinkle <git@sprinkle.io>
2016-06-14 09:35:01 -0700

    stubbed out tests for generate-order-book action

---
+++ test/create-market/actions/generate-order-book-test.js
@@ -0,0 +1,8 @@
+import {
+    assert
+} from 'chai';
+
+describe('modules/create-market/actions/generate-order-book.js', () => {
+    it('[TODO] should be able to submit a request to generate an order book');
+    it('[TODO] should be able to generate an order book');
+});
