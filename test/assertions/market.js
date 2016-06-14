03f4320291266669e6fa54a09c54cbefd39e9ab5
priecint <tp-dev@seznam.cz>
2016-06-14 13:39:02 +0200

    #4 Add assertion

--- test/assertions/market.js
+++ test/assertions/market.js
@@ -69,6 +69,9 @@ function marketAssertion(actual) {
 
 	assert.isDefined(actual.report, `market.report isn't defined`);
 	assert.isObject(actual.report, `market.report isn't an object`);
+
+	assert.isDefined(actual.orderBook, `market.orderBook isn't defined`);
+	assert.isObject(actual.orderBook, `market.orderBook isn't an object`);
 }
 // tradingFeePercent: {
 // 	value: Number,
