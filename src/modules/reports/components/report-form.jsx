b82462e1804f55d308f67d6528c9e26e08dec687
John Danz <John.Danz87@gmail.com>
2016-06-15 18:05:34 -0500

    linted the reports components. this includes the first conversion to a class component

--- src/modules/reports/components/report-form.jsx
+++ src/modules/reports/components/report-form.jsx
@@ -1,52 +1,61 @@
 import React from 'react';
 import classnames from 'classnames';
-
 import Checkbox from '../../common/components/checkbox';
 
-module.exports = React.createClass({
-	propTypes: {
+export default class ReportForm extends React.Component {
+	static propTypes = {
 		reportableOutcomes: React.PropTypes.array,
 		reportedOutcomeID: React.PropTypes.any,
 		isUnethical: React.PropTypes.bool,
 		isReported: React.PropTypes.bool,
 		onClickSubmit: React.PropTypes.func
-	},
+	};
 
-	getInitialState: function() {
-		return {
-			reportedOutcomeID: this.props.reportedOutcomeID,
-			isUnethical: this.props.isUnethical,
-			isReported: this.props.isReported
+	constructor(props) {
+		super(props);
+		this.state = {
+			reportedOutcomeID: props.reportedOutcomeID,
+			isUnethical: props.isUnethical,
+			isReported: props.isReported
 		};
-	},
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleOutcomeChange = this.handleOutcomeChange.bind(this);
+	}
 
-	componentWillReceiveProps: function(nextProps) {
+	componentWillReceiveProps(nextProps) {
 		if (nextProps.isReported !== this.state.isReported) {
 			this.setState({ isReported: nextProps.isReported });
 		}
-	},
+	}
 
-	render: function() {
-		var p = this.props,
-			s = this.state;
+	handleOutcomeChange = (e) => this.setState({ reportedOutcomeID: e.target.value });
+
+	handleSubmit() {
+		this.props.onClickSubmit(this.state.reportedOutcomeID, this.state.isUnethical);
+		this.setState({ reportedOutcomeID: null, isUnethical: null, isReported: false });
+	}
+
+	render() {
+		const p = this.props;
+		const	s = this.state;
 
 		return (
-			<section className={ classnames('report-form', { 'reported': s.isReported }) }>
+			<section className={classnames('report-form', { reported: s.isReported })}>
 				<div ref="outcomeOptions" className="outcome-options">
-					<h4>{ !s.isReported ? 'Report the outcome' : 'Outcome Reported' }</h4>
+					<h4>{!s.isReported ? 'Report the outcome' : 'Outcome Reported'}</h4>
 
-					{ (p.reportableOutcomes || []).map(outcome => (
-						<label key={ outcome.id } className={ classnames('outcome-option', { 'disabled': s.isReported }) }>
+					{(p.reportableOutcomes || []).map(outcome => (
+						<label key={outcome.id} className={classnames('outcome-option', { disabled: s.isReported })}>
 							<input
 								type="radio"
 								className="outcome-option-radio"
 								name="outcome-option-radio"
-								value={ outcome.id }
-								checked={ s.reportedOutcomeID === outcome.id }
-								disabled={ s.isReported }
-								onChange={ this.handleOutcomeChange }
-								/>
-							{ outcome.name }
+								value={outcome.id}
+								checked={s.reportedOutcomeID === outcome.id}
+								disabled={s.isReported}
+								onChange={this.handleOutcomeChange}
+							/>
+							{outcome.name}
 						</label>
 					))}
 				</div>
@@ -56,37 +65,35 @@ module.exports = React.createClass({
 
 					<Checkbox
 						ref="unethical"
-						className={ classnames('unethical-checkbox', { 'disabled': s.isReported }) }
+						className={classnames('unethical-checkbox', { disabled: s.isReported })}
 						text="Yes, this question is unethical"
-						isChecked={ !!s.isUnethical }
-						onClick={ !s.isReported && (() => this.setState({ isUnethical: !s.isUnethical })) || null } />
+						isChecked={!!s.isUnethical}
+						onClick={!s.isReported && (() => this.setState({ isUnethical: !s.isUnethical })) || null}
+					/>
 
 					<span className="unethical-message">
 						The consensus answer to this question will be over-ridden if the question is reported as unethical by 60% (or more) of those reporting this market.
 					</span>
 				</div>
 
-				{ !s.isReported &&
+				{!s.isReported &&
 					<button
 						className="button report"
-						disabled={ !s.reportedOutcomeID }
-						onClick={ !!s.reportedOutcomeID && !s.isReported && this.handleSubmit || null }>Submit Report</button>
+						disabled={!s.reportedOutcomeID}
+						onClick={!!s.reportedOutcomeID && !s.isReported && this.handleSubmit || null}
+					>
+						Submit Report
+					</button>
 				}
-				{ s.isReported &&
+				{s.isReported &&
 					<button
 						className="button report-again"
-						onClick={ () => this.setState({ isReported: false }) }>Report Again</button>
+						onClick={() => this.setState({ isReported: false })}
+					>
+						Report Again
+					</button>
 				}
 			</section>
 		);
-	},
-
-	handleOutcomeChange: function(e) {
-		return this.setState({ reportedOutcomeID: e.target.value });
-	},
-
-	handleSubmit: function() {
-		this.props.onClickSubmit(this.state.reportedOutcomeID, this.state.isUnethical);
-		this.setState({ reportedOutcomeID: null, isUnethical: null, isReported: false });
 	}
-});
+}
--- src/modules/reports/components/report-panel.jsx
+++ src/modules/reports/components/report-panel.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import classnames from 'classnames';
-
 import ReportForm from '../../reports/components/report-form';
 
-module.exports = React.createClass({
-	propTypes: {
-		className: React.PropTypes.string,
-		numPendingReports: React.PropTypes.number,
-		outcomes: React.PropTypes.array,
-		reportedOutcomeID: React.PropTypes.any,
-		isUnethical: React.PropTypes.bool,
-		isReported: React.PropTypes.bool,
-		isReportSubmitted: React.PropTypes.bool,
-		onSubmitReport: React.PropTypes.func
-	},
+const ReportPanel = (props) => {
+	const p = this.props;
+	return (
+		<section className={classnames('report-panel', p.className)}>
+			<span className="num-total-reports">{p.numPendingReports}</span>
+			<ReportForm
+				{...p}
+				isReported={p.isReported || p.isReportSubmitted}
+				onClickSubmit={p.onSubmitReport}
+			/>
+		</section>
+	);
+};
 
-	render: function() {
-		var p = this.props;
-		return (
-			<section className={ classnames('report-panel', p.className) }>
-				<span className="num-total-reports">{ p.numPendingReports }</span>
-				<ReportForm
-					{...p }
-					isReported={ p.isReported || p.isReportSubmitted }
-					onClickSubmit={ p.onSubmitReport } />
-			</section>
-		);
-	}
-});
+ReportPanel.propTypes = {
+	className: React.PropTypes.string,
+	numPendingReports: React.PropTypes.number,
+	outcomes: React.PropTypes.array,
+	reportedOutcomeID: React.PropTypes.any,
+	isUnethical: React.PropTypes.bool,
+	isReported: React.PropTypes.bool,
+	isReportSubmitted: React.PropTypes.bool,
+	onSubmitReport: React.PropTypes.func
+};
+
+export default ReportPanel;
