a0e849b5f94abfc37a876b4af42efc73cb003894
John Danz <John.Danz87@gmail.com>
2016-06-15 12:25:45 -0500

    started linting files, made minor changes to the lint rc

--- src/utils/get.js
+++ src/utils/get.js
@@ -1,5 +1,6 @@
-export function get(obj, target){
-    return target.split(".").reduce(function(o, x) {
-        return (typeof o == "undefined" || o === null) ? o : o[x];
-    }, obj);
+export function get(obj, target) {
+	return target.split('.').reduce((o, x) => {
+		const value = (typeof o === undefined || o === null) ? o : o[x];
+		return value;
+	}, obj);
 }
