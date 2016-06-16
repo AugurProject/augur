e3e59ba728244f3fe1a0ce81c666bdd859fd206c
John Danz <John.Danz87@gmail.com>
2016-06-16 13:44:50 -0500

    linted common

--- src/modules/common/components/input.jsx
+++ src/modules/common/components/input.jsx
@@ -1,88 +1,95 @@
-import React from 'react';
+import { React, Component, PropTypes } from 'react';
 import classnames from 'classnames';
 import shouldComponentUpdatePure from '../../../utils/should-component-update-pure';
 
-module.exports = React.createClass({
-	propTypes: {
-		type: React.PropTypes.string,
-		className: React.PropTypes.string,
-		value: React.PropTypes.any,
-		isMultiline: React.PropTypes.bool,
-		isClearable: React.PropTypes.bool,
-		debounceMS: React.PropTypes.number,
-		onChange: React.PropTypes.func
-	},
+export default class Input extends Component {
+	static propTypes = {
+		type: PropTypes.string,
+		className: PropTypes.string,
+		value: PropTypes.any,
+		isMultiline: PropTypes.bool,
+		isClearable: PropTypes.bool,
+		debounceMS: PropTypes.number,
+		onChange: PropTypes.func
+	};
 
-    getInitialState: function() {
-    	return {
-    		value: this.props.value || '',
-    		timeoutID: ''
-    	};
-    },
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: this.props.value || '',
+			timeoutID: ''
+		};
+		this.shouldComponentUpdate = shouldComponentUpdatePure;
+		this.componentWillReceiveProps = (nextProps) => {
+			if ((nextProps.value || nextProps.value === 0) && nextProps.value !== this.state.value && nextProps.value !== this.props.value) {
+				this.setState({ value: nextProps.value });
+			}
+		};
+		this.handleOnChange = this.handleOnChange.bind(this);
+		this.handleOnBlur = this.handleOnChange.bind(this);
+		this.handleClear = this.handleClear.bind(this);
+		this.sendValue = this.sendValue.bind(this);
+	}
 
-    componentWillReceiveProps: function(nextProps) {
-    	if ((nextProps.value || nextProps.value === 0) && nextProps.value !== this.state.value && nextProps.value !== this.props.value) {
-    		this.setState({ value: nextProps.value });
-    	}
-    },
-
-    shouldComponentUpdate: shouldComponentUpdatePure,
-
-	render() {
-		var p = this.props,
-			s = this.state;
-
-		return (
-			<div className={ classnames('input', { 'clearable': p.isClearable !== false }, this.props.className) }>
-				{ !p.isMultiline &&
-					<input { ...p }
-							className="box"
-							value={ s.value }
-							onChange={ this.handleOnChange }
-							onBlur={ this.handleOnBlur } />
-				}
-
-				{ p.isMultiline &&
-					<textarea { ...p }
-							className="box"
-							value={ s.value }
-							onChange={ this.handleOnChange }
-							onBlur={ this.handleOnBlur } />
-				}
-
-				{ !p.isMultiline && p.isClearable !== false &&
-					<button className="clear"
-							onClick={ this.handleClear }>&#xf00d;</button>
-				}
-			</div>
-		);
-	},
-
-	handleOnChange: function (e) {
-		var newValue = e.target.value;
+	handleOnChange = (e) => {
+		const newValue = e.target.value;
 		if (this.props.debounceMS !== 0) {
 			clearTimeout(this.state.timeoutID);
 			this.setState({ timeoutID: setTimeout(() => this.sendValue(newValue), this.props.debounceMS || 750) });
-		}
-		else {
+		} else {
 			this.sendValue(newValue);
 		}
 		this.setState({ value: newValue });
-	},
+	}
 
-	handleOnBlur: function() {
+	handleOnBlur = () => {
 		if (this.props.debounceMS !== 0) {
 			clearTimeout(this.state.timeoutID);
 			this.sendValue(this.state.value);
 		}
-	},
+	}
 
-	handleClear: function() {
+	handleClear = () => {
 		this.setState({ value: '' });
 		this.sendValue('');
-	},
+	}
 
-	sendValue: function(value) {
+	sendValue = (value) => {
 		this.props.onChange(value);
 	}
-});
+
+	render() {
+		const p = this.props;
+		const s = this.state;
+
+		return (
+			<div className={classnames('input', { clearable: p.isClearable !== false }, this.props.className)}>
+				{!p.isMultiline &&
+					<input
+						{...p}
+						className="box"
+						value={s.value}
+						onChange={this.handleOnChange}
+						onBlur={this.handleOnBlur}
+					/>
+				}
+
+				{p.isMultiline &&
+					<textarea
+						{...p}
+						className="box"
+						value={s.value}
+						onChange={this.handleOnChange}
+						onBlur={this.handleOnBlur}
+					/>
+				}
+
+				{!p.isMultiline && p.isClearable !== false &&
+					<button className="clear" onClick={this.handleClear}>
+						&#xf00d;
+					</button>
+				}
+			</div>
+		);
+	}
+}
--- src/modules/common/components/value-denomination.jsx
+++ src/modules/common/components/value-denomination.jsx
@@ -1,31 +1,29 @@
-import React from 'react';
+import { React, PropTypes } from 'react';
 import classnames from 'classnames';
 
-const ValueDenomination = React.createClass({
-	propTypes: {
-		className: React.PropTypes.string,
-		value: React.PropTypes.number,
-		formattedValue: React.PropTypes.number,
-		formatted: React.PropTypes.string,
-		denomination: React.PropTypes.string
-	},
+const ValueDenomination = (props) => {
+	const p = this.props;
+	return (
+		<span
+			className={classnames('value-denomination', p.className, { positive: p.formattedValue > 0, negative: p.formattedValue < 0 })}
+			title={`${p.title}: ${p.value} ${p.denomination}`}
+		>
+			{(p.formatted) &&
+				<span className="value">{p.formatted}</span>
+			}
+			{p.denomination &&
+				<span className="denomination">{p.denomination}</span>
+			}
+		</span>
+	);
+};
 
-	render: function() {
-	    var p = this.props;
-		return (
-			<span
-				className={ classnames('value-denomination', p.className, { 'positive': p.formattedValue > 0, 'negative': p.formattedValue < 0 }) }
-				title={`${p.title}: ${p.value} ${p.denomination}`}>
+ValueDenomination.propTypes = {
+	className: PropTypes.string,
+	value: PropTypes.number,
+	formattedValue: PropTypes.number,
+	formatted: PropTypes.string,
+	denomination: PropTypes.string
+};
 
-				{ (p.formatted) &&
-					<span className="value">{ p.formatted }</span>
-				}
-				{ p.denomination &&
-					<span className="denomination">{ p.denomination }</span>
-				}
-			</span>
-		);
-	}
-});
-
-module.exports = ValueDenomination;
+export default ValueDenomination;
