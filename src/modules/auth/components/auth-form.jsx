f23112971f645c67eb394d1b99982dbf8c9769dc
John Danz <John.Danz87@gmail.com>
2016-06-16 15:33:08 -0500

    auth linted

--- src/modules/auth/components/auth-form.jsx
+++ src/modules/auth/components/auth-form.jsx
@@ -1,48 +1,42 @@
-import React from 'react';
+import { React, Component, PropTypes } from 'react';
 import classnames from 'classnames';
 import Link from '../../link/components/link';
 
-module.exports = React.createClass({
-	propTypes: {
-		className: React.PropTypes.string,
+export default class AuthForm extends Component {
+	static propTypes = {
+		className: PropTypes.string,
+		title: PropTypes.string,
+		passwordPlaceholder: PropTypes.string,
+		password2Placeholder: PropTypes.string,
+		isVisibleUsername: PropTypes.bool,
+		isVisiblePassword: PropTypes.bool,
+		isVisiblePassword2: PropTypes.bool,
+		clearUsername: PropTypes.bool,
+		clearPassword: PropTypes.bool,
+		clearCode: PropTypes.bool,
+		msg: PropTypes.string,
+		msgClass: PropTypes.string,
+		topLinkText: PropTypes.string,
+		topLink: PropTypes.object,
+		botttomLinkText: PropTypes.string,
+		botttomLink: PropTypes.object,
+		closeLink: PropTypes.object,
+		submitButtonText: PropTypes.string,
+		submitButtonClass: PropTypes.string,
+		onSubmit: PropTypes.func
+	};
 
-		title: React.PropTypes.string,
-		passwordPlaceholder: React.PropTypes.string,
-		password2Placeholder: React.PropTypes.string,
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.state = { msg: this.props.msg };
+	}
 
-		isVisibleUsername: React.PropTypes.bool,
-		isVisiblePassword: React.PropTypes.bool,
-		isVisiblePassword2: React.PropTypes.bool,
-
-		clearUsername: React.PropTypes.bool,
-		clearPassword: React.PropTypes.bool,
-		clearCode: React.PropTypes.bool,
-
-		msg: React.PropTypes.string,
-		msgClass: React.PropTypes.string,
-
-		topLinkText: React.PropTypes.string,
-		topLink: React.PropTypes.object,
-
-		botttomLinkText: React.PropTypes.string,
-		botttomLink: React.PropTypes.object,
-
-		closeLink: React.PropTypes.object,
-
-		submitButtonText: React.PropTypes.string,
-        submitButtonClass: React.PropTypes.string,
-		onSubmit: React.PropTypes.func
-	},
-
-	getInitialState: function() {
-		return { msg: this.props.msg };
-	},
-
-	componentWillReceiveProps: function(nextProps) {
+	componentWillReceiveProps(nextProps) {
 		this.setState({ msg: nextProps.msg });
-	},
+	}
 
-	componentDidUpdate: function() {
+	componentDidUpdate() {
 		if (this.props.clearUsername) {
 			this.refs.username.value = '';
 		}
@@ -53,80 +47,87 @@ module.exports = React.createClass({
 		if (this.props.clearCode) {
 			this.refs.code.value = '';
 		}
-	},
+	}
 
-	render: function() {
-		var p =this.props;
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.setState({ msg: undefined });
+		setTimeout(() =>
+			this.props.onSubmit(
+				this.refs.username && this.refs.username.value,
+				this.refs.password && this.refs.password.value,
+				this.refs.password2 && this.refs.password2.value
+			), 100);
+	}
+
+	render() {
+		const p = this.props;
 
 		return (
-			<form className={ p.className } onSubmit={ this.handleSubmit }>
+			<form className={p.className} onSubmit={this.handleSubmit}>
 				<h1 className="title">
-					{ p.title }
-
-					{ p.topLinkText &&
-	                    <Link
-	                    	className="top-link"
-	                    	href={ p.topLink.href }
-	                    	onClick={ p.topLink.onClick }>{ p.topLinkText }</Link>
+					{p.title}
+					{p.topLinkText &&
+						<Link
+							className="top-link"
+							href={p.topLink.href}
+							onClick={p.topLink.onClick}
+						>
+							{p.topLinkText}
+						</Link>
 					}
 				</h1>
-
-				{ this.state.msg &&
-					<span className={ classnames('msg', p.msgClass) }>{ this.state.msg }</span>
+				{this.state.msg &&
+					<span className={classnames('msg', p.msgClass)}>
+						{this.state.msg}
+					</span>
 				}
-
 				<input
 					ref="username"
-					className={ classnames('auth-input', { 'displayNone': !p.isVisibleUsername }) }
+					className={classnames('auth-input', { displayNone: !p.isVisibleUsername })}
 					type="text"
 					placeholder="name"
 					maxLength="30"
-					autoFocus="autofocus" />
-
+					autoFocus="autofocus"
+				/>
 				<input
 					ref="password"
-					className={ classnames('auth-input', { 'displayNone': !p.isVisiblePassword }) }
+					className={classnames('auth-input', { displayNone: !p.isVisiblePassword })}
 					type="password"
-					placeholder={ p.passwordPlaceholder || 'password' }
-					maxLength="256" />
-
+					placeholder={p.passwordPlaceholder || 'password'}
+					maxLength="256"
+				/>
 				<input
 					ref="password2"
-					className={ classnames('auth-input', { 'displayNone': !p.isVisiblePassword2 }) }
+					className={classnames('auth-input', { displayNone: !p.isVisiblePassword2 })}
 					type="password"
-					placeholder={ p.password2Placeholder || 'confirm password' }
-					maxLength="256" />
-
-				{ p.bottomLinkText &&
-                    <Link
-                    	className="bottom-link"
-                    	href={ p.bottomLinkHref }
-                    	onClick={ p.onClickBottomLink }>{ p.bottomLinkText }</Link>
+					placeholder={p.password2Placeholder || 'confirm password'}
+					maxLength="256"
+				/>
+				{p.bottomLinkText &&
+					<Link
+						className="bottom-link"
+						href={p.bottomLinkHref}
+						onClick={p.onClickBottomLink}
+					>
+						{p.bottomLinkText}
+					</Link>
 				}
-
 				<input
-					className={ classnames('button', 'submit-button', p.submitButtonClass) }
+					className={classnames('button', 'submit-button', p.submitButtonClass)}
 					type="submit"
-					value={ p.submitButtonText } />
-
+					value={p.submitButtonText}
+				/>
 				<Link
 					type="button"
 					className="button x-button"
 					title="Close"
-					href={ p.closeLink.href }
-					onClick={ p.closeLink.onClick }>&#xf057;</Link>
+					href={p.closeLink.href}
+					onClick={p.closeLink.onClick}
+				>
+					&#xf057;
+				</Link>
 			</form>
 		);
-	},
-
-	handleSubmit: function(e) {
-	    e.preventDefault();
-	    this.setState({ msg: undefined });
-	    setTimeout(() =>
-		    this.props.onSubmit(
-		    	this.refs.username && this.refs.username.value,
-		    	this.refs.password && this.refs.password.value,
-		    	this.refs.password2 && this.refs.password2.value
-		    ), 100);
 	}
-});
+}
--- src/modules/auth/constants/auth-types.js
+++ src/modules/auth/constants/auth-types.js
@@ -3,9 +3,9 @@ export const LOGIN = 'login';
 export const LOGOUT = 'logout';
 
 export const AUTH_TYPES = {
-    [REGISTER]: REGISTER,
-    [LOGIN]: LOGIN,
-    [LOGOUT]: LOGOUT
+	[REGISTER]: REGISTER,
+	[LOGIN]: LOGIN,
+	[LOGOUT]: LOGOUT
 };
 
 export const DEFAULT_AUTH_TYPE = REGISTER;
