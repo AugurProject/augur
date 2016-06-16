8be77a2679eb49f3094c66584f72d0e07da49f76
John Danz <John.Danz87@gmail.com>
2016-06-16 11:42:16 -0500

    linted filters

--- src/modules/filters/components/filters.jsx
+++ src/modules/filters/components/filters.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import classnames from 'classnames';
-
+import { React, PropTypes } from 'react';
+// import classnames from 'classnames';
 import Checkbox from '../../common/components/checkbox';
 
-module.exports = React.createClass({
-    propTypes: {
-        filters: React.PropTypes.array
-    },
+const Filters = (props) => {
+	const p = this.props;
+	return (
+		<aside className="filters">
+			{p.filters.map(filter =>
+				<div key={filter.title} className="filters-group">
+					<span className="title">{filter.title}</span>
+					{filter.options.map(option =>
+						<Checkbox key={option.value} className="filter" text={option.name} isChecked={option.isSelected} onClick={option.onClick} />
+					)}
+				</div>
+			)}
+		</aside>
+	);
+};
 
-    render: function() {
-        var p = this.props;
-        return (
-            <aside className="filters">
-                { p.filters.map(filter =>
-                    <div key={ filter.title } className="filters-group">
-                        <span className="title">{ filter.title }</span>
-                        { filter.options.map(option =>
-                            <Checkbox key={ option.value } className="filter" text={ option.name } isChecked={ option.isSelected } onClick={ option.onClick } />
-                        )}
-                    </div>
-                )}
-            </aside>
-        );
-    }
-});
+Filters.propTypes = {
+	filters: PropTypes.array
+};
+
+export default Filters;
