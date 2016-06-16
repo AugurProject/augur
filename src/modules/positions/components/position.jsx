3d78836943019ba8364ddbee8a36481f6ed59d3c
John Danz <John.Danz87@gmail.com>
2016-06-16 09:24:52 -0500

    positions linted

--- src/modules/positions/components/position.jsx
+++ src/modules/positions/components/position.jsx
@@ -1,65 +1,65 @@
 import React from 'react';
-import classnames from 'classnames';
-
+// import classnames from 'classnames';
 import ValueDenomination from '../../common/components/value-denomination';
 
-module.exports = React.createClass({
-    propTypes: {
-		name: React.PropTypes.string,
-		qtyShares: React.PropTypes.object,
-		totalValue: React.PropTypes.object,
-		gainPercent: React.PropTypes.object,
-		lastPrice: React.PropTypes.object,
-		purchasePrice: React.PropTypes.object,
-		shareChange: React.PropTypes.object,
-		totalCost: React.PropTypes.object,
-		netChange: React.PropTypes.object
-    },
+const Position = (props) => {
+	const p = this.props;
+	return (
+		<div className="position">
+			<div className="main-group">
+				<span className="position-name">{p.name}</span>
+				<ValueDenomination {...p.qtyShares} />
+				<div className="position-pair gain-percent">
+					<ValueDenomination
+						{...p.gainPercent}
+						formatted={p.gainPercent.minimized}
+					/>
+					&nbsp;
+					<span className="title">{p.gainPercent.value > 0 ? 'gain' : 'loss'}</span>
+				</div>
+			</div>
+			<div className="position-group">
+				<div className="position-pair per-share-gain">
+					<span className="title">per share gain/loss</span>
+					<ValueDenomination {...p.shareChange} />
+				</div>
+				<div className="position-pair last-price">
+					<span className="title">last trade price</span>
+					<ValueDenomination {...p.lastPrice} />
+				</div>
+				<div className="position-pair purchase-price">
+					<span className="title">avg. purchase price</span>
+					<ValueDenomination {...p.purchasePrice} />
+				</div>
+			</div>
+			<div className="position-group">
+				<div className="position-pair net-change">
+					<span className="title">net gain/loss</span>
+					<ValueDenomination {...p.netChange} />
+				</div>
+				<div className="position-pair total-value">
+					<span className="title">total value</span>
+					<ValueDenomination {...p.totalValue} />
+				</div>
+				<div className="position-pair total-cost">
+					<span className="title">total cost</span>
+					<ValueDenomination {...p.totalCost} />
+				</div>
+			</div>
+		</div>
+	);
+};
 
-    render: function() {
-        var p = this.props;
-        return (
-            <div className="position">
-                <div className="main-group">
-                    <span className="position-name">{ p.name }</span>
-                    <ValueDenomination { ...p.qtyShares } />
-                    <div className="position-pair gain-percent">
-                        <ValueDenomination
-                            { ...p.gainPercent }
-                            formatted={ p.gainPercent.minimized } />
-                        &nbsp;
-                        <span className="title">{ p.gainPercent.value > 0 ? 'gain' : 'loss' }</span>
-                    </div>
-                </div>
-                <div className="position-group">
-                    <div className="position-pair per-share-gain">
-                        <span className="title">per share gain/loss</span>
-                        <ValueDenomination { ...p.shareChange } />
-                    </div>
-                    <div className="position-pair last-price">
-                        <span className="title">last trade price</span>
-                        <ValueDenomination { ...p.lastPrice } />
-                    </div>
-                    <div className="position-pair purchase-price">
-                        <span className="title">avg. purchase price</span>
-                        <ValueDenomination { ...p.purchasePrice } />
-                    </div>
-                </div>
-                <div className="position-group">
-                    <div className="position-pair net-change">
-                        <span className="title">net gain/loss</span>
-                        <ValueDenomination { ...p.netChange } />
-                    </div>
-                    <div className="position-pair total-value">
-                        <span className="title">total value</span>
-                        <ValueDenomination { ...p.totalValue } />
-                    </div>
-                    <div className="position-pair total-cost">
-                        <span className="title">total cost</span>
-                        <ValueDenomination { ...p.totalCost } />
-                    </div>
-                </div>
-            </div>
-        );
-    }
-});
+Position.propTypes = {
+	name: React.PropTypes.string,
+	qtyShares: React.PropTypes.object,
+	totalValue: React.PropTypes.object,
+	gainPercent: React.PropTypes.object,
+	lastPrice: React.PropTypes.object,
+	purchasePrice: React.PropTypes.object,
+	shareChange: React.PropTypes.object,
+	totalCost: React.PropTypes.object,
+	netChange: React.PropTypes.object
+};
+
+export default Position;
