d7733a9065c32b76f49591a62c4e1dc8b35e98e1
John Danz <John.Danz87@gmail.com>
2016-06-16 09:55:30 -0500

    completed linting markets

--- src/modules/markets/components/search-sort.jsx
+++ src/modules/markets/components/search-sort.jsx
@@ -1,43 +1,44 @@
 import React from 'react';
-import classnames from 'classnames';
-
+// import classnames from 'classnames';
 import Input from '../../common/components/input';
 import DropDown from '../../common/components/dropdown';
 
-module.exports = React.createClass({
-	propTypes: {
-		keywords: React.PropTypes.string,
+const SearchSort = (props) => {
+	const p = this.props;
+	return (
+		<div className="search-sort">
+			<div className="search-sort-content">
+				<Input className="search-bar" value={p.keywords} placeholder="Search" onChange={p.onChangeKeywords} />
 
-		selectedSort: React.PropTypes.object,
-		sortOptions: React.PropTypes.array,
+				<div className="sort-container">
+					<span className="title">Sort By</span>
 
-		onChangeKeywords: React.PropTypes.func,
-		onChangeSort: React.PropTypes.func
-	},
+					<DropDown
+						className="sort"
+						selected={p.sortOptions.find(opt => opt.value === p.selectedSort.prop)}
+						options={p.sortOptions}
+						onChange={(prop) => { const sortOption = p.sortOptions.find(opt => opt.value === prop); p.onChangeSort(sortOption.value, sortOption.isDesc); }}
+					/>
 
-	render: function() {
-		var p = this.props;
-		return (
-			<div className="search-sort">
-				<div className="search-sort-content">
-					<Input className="search-bar" value={ p.keywords } placeholder="Search" onChange={ p.onChangeKeywords } />
-
-					<div className="sort-container">
-						<span className="title">Sort By</span>
-
-						<DropDown
-							className="sort"
-							selected={ p.sortOptions.find(opt => opt.value === p.selectedSort.prop) }
-							options={ p.sortOptions }
-							onChange={ (prop) => { var sortOption = p.sortOptions.find(opt => opt.value === prop); p.onChangeSort(sortOption.value, sortOption.isDesc) }} />
-
-						<button
-							className="sort-direction-button"
-							title={ p.selectedSort.isDesc ? 'descending selected' : 'ascending selected' }
-							onClick={ () => p.onChangeSort(p.selectedSort.prop, !p.selectedSort.isDesc) }>{ p.selectedSort.isDesc ? <span>&#xf161;</span> : <span>&#xf160;</span> }</button>
-					</div>
+					<button
+						className="sort-direction-button"
+						title={p.selectedSort.isDesc ? 'descending selected' : 'ascending selected'}
+						onClick={() => p.onChangeSort(p.selectedSort.prop, !p.selectedSort.isDesc)}
+					>
+						{p.selectedSort.isDesc ? <span>&#xf161;</span> : <span>&#xf160;</span>}
+					</button>
 				</div>
 			</div>
-		);
-	}
-});
+		</div>
+	);
+};
+
+SearchSort.propTypes = {
+	keywords: React.PropTypes.string,
+	selectedSort: React.PropTypes.object,
+	sortOptions: React.PropTypes.array,
+	onChangeKeywords: React.PropTypes.func,
+	onChangeSort: React.PropTypes.func
+};
+
+export default SearchSort;
