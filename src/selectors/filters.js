7dab66b6a97ca5db71388ca2167cab7fc9bb249c
Baz <bk@thinkloop.com>
2016-06-18 05:28:52 +0000

    updated filters to improve tags and remove other filters

--- src/selectors/filters.js
+++ src/selectors/filters.js
@@ -1,21 +1,5 @@
 export default [
 	{
-		title: 'Status',
-		options: [
-			{ name: 'Open', value: 'Open', numMatched: 145, isSelected: false, onClick: () => {} },
-			{ name: 'Expired', value: 'Expired', numMatched: 124, isSelected: false, onClick: () => {} },
-			{ name: 'Reported / Missed', value: 'Reported / Missed', numMatched: 70, isSelected: false, onClick: () => {} }
-		]
-	},
-	{
-		title: 'Type',
-		options: [
-			{ name: 'Yes / No', value: 'Yes / No', numMatched: 145, isSelected: false, onClick: () => {} },
-			{ name: 'Categorical', value: 'Categorical', numMatched: 124, isSelected: false, onClick: () => {} },
-			{ name: 'Numerical', value: 'Numerical', numMatched: 70, isSelected: false, onClick: () => {} }
-		]
-	},
-	{
 		title: 'Tags',
 		options: [
 			{ name: 'Sports', value: 'Sports', numMatched: 88, isSelected: false, onClick: () => {} },
@@ -25,7 +9,45 @@ export default [
 			{ name: 'Geography', value: 'Geography', numMatched: 53, isSelected: false, onClick: () => {} },
 			{ name: 'Technology', value: 'Technology', numMatched: 24, isSelected: false, onClick: () => {} },
 			{ name: 'Entertainment', value: 'Entertainment', numMatched: 22, isSelected: false, onClick: () => {} },
-			{ name: 'Weather', value: 'Weather', numMatched: 14, isSelected: false, onClick: () => {} }
+			{ name: 'Weather', value: 'Weather', numMatched: 14, isSelected: false, onClick: () => {} },
+			{ name: 'Xssad', value: 'Xssad', numMatched: 14, isSelected: false, onClick: () => {} },
+			{ name: 'Hgfffd', value: 'Hgfffd', numMatched: 13, isSelected: false, onClick: () => {} },
+			{ name: 'DSds', value: 'DSds', numMatched: 13, isSelected: false, onClick: () => {} },
+			{ name: 'YTYsds', value: 'YTYsds', numMatched: 13, isSelected: false, onClick: () => {} },
+			{ name: 'RDESaa', value: 'RDESaa', numMatched: 13, isSelected: false, onClick: () => {} },
+			{ name: 'GGdsds', value: 'GGdsds', numMatched: 13, isSelected: false, onClick: () => {} },
+			{ name: 'Gsdsdss', value: 'Gsdsdss', numMatched: 12, isSelected: false, onClick: () => {} },
+			{ name: 'ddsds', value: 'ddsds', numMatched: 11, isSelected: false, onClick: () => {} },
+			{ name: 'dsds', value: 'dsds', numMatched: 10, isSelected: false, onClick: () => {} },
+			{ name: 'dsdsds', value: 'dsdsds', numMatched: 9, isSelected: false, onClick: () => {} },
+			{ name: 'dsdsds', value: 'dsdsds', numMatched: 8, isSelected: false, onClick: () => {} },
+			{ name: 'fdfd', value: 'fdfd', numMatched: 8, isSelected: false, onClick: () => {} },
+			{ name: 'dsdsds', value: 'dsdsds', numMatched: 7, isSelected: false, onClick: () => {} },
+			{ name: 'Wsdsccd', value: 'Wsdsccd', numMatched: 6, isSelected: false, onClick: () => {} },
+			{ name: 'vccvcv', value: 'vccvcv', numMatched: 6, isSelected: false, onClick: () => {} },
+			{ name: 'kfghkhgd', value: 'kfghkhgd', numMatched: 5, isSelected: false, onClick: () => {} },
+			{ name: 'hgdshgssh', value: 'hgdshgssh', numMatched: 5, isSelected: false, onClick: () => {} },
+			{ name: 'qqqqqq', value: 'qqqqqq', numMatched: 4, isSelected: false, onClick: () => {} },
+			{ name: 'aasass', value: 'aasass', numMatched: 4, isSelected: false, onClick: () => {} },
+			{ name: 'rtrrret', value: 'rtrrret', numMatched: 4, isSelected: false, onClick: () => {} },
+			{ name: 'utyuyyu', value: 'utyuyyu', numMatched: 4, isSelected: false, onClick: () => {} },
+			{ name: 'mnmvmn', value: 'mnmvmn', numMatched: 3, isSelected: false, onClick: () => {} },
+			{ name: 'nvccvbcbv', value: 'nvccvbcbv', numMatched: 3, isSelected: false, onClick: () => {} },
+			{ name: 'xvcxvcx', value: 'xvcxvcx', numMatched: 3, isSelected: false, onClick: () => {} },
+			{ name: 'bvvbvb', value: 'bvvbvb', numMatched: 3, isSelected: false, onClick: () => {} },
+			{ name: 'aaaaddd', value: 'aaaaddd', numMatched: 2, isSelected: false, onClick: () => {} },
+			{ name: 'iuttui', value: 'iuttui', numMatched: 2, isSelected: false, onClick: () => {} },
+			{ name: 'vxcvxcv', value: 'vxcvxcv', numMatched: 2, isSelected: false, onClick: () => {} },
+			{ name: 'xswxws', value: 'xswxws', numMatched: 2, isSelected: false, onClick: () => {} },
+			{ name: 'cececed', value: 'cececed', numMatched: 2, isSelected: false, onClick: () => {} },
+			{ name: 'bgtbgtbtg', value: 'bgtbgtbtg', numMatched: 1, isSelected: false, onClick: () => {} },
+			{ name: 'nhynhy', value: 'nhynhy', numMatched: 1, isSelected: false, onClick: () => {} },
+			{ name: 'zqzaqzaq', value: 'zqzaqzaq', numMatched: 1, isSelected: false, onClick: () => {} },
+			{ name: 'mkumkmm', value: 'mkumkmm', numMatched: 1, isSelected: false, onClick: () => {} },
+			{ name: 'nyhbfdfg', value: 'nyhbfdfg', numMatched: 1, isSelected: false, onClick: () => {} },
+			{ name: 'lhghkhgk', value: 'lhghkhgk', numMatched: 1, isSelected: false, onClick: () => {} },
+			{ name: 'fsdfsd', value: 'fsdfsd', numMatched: 1, isSelected: false, onClick: () => {} },
+			{ name: 'poupoop', value: 'poupoop', numMatched: 1, isSelected: false, onClick: () => {} }
 		]
 	}
 ];
--- test/assertions/filters.js
+++ test/assertions/filters.js
@@ -4,66 +4,12 @@ var assert = require('chai').assert;
 // 	{ title: 'Type', options: [ Object, ... ] },
 // 	{ title: 'Tags', options: [ Object, ... ] } ],
 function filtersAssertion(actual) {
-	var status = actual[0];
-	var type = actual[1];
-	var tags = actual[2];
+	var tags = actual[0];
 
 	assert.isDefined(actual, `filters isn't defined`);
 	assert.isArray(actual, `filters isn't an array`);
-	assert.equal(actual.length, 3, `filters array isn't the expected length`);
-	assert.isObject(status, `filters[0] isn't an object`);
-	assert.isObject(type, `filters[1] isn't an object`);
-	assert.isObject(tags, `filters[2] isn't an object`);
-
-	// Status object tests
-	assert.isDefined(status, `filters[0] isn't defined`);
-	assert.isObject(status, `filters[0] isn't an object`);
-	assert.isDefined(status.title, `filters[0].title isn't defined`);
-	assert.isString(status.title, `filters[0].title isn't a string`);
-	assert.equal(status.title, 'Status', `filters[0].title should equal 'Status'`);
-	assert.isDefined(status.options, `filters[0].options isn't defined`);
-	assert.isArray(status.options, `filters[0].options isn't an array`);
-	assert.isDefined(status.options[0], `filters[0].options[0] isn't defined`);
-	assert.isObject(status.options[0], `filters[0].options[0] isn't an object`);
-
-	// Status Options Tests
-	assert.isDefined(status.options[0], `[0].options[0] isn't defined`);
-	assert.isObject(status.options[0], `[0].options[0] isn't a object`);
-	assert.isDefined(status.options[0].name, `[0].options[0].name isn't defined`);
-	assert.isString(status.options[0].name, `[0].options[0].name isn't a string`);
-	assert.isDefined(status.options[0].value, `[0].options[0].value isn't defined`);
-	assert.isString(status.options[0].value, `[0].options[0].value isn't a string`);
-	assert.isDefined(status.options[0].numMatched, `[0].options[0].numMatched isn't defined`);
-	assert.isNumber(status.options[0].numMatched, `[0].options[0].numMatched isn't a number`);
-	assert.isDefined(status.options[0].isSelected, `[0].options[0].isSelected isn't defined`);
-	assert.isBoolean(status.options[0].isSelected, `[0].options[0].isSelected isn't a boolean`);
-	assert.isDefined(status.options[0].onClick, `[0].options[0].onClick isn't defined`);
-	assert.isFunction(status.options[0].onClick, `[0].options[0].onClick isn't a function`);
-
-	// Type object tests
-	assert.isDefined(type, `filters[0] isn't defined`);
-	assert.isObject(type, `filters[0] isn't an object`);
-	assert.isDefined(type.title, `filters[0].title isn't defined`);
-	assert.isString(type.title, `filters[0].title isn't a string`);
-	assert.equal(type.title, 'Type', `filters[0].title should equal 'Status'`);
-	assert.isDefined(type.options, `filters[0].options isn't defined`);
-	assert.isArray(type.options, `filters[0].options isn't an array`);
-	assert.isDefined(type.options[0], `filters[0].options[0] isn't defined`);
-	assert.isObject(type.options[0], `filters[0].options[0] isn't an object`);
-
-	// Type Options Tests
-	assert.isDefined(type.options[0], `[0].options[0] isn't defined`);
-	assert.isObject(type.options[0], `[0].options[0] isn't a object`);
-	assert.isDefined(type.options[0].name, `[0].options[0].name isn't defined`);
-	assert.isString(type.options[0].name, `[0].options[0].name isn't a string`);
-	assert.isDefined(type.options[0].value, `[0].options[0].value isn't defined`);
-	assert.isString(type.options[0].value, `[0].options[0].value isn't a string`);
-	assert.isDefined(type.options[0].numMatched, `[0].options[0].numMatched isn't defined`);
-	assert.isNumber(type.options[0].numMatched, `[0].options[0].numMatched isn't a number`);
-	assert.isDefined(type.options[0].isSelected, `[0].options[0].isSelected isn't defined`);
-	assert.isBoolean(type.options[0].isSelected, `[0].options[0].isSelected isn't a boolean`);
-	assert.isDefined(type.options[0].onClick, `[0].options[0].onClick isn't defined`);
-	assert.isFunction(type.options[0].onClick, `[0].options[0].onClick isn't a function`);
+	assert.equal(actual.length, 1, `filters array isn't the expected length`);
+	assert.isObject(tags, `filters[0] isn't an object`);
 
 	// Tags object tests
 	assert.isDefined(tags, `filters[0] isn't defined`);
