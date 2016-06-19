821c3ab946c8131a93bd4d3c6a2f8ff011b8bc38
priecint <tp-dev@seznam.cz>
2016-06-19 19:15:01 +0200

    Typo

--- test/app/selectors/active-page-test.js
+++ test/app/selectors/active-page-test.js
@@ -5,7 +5,7 @@ import selector from '../../../src/modules/app/selectors/active-page';
 import {assertions} from 'augur-ui-react-components';
 
 describe(`modules/app/selectors/active-page.js`, () => {
-	it(`should get activepage from store`, () => {
+	it(`should get active page from store`, () => {
 		let actual = selector();
 		assertions.activePage(actual);
 	});
