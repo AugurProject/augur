678a278a01cb4cf7c0628cb40e7c6fa1f3b566ba
Baz <bk@thinkloop.com>
2016-06-20 17:46:42 +0000

    fixed lint

--- src/components.jsx
+++ src/components.jsx
@@ -5,5 +5,8 @@ const components = {
 	App,
 	assertions
 };
-
 export default components;
+
+// also adding this notation to allow for importing specific pieces: import { App } from '...';
+export { App, assertions };
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import selectors from './selectors';
 const appElement = document.getElementById('app');
-import components from './components';
+import { App } from './components';
 
 window.selectors = selectors;
 console.log('********************************************* \n DEVELOPMENT MODE \n window.selectors available \n ********************************************* \n');
 
-selectors.render = () => components.App(appElement, selectors);
+selectors.render = () => App(appElement, selectors); // eslint-disable-line new-cap
 selectors.render();
