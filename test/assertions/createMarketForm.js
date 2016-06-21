4ad9925660e5a0cc41b23d2b91ffec9267f81406
Stephen Sprinkle <git@sprinkle.io>
2016-06-21 11:53:33 -0700

    step-2 createMarketForm assertions

--- test/assertions/createMarketForm.js
+++ test/assertions/createMarketForm.js
@@ -1,7 +1,7 @@
 import { assert } from 'chai';
 
 export function step1(actual){
-	describe('createMarketForm step 1 component assertions', () => {
+	describe('augur-ui-react-components createMarketForm step-1', () => {
 		it('should receive step and be a number', () => {
 			assert.isDefined(actual.step, 'step is not defined');
 			assert.isNumber(actual.step, 'step is not a number');
@@ -21,5 +21,70 @@ export function step1(actual){
 			assert.isDefined(actual.onValuesUpdated, 'onValuesUpdated is not defined');
 			assert.isFunction(actual.onValuesUpdated, 'onValuesUpdated is not a function');
 		});
+	});
+}
+
+export function step2(actual){
+	describe(`augur-ui-react-components createMarketForm step-2 ${actual.type} market state`, () => {
+		it('should receive type and be a string', () => {
+			assert.isDefined(actual.type, 'type is not defined');
+			assert.isString(actual.type, 'type is not a string');
+		});
+
+		it('should receive initialFairPrices and be an object', () => {
+			assert.isDefined(actual.initialFairPrices, 'initialFairPrices is not defined');
+			assert.isObject(actual.initialFairPrices, 'initialFairPrices is not an object');
+		});
+
+		it('should receive the proper shape within initialFairPrices', () => {
+			assert.isDefined(actual.initialFairPrices.type, 'initialFairPrices.type is not defined');
+			assert.isString(actual.initialFairPrices.type, 'initialFairPrices.type is not a string');
+
+			assert.isDefined(actual.initialFairPrices.values, 'initialFairPrices.values is not defined');
+			assert.isArray(actual.initialFairPrices.values, 'initialFairPrices.values is not an array');
+
+			assert.isDefined(actual.initialFairPrices.raw, 'initialFairPrices.raw is not defined');
+			assert.isArray(actual.initialFairPrices.raw, 'initialFairPrices.raw is not an array');
+		});
+
+		it('should receive descriptionPlaceholder and be a string', () => {
+			assert.isDefined(actual.descriptionPlaceholder, 'descriptionPlaceholder is not defined');
+			assert.isString(actual.descriptionPlaceholder, 'descriptionPlaceholder is not a string');
+		});
+		
+		it('should receive descriptionMaxLength and be a number', () => {
+			assert.isDefined(actual.descriptionMaxLength, 'descriptionMaxLength is not defined');
+			assert.isNumber(actual.descriptionMaxLength, 'descriptionMaxLength is not a number');
+		});
+
+		switch(actual.type){
+		case 'categorical':
+			it('should receive categoricalOutcomesMinNum and be a number', () => {
+				assert.isDefined(actual.categoricalOutcomesMinNum, 'categoricalOutcomesMinNum is not defined');
+				assert.isNumber(actual.categoricalOutcomesMinNum, 'categoricalOutcomesMinNum is not a number');
+			});
+
+			it('should receive categoricalOutcomesMaxNum and be a number', () => {
+				assert.isDefined(actual.categoricalOutcomesMaxNum, 'categoricalOutcomesMaxNum is not defined');
+				assert.isNumber(actual.categoricalOutcomesMaxNum, 'categoricalOutcomesMaxNum is not a number');
+			});
+
+			it('should receive categoricalOutcomeMaxLength and be a number', () => {
+				assert.isDefined(actual.categoricalOutcomeMaxLength, 'categoricalOutcomeMaxLength is not defined');
+				assert.isNumber(actual.categoricalOutcomeMaxLength, 'categoricalOutcomeMaxLength is not a number');
+			});
+			break;
+		case 'scalar':
+			it('should receive scalarSmallNum and be a number', () => {
+				assert.isDefined(actual.scalarSmallNum, 'scalarSmallNum is not defined');
+				assert.isNumber(actual.scalarSmallNum, 'scalarSmallNum is not a number');
+			});
+
+			it('should receive scalarBigNum and be a number', () => {
+				assert.isDefined(actual.scalarBigNum, 'scalarBigNum is not defined');
+				assert.isNumber(actual.scalarBigNum, 'scalarBigNum is not a number');
+			});
+			break;
+		}
 	});
 }
