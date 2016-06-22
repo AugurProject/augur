cb1ea6a98c9b707e1d11b3c0725169ef7b4b9f24
Jack Peterson <jack@tinybike.net>
2016-06-22 02:16:19 -0700

    Exclude test/core/batch from CI

--- test/core/batch.js
+++ test/core/batch.js
@@ -9,51 +9,55 @@ var assert = require("chai").assert;
 var tools = require("../tools");
 var augur = tools.setup(require("../../src"), process.argv.slice(2));
 
-var wsUrl = augur.rpc.wsUrl;
-augur.rpc.wsUrl = null;
+if (process.env.AUGURJS_INTEGRATION_TESTS) {
 
-describe("Batch", function () {
+    var wsUrl = augur.rpc.wsUrl;
+    augur.rpc.wsUrl = null;
 
-    it("batch(depositEther, reputationFaucet)", function (done) {
-        this.timeout(tools.TIMEOUT);
-        var count = 0;
-        var batch = augur.createBatch();
-        augur.tx.depositEther.send = false;
-        augur.tx.depositEther.returns = "number";
-        augur.tx.depositEther.value = 1;
-        augur.tx.reputationFaucet.send = false;
-        augur.tx.reputationFaucet.returns = "number";
-        batch.add("depositEther", [], function (r) {
-            assert(r === "1" || r === "-1");
-            if ((++count) === 2) done();
+    describe("Batch", function () {
+
+        it("batch(depositEther, reputationFaucet)", function (done) {
+            this.timeout(tools.TIMEOUT);
+            var count = 0;
+            var batch = augur.createBatch();
+            augur.tx.depositEther.send = false;
+            augur.tx.depositEther.returns = "number";
+            augur.tx.depositEther.value = 1;
+            augur.tx.reputationFaucet.send = false;
+            augur.tx.reputationFaucet.returns = "number";
+            batch.add("depositEther", [], function (r) {
+                assert(r === "1" || r === "-1");
+                if ((++count) === 2) done();
+            });
+            batch.add("reputationFaucet", [augur.branches.dev], function (r) {
+                assert.strictEqual(r, "1");
+                if ((++count) === 2) done();
+            });
+            batch.execute();
         });
-        batch.add("reputationFaucet", [augur.branches.dev], function (r) {
-            assert.strictEqual(r, "1");
-            if ((++count) === 2) done();
+
+        it("batch(reputationFaucet, depositEther)", function (done) {
+            this.timeout(tools.TIMEOUT);
+            var count = 0;
+            var batch = augur.createBatch();
+            augur.tx.depositEther.send = false;
+            augur.tx.depositEther.returns = "number";
+            augur.tx.depositEther.value = 1;
+            augur.tx.reputationFaucet.send = false;
+            augur.tx.reputationFaucet.returns = "number";
+            batch.add("reputationFaucet", [augur.branches.dev], function (r) {
+                assert.strictEqual(r, "1");
+                if ((++count) === 2) done();
+            });
+            batch.add("depositEther", [], function (r) {
+                assert(r === "1" || r === "-1");
+                if ((++count) === 2) done();
+            });
+            batch.execute();
         });
-        batch.execute();
+
     });
 
-    it("batch(reputationFaucet, depositEther)", function (done) {
-        this.timeout(tools.TIMEOUT);
-        var count = 0;
-        var batch = augur.createBatch();
-        augur.tx.depositEther.send = false;
-        augur.tx.depositEther.returns = "number";
-        augur.tx.depositEther.value = 1;
-        augur.tx.reputationFaucet.send = false;
-        augur.tx.reputationFaucet.returns = "number";
-        batch.add("reputationFaucet", [augur.branches.dev], function (r) {
-            assert.strictEqual(r, "1");
-            if ((++count) === 2) done();
-        });
-        batch.add("depositEther", [], function (r) {
-            assert(r === "1" || r === "-1");
-            if ((++count) === 2) done();
-        });
-        batch.execute();
-    });
+    after(function () { augur.rpc.wsUrl = wsUrl; });
 
-});
-
-after(function () { augur.rpc.wsUrl = wsUrl; });
+}
