a52e046f4aa6f662c5d2b64d46d73b82046b8e29
Stephen Sprinkle <git@sprinkle.io>
2016-06-24 10:15:09 -0700

    broke out validation of expiry source url

--- src/modules/create-market/selectors/form-steps/step-3.js
+++ src/modules/create-market/selectors/form-steps/step-3.js
@@ -1,9 +1,6 @@
-import {
-	TAGS_MAX_NUM, TAGS_MAX_LENGTH,
-	RESOURCES_MAX_NUM, RESOURCES_MAX_LENGTH,
-	EXPIRY_SOURCE_GENERIC, EXPIRY_SOURCE_SPECIFIC
-} from '../../../create-market/constants/market-values-constraints';
+import { TAGS_MAX_NUM, TAGS_MAX_LENGTH,	RESOURCES_MAX_NUM, RESOURCES_MAX_LENGTH } from '../../../create-market/constants/market-values-constraints';
 import validateExpirySource from '../../../create-market/validators/validate-expiry-source';
+import validateExpirySourceUrl from '../../../create-market/validators/validate-expiry-source-url';
 
 export const select = (formState) => {
 	const obj = {
@@ -13,13 +10,6 @@ export const select = (formState) => {
 		resourceMaxLength: RESOURCES_MAX_LENGTH
 	};
 	return obj;
-};
-
-export const validateExpirySourceUrl = (expirySourceUrl, expirySource) => {
-	if (expirySource === EXPIRY_SOURCE_SPECIFIC &&
-	(!expirySourceUrl || !expirySourceUrl.length)) {
-		return 'Please enter the full URL of the website';
-	}
 };
 
 export const isValid = (formState) => {
---
+++ src/modules/create-market/validators/validate-expiry-source-url.js
@@ -0,0 +1,8 @@
+import { EXPIRY_SOURCE_SPECIFIC } from '../../create-market/constants/market-values-constraints';
+
+export default function (expirySourceUrl, expirySource) {
+	if (expirySource === EXPIRY_SOURCE_SPECIFIC &&
+		(!expirySourceUrl || !expirySourceUrl.length)) {
+		return 'Please enter the full URL of the website';
+	}
+};
