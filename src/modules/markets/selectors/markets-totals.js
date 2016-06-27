e76bd3902d986a5e01cc663ebc93d9b73fb67048
Baz <bk@thinkloop.com>
2016-06-27 13:58:30 +0000

    formatted code

--- src/modules/markets/selectors/markets-totals.js
+++ src/modules/markets/selectors/markets-totals.js
@@ -2,12 +2,8 @@ import memoizerific from 'memoizerific';
 import { selectPositionsSummary } from '../../positions/selectors/positions-summary';
 
 export default function () {
-	const {
-		allMarkets,
-		filteredMarkets,
-		unpaginatedMarkets,
-		favoriteMarkets
-	} = require('../../../selectors');
+	const { allMarkets, filteredMarkets, unpaginatedMarkets, favoriteMarkets } = require('../../../selectors');
+
 	return selectMarketsTotals(
 		allMarkets,
 		filteredMarkets.length,
@@ -16,32 +12,31 @@ export default function () {
 	);
 }
 
-export const selectMarketsTotals = memoizerific(1)(
-(allMarkets, filteredMarketsLength, unpaginatedMarketsLength, favoriteMarketsLength) => {
+export const selectMarketsTotals = memoizerific(1)((allMarkets, filteredMarketsLength, unpaginatedMarketsLength, favoriteMarketsLength) => {
 	const positions = { numPositions: 0, qtyShares: 0, totalValue: 0, totalCost: 0 };
 
 	const totals = allMarkets.reduce((p, market) => {
-		p.numAll++;
-		if (market.isPendingReport) {
-			p.numPendingReports++;
-		}
+			p.numAll++;
 
-		if (market.positionsSummary && market.positionsSummary.qtyShares
-				&& market.positionsSummary.qtyShares.value) {
-			positions.numPositions += market.positionsSummary.numPositions.value;
-			positions.qtyShares += market.positionsSummary.qtyShares.value;
-			positions.totalValue += market.positionsSummary.totalValue.value || 0;
-			positions.totalCost += market.positionsSummary.totalCost.value || 0;
-		}
+			if (market.isPendingReport) {
+				p.numPendingReports++;
+			}
 
-		return p;
-	}, {
-		numAll: 0,
-		numFavorites: 0,
-		numPendingReports: 0,
-		numUnpaginated: 0,
-		numFiltered: 0
-	});
+			if (market.positionsSummary && market.positionsSummary.qtyShares && market.positionsSummary.qtyShares.value) {
+				positions.numPositions += market.positionsSummary.numPositions.value;
+				positions.qtyShares += market.positionsSummary.qtyShares.value;
+				positions.totalValue += market.positionsSummary.totalValue.value || 0;
+				positions.totalCost += market.positionsSummary.totalCost.value || 0;
+			}
+
+			return p;
+		}, {
+			numAll: 0,
+			numFavorites: 0,
+			numPendingReports: 0,
+			numUnpaginated: 0,
+			numFiltered: 0
+		});
 
 	totals.numUnpaginated = unpaginatedMarketsLength;
 	totals.numFiltered = filteredMarketsLength;
