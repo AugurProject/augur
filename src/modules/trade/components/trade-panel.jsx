9cc7668e5d43179055ebe04e4eb993b60c73e12e
Stephen Sprinkle <git@sprinkle.io>
2016-06-30 13:01:09 -0600

    re-inclusion of `place trade` button

--- src/modules/trade/components/trade-panel.jsx
+++ src/modules/trade/components/trade-panel.jsx
@@ -4,23 +4,32 @@ import TradePanelFooter from './trade-panel-footer';
 import TradePanelBody from './trade-panel-body';
 
 const TradePanel = (p) => (
-	<div
-		className="trade-panel"
-		onClick={() => p.updateSelectedOutcome(null)}
-	>
-		<table className="trade-builder">
-			<TradePanelHeader selectedOutcomeID={p.selectedOutcomeID} />
-			{p.tradeOrders && !!p.tradeOrders.length &&
-				<TradePanelFooter summary={p.tradeSummary} />
-			}
-			<TradePanelBody
-				outcomes={p.outcomes}
-				sideOptions={p.sideOptions}
-				selectedOutcomeID={p.selectedOutcomeID}
-				updateSelectedOutcome={p.updateSelectedOutcome}
-			/>
-		</table>
-	</div>
+		<div
+			className="trade-panel"
+			onClick={() => p.updateSelectedOutcome(null)}
+		>
+			<table className="trade-builder">
+				<TradePanelHeader selectedOutcomeID={p.selectedOutcomeID} />
+				{p.tradeOrders && !!p.tradeOrders.length &&
+					<TradePanelFooter summary={p.tradeSummary} />
+				}
+				<TradePanelBody
+					outcomes={p.outcomes}
+					sideOptions={p.sideOptions}
+					selectedOutcomeID={p.selectedOutcomeID}
+					updateSelectedOutcome={p.updateSelectedOutcome}
+				/>
+			</table>
+			<div className="place-trade-container">
+				<button
+					className="button place-trade"
+					disabled={!p.tradeOrders || !!!p.tradeOrders.length}
+					onClick={p.onSubmitPlaceTrade}
+				>
+					Place Trade{p.tradeOrders && p.tradeOrders.length > 1 ? 's' : ''}
+				</button>
+			</div>
+		</div>
 );
 
 TradePanel.propTypes = {
@@ -31,30 +40,3 @@ TradePanel.propTypes = {
 };
 
 export default TradePanel;
-
-// {p.tradeOrders && p.tradeOrders.map((tradeOrder, i) => (
-// 	<Transaction
-// 		key={i}
-// 		className="order"
-// 		{...tradeOrder}
-// 		status={undefined}
-// 	/>
-// ))}
-//
-// <Transaction
-// 	type="trade_summary"
-// 	shares={p.totalShares}
-// 	className="order total"
-// 	ether={p.totalEther}
-// 	gas={p.totalGas}
-// 	status={undefined}
-// />
-
-// <div className="place-trade-container">
-// 	<button
-// 		className="button place-trade" disabled={!p.tradeOrders || !p.tradeOrders.length}
-// 		onClick={p.onSubmitPlaceTrade}
-// 	>
-// 		Place Trade
-// 	</button>
-// </div>
