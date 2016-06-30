897fdd61f3999ded2dbf8948c6e472a2db1b7702
Jack Peterson <jack@tinybike.net>
2016-06-29 22:18:08 -0700

    Merged getMarketsInfoCache branch, rebuilt distributable, incremented version

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ var modules = [
 ];
 
 function Augur() {
-    this.version = "1.5.6";
+    this.version = "1.5.7";
 
     this.options = {debug: {broadcast: false, fallback: false}};
     this.protocol = NODE_JS || document.location.protocol;
