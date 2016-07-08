b501100a41fd4b9b788c5809a010993fe5de9271
Baz <bk@thinkloop.com>
2016-07-08 21:40:37 +0000

    export non-default

--- test/assertions/activePage.js
+++ test/assertions/activePage.js
@@ -1,6 +1,6 @@
 import { assert } from 'chai';
 
-export default function (activePage) {
+export function activePage(activePage) {
 	assert.isDefined(activePage, `activePage isn't defined`);
 	assert.isString(activePage, `activePage isn't a string`);
 }
