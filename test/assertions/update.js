fcf5cfebfe037e08af72d191725cce1f0ef56a09
Baz <bk@thinkloop.com>
2016-07-08 22:49:11 +0000

    removed 'update' selector test

--- test/assertions/update.js
+++
@@ -1,8 +0,0 @@
-var assert = require('chai').assert;
-// update: function
-function updateAssertion(actual) {
-	assert.isDefined(actual, `update isn't defined`)
-	assert.isFunction(actual, `update isn't a function`);
-}
-
-module.exports = updateAssertion;
