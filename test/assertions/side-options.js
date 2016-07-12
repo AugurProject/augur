c2b1db22ad65ea8affdc15c772db921612956943
Stephen Sprinkle <git@sprinkle.io>
2016-07-11 17:11:48 -0700

    increased the granularity of the shape assertion for the sideOptions array

--- test/assertions/side-options.js
+++ test/assertions/side-options.js
@@ -9,5 +9,33 @@ export default function(sideOptions) {
 		it('should be an array', () => {
 			assert.isArray(sideOptions, `sideOptions isn't an array`);
 		});
+
+		sideOptions.forEach(option => {
+			describe('option', () => {
+				it('should be an object', () => {
+					assert.isObject(option, `option isn't an object`);
+				});
+
+				describe('value', () => {
+					it('should exist', () => {
+						assert.isDefined(option.value, `value isn't defined`);
+					});
+
+					it('should be a string', () => {
+						assert.isString(option.value, `value isn't a string`);
+					});
+				});
+
+				describe('label', () => {
+					it('should exist', () => {
+						assert.isDefined(option.label, `label isn't defined`);
+					});
+
+					it('should be a string', () => {
+						assert.isString(option.label, `label isn't a string`);
+					});
+				});
+			});
+		});
 	});
 }
