fc9fd4d542b8ddb874f4e33e066c95790363ffb8
Stephen Sprinkle <git@sprinkle.io>
2016-07-12 13:47:59 -0700

    cleaned up trade panel footer

--- src/modules/trade/components/trade-panel-footer.jsx
+++ src/modules/trade/components/trade-panel-footer.jsx
@@ -4,49 +4,47 @@ import ValueDenomination from '../../../modules/common/components/value-denomina
 import TradePanelRow from '../../../modules/trade/components/trade-panel-row';
 import { SUMMARY } from '../../../modules/trade/constants/row-types';
 
-const TradePanelFooter = (p) => {
-	return (
-		<tfoot className="transaction-summary">
-			<tr className={classnames('header-row', 'summary-title')}>
-				<td colSpan="9" >Trade Summary</td>
-			</tr>
-			<tr className={classnames('header-row', 'summary-headers')}>
-				<td>Outcomes</td>
-				<td colSpan="6">Transactions</td>
-				<td>Fee</td>
-				<td>Profit/Loss</td>
-			</tr>
-			{p.summary.tradeOrders.map((trade, i) => {
-				return	<TradePanelRow
-							key={`${trade.data.outcomeName}-${i}`}
-							trade={trade}
-							constants={p.constants}
-							type={SUMMARY}
-						/>
-			})}
-			<tr className="summary-totals">
-				<td />
-				<td colSpan="2">
-					<div className="total-transaction-summary" >
-						<pre className="transaction-type"></pre>
-						<div className="transaction-shares">
-							<ValueDenomination className="shares" {...p.summary.totalShares} />
-						</div>
-						<pre className="shares-at"></pre>
-						<pre className="transaction-price"></pre>
+const TradePanelFooter = (p) => (
+	<tfoot className="transaction-summary">
+		<tr className={classnames('header-row', 'summary-title')}>
+			<td colSpan="9" >Trade Summary</td>
+		</tr>
+		<tr className={classnames('header-row', 'summary-headers')}>
+			<td>Outcomes</td>
+			<td colSpan="6">Transactions</td>
+			<td>Fee</td>
+			<td>Profit/Loss</td>
+		</tr>
+		{p.summary.tradeOrders.map((trade, i) => {
+			return	<TradePanelRow
+						key={`${trade.data.outcomeName}-${i}`}
+						trade={trade}
+						constants={p.constants}
+						type={SUMMARY}
+					/>
+		})}
+		<tr className="summary-totals">
+			<td />
+			<td colSpan="2">
+				<div className="total-transaction-summary" >
+					<pre className="transaction-type"></pre>
+					<div className="transaction-shares">
+						<ValueDenomination className="shares" {...p.summary.totalShares} />
 					</div>
-				</td>
-				<td colSpan="4" />
-				<td className="fee-to-pay" >
-					<ValueDenomination {...p.summary.feeToPay} />
-				</td>
-				<td className="total-cost" >
-					<ValueDenomination {...p.summary.totalEther} />
-				</td>
-			</tr>
-		</tfoot>
-	);
-};
+					<pre className="shares-at"></pre>
+					<pre className="transaction-price"></pre>
+				</div>
+			</td>
+			<td colSpan="4" />
+			<td className="fee-to-pay" >
+				<ValueDenomination {...p.summary.feeToPay} />
+			</td>
+			<td className="total-cost" >
+				<ValueDenomination {...p.summary.totalEther} />
+			</td>
+		</tr>
+	</tfoot>
+);
 
 TradePanelFooter.propTypes = {
 	summary: React.PropTypes.object,
