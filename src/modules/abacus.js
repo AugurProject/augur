146839dca53a3cb29d24d8732b3ae655e4b620f6
Kevin Day <kevin.r.day@gmail.com>
2016-07-15 11:47:51 -0500

    adding tradingFee back to getMarketInfo

--- src/modules/abacus.js
+++ src/modules/abacus.js
@@ -115,6 +115,7 @@ module.exports = {
                 network: this.network_id,
                 makerFee: makerFee.toFixed(),
                 takerFee: new BigNumber("1.5").times(tradingFee).minus(makerFee).toFixed(),
+                tradingFee: abi.unfix(rawInfo[6], "string"),
                 numOutcomes: abi.number(rawInfo[4]),
                 tradingPeriod: abi.number(rawInfo[5]),
                 branchId: rawInfo[7],
