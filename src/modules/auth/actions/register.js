2c7c85c252bda8b9d5485f825377e05557ce4911
John Danz <John.Danz87@gmail.com>
2016-07-18 21:19:17 -0500

    commented some stuff for now

--- src/modules/auth/actions/register.js
+++ src/modules/auth/actions/register.js
@@ -5,17 +5,16 @@ import {
 PASSWORDS_DO_NOT_MATCH,
 USERNAME_REQUIRED
 } from '../../auth/constants/form-errors';
-import { PENDING, SUCCESS, FAILED } from '../../transactions/constants/statuses';
+// import { PENDING, SUCCESS, FAILED } from '../../transactions/constants/statuses';
 import { authError } from '../../auth/actions/auth-error';
 import { updateLoginAccount } from '../../auth/actions/update-login-account';
 import { selectAuthLink } from '../../link/selectors/links';
-import { updateExistingTransaction } from '../../transactions/actions/update-existing-transaction';
-import { makeTransactionID } from '../../transactions/actions/add-transactions';
-import { addTransactions } from '../../transactions/actions/add-transactions';
+// import { updateExistingTransaction } from '../../transactions/actions/update-existing-transaction';
+// import { makeTransactionID } from '../../transactions/actions/add-transactions';
 
 export function register(name, password, password2) {
 	return (dispatch, getState) => {
-		const transactionID = makeTransactionID();
+		// const transactionID = makeTransactionID();
 		if (!name || !name.length) {
 			return dispatch(authError({ code: USERNAME_REQUIRED }));
 		}
@@ -36,17 +35,17 @@ export function register(name, password, password2) {
 		},
 		(transaction) => {
 			console.log(transaction);
- 			dispatch(updateExistingTransaction(transactionID, { status: PENDING }));
- 			console.log(getState().transactionsData);
+			// dispatch(updateExistingTransaction(transactionID, { status: PENDING }));
+			console.log(getState().transactionsData);
 		},
 		(succesfulTransaction) => {
 			console.log(succesfulTransaction);
-			dispatch(updateExistingTransaction(transactionID, { status: SUCCESS }));
+			// dispatch(updateExistingTransaction(transactionID, { status: SUCCESS }));
 			console.log(getState().transactionsData);
- 		},
+		},
 		(failedTransaction) => {
 			console.log(failedTransaction);
-			dispatch(updateExistingTransaction(transactionID, { status: FAILED }));
+			// dispatch(updateExistingTransaction(transactionID, { status: FAILED }));
 			console.log(getState().transactionsData);
 		}
 		);
