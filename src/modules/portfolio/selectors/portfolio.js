b907ef9860e030ee52bd88ae72b88bdbfb84da0b
Stephen Sprinkle <git@sprinkle.io>
2016-07-21 14:32:23 -0700

    summaries array inclusion in portfolio selector

--- src/modules/portfolio/selectors/portfolio.js
+++ src/modules/portfolio/selectors/portfolio.js
@@ -1,9 +1,14 @@
 import { selectPortfolioNavItems } from '../../../modules/portfolio/selectors/nav-items';
+import { selectPortfolioSummaries } from '../../../modules/portfolio/selectors/summaries';
 
 export default function () {
 	const { links } = require('../../../selectors');
 
+	const navItems = selectPortfolioNavItems(links);
+	const summaries = selectPortfolioSummaries();
+
 	return {
-		navItems: selectPortfolioNavItems(links)
+		navItems,
+		summaries
 	};
 }
---
+++ src/modules/portfolio/selectors/summaries.js
@@ -0,0 +1,7 @@
+// import memoizerific from 'memoizerific';
+// TODO -- holding on the getter from augur.js
+
+export const selectPortfolioSummaries = () => (
+	// EMPTY ARRAY RETURNED FOR NOW
+	[]
+);
