fe538490cfb012f156256392d0a4b705c37842d7
Jack Peterson <jack@tinybike.net>
2016-07-21 20:45:42 -0700

    augur-contracts@1.4.9

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ var modules = [
 ];
 
 function Augur() {
-    this.version = "1.8.6";
+    this.version = "1.8.7";
 
     this.options = {debug: {abi: false, broadcast: false, fallback: false, connect: false}};
     this.protocol = NODE_JS || document.location.protocol;
