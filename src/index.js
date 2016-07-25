e04943418f3fd3587864383653dd8a12b9562243
John Danz <John.Danz87@gmail.com>
2016-07-24 21:17:01 -0400

    incrementing verison to publish to npm

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ var modules = [
 ];
 
 function Augur() {
-    this.version = "1.9.1";
+    this.version = "1.9.3";
 
     this.options = {debug: {abi: false, broadcast: false, fallback: false, connect: false}};
     this.protocol = NODE_JS || document.location.protocol;
