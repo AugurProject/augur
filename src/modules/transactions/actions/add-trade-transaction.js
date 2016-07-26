AugurProject/augur

commit 320eb6f37d6018c8c8e181bedb942b5c228f1af3

update to constants to resolve trade summary display issues

src/modules/transactions/actions/add-trade-transaction.js
@@ -1,7 +1,8 @@
 import { formatEther, formatShares } from '../../../utils/format-number';
 import {
-	BUY_SHARES,
-	SELL_SHARES,
+	MULTI_TRADE,
+	BID,
+	ASK
 } from '../../transactions/constants/types';
 import { processOrder } from '../../trade/actions/place-trade';
 import { addTransaction } from '../../transactions/actions/add-transactions';
@@ -9,8 +10,8 @@ import { addTransaction } from '../../transactions/actions/add-transactions';
 export const makeTradeTransaction =
 (isSell, market, outcome, numShares, limitPrice, totalCostWithoutFeeEther, feeEther, gas, dispatch) => {
 	const totalEther = totalCostWithoutFeeEther + feeEther;
-	const txn = {
-		type: !isSell ? BUY_SHARES : SELL_SHARES,
+	const obj = {
+		type: !isSell ? BID : ASK,
 		shares: numShares,
 		sharesNegative: formatShares(-numShares),
 		limitPrice,

src/modules/transactions/constants/types.js
@@ -1,8 +1,8 @@
 export const BUY_SHARES = 'buy_shares';
 export const SELL_SHARES = 'sell_shares';
 
-export const BID_SHARES = 'bid_shares';
-export const ASK_SHARES = 'ask_shares';
+export const BID = 'bid';
+export const ASK = 'ask';
 
 export const CREATE_MARKET = 'create_market';
 export const SUBMIT_REPORT = 'submit_report';

commit 68351ac0429c9b7facb9d714c24da2740ff44f2a

Fix type of order
 - broken by 320eb6f37d6018c8c8e181bedb942b5c228f1af3

src/modules/transactions/actions/add-trade-transaction.js
@@ -1,8 +1,7 @@
 import { formatEther, formatShares } from '../../../utils/format-number';
 import {
 	BID,
-	ASK,
-	BUY_SHARES
+	ASK
 } from '../../transactions/constants/types';
 import { processOrder } from '../../trade/actions/place-trade';
 import { addTransaction } from '../../transactions/actions/add-transactions';
@@ -31,7 +30,7 @@ export const makeTradeTransaction =
 		},
 		action: (transactionID) => {
 			const order = {
-				type: txn.type === BUY_SHARES ? 'buy' : 'sell',
+				type: txn.type === BID ? 'buy' : 'sell',
 				outcomeID: txn.data.outcomeID,
 				limitPrice: txn.limitPrice,
 				etherToBuy: txn.ether.value,