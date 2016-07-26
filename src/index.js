544386502904db2314410fd25d62027d73a6284c
priecint <tp-dev@seznam.cz>
2016-07-26 22:27:17 +0200

    1.9.9

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ var modules = [
 ];
 
 function Augur() {
-    this.version = "1.9.8";
+    this.version = "1.9.9";
 
     this.options = {debug: {abi: false, broadcast: false, fallback: false, connect: false}};
     this.protocol = NODE_JS || document.location.protocol;
