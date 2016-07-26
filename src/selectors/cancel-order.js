25d40702da62f2ca57e54b2af85b01bdd251d5a5
priecint <tp-dev@seznam.cz>
2016-07-26 03:45:07 +0200

    Fix linting error

--- src/selectors/cancel-order.js
+++ src/selectors/cancel-order.js
@@ -2,28 +2,26 @@
  * Author: priecint
  */
 
-export const cancelOrder = orderId => {
-	return setTimeout(() => {
+export const cancelOrder = orderId => setTimeout(() => {
+	require('../selectors').markets.forEach((market) => {
+		market.outcomes.forEach(outcome => {
+			const order = outcome.userOpenOrders.find(openOrder => openOrder.id === orderId);
+			if (order != null) {
+				order.isCancelling = true;
+				require('../selectors').update({});
+			}
+		});
+	});
+	setTimeout(() => {
 		require('../selectors').markets.forEach((market) => {
 			market.outcomes.forEach(outcome => {
 				const order = outcome.userOpenOrders.find(openOrder => openOrder.id === orderId);
 				if (order != null) {
-					order.isCancelling = true;
+					const index = outcome.userOpenOrders.findIndex(openOrder => openOrder.id === orderId);
+					outcome.userOpenOrders.splice(index, 1);
 					require('../selectors').update({});
 				}
 			});
 		});
-		setTimeout(() => {
-			require('../selectors').markets.forEach((market) => {
-				market.outcomes.forEach(outcome => {
-					const order = outcome.userOpenOrders.find(openOrder => openOrder.id === orderId);
-					if (order != null) {
-						const index = outcome.userOpenOrders.findIndex(openOrder => openOrder.id === orderId);
-						outcome.userOpenOrders.splice(index, 1);
-						require('../selectors').update({});
-					}
-				});
-			});
-		}, 2000);
-	}, 1);
-};
+	}, 2000);
+}, 1);
