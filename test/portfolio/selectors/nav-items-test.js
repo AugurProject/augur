bb0993e6f870aebb41a74f194d0da3bb7a5f2102
Stephen Sprinkle <git@sprinkle.io>
2016-08-01 11:42:57 -0700

    refactored portfolio nav-items test to better test selector and shape assertions

--- test/portfolio/selectors/nav-items-test.js
+++ test/portfolio/selectors/nav-items-test.js
@@ -5,54 +5,94 @@ import proxyquire from 'proxyquire';
 
 import { MY_POSITIONS, MY_MARKETS, MY_REPORTS } from '../../../src/modules/app/constants/pages';
 
+import * as selector from '../../../src/modules/portfolio/selectors/nav-items';
+
 describe('modules/portfolio/selectors/nav-items', () => {
-
 	let actual, expected;
-	let stubbedSelectors = {
-		links: {
-			myPositionsLink: {
-				test: 'test'
-			},
-			myMarketsLink: {
-				test: 'test'
-			},
-			myReportsLink: {
-				test: 'test'
-			}
-		}
-	};
-
-	let proxiedSelector = proxyquire('../../../src/modules/portfolio/selectors/nav-items', {
-		'../../../selectors': stubbedSelectors
-	});
-
-	actual = proxiedSelector.default();
-
-	expected = [
-		{
-			label: 'My Positions',
-			link: {
-				test: 'test'
-			},
-			page: MY_POSITIONS
-		},
-		{
-			label: 'My Markets',
-			link: {
-				test: 'test'
-			},
-			page: MY_MARKETS
-		},
-		{
-			label: 'My Reports',
-			link: {
-				test: 'test'
-			},
-			page: MY_REPORTS
-		}
-	];
 
 	it('should return the expected array', () => {
+		proxyquire.noPreserveCache().noCallThru();
+		
+		let stubbedSelectors = {
+			links: {
+				myPositionsLink: {
+					label: 'test',
+					link: {
+						href: 'test',
+						onClick: 'fake function'
+					},
+					page: 'test'
+				},
+				myMarketsLink: {
+					label: 'test',
+					link: {
+						href: 'test',
+						onClick: 'fake function'
+					},
+					page: 'test'
+				},
+				myReportsLink: {
+					label: 'test',
+					link: {
+						href: 'test',
+						onClick: 'fake function'
+					},
+					page: 'test'
+				}
+			}
+		};
+
+		let proxiedSelector = proxyquire('../../../src/modules/portfolio/selectors/nav-items', {
+			'../../../selectors': stubbedSelectors
+		});
+
+		actual = proxiedSelector.default();
+
+		expected = [
+			{
+				label: 'My Positions',
+				link: {
+					label: 'test',
+					link: {
+						href: 'test',
+						onClick: 'fake function'
+					},
+					page: 'test'
+				},
+				page: MY_POSITIONS
+			},
+			{
+				label: 'My Markets',
+				link: {
+					label: 'test',
+					link: {
+						href: 'test',
+						onClick: 'fake function'
+					},
+					page: 'test'
+				},
+				page: MY_MARKETS
+			},
+			{
+				label: 'My Reports',
+				link: {
+					label: 'test',
+					link: {
+						href: 'test',
+						onClick: 'fake function'
+					},
+					page: 'test'
+				},
+				page: MY_REPORTS
+			}
+		];
+
 		assert.deepEqual(expected, actual, `Didn't return the expected array`);
+	});
+
+	it('should deliver the expected shape to augur-ui-react-components', () => {
+		actual = selector.default();
+
+		assertions.portfolioNavItems(actual);
 	});
 });
