3cd4302f5fc7e4ef203f1f86c48b914130f6166b
Stephen Sprinkle <git@sprinkle.io>
2016-08-01 15:47:29 -0700

    initial stub of `my-reports` compoenents

--- src/modules/my-reports/components/reports.jsx
+++ src/modules/my-reports/components/reports.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
-const MyReports = () => (
-	<span>PORTFOLIO REPORTS -- TODO</span>
+const MyReports = (p) => (
+	<table className="full-width-table">
+		<thead>
+			<tr className="cells-bordered solid dark">
+				<th className="no-cell-border"></th>
+				<th>Outcome</th>
+				<th>My Report</th>
+				<th>Fees Earned</th>
+				<th>Ended</th>
+			</tr>
+		</thead>
+		{p.reports && p.reports.map((market, id) => (
+			<tbody key={id} >
+				<tr className="cells-bordered solid dark">
+					<th className="cell-left-aligned">{market.description}</th>
+					<td>{market.endDate.formatted}</td>
+					<td>{market.fees.full}</td>
+					<td>{market.volume.formatted}</td>
+					<td>{market.numberOfTrades.formatted}</td>
+				</tr>
+			</tbody>
+		))}
+	</table>
 );
+
+MyReports.propTypes = {
+	reports: React.PropTypes.array.isRequired
+};
 
 export default MyReports;
