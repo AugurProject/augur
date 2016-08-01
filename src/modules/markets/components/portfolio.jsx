2a781194521bd1ccbf4c1103979213776de5b779
Stephen Sprinkle <git@sprinkle.io>
2016-08-01 14:41:33 -0700

    linting fix

--- src/modules/markets/components/portfolio.jsx
+++ src/modules/markets/components/portfolio.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import PortfolioMarketsRow from '../../../modules/markets/components/portfolio-row';
 
-const PortfolioMarkets = (p) => {
-	return (
-		<table className="full-width-table">
-			<thead>
-				<tr className="cells-bordered solid dark">
-					<th className="no-cell-border"></th>
-					<th>Ends</th>
-					<th>Fees Collected</th>
-					<th>Volume</th>
-					<th># Trades</th>
-					<th>Avg Trade Size</th>
-					<th>Open Volume</th>
-				</tr>
-			</thead>
-			{p.markets.map((market, id) => (
-				<PortfolioMarketsRow
-					key={id}
-					market={market}
-				/>
-			))}
-		</table>
-	);
-};
+const PortfolioMarkets = (p) => (
+	<table className="full-width-table">
+		<thead>
+			<tr className="cells-bordered solid dark">
+				<th className="no-cell-border"></th>
+				<th>Ends</th>
+				<th>Fees Collected</th>
+				<th>Volume</th>
+				<th># Trades</th>
+				<th>Avg Trade Size</th>
+				<th>Open Volume</th>
+			</tr>
+		</thead>
+		{p.markets.map((market, id) => (
+			<PortfolioMarketsRow
+				key={id}
+				market={market}
+			/>
+		))}
+	</table>
+);
 
 PortfolioMarkets.propTypes = {
 	markets: React.PropTypes.array.isRequired
