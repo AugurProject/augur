8ece0d8bfe88b260495af7bd39816009e637298c
Stephen Sprinkle <git@sprinkle.io>
2016-08-02 13:04:50 -0700

    expanded assertions for loginAccountPositions

--- test/assertions/login-account-positions.js
+++ test/assertions/login-account-positions.js
@@ -4,5 +4,15 @@ export default function (loginAccountPositions){
 	describe(`augur-ui-react-components loginAccountPositions' shape`, () => {
 		assert.isDefined(loginAccountPositions);
 		assert.isObject(loginAccountPositions);
+
+		it('markets', () => {
+			assert.isDefined(loginAccountPositions.markets);
+			assert.isArray(loginAccountPositions.markets);
+		});
+
+		it('summary', () => {
+			assert.isDefined(loginAccountPositions.summary);
+			assert.isObject(loginAccountPositions.summary);
+		});
 	});
 };
