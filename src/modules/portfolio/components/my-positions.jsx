2fcecbfc0f64e90639380fe3eb04be9f07cddbec
Stephen Sprinkle <git@sprinkle.io>
2016-08-02 14:26:20 -0700

    refined my-positions + unneeded styles

--- src/modules/portfolio/components/my-positions.jsx
+++ src/modules/portfolio/components/my-positions.jsx
@@ -2,22 +2,18 @@ import React from 'react';
 import Positions from '../../../modules/positions/components/positions';
 
 const MyPositions = (p) => (
-	<div className="positions-page">
-		<section className="page-content">
-			<div className="l-container">
-				{!!p.positions && !!p.positions.markets && !!p.positions.markets.length && p.positions.markets.map(market => (
-					<div key={market.id} className="positions-container">
-						<span className="description">{market.description}</span>
-						{!!market.outcomes && !!market.outcomes.length &&
-							<Positions
-								className="page-content positions-content"
-								outcomes={market.outcomes}
-							/>
-						}
-					</div>
-				))}
+	<div className="positions-content">
+		{!!p.positions && !!p.positions.markets && !!p.positions.markets.length && p.positions.markets.map(market => (
+			<div key={market.id} className="positions-container">
+				<span className="description">{market.description}</span>
+				{!!market.positionOutcomes && !!market.positionOutcomes.length &&
+					<Positions
+						className="page-content positions-content"
+						outcomes={market.positionOutcomes}
+					/>
+				}
 			</div>
-		</section>
+		))}
 	</div>
 );
 
