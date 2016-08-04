e21ace24649c981f8c1d296c42f7a43b7c1305ab
Stephen Sprinkle <git@sprinkle.io>
2016-08-04 15:04:18 -0700

    linting fix

--- src/modules/my-markets/components/market.jsx
+++ src/modules/my-markets/components/market.jsx
@@ -44,15 +44,3 @@ Market.propTypes = {
 };
 
 export default Market;
-
-{/*<tbody>*/}
-{/*<tr>*/}
-	{/*<td className="market-description">{p.market.description}</td>*/}
-	{/*<td className="end-date">{p.market.endDate.formatted}</td>*/}
-	{/*<td className="open-volume">{p.market.openVolume.formatted}</td>*/}
-	{/*<td className="volume">{p.market.volume.formatted}</td>*/}
-	{/*<td className="num-trades">{p.market.numberOfTrades.formatted}</td>*/}
-	{/*<td className="avg-trade-size">{p.market.averageTradeSize.full}</td>*/}
-	{/*<td className="fees-collected">{p.market.fees.full}</td>*/}
-{/*</tr>*/}
-{/*</tbody>*/}
