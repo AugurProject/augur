f0a73d3fb0ae7d80f19ee48dcf2a4dc94ffa9a0e
Stephen Sprinkle <git@sprinkle.io>
2016-08-09 09:55:39 -0700

    test for `login-account-markets`

---
+++ test/my-markets/selectors/login-account-markets-test.js
@@ -0,0 +1,43 @@
+import { assert } from 'chai';
+import { assertions } from 'augur-ui-react-components';
+
+import sinon from 'sinon';
+import proxyquire from 'proxyquire';
+
+import * as selectMyMarkets from '../../../src/modules/my-markets/selectors/my-markets';
+import * as selectMyMarketsSummary from '../../../src/modules/my-markets/selectors/my-markets-summary';
+
+describe('modules/my-markets/selectors/login-account-markets', () => {
+	proxyquire.noPreserveCache().noCallThru();
+
+	let actual;
+
+	const stubbedMyMarkets = sinon.stub(selectMyMarkets, 'default', () => ([]));
+	const stubbedMyMarketsSummary = sinon.stub(selectMyMarketsSummary, 'default', () => {});
+
+	const proxiedSelector = proxyquire('../../../src/modules/my-markets/selectors/login-account-markets', {
+		'../../../modules/my-markets/selectors/my-markets': stubbedMyMarkets,
+		'../../../modules/my-markets/selectors/my-markets-summary': stubbedMyMarketsSummary
+	});
+
+	before(() => {
+		actual = proxiedSelector.default();
+	});
+
+	after(() => {
+		selectMyMarkets.default.restore();
+		selectMyMarketsSummary.default.restore();
+	});
+
+	it(`should call 'selectMyMarkets' once`, () => {
+		assert(stubbedMyMarkets.calledOnce, `Didn't call 'selectMyMarkets' once as expected`);
+	});
+
+	it(`should call 'selectMyMarketsSummary' once`, () => {
+		assert(stubbedMyMarketsSummary.calledOnce, `Didn't call 'selectMyMarketsSummary' once as expected`);
+	});
+
+	it('should return the correct object to augur-ui-react-components', () => {
+		assertions.loginAccountMarkets(actual);
+	});
+});
