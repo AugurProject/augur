5ccea36ee4e6d88c27d096ef8661e97b6bdbb3b9
Stephen Sprinkle <git@sprinkle.io>
2016-08-11 14:33:42 -0700

    inclusion of labels for a number of `assertFormattedNumber` calls

--- test/assertions/positions-markets.js
+++ test/assertions/positions-markets.js
@@ -1,7 +1,6 @@
 import { assert } from 'chai';
 import assertFormattedNumber from '../../test/assertions/common/formatted-number';
 
-
 export default function (positionsMarkets) {
 	assert.isDefined(positionsMarkets, `positionsMarkets isn't defined`);
 	assert.isArray(positionsMarkets, `positionsMarkets isn't an array`);
@@ -19,17 +18,17 @@ function assertPositionMarket(positionMarket) {
 function assertOutcome(outcome) {
 	assert.isNumber(outcome.id, `id isn't a number`);
 	assert.isString(outcome.name, `name isn't a string`);
-	assertFormattedNumber(outcome.lastPrice);
+	assertFormattedNumber(outcome.lastPrice, 'positionsMarkets.positionOutcomes[outcome].lastPrice');
 	assertPosition(outcome.position);
 }
 
 function assertPosition(position) {
-	assertFormattedNumber(position.numPositions);
-	assertFormattedNumber(position.qtyShares);
-	assertFormattedNumber(position.purchasePrice);
-	assertFormattedNumber(position.totalValue);
-	assertFormattedNumber(position.totalCost);
-	assertFormattedNumber(position.shareChange);
-	assertFormattedNumber(position.gainPercent);
-	assertFormattedNumber(position.netChange);
+	assertFormattedNumber(position.numPositions, 'positionsMarkets.positionOutcomes[outcome].position.numPositions');
+	assertFormattedNumber(position.qtyShares, 'positionsMarkets.positionOutcomes[outcome].position.qtyShares');
+	assertFormattedNumber(position.purchasePrice, 'positionsMarkets.positionOutcomes[outcome].position.purchasePrice');
+	assertFormattedNumber(position.totalValue, 'positionsMarkets.positionOutcomes[outcome].position.totalValue');
+	assertFormattedNumber(position.totalCost, 'positionsMarkets.positionOutcomes[outcome].position.totalCost');
+	assertFormattedNumber(position.shareChange, 'positionsMarkets.positionOutcomes[outcome].position.shareChange');
+	assertFormattedNumber(position.gainPercent, 'positionsMarkets.positionOutcomes[outcome].position.gainPercent');
+	assertFormattedNumber(position.netChange, 'positionsMarkets.positionOutcomes[outcome].position.netChange');
 }
--- test/assertions/positions-summary.js
+++ test/assertions/positions-summary.js
@@ -4,14 +4,14 @@ import assertFormattedNumber from '../../test/assertions/common/formatted-number
 export default function (positionsSummary) {
 	assert.isDefined(positionsSummary, `positionsSummary isn't defined`);
 	assert.isObject(positionsSummary, `positionsSummary isn't an object`);
-	assertFormattedNumber(positionsSummary.gainPercent);
 
-	assertFormattedNumber(positionsSummary.netChange, 'netChange');
-	assertFormattedNumber(positionsSummary.numPositions, 'numPositions');
-	assertFormattedNumber(positionsSummary.purchasePrice, 'purchasePrice');
-	assertFormattedNumber(positionsSummary.qtyShares, 'qtyShares');
-	assertFormattedNumber(positionsSummary.shareChange, 'shareChange');
-	assertFormattedNumber(positionsSummary.totalCost, 'totalCost');
-	assertFormattedNumber(positionsSummary.totalValue, 'totalValue');
+	assertFormattedNumber(positionsSummary.gainPercent, 'positionsSummary.gainPercent');
+	assertFormattedNumber(positionsSummary.netChange, 'positionsSummary.netChange');
+	assertFormattedNumber(positionsSummary.numPositions, 'positionsSummary.numPositions');
+	assertFormattedNumber(positionsSummary.purchasePrice, 'positionsSummary.purchasePrice');
+	assertFormattedNumber(positionsSummary.qtyShares, 'positionsSummary.qtyShares');
+	assertFormattedNumber(positionsSummary.shareChange, 'positionsSummary.shareChange');
+	assertFormattedNumber(positionsSummary.totalCost, 'positionsSummary.totalCost');
+	assertFormattedNumber(positionsSummary.totalValue, 'positionsSummary.totalValue');
 };
 
