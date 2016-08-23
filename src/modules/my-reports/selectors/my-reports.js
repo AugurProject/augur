cda64fb8dbb6b3b34b86933130e2b2a8714210a7
Stephen Sprinkle <git@sprinkle.io>
2016-08-23 09:37:11 -0700

    inclusion of marketID + market description in reports obj

--- src/modules/my-reports/selectors/my-reports.js
+++ src/modules/my-reports/selectors/my-reports.js
@@ -1,18 +1,16 @@
-// import { augur } from '../../../services/augurjs';
-// import { formatPercent } from '../../../utils/format-number';
-// import { formatDate } from '../../../utils/format-date';
-// import store from '../../../store';
-// import memoizerific from 'memoizerific';
+import { augur } from '../../../services/augurjs';
+import { formatPercent } from '../../../utils/format-number';
+import { formatDate } from '../../../utils/format-date';
+import { loadMarketsInfo } from '../../markets/actions/load-markets-info';
+import store from '../../../store';
+import memoizerific from 'memoizerific';
 
 export default function () {
-	// const { accountReports } = store.getState();
-    //
-	// if(accountReports){
-	// 	// return generateReportObjects(accountReports);
-	// 	return [];
-	// }
+	const { eventsWithAccountReport } = store.getState();
 
-	return [];
+	if(!eventsWithAccountReport){
+		return [];
+	}
 
 	// Req'd object:
 	/*
@@ -34,6 +32,32 @@ export default function () {
 		]
 	 */
 
+	const reports = Object.keys(eventsWithAccountReport).map(eventID => {
+		const marketID = getMarketIDForEvent(eventID);
+		const description = getMarketDescription(marketID);
+	});
+
 // Whether it's been challanged -- def getRoundTwo(event):
 // Whether it's already been challanged -- def getFinal(event):
 }
+
+export const getMarketIDForEvent = memoizerific(100)(eventID => {
+	const { allMarkets } = require('../../../selectors');
+
+	// Simply getting the first market since events -> markets are 1-to-1 currently
+	augur.getMarket(eventID, 0, (res) => {
+		console.log('getMarket res -- ', res);
+		if (!!res) {
+			if(!allMarkets.filter(market => res === market.id)) store.dispatch(loadMarketsInfo([res]));
+			return res;
+		}
+		return null;
+	});
+});
+
+export const getMarketDescription = memoizerific(100)(marketID => {
+	const { allMarkets } = require('../../../selectors');
+
+	return allMarkets.filter(market => market.id === marketID)[0] && allMarkets.filter(market => market.id === marketID)[0].description || null;
+});
+
