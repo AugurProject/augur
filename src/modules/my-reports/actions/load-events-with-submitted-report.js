fa8b17b24e6335c8bfc9544dd576cf60ab2f56e8
Stephen Sprinkle <git@sprinkle.io>
2016-08-25 13:42:38 -0700

    rework of augur calls into `loadAdditionalEventData`

--- src/modules/my-reports/actions/load-events-with-submitted-report.js
+++ src/modules/my-reports/actions/load-events-with-submitted-report.js
@@ -1,4 +1,6 @@
 import { augur } from '../../../services/augurjs';
+import store from '../../../store';
+import { dateToBlock } from '../../../utils/date-to-block-to-date';
 import { updateOldestLoadedEventPeriod } from '../../my-reports/actions/update-oldest-loaded-event-period';
 import { updateEventsWithAccountReportData } from '../../my-reports/actions/update-events-with-account-report-data';
 
@@ -14,20 +16,59 @@ export function loadEventsWithSubmittedReport(loadMore) {
 			dispatch(updateOldestLoadedEventPeriod(oldestLoadedPeriod));
 
 			while (startPeriod <= blockchain.currentPeriod) {
-				getEventsWithReports(branch.id, startPeriod, loginAccount.id, dispatch);
+				getEventsWithReports(branch.id, startPeriod, loginAccount.id);
 				startPeriod++;
 			}
 		}
 	};
 }
 
-export function getEventsWithReports(branch, period, accountID, dispatch) {
+function getEventsWithReports(branch, period, accountID) {
 	augur.getEventsWithSubmittedReport(branch, period, accountID, (eventIDs) => {
-		const events = {};
+		let events = {};
 		(eventIDs || []).forEach(eventID => {
 			if (parseInt(eventID, 16)) events[eventID] = { branch, period };
 		});
 
-		dispatch(updateEventsWithAccountReportData({ events }));
+		store.dispatch(updateEventsWithAccountReportData({ events }));
+
+		loadAdditionalEventData(events);
+	});
+}
+
+function loadAdditionalEventData(events){
+	const { loginAccount, blockchain } = store.getState();
+
+	const updateEvent = (eventID, data) => {
+		let event = {};
+		event[eventID] = { ...data };
+		store.dispatch(updateEventsWithAccountReportData({ ...event }));
+	};
+
+	Object.keys(events).forEach(eventID => {
+		augur.getExpiration(eventID, expirationDate => {
+			if (!!expirationDate) {
+				updateEvent(eventID, { expirationDate });
+				const expirationBlock = dateToBlock(expirationDate, blockchain.currentBlockNumber);
+				augur.rpc.getLogs({
+					fromBlock: expirationBlock,
+					address: augur.contracts.Consensus,
+					topics: [augur.format_int256(loginAccount.id)]
+				}, repEarned => !!repEarned && updateEvent(eventID, { repEarned }));
+			}
+		});
+		augur.getFinal(eventID, isFinal => !!isFinal && updateEvent(eventID, { isFinal }));
+		augur.getMarket(eventID, 0, marketID => { // Simply getting the first market since events -> markets are 1-to-1 currently
+			if (!!marketID) {
+				updateEvent(eventID, { marketID });
+				augur.getFees(marketID, marketFees => !!marketFees && updateEvent(eventID, { marketFees }));
+			}
+		});
+		augur.getOutcome(eventID, marketOutcome => !!marketOutcome && updateEvent(eventID, { marketOutcome }));
+		augur.proportionCorrect(eventID, proportionCorrect => !!proportionCorrect && updateEvent(eventID, { proportionCorrect }));
+		augur.getReport(events[eventID].branch, events[eventID].period, eventID, loginAccount.id, accountReport => !!accountReport && updateEvent(eventID, { accountReport }));
+		augur.getRepBalance(loginAccount.id, repBalance => !!repBalance && updateEvent(eventID, { repBalance }));
+		augur.getEventWeight(events[eventID].branch, events[eventID].period, eventID, eventWeight => !!eventWeight && updateEvent(eventID, { eventWeight }));
+		augur.getRoundTwo(eventID, isChallenged => !!isChallenged && updateEvent(eventID, { isChallenged }));
 	});
 }
