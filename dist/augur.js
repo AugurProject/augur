c9a1d21209d9dab7a1f4f2b718e38bc1d7f736ca
Jack Peterson <jack@tinybike.net>
2016-08-25 22:14:58 -0700

    augur-contracts@1.8.5

--- dist/augur.js
+++ dist/augur.js
@@ -21165,7 +21165,7 @@ module.exports={
         "RoundTwoPenalize": "0x1148f9bb7db673e5126eb0fd70a23201ce704406", 
         "SendReputation": "0x0511d7363716f3b8b08b3cae74c853e99e3f8b2f", 
         "SlashRep": "0x531c5a87d8e4787c22648012e1a8282525ca8f22", 
-        "Trade": "0x2dfc03694eb5f9dbc5c0efbb188a133d6e301471", 
+        "Trade": "0x67271f461d1c363212abb27a8e36d61776b52a91", 
         "Trades": "0xe41bf99cac58dca892bb198f09aac2cb7bfb4813"
     }
 }
@@ -40044,7 +40044,7 @@ var modules = [
 ];
 
 function Augur() {
-    this.version = "2.1.7";
+    this.version = "2.1.8";
 
     this.options = {
         debug: {
