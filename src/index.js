5cd132e6ee2eb8090679b836cacb4492e539cac9
John Danz <John.Danz87@gmail.com>
2016-08-29 14:58:08 -0500

    publishing version 2.2.8

--- src/index.js
+++ src/index.js
@@ -31,13 +31,13 @@ var modules = [
 ];
 
 function Augur() {
-    this.version = "2.2.7";
+    this.version = "2.2.8";
 
     this.options = {
         debug: {
             tools: false,       // if true, testing tools (test/tools.js) included
             abi: false,         // debug logging in augur-abi
-            broadcast: false,   // broadcast debug logging in ethrpc 
+            broadcast: false,   // broadcast debug logging in ethrpc
             connect: false,     // connection debug logging in ethereumjs-connect
             trading: false      // trading-related debug logging
         }
