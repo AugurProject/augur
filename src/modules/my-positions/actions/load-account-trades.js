f01ee63140b7a7b3c4fdd0842e340cf611685b1d
Stephen Sprinkle <git@sprinkle.io>
2016-08-30 14:56:13 -0600

    on authoritative lookups of account trades, purge local trade state and replace with data from contract (has the affect of removing stale trades) + cleanup

--- src/modules/my-positions/actions/load-account-trades.js
+++ src/modules/my-positions/actions/load-account-trades.js
@@ -1,5 +1,6 @@
 import { augur } from '../../../services/augurjs';
 import { updateAccountTradesData } from '../../../modules/my-positions/actions/update-account-trades-data';
+import { clearAccountTrades } from '../../../modules/my-positions/actions/clear-account-trades';
 
 export function loadAccountTrades() {
 	return (dispatch, getState) => {
@@ -8,6 +9,7 @@ export function loadAccountTrades() {
 				if (accountTrades.error) {
 					return console.warn('ERROR loadAccountTrades', accountTrades);
 				}
+				dispatch(clearAccountTrades());
 				dispatch(updateAccountTradesData(accountTrades));
 			}
 		});
