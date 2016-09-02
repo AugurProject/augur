21e3b655b1c985924f92f47558cedaf58af09692
Jack Peterson <jack@tinybike.net>
2016-09-02 01:30:19 -0700

    Removed number of positions and percent gain from my-positions-summary header

--- src/modules/my-positions/components/my-positions-summary.jsx
+++ src/modules/my-positions/components/my-positions-summary.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import classnames from 'classnames';
-import ValueDenomination from '../../common/components/value-denomination';
 
 const PositionsSummary = (p) => (
 	<div className={classnames('positions-summary', p.className)}>
 		{!!p.numPositions &&
-			<ValueDenomination {...p.numPositions} className="num-positions" />
-		}
-
-		{p.totalValue &&
-			<ValueDenomination {...p.totalValue} className="total-value" />
-		}
-
-		{p.gainPercent &&
-			<ValueDenomination
-				{...p.gainPercent}
-				className="gain-percent"
-				formatted={p.gainPercent.formatted}
-			/>
+			<span className="num-positions">Positions</span>
 		}
 	</div>
 );
