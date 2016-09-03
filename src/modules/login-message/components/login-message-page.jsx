13e53f69ee6370caf9d8e9a429c0c75d2610c951
priecint <tp-dev@seznam.cz>
2016-09-03 04:23:14 +0200

    Improve design of login message
    [ch133]

--- src/modules/login-message/components/login-message-page.jsx
+++ src/modules/login-message/components/login-message-page.jsx
@@ -10,9 +10,9 @@ const LoginMessagePage = (p) => (
 	<main className="page login-message">
 		<SiteHeader {...p.siteHeader} />
 
-		<article className="page-content">
+		<div className="page-content">
 			<div className="l-container">
-				<p className="strong">Welcome to Augur; Beta Test V2. Please note this important information:</p>
+				<h1>Welcome to Augur; Beta Test V2. Please note this important information:</h1>
 				<p>This is a BETA TEST in advance of the Augur Project’s V1 launch. There are bugs. There are
 					features being
 					added, improved, and re-designed. There are a few hundred enhancements scheduled to be added in
@@ -20,23 +20,21 @@ const LoginMessagePage = (p) => (
 					months.
 					Your thoughtful feedback now is essential. Please use the feedback button at the bottom right of
 					every page
-					to submit your feedback, or feel free to send an email to: hugs@augur.net (^^add subject: "Beta
-					Testing
-					feedback" )
+					to submit your feedback, or feel free to send an email to: <a className="link"
+						href="mailto:hugs@augur.net?subject=Beta Testing feedback">hugs@augur.net</a>
 					From your submissions, the Dev team will coordinate fixes and new features. Changes and fixes
 					will be
 					displayed when you log-in again.
 				</p>
-				<p className="strong">Important Information:</p>
+				<h2>Important Information:</h2>
 				<ol>
 					<li>
 						Because Augur is a completely DECENTRALIZED system, if you lose your Log-In credentials it
 						is impossible
-						to recover them. Please take appropriate measures to protect the safety of your password,
+						to recover them. Please <a className="link" href="http://blog.augur.net/faq/how-do-i-savebackup-my-wallet/">take
+						appropriate measures</a> to protect the safety of your password,
 						and create a
-						way to recover your credentials if you forget them. (^^add link to the FAQ once input into
-						Zendesk, see
-						below)
+						way to recover your credentials if you forget them.
 					</li>
 					<li>
 						Do not send real ETH to your Augur account while we are testing. Each account will be given
@@ -56,7 +54,7 @@ const LoginMessagePage = (p) => (
 						cycle will be 60 days long) it is recommended that users visit the site at least every 2
 						days to
 						maintain your REP and simulate “real money” trading, resolution, and reporting conditions.
-						Learn <a href="https://www.youtube.com/watch?v=sCms-snzHk4">How Augur's Reputation Tokens
+						Learn <a className="link" href="https://www.youtube.com/watch?v=sCms-snzHk4">How Augur's Reputation Tokens
 						Work</a>
 					</li>
 					<li>
@@ -67,7 +65,7 @@ const LoginMessagePage = (p) => (
 						is called “Price/Time Priority”.
 					</li>
 				</ol>
-				<p className="strong">Beta Test System Status:</p>
+				<h2>Beta Test System Status:</h2>
 				<p>The following issues have been refined as of Sep 1, 2016:</p>
 				1/
 				2/
@@ -77,7 +75,7 @@ const LoginMessagePage = (p) => (
 				2/
 				3/
 			</div>
-		</article>
+		</div>
 
 		<SiteFooter />
 	</main>
