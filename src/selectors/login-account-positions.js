35421fcbc126eeaf059c27a0eba08b9cc91328ca
Jack Peterson <jack@tinybike.net>
2016-09-03 15:03:19 -0700

    Added selector and assertions for shares-purchased

--- src/selectors/login-account-positions.js
+++ src/selectors/login-account-positions.js
@@ -1,7 +1,9 @@
 import markets from '../selectors/positions-markets';
 import summary from '../selectors/positions-summary';
+import sharesPurchased from '../selectors/shares-purchased';
 
 export default {
 	markets,
-	summary
+	summary,
+	sharesPurchased
 };
