ed90f34fcfc9ebddf2de6818baf06642350f715e
Jack Peterson <jack@tinybike.net>
2016-09-05 02:36:15 -0700

    Updated complete sets structure; augur.js@2.5.1

--- src/modules/my-positions/actions/load-account-trades.js
+++ src/modules/my-positions/actions/load-account-trades.js
@@ -13,29 +13,29 @@ export function loadAccountTrades(marketID, skipSellCompleteSets) {
 				if (!trades || trades.error) return callback(trades);
 				callback(null, trades);
 			}),
-			sellCompleteSets: (callback) => augur.getAccountCompleteSets(account, { type: 'sell', tradeLogStyle: true, market: marketID }, (completeSets) => {
+			completeSets: (callback) => augur.getAccountCompleteSets(account, { type: 'sell', tradeLogStyle: true, market: marketID }, (completeSets) => {
 				if (!completeSets || completeSets.error) return callback(completeSets);
-				callback(null, completeSets.sell);
+				callback(null, completeSets);
 			})
 		}, (err, accountHistory) => {
 			if (err) console.error('loadAccountTrades error:', err);
 			const mergedHistory = accountHistory.trades || {};
-			if (accountHistory.sellCompleteSets) {
-				const sellCompleteSetsMarketIDs = Object.keys(accountHistory.sellCompleteSets);
-				const numSellCompleteSetsMarketIDs = sellCompleteSetsMarketIDs.length;
+			if (accountHistory.completeSets) {
+				const completeSetsMarketIDs = Object.keys(accountHistory.completeSets);
+				const numCompleteSetsMarketIDs = completeSetsMarketIDs.length;
 				let id;
 				let outcomes;
 				let numOutcomes;
 				let outcome;
-				for (let i = 0; i < numSellCompleteSetsMarketIDs; ++i) {
-					id = sellCompleteSetsMarketIDs[i];
+				for (let i = 0; i < numCompleteSetsMarketIDs; ++i) {
+					id = completeSetsMarketIDs[i];
 					if (!mergedHistory[id]) mergedHistory[id] = {};
-					outcomes = Object.keys(accountHistory.sellCompleteSets[id]);
+					outcomes = Object.keys(accountHistory.completeSets[id]);
 					numOutcomes = outcomes.length;
 					for (let j = 0; j < numOutcomes; ++j) {
 						outcome = outcomes[j];
 						if (!mergedHistory[id][outcome]) mergedHistory[id][outcome] = [];
-						mergedHistory[id][outcome] = mergedHistory[id][outcome].concat(accountHistory.sellCompleteSets[id][outcome]);
+						mergedHistory[id][outcome] = mergedHistory[id][outcome].concat(accountHistory.completeSets[id][outcome]);
 					}
 				}
 			}
