e44fe278d33978ac978cd83c8ad587760a26999d
John Danz <John.Danz87@gmail.com>
2016-09-06 11:30:45 -0500

    fixed a minor lint issue

--- src/modules/auth/constants/form-errors.js
+++ src/modules/auth/constants/form-errors.js
@@ -5,11 +5,11 @@ export const USERNAME_TAKEN = 422;
 export const USERNAME_REQUIRED = 'USERNAME_REQUIRED';
 export const PASSWORDS_DO_NOT_MATCH = 'PASSWORDS_DO_NOT_MATCH';
 export const PASSWORD_NEEDS_LOWERCASE = 'PASSWORD_NEEDS_A_LOWERCASE_LETTER';
-export const PASSWORD_NEEDS_LOWERCASE_MSG =  'password requires at least one lowercase letter.';
+export const PASSWORD_NEEDS_LOWERCASE_MSG = 'password requires at least one lowercase letter.';
 export const PASSWORD_NEEDS_UPPERCASE = 'PASSWORD_NEEDS_A_UPPERCASE_LETTER';
-export const PASSWORD_NEEDS_UPPERCASE_MSG =  'password requires at least one uppercase letter.';
+export const PASSWORD_NEEDS_UPPERCASE_MSG = 'password requires at least one uppercase letter.';
 export const PASSWORD_NEEDS_NUMBER = 'PASSWORD_NEEDS_A_NUMBER';
-export const PASSWORD_NEEDS_NUMBER_MSG =  'password requires at least one number.';
+export const PASSWORD_NEEDS_NUMBER_MSG = 'password requires at least one number.';
 
 export const INVALID_USERNAME = 'INVALID_USERNAME';
 export const INVALID_USER = 'INVALID_USER';
