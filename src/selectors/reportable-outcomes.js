9ccc6a8fcb9a8b0059b0029157b3a26a826e5727
Stephen Sprinkle <git@sprinkle.io>
2016-09-06 16:31:09 -0600

    linting fixes

--- src/selectors/reportable-outcomes.js
+++ src/selectors/reportable-outcomes.js
@@ -1,22 +1,22 @@
 import { BINARY, CATEGORICAL } from '../../src/modules/markets/constants/market-types';
 
 const reportableOutcomes = (type, outcomes) => {
-	switch(type) {
-		case BINARY:
-			return [
-				{
-					id: '1',
-					name: 'No'
-				},
-				{
-					id: '2',
-					name: 'Yes'
-				}
-			];
-		case CATEGORICAL:
-			return outcomes;
-		default:
-			return [];
+	switch (type) {
+	case BINARY:
+		return [
+			{
+				id: '1',
+				name: 'No'
+			},
+			{
+				id: '2',
+				name: 'Yes'
+			}
+		];
+	case CATEGORICAL:
+		return outcomes;
+	default:
+		return [];
 	}
 };
 
