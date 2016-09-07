769c8942cbc6c9c46ce8d3e260a28c1d2155be86
Stephen Sprinkle <git@sprinkle.io>
2016-09-07 17:25:28 -0600

    linting fixes

--- src/modules/my-markets/reducers/market-creator-fees.js
+++ src/modules/my-markets/reducers/market-creator-fees.js
@@ -1,8 +1,8 @@
 import { UPDATE_MARKET_CREATOR_FEES } from '../../my-markets/actions/update-market-creator-fees';
 
-export default function (marketCreatorFees = {}, action){
-	switch(action.type){
-		case UPDATE_MARKET_CREATOR_FEES:
+export default function (marketCreatorFees = {}, action) {
+	switch (action.type) {
+	case UPDATE_MARKET_CREATOR_FEES:
 		return {
 			...marketCreatorFees,
 			...action.data
