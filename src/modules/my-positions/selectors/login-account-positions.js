477d38485148350e30a4999d0ed571193220ec9a
Jack Peterson <jack@tinybike.net>
2016-09-07 19:20:59 -0700

    Removed shares-purchased selector; augur-ui-react-components@3.1.21, augur.js@2.6.4

--- src/modules/my-positions/selectors/login-account-positions.js
+++ src/modules/my-positions/selectors/login-account-positions.js
@@ -1,15 +1,12 @@
 import selectMyPositions from '../../../modules/my-positions/selectors/my-positions';
 import selectMyPositionsSummary from '../../../modules/my-positions/selectors/my-positions-summary';
-import selectSharesPurchased from '../../../modules/my-positions/selectors/shares-purchased';
 
 export default function () {
 	const markets = selectMyPositions();
 	const summary = selectMyPositionsSummary();
-	const sharesPurchased = selectSharesPurchased();
 
 	return {
 		markets,
-		summary,
-		sharesPurchased
+		summary
 	};
 }
