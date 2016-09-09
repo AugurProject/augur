ed33de3bcc2d622de2fe3e8abe64fbc4e4fd1637
Stephen Sprinkle <git@sprinkle.io>
2016-09-09 12:58:31 -0600

    linting fix

--- src/modules/balances/components/balances-page.jsx
+++ src/modules/balances/components/balances-page.jsx
@@ -1,35 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classnames from 'classnames';
 import SiteHeader from '../../site/components/site-header';
 import SiteFooter from '../../site/components/site-footer';
 
-export default class BalancesPage extends Component {
-	static propTypes = {
-		className: React.PropTypes.string
-	};
+const BalancesPage = (p) => (
+	<main className={classnames('page account')}>
+		<SiteHeader {...p.siteHeader} />
+		<header className="page-header">
+			<span className="big-line">Balances</span>
+		</header>
 
-	constructor(props) {
-		super(props);
-	}
+		<section className="page-content">
+			<div className="l-container">
+				<div className="balances-section">
+				</div>
+			</div>
+		</section>
+		<SiteFooter />
+	</main>
+);
 
-	render() {
-		const p = this.props;
+BalancesPage.propTypes = {
+	className: React.PropTypes.string
+};
 
-		return (
-			<main className={classnames('page account')}>
-				<SiteHeader {...p.siteHeader} />
-				<header className="page-header">
-					<span className="big-line">Balances</span>
-				</header>
-
-				<section className="page-content">
-					<div className="l-container">
-						<div className="balances-section">
-						</div>
-					</div>
-				</section>
-				<SiteFooter />
-			</main>
-		);
-	}
-}
+export default BalancesPage;
