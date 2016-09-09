6cc1acf4cb77b0d90829bb61da93568b5f6e9539
Stephen Sprinkle <git@sprinkle.io>
2016-09-09 12:33:08 -0600

    replaced local method w/ helper randomNum method

--- src/selectors/portfolio-nav-items.js
+++ src/selectors/portfolio-nav-items.js
@@ -3,10 +3,7 @@ import links from '../selectors/links';
 import { MY_POSITIONS, MY_MARKETS, MY_REPORTS } from '../modules/site/constants/pages';
 
 import { makeNumber } from '../../src/utils/make-number';
-
-function randomSign() {
-	return Math.random() < 0.5 ? -1 : 1;
-}
+import { randomNum } from '../utils/random-number';
 
 export default [
 	{
@@ -14,26 +11,26 @@ export default [
 		link: links.myPositionsLink,
 		page: MY_POSITIONS,
 		leadingTitle: 'Total Positions',
-		leadingValue: makeNumber(Math.round(Math.random() * 10), 'positions'),
+		leadingValue: makeNumber(Math.round(randomNum()), 'positions'),
 		trailingTitle: 'Total Gain/Loss',
-		trailingValue: makeNumber(randomSign() * Math.random() * 3.7, ' ETH')
+		trailingValue: makeNumber(randomNum(), ' ETH')
 	},
 	{
 		label: 'Markets',
 		link: links.myMarketsLink,
 		page: MY_MARKETS,
 		leadingTitle: 'Total Markets',
-		leadingValue: makeNumber(Math.round(Math.random() * 100), 'markets'),
+		leadingValue: makeNumber(Math.round(randomNum()), 'markets'),
 		trailingTitle: 'Total Gain/Loss',
-		trailingValue: makeNumber(Math.random() * 1.3, ' ETH')
+		trailingValue: makeNumber(randomNum(), ' ETH')
 	},
 	{
 		label: 'Reports',
 		link: links.myReportsLink,
 		page: MY_REPORTS,
 		leadingTitle: 'Total Reports',
-		leadingValue: makeNumber(Math.round(Math.random() * 1000), 'reports'),
+		leadingValue: makeNumber(Math.round(randomNum()), 'reports'),
 		trailingTitle: 'Total Gain/Loss',
-		trailingValue: makeNumber(randomSign() * Math.random() * 1.3, ' REP')
+		trailingValue: makeNumber(randomNum(), ' REP')
 	}
 ];
