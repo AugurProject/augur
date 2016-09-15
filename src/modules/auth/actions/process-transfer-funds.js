2388ba6f5e553baecf27475472bf90d11304fdcd
John Danz <John.Danz87@gmail.com>
2016-09-15 17:05:12 -0400

    put real calls in for realEther/REP, still trying to work through some issues with accounts being locked and REP being sent with no balance change to either account

--- src/modules/auth/actions/process-transfer-funds.js
+++ src/modules/auth/actions/process-transfer-funds.js
@@ -7,31 +7,40 @@ export function processTransferFunds(transactionID, fromAddress, amount, currenc
 	return (dispatch, getState) => {
 		dispatch(updateExistingTransaction(transactionID, { status: `submitting a request to transfer ${amount} ${currency} to ${toAddress}...` }));
 
-		const sent = () => {
+		const sent = (sent) => {
+			console.log('sent');
+			console.log(sent);
 			dispatch(updateExistingTransaction(transactionID, { status: `processing transferring of ${amount} ${currency} to ${toAddress}` }));
 		};
 		const success = (data) => {
+			console.log('success');
+			console.log(data);
 			dispatch(updateExistingTransaction(transactionID, { status: SUCCESS, message: `Transfer of ${amount} ${currency} to ${toAddress} Complete.`, hash: data.hash }));
 			dispatch(updateAssets());
 		};
 		const failed = (failedTransaction) => {
+			console.log('failed');
+			console.log(failedTransaction);
 			dispatch(updateExistingTransaction(transactionID, { status: FAILED, message: failedTransaction.message }));
 		};
-		// const confirmed = (confirmedTransaction) => {
-		// 	console.log('transaction Confirmed!');
-		// 	console.log(confirmedTransaction);
-		// };
-		//
-		// const { branch } = getState();
+		const confirmed = (confirmedTransaction) => {
+			console.log('transaction Confirmed!');
+			console.log(confirmedTransaction);
+		};
+
+		const { branch } = getState();
+
+		console.log(fromAddress);
+		console.log(amount);
+		console.log(toAddress);
 
 		switch (currency) {
 		case 'eth':
-			return augur.sendCashFrom(toAddress, amount, fromAddress, sent, success, failed);
+			return augur.sendCashFrom(toAddress, amount, fromAddress, sent, success, failed, confirmed);
 		case 'realEth':
-			return augur.rpc.sendEther(toAddress, amount, fromAddress, sent, success, failed);
+			return augur.rpc.sendEther(toAddress, amount, fromAddress, sent, success, failed, confirmed);
 		case 'REP':
-			// return augur.SendReputation.sendReputation(branch.id, toAddress, amount, sent, success, failed, confirmed);
-			return dispatch(updateExistingTransaction(transactionID, { status: FAILED, message: 'This failed because sending REP has not been implemented.' }));
+			return augur.SendReputation.sendReputation(branch.id, toAddress, amount, sent, success, failed, confirmed);
 		default:
 			return dispatch(updateExistingTransaction(transactionID, { status: FAILED, message: 'Unrecognized currency selected. Transaction failed.' }));
 		}
