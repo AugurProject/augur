6a75ef1de7c8ec12c07a27f0ffd8eba5d4d93a2a
Jack Peterson <jack@tinybike.net>
2016-09-19 17:55:14 -0700

    Added auto-retry callback wrapper to sendReputation (for penaliztionCatchup)

--- src/modules/sendReputation.js
+++ src/modules/sendReputation.js
@@ -7,6 +7,7 @@
 
 var clone = require("clone");
 var abi = require("augur-abi");
+var utils = require("../utilities");
 
 module.exports = {
 
@@ -14,6 +15,7 @@ module.exports = {
         // branch: hash id
         // recver: ethereum address of recipient
         // value: number -> fixed-point
+        var self = this;
         if (branch && branch.branch) {
             recver = branch.recver;
             value = branch.value;
@@ -25,6 +27,34 @@ module.exports = {
         }
         var tx = clone(this.tx.SendReputation.sendReputation);
         tx.params = [branch, recver, abi.fix(value, "hex")];
-        return this.transact(tx, onSent, onSuccess, onFailed, onConfirmed);
+
+        // if callReturn is 0, but account has sufficient Reputation and Rep
+        // redistribution is done, then re-invoke sendReputation
+        // (penalizationCatchup is being called)
+        var prepare = function (result, cb) {
+            if (!result || !result.callReturn || parseInt(result.callReturn, 16)) {
+                return cb(result);
+            }
+            self.getRepBalance(branch, result.from, function (repBalance) {
+                if (!repBalance || repBalance.error) {
+                    return onFailed(repBalance);
+                }
+                if (abi.bignum(repBalance).lt(abi.bignum(value))) {
+                    return onFailed({error: "0", message: "not enough reputation"});
+                }
+                self.getRepRedistributionDone(branch, result.from, function (repRedistributionDone) {
+                    if (!repRedistributionDone || !parseInt(repRedistributionDone, 16)) {
+                        return onFailed({error: "-3", message: "cannot send reputation until redistribution is complete"});
+                    }
+                    self.sendReputation(branch, recver, value, onSent, onSuccess, onFailed, onConfirmed);
+                });
+            });
+        };
+
+        this.transact(tx,
+            onSent,
+            utils.compose(prepare, onSuccess),
+            onFailed,
+            utils.compose(prepare, onConfirmed));
     }
 };
