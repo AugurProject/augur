a88bc6657e2e587bff56e7bc8a47eea53b26ee4d
priecint <tp-dev@seznam.cz>
2016-09-23 15:48:45 +0200

    Don't log TICK action. Add init-timer test
    [ch437]

--- src/modules/app/actions/init-timer.js
+++ src/modules/app/actions/init-timer.js
@@ -4,13 +4,18 @@
 
 export const TICK = 'TICK';
 
+const TICK_MILLIS = 1000;
+
 export default function () {
 	return (dispatch, getState) => {
 		setInterval(() => {
 			dispatch({
 				type: TICK,
+				meta: {
+					ignore: true
+				},
 				now: new Date().getTime()
 			});
-		}, 1000);
+		}, TICK_MILLIS);
 	}
 }
