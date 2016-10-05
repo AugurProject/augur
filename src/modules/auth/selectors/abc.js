4e0edfb14bd92462ab3afe9e377f5cedfd702c46
Paul Puey <paul@airbitz.co>
2016-10-05 15:09:26 -0700

    Pass in Augur logo url for edge login

--- src/modules/auth/selectors/abc.js
+++ src/modules/auth/selectors/abc.js
@@ -7,7 +7,8 @@ import { makeABCUIContext } from 'airbitz-core-js-ui';
 const a = makeABCUIContext({
 	apiKey: 'e239ec875955ec7474628a1dc3d449c8ea8e1b48',
 	accountType: 'account:repo:com.augur',
-	vendorName: 'Augur'
+	vendorName: 'Augur',
+	vendorImageUrl: 'https://airbitz.co/go/wp-content/uploads/2016/08/augur_logo_100.png'
 });
 
 export default function () {
