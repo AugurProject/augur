ca6e9a7b1f20eb432ffe9fc61dc069af3283be9e
priecint <tp-dev@seznam.cz>
2016-10-05 15:37:01 +0200

    Sort open orders in descending order, asks first
    [411]

--- src/modules/user-open-orders/selectors/user-open-orders.js
+++ src/modules/user-open-orders/selectors/user-open-orders.js
@@ -2,10 +2,11 @@
  * Author: priecint
  */
 import memoizerific from 'memoizerific';
+import BigNumber from 'bignumber.js';
 import store from '../../../store';
 import { isOrderOfUser } from '../../bids-asks/helpers/is-order-of-user';
 import { BID, ASK } from '../../bids-asks/constants/bids-asks-types';
-import { CANCELLING, CANCELLED } from '../../bids-asks/constants/order-status';
+import { CANCELLED } from '../../bids-asks/constants/order-status';
 import { formatNone, formatEther, formatShares } from '../../../utils/format-number';
 
 /**
@@ -16,12 +17,13 @@ import { formatNone, formatEther, formatShares } from '../../../utils/format-num
  * @return {Array}
  */
 export default function (outcomeId, marketOrderBook) {
-	const { loginAccount } = store.getState();
+	const { loginAccount, orderCancellation } = store.getState();
 
-	return selectUserOpenOrders(outcomeId, loginAccount, marketOrderBook);
+	return selectUserOpenOrders(outcomeId, loginAccount, marketOrderBook, orderCancellation);
 }
 
 /**
+ * Orders are sorted: asks then bids. By price in descending order
  *
  * @param {String} outcomeID
  * @param {Object} loginAccount
@@ -29,41 +31,41 @@ export default function (outcomeId, marketOrderBook) {
  *
  * @return {Array}
  */
-const selectUserOpenOrders = memoizerific(10)((outcomeID, loginAccount, marketOrderBook) => {
+const selectUserOpenOrders = memoizerific(10)((outcomeID, loginAccount, marketOrderBook, orderCancellation) => {
 	const isUserLoggedIn = loginAccount.id != null;
 
 	if (!isUserLoggedIn || marketOrderBook == null) {
 		return [];
 	}
 
-	const userBids = marketOrderBook.buy == null ? [] : getUserOpenOrders(marketOrderBook.buy, BID, outcomeID, loginAccount.id);
+	const userBids = marketOrderBook.buy == null ? [] : getUserOpenOrders(marketOrderBook.buy, BID, outcomeID, loginAccount.id, orderCancellation);
 
-	const userAsks = marketOrderBook.sell == null ? [] : getUserOpenOrders(marketOrderBook.sell, ASK, outcomeID, loginAccount.id);
+	const userAsks = marketOrderBook.sell == null ? [] : getUserOpenOrders(marketOrderBook.sell, ASK, outcomeID, loginAccount.id, orderCancellation);
 
-	return userBids.concat(userAsks);
+	return userAsks.concat(userBids);
 });
 
 /**
- * Returns user's order for specified outcome
+ * Returns user's order for specified outcome sorted by price
  *
  * @param {Object} orders
  * @param {String} orderType
  * @param {String} outcomeID
  * @param {String} userID
+ * @param {Object} orderCancellation
  *
  * @return {Array}
  */
-function getUserOpenOrders(orders, orderType, outcomeID, userID) {
+function getUserOpenOrders(orders, orderType, outcomeID, userID, orderCancellation) {
 	return Object.keys(orders)
 		.map(orderId => orders[orderId])
-		.filter(order => order.outcome === outcomeID && isOrderOfUser(order, userID) && !order.isCancelled)
+		.filter(order => order.outcome === outcomeID && isOrderOfUser(order, userID) && orderCancellation[order.id] !== CANCELLED)
+		.sort((order1, order2) => new BigNumber(order2.price, 10).comparedTo(new BigNumber(order1.price, 10)))
 		.map(order => (
 			{
 				id: order.id,
 				marketID: order.market,
 				type: orderType,
-				isCancelling: order.status === CANCELLING,
-				isCancelled: order.status === CANCELLED,
 				originalShares: formatNone(),
 				avgPrice: formatEther(order.price),
 				matchedShares: formatNone(),
