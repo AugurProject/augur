89edee74cbfb1808caa95ad9c7d318a1d61dfb13
Stephen Sprinkle <git@sprinkle.io>
2016-10-07 11:52:58 -0600

    linting fixes

--- src/modules/markets/reducers/selected-filter-sort.js
+++ src/modules/markets/reducers/selected-filter-sort.js
@@ -2,7 +2,7 @@ import { UPDATE_URL } from '../../link/actions/update-url';
 import { FILTER_SORT_TYPE, FILTER_SORT_SORT, FILTER_SORT_ISDESC } from '../../markets/constants/filter-sort';
 import { FILTER_SORT_TYPE_PARAM_NAME, FILTER_SORT_SORT_PARAM_NAME, FILTER_SORT_ISDESC_PARAM_NAME } from '../../link/constants/param-names';
 import { UPDATE_SELECTED_FILTER_SORT } from '../../markets/actions/update-selected-filter-sort';
-import store from '../../../store'
+import store from '../../../store';
 
 const INITIAL_STATE = {
 	type: FILTER_SORT_TYPE,
@@ -24,23 +24,19 @@ export default function (selectedFilterSort = INITIAL_STATE, action) {
 
 		const changes = {};
 
-		function findParam(name, value){ // Insure the param is valid
-			return filterSort[name].find(item => item.value === value);
+		if (params[FILTER_SORT_TYPE_PARAM_NAME] != null && findParam('type', params[FILTER_SORT_TYPE_PARAM_NAME])) {
+			changes.type = params[FILTER_SORT_TYPE_PARAM_NAME];
 		}
 
-		if (params[FILTER_SORT_TYPE_PARAM_NAME] != null && findParam('type', params[FILTER_SORT_TYPE_PARAM_NAME])){
-			changes['type'] = params[FILTER_SORT_TYPE_PARAM_NAME];
+		if (params[FILTER_SORT_SORT_PARAM_NAME] != null && findParam('sort', params[FILTER_SORT_SORT_PARAM_NAME])) {
+			changes.sort = params[FILTER_SORT_TYPE_PARAM_NAME];
 		}
 
-		if (params[FILTER_SORT_SORT_PARAM_NAME] != null && findParam('sort', params[FILTER_SORT_SORT_PARAM_NAME])){
-			changes['sort'] = params[FILTER_SORT_TYPE_PARAM_NAME];
+		if (params[FILTER_SORT_ISDESC_PARAM_NAME] != null && typeof params[FILTER_SORT_ISDESC_PARAM_NAME] === 'boolean' && params[FILTER_SORT_ISDESC_PARAM_NAME] !== filterSort.isDesc) {
+			changes.isDesc = params[FILTER_SORT_ISDESC_PARAM_NAME];
 		}
 
-		if (params[FILTER_SORT_ISDESC_PARAM_NAME] != null && typeof params[FILTER_SORT_ISDESC_PARAM_NAME] === "boolean" && params[FILTER_SORT_ISDESC_PARAM_NAME] !== filterSort.isDesc){
-			changes['isDesc'] = params[FILTER_SORT_ISDESC_PARAM_NAME];
-		}
-
-		if(Object.keys(changes).length) {
+		if (Object.keys(changes).length) {
 			return {
 				...selectedFilterSort,
 				...changes
@@ -52,4 +48,9 @@ export default function (selectedFilterSort = INITIAL_STATE, action) {
 	default:
 		return selectedFilterSort;
 	}
+}
+
+function findParam(name, value) { // Insure the param is valid
+	const { filterSort } = store.getState();
+	return filterSort[name].find(item => item.value === value);
 }
