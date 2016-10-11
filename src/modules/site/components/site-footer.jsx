ec6c9f1e3cab084d1be54994cff3d9a7d7c5ba25
Jack Peterson <jack@tinybike.net>
2016-10-11 04:56:24 -0700

    Removed extra links from site footer

--- src/modules/site/components/site-footer.jsx
+++ src/modules/site/components/site-footer.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 
 const SiteFooter = () => (
 	<footer>
-		<a className="link" href="https://sale.augur.net" target="_blank" rel="noopener noreferrer" >REP Login</a>
-		<a className="link" href="http://docs.augur.net" target="_blank" rel="noopener noreferrer" >Documentation</a>
-		<a className="link" href="http://blog.augur.net" target="_blank" rel="noopener noreferrer" >Blog</a>
-		<a className="link" href="https://github.com/AugurProject" target="_blank" rel="noopener noreferrer" >Github</a>
-		<a className="link" href="https://augur.zendesk.com/hc/en-us" target="_blank" rel="noopener noreferrer" >FAQ</a>
 		<a className="link" href="http://augur.strikingly.com" target="_blank" rel="noopener noreferrer" >About</a>
-		<a className="link" href="https://www.hamsterpad.com/chat/dyffy" target="_blank" rel="noopener noreferrer" >Slack</a>
 		<a className="link" href="http://augur.link/augur-beta-ToS-v2.pdf" target="_blank" rel="noopener noreferrer" >Terms of Service</a>
 	</footer>
 );
