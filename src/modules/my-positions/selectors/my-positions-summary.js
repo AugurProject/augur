7444f4403cb1d3e22e5429d2db1ca2a487fce971
Jack Peterson <jack@tinybike.net>
2016-10-14 23:22:05 -0700

    Display 0 position outcomes with realized PL

--- src/modules/my-positions/selectors/my-positions-summary.js
+++ src/modules/my-positions/selectors/my-positions-summary.js
@@ -29,7 +29,7 @@ export const generateMarketsPositionsSummary = memoizerific(50)(markets => {
 	const positionOutcomes = [];
 	markets.forEach(market => {
 		market.outcomes.forEach(outcome => {
-			if (!outcome || !outcome.position || !outcome.position.numPositions || !outcome.position.numPositions.value || !outcome.position.qtyShares || !outcome.position.qtyShares.value) {
+			if (!outcome || !outcome.position || !outcome.position.numPositions || !outcome.position.numPositions.value || ((!outcome.position.qtyShares || !outcome.position.qtyShares.value) && (!outcome.position.realizedNet || !outcome.position.realizedNet.value))) {
 				return;
 			}
 			qtyShares = qtyShares.plus(abi.bignum(outcome.position.qtyShares.value));
