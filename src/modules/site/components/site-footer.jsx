4376932447f2d05f3b33efd26bf24133314fdf28
Joseph Krug <joeykrug@gmail.com>
2016-10-17 17:12:59 -0700

    fixed footer link

--- src/modules/site/components/site-footer.jsx
+++ src/modules/site/components/site-footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SiteFooter = () => (
 	<footer>
-		<a className="link" href="http://augur.strikingly.com" target="_blank" rel="noopener noreferrer" >About</a>
+		<a className="link" href="https://augur.net" target="_blank" rel="noopener noreferrer" >About</a>
 		<a className="link" href="http://augur.link/augur-beta-ToS-v2.pdf" target="_blank" rel="noopener noreferrer" >Terms of Service</a>
 	</footer>
 );
