f1c76ae69bf48d5f3e2570ec73ead5b475bd7251
priecint <tp-dev@seznam.cz>
2016-10-19 18:39:08 +0200

    Add test case
    [ch349]

--- test/market/selectors/helpers/calculate-max-possible-shares-test.js
+++ test/market/selectors/helpers/calculate-max-possible-shares-test.js
@@ -73,6 +73,42 @@ describe('modules/market/selectors/helpers/calculate-max-possible-shares.js', ()
 				}
 			],
 			result: 3
+		},
+		{
+			loginAccount: { id: 'address', ether: 9 },
+			orders: [
+				{
+					amount: '1',
+					block: 1234,
+					id: '0xdbd851cc394595f9c50f32c1554059ec343471b49f84a4b72c44589a25f70ff3',
+					market: 'testMarketID',
+					outcome: '2',
+					owner: '0x7c0d52faab596c08f423e3478aebc6205f3f5d8c',
+					price: '1',
+					type: 'buy'
+				},
+				{
+					amount: '2',
+					block: 1234,
+					id: 'order2',
+					market: 'testMarketID',
+					outcome: '2',
+					owner: '0xtest123',
+					price: '2',
+					type: 'buy'
+				},
+				{
+					amount: '3',
+					block: 1234,
+					id: 'order3',
+					market: 'testMarketID',
+					outcome: '2',
+					owner: '0xtest123',
+					price: '3',
+					type: 'buy'
+				}
+			],
+			result: 3
 		}
 	];
 	testCases.forEach((test) => {
