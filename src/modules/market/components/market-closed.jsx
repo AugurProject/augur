836bb5809edf15c45554c423aa51c340a175221e
Stephen Sprinkle <git@sprinkle.io>
2016-10-20 16:19:00 -0600

    initial refactor (groundwork) to display logic of market view

---
+++ src/modules/market/components/market-closed.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MarketClosed = (p) => {
+	return (
+		<span>Market Closed</span>
+	);
+};
+
+export default MarketClosed;
---
+++ src/modules/market/components/market-open.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MarketOpen = (p) => {
+	return (
+		<span>Market Open</span>
+	);
+};
+
+export default MarketOpen;
