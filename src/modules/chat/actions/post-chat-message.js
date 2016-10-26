a93d32c42e9a93cb11e7b1afd51fc85282e9cf25
Jack Peterson <jack@tinybike.net>
2016-10-25 18:16:01 -0700

    Added support for anonymous chat; augur-ui-react-components@3.5.5

--- src/modules/chat/actions/post-chat-message.js
+++ src/modules/chat/actions/post-chat-message.js
@@ -3,10 +3,10 @@ import { augur } from '../../../services/augurjs';
 export function postChatMessage(roomName, message) {
 	return (dispatch, getState) => {
 		const { loginAccount } = getState();
-		if (loginAccount.id) {
-			augur.chat.postMessage(roomName, message, loginAccount.id, loginAccount.name, (err) => {
-				if (err) console.error('couldn\'t post chat message: ' + message, err);
-			});
-		}
+		const senderAddress = loginAccount.id || '0x0';
+		const senderName = loginAccount.name || loginAccount.id || 'Anonymous Coward';
+		augur.chat.postMessage(roomName, message, senderAddress, senderName, (err) => {
+			if (err) console.error('couldn\'t post chat message: ' + message, err);
+		});
 	};
 }
