3691e78b36cb1f94b136dbdda611d98ce5a0886c
Stephen Sprinkle <git@sprinkle.io>
2016-10-28 14:16:15 -0600

    adjust object shape

--- src/selectors/market-data-nav-items.js
+++ src/selectors/market-data-nav-items.js
@@ -1,15 +1,13 @@
 import { MARKET_DATA_NAV_OUTCOMES, MARKET_DATA_NAV_CHARTS, MARKET_DATA_NAV_DETAILS } from 'modules/app/constants/views';
 
 export default {
-	navItems: {
-		[MARKET_DATA_NAV_OUTCOMES]: {
-			label: 'Outcomes'
-		},
-		[MARKET_DATA_NAV_CHARTS]: {
-			label: 'Charts'
-		},
-		[MARKET_DATA_NAV_DETAILS]: {
-			label: 'Details'
-		}
+	[MARKET_DATA_NAV_OUTCOMES]: {
+		label: 'Outcomes'
+	},
+	[MARKET_DATA_NAV_CHARTS]: {
+		label: 'Charts'
+	},
+	[MARKET_DATA_NAV_DETAILS]: {
+		label: 'Details'
 	}
 };
