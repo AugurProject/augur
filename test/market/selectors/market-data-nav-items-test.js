7e9662d181c354b0dcb6a25658bb108c94a70f9b
Stephen Sprinkle <git@sprinkle.io>
2016-10-28 14:34:18 -0600

    updated test + updated NODE_PATH for tests to resolve alias path issues

---
+++ test/market/selectors/market-data-nav-items-test.js
@@ -0,0 +1,33 @@
+import { assert } from 'chai';
+
+import assertions from 'augur-ui-react-components/lib/assertions';
+
+import { MARKET_DATA_NAV_OUTCOMES, MARKET_DATA_NAV_CHARTS, MARKET_DATA_NAV_DETAILS } from 'modules/app/constants/views';
+
+import selector from 'modules/market/selectors/market-data-nav-items';
+
+describe('modules/market/selectors/market-data-nav-items.js', () => {
+	const actual = selector();
+
+	console.log('actual -- ', actual);
+
+	const expected = {
+		[MARKET_DATA_NAV_OUTCOMES]: {
+			label: 'Outcomes'
+		},
+		[MARKET_DATA_NAV_CHARTS]: {
+			label: 'Charts'
+		},
+		[MARKET_DATA_NAV_DETAILS]: {
+			label: 'Details'
+		}
+	};
+
+	it('should return the expected object', () => {
+		assert.deepEqual(actual, expected, `'marketDataNavItems' did not return the expected object`);
+	});
+
+	it('should delivery the correct object shape to the components', () => {
+		assertions.marketDataNavItems(actual);
+	});
+});
