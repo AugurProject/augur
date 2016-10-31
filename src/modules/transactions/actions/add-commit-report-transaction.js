f1191af0ea692f1b3ebe1a2db60bb823d8045354
Jack Peterson <jack@tinybike.net>
2016-10-30 21:43:38 -0700

    Used formatRealEtherEstimate for commit report gas fees estimate

--- src/modules/transactions/actions/add-commit-report-transaction.js
+++ src/modules/transactions/actions/add-commit-report-transaction.js
@@ -1,12 +1,12 @@
 import { COMMIT_REPORT } from '../../transactions/constants/types';
-import { formatRealEther } from '../../../utils/format-number';
+import { formatRealEtherEstimate } from '../../../utils/format-number';
 import { sendCommitReport } from '../../reports/actions/commit-report';
 import { addTransaction } from '../../transactions/actions/add-transactions';
 import { augur } from '../../../services/augurjs';
 
 export const makeCommitReportTransaction =
 (market, reportedOutcomeID, isUnethical, isIndeterminate, gas, etherWithoutGas, dispatch) => {
-	const gasFees = formatRealEther(augur.getTxGasEth({ ...augur.tx.MakeReports.submitReportHash }, augur.rpc.gasPrice));
+	const gasFees = formatRealEtherEstimate(augur.getTxGasEth({ ...augur.tx.MakeReports.submitReportHash }, augur.rpc.gasPrice));
 	const obj = {
 		type: COMMIT_REPORT,
 		gas,
