612ccc952c9df00694f2308808544b94dee77434
Stephen Sprinkle <git@sprinkle.io>
2016-11-01 16:34:37 -0600

    linting

--- src/modules/market/components/market-positions.jsx
+++ src/modules/market/components/market-positions.jsx
@@ -1,39 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import MarketPositionsRow from 'modules/market/components/market-positions-row';
 
 import getValue from 'utils/get-value';
 
-export default class MarketPositions extends Component {
-	constructor(props) {
-		super(props);
-	}
+const MarketPositions = (p) => {
+	const outcomePositions = getValue(p, 'market.myPositionOutcomes');
+	const marketType = getValue(p, 'market.type');
 
-	render() {
-		const p = this.props;
+	return (
+		<article className="market-positions">
+			<div className="market-positions-header">
+				<span>Outcomes</span>
+				<span>Shares</span>
+				<span>Avg Price</span>
+				<span>Last Price</span>
+				<span>Realized P/L</span>
+				<span>Unrealized P/L</span>
+				<span>Total P/L</span>
+			</div>
+			{(outcomePositions || []).map(outcome =>
+				<MarketPositionsRow
+					key={outcome.id}
+					type={marketType}
+					outcome={outcome}
+				/>
+			)}
+		</article>
+	);
+};
 
-		const outcomePositions = getValue(p, 'market.myPositionOutcomes');
-		const marketType = getValue(p, 'market.type');
-
-		return (
-			<article className="market-positions">
-				<div className="market-positions-header">
-					<span>Outcomes</span>
-					<span>Shares</span>
-					<span>Avg Price</span>
-					<span>Last Price</span>
-					<span>Realized P/L</span>
-					<span>Unrealized P/L</span>
-					<span>Total P/L</span>
-				</div>
-				{(outcomePositions || []).map(outcome =>
-					<MarketPositionsRow
-						key={outcome.id}
-						type={marketType}
-						outcome={outcome}
-					/>
-				)}
-			</article>
-		);
-	}
-}
+export default MarketPositions;
