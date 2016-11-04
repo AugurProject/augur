3fd4127d4a3ac0b709992135957a5711bdd1f819
Jack Peterson <jack@tinybike.net>
2016-11-04 00:28:43 -0700

    Convert title to tooltip in value-denomination

--- src/modules/common/components/value-denomination.jsx
+++ src/modules/common/components/value-denomination.jsx
@@ -1,23 +1,26 @@
 import React, { PropTypes } from 'react';
+import ReactTooltip from 'react-tooltip';
 import classnames from 'classnames';
 
 const ValueDenomination = p => (
-	<span
-		className={classnames('value-denomination', p.className, { positive: p.formattedValue > 0, negative: p.formattedValue < 0 })}
-		title={(p.title && p.title) || ''}
-	>
-		{p.prefix &&
-			<span className="prefix">{p.prefix}</span>
-		}
-		{p.formatted &&
-			<span className="value" title={p.fullPrecision}>{p.formatted}</span>
-		}
-		{p.denomination &&
-			<span className="denomination">{p.denomination}</span>
-		}
-		{p.postfix &&
-			<span className="postfix">{p.postfix}</span>
-		}
+	<span>
+		<span
+			className={classnames('value-denomination', p.className, { positive: p.formattedValue > 0, negative: p.formattedValue < 0 })}
+		>
+			{p.prefix &&
+				<span className="prefix">{p.prefix}</span>
+			}
+			{p.formatted &&
+				<span className="value" data-tip={p.fullPrecision}>{p.formatted}</span>
+			}
+			{p.denomination &&
+				<span className="denomination">{p.denomination}</span>
+			}
+			{p.postfix &&
+				<span className="postfix">{p.postfix}</span>
+			}
+		</span>
+		<ReactTooltip type="light" effect="solid" place="top" />
 	</span>
 );
 
@@ -29,8 +32,7 @@ ValueDenomination.propTypes = {
 	fullPrecision: PropTypes.string,
 	denomination: PropTypes.string,
 	prefix: PropTypes.string,
-	postfix: PropTypes.string,
-	title: PropTypes.string
+	postfix: PropTypes.string
 };
 
 export default ValueDenomination;
