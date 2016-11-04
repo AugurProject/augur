0113de986f8bbbec87aa857f0e59280780e636b8
Jack Peterson <jack@tinybike.net>
2016-11-04 00:26:54 -0700

    Convert title to tooltip in checkbox

--- src/modules/common/components/checkbox.jsx
+++ src/modules/common/components/checkbox.jsx
@@ -1,23 +1,27 @@
 import React, { PropTypes } from 'react';
+import ReactTooltip from 'react-tooltip';
 import classnames from 'classnames';
 
 const Checkbox = p => (
-	<button
-		className={classnames('checkbox unstyled', p.className, { checked: p.isChecked })}
-		type="button"
-		title={p.title}
-		onClick={p.onClick}
-	>
-		<span className="checkbox-box" />
-		<span className="checkbox-label" tabIndex={p.tabIndex} >
-			{p.text}
-		</span>
-		{p.text2 != null &&
-			<span className="checkbox-label2" >
-				{p.text2}
+	<span>
+		<button
+			className={classnames('checkbox unstyled', p.className, { checked: p.isChecked })}
+			type="button"
+			data-tip={p.title}
+			onClick={p.onClick}
+		>
+			<span className="checkbox-box" />
+			<span className="checkbox-label" tabIndex={p.tabIndex} >
+				{p.text}
 			</span>
-		}
-	</button>
+			{p.text2 != null &&
+				<span className="checkbox-label2" >
+					{p.text2}
+				</span>
+			}
+		</button>
+		<ReactTooltip type="light" effect="solid" place="top" />
+	</span>
 );
 
 Checkbox.propTypes = {
