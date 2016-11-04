d978982ea287ced2fd555f7277aaffb687a22c6e
Jack Peterson <jack@tinybike.net>
2016-11-03 19:59:13 -0700

    Replaced title with tooltips in branch component

--- src/modules/branch/components/branch.jsx
+++ src/modules/branch/components/branch.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import ReactTooltip from 'react-tooltip';
 import { Line } from 'rc-progress';
 
 const Branch = p => (
@@ -11,7 +12,10 @@ const Branch = p => (
 		<br />
 		<span className="reporting-cycle-info">Reporting Cycle {p.reportPeriod} &middot; {Math.round(p.currentPeriodProgress)}% complete &middot; {p.phaseLabel} phase ends {p.phaseTimeRemaining}</span>
 		<br />
-		<span className="branch-description" title={p.id}>{p.description}</span> &middot; {p.periodLength} seconds per cycle
+		<span className="branch-description" data-tip data-for="branch-id-tooltip">{p.description}</span> &middot; {p.periodLength / 3600} hours per cycle
+		<ReactTooltip id="branch-id-tooltip" type="light" effect="solid" place="top">
+			<span className="tooltip-text">Branch ID: {p.id}</span>
+		</ReactTooltip>
 	</section>
 );
 
