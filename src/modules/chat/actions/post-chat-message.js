f3c4f7babe32d822c9accd0726bd568dd5e019f0
Jack Peterson <jack@tinybike.net>
2016-11-04 01:24:02 -0700

    Linting

--- src/modules/chat/actions/post-chat-message.js
+++ src/modules/chat/actions/post-chat-message.js
@@ -3,15 +3,15 @@ import { augur } from '../../../services/augurjs';
 export function postChatMessage(roomName, message) {
 	return (dispatch, getState) => {
 		const { loginAccount } = getState();
-        let senderAddress;
-        let senderName;
-        if (loginAccount.id) {
-            senderAddress = loginAccount.id;
-            senderName = loginAccount.name || '';
-        } else {
-            senderAddress = '0x0';
-            senderName = 'Anonymous Coward';
-        }
+		let senderAddress;
+		let senderName;
+		if (loginAccount.id) {
+			senderAddress = loginAccount.id;
+			senderName = loginAccount.name || '';
+		} else {
+			senderAddress = '0x0';
+			senderName = 'Anonymous Coward';
+		}
 		augur.chat.postMessage(roomName, message, senderAddress, senderName, (err) => {
 			if (err) console.error('couldn\'t post chat message: ' + message, err);
 		});
