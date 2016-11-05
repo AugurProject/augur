5b1b138e747219393e68d631b03a712686c34bdc
Jack Peterson <jack@tinybike.net>
2016-11-05 03:14:49 -0700

    Replaced rootBranch env parameter with branch

--- src/modules/app/actions/init-augur.js
+++ src/modules/app/actions/init-augur.js
@@ -27,17 +27,10 @@ export function initAugur() {
 					dispatch(updateConnectionStatus(connected));
 					dispatch(loadChatMessages('augur'));
 					dispatch(loadLoginAccount());
-					let branchID;
-					if (env.rootBranch === false) {
-						const branches = AugurJS.augur.getBranches();
-						branchID = branches[branches.length - 1];
-					} else {
-						branchID = BRANCH_ID;
-					}
 					if (env.reportingTest) {
 						dispatch(reportingTestSetup());
 					} else {
-						dispatch(loadBranch(branchID));
+						dispatch(loadBranch(env.branchID || BRANCH_ID));
 						const { loginAccount, loginMessage } = getState();
 						if (isUserLoggedIn(loginAccount) && !isCurrentLoginMessageRead(loginMessage)) {
 							const { links } = require('../../../selectors');
