8803155eaea12094f73be6679544107061c80c89
Stephen Sprinkle <git@sprinkle.io>
2016-11-07 15:21:05 -0700

    branch layout adjustments

--- src/modules/branch/components/branch.jsx
+++ src/modules/branch/components/branch.jsx
@@ -2,6 +2,8 @@ import React, { PropTypes } from 'react';
 import ReactTooltip from 'react-tooltip';
 import { Line } from 'rc-progress';
 
+import Bullet from 'modules/common/components/bullet';
+
 const Branch = p => (
 	<article className="branch-info">
 		<Line
@@ -9,19 +11,17 @@ const Branch = p => (
 			strokeWidth="1"
 			strokeColor="#5c2634"
 		/>
-		<br />
 		<span className="reporting-cycle-info">
-			Reporting Cycle {p.reportPeriod} &middot; {Math.round(p.currentPeriodProgress)}% complete &middot; {p.phaseLabel} phase ends {p.phaseTimeRemaining}
+			Reporting Cycle {p.reportPeriod} <Bullet /> {Math.round(p.currentPeriodProgress)}% complete <Bullet /> {p.phaseLabel} phase ends {p.phaseTimeRemaining}
 		</span>
-		<br />
 		<span
 			data-tip
 			data-for="branch-id-tooltip"
 			data-event="click focus"
 			className="branch-description pointer"
 		>
-			{p.description}
-		</span> &middot; {p.periodLength / 3600} hours per cycle
+			{p.description} <Bullet /> {p.periodLength / 3600} hours per cycle
+		</span>
 		<ReactTooltip
 			id="branch-id-tooltip"
 			type="light"
