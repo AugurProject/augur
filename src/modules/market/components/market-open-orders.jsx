5ca95a85c3511ae5703d7386b4704d36eadbed64
Stephen Sprinkle <git@sprinkle.io>
2016-11-07 14:22:36 -0700

    fix to display of open orders

--- src/modules/market/components/market-open-orders.jsx
+++ src/modules/market/components/market-open-orders.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import MarketOpenOrdersGroup from 'modules/market/components/market-open-orders-group';
 import NullStateMessage from 'modules/common/components/null-state-message';
@@ -7,51 +7,72 @@ import { SCALAR } from 'modules/markets/constants/market-types';
 
 import getValue from 'utils/get-value';
 
-const MarketOpenOrders = (p) => {
-	const userOpenOrders = getValue(p, 'outcomes.outcome.userOpenOrders');
-	const nullMessage = 'No Current Open Orders';
+export default class MarketOpenOrders extends Component {
+	constructor(props) {
+		super(props);
 
-	return (
-		<article className="market-open-orders">
-			{!userOpenOrders ?
-				<NullStateMessage message={nullMessage} /> :
-				<div>
-					<div className="market-open-orders-header">
-						<span>{!p.marketType === SCALAR ? 'Outcomes' : 'Outcome'}</span>
-						<span>Type</span>
-						<span>Shares</span>
-						<span>Price</span>
-						<span>Action</span>
-					</div>
-					{
-						(p.outcomes || []).map((outcome, index) => {
-							const lastPricePercent = getValue(outcome, 'lastPricePercent.rounded');
+		this.state = {
+			hasOrders: false
+		};
 
-							return (
-								<MarketOpenOrdersGroup
-									key={outcome.name}
-									id={outcome.id}
-									name={outcome.name}
-									marketType={p.marketType}
-									lastPricePercent={lastPricePercent}
-									userOpenOrders={outcome.userOpenOrders}
-									orderCancellation={p.orderCancellation}
-									selectedShareDenomination={p.selectedShareDenomination}
-								/>
-							);
-						})
-					}
-				</div>
+		this.updateHasOrders = this.updateHasOrders.bind(this);
+	}
+
+	componentWillMount() {
+		this.updateHasOrders(this.props);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		this.updateHasOrders(nextProps);
+	}
+
+	updateHasOrders(props) {
+		(props.outcomes || []).forEach((outcome) => {
+			if (outcome.userOpenOrders && outcome.userOpenOrders.length && !this.state.hasOrders) {
+				this.setState({ hasOrders: true });
 			}
-		</article>
-	);
-};
+		});
+	}
 
-// TODO -- Prop Validations
-// OpenOrders.propTypes = {
-// 	userOpenOrdersSummary: React.PropTypes.object,
-// 	outcomes: React.PropTypes.array,
-// 	orderCancellation: React.PropTypes.object.isRequired
-// };
+	render() {
+		const p = this.props;
+		const s = this.state;
 
-export default MarketOpenOrders;
+		const nullMessage = 'No Current Open Orders';
+
+		return (
+			<article className="market-open-orders">
+				{!s.hasOrders ?
+					<NullStateMessage message={nullMessage} /> :
+					<div>
+						<div className="market-open-orders-header">
+							<span>{!p.marketType === SCALAR ? 'Outcomes' : 'Outcome'}</span>
+							<span>Type</span>
+							<span>Shares</span>
+							<span>Price</span>
+							<span>Action</span>
+						</div>
+						{
+							(p.outcomes || []).map((outcome, index) => {
+								const lastPricePercent = getValue(outcome, 'lastPricePercent.rounded');
+
+								return (
+									<MarketOpenOrdersGroup
+										key={outcome.name}
+										id={outcome.id}
+										name={outcome.name}
+										marketType={p.marketType}
+										lastPricePercent={lastPricePercent}
+										userOpenOrders={outcome.userOpenOrders}
+										orderCancellation={p.orderCancellation}
+										selectedShareDenomination={p.selectedShareDenomination}
+									/>
+								);
+							})
+						}
+					</div>
+				}
+			</article>
+		);
+	}
+}
