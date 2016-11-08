a45ebefda3c2254778793129819b4593a9be5ab5
Stephen Sprinkle <git@sprinkle.io>
2016-11-08 14:42:59 -0700

    updated selector for market-reporting-nav-items

---
+++ src/modules/market/selectors/market-reporting-nav-items.js
@@ -0,0 +1,12 @@
+import { MARKET_REPORTING_NAV_REPORT, MARKET_REPORTING_NAV_DETAILS } from 'modules/app/constants/views';
+
+export default function () {
+	return {
+		[MARKET_REPORTING_NAV_REPORT]: {
+			label: 'Report'
+		},
+		[MARKET_REPORTING_NAV_DETAILS]: {
+			label: 'Details'
+		}
+	};
+}
