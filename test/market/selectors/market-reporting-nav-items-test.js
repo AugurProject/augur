45ccbf52374300ea919103fdcd09dae5641a4eeb
Stephen Sprinkle <git@sprinkle.io>
2016-11-08 14:46:23 -0700

    test for market-reporting-nav-items + cleanup

---
+++ test/market/selectors/market-reporting-nav-items-test.js
@@ -0,0 +1,28 @@
+import { assert } from 'chai';
+
+import assertions from 'augur-ui-react-components/lib/assertions';
+
+import { MARKET_REPORTING_NAV_REPORT, MARKET_REPORTING_NAV_DETAILS } from 'modules/app/constants/views';
+
+import selector from 'modules/market/selectors/market-reporting-nav-items';
+
+describe('modules/market/selectors/market-reporting-nav-items.js', () => {
+	const actual = selector();
+
+	const expected = {
+		[MARKET_REPORTING_NAV_REPORT]: {
+			label: 'Report'
+		},
+		[MARKET_REPORTING_NAV_DETAILS]: {
+			label: 'Details'
+		}
+	};
+
+	it('should return the expected object', () => {
+		assert.deepEqual(actual, expected, `'marketReportingNavItems' did not return the expected object`);
+	});
+
+	it('should delivery the correct object shape to the components', () => {
+		assertions.marketReportingNavItems(actual);
+	});
+});
