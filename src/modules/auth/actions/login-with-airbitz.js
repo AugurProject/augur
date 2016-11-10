c26c4ec21f9112f1ca88146a64408eb3d4abbf5d
William Swanson <swansontec@gmail.com>
2016-11-10 14:23:28 -0800

    Store the private key in an Airbitz wallet
    Before, the integration was wrongly using the Airbitz account dataKey
    as the Etherium private key. This worked, but was a mis-use of the dataKey.
    The proper way to store asset keys is in a wallet, so do that instead.

--- src/modules/auth/actions/login-with-airbitz.js
+++ src/modules/auth/actions/login-with-airbitz.js
@@ -1,3 +1,4 @@
+import secureRandom from 'secure-random';
 import { augur } from '../../../services/augurjs';
 import {
 	loadLoginAccountDependents,
@@ -7,36 +8,52 @@ import { updateLoginAccount } from '../../auth/actions/update-login-account';
 import { authError } from '../../auth/actions/auth-error';
 import { addFundNewAccount } from '../../transactions/actions/add-fund-new-account-transaction';
 
+const walletType = 'wallet:repo:ethereum';
+
+function loginWithEthereumWallet(dispatch, airbitzAccount, ethereumWallet) {
+	const { links } = require('../../../selectors');
+
+	const masterPrivateKey = ethereumWallet.keys.ethereumKey;
+	augur.web.loginWithMasterKey(airbitzAccount.username, masterPrivateKey, (account) => {
+		console.log(account);
+		if (!account) {
+			return dispatch(authError({ code: 0, message: 'failed to login' }));
+		} else if (account.error) {
+			return dispatch(authError({ code: account.error, message: account.message }));
+		}
+		const loginAccount = { ...account, id: account.address, airbitzAccount };
+		if (!loginAccount || !loginAccount.id) {
+			return;
+		}
+		dispatch(loadLoginAccountLocalStorage(loginAccount.id));
+		dispatch(updateLoginAccount(loginAccount));
+		dispatch(loadLoginAccountDependents((err, ether) => {
+			console.log('got:', err, ether);
+			if (err) return console.error('loadLoginAccountDependents:', err);
+			if (ether === 0) {
+				dispatch(addFundNewAccount(loginAccount.id));
+			}
+		}));
+		if (links && links.marketsLink)	{
+			links.marketsLink.onClick(links.marketsLink.href);
+		}
+	});
+}
+
 export function loginWithAirbitz(airbitzAccount) {
 	return (dispatch, getState) => {
-		const { links } = require('../../../selectors');
-
-		// XXX use dataKey for now as the masterPrivateKey. For production, this will need to be a key
-		// created and saved in a wallet repo inside the Augur account. -paul@airbitz.co
-		const masterPrivateKey = airbitzAccount.repoInfo.dataKey;
-		augur.web.loginWithMasterKey(airbitzAccount.username, masterPrivateKey, (account) => {
-			console.log(account);
-			if (!account) {
-				return dispatch(authError({ code: 0, message: 'failed to login' }));
-			} else if (account.error) {
-				return dispatch(authError({ code: account.error, message: account.message }));
-			}
-			const loginAccount = { ...account, id: account.address, airbitzAccount };
-			if (!loginAccount || !loginAccount.id) {
-				return;
-			}
-			dispatch(loadLoginAccountLocalStorage(loginAccount.id));
-			dispatch(updateLoginAccount(loginAccount));
-			dispatch(loadLoginAccountDependents((err, ether) => {
-				console.log('got:', err, ether);
-				if (err) return console.error('loadLoginAccountDependents:', err);
-				if (ether === 0) {
-					dispatch(addFundNewAccount(loginAccount.id));
-				}
-			}));
-			if (links && links.marketsLink)	{
-				links.marketsLink.onClick(links.marketsLink.href);
-			}
-		});
+		const ethereumWallet = airbitzAccount.getFirstWallet(walletType);
+		if (ethereumWallet == null) {
+			// Create an ethereum wallet if one doesn't exist:
+			const keys = {
+				ethereumKey: new Buffer(secureRandom(32)).toString('hex')
+			};
+			airbitzAccount.createWallet(walletType, keys, (err, id) => {
+				if (err) return dispatch(authError({ code: 0, message: 'could not create wallet' }));
+				loginWithEthereumWallet(dispatch, airbitzAccount, airbitzAccount.getWallet(id));
+			});
+		} else {
+			loginWithEthereumWallet(dispatch, airbitzAccount, ethereumWallet);
+		}
 	};
 }
