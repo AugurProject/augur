bf033f4f2a48cd62a1c368f6c262dbed89fd2955
Stephen Sprinkle <git@sprinkle.io>
2016-11-17 16:02:41 -0700

    handling of the footer nav toggle + other logic

--- src/modules/app/components/footer.jsx
+++ src/modules/app/components/footer.jsx
@@ -1,22 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Nav from 'modules/app/components/nav';
 
-const Footer = (p) => {
-	console.log('Footer props -- ', p);
+import debounce from 'utils/debounce';
 
-	return (
-		<footer>
-			<Nav
-				className="nav-footer"
-				{...p}
-			/>
-			<div id="footer_content">
-				<a className="link" href="https://augur.net" target="_blank" rel="noopener noreferrer" >About</a>
-				<a className="link" href="http://augur.link/augur-beta-ToS-v2.pdf" target="_blank" rel="noopener noreferrer" >Terms of Service</a>
-			</div>
-		</footer>
-	);
-};
+export default class Footer extends Component {
+	constructor(props) {
+		super(props);
 
-export default Footer;
+		this.state = {
+			isFooterCollapsed: true,
+			verticalOffset: 0
+		};
+
+		this.handleWindowResize = debounce(this.handleWindowResize.bind(this));
+		this.toggleFooter = this.toggleFooter.bind(this);
+		this.slideFooter = this.slideFooter.bind(this);
+	}
+
+	componentDidMount() {
+		window.addEventListener('resize', this.handleWindowResize);
+		this.slideFooter();
+	}
+
+	componentDidUpdate(pP, pS) {
+		if (pS.isFooterCollapsed !== this.state.isFooterCollapsed) {
+			this.slideFooter();
+		}
+	}
+
+	toggleFooter() {
+		this.setState({ isFooterCollapsed: !this.state.isFooterCollapsed });
+	}
+
+	handleWindowResize() {
+		if (this.navRef.offsetHeight) { // navs present
+			this.slideFooter();
+		} else if (this.state.verticalOffset !== 0) {
+			this.slideFooter();
+		}
+	}
+
+	slideFooter() {
+		const s = this.state;
+		const navHeight = this.navRef.offsetHeight;
+		const footerHeight = this.footer.offsetHeight;
+
+		if (navHeight) { // navs are present
+			if (s.isFooterCollapsed) { // collapse
+				this.setState({ verticalOffset: -(footerHeight - navHeight) });
+			} else { // expand
+				this.setState({ verticalOffset: 0 });
+			}
+		} else { // navs are absent
+			this.setState({ verticalOffset: 0 });
+		}
+	}
+
+	render() {
+		const p = this.props;
+		const s = this.state;
+
+		return (
+			<footer
+				ref={(footer) => { this.footer = footer; }}
+				style={{ bottom: s.verticalOffset }}
+			>
+				<Nav
+					className="nav-footer"
+					navRef={(navRef) => { this.navRef = navRef; }}
+					toggleFooter={this.toggleFooter}
+					{...p}
+				/>
+				<div id="footer_content">
+					<a className="link" href="https://augur.net" target="_blank" rel="noopener noreferrer" >About</a>
+					<a className="link" href="http://augur.link/augur-beta-ToS-v2.pdf" target="_blank" rel="noopener noreferrer" >Terms of Service</a>
+				</div>
+			</footer>
+		);
+	}
+}
--- src/modules/app/components/nav.jsx
+++ src/modules/app/components/nav.jsx
@@ -10,102 +10,107 @@ import { ACCOUNT, MARKETS, TRANSACTIONS, MY_POSITIONS, MY_MARKETS, MY_REPORTS } 
 import { FAVORITES, PENDING_REPORTS } from 'modules/markets/constants/markets-headers';
 import { AUTH_TYPES } from 'modules/auth/constants/auth-types';
 
-const Nav = (p) => {
-	console.log('p -- ', p);
-
-	return (
-		<nav className={`app-nav ${p.className ? p.className : ''}`}>
-			{p.isSideBarAllowed &&
-				<button
-					className="app-nav-link unstyled"
-					onClick={p.toggleSideBar}
-				>
-					{p.isSideBarCollapsed ? <i></i> : <i></i>}
-				</button>
-			}
-			<Link
-				className={classnames('app-nav-link', { active: ((p.activeView === MARKETS || (!!parseInt(p.activeView, 10) && Number.isInteger(parseInt(p.activeView, 10)))) && p.marketsInfo.selectedMarketsHeader == null) })}
-				{...p.marketsLink}
+// NOTE -- 	first child div is there to pass up a ref so that other methods can
+//					acquire the row height of the navs in the footer
+const Nav = p => (
+	<nav className={`app-nav ${p.className ? p.className : ''}`}>
+		<div ref={p.navRef && p.navRef} />
+		<button
+			className="nav-toggler"
+			onClick={p.toggleFooter}
+		>
+			Toggle
+		</button>
+		{p.isSideBarAllowed &&
+			<button
+				className="app-nav-link unstyled"
+				onClick={p.toggleSideBar}
 			>
-				<i className="nav-icon"></i>
-				Markets
-			</Link>
-			{p.logged && p.hasFavorites &&
-				<Link
-					className={classnames('app-nav-link', { active: (p.activeView === MARKETS || (!!parseInt(p.activeView, 10) && Number.isInteger(parseInt(p.activeView, 10)))) && p.marketsInfo.selectedMarketsHeader === FAVORITES })}
-					{...p.favoritesLink}
-				>
-					<i className="nav-icon"></i>
-					{p.marketsInfo.numFavorites &&
-						<span className="nav-count">{p.marketsInfo.numFavorites} </span>
-					}
-					Favorites
-				</Link>
-			}
-			{p.logged && p.hasPendingReports &&
-				<Link
-					className={classnames('app-nav-link', { active: (p.activeView === MARKETS || (!!parseInt(p.activeView, 10) && Number.isInteger(parseInt(p.activeView, 10)))) && p.marketsInfo.selectedMarketsHeader === PENDING_REPORTS })}
-					{...p.pendingReportsLink}
-				>
-					<i className="nav-icon"></i>
-					{p.marketsInfo.numPendingReports &&
-						<span className="nav-count">{p.marketsInfo.numPendingReports} </span>
-					}
-					Pending Reports
-				</Link>
-			}
+				{p.isSideBarCollapsed ? <i></i> : <i></i>}
+			</button>
+		}
+		<Link
+			className={classnames('app-nav-link', { active: ((p.activeView === MARKETS || (!!parseInt(p.activeView, 10) && Number.isInteger(parseInt(p.activeView, 10)))) && p.marketsInfo.selectedMarketsHeader == null) })}
+			{...p.marketsLink}
+		>
+			<i className="nav-icon"></i>
+			Markets
+		</Link>
+		{p.logged && p.hasFavorites &&
 			<Link
-				className="augur-brand"
-				{...p.marketsLink}
+				className={classnames('app-nav-link', { active: (p.activeView === MARKETS || (!!parseInt(p.activeView, 10) && Number.isInteger(parseInt(p.activeView, 10)))) && p.marketsInfo.selectedMarketsHeader === FAVORITES })}
+				{...p.favoritesLink}
 			>
-				<AugurLogoFull />
+				<i className="nav-icon"></i>
+				{p.marketsInfo.numFavorites &&
+					<span className="nav-count">{p.marketsInfo.numFavorites} </span>
+				}
+				Favorites
 			</Link>
-			{p.logged && !!p.portfolioTotals &&
-				<Link
-					className={classnames('app-nav-link', MY_POSITIONS, { active: [MY_POSITIONS, MY_MARKETS, MY_REPORTS].indexOf(p.activeView) > -1 })}
-					{...p.myPositionsLink}
-				>
-					<i className="nav-icon">
-						
-					</i>
-					Portfolio
-				</Link>
-			}
-			{p.logged &&
-				<Link
-					className={classnames('app-nav-link', TRANSACTIONS, { active: p.activeView === TRANSACTIONS }, { working: p.isTransactionsWorking })}
-					{...p.transactionsLink}
-				>
-					<i className="nav-icon">
-						
-					</i>
-					{p.numTransactionsWorking &&
-						<span className="nav-count">{p.numTransactionsWorking} </span>
-					}
-					{p.transactionsTotals.title}
-				</Link>
-			}
-			{p.logged &&
-				<Link
-					className={classnames('app-nav-link', ACCOUNT, { active: p.activeView === ACCOUNT })}
-					{...p.accountLink}
-				>
-					<i className="nav-icon">
-						
-					</i>
-					Account
-				</Link>
-			}
-			{!p.logged &&
-				<Link className={classnames('app-nav-link', AUTH_TYPES[p.activeView], { active: !!AUTH_TYPES[p.activeView] })} {...p.authLink}>
-					<i className="nav-icon">
-						<AugurLogoIcon />
-					</i>
-					Sign Up / Login
-				</Link>
-			}
-		</nav>
-	);
-};
+		}
+		{p.logged && p.hasPendingReports &&
+			<Link
+				className={classnames('app-nav-link', { active: (p.activeView === MARKETS || (!!parseInt(p.activeView, 10) && Number.isInteger(parseInt(p.activeView, 10)))) && p.marketsInfo.selectedMarketsHeader === PENDING_REPORTS })}
+				{...p.pendingReportsLink}
+			>
+				<i className="nav-icon"></i>
+				{p.marketsInfo.numPendingReports &&
+					<span className="nav-count">{p.marketsInfo.numPendingReports} </span>
+				}
+				Pending Reports
+			</Link>
+		}
+		<Link
+			className="augur-brand"
+			{...p.marketsLink}
+		>
+			<AugurLogoFull />
+		</Link>
+		{p.logged && !!p.portfolioTotals &&
+			<Link
+				className={classnames('app-nav-link', MY_POSITIONS, { active: [MY_POSITIONS, MY_MARKETS, MY_REPORTS].indexOf(p.activeView) > -1 })}
+				{...p.myPositionsLink}
+			>
+				<i className="nav-icon">
+					
+				</i>
+				Portfolio
+			</Link>
+		}
+		{p.logged &&
+			<Link
+				className={classnames('app-nav-link', TRANSACTIONS, { active: p.activeView === TRANSACTIONS }, { working: p.isTransactionsWorking })}
+				{...p.transactionsLink}
+			>
+				<i className="nav-icon">
+					
+				</i>
+				{p.numTransactionsWorking &&
+					<span className="nav-count">{p.numTransactionsWorking} </span>
+				}
+				{p.transactionsTotals.title}
+			</Link>
+		}
+		{p.logged &&
+			<Link
+				className={classnames('app-nav-link', ACCOUNT, { active: p.activeView === ACCOUNT })}
+				{...p.accountLink}
+			>
+				<i className="nav-icon">
+					
+				</i>
+				Account
+			</Link>
+		}
+		{!p.logged &&
+			<Link className={classnames('app-nav-link', AUTH_TYPES[p.activeView], { active: !!AUTH_TYPES[p.activeView] })} {...p.authLink}>
+				<i className="nav-icon">
+					<AugurLogoIcon />
+				</i>
+				Sign Up / Login
+			</Link>
+		}
+	</nav>
+);
 
 export default Nav;
