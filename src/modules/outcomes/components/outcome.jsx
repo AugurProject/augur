fd9ae7f0e8fbd8742f756da43ade440723292979
Stephen Sprinkle <git@sprinkle.io>
2016-11-21 11:24:38 -0700

    additional styling + responsive improvments to market view

--- src/modules/outcomes/components/outcome.jsx
+++ src/modules/outcomes/components/outcome.jsx
@@ -11,6 +11,8 @@ import getValue from 'utils/get-value';
 import setShareDenomination from 'utils/set-share-denomination';
 
 const Outcome = (p) => {
+	const outcomeName = getValue(p, 'outcome.name');
+
 	const topBidShares = setShareDenomination(getValue(p, 'outcome.topBid.shares.formatted'), p.selectedShareDenomination);
 	const topAskShares = setShareDenomination(getValue(p, 'outcome.topAsk.shares.formatted'), p.selectedShareDenomination);
 
@@ -23,18 +25,42 @@ const Outcome = (p) => {
 	return (
 		<article className={classNames('outcome', { selected: p.selectedOutcome.id === p.outcome.id })}>
 			<Link
-				className={classNames('outcome-row', { selected: p.selectedOutcome.id === p.outcome.id })}
+				className={classNames('outcome-row-full', { selected: p.selectedOutcome.id === p.outcome.id })}
 				onClick={() => { p.updateSelectedOutcome(p.outcome); }}
 			>
 				{p.marketType === SCALAR ?
 					<ValueDenomination formatted={lastPricePercent} /> :
-					<span className="outcome">{p.outcome.name || ''}</span>
+					<span className="outcome">{outcomeName}</span>
 				}
 				<ValueDenomination formatted={topBidShares} />
 				<ValueDenomination className="emphasized" formatted={topBidPrice} />
 				<ValueDenomination className="emphasized" formatted={topAskPrice} />
 				<ValueDenomination formatted={topAskShares} />
 				<ValueDenomination formatted={lastPrice} />
+			</Link>
+			<Link
+				className={classNames('outcome-row-condensed', { selected: p.selectedOutcome.id === p.outcome.id })}
+				onClick={() => { p.updateSelectedOutcome(p.outcome); }}
+			>
+				{p.marketType === SCALAR ?
+					<ValueDenomination formatted={lastPricePercent} /> :
+					<span className="outcome-summary">
+						<span className="outcome-name">{outcomeName}</span>
+						<span>Last Price: <ValueDenomination formatted={lastPrice} /></span>
+					</span>
+				}
+				<span className="outcome-best-orders outcome-best-bid">
+					<span className="outcome-best-container">
+						<ValueDenomination className="emphasized" formatted={topBidPrice} />
+						<ValueDenomination formatted={topBidShares} />
+					</span>
+				</span>
+				<span className="outcome-best-orders outcome-best-ask">
+					<span className="outcome-best-container">
+						<ValueDenomination className="emphasized" formatted={topAskPrice} />
+						<ValueDenomination formatted={topAskShares} />
+					</span>
+				</span>
 			</Link>
 			<OutcomeTrade
 				marketType={p.marketType}
--- src/modules/outcomes/components/outcomes.jsx
+++ src/modules/outcomes/components/outcomes.jsx
@@ -7,12 +7,19 @@ import { SCALAR } from 'modules/markets/constants/market-types';
 const Outcomes = p => (
 	<article className="outcomes" >
 		<div className="outcomes-header">
-			<span>Outcome{!p.marketType === SCALAR && 's'}</span>
-			<span>Bid Q.</span>
-			<span>Bid</span>
-			<span>Ask</span>
-			<span>Ask Q</span>
-			<span>Last</span>
+			<div className="outcomes-header-full">
+				<span>Outcome{!p.marketType === SCALAR && 's'}</span>
+				<span>Bid Q.</span>
+				<span>Bid</span>
+				<span>Ask</span>
+				<span>Ask Q</span>
+				<span>Last</span>
+			</div>
+			<div className="outcomes-header-condensed">
+				<span>Outcome{!p.marketType === SCALAR && 's'}</span>
+				<span>Bid</span>
+				<span>Ask</span>
+			</div>
 		</div>
 		{(p.outcomes || []).map(outcome => (
 			<Outcome
