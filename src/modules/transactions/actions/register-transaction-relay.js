ae575c200ae6de7c48b5fa3cf444bf1da2ca21f3
Jack Peterson <jack@tinybike.net>
2016-11-28 01:57:10 -0800

    Linting

--- src/modules/transactions/actions/register-transaction-relay.js
+++ src/modules/transactions/actions/register-transaction-relay.js
@@ -2,12 +2,12 @@ import { rpc } from '../../../services/augurjs';
 import { updateTransactionsData } from '../../transactions/actions/update-transactions-data';
 
 export function registerTransactionRelay() {
-    return (dispatch, getState) => {
-        rpc.registerTxRelay((tx) => {
-            console.debug('rpc.txRelay:', tx);
-            if (tx && tx.payload && tx.payload.hash) {
-                dispatch(updateTransactionsData({ [tx.payload.hash]: tx }));
-            }
-        });
-    };
+	return (dispatch, getState) => {
+		rpc.registerTxRelay((tx) => {
+			console.debug('rpc.txRelay:', tx);
+			if (tx && tx.payload && tx.payload.hash) {
+				dispatch(updateTransactionsData({ [tx.payload.hash]: tx }));
+			}
+		});
+	};
 }
