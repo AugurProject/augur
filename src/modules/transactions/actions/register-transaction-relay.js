8115b0c8c99ee4086a7f4e45be1a1d09f64eb628
Jack Peterson <jack@tinybike.net>
2016-11-29 18:10:56 -0800

    Attach formatted timestamp and gasFees (with fallback values) to txRelay update object

--- src/modules/transactions/actions/register-transaction-relay.js
+++ src/modules/transactions/actions/register-transaction-relay.js
@@ -1,13 +1,24 @@
-import { rpc } from '../../../services/augurjs';
-// import { updateTransactionsData } from '../../transactions/actions/update-transactions-data';
+import { augur, rpc } from '../../../services/augurjs';
+import { formatDate } from '../../../utils/format-date';
+import { formatRealEther, formatRealEtherEstimate } from '../../../utils/format-number';
+import { updateTransactionsData } from '../../transactions/actions/update-transactions-data';
 
 export function registerTransactionRelay() {
 	return (dispatch, getState) => {
 		rpc.registerTxRelay((tx) => {
-			console.log('rpc.txRelay:', tx);
-			// if (tx && tx.response && tx.response.hash) {
-			// 	dispatch(updateTransactionsData({ [tx.response.hash]: tx }));
-			// }
+			if (tx && tx.response && tx.response.hash) {
+				const timestamp = tx.response.timestamp ?
+					formatDate(new Date(tx.response.timestamp * 1000)) :
+					formatDate(new Date());
+				const gasFees = tx.response.gasFees ?
+					formatRealEther(tx.response.gasFees) :
+					formatRealEtherEstimate(augur.getTxGasEth({
+						...tx.payload
+					}, rpc.gasPrice));
+				dispatch(updateTransactionsData({
+					[tx.response.hash]: { ...tx, timestamp, gasFees }
+				}));
+			}
 		});
 	};
 }
