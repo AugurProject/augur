7f97420d27f2219b4ad888ef9381cbb25a9f8780
Jack Peterson <jack@tinybike.net>
2016-11-30 23:18:25 -0800

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cp src/sitemap.xml build/sitemap.xml
-git commit -am "${1}"
-git push origin master
+# cp src/sitemap.xml build/sitemap.xml
+# git commit -am "${1}"
+# git push origin master
 firebase deploy
-firebase deploy --project firebase-augur
 $AUGUR_CORE/load_contracts/update_private_chain_augur.sh
 ssh augur@45.33.59.27 /home/augur/augur/update-local.sh
 git push heroku master
