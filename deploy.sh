1f0f0d65c0911f3fa91095ed14a21f41c55a0532
Jack Peterson <jack@tinybike.net>
2016-11-30 23:29:36 -0800

    Really really fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ git commit -am "${1}"
 git push origin master
 firebase deploy
 $AUGUR_CORE/load_contracts/update_private_chain_augur.sh
-ssh augur@45.33.59.27:/home/augur/augur /home/augur/augur/update-local.sh
+ssh augur@45.33.59.27 -t "cd /home/augur/augur; /home/augur/augur/update-local.sh"
 git push heroku master
