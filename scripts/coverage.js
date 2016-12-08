a8f1e79f0f5b4d3fb380da6a88ae1dc2056f3b5f
Stephen Sprinkle <git@sprinkle.io>
2016-12-07 17:12:13 -0700

    fix to coverage script

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -1,3 +1,3 @@
 const shell = require('shelljs');
 
-shell.exec('cd .. && istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rimraf ./coverage');
+shell.exec('istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rimraf ./coverage');
