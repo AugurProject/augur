0ac494125fb6672f2fee8c81ba5d6a2ebf3b4f99
Stephen Sprinkle <git@sprinkle.io>
2016-12-07 17:38:49 -0700

    moment version bump + post-install script fix

--- scripts/lifecycle/post-install.js
+++ scripts/lifecycle/post-install.js
@@ -1,4 +1,4 @@
-const execa = require('execa');
+const shell = require('shelljs');
 
 // Workaround for yarn's lifecycle script handling (failing in these cases)
-execa.shell(`npm i airbitz-core-js-ui augur.js cssnano`);
+shell.exec(`npm i airbitz-core-js-ui augur.js cssnano`);
