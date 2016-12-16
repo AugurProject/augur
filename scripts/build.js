88dc24913c60583cd6c9d753119bbe291fbee914
Stephen Sprinkle <git@sprinkle.io>
2016-12-16 14:03:21 -0700

    updated README + build script (shrinkwrap)

--- scripts/build.js
+++ scripts/build.js
@@ -2,10 +2,11 @@ const path = require('path');
 const shell = require('shelljs');
 
 const BUILD_DIRECTORY = path.resolve(__dirname, '../build');
+const NODE_MODULES = path.resolve(__dirname, '../node_modules');
 
 process.env.NODE_ENV = 'production';
 
 shell.exec(`rimraf ${BUILD_DIRECTORY}`);
 shell.exec(`mkdir -p ${BUILD_DIRECTORY}`);
 shell.exec('webpack --config webpack.config.js');
-shell.exec('npm shrinkwrap');
+shell.exec(`npm shrinkwrap --dev`);
