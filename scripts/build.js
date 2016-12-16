94b27a178bfe1ad0ea8a951a7a7eac83ab5124af
Stephen Sprinkle <git@sprinkle.io>
2016-12-16 15:57:09 -0700

    removal of shrinkwrap in favor of explicit versions in package.json

--- scripts/build.js
+++ scripts/build.js
@@ -9,4 +9,3 @@ process.env.NODE_ENV = 'production';
 shell.exec(`rimraf ${BUILD_DIRECTORY}`);
 shell.exec(`mkdir -p ${BUILD_DIRECTORY}`);
 shell.exec('webpack --config webpack.config.js');
-shell.exec(`npm shrinkwrap --dev`);
