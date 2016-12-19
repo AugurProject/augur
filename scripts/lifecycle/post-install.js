dc271fb8fe41211b5ee88cdf40118624e3454d36
Stephen Sprinkle <stephen@sprinkle.io>
2016-12-19 14:33:44 -0700

    refinement to the post-install script

--- scripts/lifecycle/post-install.js
+++ scripts/lifecycle/post-install.js
@@ -2,7 +2,7 @@ const shell = require('shelljs');
 
 const isYarn = process.env.npm_execpath.indexOf('yarn');
 
-if (isYarn !== -1) {
+if (isYarn !== -1 && process.env.NODE_ENV !== 'production') {
 	// Workaround for yarn's lifecycle script handling (failing in these cases)
 	shell.exec(`npm i airbitz-core-js-ui augur.js cssnano`);
 }
