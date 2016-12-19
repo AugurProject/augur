66e34d5d720705b7bd10502758b53d46b09ffb10
Stephen Sprinkle <stephen@sprinkle.io>
2016-12-19 14:45:54 -0700

    revert package + post-install change

--- scripts/lifecycle/post-install.js
+++ scripts/lifecycle/post-install.js
@@ -2,7 +2,7 @@ const shell = require('shelljs');
 
 const isYarn = process.env.npm_execpath.indexOf('yarn');
 
-if (isYarn !== -1 && process.env.NODE_ENV !== 'production') {
+if (isYarn !== -1) {
 	// Workaround for yarn's lifecycle script handling (failing in these cases)
 	shell.exec(`npm i airbitz-core-js-ui augur.js cssnano`);
 }
