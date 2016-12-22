43e5bf85d7e03c4662acdf2d8c0afba4bc274a87
Stephen Sprinkle <stephen@sprinkle.io>
2016-12-22 09:02:06 -0700

    improved script messaging + exit properly from test script

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,16 @@
 const shell = require('shelljs');
+const ora = require('ora');
 
 process.env.NODE_ENV = 'test';
 
-shell.exec('mocha --require babel-register');
+const spinner = ora('Running Tests').start();
+
+shell.exec('mocha --require babel-register', (code, stdout, stderr) => {
+	if (code !== 0) {
+		spinner.text = 'Tests Failing';
+		spinner.fail();
+		shell.exit(code);
+	}
+
+	spinner.succeed();
+});
