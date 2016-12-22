963d76ae4cb0a23b333d5f6e31c60787fcdac85c
Stephen Sprinkle <stephen@sprinkle.io>
2016-12-22 11:15:25 -0700

    improved linting script + eslint config

--- scripts/lint.js
+++ scripts/lint.js
@@ -1,9 +1,58 @@
 const path = require('path');
 const shell = require('shelljs');
+const Listr = require('listr');
 
 const srcPath = path.resolve(__dirname, '../src');
 const testPath = path.resolve(__dirname, '../test');
 
-shell.exec(`eslint --ext .js .jsx ${srcPath}`);
-shell.exec(`eslint --ext .js ${testPath}`);
-shell.exec(`stylelint '${srcPath}/**/*.less'`);
+const lintSource = new Promise((resolve, reject) => {
+	shell.exec(`eslint --ext .js,.jsx ${srcPath}`, (code) => {
+		if (code !== 0) {
+			reject(new Error());
+			shell.exit(code);
+		}
+
+		resolve();
+	});
+});
+
+const lintTests = new Promise((resolve, reject) => {
+	shell.exec(`eslint --ext .js ${testPath}`, (code) => {
+		if (code !== 0) {
+			reject(new Error());
+			shell.exit(code);
+		}
+
+		resolve();
+	});
+});
+
+const lintStyles = new Promise((resolve, reject) => {
+	shell.exec(`stylelint '${srcPath}/**/*.less'`, (code) => {
+		if (code !== 0) {
+			reject(new Error());
+			shell.exit(code);
+		}
+
+		resolve();
+	});
+});
+
+const tasks = new Listr([
+	{
+		title: 'Linting Source',
+		task: () => lintSource
+	},
+	{
+		title: 'Linting Tests',
+		task: () => lintTests
+	},
+	{
+		title: 'Linting Styles',
+		task: () => lintStyles
+	},
+], {
+	concurrent: true
+});
+
+tasks.run().catch((err) => {});
