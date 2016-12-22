01d5021584e663a3f6b848f621eeb113c6f225d1
Stephen Sprinkle <stephen@sprinkle.io>
2016-12-22 13:50:34 -0700

    update to coverage script

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -2,7 +2,9 @@ const shell = require('shelljs');
 
 shell.echo('== Running Augur Coverage ==');
 
-shell.exec('istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rimraf ./coverage', (code) => {
+process.env.NODE_ENV = 'test';
+
+shell.exec('istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- --require babel-register -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rimraf ./coverage', (code) => {
 	if (code !== 0) {
 		shell.exit(code);
 	}
