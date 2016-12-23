db3b11c26aef8abc63fe2316af772ca5961eacc4
Stephen Sprinkle <stephen@sprinkle.io>
2016-12-23 11:57:11 -0700

    updated tests + cleanup

--- test/market/selectors/market-data-nav-items-test.js
+++ test/market/selectors/market-data-nav-items-test.js
@@ -3,7 +3,7 @@ import { assert } from 'chai';
 
 import marketDataNavItemsAssertions from 'assertions/market-data-nav-items';
 
-import { MARKET_DATA_NAV_OUTCOMES, MARKET_DATA_ORDERS, MARKET_DATA_NAV_CHARTS, MARKET_DATA_NAV_DETAILS } from 'modules/app/constants/views';
+import { MARKET_DATA_NAV_OUTCOMES, MARKET_DATA_ORDERS, MARKET_DATA_NAV_CHARTS, MARKET_DATA_NAV_DETAILS, MARKET_DATA_NAV_REPORT } from 'modules/app/constants/views';
 
 import selector from 'modules/market/selectors/market-data-nav-items';
 
@@ -16,13 +16,17 @@ describe('modules/market/selectors/market-data-nav-items.js', () => {
 		},
 		[MARKET_DATA_ORDERS]: {
 			label: 'Orders',
-			mobileOnly: true
+			isMobile: true
 		},
 		[MARKET_DATA_NAV_CHARTS]: {
 			label: 'Charts'
 		},
 		[MARKET_DATA_NAV_DETAILS]: {
 			label: 'Details'
+		},
+		[MARKET_DATA_NAV_REPORT]: {
+			label: 'Report',
+			isPendingReport: true
 		}
 	};
 
