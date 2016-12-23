cb130a8d7162e73aeb1e4a0c849a9f681e80373a
Jack Peterson <jack@tinybike.net>
2016-12-22 18:41:44 -0800

    Added reported outcome name formatter

--- src/modules/reports/selectors/reportable-outcomes.js
+++ src/modules/reports/selectors/reportable-outcomes.js
@@ -1,22 +1,48 @@
-import { BINARY, CATEGORICAL } from '../../markets/constants/market-types';
-import { BINARY_NO_ID, BINARY_NO_OUTCOME_NAME, BINARY_YES_ID, BINARY_YES_OUTCOME_NAME } from '../../markets/constants/market-outcomes';
+import { augur } from '../../../services/augurjs';
+import { BINARY, CATEGORICAL, SCALAR } from '../../markets/constants/market-types';
+import { BINARY_NO_ID, BINARY_NO_OUTCOME_NAME, BINARY_YES_ID, BINARY_YES_OUTCOME_NAME, CATEGORICAL_SCALAR_INDETERMINATE_OUTCOME_ID, INDETERMINATE_OUTCOME_NAME } from '../../markets/constants/market-outcomes';
 
 export const selectReportableOutcomes = (type, outcomes) => {
 	switch (type) {
 		case BINARY:
-			return [
-				{
-					id: `${BINARY_NO_ID}`,
-					name: BINARY_NO_OUTCOME_NAME
-				},
-				{
-					id: `${BINARY_YES_ID}`,
-					name: BINARY_YES_OUTCOME_NAME
-				}
-			];
+			return [{
+				id: `${BINARY_NO_ID}`,
+				name: BINARY_NO_OUTCOME_NAME
+			}, {
+				id: `${BINARY_YES_ID}`,
+				name: BINARY_YES_OUTCOME_NAME
+			}];
 		case CATEGORICAL:
 			return outcomes.slice();
 		default:
 			return [];
 	}
 };
+
+export function selectOutcomeName(outcomeID, eventType, outcomesData = {}) {
+	let outcomeName;
+	if (eventType === BINARY) {
+		if (outcomeID === '1') {
+			outcomeName = BINARY_NO_OUTCOME_NAME;
+		} else if (outcomeID === '2') {
+			outcomeName = BINARY_YES_OUTCOME_NAME;
+		} else {
+			outcomeName = INDETERMINATE_OUTCOME_NAME;
+		}
+	} else if (outcomeID === CATEGORICAL_SCALAR_INDETERMINATE_OUTCOME_ID) {
+		outcomeName = INDETERMINATE_OUTCOME_NAME;
+	} else if (eventType === SCALAR) {
+		outcomeName = outcomeID;
+	} else {
+		outcomeName = outcomesData[outcomeID] ? outcomesData[outcomeID].name : outcomeID;
+	}
+	return outcomeName;
+}
+
+export function formatReportedOutcome(rawReportedOutcome, isEthical, minValue, maxValue, eventType, outcomesData = {}) {
+	const report = augur.unfixReport(rawReportedOutcome, minValue, maxValue, eventType);
+	if (report.isIndeterminate) return INDETERMINATE_OUTCOME_NAME;
+	const outcomeName = selectOutcomeName(report.report, eventType, outcomesData || {});
+	if (isEthical) return outcomeName;
+	return `${outcomeName} and Unethical`;
+}
