2aa3399ce18e19e8df615a792202a009468a0df0
Jack Peterson <jack@tinybike.net>
2016-12-24 00:32:35 -0800

    Removed market data timestamp stuff

--- src/modules/app/reducers/requests.js
+++ src/modules/app/reducers/requests.js
@@ -1,5 +1,4 @@
 import { MARKET_DATA_LOADING } from '../../market/actions/load-full-market';
-import { UPDATE_MARKET_DATA_TIMESTAMP } from '../../market/actions/update-market-data-timestamp';
 
 // For tracking (AJAX) requests, whether they are running so UI can display loading indicator
 export default (requests = {}, action) => {
@@ -12,16 +11,6 @@ export default (requests = {}, action) => {
 					[action.marketID]: true
 				}
 			};
-		case UPDATE_MARKET_DATA_TIMESTAMP: {
-			const newRequests = { ...requests };
-			if (newRequests[MARKET_DATA_LOADING] != null) {
-				if (newRequests[MARKET_DATA_LOADING][action.marketID] === true) {
-					delete newRequests[MARKET_DATA_LOADING][action.marketID];
-					return newRequests;
-				}
-			}
-			return requests;
-		}
 		default:
 			return requests;
 	}
--- src/reducers.js
+++ src/reducers.js
@@ -12,7 +12,6 @@ import loginAccount from './modules/auth/reducers/login-account';
 import activeView from './modules/app/reducers/active-view';
 
 import marketsData from './modules/markets/reducers/markets-data';
-import marketDataTimestamps from './modules/markets/reducers/market-data-timestamps';
 import outcomesData from './modules/markets/reducers/outcomes-data';
 import favorites from './modules/markets/reducers/favorites';
 import pagination from './modules/markets/reducers/pagination';
@@ -65,7 +64,6 @@ export function createReducer() {
 		activeView,
 
 		marketsData,
-		marketDataTimestamps,
 		outcomesData,
 		favorites,
 		pagination,
