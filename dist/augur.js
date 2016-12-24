d5eabfbe0b61117507e8ea8cf7d92a592183f48f
Jack Peterson <jack@tinybike.net>
2016-12-24 04:14:14 -0800

    Incremented version

--- dist/augur.js
+++ dist/augur.js
@@ -41558,11 +41558,11 @@ var modules = [
 ];
 
 function Augur() {
-    this.version = "3.4.3";
+    this.version = "3.4.4";
 
     this.options = {
         debug: {
-            tools: true,       // if true, testing tools (test/tools.js) included
+            tools: false,       // if true, testing tools (test/tools.js) included
             abi: false,         // debug logging in augur-abi
             broadcast: false,   // broadcast debug logging in ethrpc
             connect: false,     // connection debug logging in ethereumjs-connect
