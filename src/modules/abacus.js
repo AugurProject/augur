1f66c6ef7e3143840e40088d873ba8bda1c84f46
Jack Peterson <jack@tinybike.net>
2016-12-24 02:20:17 -0800

    Added calculateValidityBond method to modules/abacus

--- src/modules/abacus.js
+++ src/modules/abacus.js
@@ -84,6 +84,21 @@ module.exports = {
         };
     },
 
+    calculateValidityBond: function (tradingFee, periodLength, baseReporters, numEventsCreatedInPast24Hours, numEventsInReportPeriod) {
+        var bnPeriodLength = abi.bignum(periodLength);
+        var bnBaseReporters = abi.bignum(baseReporters);
+        var bnPast24 = abi.bignum(numEventsCreatedInPast24Hours);
+        var bnNumberEvents = abi.bignum(numEventsInReportPeriod);
+        var bnGasPrice = abi.bignum(this.rpc.gasPrice);
+        var creationFee = abi.bignum("0.03").times(bnBaseReporters).dividedBy(tradingFee);
+        var minFee = constants.COST_PER_REPORTER.times(bnBaseReporters).times(bnGasPrice);
+        if (creationFee.lt(minFee)) creationFee = minFee;
+        return creationFee.plus(bnPast24.dividedBy(bnPeriodLength).plus(1))
+            .dividedBy(bnNumberEvents.plus(1))
+            .dividedBy(2)
+            .toFixed();
+    },
+
     // type: "buy" or "sell"
     // gasLimit (optional): block gas limit as an integer
     maxOrdersPerTrade: function (type, gasLimit) {
