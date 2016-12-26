839412970b6afdc684657d19853a907856996b39
Jack Peterson <jack@tinybike.net>
2016-12-25 17:47:15 -0800

    Only register timestamp during Airbitz login if new account

--- src/modules/auth/actions/login-with-airbitz.js
+++ src/modules/auth/actions/login-with-airbitz.js
@@ -1,60 +1,63 @@
 import secureRandom from 'secure-random';
-import { augur } from '../../../services/augurjs';
+import { abi, augur, constants } from '../../../services/augurjs';
+import { AIRBITZ_WALLET_TYPE } from '../../auth/constants/auth-types';
 import {
 	loadLoginAccountDependents,
 	loadLoginAccountLocalStorage
 } from '../../auth/actions/load-login-account';
 import { updateLoginAccount } from '../../auth/actions/update-login-account';
+import { updateAssets } from '../../auth/actions/update-assets';
 import { authError } from '../../auth/actions/auth-error';
-import { addFundNewAccount } from '../../transactions/actions/add-fund-new-account-transaction';
 
-const walletType = 'wallet:repo:ethereum';
+export function loginWithEthereumWallet(airbitzAccount, ethereumWallet) {
+	return (dispatch, getState) => {
+		const { links } = require('../../../selectors');
 
-function loginWithEthereumWallet(dispatch, airbitzAccount, ethereumWallet) {
-	const { links } = require('../../../selectors');
-
-	const masterPrivateKey = ethereumWallet.keys.ethereumKey;
-	augur.web.loginWithMasterKey(airbitzAccount.username, masterPrivateKey, (account) => {
-		console.log(account);
-		if (!account) {
-			return dispatch(authError({ code: 0, message: 'failed to login' }));
-		} else if (account.error) {
-			return dispatch(authError({ code: account.error, message: account.message }));
-		}
-		const loginAccount = { ...account, airbitzAccount };
-		if (!loginAccount || !loginAccount.address) {
-			return;
-		}
-		dispatch(loadLoginAccountLocalStorage(loginAccount.address));
-		dispatch(updateLoginAccount(loginAccount));
-		dispatch(loadLoginAccountDependents((err, balances) => {
-			if (err || !balances) {
-				return console.error('loadLoginAccountDependents:', err);
+		const masterPrivateKey = ethereumWallet.keys.ethereumKey;
+		augur.web.loginWithMasterKey(airbitzAccount.username, masterPrivateKey, (account) => {
+			console.log(account);
+			if (!account) {
+				return dispatch(authError({ code: 0, message: 'failed to login' }));
+			} else if (account.error) {
+				return dispatch(authError({ code: account.error, message: account.message }));
 			}
-			if (balances.ether === '0') {
-				dispatch(addFundNewAccount(loginAccount.address));
+			const loginAccount = { ...account, airbitzAccount };
+			if (!loginAccount || !loginAccount.address) {
+				return;
 			}
-		}));
-		if (links && links.marketsLink)	{
-			links.marketsLink.onClick(links.marketsLink.href);
-		}
-	});
+			dispatch(loadLoginAccountLocalStorage(loginAccount.address));
+			dispatch(updateLoginAccount(loginAccount));
+			dispatch(loadLoginAccountDependents((err, balances) => {
+				if (err || !balances) {
+					return console.error('loadLoginAccountDependents:', err);
+				}
+				if (!balances.ether || abi.bignum(balances.ether).eq(constants.ZERO)) {
+					augur.fundNewAccount(getState().branch.id, augur.utils.noop, () => dispatch(updateAssets()), e => console.error(e));
+				}
+			}));
+			if (links && links.marketsLink)	{
+				links.marketsLink.onClick(links.marketsLink.href);
+			}
+		});
+	};
 }
 
 export function loginWithAirbitz(airbitzAccount) {
 	return (dispatch, getState) => {
-		const ethereumWallet = airbitzAccount.getFirstWallet(walletType);
+		const ethereumWallet = airbitzAccount.getFirstWallet(AIRBITZ_WALLET_TYPE);
+
+		// Create an ethereum wallet if one doesn't exist
 		if (ethereumWallet == null) {
-			// Create an ethereum wallet if one doesn't exist:
 			const keys = {
 				ethereumKey: new Buffer(secureRandom(32)).toString('hex')
 			};
-			airbitzAccount.createWallet(walletType, keys, (err, id) => {
+			airbitzAccount.createWallet(AIRBITZ_WALLET_TYPE, keys, (err, id) => {
 				if (err) return dispatch(authError({ code: 0, message: 'could not create wallet' }));
-				loginWithEthereumWallet(dispatch, airbitzAccount, airbitzAccount.getWallet(id));
+				dispatch(loginWithEthereumWallet(airbitzAccount, airbitzAccount.getWallet(id)));
 			});
+
 		} else {
-			loginWithEthereumWallet(dispatch, airbitzAccount, ethereumWallet);
+			dispatch(loginWithEthereumWallet(airbitzAccount, ethereumWallet));
 		}
 	};
 }
