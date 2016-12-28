420ba6ef58c36c191417da78cea672e70d0b7164
Jack Peterson <jack@tinybike.net>
2016-12-28 13:34:34 -0800

    Refactored loadMarketsInfo: moved branch ID filtering and created market info loading into separate functions

--- src/modules/markets/actions/load-markets-info.js
+++ src/modules/markets/actions/load-markets-info.js
@@ -5,34 +5,44 @@ import { loadMarketCreatorFees } from '../../my-markets/actions/load-market-crea
 
 const MARKETS_PER_BATCH = 10;
 
+export function loadCreatedMarketInfo(marketID) {
+	return (dispatch, getState) => {
+		const { loginAccount, marketsData } = getState();
+		if (marketsData[marketID].author === loginAccount.address) {
+			dispatch(loadFullMarket(marketID));
+			dispatch(loadMarketCreatorFees(marketID));
+		}
+	};
+}
+
+export function filterMarketsInfoByBranch(marketsData, branchID) {
+	const marketInfoIDs = Object.keys(marketsData);
+	const numMarkets = marketInfoIDs.length;
+	for (let i = 0; i < numMarkets; ++i) {
+		if (marketsData[marketInfoIDs[i]].branchId !== branchID) {
+			delete marketsData[marketInfoIDs[i]];
+		}
+	}
+	return marketsData;
+}
+
 export function loadMarketsInfo(marketIDs, cb) {
 	return (dispatch, getState) => {
 		const numMarketsToLoad = marketIDs.length;
 		(function loader(stepStart) {
 			const stepEnd = stepStart + MARKETS_PER_BATCH;
 			const marketsToLoad = marketIDs.slice(stepStart, Math.min(numMarketsToLoad, stepEnd));
-			const { loginAccount } = getState();
-			augur.batchGetMarketInfo(marketsToLoad, loginAccount.address, (marketsData) => {
+			augur.batchGetMarketInfo(marketsToLoad, getState().loginAccount.address, (marketsData) => {
 				if (!marketsData || marketsData.error) {
 					console.error('ERROR loadMarketsInfo()', marketsData);
 				} else {
-					const branchID = getState().branch.id;
-					let marketInfoIDs = Object.keys(marketsData);
-					const numMarkets = marketInfoIDs.length;
-					for (let i = 0; i < numMarkets; ++i) {
-						if (marketsData[marketInfoIDs[i]].branchId !== branchID) {
-							delete marketsData[marketInfoIDs[i]];
-						}
-					}
-					marketInfoIDs = Object.keys(marketsData);
+					const branchMarketsData = filterMarketsInfoByBranch(marketsData, getState().branch.id);
+					const marketInfoIDs = Object.keys(branchMarketsData);
 					if (marketInfoIDs.length) {
-						dispatch(updateMarketsData(marketsData));
+						dispatch(updateMarketsData(branchMarketsData));
 						marketInfoIDs.forEach((marketID) => {
-							dispatch(updateEventMarketsMap(marketsData[marketID].events[0].id, [marketID]));
-							if (marketsData[marketID].author === loginAccount.address) {
-								dispatch(loadFullMarket(marketID));
-								dispatch(loadMarketCreatorFees(marketID));
-							}
+							dispatch(updateEventMarketsMap(branchMarketsData[marketID].events[0].id, [marketID]));
+							dispatch(loadCreatedMarketInfo(marketID));
 						});
 					}
 				}
