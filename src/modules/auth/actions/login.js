bea1fef6f412b960fc86cf526b0dd2d08a7ab346
Jack Peterson <jack@tinybike.net>
2017-01-02 01:18:24 -0800

    Updated conditional funding logic in login callback

--- src/modules/auth/actions/login.js
+++ src/modules/auth/actions/login.js
@@ -1,18 +1,17 @@
-import { augur } from '../../../services/augurjs';
+import { abi, accounts, constants } from '../../../services/augurjs';
 import {
 	loadLoginAccountDependents,
 	loadLoginAccountLocalStorage
 } from '../../auth/actions/load-login-account';
 import { updateLoginAccount } from '../../auth/actions/update-login-account';
 import { authError } from '../../auth/actions/auth-error';
-import { addFundNewAccount } from '../../transactions/actions/add-fund-new-account-transaction';
-import isCurrentLoginMessageRead from '../../login-message/helpers/is-current-login-message-read';
 import { updateAccountSettings } from '../../auth/actions/update-account-settings';
+import { fundNewAccount } from '../../auth/actions/fund-new-account';
+import isCurrentLoginMessageRead from '../../login-message/helpers/is-current-login-message-read';
 
-export function login(secureLoginID, password, rememberMe) {
+export function login(loginID, password, rememberMe) {
 	return (dispatch, getState) => {
-		const localStorageRef = typeof window !== 'undefined' && window.localStorage;
-		augur.web.login(secureLoginID, password, (account) => {
+		accounts.login(loginID, password, (account) => {
 			if (!account) {
 				return dispatch(authError({ code: 0, message: 'failed to login' }));
 			} else if (account.error) {
@@ -20,40 +19,37 @@ export function login(secureLoginID, password, rememberMe) {
 			}
 			const loginAccount = {
 				...account,
-				loginID: account.loginID || account.secureLoginID,
 				settings: {},
 				onUpdateAccountSettings: settings => dispatch(updateAccountSettings(settings))
 			};
-			if (!loginAccount || !loginAccount.address) {
-				return;
-			}
-			if (rememberMe && localStorageRef && localStorageRef.setItem) {
-				const persistentAccount = Object.assign({}, loginAccount);
-				if (Buffer.isBuffer(persistentAccount.privateKey)) {
-					persistentAccount.privateKey = persistentAccount.privateKey.toString('hex');
+			if (loginAccount && loginAccount.address) {
+				const localStorageRef = typeof window !== 'undefined' && window.localStorage;
+				if (rememberMe && localStorageRef && localStorageRef.setItem) {
+					const persistentAccount = Object.assign({}, loginAccount);
+					if (Buffer.isBuffer(persistentAccount.privateKey)) {
+						persistentAccount.privateKey = persistentAccount.privateKey.toString('hex');
+					}
+					if (Buffer.isBuffer(persistentAccount.derivedKey)) {
+						persistentAccount.derivedKey = persistentAccount.derivedKey.toString('hex');
+					}
+					localStorageRef.setItem('account', JSON.stringify(persistentAccount));
 				}
-				if (Buffer.isBuffer(persistentAccount.derivedKey)) {
-					persistentAccount.derivedKey = persistentAccount.derivedKey.toString('hex');
-				}
-				localStorageRef.setItem('account', JSON.stringify(persistentAccount));
-			}
-			dispatch(loadLoginAccountLocalStorage(loginAccount.address));
-			dispatch(updateLoginAccount(loginAccount));
-			dispatch(loadLoginAccountDependents((err, balances) => {
-				if (err || !balances) {
-					return console.error('loadLoginAccountDependents:', err);
-				}
-				if (balances.ether === '0') {
-					dispatch(addFundNewAccount(loginAccount.address));
-				}
-			}));
+				dispatch(loadLoginAccountLocalStorage(loginAccount.address));
+				dispatch(updateLoginAccount(loginAccount));
+				dispatch(loadLoginAccountDependents((err, balances) => {
+					if (err || !balances) return console.error(err);
+					if (!balances.ether || abi.bignum(balances.ether).eq(constants.ZERO)) {
+						dispatch(fundNewAccount());
+					}
+				}));
 
-			// need to load selectors here as they get updated above
-			const { links } = require('../../../selectors');
-			if (isCurrentLoginMessageRead(getState().loginMessage)) {
-				links.marketsLink.onClick(links.marketsLink.href);
-			} else {
-				links.loginMessageLink.onClick();
+				// need to load selectors here as they get updated above
+				const { links } = require('../../../selectors');
+				if (isCurrentLoginMessageRead(getState().loginMessage)) {
+					links.marketsLink.onClick(links.marketsLink.href);
+				} else {
+					links.loginMessageLink.onClick();
+				}
 			}
 		});
 	};
