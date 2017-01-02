52ae32bcab71be49ecc317e6dd8c8a47e3af7b30
Jack Peterson <jack@tinybike.net>
2017-01-02 15:14:29 -0800

    Linting

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,5 +1,5 @@
-import { abi, constants } from '../../../services/augurjs';
+import { abi, constants } from '../services/augurjs';
 
-export const isAlmostZero = (n) => abi.bignum(n).abs().lte(constants.PRECISION.zero);
+export const isAlmostZero = n => abi.bignum(n).abs().lte(constants.PRECISION.zero);
 
-export const isZero = (n) => abi.bignum(n).eq(constants.ZERO);
+export const isZero = n => abi.bignum(n).eq(constants.ZERO);
