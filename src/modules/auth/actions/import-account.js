41ce9356b1d0f679b88b487df1124109e7b258f8
Jack Peterson <jack@tinybike.net>
2017-01-02 23:27:55 -0800

    Removed catch-all try/catch in importAccount

--- src/modules/auth/actions/import-account.js
+++ src/modules/auth/actions/import-account.js
@@ -1,9 +1,8 @@
-import { augur } from '../../../services/augurjs';
+import { accounts } from '../../../services/augurjs';
 import {
 	loadLoginAccountDependents,
 	loadLoginAccountLocalStorage
 } from '../../auth/actions/load-login-account';
-import { authError } from '../../auth/actions/auth-error';
 import { updateLoginAccount } from '../../auth/actions/update-login-account';
 import { fundNewAccount } from '../../auth/actions/fund-new-account';
 import { registerTimestamp } from '../../auth/actions/register-timestamp';
@@ -13,36 +12,31 @@ export function importAccount(name, password, rememberMe, keystore) {
 	return (dispatch, getState) => {
 		const { links } = require('../../../selectors');
 		const localStorageRef = typeof window !== 'undefined' && window.localStorage;
-		try {
-			augur.web.importAccount(name, password, keystore, (loginAccount) => {
-				const importedAccount = { ...loginAccount };
-				if (importedAccount && importedAccount.keystore) {
-					if (rememberMe && localStorageRef && localStorageRef.setItem) {
-						const persistentAccount = Object.assign({}, importedAccount);
-						if (Buffer.isBuffer(persistentAccount.privateKey)) {
-							persistentAccount.privateKey = persistentAccount.privateKey.toString('hex');
-						}
-						localStorageRef.setItem('account', JSON.stringify(persistentAccount));
+		accounts.importAccount(name, password, keystore, (loginAccount) => {
+			const importedAccount = { ...loginAccount };
+			if (importedAccount && importedAccount.keystore) {
+				if (rememberMe && localStorageRef && localStorageRef.setItem) {
+					const persistentAccount = Object.assign({}, importedAccount);
+					if (Buffer.isBuffer(persistentAccount.privateKey)) {
+						persistentAccount.privateKey = persistentAccount.privateKey.toString('hex');
 					}
-					dispatch(loadLoginAccountLocalStorage(importedAccount.address));
-					dispatch(updateLoginAccount(importedAccount));
-					dispatch(loadLoginAccountDependents((err, balances) => {
-						if (err || !balances) return console.error(err);
-						if (!getState().loginAccount.registerBlockNumber) {
-							dispatch(registerTimestamp());
-						}
-						if (anyAccountBalancesZero(balances)) {
-							dispatch(fundNewAccount(e => e && console.error(e)));
-						}
-					}));
-					if (links && links.marketsLink)	{
-						return links.marketsLink.onClick(links.marketsLink.href);
-					}
+					localStorageRef.setItem('account', JSON.stringify(persistentAccount));
 				}
-			});
-		} catch (exc) {
-			console.error(exc);
-			dispatch(authError(exc));
-		}
+				dispatch(loadLoginAccountLocalStorage(importedAccount.address));
+				dispatch(updateLoginAccount(importedAccount));
+				dispatch(loadLoginAccountDependents((err, balances) => {
+					if (err || !balances) return console.error(err);
+					if (!getState().loginAccount.registerBlockNumber) {
+						dispatch(registerTimestamp());
+					}
+					if (anyAccountBalancesZero(balances)) {
+						dispatch(fundNewAccount(e => e && console.error(e)));
+					}
+				}));
+				if (links && links.marketsLink)	{
+					return links.marketsLink.onClick(links.marketsLink.href);
+				}
+			}
+		});
 	};
 }
