c1b3991eafcbc7719ecdc7e8827139c15061377a
Jack Peterson <jack@tinybike.net>
2017-01-04 01:12:56 -0800

    Removed manual transaction display updates from cancel transactions

--- src/modules/bids-asks/actions/cancel-order.js
+++ src/modules/bids-asks/actions/cancel-order.js
@@ -1,12 +1,7 @@
-import { formatEther, formatShares, formatRealEtherEstimate } from '../../../utils/format-number';
-import { addCancelTransaction } from '../../transactions/actions/add-cancel-transaction';
-import { updateOrderStatus } from '../../bids-asks/actions/update-order-status';
-import { updateExistingTransaction } from '../../transactions/actions/update-existing-transaction';
-import getOrder from '../../bids-asks/helpers/get-order';
 import { augur } from '../../../services/augurjs';
-import { deleteTransaction } from '../../transactions/actions/delete-transaction';
+import { updateOrderStatus } from '../../bids-asks/actions/update-order-status';
+import getOrder from '../../bids-asks/helpers/get-order';
 import { CANCELLED, CANCELLING, CANCELLATION_FAILED } from '../../bids-asks/constants/order-status';
-import { CANCELLING_ORDER, FAILED } from '../../transactions/constants/statuses';
 
 const TIME_TO_WAIT_BEFORE_FINAL_ACTION_MILLIS = 3000;
 
@@ -18,56 +13,25 @@ export function cancelOrder(orderID, marketID, type) {
 		const { orderBooks, outcomesData, marketsData } = getState();
 		const order = getOrder(orderID, marketID, type, orderBooks);
 		const market = marketsData[marketID];
-
-		if (order == null || market == null || outcomesData[marketID] == null) {
-			return;
-		}
-
-		const outcome = outcomesData[marketID][order.outcome];
-		if (outcome == null) {
-			return;
-		}
-
-		dispatch(addCancelTransaction(order, { ...market, id: marketID }, outcome));
-	};
-}
-
-export function processCancelOrder(transactionID, orderID) {
-	return (dispatch, getState) => {
-
-		const { transactionsData, orderBooks } = getState();
-		const transaction = transactionsData[transactionID];
-		if (transaction == null) {
-			return;
-		}
-
-		const order = getOrder(orderID, transaction.data.market.id, transaction.data.order.type, orderBooks);
-		if (order == null || !order.amount || !order.price) {
-			return;
-		}
-
-		dispatch(updateOrderStatus(orderID, CANCELLING, transaction.data.market.id, transaction.data.order.type));
-		dispatch(updateExistingTransaction(transactionID, {
-			status: CANCELLING_ORDER,
-			message: `canceling order to ${order.type} ${formatShares(order.amount).full} for ${formatEther(order.price).full} each`,
-			gasFees: formatRealEtherEstimate(augur.getTxGasEth({ ...augur.tx.BuyAndSellShares.cancel }, augur.rpc.gasPrice))
-		}));
-
-		augur.cancel({
-			trade_id: orderID,
-			onSent: res => console.log('augur.cancel sent: %o', res),
-			onSuccess: (res) => {
-				console.log('augur.cancel success: %o', res);
-				dispatch(updateOrderStatus(orderID, CANCELLED, transaction.data.market.id, transaction.data.order.type));
-				dispatch(deleteTransaction(transactionID));
-			},
-			onFailed: (res) => {
-				console.log('augur.cancel failed: %o', res);
-				dispatch(updateOrderStatus(orderID, CANCELLATION_FAILED, transaction.data.market.id, transaction.data.order.type));
-				dispatch(updateExistingTransaction(transactionID, { status: FAILED }));
-				setTimeout(() => dispatch(updateOrderStatus(orderID, null, transaction.data.market.id, transaction.data.order.type)), TIME_TO_WAIT_BEFORE_FINAL_ACTION_MILLIS);
+		if (order != null && market != null && outcomesData[marketID] != null) {
+			const outcome = outcomesData[marketID][order.outcome];
+			if (outcome != null) {
+				dispatch(updateOrderStatus(orderID, CANCELLING, marketID, type));
+				augur.cancel({
+					trade_id: orderID,
+					onSent: res => console.log('cancel sent:', res),
+					onSuccess: (res) => {
+						console.log('cancel success:', res);
+						dispatch(updateOrderStatus(orderID, CANCELLED, marketID, type));
+					},
+					onFailed: (err) => {
+						console.error('cancel failed:', err);
+						dispatch(updateOrderStatus(orderID, CANCELLATION_FAILED, marketID, type));
+						setTimeout(() => dispatch(updateOrderStatus(orderID, null, marketID, type)), TIME_TO_WAIT_BEFORE_FINAL_ACTION_MILLIS);
+					}
+				});
 			}
-		});
+		}
 	};
 }
 
