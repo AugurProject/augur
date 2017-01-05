b60d96ed2a7766f00e4032ac30f07789ac896c14
Jack Peterson <jack@tinybike.net>
2017-01-04 22:45:06 -0800

    Consolidated create market actions into submitNewMarket; removed manual create market transaction updates

--- src/modules/create-market/actions/submit-new-market.js
+++ src/modules/create-market/actions/submit-new-market.js
@@ -1,55 +1,35 @@
 import { BINARY, CATEGORICAL, SCALAR } from '../../markets/constants/market-types';
-import { FAILED, CREATING_MARKET } from '../../transactions/constants/statuses';
 import { CATEGORICAL_OUTCOMES_SEPARATOR, CATEGORICAL_OUTCOME_SEPARATOR } from '../../markets/constants/market-outcomes';
-import { BRANCH_ID } from '../../app/constants/network';
 import { augur } from '../../../services/augurjs';
-import { updateExistingTransaction } from '../../transactions/actions/update-existing-transaction';
-import { addCreateMarketTransaction } from '../../transactions/actions/add-create-market-transaction';
 import { selectTransactionsLink } from '../../link/selectors/links';
 import { submitGenerateOrderBook } from '../../create-market/actions/generate-order-book';
 import { clearMakeInProgress } from '../../create-market/actions/update-make-in-progress';
-import { deleteTransaction } from '../../transactions/actions/delete-transaction';
 
 export function submitNewMarket(newMarket) {
-	return (dispatch) => {
-		selectTransactionsLink(dispatch).onClick();
-		dispatch(addCreateMarketTransaction(newMarket));
-	};
-}
-
-export function createMarket(transactionID, newMarket) {
 	return (dispatch, getState) => {
+		selectTransactionsLink(dispatch).onClick();
 		const { branch } = getState();
-
-		if (newMarket.type === BINARY) {
-			newMarket.minValue = 1;
-			newMarket.maxValue = 2;
-			newMarket.numOutcomes = 2;
-		} else if (newMarket.type === SCALAR) {
-			newMarket.minValue = newMarket.scalarSmallNum;
-			newMarket.maxValue = newMarket.scalarBigNum;
-			newMarket.numOutcomes = 2;
-		} else if (newMarket.type === CATEGORICAL) {
-			newMarket.minValue = 1;
-			newMarket.maxValue = newMarket.outcomes.length;
-			newMarket.numOutcomes = newMarket.outcomes.length;
-			newMarket.formattedDescription = newMarket.description + CATEGORICAL_OUTCOMES_SEPARATOR + newMarket.outcomes.map(outcome => outcome.name).join(CATEGORICAL_OUTCOME_SEPARATOR);
-		} else {
-			console.warn('createMarket unsupported type:', newMarket.type);
-			return;
+		switch (newMarket.type) {
+			case CATEGORICAL:
+				newMarket.minValue = 1;
+				newMarket.maxValue = newMarket.outcomes.length;
+				newMarket.numOutcomes = newMarket.outcomes.length;
+				newMarket.formattedDescription = newMarket.description + CATEGORICAL_OUTCOMES_SEPARATOR + newMarket.outcomes.map(outcome => outcome.name).join(CATEGORICAL_OUTCOME_SEPARATOR);
+				break;
+			case SCALAR:
+				newMarket.minValue = newMarket.scalarSmallNum;
+				newMarket.maxValue = newMarket.scalarBigNum;
+				newMarket.numOutcomes = 2;
+				break;
+			case BINARY:
+			default:
+				newMarket.minValue = 1;
+				newMarket.maxValue = 2;
+				newMarket.numOutcomes = 2;
 		}
-
-		dispatch(updateExistingTransaction(transactionID, {
-			status: 'sending...',
-			marketCreationFee: newMarket.marketCreationFee,
-			bond: { label: 'event validity', value: newMarket.eventBond },
-			gasFees: newMarket.gasFees
-		}));
-
 		console.log('creating market:', newMarket);
-
 		augur.createSingleEventMarket({
-			branchId: branch.id || BRANCH_ID,
+			branchId: branch.id,
 			description: newMarket.formattedDescription,
 			expDate: newMarket.endDate.value.getTime() / 1000,
 			minValue: newMarket.minValue,
@@ -60,12 +40,9 @@ export function createMarket(transactionID, newMarket) {
 			tags: newMarket.tags,
 			makerFee: newMarket.makerFee / 100,
 			extraInfo: newMarket.detailsText,
-			onSent: (res) => {
-				dispatch(updateExistingTransaction(transactionID, { status: CREATING_MARKET }));
-			},
+			onSent: res => console.log('createSingleEventMarket sent:', res),
 			onSuccess: (res) => {
-				console.log('success:', res);
-				dispatch(deleteTransaction(transactionID));
+				console.log('createSingleEventMarket success:', res);
 				dispatch(clearMakeInProgress());
 				if (newMarket.isCreatingOrderBook) {
 					dispatch(submitGenerateOrderBook({
@@ -75,12 +52,7 @@ export function createMarket(transactionID, newMarket) {
 					}));
 				}
 			},
-			onFailed: (err) => {
-				dispatch(updateExistingTransaction(transactionID, {
-					status: FAILED,
-					message: err.message
-				}));
-			}
+			onFailed: err => console.error('createSingleEventMarket failed:', err)
 		});
 	};
 }
