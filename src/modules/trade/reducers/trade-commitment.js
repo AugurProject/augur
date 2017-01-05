8e13b4e2138b8d07781589bc973184b9daa76e85
Jack Peterson <jack@tinybike.net>
2017-01-05 02:23:46 -0800

    Used array for tradeCommitment store

--- src/modules/trade/reducers/trade-commitment.js
+++ src/modules/trade/reducers/trade-commitment.js
@@ -1,12 +1,9 @@
 import { UPDATE_TRADE_COMMITMENT } from '../../trade/actions/update-trade-commitment';
 
-export default function (tradeCommitment = {}, action) {
+export default function (tradeCommitment = [], action) {
 	switch (action.type) {
 		case UPDATE_TRADE_COMMITMENT:
-			return {
-				...tradeCommitment,
-				tradeIDs: action.tradeIDs.slice()
-			};
+			return action.tradeIDs.slice();
 		default:
 			return tradeCommitment;
 	}
