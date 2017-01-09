8c5f2ebc068a2f75e50f3a0d51c39b56eaec4974
Jack Peterson <jack@tinybike.net>
2017-01-08 22:10:33 -0800

    Updated load-markets-info tests

--- test/markets/actions/load-markets-info-test.js
+++ test/markets/actions/load-markets-info-test.js
@@ -17,8 +17,10 @@ describe(`modules/markets/actions/load-markets-info.js`, () => {
 	};
 	const mockLoadFullMarket = {};
 	const mockLoadMarketCreatorFees = {};
+	const mockLoadCreatedMarketInfo = {};
 	mockLoadFullMarket.loadFullMarket = sinon.stub().returns({ type: 'MOCK_LOAD_FULL_MARKET' });
 	mockLoadMarketCreatorFees.loadMarketCreatorFees = sinon.stub().returns({ type: 'MOCK_LOAD_MARKET_CREATOR_FEES' });
+	mockLoadCreatedMarketInfo.loadCreatedMarketInfo = sinon.stub().returns({ type: 'MOCK_LOAD_CREATED_MARKET_INFO' });
 
 	beforeEach(() => {
 		store.clearActions();
@@ -53,7 +55,7 @@ describe(`modules/markets/actions/load-markets-info.js`, () => {
 
 	const action = proxyquire('../../../src/modules/markets/actions/load-markets-info', {
 		'../../market/actions/load-full-market': mockLoadFullMarket,
-		'../../my-markets/actions/load-create-market-info': mockLoadCreateMarketInfo,
+		'../../my-markets/actions/load-created-market-info': mockLoadCreatedMarketInfo,
 		'../../my-markets/actions/load-market-creator-fees': mockLoadMarketCreatorFees,
 		'../../../services/augurjs': mockAugurJS
 	});
@@ -64,7 +66,7 @@ describe(`modules/markets/actions/load-markets-info.js`, () => {
 			marketsData: {
 				test123: {
 					author: '0x0000000000000000000000000000000000000001',
-					branchId: testState.branch.id,
+					branchId: '0xf69b5',
 					events: [{
 						id: 'event1',
 						minValue: 1,
@@ -82,9 +84,13 @@ describe(`modules/markets/actions/load-markets-info.js`, () => {
 				}
 			}
 		}, {
-			type: 'MOCK_LOAD_FULL_MARKET'
+			type: 'UPDATE_EVENT_MARKETS_MAP',
+			eventID: 'event1',
+			marketIDs: [
+				'test123'
+			]
 		}, {
-			type: 'MOCK_LOAD_MARKET_CREATOR_FEES'
+			type: 'MOCK_LOAD_CREATED_MARKET_INFO'
 		}];
 		store.dispatch(action.loadMarketsInfo(['test123']));
 		assert.deepEqual(store.getActions(), out, `Didn't dispatch the expected action objects`);
