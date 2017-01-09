757b26d0d568a15a9b55b6e6dda1d24baf2c9cbe
Jack Peterson <jack@tinybike.net>
2017-01-08 21:17:20 -0800

    Moved transfer history log lookups to loadTransferHistory function so log search is over both _from and _to fields

--- src/modules/account/actions/load-funding-history.js
+++ src/modules/account/actions/load-funding-history.js
@@ -15,9 +15,7 @@ export function loadFundingHistory(cb) {
 			'registration',
 			'deposit',
 			'withdraw',
-			'Approval',
-			'Transfer',
-			'sentCash'
+			'Approval'
 		], constants.PARALLEL_LIMIT, (label, nextLabel) => {
 			const p = label === 'fundedAccount' ? { ...params, fromBlock: null } : params;
 			augur.getLogs(label, p, null, (err, logs) => {
@@ -29,3 +27,31 @@ export function loadFundingHistory(cb) {
 		}, callback);
 	};
 }
+
+export function loadTransferHistory(cb) {
+	return (dispatch, getState) => {
+		const callback = cb || (e => console.log('loadTransferHistory:', e));
+		const { loginAccount } = getState();
+		const params = {};
+		if (loginAccount.registerBlockNumber) {
+			params.fromBlock = loginAccount.registerBlockNumber;
+		}
+		async.eachLimit([
+			'Transfer',
+			'sentCash'
+		], constants.PARALLEL_LIMIT, (label, nextLabel) => {
+			augur.getLogs(label, { ...params, _from: loginAccount.address }, null, (err, logs) => {
+				console.log(label, params, err, logs);
+				if (err) return nextLabel(err);
+				if (logs && logs.length) dispatch(convertLogsToTransactions(label, logs));
+				augur.getLogs(label, { ...params, _to: loginAccount.address }, null, (err, logs) => {
+					console.log(label, params, err, logs);
+					if (err) return nextLabel(err);
+					if (logs && logs.length) dispatch(convertLogsToTransactions(label, logs));
+					nextLabel();
+				});
+			});
+		}, callback);
+	};
+}
+
