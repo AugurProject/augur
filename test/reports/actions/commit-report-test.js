3d77bb7a7aa9437caef6169c4827820f6f935bb2
Jack Peterson <jack@tinybike.net>
2017-01-10 13:04:11 -0800

    Updated commit-report tests

--- test/reports/actions/commit-report-test.js
+++ test/reports/actions/commit-report-test.js
@@ -9,20 +9,26 @@ describe(`modules/reports/actions/commit-report.js`, () => {
 	proxyquire.noPreserveCache().noCallThru();
 	const middlewares = [thunk];
 	const mockStore = configureMockStore(middlewares);
-
 	const test = (t) => {
 		it(t.description, () => {
 			const store = mockStore(t.state);
-			const AugurJS = { augur: {} };
+			const AugurJS = { augur: { submitReportHash: () => {} } };
 			const BytesToHex = {};
 			const NextReportPage = {};
+			const ReportEncryption = {};
 			const action = proxyquire('../../../src/modules/reports/actions/commit-report.js', {
 				'../../../services/augurjs': AugurJS,
 				'../../../utils/bytes-to-hex': BytesToHex,
-				'../../reports/actions/next-report-page': NextReportPage
+				'../../reports/actions/next-report-page': NextReportPage,
+				'../../reports/actions/report-encryption': ReportEncryption
 			});
 			AugurJS.augur.fixReport = sinon.stub().returns('0xde0b6b3a7640000');
 			AugurJS.augur.makeHash = sinon.stub().returns('0xdeadbeef');
+			ReportEncryption.encryptReport = sinon.stub().returns({ report: 0, salt: 0 });
+			sinon.stub(AugurJS.augur, 'submitReportHash', (o) => {
+				store.dispatch({ type: 'AUGURJS_SUBMIT_REPORT_HASH', params: JSON.parse(JSON.stringify(o)) });
+				o.onSuccess({ hash: '0xdeadbeef', callReturn: '1' });
+			});
 			BytesToHex.bytesToHex = sinon.stub().returns('0x1337');
 			NextReportPage.nextReportPage = sinon.stub().returns({ type: 'NEXT_REPORT_PAGE' });
 			store.dispatch(action.commitReport(t.params.market, t.params.reportedOutcomeID, t.params.isUnethical, t.params.isIndeterminate));
@@ -60,31 +66,41 @@ describe(`modules/reports/actions/commit-report.js`, () => {
 					'0xb1': {
 						'0xe1': {
 							eventID: '0xe1',
-							period: 7,
 							marketID: '0xa1',
-							reportHash: '0xdeadbeef',
+							period: 7,
 							reportedOutcomeID: '1',
-							salt: '0x1337',
+							isCategorical: false,
+							isScalar: false,
 							isIndeterminate: false,
 							isUnethical: false,
+							salt: '0x1337',
+							reportHash: '0xdeadbeef',
 							isCommitted: false,
-							isRevealed: false,
-							isCategorical: false,
-							isScalar: false
+							isRevealed: false
 						}
 					}
 				}
 			}, {
-				type: 'ADD_COMMIT_REPORT_TRANSACTION',
-				market: {
-					id: '0xa1',
-					eventID: '0xe1',
-					type: 'binary',
-					description: 'Market 1'
-				},
-				reportedOutcomeID: '1',
-				isUnethical: false,
-				isIndeterminate: false
+				type: 'AUGURJS_SUBMIT_REPORT_HASH',
+				params: {
+					event: '0xe1',
+					reportHash: '0xdeadbeef',
+					encryptedReport: 0,
+					encryptedSalt: 0,
+					ethics: 1,
+					branch: '0xb1',
+					period: 7
+				}
+			}, {
+				type: 'UPDATE_REPORTS',
+				reports: {
+					'0xb1': {
+						'0xe1': {
+							eventID: '0xe1',
+							isCommitted: true
+						}
+					}
+				}
 			}, {
 				type: 'NEXT_REPORT_PAGE'
 			}]);
@@ -135,16 +151,26 @@ describe(`modules/reports/actions/commit-report.js`, () => {
 					}
 				}
 			}, {
-				type: 'ADD_COMMIT_REPORT_TRANSACTION',
-				market: {
-					id: '0xa1',
-					eventID: '0xe1',
-					type: 'binary',
-					description: 'Market 1'
-				},
-				reportedOutcomeID: '2',
-				isUnethical: false,
-				isIndeterminate: false
+				type: 'AUGURJS_SUBMIT_REPORT_HASH',
+				params: {
+					event: '0xe1',
+					reportHash: '0xdeadbeef',
+					encryptedReport: 0,
+					encryptedSalt: 0,
+					ethics: 1,
+					branch: '0xb1',
+					period: 7
+				}
+			}, {
+				type: 'UPDATE_REPORTS',
+				reports: {
+					'0xb1': {
+						'0xe1': {
+							eventID: '0xe1',
+							isCommitted: true
+						}
+					}
+				}
 			}, {
 				type: 'NEXT_REPORT_PAGE'
 			}]);
@@ -195,16 +221,26 @@ describe(`modules/reports/actions/commit-report.js`, () => {
 					}
 				}
 			}, {
-				type: 'ADD_COMMIT_REPORT_TRANSACTION',
-				market: {
-					id: '0xa1',
-					eventID: '0xe1',
-					type: 'categorical',
-					description: 'Market 1'
-				},
-				reportedOutcomeID: '5',
-				isUnethical: false,
-				isIndeterminate: false
+				type: 'AUGURJS_SUBMIT_REPORT_HASH',
+				params: {
+					event: '0xe1',
+					reportHash: '0xdeadbeef',
+					encryptedReport: 0,
+					encryptedSalt: 0,
+					ethics: 1,
+					branch: '0xb1',
+					period: 7
+				}
+			}, {
+				type: 'UPDATE_REPORTS',
+				reports: {
+					'0xb1': {
+						'0xe1': {
+							eventID: '0xe1',
+							isCommitted: true
+						}
+					}
+				}
 			}, {
 				type: 'NEXT_REPORT_PAGE'
 			}]);
@@ -255,16 +291,26 @@ describe(`modules/reports/actions/commit-report.js`, () => {
 					}
 				}
 			}, {
-				type: 'ADD_COMMIT_REPORT_TRANSACTION',
-				market: {
-					id: '0xa1',
-					eventID: '0xe1',
-					type: 'scalar',
-					description: 'Market 1'
-				},
-				reportedOutcomeID: '1.2345',
-				isUnethical: false,
-				isIndeterminate: false
+				type: 'AUGURJS_SUBMIT_REPORT_HASH',
+				params: {
+					event: '0xe1',
+					reportHash: '0xdeadbeef',
+					encryptedReport: 0,
+					encryptedSalt: 0,
+					ethics: 1,
+					branch: '0xb1',
+					period: 7
+				}
+			}, {
+				type: 'UPDATE_REPORTS',
+				reports: {
+					'0xb1': {
+						'0xe1': {
+							eventID: '0xe1',
+							isCommitted: true
+						}
+					}
+				}
 			}, {
 				type: 'NEXT_REPORT_PAGE'
 			}]);
@@ -315,16 +361,26 @@ describe(`modules/reports/actions/commit-report.js`, () => {
 					}
 				}
 			}, {
-				type: 'ADD_COMMIT_REPORT_TRANSACTION',
-				market: {
-					id: '0xa1',
-					eventID: '0xe1',
-					type: 'scalar',
-					description: 'Market 1'
-				},
-				reportedOutcomeID: '1.2345',
-				isUnethical: true,
-				isIndeterminate: false
+				type: 'AUGURJS_SUBMIT_REPORT_HASH',
+				params: {
+					event: '0xe1',
+					reportHash: '0xdeadbeef',
+					encryptedReport: 0,
+					encryptedSalt: 0,
+					ethics: 0,
+					branch: '0xb1',
+					period: 7
+				}
+			}, {
+				type: 'UPDATE_REPORTS',
+				reports: {
+					'0xb1': {
+						'0xe1': {
+							eventID: '0xe1',
+							isCommitted: true
+						}
+					}
+				}
 			}, {
 				type: 'NEXT_REPORT_PAGE'
 			}]);
@@ -375,16 +431,26 @@ describe(`modules/reports/actions/commit-report.js`, () => {
 					}
 				}
 			}, {
-				type: 'ADD_COMMIT_REPORT_TRANSACTION',
-				market: {
-					id: '0xa1',
-					eventID: '0xe1',
-					type: 'binary',
-					description: 'Market 1'
-				},
-				reportedOutcomeID: '1.5',
-				isUnethical: false,
-				isIndeterminate: true
+				type: 'AUGURJS_SUBMIT_REPORT_HASH',
+				params: {
+					event: '0xe1',
+					reportHash: '0xdeadbeef',
+					encryptedReport: 0,
+					encryptedSalt: 0,
+					ethics: 1,
+					branch: '0xb1',
+					period: 7
+				}
+			}, {
+				type: 'UPDATE_REPORTS',
+				reports: {
+					'0xb1': {
+						'0xe1': {
+							eventID: '0xe1',
+							isCommitted: true
+						}
+					}
+				}
 			}, {
 				type: 'NEXT_REPORT_PAGE'
 			}]);
@@ -435,16 +501,26 @@ describe(`modules/reports/actions/commit-report.js`, () => {
 					}
 				}
 			}, {
-				type: 'ADD_COMMIT_REPORT_TRANSACTION',
-				market: {
-					id: '0xa1',
-					eventID: '0xe1',
-					type: 'categorical',
-					description: 'Market 1'
-				},
-				reportedOutcomeID: '1.5',
-				isUnethical: false,
-				isIndeterminate: true
+				type: 'AUGURJS_SUBMIT_REPORT_HASH',
+				params: {
+					event: '0xe1',
+					reportHash: '0xdeadbeef',
+					encryptedReport: 0,
+					encryptedSalt: 0,
+					ethics: 1,
+					branch: '0xb1',
+					period: 7
+				}
+			}, {
+				type: 'UPDATE_REPORTS',
+				reports: {
+					'0xb1': {
+						'0xe1': {
+							eventID: '0xe1',
+							isCommitted: true
+						}
+					}
+				}
 			}, {
 				type: 'NEXT_REPORT_PAGE'
 			}]);
@@ -495,16 +571,26 @@ describe(`modules/reports/actions/commit-report.js`, () => {
 					}
 				}
 			}, {
-				type: 'ADD_COMMIT_REPORT_TRANSACTION',
-				market: {
-					id: '0xa1',
-					eventID: '0xe1',
-					type: 'scalar',
-					description: 'Market 1'
-				},
-				reportedOutcomeID: '1.500000000000000001',
-				isUnethical: false,
-				isIndeterminate: true
+				type: 'AUGURJS_SUBMIT_REPORT_HASH',
+				params: {
+					event: '0xe1',
+					reportHash: '0xdeadbeef',
+					encryptedReport: 0,
+					encryptedSalt: 0,
+					ethics: 1,
+					branch: '0xb1',
+					period: 7
+				}
+			}, {
+				type: 'UPDATE_REPORTS',
+				reports: {
+					'0xb1': {
+						'0xe1': {
+							eventID: '0xe1',
+							isCommitted: true
+						}
+					}
+				}
 			}, {
 				type: 'NEXT_REPORT_PAGE'
 			}]);
@@ -555,16 +641,26 @@ describe(`modules/reports/actions/commit-report.js`, () => {
 					}
 				}
 			}, {
-				type: 'ADD_COMMIT_REPORT_TRANSACTION',
-				market: {
-					id: '0xa1',
-					eventID: '0xe1',
-					type: 'binary',
-					description: 'Market 1'
-				},
-				reportedOutcomeID: '1.5',
-				isUnethical: true,
-				isIndeterminate: true
+				type: 'AUGURJS_SUBMIT_REPORT_HASH',
+				params: {
+					event: '0xe1',
+					reportHash: '0xdeadbeef',
+					encryptedReport: 0,
+					encryptedSalt: 0,
+					ethics: 0,
+					branch: '0xb1',
+					period: 7
+				}
+			}, {
+				type: 'UPDATE_REPORTS',
+				reports: {
+					'0xb1': {
+						'0xe1': {
+							eventID: '0xe1',
+							isCommitted: true
+						}
+					}
+				}
 			}, {
 				type: 'NEXT_REPORT_PAGE'
 			}]);
@@ -615,16 +711,26 @@ describe(`modules/reports/actions/commit-report.js`, () => {
 					}
 				}
 			}, {
-				type: 'ADD_COMMIT_REPORT_TRANSACTION',
-				market: {
-					id: '0xa1',
-					eventID: '0xe1',
-					type: 'categorical',
-					description: 'Market 1'
-				},
-				reportedOutcomeID: '1.5',
-				isUnethical: true,
-				isIndeterminate: true
+				type: 'AUGURJS_SUBMIT_REPORT_HASH',
+				params: {
+					event: '0xe1',
+					reportHash: '0xdeadbeef',
+					encryptedReport: 0,
+					encryptedSalt: 0,
+					ethics: 0,
+					branch: '0xb1',
+					period: 7
+				}
+			}, {
+				type: 'UPDATE_REPORTS',
+				reports: {
+					'0xb1': {
+						'0xe1': {
+							eventID: '0xe1',
+							isCommitted: true
+						}
+					}
+				}
 			}, {
 				type: 'NEXT_REPORT_PAGE'
 			}]);
@@ -675,16 +781,26 @@ describe(`modules/reports/actions/commit-report.js`, () => {
 					}
 				}
 			}, {
-				type: 'ADD_COMMIT_REPORT_TRANSACTION',
-				market: {
-					id: '0xa1',
-					eventID: '0xe1',
-					type: 'scalar',
-					description: 'Market 1'
-				},
-				reportedOutcomeID: '1.500000000000000001',
-				isUnethical: true,
-				isIndeterminate: true
+				type: 'AUGURJS_SUBMIT_REPORT_HASH',
+				params: {
+					event: '0xe1',
+					reportHash: '0xdeadbeef',
+					encryptedReport: 0,
+					encryptedSalt: 0,
+					ethics: 0,
+					branch: '0xb1',
+					period: 7
+				}
+			}, {
+				type: 'UPDATE_REPORTS',
+				reports: {
+					'0xb1': {
+						'0xe1': {
+							eventID: '0xe1',
+							isCommitted: true
+						}
+					}
+				}
 			}, {
 				type: 'NEXT_REPORT_PAGE'
 			}]);
