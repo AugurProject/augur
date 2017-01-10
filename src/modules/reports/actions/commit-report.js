9587f5a144ad1933a5a4568f462907adac3d1993
Jack Peterson <jack@tinybike.net>
2017-01-10 13:01:10 -0800

    Cleaned up commit-report action

--- src/modules/reports/actions/commit-report.js
+++ src/modules/reports/actions/commit-report.js
@@ -4,28 +4,24 @@ import { bytesToHex } from '../../../utils/bytes-to-hex';
 import { CATEGORICAL, SCALAR } from '../../markets/constants/market-types';
 import { updateReport } from '../../reports/actions/update-reports';
 import { nextReportPage } from '../../reports/actions/next-report-page';
-import { selectOutcomeName } from '../../reports/selectors/reportable-outcomes';
 import { encryptReport } from '../../reports/actions/report-encryption';
 
 export function commitReport(market, reportedOutcomeID, isUnethical, isIndeterminate) {
 	return (dispatch, getState) => {
-		const { branch, loginAccount, outcomesData } = getState();
+		const { branch, loginAccount } = getState();
 		if (!loginAccount.address || !market || !reportedOutcomeID) {
 			console.error('commitReport failed:', loginAccount.address, market, reportedOutcomeID);
 			return dispatch(nextReportPage());
 		}
-		const marketID = market.id;
 		const eventID = market.eventID;
 		const branchID = branch.id;
-		const period = branch.reportPeriod;
-		console.debug(`committing to report ${reportedOutcomeID} on market ${marketID} event ${eventID} period ${period}...`);
+		console.log(`committing to report ${reportedOutcomeID} on market ${market.id} event ${eventID} period ${branch.reportPeriod}...`);
 		const salt = bytesToHex(secureRandom(32));
 		const fixedReport = augur.fixReport(reportedOutcomeID, market.minValue, market.maxValue, market.type, isIndeterminate);
-		console.log('fixedReport:', fixedReport);
 		const report = {
 			eventID,
-			marketID,
-			period,
+			marketID: market.id,
+			period: branch.reportPeriod,
 			reportedOutcomeID,
 			isCategorical: market.type === CATEGORICAL,
 			isScalar: market.type === SCALAR,
@@ -36,10 +32,7 @@ export function commitReport(market, reportedOutcomeID, isUnethical, isIndetermi
 			isCommitted: false,
 			isRevealed: false
 		};
-		dispatch(updateReport(branchID, eventID, report));
-		console.log('updated report:', eventID, report);
 		const encrypted = encryptReport(fixedReport, loginAccount.derivedKey, salt);
-		const outcomeName = selectOutcomeName(reportedOutcomeID, market.type, outcomesData[marketID]);
 		dispatch(updateReport(branchID, eventID, { ...report }));
 		augur.submitReportHash({
 			event: eventID,
@@ -48,22 +41,17 @@ export function commitReport(market, reportedOutcomeID, isUnethical, isIndetermi
 			encryptedSalt: encrypted.salt,
 			ethics: Number(!isUnethical),
 			branch: branchID,
-			period,
+			period: branch.reportPeriod,
 			periodLength: branch.periodLength,
-			onSent: res => console.log(`committing to report outcome ${outcomeName}: ${res.hash} ${res.callReturn}`),
-			onSuccess: (res) => {
-				const branchReports = getState().reports[branchID] || {};
-				console.log('updating branchReports:', eventID, marketID, branchReports);
+			onSent: r => console.log('submitReportHash sent:', r),
+			onSuccess: r => dispatch(updateReport(branchID, eventID, {
+				...(getState().reports[branchID] || {})[eventID],
+				isCommitted: true
+			})),
+			onFailed: (e) => {
+				console.error('submitReportHash failed:', e);
 				dispatch(updateReport(branchID, eventID, {
-					...branchReports[eventID],
-					isCommitted: true
-				}));
-			},
-			onFailed: (err) => {
-				console.error('submitReportHash failed', err);
-				const branchReports = getState().reports[branchID] || {};
-				dispatch(updateReport(branchID, eventID, {
-					...branchReports[eventID],
+					...(getState().reports[branchID] || {})[eventID],
 					isCommitted: false
 				}));
 			}
