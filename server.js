592280328e8c07978d23a96d9781643e39c78d7b
Stephen Sprinkle <stephen@sprinkle.io>
2017-01-12 10:46:05 -0700

    refactored indent of root dir files

--- server.js
+++ server.js
@@ -6,24 +6,24 @@ const app = express();
 const PROD_HOST = 'app.augur.net';
 
 if (process.env.NODE_ENV === 'development') {
-	const webpack = require('webpack');
-	const devMiddleware = require('webpack-dev-middleware');
-	const hotMiddleware = require('webpack-hot-middleware');
-	const config = require('./webpack.config');
-	const compiler = webpack(config);
+  const webpack = require('webpack');
+  const devMiddleware = require('webpack-dev-middleware');
+  const hotMiddleware = require('webpack-hot-middleware');
+  const config = require('./webpack.config');
+  const compiler = webpack(config);
 
 	// Hot Module Reload
-	app.use(devMiddleware(compiler, {
-		publicPath: config.output.publicPath,
-		stats: {
-			colors: true
-		}
-	}));
+  app.use(devMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true
+    }
+  }));
 
-	app.use(hotMiddleware(compiler));
+  app.use(hotMiddleware(compiler));
 } else {
 	// Static Path
-	app.use(express.static('build'));
+  app.use(express.static('build'));
 }
 
 app.use(helmet());
@@ -32,15 +32,15 @@ app.use(require('prerender-node').set('prerenderToken', process.env.RENDERTOKEN)
 // redirect production site to secure version
 app.get('*', (req, res, next) => {
 	// res.sendFile(path.resolve(__dirname, 'build/index.html'));
-	if (req.headers['x-forwarded-proto'] !== 'https' && req.get('host') === PROD_HOST) {
-		res.redirect('https://' + PROD_HOST + req.url);
-	} else {
-		next();   // continue to other routes if we're not redirecting
-	}
+  if (req.headers['x-forwarded-proto'] !== 'https' && req.get('host') === PROD_HOST) {
+    res.redirect('https://' + PROD_HOST + req.url);
+  } else {
+    next();   // continue to other routes if we're not redirecting
+  }
 });
 
 app.get('/', (req, res) => {
-	console.log('loaded');
+  console.log('loaded');
 });
 
 app.listen(process.env.PORT || 8080);
