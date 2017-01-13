d63c1f91d955b52ab90c3d2a71dee3fbd5c5ada5
Stephen Sprinkle <stephen@sprinkle.io>
2017-01-13 13:13:50 -0700

    set mocha timeout in coverage script

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -5,7 +5,7 @@ shell.echo('== Running Augur Coverage ==');
 process.env.NODE_ENV = 'test';
 process.env.FORCE_COLOR = true;
 
-shell.exec('istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- --require babel-register -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rimraf ./coverage', (code) => {
+shell.exec('istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- --require babel-register --timeout 10000 -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rimraf ./coverage', (code) => {
   if (code !== 0) {
     shell.exit(code);
   }
