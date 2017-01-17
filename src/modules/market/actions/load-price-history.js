61a200ebdcbbfbab51182c8fa235303963216f02
Jack Peterson <jack@tinybike.net>
2017-01-16 22:09:49 -0800

    Added TODO note about (possibly) redundant data stores

--- src/modules/market/actions/load-price-history.js
+++ src/modules/market/actions/load-price-history.js
@@ -2,12 +2,11 @@ import { augur } from '../../../services/augurjs';
 import { updateMarketPriceHistory } from '../../market/actions/update-market-price-history';
 import { updateMarketTradesData } from '../../portfolio/actions/update-market-trades-data';
 
-export function loadPriceHistory(marketID) {
-  return (dispatch, getState) => {
-    augur.getMarketPriceHistory(marketID, (err, priceHistory) => {
-      if (err) return console.error('loadPriceHistory:', err);
-      dispatch(updateMarketTradesData({ [marketID]: priceHistory }));
-      dispatch(updateMarketPriceHistory(marketID, priceHistory));
-    });
-  };
-}
+export const loadPriceHistory = marketID => dispatch => (
+  augur.getMarketPriceHistory(marketID, (err, priceHistory) => {
+    if (err) return console.error('loadPriceHistory', err);
+    // TODO check if these data stores are redundant (remove one of them if so)
+    dispatch(updateMarketTradesData({ [marketID]: priceHistory }));
+    dispatch(updateMarketPriceHistory(marketID, priceHistory));
+  })
+);
