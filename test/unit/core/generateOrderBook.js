5fce2a9404f17c8d1c20a731763e32846987a2db
John Danz <John.Danz87@gmail.com>
2017-01-23 12:05:20 -0600

    added a case where priceDepth === Infinity which should fail

--- test/unit/core/generateOrderBook.js
+++ test/unit/core/generateOrderBook.js
@@ -3,10 +3,10 @@ var BigNumber = require("bignumber.js");
 var abi = require("augur-abi");
 var augur = require('../../../src');
 var utils = require('../../../src/utilities.js');
-// 6 tests total
+// 8 tests total
 
 describe("generateOrderBook", function() {
-// 6 tests total
+// 8 tests total
 /**
  * generateOrderBook: convenience method for generating an initial order book
  * for a newly created market. generateOrderBook calculates the number of
@@ -124,6 +124,37 @@ describe("generateOrderBook", function() {
     		initialFairPrices: ['0.506573092', '0.49422020'],
     		startingQuantity: 501,
     		bestStartingQuantity: 500,
+    		priceWidth: '0.0812',
+    		isSimulation: false
+    	},
+    	cb: {
+    		onSimulate: function(simulation) {},
+    		onBuyCompleteSets: function(res) {},
+    		onSetupOutcome: function(outcome) {},
+    		onSetupOrder: function(order) {},
+    		onSuccess: function(orderBook) {},
+    		onFailed: function(err) {
+                // this callback should be called, lets assert we get the expected error back.
+                assert.deepEqual(err, augur.errors.INSUFFICIENT_LIQUIDITY);
+            }
+    	},
+        getMarketInfo: function(market, account, callback) {
+            // return marketInfo with the expected numOutcomes and a type of binary.
+            callback({ numOutcomes: 2, type: 'binary' });
+        },
+        buyCompleteSets: function(market, amount, onSent, onSuccess, onFailed) {},
+        buy: function(amount, price, market, outcome, scalarMinMax, onSent, onSuccess, onFailed) {},
+        sell: function(amount, price, market, outcome, scalarMinMax, onSent, onSuccess, onFailed) {},
+        getOrderBook: function(market, scalarMinMax, onSuccess) {},
+    });
+    test({
+        descrption: 'Should handle a binary market where priceDepth is Infinity, triggering an onFailed called.',
+        p: {
+    		market: '0xa1',
+    		liquidity: 100,
+    		initialFairPrices: ['0.506573092', '0.49422020'],
+    		startingQuantity: 51,
+    		bestStartingQuantity: 50,
     		priceWidth: '0.0812',
     		isSimulation: false
     	},
