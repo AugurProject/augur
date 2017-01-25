9643c12a9737dbdef562b0dfc0ba6db791f031f8
John Danz <John.Danz87@gmail.com>
2017-01-25 17:12:02 -0600

    strated working on takeOrder Unit tests, work in progress

--- test/unit/trading/takeOrder.js
+++ test/unit/trading/takeOrder.js
@@ -1,3 +1,94 @@
+"use strict";
+
+var assert = require('chai').assert;
+var augur = require('../../../src');
+
+describe("takeOrder.placeBuy", function() {
+    // ? tests total
+    var executeTrade = augur.executeTrade;
+    var placeBid = augur.placeBid;
+    var callCounts = {
+        executeTrade: 0,
+        placeBid: 0,
+        tradeCommitmentCallback: 0,
+        tradeCommitLockCallback: 0
+    };
+    // a function to quickly reset the callCounts object.
+    function ClearCallCounts() {
+        var keys = Object.keys(callCounts);
+        for (keys in callCounts) {
+            callCounts[keys] = 0;
+        }
+    };
+    afterEach(function() {
+        ClearCallCounts();
+        augur.executeTrade = executeTrade;
+        augur.placeBid = placeBid;
+    });
+    var test = function(t) {
+        it(t.description, function() {
+            augur.executeTrade = t.executeTrade;
+            augur.placeBid = t.placeBid;
+
+            augur.placeBuy(t.market, t.outcomeID, t.numShares, t.limitPrice, t.address, t.totalCost, t.tradingFees, t.orderBooks, t.doNotMakeOrders, t.tradeGroupID, t.tradeCommitmentCallback, t.tradeCommitLockCallback);
+
+            t.assertions();
+        });
+    };
+    test({
+        description: 'Should return if an error is returned from executeTrade',
+        market: { id: '0xa1' },
+        outcomeID: '1',
+        numShares: '100',
+        limitPrice: '0.5',
+        address: '0x1',
+        totalCost: '51',
+        tradingFees: '0.01',
+        orderBooks: { '0xa1': { buy: {}, sell: {} }},
+        doNotMakeOrders: true,
+        tradeGroupID: '0x000abc123',
+        tradeCommitmentCallback: function() {
+            callCounts.tradeCommitmentCallback++;
+        },
+        tradeCommitLockCallback: function(lock) {
+            callCounts.tradeCommitLockCallback++;
+            switch(callCounts.tradeCommitLockCallback) {
+            case 2:
+                assert.isFalse(lock);
+                break;
+            default:
+                assert.isTrue(lock);
+                break;
+            }
+        },
+        executeTrade: function(marketID, outcomeID, numShares, totalEthWithFee, tradingFees, tradeGroupID, address, orderBooks, getTradeIDs, tradeCommitmentCallback, cb) {
+            callCounts.executeTrade++;
+            assert.equal(marketID, '0xa1');
+            assert.equal(outcomeID, '1');
+            assert.equal(numShares, 0);
+            assert.equal(totalEthWithFee, '51');
+            assert.equal(tradingFees, '0.01');
+            assert.equal(tradeGroupID, '0x000abc123');
+            assert.equal(address, '0x1');
+            assert.deepEqual(orderBooks, { '0xa1': { buy: {}, sell: {} }});
+            assert.isFunction(getTradeIDs);
+            assert.isFunction(tradeCommitmentCallback);
+            // return an error in this case
+            cb({ error: 999, message: 'Uh-Oh!' }, undefined);
+        },
+        placeBid: function() {
+            callCounts.placeBid++;
+        },
+        assertions: function() {
+            assert.deepEqual(callCounts, {
+                executeTrade: 1,
+                placeBid: 0,
+                tradeCommitmentCallback: 0,
+                tradeCommitLockCallback: 2
+            });
+        }
+    });
+});
 // import { describe, it } from 'mocha';
 // import { assert } from 'chai';
 // import proxyquire from 'proxyquire';
