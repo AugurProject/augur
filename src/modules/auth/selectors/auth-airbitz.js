84c47eb5e063df10f54e2deeb34fb89889f5ce41
Stephen Sprinkle <stephen@sprinkle.io>
2017-01-26 16:33:09 -0700

    removed authType due to unused state

--- src/modules/auth/selectors/auth-airbitz.js
+++ src/modules/auth/selectors/auth-airbitz.js
@@ -2,11 +2,9 @@ import store from 'src/store';
 
 import { selectAirbitzLink, selectAirbitzOnLoad } from 'modules/link/selectors/links';
 
-import { REGISTER, LOGIN } from 'modules/auth/constants/auth-types';
-
 export default function () {
   return {
-    airbitzLoginLink: selectAirbitzLink(LOGIN, store.dispatch),
+    airbitzLoginLink: selectAirbitzLink(null, store.dispatch),
     airbitzOnLoad: selectAirbitzOnLoad(store.dispatch)
   };
 }
