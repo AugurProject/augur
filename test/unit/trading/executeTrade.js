2ac8a6f97784064d8a8cad3c11cb5bdca81e133d
John Danz <John.Danz87@gmail.com>
2017-01-27 11:08:27 -0600

    Added unit tests for executeTrade.executeShortSell, 1/2 executeTrade

--- test/unit/trading/executeTrade.js
+++ test/unit/trading/executeTrade.js
@@ -12,7 +12,7 @@ describe.skip("executeTrade.executeTrade", function() {
 });
 
 describe("executeTrade.executeShortSell", function() {
-    // ? tests total
+    // 7 tests total
     var short_sell = augur.short_sell;
     var callCounts = {
         getTradeIDs: 0,
@@ -135,6 +135,373 @@ describe("executeTrade.executeShortSell", function() {
                 getTradeIDs: 1,
                 tradeCommitmentCallback: 0,
                 short_sell: 0
+            });
+        }
+    });
+    test({
+        description: 'Should handle a matchingID and execute a shortSell',
+        marketID: '0xa1',
+        outcomeID: '1',
+        numShares: '100',
+        tradingFees: '0.01',
+        tradeGroupID: '0x000abc123',
+        address: '0x1',
+        orderBooks: { '0xa1': { buy: { '0xb1': { amount: '100', price: '0.4' } }, sell: {} } },
+        getTradeIDs: function() {
+            callCounts.getTradeIDs++;
+            return ['0xb1'];
+        },
+        tradeCommitmentCallback: function(commit) {
+            callCounts.tradeCommitmentCallback++;
+            switch(callCounts.tradeCommitmentCallback) {
+            case 1:
+                assert.deepEqual(commit, { gasFees: '0.045' });
+                break;
+            case 2:
+                assert.deepEqual(commit, {
+                  tradeHash: '0x0000000000000000000000000000000000000000000000000000000abc543012',
+                  orders: [{ amount: '100', price: '0.4' }],
+                  maxValue: "0",
+                  maxAmount: '100',
+                  remainingEth: "0",
+                  remainingShares: '100',
+                  filledEth: '0',
+                  filledShares: '0',
+                  tradingFees: '0.01',
+                  gasFees: '0.045'
+                });
+                break;
+            default:
+                assert.deepEqual(commit, {
+                  filledShares: '100',
+                  filledEth: '50',
+                  remainingShares: '0',
+                  tradingFees: '0.01',
+                  gasFees: '0.09'
+                });
+                break;
+            }
+        },
+        short_sell: function(trade) {
+            callCounts.short_sell++;
+            assert.equal(trade.max_amount, '100');
+            assert.equal(trade.buyer_trade_id, '0xb1');
+            assert.equal(trade.sender, '0x1');
+            assert.equal(trade.tradeGroupID, '0x000abc123');
+            assert.isFunction(trade.onTradeHash);
+            assert.isFunction(trade.onCommitSent);
+            assert.isFunction(trade.onCommitSuccess);
+            assert.isFunction(trade.onCommitFailed);
+            assert.isFunction(trade.onNextBlock);
+            assert.isFunction(trade.onTradeSent);
+            assert.isFunction(trade.onTradeSuccess);
+            assert.isFunction(trade.onTradeFailed);
+            trade.onCommitSent('1');
+            trade.onCommitSuccess({ gasFees: '0.045'});
+            trade.onNextBlock();
+            trade.onTradeSent();
+            trade.onTradeHash('0xabc543012');
+            trade.onTradeSuccess({
+                unmatchedShares: '0',
+                matchedShares: '100',
+                cashFromTrade: '50',
+                tradingFees: '0.01',
+                gasFees: '0.045',
+            });
+        },
+        assertions: function(err, res) {
+            assert.isNull(err);
+            assert.deepEqual(res, {
+                filledEth: new BigNumber('50'),
+                filledShares: new BigNumber('100'),
+                gasFees: new BigNumber('0.09'),
+                remainingShares: new BigNumber('0'),
+                tradingFees: new BigNumber('0.01'),
+            });
+            assert.deepEqual(callCounts, {
+                getTradeIDs: 1,
+                tradeCommitmentCallback: 3,
+                short_sell: 1
+            });
+        }
+    });
+    test({
+        description: 'Should handle a shortSell that requires 2 orders to fufill the full shortSell',
+        marketID: '0xa1',
+        outcomeID: '1',
+        numShares: '100',
+        tradingFees: '0.01',
+        tradeGroupID: '0x000abc123',
+        address: '0x1',
+        orderBooks: {
+		'0xa1': {
+			buy: {
+    				'0xb1': {
+    					amount: '50',
+    					price: '0.5'
+    				},
+                    '0xb2': {
+    					amount: '50',
+    					price: '0.5'
+    				},
+				},
+		      sell: {}
+				}
+			},
+        getTradeIDs: function() {
+            callCounts.getTradeIDs++;
+            return ['0xb1', '0xb2'];
+        },
+        tradeCommitmentCallback: function(commit) {
+            callCounts.tradeCommitmentCallback++;
+            switch(callCounts.tradeCommitmentCallback) {
+            case 1:
+                assert.deepEqual(commit, { gasFees: '0.045' });
+                break;
+            case 2:
+                assert.deepEqual(commit, {
+                  tradeHash: '0x0000000000000000000000000000000000000000000000000000000abc543012',
+                  orders: [{ amount: '50', price: '0.5' }],
+                  maxValue: "0",
+                  maxAmount: '100',
+                  remainingEth: "0",
+                  remainingShares: '100',
+                  filledEth: '0',
+                  filledShares: '0',
+                  tradingFees: '0.01',
+                  gasFees: '0.045'
+                });
+                break;
+            case 3:
+                assert.deepEqual(commit, {
+                  filledShares: '50',
+                  filledEth: '25',
+                  remainingShares: '50',
+                  tradingFees: '0.01',
+                  gasFees: '0.09'
+                });
+                break;
+            case 4:
+                assert.deepEqual(commit, { gasFees: '0.135' });
+                break;
+            case 5:
+                assert.deepEqual(commit, {
+                  tradeHash: '0x0000000000000000000000000000000000000000000000000000000abc543012',
+                  orders: [{ amount: '50', price: '0.5' }],
+                  maxValue: "0",
+                  maxAmount: '50',
+                  remainingEth: "0",
+                  remainingShares: '50',
+                  filledEth: '25',
+                  filledShares: '50',
+                  tradingFees: '0.01',
+                  gasFees: '0.135'
+                });
+                break;
+            default:
+                assert.deepEqual(commit, {
+                  filledShares: '100',
+                  filledEth: '50',
+                  remainingShares: '0',
+                  tradingFees: '0.02',
+                  gasFees: '0.18'
+                });
+                break;
+            }
+        },
+        short_sell: function(trade) {
+            callCounts.short_sell++;
+            assert.oneOf(trade.max_amount, ['100', '50']);
+            assert.oneOf(trade.buyer_trade_id, ['0xb1', '0xb2']);
+            assert.equal(trade.sender, '0x1');
+            assert.equal(trade.tradeGroupID, '0x000abc123');
+            assert.isFunction(trade.onTradeHash);
+            assert.isFunction(trade.onCommitSent);
+            assert.isFunction(trade.onCommitSuccess);
+            assert.isFunction(trade.onCommitFailed);
+            assert.isFunction(trade.onNextBlock);
+            assert.isFunction(trade.onTradeSent);
+            assert.isFunction(trade.onTradeSuccess);
+            assert.isFunction(trade.onTradeFailed);
+            switch (trade.buyer_trade_id) {
+            case '0xb1':
+                trade.onCommitSent('1');
+                trade.onCommitSuccess({ gasFees: '0.045'});
+                trade.onNextBlock();
+                trade.onTradeSent();
+                trade.onTradeHash('0xabc543012');
+                trade.onTradeSuccess({
+                    unmatchedShares: '50',
+                    matchedShares: '50',
+                    cashFromTrade: '25',
+                    tradingFees: '0.01',
+                    gasFees: '0.045',
+                });
+                break;
+            default:
+                trade.onCommitSent('1');
+                trade.onCommitSuccess({ gasFees: '0.045'});
+                trade.onNextBlock();
+                trade.onTradeSent();
+                trade.onTradeHash('0xabc543012');
+                trade.onTradeSuccess({
+                    unmatchedShares: undefined,
+                    matchedShares: '50',
+                    cashFromTrade: '25',
+                    tradingFees: '0.01',
+                    gasFees: '0.045',
+                });
+                break;
+            }
+
+        },
+        assertions: function(err, res) {
+            assert.isNull(err);
+            assert.deepEqual(res, {
+                filledEth: new BigNumber('50'),
+                filledShares: new BigNumber('100'),
+                gasFees: new BigNumber('0.18'),
+                remainingShares: new BigNumber('0'),
+                tradingFees: new BigNumber('0.02'),
+            });
+            assert.deepEqual(callCounts, {
+                getTradeIDs: 1,
+                tradeCommitmentCallback: 6,
+                short_sell: 2
+            });
+        }
+    });
+    test({
+        description: 'Should handle a shortSell where onCommitFailed is called',
+        marketID: '0xa1',
+        outcomeID: '1',
+        numShares: '100',
+        tradingFees: '0.01',
+        tradeGroupID: '0x000abc123',
+        address: '0x1',
+        orderBooks: {
+		'0xa1': {
+			buy: {
+    				'0xb1': {
+    					amount: '100',
+    					price: '0.4'
+    				}
+				},
+		      sell: {}
+				}
+			},
+        getTradeIDs: function() {
+            callCounts.getTradeIDs++;
+            return ['0xb1'];
+        },
+        tradeCommitmentCallback: function(commit) {
+            callCounts.tradeCommitmentCallback++;
+        },
+        short_sell: function(trade) {
+            callCounts.short_sell++;
+            assert.oneOf(trade.max_amount, ['100', '50']);
+            assert.oneOf(trade.buyer_trade_id, ['0xb1', '0xb2']);
+            assert.equal(trade.sender, '0x1');
+            assert.equal(trade.tradeGroupID, '0x000abc123');
+            assert.isFunction(trade.onTradeHash);
+            assert.isFunction(trade.onCommitSent);
+            assert.isFunction(trade.onCommitSuccess);
+            assert.isFunction(trade.onCommitFailed);
+            assert.isFunction(trade.onNextBlock);
+            assert.isFunction(trade.onTradeSent);
+            assert.isFunction(trade.onTradeSuccess);
+            assert.isFunction(trade.onTradeFailed);
+            trade.onCommitFailed({ error: 999, message: 'Uh-Oh!' });
+
+        },
+        assertions: function(err, res) {
+            assert.deepEqual(err, { error: 999, message: 'Uh-Oh!' });
+            assert.isUndefined(res);
+            assert.deepEqual(callCounts, {
+                getTradeIDs: 1,
+                tradeCommitmentCallback: 0,
+                short_sell: 1
+            });
+        }
+    });
+    test({
+        description: 'Should handle a shortSell where onFailed is called',
+        marketID: '0xa1',
+        outcomeID: '1',
+        numShares: '100',
+        tradingFees: '0.01',
+        tradeGroupID: '0x000abc123',
+        address: '0x1',
+        orderBooks: {
+        '0xa1': {
+            buy: {
+                    '0xb1': {
+                        amount: '100',
+                        price: '0.4'
+                    }
+                },
+              sell: {}
+                }
+            },
+        getTradeIDs: function() {
+            callCounts.getTradeIDs++;
+            return ['0xb1'];
+        },
+        tradeCommitmentCallback: function(commit) {
+            callCounts.tradeCommitmentCallback++;
+            switch(callCounts.tradeCommitmentCallback) {
+            case 1:
+                assert.deepEqual(commit, { gasFees: '0.045' });
+                break;
+            default:
+                assert.deepEqual(commit, {
+                  tradeHash: '0x0000000000000000000000000000000000000000000000000000000abc543012',
+                  orders: [{ amount: '100', price: '0.4' }],
+                  maxValue: "0",
+                  maxAmount: '100',
+                  remainingEth: "0",
+                  remainingShares: '100',
+                  filledEth: '0',
+                  filledShares: '0',
+                  tradingFees: '0.01',
+                  gasFees: '0.045'
+                });
+                break;
+            }
+        },
+        short_sell: function(trade) {
+            callCounts.short_sell++;
+            assert.equal(trade.max_amount, '100');
+            assert.equal(trade.buyer_trade_id, '0xb1');
+            assert.equal(trade.sender, '0x1');
+            assert.equal(trade.tradeGroupID, '0x000abc123');
+            assert.isFunction(trade.onTradeHash);
+            assert.isFunction(trade.onCommitSent);
+            assert.isFunction(trade.onCommitSuccess);
+            assert.isFunction(trade.onCommitFailed);
+            assert.isFunction(trade.onNextBlock);
+            assert.isFunction(trade.onTradeSent);
+            assert.isFunction(trade.onTradeSuccess);
+            assert.isFunction(trade.onTradeFailed);
+            trade.onCommitSent('1');
+            trade.onCommitSuccess({ gasFees: '0.045'});
+            trade.onNextBlock();
+            trade.onTradeSent();
+            trade.onTradeHash('0xabc543012');
+            trade.onTradeFailed({
+                error: 999, message: 'Uh-Oh!'
+            });
+
+        },
+        assertions: function(err, res) {
+            assert.deepEqual(err, {
+                error: 999, message: 'Uh-Oh!'
+            });
+            assert.isUndefined(res);
+            assert.deepEqual(callCounts, {
+                getTradeIDs: 1,
+                tradeCommitmentCallback: 2,
+                short_sell: 1
             });
         }
     });
