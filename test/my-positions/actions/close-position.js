ec2dc1c63fb34998962b7f7d2f569dfe1e77c20d
Stephen Sprinkle <stephen@sprinkle.io>
2017-01-31 10:40:14 -0700

    test for close position action

--- test/my-positions/actions/close-position.js
+++ test/my-positions/actions/close-position.js
@@ -3,68 +3,94 @@ import { describe, it } from 'mocha';
 import { assert } from 'chai';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import BigNumber from 'bignumber.js';
+import proxyquire from 'proxyquire';
+import sinon from 'sinon';
+
+import { ADD_CLOSE_POSITION_TRADE_GROUP } from 'modules/my-positions/actions/add-close-position-trade-group';
+import { CLOSE_DIALOG_FAILED } from 'modules/market/constants/close-dialog-status';
 
 describe('modules/my-positions/actions/close-position.js', () => {
+  proxyquire.noPreserveCache().noCallThru();
+
   const middlewares = [thunk];
   const mockStore = configureMockStore(middlewares);
 
   describe('closePosition', () => {
     const test = (t) => {
-      it(t.decription, () => {
+      it(t.description, () => {
+        // mock state
         const store = mockStore(t.state);
+
+        // mock methods
+        const mockUpdateTradesInProgress = { updateTradesInProgress: () => {} };
+        const mockPlaceTrade = { placeTrade: () => {} };
+        const mockAddClosePositionTradeGroup = { addClosePositionTradeGroup: () => {} };
+
+        // stubbed methods
+        sinon.stub(mockUpdateTradesInProgress, 'updateTradesInProgress', (marketID, outcomeID, side, numShares, limitPrice, maxCost, cb) => (dispatch, getState) => {
+          cb();
+        });
+        sinon.stub(mockPlaceTrade, 'placeTrade', (marketID, outcomeID, doNotMakeOrders, cb) => (dispatch, getState) => {
+          if (t.placeTradeFails) {
+            cb(true);
+          } else {
+            cb(null, t.tradeGroupID);
+          }
+        });
+        sinon.stub(mockAddClosePositionTradeGroup, 'addClosePositionTradeGroup', (marketID, outcomeID, tradeGroupID) => (dispatch, getState) => {});
+
+        // proxied action
+        const action = proxyquire('../../../src/modules/my-positions/actions/close-position.js', {
+          '../../trade/actions/update-trades-in-progress': mockUpdateTradesInProgress,
+          '../../trade/actions/place-trade': mockPlaceTrade,
+          '../../my-positions/actions/add-close-position-trade-group': mockAddClosePositionTradeGroup
+        });
+
+        store.dispatch(action.closePosition(t.arguments.marketID, t.arguments.outcomeID));
         t.assertions(store.getActions());
+        store.clearActions();
       });
-    }
+    };
 
     test({
-      description: 'should establish the correct `outcomeShares`',
+      description: `placeTrade returns tradeGroupID`,
+      arguments: {
+        marketID: '0xMarketID',
+        outcomeID: '0xOutcomeID',
+      },
+      tradeGroupID: '0x00000TradeGroupID',
+      assertions: (actions) => {
+        assert.deepEqual(actions, [
+          {
+            type: ADD_CLOSE_POSITION_TRADE_GROUP,
+            marketID: '0xMarketID',
+            outcomeID: '0xOutcomeID',
+            tradeGroupID: '0x00000TradeGroupID'
+          }
+        ]);
+      }
+    });
 
-    })
+    test({
+      description: `placeTrade returns tradeGroupID`,
+      arguments: {
+        marketID: '0xMarketID',
+        outcomeID: '0xOutcomeID',
+      },
+      tradeGroupID: '0x00000TradeGroupID',
+      placeTradeFails: true,
+      assertions: (actions) => {
+        assert.deepEqual(actions, [
+          {
+            type: ADD_CLOSE_POSITION_TRADE_GROUP,
+            marketID: '0xMarketID',
+            outcomeID: '0xOutcomeID',
+            tradeGroupID: CLOSE_DIALOG_FAILED
+          }
+        ]);
+      }
+    });
   });
 
-  describe('getBestFillParameters', () => {
-
-  });
+  describe('getBestFillParameters');
 });
-
-
-// describe("file.method", function() {
-//   // N tests total
-//   var someFunctionToMock = augur.someFunctionToMock;
-//   var callCounts = {
-//     someFunctionToMock: 0
-//   };
-//   afterEach(function() {
-//     ClearCallCounts(callCounts);
-//     augur.someFunctionToMock = someFunctionToMock;
-//   });
-//   var test = function(t) {
-//     // 2 its when required, one for sync test one for async
-//     it(t.description + ' sync', function() {
-//       augur.someFunctionToMock = t.someFunctionToMock;
-//
-//       t.assertions(null, augur.functionWeAreTesting(t.arg1, t.arg2));
-//     });
-//     it(t.description + ' async', function(done) {
-//       augur.someFunctionToMock = t.someFunctionToMock;
-//
-//       augur.functionWeAreTesting(t.arg1, t.arg2, function(err, data) {
-//         t.assertions(err, data);
-//         done();
-//       });
-//     });
-//   };
-//   test({
-//     description: 'some description of the test case',
-//     arg1: [],
-//     arg2: 'hell World',
-//     someFunctionToMock: function(arg) {
-//       return arg;
-//     },
-//     assertions: function(err, data) {
-//       assert.isNull(err);
-//       assert.deepEqual(data, ['some data']);
-//     }
-//   });
-// });
