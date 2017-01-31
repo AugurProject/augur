e2e26b0e9b12817da271cfbe9ab6fd57ecaf1fcd
Jack Peterson <jack@tinybike.net>
2017-01-31 00:32:26 -0800

    Added cancelOpenOrdersInClosedMarkets action to claimProceeds

--- src/modules/my-positions/actions/claim-proceeds.js
+++ src/modules/my-positions/actions/claim-proceeds.js
@@ -2,21 +2,21 @@ import async from 'async';
 import { augur } from '../../../services/augurjs';
 import { updateAssets } from '../../auth/actions/update-assets';
 import { loadAccountTrades } from '../../my-positions/actions/load-account-trades';
+import { cancelOpenOrdersInClosedMarkets } from '../../user-open-orders/actions/cancel-open-orders-in-closed-markets';
 import selectWinningPositions from '../../my-positions/selectors/winning-positions';
 
-export function claimProceeds() {
-  return (dispatch, getState) => {
-    const { branch, loginAccount, outcomesData } = getState();
-    if (loginAccount.address) {
-      const winningPositions = selectWinningPositions(outcomesData);
-      console.log('closed markets with winning shares:', winningPositions);
-      if (winningPositions.length) {
-        augur.claimMarketsProceeds(branch.id, winningPositions, (err, claimedMarkets) => {
-          if (err) return console.error('claimMarketsProceeds failed:', err);
-          dispatch(updateAssets());
-          async.each(claimedMarkets, (market, nextMarket) => dispatch(loadAccountTrades(market.id, () => nextMarket())));
-        });
-      }
+export const claimProceeds = () => (dispatch, getState) => {
+  const { branch, loginAccount, outcomesData } = getState();
+  if (loginAccount.address) {
+    const winningPositions = selectWinningPositions(outcomesData);
+    console.log('closed markets with winning shares:', winningPositions);
+    if (winningPositions.length) {
+      augur.claimMarketsProceeds(branch.id, winningPositions, (err, claimedMarkets) => {
+        if (err) return console.error('claimMarketsProceeds failed:', err);
+        dispatch(updateAssets());
+        async.each(claimedMarkets, (market, nextMarket) => dispatch(loadAccountTrades(market.id, () => nextMarket())));
+      });
     }
-  };
-}
+    dispatch(cancelOpenOrdersInClosedMarkets());
+  }
+};
