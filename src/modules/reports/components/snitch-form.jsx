f72ba93238e238f1033c1e5288d8413a79ebd415
Jack Peterson <jack@tinybike.net>
2017-01-31 21:45:31 -0800

    Renamed slash-rep-form to snitch-form

--- src/modules/reports/components/snitch-form.jsx
+++ src/modules/reports/components/snitch-form.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import classnames from 'classnames';
 import Checkbox from 'modules/common/components/checkbox';
 
-export default class SlashRepForm extends Component {
+export default class SnitchForm extends Component {
   // TODO -- Prop Validations
   static propTypes = {};
 
@@ -21,26 +20,26 @@ export default class SlashRepForm extends Component {
     const p = this.props;
     const s = this.state;
     return (
-      <article className="slash-rep-form">
-        <div className="slash-rep-message">
+      <article className="snitch-form">
+        <div className="snitch-message">
           It is important for Augur&#39;s security that Reporters keep their reports <b>secret</b> until the first half of the reporting cycle is complete (in ${p.branch.phaseTimeRemaining}).  If another Reporter has disclosed their Report (and its accompanying randomly-generated unique identifier) to you, you can enter the other Reporter&#39;s reported values here and you will receive half of their Reputation balance.  (Note: there is no penalty to you if this fails.  The other Reporter will only be notified if the information you enter matches their report.)
         </div>
-        <label htmlFor="slash-rep-reporter">
+        <label htmlFor="snitch-reporter">
           <h4>Ethereum address of cheater</h4>
           <input
             type="text"
-            className="slash-rep-input"
-            name="slash-rep-reporter"
+            className="snitch-input"
+            name="snitch-reporter"
             value={s.reporter}
             onChange={reporter => this.handleSlashRepChange({ reporter })}
           />
         </label>
-        <label htmlFor="slash-rep-report">
+        <label htmlFor="snitch-report">
           <h4>Outcome reported by cheater</h4>
           <input
             type="text"
-            className="slash-rep-input"
-            name="slash-rep-report"
+            className="snitch-input"
+            name="snitch-report"
             disabled={!!s.isIndeterminate}
             value={s.report}
             onChange={report => this.setState({ report })}
@@ -56,26 +55,26 @@ export default class SlashRepForm extends Component {
             onClick={isUnethical => this.setState({ isUnethical })}
           />
         </label>
-        <label htmlFor="slash-rep-salt">
+        <label htmlFor="snitch-salt">
           <h4>Report&#39;s unique ID (salt)</h4>
           <input
             type="text"
-            className="slash-rep-input"
-            name="slash-rep-salt"
+            className="snitch-input"
+            name="snitch-salt"
             value={s.salt}
             onChange={salt => this.setState({ salt })}
           />
         </label>
-	      <div className="submit-slash-rep">
-	        <button
-	          className="button"
-	          disabled={(!s.reporter && !s.report && !s.salt)}
-	          onClick={() => p.onSubmitSlashRep(s.salt, s.report, s.reporter, s.isIndeterminate, s.isUnethical)}
-	        >
-	          Fine Reporter
-	        </button>
-	      </div>
+        <div className="submit-snitch">
+          <button
+            className="button"
+            disabled={(!s.reporter && !s.report && !s.salt)}
+            onClick={() => p.onSubmitSlashRep(s.salt, s.report, s.reporter, s.isIndeterminate, s.isUnethical)}
+          >
+            Fine Reporter
+          </button>
+        </div>
       </article>
     );
   }
- }
+}
