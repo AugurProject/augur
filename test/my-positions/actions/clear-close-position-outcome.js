418e4605d53af78867cabaaed0dfe93703ede1dd
Stephen Sprinkle <stephen@sprinkle.io>
2017-02-01 10:13:50 -0700

    fix to test

--- test/my-positions/actions/clear-close-position-outcome.js
+++ test/my-positions/actions/clear-close-position-outcome.js
@@ -1,13 +1,13 @@
 import { describe, it } from 'mocha';
 import { assert } from 'chai';
 
-import { CLEAR_CLOSE_POSITION_OUTCOME, clearClosePositionTradeGroup } from 'modules/my-positions/actions/clear-close-position-outcome';
+import { CLEAR_CLOSE_POSITION_OUTCOME, clearClosePositionOutcome } from 'modules/my-positions/actions/clear-close-position-outcome';
 
 describe('modules/my-positions/actions/clear-close-position-outcome.js', () => {
   describe('clearClosePositionTradeGroup', () => {
     const test = (t) => {
       it(t.description, () => {
-        t.assertions(clearClosePositionTradeGroup(t.arguments.marketID, t.arguments.outcomeID));
+        t.assertions(clearClosePositionOutcome(t.arguments.marketID, t.arguments.outcomeID));
       });
     };
 
