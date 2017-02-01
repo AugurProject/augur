e14b8f4f3faab63095edaa9a922396ce66f893bb
Jack Peterson <jack@tinybike.net>
2017-01-31 18:07:11 -0800

    Added slash-rep action to reports module

---
+++ src/modules/reports/actions/slash-rep.js
@@ -0,0 +1,20 @@
+import { augur } from '../../../services/augurjs';
+import { selectMarketFromEventID } from '../../market/selectors/market';
+
+export const slashRep = (market, salt, report, reporter, isIndeterminate, isUnethical) => (
+  dispatch => augur.slashRep({
+    branch: market.branchId,
+    salt,
+    report,
+    reporter,
+    isIndeterminate,
+    isUnethical,
+    eventID: market.eventID,
+    minValue: market.minValue,
+    maxValue: market.maxValue,
+    type: market.type,
+    onSent: r => console.debug('slashRep sent:', r),
+    onSuccess: r => console.debug('slashRep success:', r),
+    onFailed: e => console.error('slashRep failed:', e)
+  })
+);
