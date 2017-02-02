9c9b45079b4f7d23a675bb46b04a7b6aba764348
Stephen Sprinkle <stephen@sprinkle.io>
2017-02-02 14:34:42 -0700

    import form styling

--- src/modules/auth/components/auth-import.jsx
+++ src/modules/auth/components/auth-import.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
+
+import Input from 'modules/common/components/input';
 
 export default class AuthImport extends Component {
   constructor(props) {
@@ -7,7 +10,10 @@ export default class AuthImport extends Component {
     this.state = {
       password: '',
       loginAccount: null,
-      rememberMe: true
+      rememberMe: true,
+      // These prevent a flash on component mount
+      isPasswordDisplayable: false,
+      isImportActionsDisplayable: false
     };
   }
 
@@ -19,66 +25,83 @@ export default class AuthImport extends Component {
       <form
         className="auth-import-form"
         onSubmit={(e) => {
-          e.stopPropagation();
           e.preventDefault();
 
           console.log('submit! -- ', s.loginAccount);
 
-          p.importAccountFromFile(s.password, s.rememberMe, s.loginAccount);
+          if (p.loginAccount && p.password) {
+            p.importAccountFromFile(s.password, s.rememberMe, s.loginAccount);
+          }
         }}
       >
+        <span>Import account from file</span>
         <input
+          className="auth-import-file"
           name="file"
           type="file"
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
           onChange={(e) => {
-            console.log('e.target -- ', e.target.files[0]);
+            console.log('e -- ', e.target.files);
+            if (e.target.files.length) {
+              const fileReader = new FileReader();
 
-            const fileReader = new FileReader();
+              fileReader.readAsText(e.target.files[0]);
 
-            fileReader.readAsText(e.target.files[0]);
-            fileReader.onload = (e) => {
-              this.setState({ loginAccount: JSON.parse(e.target.result) });
-            };
+              fileReader.onload = (e) => {
+                this.setState({ loginAccount: JSON.parse(e.target.result) });
+              };
+
+              if (!this.state.isPasswordDisplayable) {
+                this.setState({ isPasswordDisplayable: true });
+              }
+            } else if (this.state.loginAccount) {
+              this.setState({ loginAccount: null });
+            }
           }}
         />
-        <input
+        <Input
+          className={classNames('auth-import-password', {
+            animateIn: s.loginAccount,
+            animateOut: !s.loginAccount && s.isPasswordDisplayable
+          })}
           name="password"
           type="password"
           placeholder="Password"
-          value={s.password}
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-          onChange={(e) => {
-            this.setState({ password: e.target.value });
+          canToggleVisibility
+          onChange={(password) => {
+            this.setState({ password });
+
+            if (!this.state.isImportActionsDisplayable) {
+              this.setState({ isImportActionsDisplayable: true });
+            }
           }}
         />
-        <label // eslint-disable-line jsx-a11y/no-static-element-interactions
-          htmlFor="remember_me_input"
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
+        <div
+          className={classNames('auth-import-actions', {
+            animateInPartial: s.loginAccount && s.password,
+            animateOutPartial: (!s.loginAccount || !s.password) && s.isImportActionsDisplayable
+          })}
         >
-          Remember Me:
-          <input
-            id="remember_me_input"
-            type="checkbox"
-            checked={s.rememberMe}
-            onChange={(e) => {
-              this.setState({ rememberMe: e.target.checked });
-            }}
-          />
-        </label>
-        <input
-          type="submit"
-          value="Import"
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-        />
+          <label // eslint-disable-line jsx-a11y/no-static-element-interactions
+            className="auth-signup-remember-me"
+            htmlFor="remember_me_input"
+          >
+            Remember Me:
+            <input
+              id="remember_me_input"
+              type="checkbox"
+              checked={s.rememberMe}
+              onChange={(e) => {
+                this.setState({ rememberMe: e.target.checked });
+              }}
+            />
+          </label>
+          <button
+            className="submit"
+            type="submit"
+          >
+            Import
+          </button>
+        </div>
       </form>
     );
   }
