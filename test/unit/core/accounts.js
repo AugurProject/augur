a425dbd7c5cc71c1e865c4cdd54e96e426287e9f
John Danz <John.Danz87@gmail.com>
2017-02-02 10:49:34 -0600

    finished register tests for accounts, 1/12

--- test/unit/core/accounts.js
+++ test/unit/core/accounts.js
@@ -3,6 +3,8 @@
 var assert = require('chai').assert;
 var augur = require('../../../src');
 var errors = require("ethrpc").errors;
+var keys = require("keythereum");
+var constants = require("../../../src/constants");
 
 var accounts = [{
     loginID: undefined,
@@ -23,33 +25,39 @@ var accounts = [{
   }];
 
 describe("accounts.register", function() {
-  // ? tests total
-  before(function() {
-    // run this one time before running any register tests.
-    // make sure accounts is reset to default state.
-    accounts = [{
-        loginID: undefined,
-        name: 'account1',
-        address: undefined,
-        password: 'helloWorld',
-        keystore: undefined,
-        privateKey: undefined,
-        derivedKey: undefined
-      }, {
-        loginID: undefined,
-        name: 'account2',
-        address: undefined,
-        password: 'password',
-        keystore: undefined,
-        privateKey: undefined,
-        derivedKey: undefined
-      }];
-  });
+  // 8 tests total
+  var create = keys.create;
+  var deriveKey = keys.deriveKey;
+  var KDF = constants.KDF;
+  accounts = [{
+      loginID: undefined,
+      name: 'account1',
+      address: undefined,
+      password: 'helloWorld',
+      keystore: undefined,
+      privateKey: undefined,
+      derivedKey: undefined
+    }, {
+      loginID: undefined,
+      name: 'account2',
+      address: undefined,
+      password: 'password',
+      keystore: undefined,
+      privateKey: undefined,
+      derivedKey: undefined
+  }];
   afterEach(function() {
     augur.accounts.account = {};
+    keys.create = create;
+    keys.deriveKey = deriveKey;
+    constants.KDF = KDF;
   });
   var test = function(t) {
     it(t.description, function(done) {
+      keys.create = t.create || create;
+      keys.deriveKey = t.deriveKey || deriveKey;
+      constants.KDF = t.KDF || KDF;
+
       augur.accounts.register(t.name, t.password, function(result) {
         t.assertions(result);
         done();
@@ -70,6 +78,28 @@ describe("accounts.register", function() {
     password: undefined,
     assertions: function(result) {
       assert.deepEqual(result, errors.PASSWORD_TOO_SHORT);
+    }
+  });
+  test({
+    description: 'should return an error if there is an issue creating the private key',
+    name: 'testname1',
+    password: 'somevalidpassword',
+    create: function(params, cb) {
+      cb({error: 999, message: 'Uh-Oh!'});
+    },
+    assertions: function(result) {
+      assert.deepEqual(result, {error: 999, message: 'Uh-Oh!'});
+    }
+  });
+  test({
+    description: 'should return an error if there is an issue deriving the secret key',
+    name: 'testname1',
+    password: 'somevalidpassword',
+    deriveKey: function(password, salt, options, cb) {
+      cb({ error: 999, message: 'Uh-Oh!' });
+    },
+    assertions: function(result) {
+      assert.deepEqual(result, {error: 999, message: 'Uh-Oh!'});
     }
   });
   test({
@@ -112,6 +142,41 @@ describe("accounts.register", function() {
       accounts[1].privateKey = result.privateKey;
       accounts[1].keystore = result.keystore;
       accounts[1].derivedKey = result.derivedKey;
+    }
+  });
+  test({
+    description: 'should register an account given a valid name and password, should handle pbkdf2',
+    name: 'testAccountName1',
+    password: 'thisisavalidpassword',
+    KDF: 'pbkdf2',
+    assertions: function(result) {
+      assert.equal(result.name, 'testAccountName1');
+      assert.isString(result.loginID);
+      assert.isString(result.address);
+      assert.isObject(result.keystore);
+      assert(Buffer.isBuffer(result.privateKey));
+      assert(Buffer.isBuffer(result.derivedKey));
+      assert.deepEqual(result, augur.accounts.account);
+    }
+  });
+  test({
+    description: 'should register an account given a valid name and password but derived key returns a hex string',
+    name: 'testAccountName1',
+    password: 'thisisavalidpassword',
+    deriveKey: function(password, salt, options, cb) {
+      // we are going to use our mock function to call the original function. However we need to apply keys as the this inside of the original function as it's not attached the the original keys object anymore. We do this to be able to pass a hex string version of derivedKey. This is to check the if statement that converts the derivedKey to a buffer if it isn't already.
+      deriveKey.apply(keys, [password, salt, options, function(derivedKey) {
+        cb(derivedKey.toString('hex'));
+      }]);
+    },
+    assertions: function(result) {
+      assert.equal(result.name, 'testAccountName1');
+      assert.isString(result.loginID);
+      assert.isString(result.address);
+      assert.isObject(result.keystore);
+      assert(Buffer.isBuffer(result.privateKey));
+      assert(Buffer.isBuffer(result.derivedKey));
+      assert.deepEqual(result, augur.accounts.account);
     }
   });
 });
