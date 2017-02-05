ac4e5c117bbfb014e0022ccd4f1d8022e4c063dd
Jack Peterson <jack@tinybike.net>
2017-02-04 17:02:58 -0800

    Moved most update-market-order-book logic to augur.js

--- src/modules/bids-asks/actions/update-market-order-book.js
+++ src/modules/bids-asks/actions/update-market-order-book.js
@@ -1,5 +1,4 @@
-import BigNumber from 'bignumber.js';
-import { abi, augur, constants } from '../../../services/augurjs';
+import { augur } from '../../../services/augurjs';
 import { updateMarketPriceHistory } from '../../market/actions/update-market-price-history';
 import { updateMarketTradesData } from '../../portfolio/actions/update-market-trades-data';
 
@@ -11,36 +10,17 @@ export const clearMarketOrderBook = marketId => ({ type: CLEAR_MARKET_ORDER_BOOK
 
 export const addOrder = log => (dispatch, getState) => {
   const orderBook = { ...getState().orderBooks[log.market] };
-  if (orderBook) {
-    const orderBookSide = orderBook[log.type];
-    const market = getState().marketsData[log.market];
-    if (market) {
-      if (orderBookSide) {
-        orderBookSide[log.tradeid] = convertAddTxLogToOrder(log, market);
-        dispatch(updateMarketOrderBook(log.market, orderBook));
-      } else {
-        const matchedType = log.type === 'buy' ? 'sell' : 'buy';
-        const order = convertAddTxLogToOrder(log, market);
-        dispatch(updateMarketOrderBook(log.market, {
-          [log.type]: { [order.id]: order },
-          [matchedType]: {}
-        }));
-      }
-    }
+  const market = getState().marketsData[log.market];
+  if (market && orderBook) {
+    const order = augur.convertAddTxLogToOrder(log, market.type, market.minValue);
+    dispatch(updateMarketOrderBook(log.market, augur.addOrder(order, orderBook)));
   }
 };
 
 export const removeOrder = log => (dispatch, getState) => {
   const orderBook = { ...getState().orderBooks[log.market] };
-  if (orderBook) {
-    const orderBookSide = orderBook[log.type];
-    const market = getState().marketsData[log.market];
-    if (orderBookSide && market) {
-      if (orderBookSide[log.tradeid]) {
-        delete orderBookSide[log.tradeid];
-        dispatch(updateMarketOrderBook(log.market, orderBook));
-      }
-    }
+  if (orderBook && getState().marketsData[log.market]) {
+    dispatch(updateMarketOrderBook(log.market, augur.removeOrder(log.tradeid, log.type, orderBook)));
   }
 };
 
@@ -49,23 +29,9 @@ export const fillOrder = log => (dispatch, getState) => {
   const orderBook = { ...orderBooks[log.market] };
   const market = marketsData[log.market];
   if (market) {
-    if (orderBook) {
-      const matchedType = log.type === 'buy' ? 'sell' : 'buy';
-      const orderBookSide = orderBook[matchedType];
-      if (orderBookSide) {
-        const order = orderBookSide[log.tradeid];
-        if (order) {
-          const updatedAmount = abi.bignum(order.fullPrecisionAmount).minus(abi.bignum(log.amount));
-          if (updatedAmount.lte(constants.PRECISION.zero)) {
-            delete orderBookSide[log.tradeid];
-          } else {
-            order.fullPrecisionAount = updatedAmount.toFixed();
-            order.amount = augur.roundToPrecision(updatedAmount, constants.MINIMUM_TRADE_SIZE);
-          }
-          dispatch(updateMarketOrderBook(log.market, orderBook));
-        }
-      }
-    }
+    const matchedType = log.type === 'buy' ? 'sell' : 'buy';
+    const updatedOrderBook = augur.fillOrder(log.tradeid, log.amount, matchedType, orderBook);
+    dispatch(updateMarketOrderBook(log.market, updatedOrderBook));
     const marketPriceHistory = priceHistory[log.market] ? { ...priceHistory[log.market] } : {};
     if (!marketPriceHistory[log.outcome]) marketPriceHistory[log.outcome] = [];
     marketPriceHistory[log.outcome].push(log);
@@ -73,28 +39,3 @@ export const fillOrder = log => (dispatch, getState) => {
     dispatch(updateMarketPriceHistory(log.market, marketPriceHistory));
   }
 };
-
-function convertAddTxLogToOrder(log, market) {
-  let round;
-  let roundingMode;
-  if (log.type === 'buy') {
-    round = 'floor';
-    roundingMode = BigNumber.ROUND_DOWN;
-  } else {
-    round = 'ceil';
-    roundingMode = BigNumber.ROUND_UP;
-  }
-  const adjustedLog = market.type === 'scalar' ? augur.adjustScalarOrder({ ...log }, market.minValue) : { ...log };
-  return {
-    id: adjustedLog.tradeid,
-    type: adjustedLog.type,
-    market: adjustedLog.market,
-    amount: augur.roundToPrecision(abi.bignum(adjustedLog.amount), constants.MINIMUM_TRADE_SIZE),
-    fullPrecisionAmount: adjustedLog.amount,
-    price: augur.roundToPrecision(abi.bignum(adjustedLog.price), constants.PRECISION.zero, round, roundingMode),
-    fullPrecisionPrice: adjustedLog.price,
-    owner: adjustedLog.sender,
-    block: adjustedLog.blockNumber,
-    outcome: adjustedLog.outcome.toString()
-  };
-}
