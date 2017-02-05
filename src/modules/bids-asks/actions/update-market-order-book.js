ef06c18d4a107bcd72cdbd582faf1d6b395b6c89
Jack Peterson <jack@tinybike.net>
2017-02-04 21:07:38 -0800

    Added replaceMarketOrderBook action and reducer to bids-asks

--- src/modules/bids-asks/actions/update-market-order-book.js
+++ src/modules/bids-asks/actions/update-market-order-book.js
@@ -3,9 +3,11 @@ import { updateMarketPriceHistory } from '../../market/actions/update-market-pri
 import { updateMarketTradesData } from '../../portfolio/actions/update-market-trades-data';
 
 export const UPDATE_MARKET_ORDER_BOOK = 'UPDATE_MARKET_ORDER_BOOK';
+export const REPLACE_MARKET_ORDER_BOOK = 'REPLACE_MARKET_ORDER_BOOK';
 export const CLEAR_MARKET_ORDER_BOOK = 'CLEAR_MARKET_ORDER_BOOK';
 
 export const updateMarketOrderBook = (marketId, marketOrderBook) => ({ type: UPDATE_MARKET_ORDER_BOOK, marketId, marketOrderBook });
+export const replaceMarketOrderBook = (marketId, marketOrderBook) => ({ type: REPLACE_MARKET_ORDER_BOOK, marketId, marketOrderBook });
 export const clearMarketOrderBook = marketId => ({ type: CLEAR_MARKET_ORDER_BOOK, marketId });
 
 export const addOrder = log => (dispatch, getState) => {
@@ -13,14 +15,14 @@ export const addOrder = log => (dispatch, getState) => {
   const market = getState().marketsData[log.market];
   if (market && orderBook) {
     const order = augur.convertAddTxLogToOrder(log, market.type, market.minValue);
-    dispatch(updateMarketOrderBook(log.market, augur.addOrder(order, orderBook)));
+    dispatch(replaceMarketOrderBook(log.market, augur.addOrder(order, orderBook)));
   }
 };
 
 export const removeOrder = log => (dispatch, getState) => {
   const orderBook = { ...getState().orderBooks[log.market] };
   if (orderBook && getState().marketsData[log.market]) {
-    dispatch(updateMarketOrderBook(log.market, augur.removeOrder(log.tradeid, log.type, orderBook)));
+    dispatch(replaceMarketOrderBook(log.market, augur.removeOrder(log.tradeid, log.type, orderBook)));
   }
 };
 
@@ -31,7 +33,10 @@ export const fillOrder = log => (dispatch, getState) => {
   if (market) {
     const matchedType = log.type === 'buy' ? 'sell' : 'buy';
     const updatedOrderBook = augur.fillOrder(log.tradeid, log.amount, matchedType, orderBook);
-    dispatch(updateMarketOrderBook(log.market, updatedOrderBook));
+    if (this.options.debug.trading) {
+      console.debug('updatedOrderBook:', updatedOrderBook);
+    }
+    dispatch(replaceMarketOrderBook(log.market, updatedOrderBook));
     const marketPriceHistory = priceHistory[log.market] ? { ...priceHistory[log.market] } : {};
     if (!marketPriceHistory[log.outcome]) marketPriceHistory[log.outcome] = [];
     marketPriceHistory[log.outcome].push(log);
