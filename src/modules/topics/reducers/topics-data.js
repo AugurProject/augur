908e7a88f2358f1cacd8b3aa3908d1775abc211d
Jack Peterson <jack@tinybike.net>
2017-02-07 12:35:54 -0800

    Updated topics reducer for topics object instead of array

--- src/modules/topics/reducers/topics-data.js
+++ src/modules/topics/reducers/topics-data.js
@@ -1,10 +1,12 @@
-import { augur } from '../../../services/augurjs';
 import { UPDATE_TOPICS, CLEAR_TOPICS } from '../../topics/actions/update-topics';
 
 export default function (topics = [], action) {
   switch (action.type) {
     case UPDATE_TOPICS: {
-      return topics.concat(action.topics).sort(augur.sortTagsInfo);
+      return {
+        ...topics,
+        ...action.topics
+      };
     }
     case CLEAR_TOPICS:
       return [];
