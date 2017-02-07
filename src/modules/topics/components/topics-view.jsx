9fabf43f8b66afb7eb4f26109ee846891f05f6d1
Stephen Sprinkle <stephen@sprinkle.io>
2017-02-07 11:52:45 -0700

    stubbed component + styling

---
+++ src/modules/topics/components/topics-view.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TopicsView = (p) => {
+  // const nullMessage = 'No Topics Available';
+
+  console.log('p -- ', p);
+
+  return (
+    <section id="topics_view">
+      <span>Topics View</span>
+    </section>
+  );
+};
+
+export default TopicsView;
