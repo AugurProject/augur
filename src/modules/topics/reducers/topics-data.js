1c9c6ad1f2a7aa0982943144dd813bb20dc0be97
Jack Peterson <jack@tinybike.net>
2017-02-07 16:46:39 -0800

    Added increase(Market)TopicPopularity actions and reducer; call increaseMarketTopicPopularity in fillOrder

--- src/modules/topics/reducers/topics-data.js
+++ src/modules/topics/reducers/topics-data.js
@@ -1,15 +1,19 @@
-import { UPDATE_TOPICS, CLEAR_TOPICS } from '../../topics/actions/update-topics';
+import { UPDATE_TOPICS, CLEAR_TOPICS, INCREASE_TOPIC_POPULARITY } from '../../topics/actions/update-topics';
 
-export default function (topics = [], action) {
+export default function (topics = {}, action) {
   switch (action.type) {
-    case UPDATE_TOPICS: {
+    case UPDATE_TOPICS:
       return {
         ...topics,
         ...action.topics
       };
-    }
+    case INCREASE_TOPIC_POPULARITY:
+      return {
+        ...topics,
+        [action.topic]: !topics[action.topic] ? action.amount : topics[action.topic] + action.amount
+      };
     case CLEAR_TOPICS:
-      return [];
+      return {};
     default:
       return topics;
   }
