8a926ea71a2ab2c2a821d192e4fa6b751e55d29f
Stephen Sprinkle <stephen@sprinkle.io>
2017-02-10 16:24:10 -0700

    first post-restructure pass over styling

--- src/modules/common/components/paginator.jsx
+++ src/modules/common/components/paginator.jsx
@@ -3,32 +3,34 @@ import Link from 'modules/link/components/link';
 
 const Paginator = p => (
   <article className="paginator">
-    <div className="pagination-group-1">
-      {!!p.pagination && !!p.pagination.previousPageNum &&
-        <Link
-          {...p.pagination.previousPageLink}
-          className="button"
-        >
-          <i className="fa fa-angle-left" />
-        </Link>
-      }
-    </div>
+    <div className="pagination-control-container">
+      <div className="pagination-group-1">
+        {!!p.pagination && !!p.pagination.previousPageNum &&
+          <Link
+            {...p.pagination.previousPageLink}
+            className="button"
+          >
+            <i className="fa fa-angle-left" />
+          </Link>
+        }
+      </div>
 
-    <div className="pagination-group-2">
-      <span className="pagination-count">
-        {`${p.pagination.startItemNum} - ${p.pagination.endItemNum}`} <strong>of</strong> {p.pagination.numUnpaginated}
-      </span>
-    </div>
+      <div className="pagination-group-2">
+        <span className="pagination-count">
+          {`${p.pagination.startItemNum} - ${p.pagination.endItemNum}`} <strong>of</strong> {p.pagination.numUnpaginated}
+        </span>
+      </div>
 
-    <div className="pagination-group-3">
-      {!!p.pagination && !!p.pagination.nextPageNum &&
-        <Link
-          {...p.pagination.nextPageLink}
-          className="button"
-        >
-          <i className="fa fa-angle-right" />
-        </Link>
-      }
+      <div className="pagination-group-3">
+        {!!p.pagination && !!p.pagination.nextPageNum &&
+          <Link
+            {...p.pagination.nextPageLink}
+            className="button"
+          >
+            <i className="fa fa-angle-right" />
+          </Link>
+        }
+      </div>
     </div>
   </article>
 );
--- src/modules/topics/components/topic.jsx
+++ src/modules/topics/components/topic.jsx
@@ -5,9 +5,15 @@ import TopicIcon from 'modules/topics/components/topic-icon';
 
 const Topic = p => (
   <button className={classNames('unstyled topic-button', { isHero: p.isHero })}>
-    <TopicIcon {...p} />
-    <span>{p.popularity}</span>
-    <span>{p.topic.toUpperCase()}</span>
+    <div className="topic-content">
+      <TopicIcon {...p} />
+      <span className="topic-popularity">
+        {p.popularity}
+      </span>
+      <span className="topic-name">
+        {p.topic.toUpperCase()}
+      </span>
+    </div>
   </button>
 );
 
