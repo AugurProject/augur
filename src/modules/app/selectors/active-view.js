5be332d2b38517bf92b545142dd39a07b2016978
Stephen Sprinkle <stephen@sprinkle.io>
2017-02-11 10:47:28 -0700

    only load markets once a user has nav'd to the markets view

--- src/modules/app/selectors/active-view.js
+++ src/modules/app/selectors/active-view.js
@@ -1,6 +1,15 @@
-import store from '../../../store';
+import store from 'src/store';
+
+import { loadMarkets } from 'modules/markets/actions/load-markets';
+
+import { MARKETS } from 'modules/app/constants/views';
 
 export default function () {
-  const { activeView } = store.getState();
+  const { activeView, marketsData, branch } = store.getState();
+
+  if (activeView === MARKETS && Object.keys(marketsData).length === 0) {
+    store.dispatch(loadMarkets(branch.id));
+  }
+
   return activeView;
 }
