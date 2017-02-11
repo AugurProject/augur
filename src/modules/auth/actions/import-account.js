1d00d10f839dbe5b775d969261f07dad655ef4d7
Jack Peterson <jack@tinybike.net>
2017-02-10 19:58:13 -0800

    Cleanup import-account

--- src/modules/auth/actions/import-account.js
+++ src/modules/auth/actions/import-account.js
@@ -1,41 +1,38 @@
-import { accounts } from '../../../services/augurjs';
+import { augur } from '../../../services/augurjs';
 import { loadFullAccountData } from '../../auth/actions/load-login-account';
 import { fundNewAccount } from '../../auth/actions/fund-new-account';
 import { registerTimestamp } from '../../auth/actions/register-timestamp';
 import { anyAccountBalancesZero } from '../../auth/selectors/balances';
 
-export function importAccount(name, password, rememberMe, keystore) {
-  return (dispatch, getState) => {
-    const { links } = require('../../../selectors');
-    const localStorageRef = typeof window !== 'undefined' && window.localStorage;
-    accounts.importAccount(name, password, keystore, (loginAccount) => {
-      console.log('importAccount -- ', importAccount);
-      const importedAccount = { ...loginAccount };
-      if (importedAccount && importedAccount.keystore) {
-        if (rememberMe && localStorageRef && localStorageRef.setItem) {
-          const persistentAccount = { ...importedAccount };
-          if (Buffer.isBuffer(persistentAccount.privateKey)) {
-            persistentAccount.privateKey = persistentAccount.privateKey.toString('hex');
-          }
-          localStorageRef.setItem('account', JSON.stringify(persistentAccount));
+export const importAccount = (name, password, rememberMe, keystore) => (dispatch, getState) => {
+  const { links } = require('../../../selectors');
+  const localStorageRef = typeof window !== 'undefined' && window.localStorage;
+  augur.accounts.importAccount(name, password, keystore, (importedAccount) => {
+    console.log('importAccount -- ', importAccount);
+    if (importedAccount && importedAccount.keystore) {
+      if (rememberMe && localStorageRef && localStorageRef.setItem) {
+        const persistentAccount = { ...importedAccount };
+        if (Buffer.isBuffer(persistentAccount.privateKey)) {
+          persistentAccount.privateKey = persistentAccount.privateKey.toString('hex');
         }
-        dispatch(loadFullAccountData(importedAccount, (err, balances) => {
-          if (err || !balances) return console.error(err);
-          if (anyAccountBalancesZero(balances)) {
-            dispatch(fundNewAccount((e) => {
-              if (e) return console.error(e);
-              if (!getState().loginAccount.registerBlockNumber) {
-                dispatch(registerTimestamp());
-              }
-            }));
-          } else if (!getState().loginAccount.registerBlockNumber) {
-            dispatch(registerTimestamp());
-          }
-        }));
-        if (links && links.marketsLink) {
-          return links.marketsLink.onClick(links.marketsLink.href);
-        }
+        localStorageRef.setItem('account', JSON.stringify(persistentAccount));
       }
-    });
-  };
-}
+      dispatch(loadFullAccountData(importedAccount, (err, balances) => {
+        if (err || !balances) return console.error(err);
+        if (anyAccountBalancesZero(balances)) {
+          dispatch(fundNewAccount((e) => {
+            if (e) return console.error(e);
+            if (!getState().loginAccount.registerBlockNumber) {
+              dispatch(registerTimestamp());
+            }
+          }));
+        } else if (!getState().loginAccount.registerBlockNumber) {
+          dispatch(registerTimestamp());
+        }
+      }));
+      if (links && links.marketsLink) {
+        return links.marketsLink.onClick(links.marketsLink.href);
+      }
+    }
+  });
+};
