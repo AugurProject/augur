eb480d7a59a56e7beac6f2015d95496bf11e152a
Jack Peterson <jack@tinybike.net>
2017-02-12 14:15:01 -0800

    Linting

--- src/modules/auth/actions/use-unlocked-account.js
+++ src/modules/auth/actions/use-unlocked-account.js
@@ -2,7 +2,7 @@ import { augur } from '../../../services/augurjs';
 import { loadFullAccountData } from '../../auth/actions/load-account-data';
 
 // Use unlocked local address (if actually unlocked)
-export const useUnlockedAccount = unlockedAddress => dispatch => {
+export const useUnlockedAccount = unlockedAddress => (dispatch) => {
   if (!unlockedAddress) return console.error('no account address');
   augur.rpc.unlocked(unlockedAddress, (isUnlocked) => {
     if (!isUnlocked || isUnlocked.error) {
@@ -11,5 +11,5 @@ export const useUnlockedAccount = unlockedAddress => dispatch => {
     augur.accounts.logout(); // clear the client-side account
     console.info('using unlocked account:', unlockedAddress);
     dispatch(loadFullAccountData({ address: unlockedAddress, isUnlocked: true }));
-  })
+  });
 };
