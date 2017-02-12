925bf7aeb2c0be22653aa01bc466da19c3e94714
Jack Peterson <jack@tinybike.net>
2017-02-12 14:12:26 -0800

    Added use-unlocked-account tests

--- src/modules/auth/actions/use-unlocked-account.js
+++ src/modules/auth/actions/use-unlocked-account.js
@@ -2,7 +2,8 @@ import { augur } from '../../../services/augurjs';
 import { loadFullAccountData } from '../../auth/actions/load-account-data';
 
 // Use unlocked local address (if actually unlocked)
-export const useUnlockedAccount = unlockedAddress => dispatch => (
+export const useUnlockedAccount = unlockedAddress => dispatch => {
+  if (!unlockedAddress) return console.error('no account address');
   augur.rpc.unlocked(unlockedAddress, (isUnlocked) => {
     if (!isUnlocked || isUnlocked.error) {
       return console.warn('account is locked:', unlockedAddress, isUnlocked);
@@ -11,4 +12,4 @@ export const useUnlockedAccount = unlockedAddress => dispatch => (
     console.info('using unlocked account:', unlockedAddress);
     dispatch(loadFullAccountData({ address: unlockedAddress, isUnlocked: true }));
   })
-);
+};
