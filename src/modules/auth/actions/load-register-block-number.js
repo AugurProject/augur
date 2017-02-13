6cf3659363d522ec66af6a5cd03e47be47a48759
Jack Peterson <jack@tinybike.net>
2017-02-13 02:02:39 -0800

    Expanded augur.Register.register wrapper and renamed to loadRegisterBlockNumber

---
+++ src/modules/auth/actions/load-register-block-number.js
@@ -0,0 +1,22 @@
+import { augur } from '../../../services/augurjs';
+import { loadAccountHistory } from '../../../modules/auth/actions/load-account-history';
+import { updateLoginAccount } from '../../../modules/auth/actions/update-login-account';
+
+export const loadRegisterBlockNumber = () => (dispatch, getState) => {
+  const { loginAccount } = getState();
+  if (loginAccount && loginAccount.address) {
+    if (loginAccount.registerBlockNumber) return dispatch(loadAccountHistory());
+    augur.getRegisterBlockNumber(loginAccount.address, (err, blockNumber) => {
+      if (err) return console.error(err);
+      if (!blockNumber) {
+        augur.Register.register(augur.utils.noop, (r) => {
+          dispatch(updateLoginAccount({ registerBlockNumber: r.blockNumber }));
+          dispatch(loadAccountHistory());
+        }, e => console.error(e));
+      } else {
+        dispatch(updateLoginAccount({ registerBlockNumber: blockNumber }));
+        dispatch(loadAccountHistory());
+      }
+    });
+  }
+};
--- src/modules/auth/actions/register-timestamp.js
+++
@@ -1,8 +0,0 @@
-import { augur, utils } from '../../../services/augurjs';
-import { updateLoginAccount } from '../../auth/actions/update-login-account';
-
-export const registerTimestamp = () => (dispatch, getState) => (
-  augur.Register.register(utils.noop, r => dispatch(updateLoginAccount({
-    registerBlockNumber: r.blockNumber
-  })), e => e && console.error(e))
-);
