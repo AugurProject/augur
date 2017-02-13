c01296dd9f474cb1231ec54558801ea944d01982
Jack Peterson <jack@tinybike.net>
2017-02-13 02:15:33 -0800

    Use loginAccount state instead of account parameter in display-login-message

--- src/modules/login-message/actions/display-login-message.js
+++ src/modules/login-message/actions/display-login-message.js
@@ -2,11 +2,11 @@ import isCurrentLoginMessageRead from '../../login-message/helpers/is-current-lo
 import isUserLoggedIn from '../../auth/helpers/is-user-logged-in';
 
 // decide if we need to display the login message
-export const displayLoginMessageOrMarkets = account => (dispatch, getState) => {
+export const displayLoginMessageOrMarkets = () => (dispatch, getState) => {
   const { links } = require('../../../selectors');
   if (links && links.marketsLink) {
-    const { loginMessage } = getState();
-    if (isUserLoggedIn(account) && !isCurrentLoginMessageRead(loginMessage)) {
+    const { loginAccount, loginMessage } = getState();
+    if (isUserLoggedIn(loginAccount) && !isCurrentLoginMessageRead(loginMessage)) {
       links.loginMessageLink.onClick();
     } else {
       links.marketsLink.onClick();
