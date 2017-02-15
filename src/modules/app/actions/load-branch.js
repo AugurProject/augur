c3132c4ceac97a8bd5465a7b66bb4ba86ebbabd8
Jack Peterson <jack@tinybike.net>
2017-02-15 13:47:20 -0800

    Removed early-exit from syncBranch error in loadBranch

--- src/modules/app/actions/load-branch.js
+++ src/modules/app/actions/load-branch.js
@@ -9,26 +9,24 @@ import { loadMarkets } from '../../markets/actions/load-markets';
 import { loadFullMarket } from '../../market/actions/load-full-market';
 import { clearMarketsData } from '../../markets/actions/update-markets-data';
 
-export function loadBranch(branchID) {
-  return (dispatch, getState) => {
-    dispatch(clearMarketsData());
-    augur.loadBranch(branchID, (err, branch) => {
-      if (err) return console.log('ERROR loadBranch', err);
-      dispatch(updateBranch({
-        ...branch,
-        ...reportingCycle(branch.periodLength)
-      }));
-      const { selectedMarketID } = getState();
-      if (selectedMarketID !== null) {
-        dispatch(loadFullMarket(selectedMarketID));
-      }
-      dispatch(loadTopics(branchID));
-      dispatch(loadMarkets(branchID));
-      dispatch(syncBlockchain());
-      dispatch(syncBranch((err) => {
-        if (err) return console.error('syncBranch:', err);
-        dispatch(listenToUpdates());
-      }));
-    });
-  };
-}
+export const loadBranch = branchID => (dispatch, getState) => {
+  dispatch(clearMarketsData());
+  augur.loadBranch(branchID, (err, branch) => {
+    if (err) return console.log('ERROR loadBranch', err);
+    dispatch(updateBranch({
+      ...branch,
+      ...reportingCycle(branch.periodLength)
+    }));
+    const { selectedMarketID } = getState();
+    if (selectedMarketID !== null) {
+      dispatch(loadFullMarket(selectedMarketID));
+    }
+    dispatch(loadTopics(branchID));
+    dispatch(loadMarkets(branchID));
+    dispatch(syncBlockchain());
+    dispatch(syncBranch((err) => {
+      if (err) console.error('syncBranch:', err);
+      dispatch(listenToUpdates());
+    }));
+  });
+};
