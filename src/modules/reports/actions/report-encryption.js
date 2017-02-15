b0993112b8cf06c1c0df569faf308d5443983c8c
Jack Peterson <jack@tinybike.net>
2017-02-15 13:46:25 -0800

    Cleaned up encrypt/decryptReport; added encrypt/decryptReport unit tests

--- src/modules/reports/actions/report-encryption.js
+++ src/modules/reports/actions/report-encryption.js
@@ -1,28 +1,25 @@
 import { augur } from '../../../services/augurjs';
 
-export function encryptReport(report, encryptionKey, salt) {
+export const encryptReport = (report, salt) => {
+  const encryptionKey = augur.accounts.account.derivedKey;
   const encrypted = { report: 0, salt: 0 };
   if (encryptionKey) {
     encrypted.report = augur.encryptReport(report, encryptionKey, salt);
     encrypted.salt = augur.encryptReport(salt, encryptionKey);
-    console.debug('encrypted report:', encrypted);
   }
   return encrypted;
-}
+};
 
-export function decryptReport(branchID, period, eventID, callback) {
-  if (!augur.accounts.account.derivedKey) return callback(null);
-  augur.getAndDecryptReport(branchID, period, augur.accounts.account.address, eventID, {
-    derivedKey: augur.accounts.account.derivedKey
-  }, (plaintext) => {
+export const decryptReport = (branchID, period, eventID, callback) => {
+  const { address, derivedKey } = augur.accounts.account;
+  if (!derivedKey) return callback(null);
+  augur.getAndDecryptReport(branchID, period, address, eventID, { derivedKey }, (plaintext) => {
     if (!plaintext) return callback('getAndDecryptReport failed');
-    if (!plaintext.report || plaintext.error) {
-      return callback(plaintext);
-    }
+    if (!plaintext.report || plaintext.error) return callback(plaintext);
     callback(null, {
       reportedOutcomeID: !parseInt(plaintext.report, 16) ? null : plaintext.report,
       salt: parseInt(plaintext.salt, 16) === 0 ? null : plaintext.salt,
       isUnethical: parseInt(plaintext.ethics, 16) === 0
     });
   });
-}
+};
