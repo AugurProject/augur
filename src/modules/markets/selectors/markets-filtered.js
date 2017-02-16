da4905400b8758c5fb22d507f42a5c056c9110c2
Stephen Sprinkle <stephen@sprinkle.io>
2017-02-16 12:16:45 -0700

    fix to markets topic filter

--- src/modules/markets/selectors/markets-filtered.js
+++ src/modules/markets/selectors/markets-filtered.js
@@ -61,7 +61,7 @@ export const isMarketFiltersMatch = (market, keywords, selectedFilterSort, selec
       return true;
     }
 
-    return market.tags[0].name === selectedTopic;
+    return market.tags.length && market.tags[0].name === selectedTopic;
   }
 
   function isDisplayable(market) {
