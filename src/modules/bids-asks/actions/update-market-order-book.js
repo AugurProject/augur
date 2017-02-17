6b8822416078c8c928dd536d2062ce2d59e7d8c4
Jack Peterson <jack@tinybike.net>
2017-02-17 14:28:59 -0800

    Renamed increase(Market)TopicPopularity to update(Market)TopicPopularity

--- src/modules/bids-asks/actions/update-market-order-book.js
+++ src/modules/bids-asks/actions/update-market-order-book.js
@@ -1,7 +1,7 @@
 import { augur } from '../../../services/augurjs';
 import { updateMarketPriceHistory } from '../../market/actions/update-market-price-history';
 import { updateMarketTradesData } from '../../portfolio/actions/update-market-trades-data';
-import { increaseMarketTopicPopularity } from '../../topics/actions/update-topics';
+import { updateMarketTopicPopularity } from '../../topics/actions/update-topics';
 
 export const UPDATE_MARKET_ORDER_BOOK = 'UPDATE_MARKET_ORDER_BOOK';
 export const REPLACE_MARKET_ORDER_BOOK = 'REPLACE_MARKET_ORDER_BOOK';
@@ -17,12 +17,12 @@ export const addOrder = log => (dispatch, getState) => {
   if (market && orderBook) {
     const order = augur.convertAddTxLogToOrder(log, market.type, market.minValue);
     dispatch(replaceMarketOrderBook(log.market, augur.addOrder(order, orderBook)));
-    dispatch(increaseMarketTopicPopularity(log.market, log.amount));
   }
 };
 
 export const removeOrder = log => (dispatch, getState) => {
-  const orderBook = { ...getState().orderBooks[log.market] };
+  const { orderBooks } = getState();
+  const orderBook = orderBooks[log.market];
   if (orderBook && getState().marketsData[log.market]) {
     dispatch(replaceMarketOrderBook(log.market, augur.removeOrder(log.tradeid, log.type, orderBook)));
   }
@@ -39,11 +39,11 @@ export const fillOrder = log => (dispatch, getState) => {
       console.debug('updatedOrderBook:', updatedOrderBook);
     }
     dispatch(replaceMarketOrderBook(log.market, updatedOrderBook));
-    dispatch(increaseMarketTopicPopularity(log.market, log.amount));
     const marketPriceHistory = priceHistory[log.market] ? { ...priceHistory[log.market] } : {};
     if (!marketPriceHistory[log.outcome]) marketPriceHistory[log.outcome] = [];
     marketPriceHistory[log.outcome].push(log);
     dispatch(updateMarketTradesData({ [log.market]: marketPriceHistory }));
     dispatch(updateMarketPriceHistory(log.market, marketPriceHistory));
   }
+  dispatch(updateMarketTopicPopularity(log.market, log.amount));
 };
