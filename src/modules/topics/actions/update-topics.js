0a646afc1156bfbf10b6817ee37e9c76421b8b01
Jack Peterson <jack@tinybike.net>
2017-02-24 20:35:25 -0800

    Replaced setTimeout badness in updateMarketTopicPopularity

--- src/modules/topics/actions/update-topics.js
+++ src/modules/topics/actions/update-topics.js
@@ -1,3 +1,4 @@
+import { augur } from '../../../services/augurjs';
 import { loadMarketsInfo } from '../../markets/actions/load-markets-info';
 
 export const UPDATE_TOPICS = 'UPDATE_TOPICS';
@@ -8,17 +9,16 @@ export const updateTopics = topics => ({ type: UPDATE_TOPICS, topics });
 export const clearTopics = () => ({ type: CLEAR_TOPICS });
 export const updateTopicPopularity = (topic, amount) => ({ type: UPDATE_TOPIC_POPULARITY, topic, amount });
 
-export const updateMarketTopicPopularity = (marketID, amount, isRetry) => (dispatch, getState) => {
-  console.debug('updateMarketTopicPopularity:', marketID, amount, isRetry);
+export const updateMarketTopicPopularity = (marketID, amount) => (dispatch, getState) => {
+  console.debug('updateMarketTopicPopularity:', marketID, amount);
   const market = getState().marketsData[marketID];
   if (market && market.tags && market.tags.length) {
     if (market.tags[0] !== null) dispatch(updateTopicPopularity(market.tags[0], Number(amount)));
   } else {
-    if (isRetry) return console.error('couldn\'t get topic for market', marketID);
-    if (market && market.isLoading) {
-      setTimeout(() => dispatch(updateMarketTopicPopularity(marketID, amount, true)), 3000); // eek ugly
-    } else {
-      dispatch(loadMarketsInfo([marketID], () => dispatch(updateMarketTopicPopularity(marketID, amount, true))));
-    }
+    augur.returnTags(marketID, (tags) => {
+      if (tags && tags.constructor === Array && tags.length && tags[0] !== null) {
+        dispatch(updateTopicPopularity(augur.decodeTag(tags[0]), Number(amount)));
+      }
+    });
   }
 };
