41f1f1d775395309a1d66e88a01f7dd40ee865a1
Stephen Sprinkle <stephen@sprinkle.io>
2017-02-27 11:27:36 -0700

    market properties null state

--- src/modules/create-market/components/create-market-preview.jsx
+++ src/modules/create-market/components/create-market-preview.jsx
@@ -5,34 +5,32 @@ import classNames from 'classnames';
 const CreateMarketPreview = p => (
   <article className="create-market-preview">
     <div className="create-market-details">
-      <div className="create-market-tags">
-        <ul>
-          <li
-            className={classNames('tag', {
-              isNull: !p.topic,
-              hasValue: !!p.topic
-            })}
-          >
-            {p.topic}
-          </li>
-          <li
-            className={classNames('tag', {
-              isNull: !(p.keywords && p.keywords[0]),
-              hasValue: p.keywords && !!p.keywords[0]
-            })}
-          >
-            {p.keywords && p.keywords[0]}
-          </li>
-          <li
-            className={classNames('tag', {
-              isNull: !(p.keywords && p.keywords[1]),
-              hasValue: p.keywords && !!p.keywords[1]
-            })}
-          >
-            {p.keywords && p.keywords[1]}
-          </li>
-        </ul>
-      </div>
+      <ul className="create-market-tags">
+        <li
+          className={classNames('create-market-tag', {
+            isNull: !p.topic,
+            hasValue: !!p.topic
+          })}
+        >
+          {p.topic}
+        </li>
+        <li
+          className={classNames('creat-market-tag', {
+            isNull: !(p.keywords && p.keywords[0]),
+            hasValue: p.keywords && !!p.keywords[0]
+          })}
+        >
+          {p.keywords && p.keywords[0]}
+        </li>
+        <li
+          className={classNames('create-market-tag', {
+            isNull: !(p.keywords && p.keywords[1]),
+            hasValue: p.keywords && !!p.keywords[1]
+          })}
+        >
+          {p.keywords && p.keywords[1]}
+        </li>
+      </ul>
       <span
         className={classNames('create-market-description', {
           isNull: !p.description,
@@ -41,7 +39,32 @@ const CreateMarketPreview = p => (
       >
         {p.description}
       </span>
-      <div className="create-market-properties"></div>
+      <ul className="create-market-properties">
+        <li
+          className={classNames('create-market-property', {
+            isNull: !p.endDate,
+            hasValue: !!p.endDate
+          })}
+        >
+          {p.endDate}
+        </li>
+        <li
+          className={classNames('create-market-property', {
+            isNull: !p.makerFee,
+            hasValue: !!p.makerFee
+          })}
+        >
+          {p.makerFee}
+        </li>
+        <li
+          className={classNames('create-market-property', {
+            isNull: !p.takerFee,
+            hasValue: !!p.takerFee
+          })}
+        >
+          {p.takerFee}
+        </li>
+      </ul>
     </div>
     <ul className="create-market-outcomes">
     </ul>
