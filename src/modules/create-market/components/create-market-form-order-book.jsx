5eefb49a8a880e191a74fe492a42a3d066d4f110
Stephen Sprinkle <stephen@sprinkle.io>
2017-03-01 10:39:07 -0700

    order input

--- src/modules/create-market/components/create-market-form-order-book.jsx
+++ src/modules/create-market/components/create-market-form-order-book.jsx
@@ -1,17 +1,35 @@
 import React, { Component, PropTypes } from 'react';
+import BigNumber from 'bignumber.js';
 
+import ComponentNav from 'modules/common/components/component-nav';
 import Input from 'modules/common/components/input';
 import CreateMarketFormErrors from 'modules/create-market/components/create-market-form-errors';
+
+import { BID, ASK } from 'modules/transactions/constants/types';
 
 export default class CreateMarketFormOrderBook extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      errors: [],
-      selectedOutcome: props.outcomes[0]
+    this.navItems = {
+      [BID]: {
+        label: 'Bid'
+      },
+      [ASK]: {
+        label: 'Ask'
+      }
     };
 
+    this.state = {
+      errors: [],
+      selectedOutcome: props.outcomes[0],
+      selectedNav: Object.keys(this.navItems)[0],
+      orderPrice: '',
+      orderQuantity: ''
+    };
+
+    this.handleOrderPriceUpdate = this.handleOrderPriceUpdate.bind(this);
+    this.handleOrderQuantityUpdate = this.handleOrderQuantityUpdate.bind(this);
     this.validateForm = this.validateForm.bind(this);
   }
 
@@ -22,6 +40,20 @@ export default class CreateMarketFormOrderBook extends Component {
 
   componentWillUpdate(nextProps, nextState) {
     // if (this.state.errors !== nextState.errors) nextProps.updateValidity(!nextState.errors.length);
+  }
+
+  handleOrderPriceUpdate(orderPriceRaw) {
+    const orderPrice = orderPriceRaw instanceof BigNumber ? orderPriceRaw.toNumber() : parseFloat(orderPriceRaw);
+
+    // TODO -- handle validation
+    this.setState({ orderPrice });
+  }
+
+  handleOrderQuantityUpdate(orderQuantityRaw) {
+    const orderQuantity = orderQuantityRaw instanceof BigNumber ? orderQuantityRaw.toNumber() : parseFloat(orderQuantityRaw);
+
+    // TODO -- handle validation
+    this.setState({ orderQuantity });
   }
 
   validateForm(orderBook) {
@@ -61,8 +93,35 @@ export default class CreateMarketFormOrderBook extends Component {
               })}
             </div>
           </div>
-          <div className="order-book-entry">
-
+          <div className="order-book-entry-container">
+            <div className="order-book-entry">
+              <ComponentNav
+                fullWidth
+                navItems={this.navItems}
+                selectedNav={s.selectedNav}
+                updateSelectedNav={selectedNav => this.setState({ selectedNav })}
+              />
+              <div className="order-book-entry-inputs">
+                <Input
+                  type="number"
+                  placeholder="Price"
+                  value={s.orderPrice}
+                  isIncrementable
+                  incrementAmount={0.1}
+                  updateValue={this.handleOrderPriceUpdate}
+                  onChange={this.handleOrderPriceUpdate}
+                />
+                <Input
+                  type="number"
+                  placeholder="Quantity"
+                  value={s.orderQuantity}
+                  isIncrementable
+                  incrementAmount={0.1}
+                  updateValue={this.handleOrderQuantityUpdate}
+                  onChange={this.handleOrderQuantityUpdate}
+                />
+              </div>
+            </div>
           </div>
         </div>
         <div className="order-book-preview">
