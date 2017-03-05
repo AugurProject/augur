e2cef67307b79aee4d8cb56d277cba6dd107b099
Stephen Sprinkle <stephen@sprinkle.io>
2017-03-04 17:06:32 -0700

    proper composition of create market title

--- src/modules/create-market/components/create-market-main-title.jsx
+++ src/modules/create-market/components/create-market-main-title.jsx
@@ -2,6 +2,8 @@ import React, { PropTypes } from 'react';
 
 import EmDash from 'modules/common/components/em-dash';
 
+import { BINARY, CATEGORICAL, SCALAR } from 'modules/markets/constants/market-types';
+
 const CreateMarketMainTitle = (p) => {
   const initialTitleClass = () => {
     if (p.validations.length) {
@@ -23,11 +25,24 @@ const CreateMarketMainTitle = (p) => {
     return '';
   };
 
+  const marketType = () => {
+    switch (p.type) {
+      case BINARY:
+        return 'Yes/No';
+      case CATEGORICAL:
+        return 'Multiple Choice';
+      case SCALAR:
+        return 'Numerical';
+      default:
+        return '';
+    }
+  };
+
   return (
     <article className="create-market-main-title">
       <h1 className={`title-animation-helper ${previewTitleClass()}`} />
       <h1 className={`initial-title ${initialTitleClass()}`}>Create Market</h1>
-      <h1 className={`preview-title ${previewTitleClass()}`}>Market Preview <EmDash /></h1>
+      <h1 className={`preview-title ${previewTitleClass()}`}>Market Preview <EmDash /> {marketType()}</h1>
     </article>
   );
 };
