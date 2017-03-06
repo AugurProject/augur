e3a6fff23969a0d26184cf2ed8422eeaddd4628e
tomkysar <tom@augur.net>
2017-03-06 12:41:00 +0000

    Updated login message

--- src/modules/login-message/components/login-message-view.jsx
+++ src/modules/login-message/components/login-message-view.jsx
@@ -53,6 +53,12 @@ const LoginMessagePage = p => (
         </li>
       </ol>
       <h2>Technical updates:</h2>
+      <h3>March 6, 2017</h3>
+      <ol>
+        <li>
+          Added a guide and link on how to recover REP / ETH sent to the beta.
+        </li>
+      </ol>
       <h3>March 5, 2017</h3>
       <ol>
         <li>
