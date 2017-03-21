bb3c3e92217b0a48849728c34478bd1ad766904c
Jack Peterson <jack@tinybike.net>
2017-03-21 13:37:17 -0700

    Rebuilt for deployment

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ yarn test
 git push origin master
 firebase deploy
 $AUGUR_CORE/load_contracts/update_private_chain_augur.sh
-ssh augur@45.33.59.27 -t "cd /home/augur/augur; /home/augur/augur/update-local.sh"
 git push heroku master
+ssh augur@45.33.59.27 -t "cd /home/augur/augur; /home/augur/augur/update-local.sh"
