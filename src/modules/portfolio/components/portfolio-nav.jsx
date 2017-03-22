e08e1a473911d31553b8715a14288861e8fb106e
Stephen Sprinkle <stephen@sprinkle.io>
2017-03-22 16:58:11 -0600

    cleanup + linting

--- src/modules/portfolio/components/portfolio-nav.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const PortfolioNav = p => <span>Portfolio Nav</span>;
-
-export default PortfolioNav;
--- src/modules/portfolio/components/portfolio-navs.jsx
+++ src/modules/portfolio/components/portfolio-navs.jsx
@@ -91,4 +91,4 @@ export default class PortfolioNavs extends Component {
       </article>
     );
   }
-};
+}
