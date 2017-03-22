c7163e89f0efc19d684271f844900a3fe2fec400
Stephen Sprinkle <stephen@sprinkle.io>
2017-03-22 16:36:55 -0600

    cleaned up the viewProps for the transactions component

--- src/modules/portfolio/components/portfolio-view.jsx
+++ src/modules/portfolio/components/portfolio-view.jsx
@@ -6,25 +6,23 @@ import Reports from 'modules/portfolio/components/reports';
 
 import { MY_POSITIONS, MY_MARKETS, MY_REPORTS } from 'modules/app/constants/views';
 
-const PortfolioView = (p) => {
-  console.log('@@@ PortfolioView -- ', p);
-
-  return (
-    <section id="portfolio_view" >
-      {p.activeView === MY_POSITIONS &&
-        <Positions {...p.positions} />
-      }
-      {p.activeView === MY_MARKETS &&
-        <Markets {...p.markets} />
-      }
-      {p.activeView === MY_REPORTS &&
-        <Reports {...p.reports} branch={p.branch} />
-      }
-    </section>
-  );
-};
+const PortfolioView = p => (
+  <section id="portfolio_view" >
+    {p.activeView === MY_POSITIONS &&
+      <Positions {...p.positions} />
+    }
+    {p.activeView === MY_MARKETS &&
+      <Markets {...p.markets} />
+    }
+    {p.activeView === MY_REPORTS &&
+      <Reports {...p.reports} branch={p.branch} />
+    }
+  </section>
+);
 
 PortfolioView.propTypes = {
+  activeView: PropTypes.string.isRequired,
+  branch: PropTypes.object.isRequired,
   navItems: PropTypes.array.isRequired,
   totals: PropTypes.object.isRequired,
   positions: PropTypes.object.isRequired,
