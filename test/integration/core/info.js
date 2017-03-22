7ca8cc0c30d27dfdce5faacfa57ee8f7fe17b90c
Jack Peterson <jack@tinybike.net>
2017-03-21 22:32:09 -0700

    Updated core/info integration tests

--- test/integration/core/info.js
+++ test/integration/core/info.js
@@ -15,24 +15,29 @@ var branchID = augur.constants.DEFAULT_BRANCH_ID;
 var accounts = tools.get_test_accounts(augur, tools.MAX_TEST_ACCOUNTS);
 var testAccount = accounts[0];
 var numMarkets = parseInt(augur.getNumMarketsBranch(branchID), 10);
-var markets = augur.getSomeMarketsInBranch(branchID, numMarkets - 100, numMarkets);
+var markets;
+if (numMarkets > 100) {
+  markets = augur.getSomeMarketsInBranch(branchID, numMarkets - 100, numMarkets);
+} else {
+  markets = augur.getMarketsInBranch(branchID);
+}
 var marketID = markets[0];
 var market_creator_1 = testAccount;
 var marketID2 = markets[1];
 var market_creator_2 = testAccount;
 var eventID = augur.getMarketEvent(marketID, 0);
 
-function check_account(account, testAccount) {
+function check_account(account) {
   assert.isAbove(abi.bignum(account).toNumber(), 0);
-  if (augur.rpc.nodes.local && augur.rpc.version() === "10101") {
-    assert(abi.bignum(account).eq(abi.bignum(testAccount)));
+  if (augur.rpc.version() === "10101") {
+    assert.include(accounts, account);
   }
 }
 
 describe("info.se", function () {
   describe("getCreator(" + eventID + ") [event]", function () {
     var test = function (r) {
-      check_account(r, testAccount);
+      check_account(r);
     };
     it("sync", function () {
       test(augur.getCreator(eventID));
@@ -45,7 +50,7 @@ describe("info.se", function () {
   });
   describe("getCreator(" + marketID + ") [market]", function () {
     var test = function (r) {
-      check_account(r, testAccount);
+      check_account(r);
     };
     it("sync", function () {
       test(augur.getCreator(marketID));
