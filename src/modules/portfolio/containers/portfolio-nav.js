026a0f1a8770603a1ebe38110aaaeff80ccf5bae
Stephen Sprinkle <stephen@sprinkle.io>
2017-03-22 11:08:42 -0600

    initial display of component within core stats component

---
+++ src/modules/portfolio/containers/portfolio-nav.js
@@ -0,0 +1,15 @@
+// NOTE --  The following is mostly a half-step towards the ultimate implementation
+//          of reselect (deferring work until the full-app refactor)
+
+import { connect } from 'react-redux';
+import portfolioNavs from 'modules/portfolio/components/portfolio-navs';
+
+import portfolioNavItems from 'modules/portfolio/selectors/portfolio-nav-items';
+
+const mapStateToProps = state => ({
+  portfolioStats: portfolioNavItems()
+});
+
+const portfolioNav = connect(mapStateToProps)(portfolioNavs);
+
+export default portfolioNav;
