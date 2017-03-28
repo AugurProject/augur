d713ec96717e2700e1b1d7042de28423210f22e6
Stephen Sprinkle <stephen@sprinkle.io>
2017-03-28 10:55:00 -0600

    sort order book for chart display

--- src/modules/order-book/selectors/order-book-series.js
+++ src/modules/order-book/selectors/order-book-series.js
@@ -1,30 +1,29 @@
+import BigNumber from 'bignumber.js';
+
 export function orderBookSeries(orderBook) {
   console.log('###orderBookSeries -- ', orderBook);
-  // const orderBookSeries = Object.keys(orderBook).reduce((p, outcome) => {
-  //   if (p[outcome] == null) p[outcome] = {};
-  //
-  //   Object.keys(orderBook[outcome]).forEach((type) => {
-  //     if (p[outcome][type] == null) p[outcome][type] = [];
-  //
-  //     let totalQuantity = new BigNumber(0);
-  //
-  //     orderBook[outcome][type].forEach((order) => {
-  //       const matchedPriceIndex = p[outcome][type].findIndex(existing => existing[0] === order.price.toNumber());
-  //
-  //       totalQuantity = totalQuantity.plus(order.quantity);
-  //
-  //       if (matchedPriceIndex > -1) {
-  //         p[outcome][type][matchedPriceIndex][1] = totalQuantity.toNumber();
-  //       } else {
-  //         p[outcome][type].push([order.price.toNumber(), totalQuantity.toNumber()]);
-  //       }
-  //     });
-  //
-  //     p[outcome][type].sort((a, b) => a[0] - b[0]);
-  //   });
-  //
-  //   return p;
-  // }, {});
-  //
-  // this.props.updateNewMarket({ orderBookSeries });
+
+  const orderBookSeries = Object.keys(orderBook).reduce((p, type) => {
+    if (p[type] == null) p[type] = [];
+
+    let totalQuantity = new BigNumber(0);
+
+    orderBook[type].forEach((order) => {
+      const matchedPriceIndex = p[type].findIndex(existing => existing[0] === order.price.value);
+
+      totalQuantity = totalQuantity.plus(order.shares.value);
+
+      if (matchedPriceIndex > -1) {
+        p[type][matchedPriceIndex][1] = totalQuantity.toNumber();
+      } else {
+        p[type].push([order.price.value, totalQuantity.toNumber()]);
+      }
+    });
+
+    p[type].sort((a, b) => a[0] - b[0]);
+
+    return p;
+  }, {});
+
+  console.log('###orderBookSeries RESULT -- ', orderBookSeries);
 }
