80c3d00098550c2a62b2339175b66add98aa5229
Jack Peterson <jack@tinybike.net>
2017-04-03 17:20:13 -0700

    Fixed and memoized Airbitz selector

--- src/modules/auth/selectors/abc.js
+++ src/modules/auth/selectors/abc.js
@@ -1,10 +1,16 @@
+import { createSelector } from 'reselect';
 import { makeABCUIContext } from 'airbitz-core-js-ui';
+import store from 'src/store';
 
 export default function () {
-  return makeABCUIContext({
+  return selectABCUIContext(store.getState());
+}
+
+export const selectABCUIContext = createSelector(
+  () => makeABCUIContext({
     apiKey: 'e239ec875955ec7474628a1dc3d449c8ea8e1b48',
     appId: 'net.augur.app',
     vendorName: 'Augur',
     vendorImageUrl: 'https://airbitz.co/go/wp-content/uploads/2016/08/augur_logo_100.png'
-  });
-}
+  })
+);
