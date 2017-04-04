5349c4ad150939888d7750b0c6266030dd50c67c
Jack Peterson <jack@tinybike.net>
2017-04-04 12:07:27 -0700

    Use selectLoginAccount in transactions container

--- src/modules/transactions/container.js
+++ src/modules/transactions/container.js
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux';
 import { selectTransactions } from 'modules/transactions/selectors/transactions';
 import { selectTransactionsTotals } from 'modules/transactions/selectors/transactions-totals';
+import { selectLoginAccount } from 'modules/account/selectors/login-account';
 import TransactionsView from 'modules/transactions/components/transactions-view';
 
 const mapStateToProps = state => ({
   branch: state.branch,
-  loginAccount: state.loginAccount,
+  loginAccount: selectLoginAccount(state),
   currentBlockNumber: state.blockchain.currentBlockNumber,
   transactions: selectTransactions(state),
   transactionsTotals: selectTransactionsTotals(state)
