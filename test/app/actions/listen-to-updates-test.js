eb30044bb0d67b34c3fba5440511e450d18a0667
Stephen Sprinkle <stephen@sprinkle.io>
2017-04-05 10:37:27 -0600

    reworked stubs + added test for collectedFees w/ matching user

--- test/app/actions/listen-to-updates-test.js
+++ test/app/actions/listen-to-updates-test.js
@@ -15,39 +15,64 @@ describe(`modules/app/actions/listen-to-updates.js`, () => {
   const store = mockStore(state);
   const AugurJS = {
     augur: {
-      filters: { listen: () => {} },
-      CompositeGetters: { getPositionInMarket: () => {} }
+      filters: {
+        listen: () => {}
+      },
+      CompositeGetters: {
+        getPositionInMarket: sinon.stub.yields(['0x0', '0x1'])
+      }
     },
     abi: {
-      number: () => {},
+      number: sinon.stub().returns([0, 1]),
       bignum: () => {}
     }
   };
-  const SyncBlockchain = {};
-  const SyncBranch = {};
-  const UpdateBranch = {};
-  const UpdateAssets = {};
-  const OutcomePrice = {};
+  // sinon.stub(AugurJS.augur.CompositeGetters, 'getPositionInMarket', (market, trader, cb) => {
+  //   cb(['0x0', '0x1']);
+  // });
+
+  const SyncBlockchain = {
+    syncBlockchain: sinon.stub().returns({ type: 'SYNC_BLOCKCHAIN' })
+  };
+
+  const SyncBranch = {
+    syncBranch: sinon.stub().returns({ type: 'SYNC_BRANCH' })
+  };
+
+  const UpdateBranch = {
+    updateBranch: sinon.stub().returns({ type: 'UPDATE_BRANCH' })
+  };
+
+  const UpdateAssets = {
+    updateAssets: sinon.stub().returns({ type: 'UPDATE_ASSETS' })
+  };
+
+  const OutcomePrice = {
+    updateOutcomePrice: sinon.stub().returns({ type: 'UPDATE_OUTCOME_PRICE' })
+  };
+
   const LoadMarketsInfo = {
     loadMarketsInfo: () => {}
   };
+  sinon.stub(LoadMarketsInfo, 'loadMarketsInfo', marketID => ({
+    type: 'LOAD_BASIC_MARKET',
+    marketID
+  }));
+
   const UpdateMarketOrderBook = {
     addOrder: sinon.stub().returns({ type: 'ADD_ORDER' }),
     fillOrder: sinon.stub().returns({ type: 'FILL_ORDER' }),
     removeOrder: sinon.stub().returns({ type: 'REMOVE_ORDER' })
   };
-  const UpdateTopics = {};
-  UpdateAssets.updateAssets = sinon.stub().returns({ type: 'UPDATE_ASSETS' });
-  SyncBlockchain.syncBlockchain = sinon.stub().returns({ type: 'SYNC_BLOCKCHAIN' });
-  SyncBranch.syncBranch = sinon.stub().returns({ type: 'SYNC_BRANCH' });
-  UpdateBranch.updateBranch = sinon.stub().returns({ type: 'UPDATE_BRANCH' });
-  OutcomePrice.updateOutcomePrice = sinon.stub().returns({ type: 'UPDATE_OUTCOME_PRICE' });
-  sinon.stub(LoadMarketsInfo, 'loadMarketsInfo', marketID => ({
-    type: 'LOAD_BASIC_MARKET',
-    marketID
-  }));
-  UpdateTopics.updateMarketTopicPopularity = sinon.stub().returns({ type: 'UPDATE_MARKET_TOPIC_POPULARITY' });
-  AugurJS.abi.number = sinon.stub().returns([0, 1]);
+
+  const UpdateTopics = {
+    updateMarketTopicPopularity: sinon.stub().returns({ type: 'UPDATE_MARKET_TOPIC_POPULARITY' })
+  };
+
+  const ConverLogsToTransactions = {
+    convertLogsToTransactions: sinon.stub().returns({ type: 'CONVERT_LOGS_TO_TRANSACTIONS' })
+  };
+
   // sinon.stub(AugurJS.augur.filters, 'listen', (cb) => {
   //   cb.block('blockhash');
   //   cb.log_fill_tx({
@@ -61,9 +86,7 @@ describe(`modules/app/actions/listen-to-updates.js`, () => {
   //   cb.marketCreated({ marketID: 'testID1', topic: 'topical' });
   //   cb.tradingFeeUpdated({ marketID: 'testID1' });
   // });
-  sinon.stub(AugurJS.augur.CompositeGetters, 'getPositionInMarket', (market, trader, cb) => {
-    cb(['0x0', '0x1']);
-  });
+
 
   const action = proxyquire('../../../src/modules/app/actions/listen-to-updates.js', {
     '../../../services/augurjs': AugurJS,
@@ -74,7 +97,8 @@ describe(`modules/app/actions/listen-to-updates.js`, () => {
     '../../markets/actions/update-outcome-price': OutcomePrice,
     '../../markets/actions/load-markets-info': LoadMarketsInfo,
     '../../bids-asks/actions/update-market-order-book': UpdateMarketOrderBook,
-    '../../topics/actions/update-topics': UpdateTopics
+    '../../topics/actions/update-topics': UpdateTopics,
+    '../../transactions/actions/convert-logs-to-transactions': ConverLogsToTransactions
   });
 
   beforeEach(() => {
@@ -133,24 +157,29 @@ describe(`modules/app/actions/listen-to-updates.js`, () => {
     }
   });
 
-  // test({
-  //   description: `should dispatch actions from collectedFees callback if sender IS logged in account`,
-  //   assertions: (store) => {
-  //     sinon.stub(AugurJS.augur.filters, 'listen', (cb) => {
-  //       cb.collectedFees({
-  //         sender: '0x0000000000000000000000000000000000000001'
-  //       });
-  //     });
-  //
-  //     store.dispatch(action.listenToUpdates());
-  //
-  //     console.log('actions -- ', store.getActions());
-  //
-  //     const expected = [];
-  //
-  //     // assert.deepEqual(store.getActions(), expected, `Didn't return the expected actions`);
-  //   }
-  // });
+  test({
+    description: `should dispatch actions from collectedFees callback if sender IS logged in account`,
+    assertions: (store) => {
+      sinon.stub(AugurJS.augur.filters, 'listen', (cb) => {
+        cb.collectedFees({
+          sender: '0x0000000000000000000000000000000000000001'
+        });
+      });
+
+      store.dispatch(action.listenToUpdates());
+
+      const expected = [
+        {
+          type: 'UPDATE_ASSETS'
+        },
+        {
+          type: 'CONVERT_LOGS_TO_TRANSACTIONS'
+        }
+      ];
+
+      assert.deepEqual(store.getActions(), expected, `Didn't return the expected actions`);
+    }
+  });
 });
 
 // store.dispatch(action.listenToUpdates());
