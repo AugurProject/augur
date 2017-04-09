f0bd0940f2e3814a78a6b11a5e84054c7afb009e
Jack Peterson <jack@tinybike.net>
2017-04-09 15:41:46 -0700

    Minor cleanup

--- src/modules/transact.js
+++ src/modules/transact.js
@@ -9,25 +9,27 @@ var store = require("../store");
 module.exports = {
 
   fire: function (tx, callback, wrapper, aux) {
-    var activeAccount = store.getState().activeAccount;
+    var state = store.getState();
+    var activeAccount = state.activeAccount;
     if (activeAccount && activeAccount.address) {
       tx.from = activeAccount.address;
     } else {
-      tx.from = tx.from || store.getState().fromAddress || store.getState().coinbaseAddress;
+      tx.from = tx.from || state.fromAddress || state.coinbaseAddress;
     }
     return this.rpc.fire(tx, callback, wrapper, aux);
   },
 
   transact: function (tx, onSent, onSuccess, onFailed) {
     var self = this;
-    var activeAccount = store.getState().activeAccount;
+    var state = store.getState();
+    var activeAccount = state.activeAccount;
     if (activeAccount && activeAccount.address && activeAccount.privateKey) {
       tx.from = activeAccount.address;
       tx.invoke = function (payload, onSent, onSuccess, onFailed) {
         return self.rpc.packageAndSubmitRawTransaction(payload, activeAccount.address, activeAccount.privateKey, onSent, onSuccess, onFailed);
       };
     } else {
-      tx.from = tx.from || store.getState().fromAddress || store.getState().coinbaseAddress;
+      tx.from = tx.from || state.fromAddress || state.coinbaseAddress;
     }
     return this.rpc.transact(tx, onSent, onSuccess, onFailed);
   }
