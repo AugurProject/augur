4fdbd4b155f4aad2c1208c8e3c7332558c59fe8d
Stephen Sprinkle <stephen@sprinkle.io>
2017-04-10 10:43:57 -0600

    added tests for loadDataForReportingTransaction action

--- test/transactions/actions/construct-transaction-test.js
+++ test/transactions/actions/construct-transaction-test.js
@@ -16,17 +16,28 @@ describe('modules/transactions/actions/contruct-transaction.js', () => {
   const mockStore = configureMockStore(middlewares);
 
   const MOCK_ACTION_TYPES = {
-    LOAD_MARKET_THEN_RETRY_CONVERSION: 'LOAD_MARKET_THEN_RETRY_CONVERSION'
+    LOAD_MARKET_THEN_RETRY_CONVERSION: 'LOAD_MARKET_THEN_RETRY_CONVERSION',
+    LOOKUP_EVENT_MARKETS_THEN_RETRY_CONVERSION: 'LOOKUP_EVENT_MARKETS_THEN_RETRY_CONVERSION'
   };
 
+  const mockRetryConversion = {
+    loadMarketThenRetryConversion: sinon.stub().returns({
+      type: MOCK_ACTION_TYPES.LOAD_MARKET_THEN_RETRY_CONVERSION
+    }),
+    lookupEventMarketsThenRetryConversion: sinon.stub().returns({
+      type: MOCK_ACTION_TYPES.LOOKUP_EVENT_MARKETS_THEN_RETRY_CONVERSION
+    })
+  };
+
+  const mockMarket = {
+    selectMarketIDFromEventID: sinon.stub()
+  };
+  mockMarket.selectMarketIDFromEventID.withArgs(undefined).returns(null);
+  mockMarket.selectMarketIDFromEventID.withArgs('0xEVENTID').returns('0xMARKETID');
+
   describe('loadDataForMarketTransaction', () => {
-    const mockLoadMarketThenRetryConversion = {
-      loadMarketThenRetryConversion: sinon.stub().returns({
-        type: MOCK_ACTION_TYPES.LOAD_MARKET_THEN_RETRY_CONVERSION
-      })
-    };
     const action = proxyquire('../../../src/modules/transactions/actions/construct-transaction', {
-      './retry-conversion': mockLoadMarketThenRetryConversion
+      './retry-conversion': mockRetryConversion
     });
 
     const test = (t) => {
@@ -109,6 +120,156 @@ describe('modules/transactions/actions/contruct-transaction.js', () => {
         };
 
         assert.deepEqual(actual, expected, `Didn't return the expected object`);
+      }
+    });
+  });
+
+  describe('loadDataForReportingTransaction', () => {
+    const action = proxyquire('../../../src/modules/transactions/actions/construct-transaction', {
+      './retry-conversion': mockRetryConversion,
+      '../../market/selectors/market': mockMarket
+    });
+
+    const test = (t) => {
+      it(t.description, () => {
+        const store = mockStore(t.state || {});
+
+        t.assertions(store);
+      });
+    };
+
+    test({
+      description: `should dispatch the expected actions with no markets loaded and no eventMarketMap`,
+      state: {
+        marketsData: {},
+        outcomesData: {}
+      },
+      assertions: (store) => {
+        const label = 'label';
+        const log = {
+          market: '0xMARKETID'
+          // Lack of event market map is mocked by excluding the event
+        };
+        const isRetry = false;
+        const callback = () => {};
+
+        store.dispatch(action.loadDataForReportingTransaction(label, log, isRetry, callback));
+
+        const actual = store.getActions();
+
+        const expected = [
+          {
+            type: MOCK_ACTION_TYPES.LOOKUP_EVENT_MARKETS_THEN_RETRY_CONVERSION
+          }
+        ];
+
+        assert.deepEqual(actual, expected, `Didn't return the expected actions`);
+      }
+    });
+
+    test({
+      description: `should dispatch the expected actions with no markets loaded and no eventMarketMap and isRetry`,
+      state: {
+        marketsData: {},
+        outcomesData: {}
+      },
+      assertions: (store) => {
+        const label = 'label';
+        const log = {
+          market: '0xMARKETID'
+          // Lack of event market map is mocked by excluding the event
+        };
+        const isRetry = true;
+        const callback = sinon.stub();
+
+        store.dispatch(action.loadDataForReportingTransaction(label, log, isRetry, callback));
+
+        assert.isTrue(callback.calledOnce, `Didn't call callback once as expected`);
+      }
+    });
+
+    test({
+      description: `should dispatch the expected actions with no markets loaded and with an eventMarketMap`,
+      state: {
+        marketsData: {},
+        outcomesData: {}
+      },
+      assertions: (store) => {
+        const label = 'label';
+        const log = {
+          market: '0xMARKETID',
+          event: '0xEVENTID'
+        };
+        const isRetry = false;
+        const callback = () => {};
+
+        store.dispatch(action.loadDataForReportingTransaction(label, log, isRetry, callback));
+
+        const actual = store.getActions();
+
+        const expected = [
+          {
+            type: MOCK_ACTION_TYPES.LOAD_MARKET_THEN_RETRY_CONVERSION
+          }
+        ];
+
+        assert.deepEqual(actual, expected, `Didn't return the expected actions`);
+      }
+    });
+
+    test({
+      description: `should dispatch the expected actions with no markets loaded and with an eventMarketMap and isRetry`,
+      state: {
+        marketsData: {},
+        outcomesData: {}
+      },
+      assertions: (store) => {
+        const label = 'label';
+        const log = {
+          market: '0xMARKETID',
+          event: '0xEVENTID'
+        };
+        const isRetry = true;
+        const callback = sinon.stub();
+
+        store.dispatch(action.loadDataForReportingTransaction(label, log, isRetry, callback));
+
+        assert.isTrue(callback.calledOnce, `Didn't call callback once as expected`);
+      }
+    });
+
+    test({
+      description: `should return the expected object with markets loaded and with an eventMarketMap`,
+      state: {
+        marketsData: {
+          '0xMARKETID': {
+            description: 'testing'
+          }
+        },
+        outcomesData: {
+          '0xMARKETID': {}
+        }
+      },
+      assertions: (store) => {
+        const label = 'label';
+        const log = {
+          market: '0xMARKETID',
+          event: '0xEVENTID'
+        };
+        const isRetry = false;
+        const callback = sinon.stub();
+
+        const result = store.dispatch(action.loadDataForReportingTransaction(label, log, isRetry, callback));
+
+        const expected = {
+          marketID: '0xMARKETID',
+          market: {
+            description: 'testing'
+          },
+          outcomes: {}
+        };
+
+        assert.deepEqual(result, expected, `Didn't return the expected actions`);
       }
     });
   });
