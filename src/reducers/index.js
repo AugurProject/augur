b1121502585dcc459706ac4629563b53cbaa1c03
Jack Peterson <jack@tinybike.net>
2017-04-11 00:52:00 -0700

    Renamed individual reducer functions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,26 +1,23 @@
 "use strict";
 
-var contractAddresses = require("./contract-addresses");
-var contractsAPI = require("./contracts-api");
-var coinbaseAddress = require("./coinbase-address");
-var fromAddress = require("./from-address");
-var activeAccount = require("./active-account");
-var subscriptions = require("./subscriptions");
+var contractAddressesReducer = require("./contract-addresses");
+var contractsAPIReducer = require("./contracts-api");
+var coinbaseAddressReducer = require("./coinbase-address");
+var fromAddressReducer = require("./from-address");
+var activeAccountReducer = require("./active-account");
+var subscriptionsReducer = require("./subscriptions");
 
 var reducer = function (state, action) {
   return {
-    contractAddresses: contractAddresses(state.contractAddresses, action),
-    contractsAPI: contractsAPI(state.contractsAPI, action),
-    coinbaseAddress: coinbaseAddress(state.coinbaseAddress, action),
-    fromAddress: fromAddress(state.fromAddress, action),
-    activeAccount: activeAccount(state.activeAccount, action),
-    subscriptions: subscriptions(state.subscriptions, action)
+    contractAddresses: contractAddressesReducer(state.contractAddresses, action),
+    contractsAPI: contractsAPIReducer(state.contractsAPI, action),
+    coinbaseAddress: coinbaseAddressReducer(state.coinbaseAddress, action),
+    fromAddress: fromAddressReducer(state.fromAddress, action),
+    activeAccount: activeAccountReducer(state.activeAccount, action),
+    subscriptions: subscriptionsReducer(state.subscriptions, action)
   };
 };
 
 module.exports = function (state, action) {
-  // if (action.type === "RESET_STATE") {
-  //   return reducer({}, action);
-  // }
   return reducer(state || {}, action);
 };
