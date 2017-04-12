4ede18aaf5a095a795e9a27b9775eedebd5393bb
Jack Peterson <jack@tinybike.net>
2017-04-12 16:16:17 -0700

    Re-reduxed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -19,5 +19,8 @@ var reducer = function (state, action) {
 };
 
 module.exports = function (state, action) {
+  if (action.type === "RESET_STATE") {
+    return reducer({}, action);
+  }
   return reducer(state || {}, action);
 };
--- src/store.js
+++ src/store.js
@@ -1,19 +1,3 @@
 "use strict";
 
-var reducer = require("./reducers");
-
-var state;
-
-var getState = function () {
-  return state;
-};
-
-var dispatch = function (action) {
-  state = reducer(state, action);
-};
-
-var reset = function () {
-  state = reducer({});
-};
-
-module.exports = { getState: getState, dispatch: dispatch, reset: reset };
+module.exports = require("redux").createStore(require("./reducers"));
