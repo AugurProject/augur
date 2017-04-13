258a1e678be84082f50e92cecb4bc5d4e39867fb
Stephen Sprinkle <stephen@sprinkle.io>
2017-04-13 17:05:11 -0600

    updated tests for my-positions selector

--- test/my-positions/selectors/my-positions-test.js
+++ test/my-positions/selectors/my-positions-test.js
@@ -2,75 +2,76 @@ import { describe, it } from 'mocha';
 import { assert } from 'chai';
 import proxyquire from 'proxyquire';
 
+import myPositions, { __RewireAPI__ } from 'modules/my-positions/selectors/my-positions';
+
 describe(`modules/my-positions/selectors/my-positions.js`, () => {
   proxyquire.noPreserveCache().noCallThru();
-  const mockSelectors = {
-    positionsMarkets: [{
-      id: 'test1',
-      isPendingReport: false,
-      positionsSummary: {
-        qtyShares: {
-          value: 5
-        }
-      },
-      description: 'test 1',
-      tags: ['testtag', 'test']
-    }, {
-      id: 'test2',
-      isPendingReport: true,
-      positionsSummary: {
-        qtyShares: {
-          value: 10
-        }
-      },
-      description: 'test 2',
-      tags: ['testtag', 'test']
-    }, {
-      id: 'test3',
-      isPendingReport: false,
-      positionsSummary: {
-        qtyShares: {
-          value: 5
-        }
-      },
-      description: 'test 3',
-      tags: ['testtag', 'test']
-    }, {
-      id: 'test4',
-      isPendingReport: true,
-      description: 'test 4',
-      tags: ['testtag', 'test']
-    }, {
-      id: 'test5',
-      isPendingReport: false,
-      positionsSummary: {
-        qtyShares: {
-          value: 5
-        }
-      },
-      description: 'test 5',
-      tags: ['testtag', 'test']
-    }, {
-      id: 'test6',
-      isPendingReport: true,
-      positionsSummary: {
-        qtyShares: {
-          value: 10
-        }
-      },
-      description: 'test 6',
-      tags: ['testtag', 'test']
-    }]
+
+  const test = (t) => {
+    it(t.description, () => {
+      t.assertions();
+    });
   };
 
-  const selector = proxyquire('../../../src/modules/my-positions/selectors/my-positions.js', {
-    '../../../selectors': mockSelectors
+  describe('default', () => {
+    test({
+      description: `should call the expected method`,
+      assertions: () => {
+        __RewireAPI__.__set__('selectPositionsMarkets', () => 'selectPositionsMarkets');
+
+        const actual = myPositions();
+
+        const expected = 'selectPositionsMarkets';
+
+        assert.strictEqual(actual, expected, `Didn't call the expected method`);
+      }
+    });
   });
 
-  it(`should return only positions markets`, () => {
-    const test = selector.default();
-    const out = mockSelectors.positionsMarkets.filter(market => market.positionsSummary && market.positionsSummary.numPositions && market.positionsSummary.numPositions.value);
+  describe('selectPositionsMarkets', () => {
+    test({
+      description: `should return the expected array`,
+      assertions: () => {
+        const mockMarketsAll = () => (
+          [
+            {
+              id: '0xMARKETID1'
+            },
+            {
+              id: '0xMARKETID2'
+            },
+            {
+              id: '0xMARKETID3'
+            }
+          ]
+        );
+        const mockSelectState = {
+          selectAccountPositionsState: () => (
+            {
+              '0xMARKETID1': {},
+              '0xMARKETID3': {}
+            }
+          )
+        };
 
-    assert.deepEqual(test, out, `Didn't return the expected markets`);
+        const selector = proxyquire('../../../src/modules/my-positions/selectors/my-positions.js', {
+          '../../markets/selectors/markets-all': mockMarketsAll,
+          '../../../select-state': mockSelectState
+        });
+
+        const actual = selector.selectPositionsMarkets();
+
+        const expected = [
+          {
+            id: '0xMARKETID1'
+          },
+          {
+            id: '0xMARKETID3'
+          }
+        ];
+
+        assert.deepEqual(actual, expected, `Didn't return the expected array`);
+      }
+    });
   });
 });
