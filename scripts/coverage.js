1e16a55d835363ac2b243b464048f81f46c30ac7
Stephen Sprinkle <stephen@sprinkle.io>
2017-04-14 13:55:20 -0600

    only use istanbul babel plugin when running coverage (slow)

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -2,7 +2,7 @@ const shell = require('shelljs');
 
 shell.echo('== Running Augur Coverage ==');
 
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'coverage';
 process.env.FORCE_COLOR = true;
 
 shell.exec('istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- --require babel-register --timeout 10000 -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rimraf ./coverage', (code) => {
