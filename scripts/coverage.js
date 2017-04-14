d2d051dcf379f734db7e70db2a4a4205db92f0d4
Stephen Sprinkle <stephen@sprinkle.io>
2017-04-14 15:30:25 -0600

    updated local coverage script to allow for local execution + updated travis.yml to pass coverage details to coveralls

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -1,21 +1,14 @@
 const shell = require('shelljs');
 
-shell.echo('== Running Augur Coverage ==');
+const colors = require('./common/colors');
+
+shell.echo(colors.title('== Running Augur Coverage =='));
 
 process.env.NODE_ENV = 'test';
 process.env.FORCE_COLOR = true;
 
-shell.exec('nyc mocha && nyc report --require babel-register --reporter=lcov && cat ./coverage/lcov.info | coveralls && rimraf ./coverage && rimraf ./.nyc_output', (code) => {
+shell.exec('./node_modules/nyc/bin/nyc.js ./node_modules/mocha/bin/_mocha', (code) => {
   if (code !== 0) {
     shell.exit(code);
   }
 });
-
-// process.env.NODE_ENV = 'coverage';
-// process.env.FORCE_COLOR = true;
-//
-// shell.exec('istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- --require babel-register --timeout 10000 -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rimraf ./coverage', (code) => {
-//   if (code !== 0) {
-//     shell.exit(code);
-//   }
-// });
