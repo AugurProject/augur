6251c30618804829d334e641db2e27b0ffbc5ce2
Stephen Sprinkle <stephen@sprinkle.io>
2017-04-14 15:43:39 -0600

    updated script + gitignore

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -7,7 +7,7 @@ shell.echo(colors.title('== Running Augur Coverage =='));
 process.env.NODE_ENV = 'test';
 process.env.FORCE_COLOR = true;
 
-shell.exec('./node_modules/nyc/bin/nyc.js ./node_modules/mocha/bin/_mocha', (code) => {
+shell.exec('./node_modules/nyc/bin/nyc.js ./node_modules/mocha/bin/_mocha && node ./node_modules/rimraf/bin.js ./.nyc_output', (code) => {
   if (code !== 0) {
     shell.exit(code);
   }
