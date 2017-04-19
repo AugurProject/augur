a51256bf9e6b42f3953770a69de7ffd9e2b8a5cf
Stephen Sprinkle <stephen@sprinkle.io>
2017-04-19 16:44:50 -0600

    some initial rework to styling + null state message

--- src/modules/transactions/components/transactions.jsx
+++ src/modules/transactions/components/transactions.jsx
@@ -1,27 +1,38 @@
+/* eslint arrow-body-style: 0 */ // Resolves issue w/ conflicting linting rule 'no-confusing-arrow'
+
 import React from 'react';
 import Transaction from 'modules/transactions/components/transaction';
+import NullStateMessage from 'modules/common/components/null-state-message';
 
 const Transactions = p => (
   <article className="transactions-list">
-    {p.transactions.map((transaction, i) => transaction.transactions ?
-      <div className="transactions-group">
-        <span>Grouped Transactions</span>
-        {transaction.transactions.map((transaction, i) => (
-          <Transaction
-            key={transaction.id}
-            currentBlockNumber={p.currentBlockNumber}
-            index={p.transactions.length - i}
-            {...transaction}
-          />
-        ))}
+    {p.transactions.length ?
+      <div>
+        {p.transactions.map((transaction, i) => {
+          return transaction.transactions ?
+            <div className="transactions-group">
+              <span>Grouped Transactions</span>
+              {transaction.transactions.map((groupedTransaction, i) => (
+                <Transaction
+                  key={groupedTransaction.id}
+                  currentBlockNumber={p.currentBlockNumber}
+                  index={i + 1}
+                  {...groupedTransaction}
+                />
+              ))}
+            </div> :
+            <Transaction
+              key={transaction.id}
+              currentBlockNumber={p.currentBlockNumber}
+              index={p.transactions.length - i}
+              {...transaction}
+            />;
+        })}
       </div> :
-      <Transaction
-        key={transaction.id}
-        currentBlockNumber={p.currentBlockNumber}
-        index={p.transactions.length - i}
-        {...transaction}
+      <NullStateMessage
+        message="No Transactions Data"
       />
-    )}
+    }
   </article>
 );
 
