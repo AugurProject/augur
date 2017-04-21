448fc5bfdfc31cbeef9bbd6e244c1cf1a4e583a2
Jack Peterson <jack@tinybike.net>
2017-04-21 09:52:31 -0700

    Updated sync-blockchain

--- src/modules/app/actions/sync-blockchain.js
+++ src/modules/app/actions/sync-blockchain.js
@@ -2,7 +2,7 @@ import { rpc } from '../../../services/augurjs';
 import { updateBlockchain } from '../../app/actions/update-blockchain';
 
 export const syncBlockchain = () => dispatch => dispatch(updateBlockchain({
-  currentBlockNumber: rpc.block.number,
-  currentBlockTimestamp: parseInt(rpc.block.timestamp, 16),
+  currentBlockNumber: parseInt(rpc.getCurrentBlock().number, 16),
+  currentBlockTimestamp: parseInt(rpc.getCurrentBlock().timestamp, 16),
   currentBlockMillisSinceEpoch: Date.now()
 }));
