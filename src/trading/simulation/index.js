d0159a524bcea15bf3b36dec42ec7c6a7ae2a790
Jack Peterson <jack@tinybike.net>
2017-04-23 22:03:07 -0700

    Added get actions to trading/simulation exports

--- src/trading/simulation/index.js
+++ src/trading/simulation/index.js
@@ -1,6 +1,12 @@
 "use strict";
 
 module.exports = {
+  getAskAction: require("./get-ask-action"),
+  getBidAction: require("./get-bid-action"),
+  getBuyAction: require("./get-buy-action"),
+  getSellAction: require("./get-sell-action"),
+  getShortSellAction: require("./get-short-sell-action"),
+  getShortAskAction: require("./get-short-ask-action"),
   getTradingActions: require("./get-trading-actions"),
   getTxGasEth: require("./get-tx-gas-eth"),
   filterByPriceAndOutcomeAndUserSortByPrice: require("./filter-by-price-and-outcome-and-user-sort-by-price")
