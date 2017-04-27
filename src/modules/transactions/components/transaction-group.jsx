1fe6c060a4568aedecb9dad99ed30db5fbd826c8
Stephen Sprinkle <stephen@sprinkle.io>
2017-04-27 15:34:43 -0600

    fix to transactions re-transitioning on each re-mount

--- src/modules/transactions/components/transaction-group.jsx
+++ src/modules/transactions/components/transaction-group.jsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import classNames from 'classnames';
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 
 import Transaction from 'modules/transactions/components/transaction';
 
-const TransactionGroup = p => (
-  <article className={classNames('transaction-group', p.status)} >
-    <div className={classNames('transaction-group-summary', p.status)}>
-      <span className="transaction-action">{p.message}</span>
-      <span className="transaction-group-summary-description">{p.description}</span>
-    </div>
-    <CSSTransitionGroup
-      transitionName="transaction"
-      transitionAppear
-      transitionAppearTimeout={1200}
-      transitionEnterTimeout={1200}
-      transitionLeave={false}
-    >
-      {p.transactions.map((transaction, i) => (
-        <Transaction
-          key={transaction.id}
-          currentBlockNumber={p.currentBlockNumber}
-          isGroupedTransaction
-          {...transaction}
-        />
-      ))}
-    </CSSTransitionGroup>
-  </article>
-);
+const TransactionGroup = (p) => {
+  const animationInSpeed = parseInt(window.getComputedStyle(document.body).getPropertyValue('--animation-speed-very-slow'), 10);
+
+  return (
+    <article className={classNames('transaction-group', p.status)} >
+      <div className={classNames('transaction-group-summary', p.status)}>
+        <span className="transaction-action">{p.message}</span>
+        <span className="transaction-group-summary-description">{p.description}</span>
+      </div>
+      <CSSTransitionGroup
+        transitionName="transaction"
+        transitionEnterTimeout={animationInSpeed}
+        transitionLeave={false}
+      >
+        {p.transactions.map((transaction, i) => (
+          <Transaction
+            key={transaction.id}
+            currentBlockNumber={p.currentBlockNumber}
+            isGroupedTransaction
+            {...transaction}
+          />
+        ))}
+      </CSSTransitionGroup>
+    </article>
+  );
+}
 
 export default TransactionGroup;
--- src/modules/transactions/components/transactions.jsx
+++ src/modules/transactions/components/transactions.jsx
@@ -1,39 +1,41 @@
 import React, { PropTypes } from 'react';
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import Transaction from 'modules/transactions/components/transaction';
 import TransactionGroup from 'modules/transactions/components/transaction-group';
 import NullStateMessage from 'modules/common/components/null-state-message';
 
-const Transactions = p => (
-  <article className="transactions">
-    {p.paginatedTransactions.length ?
-      <CSSTransitionGroup
-        transitionName="transaction"
-        transitionAppear
-        transitionAppearTimeout={1200}
-        transitionEnterTimeout={1200}
-        transitionLeave={false}
-      >
-        {p.paginatedTransactions.map((transaction, i) => (
-          transaction.transactions && transaction.transactions.length > 1 ?
-            <TransactionGroup
-              key={transaction.transactions[0].hash}
-              currentBlockNumber={p.currentBlockNumber}
-              {...transaction}
-            /> :
-            <Transaction
-              key={transaction.hash}
-              currentBlockNumber={p.currentBlockNumber}
-              {...transaction}
-            />
-        ))}
-      </CSSTransitionGroup> :
-      <NullStateMessage
-        message="No Transaction Data"
-      />
-    }
-  </article>
-);
+const Transactions = (p) => {
+  const animationInSpeed = parseInt(window.getComputedStyle(document.body).getPropertyValue('--animation-speed-very-slow'), 10);
+
+  return (
+    <article className="transactions">
+      {p.paginatedTransactions.length ?
+        <CSSTransitionGroup
+          transitionName="transaction"
+          transitionEnterTimeout={animationInSpeed}
+          transitionLeave={false}
+        >
+          {p.paginatedTransactions.map((transaction, i) => (
+            transaction.transactions && transaction.transactions.length > 1 ?
+              <TransactionGroup
+                key={transaction.transactions[0].hash}
+                currentBlockNumber={p.currentBlockNumber}
+                {...transaction}
+              /> :
+              <Transaction
+                key={transaction.hash}
+                currentBlockNumber={p.currentBlockNumber}
+                {...transaction}
+              />
+          ))}
+        </CSSTransitionGroup> :
+        <NullStateMessage
+          message="No Transaction Data"
+        />
+      }
+    </article>
+  );
+};
 
 Transactions.propTypes = {
   className: PropTypes.string,
