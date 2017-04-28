0d3de0d996b1ad0fafede0b5de8db714d65a6c52
Stephen Sprinkle <stephen@sprinkle.io>
2017-04-28 15:56:08 -0600

    clear all notifications layout

--- src/modules/notifications/components/notifications-view.jsx
+++ src/modules/notifications/components/notifications-view.jsx
@@ -24,7 +24,7 @@ const NotificationsView = (p) => {
               p.clearNotifications();
             }}
           >
-            clear
+            clear all
           </button>
         }
       </div>
