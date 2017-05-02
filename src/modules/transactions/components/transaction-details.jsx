35ae8052d82cf334eb9056a0c9ac7e5e2ace32f8
Stephen Sprinkle <stephen@sprinkle.io>
2017-05-02 10:57:46 -0600

    cleaned up the styling for the transaction details

--- src/modules/transactions/components/transaction-details.jsx
+++ src/modules/transactions/components/transaction-details.jsx
@@ -1,121 +1,123 @@
 import React from 'react';
-import classNames from 'classnames';
 import ValueDenomination from 'modules/common/components/value-denomination';
-import { formatConfirmations } from 'utils/format-number';
 import { SUCCESS } from 'modules/transactions/constants/statuses';
 
-const TransactionDetails = p => (
-  <article className={classNames('transaction-details', p.isVisible ? 'visible' : 'hidden')}>
-    <div className="transaction-details-content">
-      {!!p.tradingFees && p.tradingFees.value !== null && p.tradingFees.value !== undefined &&
-        <span>
-          <ValueDenomination
-            className="tradingFees-message"
-            {...p.tradingFees}
-            prefix="trading fees:"
-          />
-          <br />
-        </span>
-      }
-      <ul>
-        {(p.data.balances || []).map(b => (
-          <li key={`${p.id}-${b.change && b.change.full}-${b.balance && b.balance.full}`}>
-            {!!b.change &&
+import getValue from 'utils/get-value';
+import { formatConfirmations } from 'utils/format-number';
+
+const TransactionDetails = (p) => {
+  console.log('### p -- ', p);
+
+  const tradingFees = getValue(p, 'tradingFees.value');
+  const balances = getValue(p, 'data.balances');
+  const totalCost = getValue(p, 'totalCost.value');
+  const totalReturn = getValue(p, 'totalReturn.value');
+  const marketCreationFee = getValue(p, 'marketCreationFee.value');
+  const bond = getValue(p, 'bond.value');
+  const gasFees = getValue(p, 'gasFees.value');
+
+  return (
+    <article className="transaction-details">
+      <div className="transaction-details-content">
+        {!!tradingFees &&
+          <div className="transaction-detail">
+            <ValueDenomination
+              className="tradingFees-message"
+              {...p.tradingFees}
+              prefix="Trading Fees:"
+            />
+          </div>
+        }
+        <ul>
+          {(balances || []).map(b => (
+            <li
+              key={`${b.change && b.change.full}-${b.balance && b.balance.full}`}
+              className="transaction-detail"
+            >
+              <span className="transaction-detail-title">Balance Change: </span>
+              {!!b.change &&
+                <ValueDenomination {...b.change} />
+              }
+              {!!b.balance &&
+                <ValueDenomination
+                  prefix=" [ balance:" postfix="]"
+                  {...b.balance}
+                />
+              }
+            </li>
+          ))}
+        </ul>
+        {!!p.freeze &&
+          <div className="transaction-detail">
+            <span className="transaction-detail-title">{p.freeze.verb} Funds: </span>
+            {p.freeze.noFeeCost &&
               <ValueDenomination
-                className="balance-message balance-change"
-                {...b.change}
+                {...p.freeze.noFeeCost}
+                postfix="+ "
               />
             }
-            {!!b.balance &&
-              <ValueDenomination
-                className="balance-message"
-                {...b.balance}
-                prefix=" [ balance:" postfix="]"
-              />
-            }
-          </li>
-        ))}
-      </ul>
-      {!!p.freeze &&
-        <span className="freeze-message">
-          {p.freeze.noFeeCost &&
-          <ValueDenomination
-            className="freeze-noFeeCost-message"
-            {...p.freeze.noFeeCost}
-            prefix={p.freeze.verb}
-            postfix="+ "
-          />
-            }
-          <ValueDenomination
-            className="freeze-tradingFees-message"
-            {...p.freeze.tradingFees}
-            hidePrefix={!!p.freeze.noFeeCost}
-            prefix={p.freeze.verb}
-            postfix="in potential trading fees"
-          />
-          <br />
-        </span>
-      }
-      {!!p.totalCost && p.totalCost.value !== null && p.totalCost.value !== undefined &&
-        <span>
-          <ValueDenomination
-            className="totalCost-message"
-            {...p.totalCost}
-            prefix="total cost:"
-          />
-          <br />
-        </span>
-      }
-      {!!p.totalReturn && p.totalReturn.value !== null && p.totalReturn.value !== undefined &&
-        <span>
-          <ValueDenomination
-            className="totalReturn-message"
-            {...p.totalReturn}
-            prefix="total return:"
-          />
-          <br />
-        </span>
-      }
-      {!!p.marketCreationFee && p.marketCreationFee.value !== null && p.marketCreationFee !== undefined &&
-        <span>
-          <ValueDenomination
-            className="marketCreationFee-message"
-            {...p.marketCreationFee}
-            prefix="market creation fee:"
-          />
-          <br />
-        </span>
-      }
-      {!!p.bond && !!p.bond.value &&
-        <span>
-          <ValueDenomination
-            className="bond-message"
-            {...p.bond.value}
-            prefix={`${p.bond.label} bond:`}
-          />
-          <br />
-        </span>
-      }
-      {!!p.gasFees && p.gasFees.value !== null && p.gasFees.value !== undefined &&
-        <span>
-          <ValueDenomination
-            className="gasFees-message"
-            {...p.gasFees}
-            prefix="gas cost:"
-          />
-          <br />
-        </span>
-      }
-      <span className="transaction-status">
-        status: <span className="detail-value">{p.status}</span>
-      </span>
-      {p.status === SUCCESS &&
-        <span className="transaction-confirmations">
-          <ValueDenomination {...formatConfirmations(p.currentBlockNumber - p.blockNumber)} />
-        </span>
-      }
-    </div>
-  </article>
-);
+            <ValueDenomination
+              {...p.freeze.tradingFees}
+              postfix="in potential trading fees"
+            />
+          </div>
+        }
+        {!!totalCost &&
+          <div className="transaction-detail">
+            <ValueDenomination
+              prefix="total cost:"
+              {...p.totalCost}
+            />
+          </div>
+        }
+        {!!totalReturn &&
+          <div className="transaction-detail">
+            <ValueDenomination
+              prefix="total return:"
+              {...p.totalReturn}
+            />
+          </div>
+        }
+        {!!marketCreationFee &&
+          <div className="transaction-detail">
+            <ValueDenomination
+              prefix="market creation fee:"
+              {...p.marketCreationFee}
+            />
+          </div>
+        }
+        {!!bond &&
+          <div className="transaction-detail">
+            <ValueDenomination
+              prefix={`${p.bond.label} bond:`}
+              {...p.bond.value}
+            />
+          </div>
+        }
+        {!!gasFees &&
+          <div className="transaction-detail">
+            <ValueDenomination
+              prefix="gas cost:"
+              {...p.gasFees}
+            />
+          </div>
+        }
+        <div className="transaction-detail">
+          <span className="transaction-detail-title">Status: </span>
+          <span>{p.status}</span>
+        </div>
+        {p.status === SUCCESS &&
+          <div className="transaction-detail">
+            <span className="transaction-detail-title">Confirmations: </span>
+            <ValueDenomination
+              hideDenomination
+              {...formatConfirmations(p.currentBlockNumber - p.blockNumber)}
+            />
+          </div>
+        }
+      </div>
+    </article>
+  );
+};
 
 export default TransactionDetails;
