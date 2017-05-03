20ee6119a7ae7d97ed3935f1860775af805c0e16
Stephen Sprinkle <stephen@sprinkle.io>
2017-05-03 09:46:23 -0600

    fix to transactions totals test

--- test/auth/actions/load-account-history-test.js
+++ test/auth/actions/load-account-history-test.js
@@ -19,13 +19,13 @@ describe(`modules/auth/actions/load-account-history.js`, () => {
       const SyncBranch = {};
       const UpdateReports = {};
       const action = proxyquire('../../../src/modules/auth/actions/load-account-history.js', {
-        '../../../modules/my-positions/actions/load-account-trades': LoadAccountTrades,
-        '../../../modules/bids-asks/actions/load-bids-asks-history': LoadBidsAsksHistory,
-        '../../../modules/create-market/actions/load-create-market-history': LoadCreateMarketHistory,
-        '../../../modules/account/actions/load-funding-history': LoadFundingHistory,
-        '../../../modules/my-reports/actions/load-reporting-history': LoadReportingHistory,
-        '../../../modules/branch/actions/sync-branch': SyncBranch,
-        '../../../modules/reports/actions/update-reports': UpdateReports
+        '../../my-positions/actions/load-account-trades': LoadAccountTrades,
+        '../../bids-asks/actions/load-bids-asks-history': LoadBidsAsksHistory,
+        '../../create-market/actions/load-create-market-history': LoadCreateMarketHistory,
+        '../../ccount/actions/load-funding-history': LoadFundingHistory,
+        '../../my-reports/actions/load-reporting-history': LoadReportingHistory,
+        '../../branch/actions/sync-branch': SyncBranch,
+        '../../reports/actions/update-reports': UpdateReports
       });
       LoadAccountTrades.loadAccountTrades = sinon.stub().returns({ type: 'LOAD_ACCOUNT_TRADES' });
       LoadBidsAsksHistory.loadBidsAsksHistory = sinon.stub().returns({ type: 'LOAD_BIDS_ASKS_HISTORY' });
