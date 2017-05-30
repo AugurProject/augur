e59a6013841322ad4bf280558bbc54c70f96e669
Stephen Sprinkle <stephen@sprinkle.io>
2017-05-30 14:22:42 -0600

    added test for account-nav-items

---
+++ test/account/constants/account-nav-items-test.js
@@ -0,0 +1,32 @@
+import { describe, it } from 'mocha';
+import { assert } from 'chai';
+
+import { ACCOUNT_NAV_ITEMS } from 'modules/account/constants/account-nav-items';
+
+import { ACCOUNT_DEPOSIT, ACCOUNT_CONVERT, ACCOUNT_TRANSFER, ACCOUNT_EXPORT } from 'modules/app/constants/views';
+
+describe('modules/account/constants/account-nav-items.js', () => {
+  const test = t => it(t.description, () => t.assertions());
+
+  test({
+    description: `should return the expected constants`,
+    assertions: () => {
+      const expected = {
+        [ACCOUNT_DEPOSIT]: {
+          label: 'Deposit'
+        },
+        [ACCOUNT_CONVERT]: {
+          label: 'Convert'
+        },
+        [ACCOUNT_TRANSFER]: {
+          label: 'Transfer'
+        },
+        [ACCOUNT_EXPORT]: {
+          label: 'Export'
+        }
+      };
+
+      assert.deepEqual(ACCOUNT_NAV_ITEMS, expected, `Didn't return the expected constants`);
+    }
+  });
+});
