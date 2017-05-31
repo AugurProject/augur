216ec6b0ee08264371791bf66557490f8cccf259
Stephen Sprinkle <stephen@sprinkle.io>
2017-05-31 13:04:57 -0600

    removed deprecated prop assertions + cleaned out test

--- test/auth/selectors/login-account-test.js
+++ test/auth/selectors/login-account-test.js
@@ -1,20 +1,6 @@
 import { describe, it } from 'mocha';
-import proxyquire from 'proxyquire';
-import loginAccountAssertions from 'assertions/login-account';
+import { assert } from 'chai';
 
-describe(`modules/account/selectors/login-account.js`, () => {
-  proxyquire.noPreserveCache();
-  const selector = proxyquire('../../../src/modules/account/selectors/login-account', {});
-  it(`should login an account`, () => {
-    loginAccountAssertions(selector.selectLoginAccount({
-      loginAccount: {
-        address: '0xb0b',
-        isUnlocked: false,
-        localNode: false,
-        name: 'bob',
-        loginID: 'loginID',
-        privateKey: new Buffer('1337', 'hex')
-      }
-    }));
-  });
+describe(`modules/auth/selectors/login-account.js`, () => {
+
 });
