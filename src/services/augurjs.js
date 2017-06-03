91bd72a44983f2bb4d2455eecda64a49edbdaa0c
Jack Peterson <jack@tinybike.net>
2017-06-03 13:57:55 -0700

    Added networkID to env configuration files

--- src/services/augurjs.js
+++ src/services/augurjs.js
@@ -11,6 +11,7 @@ export const connect = (env, callback) => {
   const isEnvWss = (env.gethWebsocketsURL && env.gethWebsocketsURL.split('//')[0] === 'wss:');
   if (env.gethHttpURL && (!isHttps || isEnvHttps)) options.httpAddresses.push(env.gethHttpURL);
   if (env.gethWebsocketsURL && (!isHttps || isEnvWss)) options.wsAddresses.push(env.gethWebsocketsURL);
+  if (env.networkID) options.networkID = env.networkID;
   if (env.hostedNodeFallback) options.httpAddresses.push('https://eth9000.augur.net');
   if (env.hostedNodeFallback) options.wsAddresses.push('wss://ws9000.augur.net');
   Object.keys(env.debug).forEach((opt) => { augur.options.debug[opt] = env.debug[opt]; });
