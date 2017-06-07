df46ca4e2b74a0db5fa4d005c89271eb14ee4237
Stephen Sprinkle <stephen@sprinkle.io>
2017-06-07 11:31:24 -0700

    cleaned up tests + added test for active page reducer

--- test/app/reducers/header-height-test.js
+++ test/app/reducers/header-height-test.js
@@ -1,19 +1,11 @@
 import { describe, it } from 'mocha';
 import { assert } from 'chai';
-import thunk from 'redux-thunk';
-import configureMockStore from 'redux-mock-store';
 
 import headerReducer from 'modules/app/reducers/header-height';
 import { UPDATE_HEADER_HEIGHT } from 'modules/app/actions/update-header-height';
 
-describe('modules/app/reducers/header-height', () => {
-  const mockStore = configureMockStore([thunk]);
-
-  const test = t => it(t.description, () => {
-    const store = mockStore(t.state || {});
-
-    t.assertions(store);
-  });
+describe('modules/app/reducers/header-height.js', () => {
+  const test = t => it(t.description, () => t.assertions());
 
   test({
     description: `should return the default state`,
@@ -27,25 +19,9 @@ describe('modules/app/reducers/header-height', () => {
   });
 
   test({
-    description: `should return the expected state`,
-    assertions: () => {
-      const actual = headerReducer(undefined, {
-        type: UPDATE_HEADER_HEIGHT,
-        data: {
-          headerHeight: 10
-        }
-      });
-
-      const expected = 10;
-
-      assert.strictEqual(actual, expected, `didn't return the expected value`);
-    }
-  });
-
-  test({
     description: `should return the updated state`,
     assertions: () => {
-      const actual = headerReducer(20, {
+      const actual = headerReducer(undefined, {
         type: UPDATE_HEADER_HEIGHT,
         data: {
           headerHeight: 10
--- test/app/reducers/is-mobile-test.js
+++ test/app/reducers/is-mobile-test.js
@@ -1,19 +1,11 @@
 import { describe, it } from 'mocha';
 import { assert } from 'chai';
-import thunk from 'redux-thunk';
-import configureMockStore from 'redux-mock-store';
 
 import isMobileReducer from 'modules/app/reducers/is-mobile';
 import { UPDATE_IS_MOBILE } from 'modules/app/actions/update-is-mobile';
 
-describe('modules/app/reducers/header-height', () => {
-  const mockStore = configureMockStore([thunk]);
-
-  const test = t => it(t.description, () => {
-    const store = mockStore(t.state || {});
-
-    t.assertions(store);
-  });
+describe('modules/app/reducers/is-mobile.js', () => {
+  const test = t => it(t.description, () => t.assertions());
 
   test({
     description: `should return the default state`,
@@ -27,25 +19,9 @@ describe('modules/app/reducers/header-height', () => {
   });
 
   test({
-    description: `should return the expected state`,
-    assertions: () => {
-      const actual = isMobileReducer(undefined, {
-        type: UPDATE_IS_MOBILE,
-        data: {
-          isMobile: true
-        }
-      });
-
-      const expected = true;
-
-      assert.strictEqual(actual, expected, `didn't return the expected value`);
-    }
-  });
-
-  test({
     description: `should return the updated state`,
     assertions: () => {
-      const actual = isMobileReducer(true, {
+      const actual = isMobileReducer(undefined, {
         type: UPDATE_IS_MOBILE,
         data: {
           isMobile: false
