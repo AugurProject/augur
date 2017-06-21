d0d8784f2b27a70fb73f8f501ec6eaf0d5dc264b
Stephen Sprinkle <git@sprinkle.io>
2017-06-21 16:32:10 -0700

    linting

--- src/modules/account/components/account-export-airbitz.jsx
+++ src/modules/account/components/account-export-airbitz.jsx
@@ -21,7 +21,7 @@ export default class AccountExportAirbitz extends Component {
 
   constructor(props) {
     super(props);
-    
+
     this.state = {
       password: '',
       passwordConfirm: '',
