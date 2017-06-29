6d5332171667050ca6f382dfe7c348ffe49ddbe9
John Danz <John.Danz87@gmail.com>
2017-06-29 11:05:54 -0500

    now correctly showing no tooltip if the label is not truncated for checkbox.jsx

--- src/modules/common/components/checkbox.jsx
+++ src/modules/common/components/checkbox.jsx
@@ -44,7 +44,7 @@ export default class Checkbox extends Component {
       } else {
         this.setState({
           isLabelTruncated: false,
-          dataTip: this.props.title || this.props.text
+          dataTip: this.props.title || ''
         });
       }
     }
