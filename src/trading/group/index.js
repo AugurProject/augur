eb83a5589d498d1475c5195f7280306c90f388c5
Jack Peterson <jack@tinybike.net>
2017-07-04 16:47:11 -0700

    Removed old trading files

--- src/trading/group/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports = {
-  executeTradingActions: require("./execute-trading-actions"),
-  generateTradeGroupID: require("./generate-trade-group-id"),
-  placeTrade: require("./place-trade")
-};
--- src/trading/index.js
+++ src/trading/index.js
@@ -2,13 +2,10 @@
 
 module.exports = {
   fees: require("./fees"),
-  group: require("./group"),
-  makeOrder: require("./make-order"),
   orderBook: require("./order-book"),
   payout: require("./payout"),
   positions: require("./positions"),
   simulation: require("./simulation"),
-  takeOrder: require("./take-order"),
   cancel: require("./cancel"),
   normalizePrice: require("./normalize-price"),
   denormalizePrice: require("./denormalize-price")
