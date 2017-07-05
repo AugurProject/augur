90a8e6c5e6366337e2ced6a08f8b33e1e5322ef0
Jack Peterson <jack@tinybike.net>
2017-07-04 17:23:03 -0700

    Order book price normalization

--- src/parsers/order-book.js
+++ src/parsers/order-book.js
@@ -2,19 +2,22 @@
 
 var BigNumber = require("bignumber.js");
 var parseTradeInfo = require("./trade-info");
-var adjustScalarOrder = require("../trading/order-book/adjust-scalar-order");
+var denormalizePrice = require("../trading/denormalize-price");
 
 module.exports = function (orderArray, scalarMinMax) {
-  var minValue, order, isScalar, numOrders, orderBook, i;
+  var order, numOrders, orderBook, i;
   if (!orderArray || orderArray.error) return orderArray;
-  isScalar = scalarMinMax && scalarMinMax.minValue !== undefined && scalarMinMax.maxValue !== undefined;
-  if (isScalar) minValue = new BigNumber(scalarMinMax.minValue, 10);
   numOrders = orderArray.length / 8;
   orderBook = { buy: {}, sell: {} };
   for (i = 0; i < numOrders; ++i) {
     order = parseTradeInfo(orderArray.slice(8*i, 8*(i + 1)));
     if (order) {
-      if (isScalar) order = adjustScalarOrder(order, minValue);
+      order.price = denormalizePrice(scalarMinMax.minValue, scalarMinMax.maxValue, order.price);
+      if (order.fullPrecisionPrice) {
+        order.fullPrecisionPrice = denormalizePrice(scalarMinMax.minValue, scalarMinMax.maxValue, order.fullPrecisionPrice);
+      } else {
+        order.fullPrecisionPrice = order.price;
+      }      
       orderBook[order.type][order.id] = order;
     }
   }
