5f5fb5684f788da78aad832c5ddeec7426260bef
jackson b <primaryxeffect@gmail.com>
2017-07-21 21:06:00 -0700

    Full Markets nav functionality (+tags), framework for future side-nav functionality, top bar now displays stats, general style fixes, light clean-up

--- src/modules/app/components/new-top-bar.js
+++ src/modules/app/components/new-top-bar.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TopBar extends Component {
-  render() {
-    return (
-      <div className='topbar'>
-        Top Bar
-      </div>
-    );
-  }
-}
+const TopBar = (props) => (
+  <div className="topbar">
+    <span className="stat-eth stat">
+      <span className="stat-label">ETH</span>
+      <span className="stat-value">
+        {props.stats[0].totalRealEth.value.formatted}
+      </span>
+    </span>
+    <span className="stat-rep stat">
+      <span className="stat-label">REP</span>
+      <span className="stat-value">
+        {props.stats[0].totalRep.value.formatted}
+      </span>
+    </span>
+  </div>
+);
 
 export default TopBar;
---
+++ src/modules/app/components/origami-svg.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+
+class Origami extends Component {
+  constructor() {
+    super();
+
+    this.currentRAF = null;
+
+    const sidebarWidth = 72;
+    const topbarHeight = 42;
+    this.sidebarWidth = sidebarWidth;
+    this.topbarHeight = topbarHeight
+
+    // TODO: de-hardcode values, replace w/ CSS properties
+    this.plgram = {
+      tl: [85 + sidebarWidth, topbarHeight],
+      tr: [222 + sidebarWidth, topbarHeight],
+      bl: [0 + sidebarWidth, topbarHeight * 2],
+      br: [110 + sidebarWidth, topbarHeight * 2]
+    };
+
+    this.xOffsetMax = 110;
+  }
+
+  pointsToString(points) {
+    return points.map((point) => (point[0]  + ',' + point[1])).join(' ');
+  }
+
+  offsetPoint(point) {
+    const currentXOffset = this.props.menuScalar * this.xOffsetMax;
+    return [point[0] + currentXOffset, point[1]];
+  }
+
+  render() {
+    const boundOffset = (point) => this.offsetPoint(point);
+    // TODO: encode this SVG data more efficiently?
+    // it's useful to have points as variables since they'll animate
+    const shapes = [
+      {
+        color: "#412468",
+        points: ([
+          [this.sidebarWidth, 0],
+          [this.sidebarWidth + (85 * 2), 0],
+          this.plgram['tl'],
+          this.plgram['br'],
+          this.plgram['bl']
+        ])
+      },
+      {
+        color: '#553580',
+        points: ([
+          [this.sidebarWidth + (85 * 2), 0],
+          this.plgram['tl'],
+          this.plgram['tr'],
+          [this.sidebarWidth + (85 * 2) + 130, 0],
+        ])
+      },
+      {
+        color: "#9592A4",
+        points: ([
+          this.plgram['tl'],
+          this.plgram['bl'],
+          this.plgram['br']
+        ]).map(boundOffset)
+      },
+      {
+        color: "#C5C3CD",
+        points: ([
+          this.plgram['tl'],
+          this.plgram['tr'],
+          this.plgram['br']
+        ]).map(boundOffset)
+      }
+    ];
+
+    if (this.props.menuScalar > 0) {
+      shapes.push({
+        color: "#5A4774",
+        points: [
+          this.plgram['tl'],
+          boundOffset(this.plgram['bl']),
+          boundOffset(this.plgram['tl']),
+        ]
+      });
+    }
+
+    const polygons = shapes.map((shape) => {
+      const pointString = this.pointsToString(shape.points);
+      return (<polygon fill={shape.color} points={pointString} />);
+    });
+
+    return (<svg id="paralellogo">{polygons}</svg>);
+  }
+}
+
+export default Origami;
