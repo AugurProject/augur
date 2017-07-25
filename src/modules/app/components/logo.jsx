3b5bda49d7f812e723663c9a4b3f77fbf4d8defc
jackson b <primaryxeffect@gmail.com>
2017-07-25 16:04:14 -0700

    Add logo

---
+++ src/modules/app/components/logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = (props) => (
+  <div className="top-logo">
+    <img src="../../assets/images/Augur-Mark-Icon-White.svg" />
+    <span className="logotext">
+      Augur
+    </span>
+  </div>
+);
+
+export default Logo;
