42b96bda3c69a511e67b820b66e09b0eb77c6e0d
jackson b <primaryxeffect@gmail.com>
2017-07-25 16:25:47 -0700

    Componentize logo mark

--- src/modules/app/components/logo.jsx
+++ src/modules/app/components/logo.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import AugurLogoIcon from '../../common/components/augur-logo-icon';
 
 const Logo = (props) => (
   <div className="top-logo">
-    <img src="../../assets/images/Augur-Mark-Icon-White.svg" />
+		<AugurLogoIcon />
     <span className="logotext">
       Augur
     </span>
