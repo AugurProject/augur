2da2479380d135f979e58b4cb3d53b55970d9a5a
Stephen Sprinkle <stephen@sprinkle.io>
2017-07-27 11:05:43 -0600

    format query params formatted in an array to string

--- src/modules/app/helpers/make-query.js
+++ src/modules/app/helpers/make-query.js
@@ -1,1 +1,8 @@
-export default function makeQuery(query)
+/**
+  * Returns a formatted query string
+  * @param {array} query - The array of query params
+  * @returns {string}
+*/
+export default function makeQuery(query) {
+  return query.reduce((p, param, i) => `${p}${i === 0 ? '' : '&'}${Object.keys(param)[0]}=${param[Object.keys(param)[0]]}`, '');
+}
