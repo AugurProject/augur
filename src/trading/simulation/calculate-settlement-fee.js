43094d581a54955eb532b8e659cbeac058ac39ba
Jack Peterson <jack@tinybike.net>
2017-08-04 17:54:56 -0700

    Calculate settlement fee

--- src/trading/simulation/calculate-settlement-fee.js
+++ src/trading/simulation/calculate-settlement-fee.js
@@ -3,15 +3,28 @@
 var constants = require("../../constants");
 var ZERO = constants.ZERO;
 
-// sharePrice can be long or short
+// sharePrice can be long (taking ask) or short (taking bid)
 function calculateSettlementFee(completeSets, marketCreatorFeeRate, range, shouldCollectReportingFees, reportingFeeRate, sharePrice) {
+  console.log("completeSets:", completeSets.toFixed());
+  console.log("marketCreatorFeeRate:", marketCreatorFeeRate.toFixed());
+  console.log("range:", range.toFixed());
+  console.log("shouldCollectReportingFees:", shouldCollectReportingFees);
+  console.log("reportingFeeRate:", reportingFeeRate.toFixed());
+  console.log("sharePrice:", sharePrice.toFixed());
+  console.log();
   var marketCreatorFee = marketCreatorFeeRate.times(completeSets).times(range);
+  console.log("marketCreatorFee:", marketCreatorFee.toFixed());
   var reportingFee = (shouldCollectReportingFees) ? reportingFeeRate.times(completeSets).times(range) : ZERO;
+  console.log("reportingFee:", reportingFee.toFixed());
+  var completeSetPayout = completeSets.times(range).minus(marketCreatorFee).minus(reportingFee);
+  console.log("completeSetPayout:", completeSetPayout.toFixed());
   var completeSetFee = marketCreatorFee.plus(reportingFee);
-  var fee = completeSetFee.times(sharePrice).dividedBy(range);
-  var makerShare = completeSets.times(sharePrice).minus(fee);
-  var takerShare = completeSetFee.minus(makerShare);
-  return takerShare;
+  console.log("completeSetFee:", completeSetFee.toFixed());
+  var makerFee = completeSetFee.times(sharePrice).dividedBy(range);
+  var takerFee = completeSetFee.minus(makerFee);
+  console.log("makerFee:", makerFee.toFixed());
+  console.log("takerFee:", takerFee.toFixed());
+  return takerFee;
 }
 
 module.exports = calculateSettlementFee;
--- test/unit/trading/simulation/calculate-settlement-fee.js
+++ test/unit/trading/simulation/calculate-settlement-fee.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var abi = require("augur-abi");
 var assert = require("chai").assert;
 var BigNumber = require("bignumber.js");
 var calculateSettlementFee = require("../../../../src/trading/simulation/calculate-settlement-fee");
@@ -17,7 +18,7 @@ describe("trading/simulation/calculate-settlement-fee", function () {
     });
   };
   test({
-    description: "calculate settlement fee",
+    description: "2 complete sets, 1% reporting fee, no market creator fee",
     params: {
       completeSets: new BigNumber("2", 10),
       marketCreatorFeeRate: new BigNumber("0", 10),
@@ -27,7 +28,49 @@ describe("trading/simulation/calculate-settlement-fee", function () {
       sharePrice: new BigNumber("0.6", 10)
     },
     assertions: function (output) {
-      console.log(output.toFixed());
+      assert.deepEqual(output, new BigNumber("0.008", 10));
+    }
+  });
+  test({
+    description: "2 complete sets, 1% reporting fee, 1.5% market creator fee",
+    params: {
+      completeSets: new BigNumber("2", 10),
+      marketCreatorFeeRate: new BigNumber("0.015", 10),
+      range: new BigNumber("1", 10),
+      shouldCollectReportingFees: 1,
+      reportingFeeRate: new BigNumber("0.01", 10),
+      sharePrice: new BigNumber("0.6", 10)
+    },
+    assertions: function (output) {
+      assert.deepEqual(output, new BigNumber("0.02", 10));
+    }
+  });
+  test({
+    description: "2 complete sets, 1% reporting fee, 1.5% market creator fee, do not collect reporting fees",
+    params: {
+      completeSets: new BigNumber("2", 10),
+      marketCreatorFeeRate: new BigNumber("0.015", 10),
+      range: new BigNumber("1", 10),
+      shouldCollectReportingFees: 0,
+      reportingFeeRate: new BigNumber("0.01", 10),
+      sharePrice: new BigNumber("0.6", 10)
+    },
+    assertions: function (output) {
+      assert.deepEqual(output, new BigNumber("0.012", 10));
+    }
+  });
+  test({
+    description: "2 complete sets, 1% reporting fee, 1.5% market creator fee, range 3",
+    params: {
+      completeSets: new BigNumber("2", 10),
+      marketCreatorFeeRate: new BigNumber("0.015", 10),
+      range: new BigNumber("3", 10),
+      shouldCollectReportingFees: 1,
+      reportingFeeRate: new BigNumber("0.01", 10),
+      sharePrice: new BigNumber("0.6", 10)
+    },
+    assertions: function (output) {
+      assert.deepEqual(output, new BigNumber("0.012", 10));
     }
   });
 });
