f040c16a2524d8f402895b2ef613e561161b535e
Stephen Sprinkle <stephen@sprinkle.io>
2017-08-10 10:05:12 -0600

    cleanup

--- src/modules/filter-sort/components/filter-market-state.jsx
+++ src/modules/filter-sort/components/filter-market-state.jsx
@@ -84,8 +84,6 @@ export default class FilterMarketState extends Component {
       return p;
     }, []);
 
-    console.log('matchedItems -- ', matchedItems);
-
     this.props.updateFilter(matchedItems);
   }
 
@@ -118,56 +116,3 @@ export default class FilterMarketState extends Component {
     );
   }
 }
-
-
-// // Filter/Sort Defaults
-// export const FILTER_SORT_TYPE = 'open';
-// export const FILTER_SORT_SORT = 'volume';
-// export const FILTER_SORT_ISDESC = true;
-//
-// // Filter Types
-// export const FILTER_TYPE_OPEN = 'open';
-// export const FILTER_TYPE_REPORTING = 'reporting';
-// export const FILTER_TYPE_CLOSED = 'closed';
-//
-// export const SELECT_TYPE_OPTIONS = [
-//   {
-//     label: 'Open',
-//     value: FILTER_TYPE_OPEN
-//   },
-//   {
-//     label: 'Reporting',
-//     value: FILTER_TYPE_REPORTING
-//   },
-//   {
-//     label: 'Closed',
-//     value: FILTER_TYPE_CLOSED
-//   }
-// ];
-//
-// export const SELECT_SORT_OPTIONS = [
-//   {
-//     label: 'Volume',
-//     value: 'volume'
-//   },
-//   {
-//     label: 'Newest',
-//     value: 'creationTime'
-//   },
-//   {
-//     label: 'Expiration',
-//     value: 'endDate'
-//   },
-//   {
-//     label: 'Taker Fee',
-//     value: 'takerFeePercent'
-//   },
-//   {
-//     label: 'Maker Fee',
-//     value: 'makerFeePercent'
-//   }
-// ];
-//
-// export const SELECT_ORDER_OPTIONS = {
-//   isDesc: true
-// };
