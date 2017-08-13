464618586c9ae93b4cf537f9283eba721ad2043f
Jack Peterson <jack@tinybike.net>
2017-08-12 23:27:00 -0700

    Filled in generateAbiMap: convert standard contract ABI JSON files to custom ABI object

--- src/contracts/generate-abi-map.js
+++ src/contracts/generate-abi-map.js
@@ -1,8 +1,44 @@
 "use strict";
 
-// { }
-function generateAbiMap() {
+var contractAbis = require("./abi");
+var keccak256 = require("../utils/keccak256");
 
+function generateAbiMap() {
+  var functions = {};
+  var events = {};
+  Object.keys(contractAbis).forEach(function (contractName) {
+    var functionsAndEventsArray = contractAbis[contractName];
+    functionsAndEventsArray.forEach(function (functionOrEvent) {
+      var shortName = functionOrEvent.name.split("(")[0];
+      if (functionOrEvent.type === "function") {
+        var inputs = [];
+        var signature = [];
+        var fixed = [];
+        functionOrEvent.inputs.forEach(function (input, index) {
+          inputs.push(input.name);
+          signature.push(input.type);
+          if (input.name.slice(0, 3) === "fxp") fixed.push(index);
+        });
+        var output = functionOrEvent.outputs[0];
+        if (!functions[contractName]) functions[contractName] = {};
+        functions[contractName][shortName] = {
+          constant: functionOrEvent.constant,
+          name: functionOrEvent.name,
+          inputs: inputs,
+          signature: signature,
+          fixed: fixed,
+          returns: (output.name === "fxp") ? "unfix" : output.type
+        };
+      } else if (functionOrEvent.type === "event") {
+        events[shortName] = {
+          contract: contractName,
+          inputs: functionOrEvent.inputs,
+          signature: "0x" + keccak256(Buffer.from(functionOrEvent.name, "utf8")).toString("hex")
+        };
+      }
+    });
+  });
+  return {functions: functions, events: events};
 }
 
 module.exports = generateAbiMap;
