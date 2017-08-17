b4b43f53a33397035992d9719dbcdb3b1840f736
Jack Peterson <jack@tinybike.net>
2017-08-16 19:51:03 -0700

    Updated construct-transaction labels; removed unused transaction types

--- src/modules/transactions/actions/construct-transaction.js
+++ src/modules/transactions/actions/construct-transaction.js
@@ -1,4 +1,4 @@
-import { abi, augur, constants } from 'services/augurjs';
+import { abi, constants } from 'services/augurjs';
 import { ZERO } from 'modules/trade/constants/numbers';
 import { BINARY, SCALAR } from 'modules/markets/constants/market-types';
 import * as TYPES from 'modules/transactions/constants/types';
@@ -101,59 +101,6 @@ export function constructRegistrationTransaction(log) {
   return transaction;
 }
 
-// export function constructPenalizationCaughtUpTransaction(log) {
-//   const transaction = { data: {} };
-//   transaction.type = 'Reporting Cycle Catch-Up';
-//   transaction.description = `Missed Reporting cycles ${log.penalizedFrom} to cycle ${log.penalizedUpTo}`;
-//   // TODO link to this cycle in My Reports
-//   if (log.repLost && log.newRepBalance) {
-//     transaction.data.balances = [{
-//       change: formatRep(log.repLost, { positiveSign: true }),
-//       balance: formatRep(log.newRepBalance)
-//     }];
-//   }
-//   const action = log.inProgress ? 'catching up' : 'caught up';
-//   transaction.message = `${action} ${parseInt(log.penalizedUpTo, 10) - parseInt(log.penalizedFrom, 10)} cycles`;
-//   return transaction;
-// }
-
-export function constructSentEtherTransaction(log, address) {
-  const transaction = { data: {} };
-  let action;
-  if (log._from === address) {
-    transaction.type = 'Send Ether';
-    transaction.description = `Send Ether to ${abi.strip_0x(log._to)}`;
-    transaction.data.balances = [{
-      change: formatEther(abi.bignum(log._value).neg(), { positiveSign: true })
-    }];
-    action = log.inProgress ? 'sending' : 'sent';
-  }
-  transaction.message = `${action} ETH`;
-  return transaction;
-}
-
-// export function constructSentCashTransaction(log, address) {
-//   const transaction = { data: {} };
-//   let action;
-//   if (log._from === address) {
-//     transaction.type = 'Send Ether Tokens';
-//     transaction.description = `Send Ether Tokens to ${abi.strip_0x(log._to)}`;
-//     transaction.data.balances = [{
-//       change: formatEtherTokens(abi.bignum(log._value).neg(), { positiveSign: true })
-//     }];
-//     action = log.inProgress ? 'sending' : 'sent';
-//   } else if (log._to === address) {
-//     transaction.type = 'Receive Ether Tokens';
-//     transaction.description = `Receive Ether Tokens from ${abi.strip_0x(log._from)}`;
-//     transaction.data.balances = [{
-//       change: formatEtherTokens(log._value, { positiveSign: true })
-//     }];
-//     action = log.inProgress ? 'receiving' : 'received';
-//   }
-//   transaction.message = `${action} ETH Tokens`;
-//   return transaction;
-// }
-
 export function constructTransferTransaction(log, address) {
   const transaction = { data: {} };
   let action;
@@ -176,7 +123,7 @@ export function constructTransferTransaction(log, address) {
   return transaction;
 }
 
-export function constructMarketCreatedTransaction(log, description, dispatch) {
+export function constructCreateMarketTransaction(log, description, dispatch) {
   const transaction = { data: {} };
   transaction.type = TYPES.CREATE_MARKET;
   transaction.description = description.split('~|>')[0];
@@ -209,14 +156,14 @@ export function constructPayoutTransaction(log, market, dispatch) {
   return transaction;
 }
 
-// export function constructTradingFeeUpdatedTransaction(log, market, dispatch) {
-//   const transaction = { data: {} };
-//   transaction.description = market.description;
-//   transaction.data.marketLink = selectMarketLink({ id: log.marketID, description: market.description }, dispatch);
-//   transaction.data.marketID = log.marketID ? log.marketID : null;
-//   transaction.message = `updated trading fee: ${formatPercent(abi.bignum(log.tradingFee).times(100)).full}`;
-//   return transaction;
-// }
+export function constructDecreaseTradingFeeTransaction(log, market, dispatch) {
+  const transaction = { data: {} };
+  transaction.description = market.description;
+  transaction.data.marketLink = selectMarketLink({ id: log.marketID, description: market.description }, dispatch);
+  transaction.data.marketID = log.marketID ? log.marketID : null;
+  transaction.message = `updated trading fee: ${formatPercent(abi.bignum(log.tradingFee).times(100)).full}`;
+  return transaction;
+}
 
 // export function constructPenalizeTransaction(log, marketID, market, outcomes, dispatch) {
 //   const transaction = { data: {} };
@@ -273,43 +220,7 @@ export function constructPayoutTransaction(log, market, dispatch) {
 //   return transaction;
 // }
 
-// export function constructSubmittedReportHashTransaction(log, marketID, market, outcomes, decryptionKey, dispatch) {
-//   const transaction = { data: {} };
-//   transaction.type = TYPES.COMMIT_REPORT;
-//   transaction.description = market.description;
-//   transaction.data.marketLink = selectMarketLink({ id: marketID, description: market.description }, dispatch);
-//   transaction.data.marketID = marketID || null;
-//   transaction.data.market = market;
-//   const isUnethical = !log.ethics || abi.bignum(log.ethics).eq(constants.ZERO);
-//   transaction.data.isUnethical = isUnethical;
-//   const action = log.inProgress ? 'committing' : 'committed';
-//   transaction.message = `${action} to report`;
-//   if (decryptionKey) {
-//     const formattedReport = formatReportedOutcome(augur.reporting.crypto.parseAndDecryptReport([
-//       log.encryptedReport,
-//       log.encryptedSalt
-//     ], { derivedKey: decryptionKey }).report, market.minValue, market.maxValue, market.type, outcomes);
-//     transaction.data.reportedOutcomeID = formattedReport;
-//     transaction.data.outcome = { name: formattedReport };
-//     transaction.message = `${transaction.message}: ${formattedReport}`;
-//   }
-//   if (!log.inProgress) {
-//     dispatch(updateEventsWithAccountReportData({
-//       [market.eventID]: {
-//         marketID,
-//         branch: market.branchID,
-//         period: market.tradingPeriod,
-//         description: market.description,
-//         expirationDate: new Date(market.endDate * 1000),
-//         isUnethical,
-//         isCommitted: true
-//       }
-//     }));
-//   }
-//   return transaction;
-// }
-
-export function constructSubmittedReportTransaction(log, marketID, market, outcomes, dispatch) {
+export function constructSubmitReportTransaction(log, marketID, market, outcomes, dispatch) {
   const transaction = { data: {} };
   transaction.type = TYPES.REVEAL_REPORT;
   transaction.description = market.description;
@@ -334,42 +245,7 @@ export function constructSubmittedReportTransaction(log, marketID, market, outco
   return transaction;
 }
 
-// export function constructSlashedRepTransaction(log, market, outcomes, address, dispatch) {
-//   const transaction = { data: {} };
-//   console.log('constructSlashedRepTransaction:', log, market, outcomes, address);
-//   transaction.description = market.description;
-//   transaction.data.marketLink = selectMarketLink({ id: market.id, description: market.description }, dispatch);
-//   transaction.data.marketID = market.id ? market.id : null;
-//   transaction.data.market = market;
-//   if (log.sender === address) {
-//     transaction.type = 'Snitch Reward';
-//     if (log.repSlashed) {
-//       const slasherRepGained = abi.bignum(log.repSlashed).dividedBy(2).toFixed();
-//       transaction.data.balances = [{
-//         change: formatRep(slasherRepGained, { positiveSign: true }),
-//         balance: formatRep(log.slasherBalance)
-//       }];
-//     }
-//     if (log.inProgress) {
-//       transaction.message = `fining ${abi.strip_0x(log.reporter)}`;
-//     } else {
-//       transaction.message = `fined ${abi.strip_0x(log.reporter)} ${formatRep(log.repSlashed).full}`;
-//     }
-//   } else {
-//     transaction.type = 'Pay Collusion Fine';
-//     if (log.repSlashed) {
-//       transaction.data.balances = [{
-//         change: formatRep(abi.bignum(log.repSlashed).neg(), { positiveSign: true }),
-//         balance: formatRep(0)
-//       }];
-//     }
-//     transaction.message = `fined by ${abi.strip_0x(log.sender)}`;
-//   }
-//   console.log('slashed rep transaction:', transaction);
-//   return transaction;
-// }
-
-// export const constructLogFillTxTransaction = (trade, marketID, marketType, minValue, description, outcomeID, outcomeName, status) => (dispatch, getState) => {
+export const constructTakeOrderTransaction = (trade, marketID, marketType, minValue, description, outcomeID, outcomeName, status) => (dispatch, getState) => {
 //   console.log('constructLogFillTransaction:', trade);
 //   if (!trade.amount || !trade.price || (!trade.makerFee && !trade.takerFee)) return null;
 //   const transactionID = `${trade.transactionHash}-${trade.tradeid}`;
@@ -427,49 +303,9 @@ export function constructSubmittedReportTransaction(log, marketID, market, outco
 //     }
 //   };
 //   return transaction;
-// };
+};
 
-// export const constructLogShortFillTxTransaction = (trade, marketID, marketType, maxValue, description, outcomeID, outcomeName, status) => (dispatch, getState) => {
-//   const transactionID = `${trade.transactionHash}-${trade.tradeid}`;
-//   const price = formatEtherTokens(trade.price);
-//   const shares = formatShares(trade.amount);
-//   const bnPrice = abi.bignum(trade.price);
-//   const tradingFees = abi.bignum(trade.takerFee);
-//   const bnShares = abi.bignum(trade.amount);
-//   const totalCost = marketType === SCALAR ?
-//     abi.bignum(maxValue).times(bnShares).minus(bnPrice.times(bnShares).plus(tradingFees)) :
-//     bnShares.minus(bnPrice.times(bnShares).plus(tradingFees));
-//   const totalCostPerShare = totalCost.dividedBy(bnShares);
-//   const action = trade.inProgress ? 'short selling' : 'short sold';
-//   const transaction = {
-//     [transactionID]: {
-//       type: TYPES.SHORT_SELL,
-//       hash: trade.transactionHash,
-//       status,
-//       description,
-//       data: {
-//         marketType,
-//         outcomeName: outcomeName || outcomeID,
-//         outcomeID,
-//         marketID,
-//         marketLink: selectMarketLink({ id: marketID, description }, dispatch)
-//       },
-//       message: `${action} ${shares.full} for ${formatEtherTokens(totalCostPerShare).full} / share`,
-//       numShares: shares,
-//       noFeePrice: price,
-//       avgPrice: formatEtherTokens(totalCost.minus(bnShares).dividedBy(bnShares).abs()),
-//       timestamp: formatDate(new Date(trade.timestamp * 1000)),
-//       tradingFees: formatEtherTokens(tradingFees),
-//       feePercent: formatPercent(tradingFees.dividedBy(totalCost).times(100)),
-//       totalCost: formatEtherTokens(totalCost),
-//       gasFees: trade.gasFees && abi.bignum(trade.gasFees).gt(ZERO) ? formatEther(trade.gasFees) : null,
-//       blockNumber: trade.blockNumber
-//     }
-//   };
-//   return transaction;
-// };
-
-// export const constructLogAddTxTransaction = (trade, marketID, marketType, description, outcomeID, outcomeName, market, status) => (dispatch, getState) => {
+export const constructMakeOrderTransaction = (trade, marketID, marketType, description, outcomeID, outcomeName, market, status) => (dispatch, getState) => {
 //   let type;
 //   let action;
 //   if (trade.type === TYPES.BUY) {
@@ -542,9 +378,9 @@ export function constructSubmittedReportTransaction(log, marketID, market, outco
 //       tradeID: trade.tradeid
 //     }
 //   };
-// };
+};
 
-export const constructLogCancelTransaction = (trade, marketID, marketType, description, outcomeID, outcomeName, status) => (dispatch, getState) => {
+export const constructCancelOrderTransaction = (trade, marketID, marketType, description, outcomeID, outcomeName, status) => (dispatch, getState) => {
   const price = formatEtherTokens(trade.price);
   const shares = formatShares(trade.amount);
   const action = trade.inProgress ? 'canceling' : 'canceled';
@@ -589,17 +425,14 @@ export const constructTradingTransaction = (label, trade, marketID, outcomeID, s
     outcomeName = (marketOutcomesData ? marketOutcomesData[outcomeID] : {}).name;
   }
   switch (label) {
-    case TYPES.LOG_FILL_TX: {
-      return dispatch(constructLogFillTxTransaction(trade, marketID, marketType, market.minValue, description, outcomeID, outcomeName, status));
+    case TYPES.TAKE_ORDER: {
+      return dispatch(constructTakeOrderTransaction(trade, marketID, marketType, market.minValue, description, outcomeID, outcomeName, status));
     }
-    case TYPES.LOG_SHORT_FILL_TX: {
-      return dispatch(constructLogShortFillTxTransaction(trade, marketID, marketType, market.maxValue, description, outcomeID, outcomeName, status));
+    case TYPES.MAKE_ORDER: {
+      return dispatch(constructMakeOrderTransaction(trade, marketID, marketType, description, outcomeID, outcomeName, market, status));
     }
-    case TYPES.LOG_ADD_TX: {
-      return dispatch(constructLogAddTxTransaction(trade, marketID, marketType, description, outcomeID, outcomeName, market, status));
-    }
-    case TYPES.LOG_CANCEL: {
-      return dispatch(constructLogCancelTransaction(trade, marketID, marketType, description, outcomeID, outcomeName, status));
+    case TYPES.CANCEL_ORDER: {
+      return dispatch(constructCancelOrderTransaction(trade, marketID, marketType, description, outcomeID, outcomeName, status));
     }
     default:
       return null;
@@ -610,24 +443,8 @@ export const constructMarketTransaction = (label, log, market) => (dispatch, get
   switch (label) {
     case TYPES.PAYOUT:
       return constructPayoutTransaction(log, market, dispatch);
-    case TYPES.TRADING_FEE_UPDATED:
-      return constructTradingFeeUpdatedTransaction(log, market, dispatch);
-    default:
-      return null;
-  }
-};
-
-export const constructReportingTransaction = (label, log, marketID, market, outcomes) => (dispatch, getState) => {
-  const { address, derivedKey } = getState().loginAccount;
-  switch (label) {
-    case TYPES.PENALIZE:
-      return constructPenalizeTransaction(log, marketID, market, outcomes, dispatch);
-    case TYPES.SUBMITTED_REPORT:
-      return constructSubmittedReportTransaction(log, marketID, market, outcomes, dispatch);
-    case TYPES.SUBMITTED_REPORT_HASH:
-      return constructSubmittedReportHashTransaction(log, marketID, market, outcomes, derivedKey, dispatch);
-    case TYPES.SLASHED_REP:
-      return constructSlashedRepTransaction(log, market, outcomes, address, dispatch);
+    case TYPES.DECREASE_TRADING_FEE:
+      return constructDecreaseTradingFeeTransaction(log, market, dispatch);
     default:
       return null;
   }
@@ -637,48 +454,27 @@ export const constructTransaction = (label, log, isRetry, callback) => (dispatch
   switch (label) {
     case TYPES.APPROVAL:
       return constructApprovalTransaction(log);
-    case TYPES.COLLECTED_FEES:
-      return constructCollectedFeesTransaction(log);
-    case TYPES.WITHDRAW_ETHER:
-      return constructConvertEthTokenToEthTransaction(log);
-    case TYPES.DEPOSIT_ETHER:
-      return constructConvertEthToEthTokenTransaction(log);
-    case TYPES.FUNDED_ACCOUNT:
-      return constructFundedAccountTransaction(log);
-    case TYPES.PENALIZATION_CAUGHT_UP:
-      return constructPenalizationCaughtUpTransaction(log);
     case TYPES.REGISTRATION:
       return constructRegistrationTransaction(log);
-    case TYPES.WITHDRAW:
-      return constructWithdrawTransaction(log);
-    case TYPES.SENT_CASH:
-      if (getState().transactionsData[log.transactionHash]) return null;
-      return constructSentCashTransaction(log, getState().loginAccount.address);
-    case TYPES.SENT_ETHER:
-      if (getState().transactionsData[log.transactionHash]) return null;
-      return constructSentEtherTransaction(log, getState().loginAccount.address);
     case TYPES.TRANSFER:
       if (getState().transactionsData[log.transactionHash]) return null;
       return constructTransferTransaction(log, getState().loginAccount.address);
-    case TYPES.MARKET_CREATED: {
-      if (log.description) return constructMarketCreatedTransaction(log, log.description, dispatch);
+    case TYPES.CREATE_MARKET: {
+      if (log.description) return constructCreateMarketTransaction(log, log.description, dispatch);
       const market = dispatch(loadDataForMarketTransaction(label, log, isRetry, callback));
       if (!market || !market.description) break;
-      return constructMarketCreatedTransaction(log, market.description, dispatch);
+      return constructCreateMarketTransaction(log, market.description, dispatch);
     }
     case TYPES.PAYOUT:
-    case TYPES.TRADING_FEE_UPDATED: {
+    case TYPES.DECREASE_TRADING_FEE: {
       const market = dispatch(loadDataForMarketTransaction(label, log, isRetry, callback));
       if (!market || !market.description) break;
       return dispatch(constructMarketTransaction(label, log, market));
     }
-    case TYPES.PENALIZE:
-    case TYPES.SLASHED_REP:
-    case TYPES.SUBMITTED_REPORT:
-    case TYPES.SUBMITTED_REPORT_HASH: {
+    case TYPES.SUBMIT_REPORT: {
       const aux = dispatch(loadDataForReportingTransaction(label, log, isRetry, callback));
       if (!aux) break;
-      return dispatch(constructReportingTransaction(label, log, aux.marketID, aux.market, aux.outcomes));
+      return constructSubmitReportTransaction(log, aux.marketID, aux.market, aux.outcomes, dispatch);
     }
     default:
       return constructDefaultTransaction(label, log);
