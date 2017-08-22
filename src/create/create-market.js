9a503c3b8a31b068aeb2a430487c4ed75c1ec329
Jack Peterson <jack@tinybike.net>
2017-08-22 15:41:37 -0700

    Updated create-market

--- src/create/create-market.js
+++ src/create/create-market.js
@@ -2,35 +2,18 @@
 
 var assign = require("lodash.assign");
 var abi = require("augur-abi");
-var api = require("../api");
 var rpcInterface = require("../rpc-interface");
+var api = require("../api");
 var calculateRequiredMarketValue = require("../create/calculate-required-market-value");
-var calculateTradingFees = require("../trading/fees/calculate-trading-fees");
-var encodeTagArray = require("../format/tag/encode-tag-array");
+var encodeTag = require("../format/tag/encode-tag");
 
-// new: { branch, endTime, numOutcomes, feePerEthInWei, denominationToken, automatedReporterAddress, topic, minValue, maxValue }
-// # TODO: we need to update this signature (and all of the places that call it) to allow the creator (UI) to pass in a number of other things which will all be logged here
-// # TODO: log short description
-// # TODO: log long description
-// # TODO: log min display price
-// # TODO: log max display price
-// # TODO: log tags (up to 0-2)
-// # TODO: log outcome labels (same number as numOutcomes)
-// # TODO: log type (scalar, binary, categorical)
-
-// createMarket(branch, endTime, numOutcomes, feePerEthInAttoeth, denominationToken, automatedReporterAddress, topic, minDisplayPrice, maxDisplayPrice, type, shortDescription: str, longDescription: str, tags: arr, outcomeLabels: arr)
-
-// old: { branch, takerFee, event, tags, makerFee, extraInfo, onSent, onSuccess, onFailed }
+// { uint256 _endTime, uint256 _numOutcomes, uint256 _payoutDenominator, int256 _feePerEthInWei, address _denominationToken, address _creator, int256 _minDisplayPrice, int256 _maxDisplayPrice, address _automatedReporterAddress, int256 _topic }
 function createMarket(p) {
-  var formattedTags = encodeTagArray(p.tags);
-  var fees = calculateTradingFees(p.makerFee, p.takerFee);
-  api().CreateMarket.createMarket(assign({}, p, {
-    tradingFee: abi.fix(fees.tradingFee, "hex"),
-    tag1: formattedTags[0],
-    tag2: formattedTags[1],
-    tag3: formattedTags[2],
-    makerFees: abi.fix(fees.makerProportionOfFee, "hex"),
-    extraInfo: p.extraInfo || "",
+  api().ReportingWindow.createNewMarket(assign({}, p, {
+    // TODO replace with 'fixed' in abi map
+    _minDisplayPrice: abi.fix(p._minDisplayPrice, "hex"),
+    _maxDisplayPrice: abi.fix(p._maxDisplayPrice, "hex"),
+    _topic: encodeTag(p._topic),
     tx: { value: calculateRequiredMarketValue(rpcInterface.getGasPrice()) }
   }));
 }
