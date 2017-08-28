da0457a7f6f48da9ed79553dc2173fc2b91a72fa
jackson b <primaryxeffect@gmail.com>
2017-08-28 05:02:35 -0700

    Mostly-finished refactor to comply with new component structure. Pending some fixes

--- src/modules/topics/components/topics-view/topics-view.jsx
+++ src/modules/topics/components/topics-view/topics-view.jsx
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 // TODO: implement null state for topics list (needs design)
-// import NullStateMessage from 'modules/common/components/null-state-message';
-import TopicList from 'modules/topics/components/topic-list';
-import Paginator from 'modules/common/components/paginator';
-import GraphBG from 'modules/common/components/graph-background';
+// import NullStateMessage from 'modules/common/components/null-state-message'
+import TopicList from 'modules/topics/components/topic-list/topic-list'
+import Paginator from 'modules/common/components/paginator'
+import GraphBG from 'modules/common/components/graph-background'
 
-import parseQuery from 'modules/app/helpers/parse-query';
-import makePath from 'modules/app/helpers/make-path';
+import parseQuery from 'modules/app/helpers/parse-query'
+import makePath from 'modules/app/helpers/make-path'
 
-import { PAGINATION_PARAM_NAME, TOPIC_PARAM_NAME } from 'modules/app/constants/param-names';
-import { MARKETS } from 'modules/app/constants/views';
+import Styles from 'modules/topics/components/topics-view/topics-view.styles'
 
-import { tween } from 'shifty';
+import { PAGINATION_PARAM_NAME, TOPIC_PARAM_NAME } from 'modules/app/constants/param-names'
+import { MARKETS } from 'modules/app/constants/views'
+
+import { tween } from 'shifty'
 
 export default class TopicsView extends Component {
   static propTypes = {
@@ -27,7 +29,7 @@ export default class TopicsView extends Component {
   }
 
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       lowerBound: null,
@@ -44,41 +46,41 @@ export default class TopicsView extends Component {
       icoFontClasses: [],
       heroTopicIndex: null,
       heroTopicOpacity: 0
-    };
+    }
 
-    this.setCurrentPage = this.setCurrentPage.bind(this);
-    this.setSegment = this.setSegment.bind(this);
+    this.setCurrentPage = this.setCurrentPage.bind(this)
+    this.setSegment = this.setSegment.bind(this)
   }
 
   componentWillMount() {
-    this.setCurrentPage(this.props.location);
+    this.setCurrentPage(this.props.location)
   }
 
   componentDidMount() {
     if (this.props.topics.length > 0) {
-      this.startCategoryCarousel();
+      this.startCategoryCarousel()
     }
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.location !== nextProps.location) this.setCurrentPage(nextProps.location);
+    if (this.props.location !== nextProps.location) this.setCurrentPage(nextProps.location)
     if (this.props.topics.length === 0 && nextProps.topics.length > 0) {
-      this.startCategoryCarousel();
+      this.startCategoryCarousel()
     }
   }
 
   setCurrentPage(location) {
-    const currentPage = parseInt(parseQuery(location.search)[PAGINATION_PARAM_NAME] || 1, 10);
+    const currentPage = parseInt(parseQuery(location.search)[PAGINATION_PARAM_NAME] || 1, 10)
 
-    this.setState({ currentPage });
+    this.setState({ currentPage })
   }
 
   setSegment(lowerBound, upperBound, boundedLength) {
-    this.setState({ lowerBound, upperBound, boundedLength });
+    this.setState({ lowerBound, upperBound, boundedLength })
   }
 
   startCategoryCarousel() {
-    this.setState({ heroTopicIndex: 0 });
+    this.setState({ heroTopicIndex: 0 })
 
     const doCarouselTween = (from, to, cb) => tween({
       from: { value: from },
@@ -86,36 +88,36 @@ export default class TopicsView extends Component {
       duration: 500,
       easing: 'easeOutQuad',
       step: (stepObj) => {
-        this.setState({ heroTopicOpacity: stepObj.value });
+        this.setState({ heroTopicOpacity: stepObj.value })
       }
-    }).then(cb);
+    }).then(cb)
 
     const waitThenChange = () => {
       window.setTimeout(() => {
         doCarouselTween(1, 0, () => {
-          const s = this.state;
-          const p = this.props;
-          const nextIndex = (s.heroTopicIndex + 1) % p.topics.length;
-          this.setState({ heroTopicIndex: nextIndex });
-          doCarouselTween(0, 1, waitThenChange);
-        });
-      }, 5000);
-    };
+          const s = this.state
+          const p = this.props
+          const nextIndex = (s.heroTopicIndex + 1) % p.topics.length
+          this.setState({ heroTopicIndex: nextIndex })
+          doCarouselTween(0, 1, waitThenChange)
+        })
+      }, 5000)
+    }
 
-    doCarouselTween(0, 1, waitThenChange);
+    doCarouselTween(0, 1, waitThenChange)
   }
 
   render() {
-    const p = this.props;
-    const s = this.state;
+    const p = this.props
+    const s = this.state
 
-    const heroTopic = p.topics[s.heroTopicIndex];
+    const heroTopic = p.topics[s.heroTopicIndex]
 
     return (
-      <section id="topics_view">
+      <section className={Styles.Topics}>
         <GraphBG />
-        <div id="topics_container">
-          <div id="topics_heading">
+        <div className={Styles.Topics__container}>
+          <div className={Styles.TopicsHeading}>
             <h3>Bet on...</h3>
             <h2 style={{ opacity: s.heroTopicOpacity }}>
               {heroTopic &&
@@ -130,7 +132,7 @@ export default class TopicsView extends Component {
               }
               {!heroTopic && '...'}
             </h2>
-            <div className="separator-bar" />
+            <div className={Styles.TopicsHeading__separator} />
           </div>
           {(p.topics && p.topics.length && s.boundedLength) &&
             <TopicList
@@ -150,6 +152,6 @@ export default class TopicsView extends Component {
           }
         </div>
       </section>
-    );
+    )
   }
 }
