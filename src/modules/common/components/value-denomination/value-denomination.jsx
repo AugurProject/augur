2f59e787b3eab8006838602666702aff3ed2b002
Serena Randolph <serena.randolph@gmail.com>
2017-08-29 11:59:32 -0700

    updating value denomination styles to css modules format

--- src/modules/common/components/value-denomination/value-denomination.jsx
+++ src/modules/common/components/value-denomination/value-denomination.jsx
@@ -5,19 +5,17 @@ import classNames from 'classnames';
 
 import EmDash from 'modules/common/components/em-dash';
 
+import Styles from 'modules/common/components/value-denomination/value-denomination.styles';
+
 const ValueDenomination = p => (
   <span
-    className={classNames('value-denomination', p.className, {
-      positive: p.formattedValue > 0,
-      negative: p.formattedValue < 0
-    })}
+    className={Styles[p.className]}
   >
     {p.prefix && !p.hidePrefix &&
-      <span className="prefix">{p.prefix}</span>
+      <span className={Styles.ValueDenomination__prefix}>{p.prefix}</span>
     }
     {p.formatted && p.fullPrecision &&
       <span
-        className="value pointer"
         data-tip={p.fullPrecision}
         data-event="click focus"
       >
@@ -25,16 +23,16 @@ const ValueDenomination = p => (
       </span>
     }
     {p.formatted && !p.fullPrecision &&
-      <span className="value">{p.formatted}</span>
+      <span>{p.formatted}</span>
     }
     {p.denomination && !p.hideDenomination &&
-      <span className="denomination">{p.denomination}</span>
+      <span className={Styles.ValueDenomination__denomination}>{p.denomination}</span>
     }
     {p.postfix && !p.hidePostfix &&
-      <span className="postfix">{p.postfix}</span>
+      <span className={Styles.ValueDenomimntion__postfix}>{p.postfix}</span>
     }
     {!p.value && p.value !== 0 && !p.formatted && p.formatted !== '0' && // null/undefined state handler
-      <span className="value" ><EmDash /></span>
+      <span><EmDash /></span>
     }
     <ReactTooltip type="light" effect="solid" place="top" />
   </span>
