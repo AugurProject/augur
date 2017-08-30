32949200c9cab50ecb404d7f0860243cb6d7770f
Stephen Sprinkle <stephen@sprinkle.io>
2017-08-30 16:21:06 -0700

    added toggleTag helper method

---
+++ src/modules/app/helpers/toggle-tag.js
@@ -0,0 +1,40 @@
+import parseQuery from 'modules/app/helpers/parse-query'
+import makeQuery from 'modules/app/helpers/make-query'
+import parseStringToArray from 'modules/app/helpers/parse-string-to-array'
+
+import { TAGS_PARAM_NAME } from 'modules/app/constants/param-names'
+
+export default function (tag, location, history) {
+  let searchParams = parseQuery(location.search)
+
+  if (searchParams[TAGS_PARAM_NAME] == null || !searchParams[TAGS_PARAM_NAME].length) {
+    searchParams[TAGS_PARAM_NAME] = tag
+    searchParams = makeQuery(searchParams)
+
+    return this.props.history.push({
+      ...this.props.location,
+      search: searchParams
+    })
+  }
+
+  const tags = parseStringToArray(decodeURIComponent(searchParams[TAGS_PARAM_NAME]), '')
+
+  if (tags.indexOf(tag) !== -1) { // Remove Tag
+    tags.splice(tags.indexOf(tag), 1)
+  } else { // add tag
+    tags.push(tag)
+  }
+
+  if (tags.length) {
+    searchParams[TAGS_PARAM_NAME] = tags.join('')
+  } else {
+    delete searchParams[TAGS_PARAM_NAME]
+  }
+
+  searchParams = makeQuery(searchParams)
+
+  this.props.history.push({
+    ...this.props.location,
+    search: searchParams
+  })
+}
