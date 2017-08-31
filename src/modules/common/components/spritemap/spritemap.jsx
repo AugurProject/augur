e5e72fd55920b97c684a64c2dc2e33e9a7e1dd86
Serena Randolph <serena.randolph@gmail.com>
2017-08-31 10:38:40 -0700

    Moving spritemap to component and using exports

---
+++ src/modules/common/components/spritemap/spritemap.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const MarketStatusOpen = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g fill="none" fillRule="evenodd"><g id="Markets/Card/Binary" stroke="#9B8BF5"><g id="Icon/Open"><circle id="Oval-5" cx="12" cy="12" r="4" /><circle id="Oval-5" cx="12" cy="12" r="11" /></g></g></g>
+  </svg>
+)
+
+export const MarketStatusReported = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g id="Symbols" fill="none" fillRule="evenodd"><g id="Markets/Card/Multi" stroke="#412468"><g id="Icon/Reporting"><g id="Group" transform="translate(1 1)"><path id="Page-1" strokeLinecap="round" strokeLinejoin="round" d="M5 11.43l4.708 3.44L17 7" /><circle id="Oval-2" cx="11" cy="11" r="11" /></g></g></g></g>
+  </svg>
+)
+
+export const MarketStatusClosed = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g fill="none" fillRule="evenodd"><g id="Markets/Card/Multi-Long" stroke="#A7A2B2"><g id="Icon/Closed"><g id="Group" transform="translate(1 1)"><circle id="Oval-2" cx="11" cy="11" r="11" /><path d="M3 4l15.5 14.5" id="Line" strokeLinecap="square" /></g></g></g></g>
+  </svg>
+)
