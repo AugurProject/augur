2843fa04fbf0e68b4d14f8ef608c05f2ffc8b029
Serena Randolph <serena.randolph@gmail.com>
2017-08-31 14:43:04 -0700

    Linking only the appropriate text within the market preview card.

---
+++ src/modules/market/components/market-link/market-link.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+import makePath from 'modules/app/helpers/make-path'
+import makeQuery from 'modules/app/helpers/make-query'
+
+import { MARKET } from 'modules/app/constants/views'
+import { MARKET_ID_PARAM_NAME, MARKET_DESCRIPTION_PARAM_NAME } from 'modules/app/constants/param-names'
+
+const MarketLink = p => (
+  <span>
+    {
+      (p.id && p.formattedDescription) ?
+        <Link
+          className={p.className}
+          to={{
+            pathname: makePath(MARKET),
+            search: makeQuery({
+              [MARKET_DESCRIPTION_PARAM_NAME]: p.formattedDescription,
+              [MARKET_ID_PARAM_NAME]: p.id
+            })
+          }}
+        >
+          {p.children}
+        </Link>
+      :
+        <span>{p.children}</span>
+    }
+  </span>
+)
+
+MarketLink.propTypes = {
+  id: PropTypes.string.isRequired,
+  formattedDescription: PropTypes.string.isRequired,
+  className: PropTypes.string
+}
+
+export default MarketLink
