b0347343c57a75fa75e4a59261d89fcdcf930f37
Serena Randolph <serena.randolph@gmail.com>
2017-09-05 09:33:16 -0700

    Range styles.

--- src/modules/market/components/market-preview/market-preview.jsx
+++ src/modules/market/components/market-preview/market-preview.jsx
@@ -3,14 +3,12 @@ import PropTypes from 'prop-types'
 
 import MarketBasics from 'modules/market/components/market-basics/market-basics'
 import MarketProperties from 'modules/market/components/market-properties/market-properties'
-import MarketPreviewOutcomes from 'modules/market/components/market-preview-outcomes/market-preview-outcomes'
 
 import Styles from 'modules/market/components/market-preview/market-preview.styles'
 
 const MarketPreview = p => (
   <article className={Styles.MarketPreview}>
     <MarketBasics {...p} />
-    <MarketPreviewOutcomes outcomes={p.outcomes} />
     <div className={Styles.MarketPreview__footer}>
       <MarketProperties {...p} />
     </div>
