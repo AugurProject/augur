ca5e2f2509cee32df2ba84494fa389b7e6229a74
Jack Peterson <jack@tinybike.net>
2017-09-05 01:54:07 -0700

    Updated getTopicsInfoChunked callback names

--- src/topics/get-topics-info-chunked.js
+++ src/topics/get-topics-info-chunked.js
@@ -8,18 +8,18 @@ var noop = require("../utils/noop");
 var GETTER_CHUNK_SIZE = require("../constants").GETTER_CHUNK_SIZE;
 
 // { branch, offset, numTopicsToLoad, totalTopics }
-function getTopicsInfoChunked(p, chunkCB, callback) {
-  if (!isFunction(chunkCB)) chunkCB = noop;
-  if (!isFunction(callback)) callback = noop;
+function getTopicsInfoChunked(p, onChunkReceived, onComplete) {
+  if (!isFunction(onChunkReceived)) onChunkReceived = noop;
+  if (!isFunction(onComplete)) onComplete = noop;
   if (!p.totalTopics) {
     api().Topics.getNumTopicsInBranch({ branch: p.branch }, function (totalTopics) {
       if (!totalTopics || totalTopics.error || !parseInt(totalTopics, 10)) {
-        return callback(totalTopics);
+        return onComplete(totalTopics);
       }
       getTopicsInfoChunked(assign({}, p, {
         numTopicsToLoad: Math.min(parseInt(totalTopics, 10), GETTER_CHUNK_SIZE),
         totalTopics: totalTopics
-      }), chunkCB, callback);
+      }), onChunkReceived, onComplete);
     });
   } else {
     getTopicsInfo({
@@ -27,12 +27,12 @@ function getTopicsInfoChunked(p, chunkCB, callback) {
       offset: p.offset,
       numTopicsToLoad: p.numTopicsToLoad || p.totalTopics
     }, function (topicsInfoChunk) {
-      if (!topicsInfoChunk || topicsInfoChunk.error) return callback(topicsInfoChunk);
-      chunkCB(topicsInfoChunk);
+      if (!topicsInfoChunk || topicsInfoChunk.error) return onComplete(topicsInfoChunk);
+      onChunkReceived(topicsInfoChunk);
       if (p.offset + p.numTopicsToLoad < p.totalTopics) {
-        getTopicsInfoChunked(assign({}, p, { offset: p.offset + p.numTopicsToLoad }), chunkCB, callback);
+        getTopicsInfoChunked(assign({}, p, { offset: p.offset + p.numTopicsToLoad }), onChunkReceived, onComplete);
       } else {
-        callback(null);
+        onComplete(null);
       }
     });
   }
