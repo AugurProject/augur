52f9b3e3755219b66ea8b4b6202c8ebc9b5a28a4
Jack Peterson <jack@tinybike.net>
2017-09-05 00:54:04 -0700

    Updated create module exports

--- src/create/index.js
+++ src/create/index.js
@@ -1,12 +1,7 @@
 "use strict";
 
 module.exports = {
-  generateOrderBook: require("./generate-order-book"),
   calculateRequiredMarketValue: require("./calculate-required-market-value"),
   calculateValidityBond: require("./calculate-validity-bond"),
-  createBranch: require("./create-branch"),
-  createEvent: require("./create-event"),
-  createMarket: require("./create-market"),
-  createSingleEventMarket: require("./create-single-event-market"),
-  createSubbranch: require("./create-subbranch")
+  createMarket: require("./create-market")
 };
