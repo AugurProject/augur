a6ff293a661d47265aaead3b749b7ae6ad71ca20
jackson b <primaryxeffect@gmail.com>
2017-09-06 11:56:34 -0700

    Correct chunk name

--- src/modules/app/components/routes.jsx
+++ src/modules/app/components/routes.jsx
@@ -51,7 +51,7 @@ const CreateMarket = asyncComponent({
 })
 const StyleSandbox = asyncComponent({
   moduleName: 'StyleSandbox',
-  loader: () => import(/* webpackChunkName: 'topics' */ 'modules/style-sandbox/components/style-sandbox/style-sandbox')
+  loader: () => import(/* webpackChunkName: 'style-sandbox' */ 'modules/style-sandbox/components/style-sandbox/style-sandbox')
     .then(module => module.default)
 })
 
