678db1da3b2d6922edb23a088c2e9c767b756c4e
Jack Peterson <jack@tinybike.net>
2017-09-05 20:00:11 -0700

    Moved assets/load-assets logic to UI

--- src/create/index.js
+++ src/create/index.js
@@ -3,6 +3,7 @@
 module.exports = {
   calculateRequiredMarketValue: require("./calculate-required-market-value"),
   calculateValidityBond: require("./calculate-validity-bond"),
+  createNewMarket: require("./create-new-market"),
   createCategoricalMarket: require("./create-categorical-market"),
   createScalarMarket: require("./create-scalar-market")
 };
