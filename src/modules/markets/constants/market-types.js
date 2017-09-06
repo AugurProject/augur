e2fa3403379df89cca77b1cdd6eeb8bc965ee1e3
Stephen Sprinkle <stephen@sprinkle.io>
2017-09-06 09:58:06 -0700

    cleanup `market-types` constants file

--- src/modules/markets/constants/market-types.js
+++ src/modules/markets/constants/market-types.js
@@ -2,10 +2,3 @@ export const BINARY = 'binary'
 export const CATEGORICAL = 'categorical'
 export const SCALAR = 'scalar'
 export const COMBINATORIAL = 'combinatorial'
-
-export const MARKET_TYPES = {
-  [BINARY]: BINARY,
-  [CATEGORICAL]: CATEGORICAL,
-  [SCALAR]: SCALAR,
-  [COMBINATORIAL]: COMBINATORIAL
-}
