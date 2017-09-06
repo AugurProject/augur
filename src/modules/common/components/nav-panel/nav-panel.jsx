ef389d0d73316ad0bf24f2a5530b03a1118bce2e
jackson b <primaryxeffect@gmail.com>
2017-09-05 17:53:39 -0700

    Various fixes re: PR comments

--- src/modules/common/components/nav-panel/nav-panel.jsx
+++ src/modules/common/components/nav-panel/nav-panel.jsx
@@ -4,46 +4,65 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import Styles from 'modules/common/components/nav-panel/nav-panel.styles'
 
+import parseQuery from 'modules/app/helpers/parse-query'
+import makeQuery from 'modules/app/helpers/make-query'
+import { NAVPANEL_ID_PARAM_NAME } from 'modules/app/constants/param-names'
 
-const NavPanel = p => (
-  <div
-    className={classNames({
-      [Styles.NavPanel]: true,
-      [Styles['NavPanel--flipped']]: p.flipped
-    })}
-  >
-    <aside className={Styles.NavPanel__controls}>
-      {p.items && p.items.map((item, ind) => {
-        const Icon = item.iconComponent
-        return (
-          <button
-            className={classNames(
-              {
-                [Styles.NavPanel__control]: true,
-                [Styles['NavPanel__control--active']]: item.active
-              })}
-            key={`${item.title}${ind - p.items.length}`}
-            onClick={item.onClick}
-          >
-            <Icon className={Styles.NavPanel__icon} />
-            {item.title}
-          </button>
-        )
+const NavPanel = (p) => {
+  const selectedNavId = decodeURIComponent(parseQuery(p.location.search)[NAVPANEL_ID_PARAM_NAME] || '')
+
+  const toggleNavId = (id) => {
+    const searchParams = makeQuery({
+      ...(parseQuery(p.location.search)),
+      [NAVPANEL_ID_PARAM_NAME]: encodeURIComponent(id)
+    })
+
+    p.history.push({
+      ...p.location,
+      search: searchParams
+    })
+  }
+
+  return (
+    <div
+      className={classNames({
+        [Styles.NavPanel]: true,
+        [Styles['NavPanel--flipped']]: p.flipped
       })}
-    </aside>
-    <div className={Styles.NavPanel__content}>
-      {p.children}
+    >
+      <aside className={Styles.NavPanel__controls}>
+        {p.items && p.items.map((item, ind) => {
+          const Icon = item.iconComponent
+          const active = selectedNavId === item.title
+
+          return (
+            <button
+              className={classNames(
+                Styles.NavPanel__control,
+                {
+                  [Styles['NavPanel__control--active']]: active
+                })}
+              key={`${item.title}${ind - p.items.length}`}
+              onClick={() => toggleNavId(item.title)}
+            >
+              <Icon className={Styles.NavPanel__icon} />
+              {item.title}
+            </button>
+          )
+        })}
+      </aside>
+      <div className={Styles.NavPanel__content}>
+        {p.children}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 NavPanel.propTypes = {
   flipped: PropTypes.bool,
-  items: PropTypes.array.isRequired
-}
-
-NavPanel.defaultProps = {
-  flipped: true
+  history: PropTypes.object.isRequired,
+  items: PropTypes.array.isRequired,
+  location: PropTypes.object.isRequired
 }
 
 export default NavPanel
