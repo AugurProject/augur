ea86576b47c29924ab5111d440d085fc8a4a7314
Serena Randolph <serena.randolph@gmail.com>
2017-09-07 11:42:45 -0700

    Linting.

--- src/modules/market/components/market-outcomes-categorical/market-outcomes-categorical.jsx
+++ src/modules/market/components/market-outcomes-categorical/market-outcomes-categorical.jsx
@@ -1,9 +1,11 @@
+/* eslint react/no-array-index-key: 0 */  // It's OK in this specific instance as order remains the same
+
 import React from 'react'
 import PropTypes from 'prop-types'
 
 import Styles from 'modules/market/components/market-outcomes-categorical/market-outcomes-categorical.styles'
 
-const CategoricalOutcome = ({outcome}) => (
+const CategoricalOutcome = ({ outcome }) => (
   <div className={Styles.MarketOutcomesCategorical__outcome}>
     <span className={Styles['MarketOutcomesCategorical__outcome-name']}>{outcome.name}</span>
     <span className={Styles['MarketOutcomesCategorical__outcome-value']}>60%</span>
@@ -11,10 +13,10 @@ const CategoricalOutcome = ({outcome}) => (
 )
 
 const renderCategoricalOutcomes = (outcomes) => {
-  let renderedOutcomes = [];
-  let outcomeSet = [];
+  const renderedOutcomes = []
+  let outcomeSet = []
 
-  outcomes.map((outcome, i) => {
+  outcomes.forEach((outcome, i) => {
     outcomeSet.push(<CategoricalOutcome key={outcome.id} outcome={outcome} />)
 
     if ((i !== 0 && i % 3 === 2) || i + 1 === outcomes.length) {
@@ -30,24 +32,23 @@ const renderCategoricalOutcomes = (outcomes) => {
   return renderedOutcomes
 }
 
-const MarketOutcomesCategorical = (p) => {
-  const firstThreeOutcomes = p.outcomes.slice(0, 3)
-  const remainingOutcomes = p.outcomes.slice(3, p.outcomes.length)
-
-  return (
-    <div className={Styles.MarketOutcomesCategorical}>
-      <div className={Styles.MarketOutcomesCategorical__outcomes}>
-        {p.outcomes && renderCategoricalOutcomes(p.outcomes)}
-      </div>
+const MarketOutcomesCategorical = p => (
+  <div className={Styles.MarketOutcomesCategorical}>
+    <div className={Styles.MarketOutcomesCategorical__outcomes}>
+      {p.outcomes && renderCategoricalOutcomes(p.outcomes)}
     </div>
-  )
-}
+  </div>
+)
 
 MarketOutcomesCategorical.propTypes = {
   outcomes: PropTypes.array.isRequired,
   max: PropTypes.string,
   min: PropTypes.string,
   type: PropTypes.string
+}
+
+CategoricalOutcome.propTypes = {
+  outcome: PropTypes.object.isRequired
 }
 
 export default MarketOutcomesCategorical
