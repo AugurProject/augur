f507f497770a8f9f556ee3735d65b3e3c64ce767
Jack Peterson <jack@tinybike.net>
2017-09-07 21:12:09 -0700

    Added market creation cost lookup functions

--- src/create/index.js
+++ src/create/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
 module.exports = {
-  calculateRequiredMarketValue: require("./calculate-required-market-value"),
-  calculateValidityBond: require("./calculate-validity-bond"),
-  createNewMarket: require("./create-new-market"),
   createCategoricalMarket: require("./create-categorical-market"),
-  createScalarMarket: require("./create-scalar-market")
+  createScalarMarket: require("./create-scalar-market"),
+  createNewMarket: require("./create-new-market"),
+  getMarketCreationCost: require("./get-market-creation-cost"),
+  getMarketCreationCostBreakdown: require("./get-market-creation-cost-breakdown")
 };
