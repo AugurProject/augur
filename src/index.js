68110d222112715ae7db875f07f6e1e1efd1e10a
Jack Peterson <jack@tinybike.net>
2017-09-07 21:27:22 -0700

    Incremented version

--- src/index.js
+++ src/index.js
@@ -21,18 +21,15 @@ function Augur() {
   this.version = "4.1.0";
   this.options = {
     debug: {
-      tools: false,       // if true, testing tools (test/tools.js) included
       broadcast: false,   // broadcast debug logging in ethrpc
       connect: false,     // connection debug logging in ethrpc and ethereumjs-connect
       trading: false,     // trading-related debug logging
       reporting: false,   // reporting-related debug logging
       filters: false,     // filters-related debug logging
-      sync: false,        // show warning on synchronous RPC request
-      accounts: false     // show info about funding from faucet
+      sync: false         // show warning on synchronous RPC request
     },
     loadZeroVolumeMarkets: true
   };
-  this.speedomatic = require("speedomatic");
   this.accounts = require("./accounts");
   this.api = require("./api")();
   this.generateContractAPI = require("./api").generateContractAPI;
@@ -49,7 +46,6 @@ function Augur() {
   this.topics = require("./topics");
   this.trading = require("./trading");
   this.utils = require("./utils");
-  if (this.options.debug.tools) this.tools = require("../test/tools");
 }
 
 module.exports = Augur;
