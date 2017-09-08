a465fdb968bd87d182661b011c59c49d7dbb9aad
Serena Randolph <serena.randolph@gmail.com>
2017-09-08 08:03:32 -0700

    Beginning to hook up show more button.

--- src/modules/market/components/market-outcomes-categorical/market-outcomes-categorical.jsx
+++ src/modules/market/components/market-outcomes-categorical/market-outcomes-categorical.jsx
@@ -3,12 +3,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import getValue from 'utils/get-value'
+
 import Styles from 'modules/market/components/market-outcomes-categorical/market-outcomes-categorical.styles'
 
 const CategoricalOutcome = ({ outcome }) => (
   <div className={Styles.MarketOutcomesCategorical__outcome}>
     <span className={Styles['MarketOutcomesCategorical__outcome-name']}>{outcome.name}</span>
-    <span className={Styles['MarketOutcomesCategorical__outcome-value']}>60%</span>
+    <span className={Styles['MarketOutcomesCategorical__outcome-value']}>{getValue(outcome, 'lastPricePercent.full')}</span>
   </div>
 )
 
@@ -32,13 +34,27 @@ const renderCategoricalOutcomes = (outcomes) => {
   return renderedOutcomes
 }
 
-const MarketOutcomesCategorical = p => (
-  <div className={Styles.MarketOutcomesCategorical}>
-    <div className={Styles.MarketOutcomesCategorical__outcomes}>
-      {p.outcomes && renderCategoricalOutcomes(p.outcomes)}
+const MarketOutcomesCategorical = p => {
+  let refOutcomeWrapper = null
+  let refOutcomeTable = null
+
+  const showMore = () => {
+    {/* refOutcomeWrapper.style = {
+      height: `${refOutcomeTable.clientHeight}px`
+    } */}
+  }
+
+  return (
+    <div className={Styles.MarketOutcomesCategorical} ref={outcomeWrapper => {refOutcomeWrapper = outcomeWrapper}}>
+      <div className={Styles['MarketOutcomesCategorical__outcomes-container']}>
+        <span className={Styles['MarketOutcomesCategorical__show-more']} onClick={showMore}>+ more</span>
+        <div className={Styles.MarketOutcomesCategorical__outcomes} ref={outcomeTable => {refOutcomeTable = outcomeTable}}>
+          {p.outcomes && renderCategoricalOutcomes(p.outcomes)}
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 MarketOutcomesCategorical.propTypes = {
   outcomes: PropTypes.array.isRequired,
