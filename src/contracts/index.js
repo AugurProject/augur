d4c75acfde8beec9582ef90ea6e4fd2595584813
Jack Peterson <jack@tinybike.net>
2017-09-09 15:22:00 -0700

    Fixed contracts module reloading

--- src/contracts/index.js
+++ src/contracts/index.js
@@ -1,8 +1,7 @@
 "use strict";
 
+var assign = require("lodash.assign");
 var contracts = require("augur-contracts");
 var generateAbiMap = require("./generate-abi-map");
 
-contracts.abi = generateAbiMap(contracts.abi);
-
-module.exports = contracts;
+module.exports = assign({}, contracts, { abi: generateAbiMap(contracts.abi) });
