e431b24a5727f5877dc3172c04a729c0641f4d41
Stephen Sprinkle <stephen@sprinkle.io>
2017-09-14 13:58:39 -0700

    layout refinements + initial scaffolding for account help section

--- src/modules/auth/components/help/help.jsx
+++ src/modules/auth/components/help/help.jsx
@@ -5,7 +5,27 @@ import Styles from 'modules/auth/components/help/help.styles'
 export default function Help(p) {
   return (
     <div className={Styles.Help} >
-      <span>HELP</span>
+      <h2 className={Styles.Help__Header}>Confused? Get Help Here.</h2>
+      <div className={Styles.Help__Definitions}>
+        <div className={Styles.Help__Definition}>
+          <span>
+            What is Augur? +
+          </span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div className={Styles.Help__Definition}>
+          <span>
+            How does a wallet work? +
+          </span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div className={Styles.Help__Definition}>
+          <span>
+            Which wallet is best for me? +
+          </span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
     </div>
   )
 }
