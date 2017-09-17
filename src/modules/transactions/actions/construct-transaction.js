b7c86d99f6e264eb82118047bb98c838cea3e8cf
Jack Peterson <jack@tinybike.net>
2017-09-17 12:53:15 -0700

    Destructured speedomatic import in construct-transaction

--- src/modules/transactions/actions/construct-transaction.js
+++ src/modules/transactions/actions/construct-transaction.js
@@ -1,5 +1,5 @@
 import BigNumber from 'bignumber.js';
-import speedomatic from 'speedomatic';
+import { strip0xPrefix } from 'speedomatic';
 import { constants } from 'services/augurjs';
 import { ZERO } from 'modules/trade/constants/numbers';
 import { BINARY, SCALAR } from 'modules/markets/constants/market-types';
@@ -59,34 +59,6 @@ export function constructApprovalTransaction(log) {
   return transaction;
 }
 
-// export function constructCollectedFeesTransaction(log) {
-//   const transaction = { data: {} };
-//   const repGain = new BigNumber(log.repGain, 10);
-//   const initialRepBalance = log.initialRepBalance !== undefined ? log.initialRepBalance : new BigNumber(log.newRepBalance, 10).minus(repGain).toFixed();
-//   const action = log.inProgress ? 'reporting' : 'reported';
-//   transaction.message = `${action} with ${formatRep(initialRepBalance).full}`;
-//   transaction.type = `Reporting Payment`;
-//   if (log.totalReportingRep) {
-//     const totalReportingRep = new BigNumber(log.totalReportingRep, 10);
-//     if (!totalReportingRep.eq(constants.ZERO)) {
-//       const percentRep = formatPercent(new BigNumber(initialRepBalance, 10).dividedBy(totalReportingRep).times(100), { decimals: 0 });
-//       transaction.message = `${transaction.message} (${percentRep.full})`;
-//     }
-//   }
-//   transaction.description = `Reporting cycle #${log.period}`;
-//   if (log.cashFeesCollected !== undefined && log.repGain !== undefined) {
-//     transaction.data.balances = [{
-//       change: formatEtherTokens(log.cashFeesCollected, { positiveSign: true }),
-//       balance: formatEtherTokens(log.newCashBalance)
-//     }, {
-//       change: formatRep(log.repGain, { positiveSign: true }),
-//       balance: formatRep(log.newRepBalance)
-//     }];
-//   }
-//   transaction.bond = { label: 'reporting', value: formatEther(log.notReportingBond) };
-//   return transaction;
-// }
-
 export function constructRegistrationTransaction(log) {
   const transaction = { data: {} };
   transaction.type = 'Register New Account';
@@ -101,14 +73,14 @@ export function constructTransferTransaction(log, address) {
   let action;
   if (log._from === address) {
     transaction.type = 'Send Reputation';
-    transaction.description = `Send Reputation to ${speedomatic.strip0xPrefix(log._to)}`;
+    transaction.description = `Send Reputation to ${strip0xPrefix(log._to)}`;
     transaction.data.balances = [{
       change: formatRep(new BigNumber(log._value, 10).neg(), { positiveSign: true })
     }];
     action = log.inProgress ? 'sending' : 'sent';
   } else if (log._to === address) {
     transaction.type = 'Receive Reputation';
-    transaction.description = `Receive Reputation from ${speedomatic.strip0xPrefix(log._from)}`;
+    transaction.description = `Receive Reputation from ${strip0xPrefix(log._from)}`;
     transaction.data.balances = [{
       change: formatRep(log._value, { positiveSign: true })
     }];
@@ -226,138 +198,11 @@ export function constructSubmitReportTransaction(log, marketID, market, outcomes
 }
 
 export const constructTakeOrderTransaction = (trade, marketID, marketType, minPrice, description, outcomeID, outcomeName, status) => (dispatch, getState) => {
-//   console.log('constructLogFillTransaction:', trade);
-//   if (!trade.amount || !trade.price || (!trade.makerFee && !trade.settlementFee)) return null;
-//   const transactionID = `${trade.transactionHash}-${trade.orderId}`;
-//   const tradeGroupID = trade.tradeGroupID;
-//   const price = formatEtherTokens(trade.price);
-//   const shares = formatShares(trade.amount);
-//   const tradingFees = trade.maker ? new BigNumber(trade.makerFee, 10) : new BigNumber(trade.settlementFee, 10);
-//   const bnShares = new BigNumber(trade.amount, 10);
-//   const bnPrice = marketType === SCALAR ? new BigNumber(augur.trading.shrinkScalarPrice(minPrice, trade.price), 10) : new BigNumber(trade.price, 10);
-//   const totalCost = bnPrice.times(bnShares).plus(tradingFees);
-//   const totalReturn = bnPrice.times(bnShares).minus(tradingFees);
-//   const totalCostPerShare = totalCost.dividedBy(bnShares);
-//   const totalReturnPerShare = totalReturn.dividedBy(bnShares);
-//   let type;
-//   let perfectType;
-//   let formattedTotalCost;
-//   let formattedTotalReturn;
-//   if (trade.maker) {
-//     type = trade.type === TYPES.SELL ? TYPES.MATCH_BID : TYPES.MATCH_ASK;
-//     perfectType = trade.type === TYPES.SELL ? 'bought' : 'sold';
-//     formattedTotalCost = trade.type === TYPES.SELL ? formatEtherTokens(totalCost) : undefined;
-//     formattedTotalReturn = trade.type === TYPES.BUY ? formatEtherTokens(totalReturn) : undefined;
-//   } else {
-//     type = trade.type === TYPES.BUY ? TYPES.BUY : TYPES.SELL;
-//     perfectType = trade.type === TYPES.BUY ? 'bought' : 'sold';
-//     formattedTotalCost = trade.type === TYPES.BUY ? formatEtherTokens(totalCost) : undefined;
-//     formattedTotalReturn = trade.type === TYPES.SELL ? formatEtherTokens(totalReturn) : undefined;
-//   }
-//   const action = trade.inProgress ? type : perfectType;
-//   const transaction = {
-//     [transactionID]: {
-//       type,
-//       hash: trade.transactionHash,
-//       tradeGroupID,
-//       status,
-//       description,
-//       data: {
-//         marketType,
-//         outcomeName: outcomeName || outcomeID,
-//         outcomeID,
-//         marketID,
-//         marketLink: selectMarketLink({ id: marketID, description }, dispatch),
-//       },
-//       message: `${action} ${shares.full} for ${formatEtherTokens(trade.type === TYPES.BUY ? totalCostPerShare : totalReturnPerShare).full} / share`,
-//       numShares: shares,
-//       noFeePrice: price,
-//       avgPrice: price,
-//       timestamp: formatDate(new Date(trade.timestamp * 1000)),
-//       tradingFees: formatEtherTokens(tradingFees),
-//       feePercent: formatPercent(tradingFees.dividedBy(totalCost).times(100)),
-//       totalCost: formattedTotalCost,
-//       totalReturn: formattedTotalReturn,
-//       gasFees: trade.gasFees && new BigNumber(trade.gasFees, 10).gt(ZERO) ? formatEther(trade.gasFees) : null,
-//       blockNumber: trade.blockNumber
-//     }
-//   };
-//   return transaction;
+
 };
 
 export const constructMakeOrderTransaction = (trade, marketID, marketType, description, outcomeID, outcomeName, market, status) => (dispatch, getState) => {
-//   let type;
-//   let action;
-//   if (trade.type === TYPES.BUY) {
-//     type = TYPES.BID;
-//     action = trade.inProgress ? 'bidding' : 'bid';
-//   } else if (trade.isShortAsk) {
-//     type = TYPES.SHORT_ASK;
-//     action = trade.inProgress ? 'short asking' : 'short ask';
-//   } else {
-//     type = TYPES.ASK;
-//     action = trade.inProgress ? 'asking' : 'ask';
-//   }
-//   const price = formatEtherTokens(trade.price);
-//   const shares = formatShares(trade.amount);
-//   const makerFee = market.makerFee;
-//   const settlementFee = market.settlementFee;
-//   const maxPrice = new BigNumber(market.maxPrice, 10);
-//   const minPrice = new BigNumber(market.minPrice, 10);
-//   const fees = augur.trading.fees.calculateFxpTradingFees(makerFee, settlementFee);
-//   const rawPrice = marketType === SCALAR ? augur.trading.expandScalarPrice(minPrice, trade.price) : trade.price;
-//   const range = marketType === SCALAR ? speedomatic.fix(maxPrice.minus(minPrice)) : constants.ONE;
-//   const adjustedFees = augur.trading.fees.calculateFxpMakerTakerFees(augur.trading.fees.calculateFxpAdjustedTradingFee(fees.tradingFee, speedomatic.fix(trade.price), range), fees.makerProportionOfFee, false, true);
-//   const fxpShares = speedomatic.fix(trade.amount);
-//   const fxpPrice = speedomatic.fix(trade.price);
-//   const tradingFees = adjustedFees.maker.times(fxpShares).dividedBy(constants.ONE)
-//     .floor()
-//     .times(fxpPrice)
-//     .dividedBy(constants.ONE)
-//     .floor();
-//   let noFeeCost;
-//   if (trade.isShortAsk) {
-//     noFeeCost = fxpShares;
-//   } else {
-//     noFeeCost = fxpPrice.times(fxpShares).dividedBy(constants.ONE).floor();
-//   }
-//   const totalCost = noFeeCost.plus(tradingFees);
-//   const totalCostPerShare = totalCost.dividedBy(fxpShares).times(constants.ONE).floor();
-//   const totalReturn = fxpPrice.times(fxpShares).dividedBy(constants.ONE)
-//     .floor()
-//     .minus(tradingFees);
-//   const totalReturnPerShare = totalReturn.dividedBy(fxpShares).times(constants.ONE).floor();
-//   return {
-//     [trade.transactionHash]: {
-//       type,
-//       status,
-//       description,
-//       data: {
-//         marketType,
-//         outcomeName: outcomeName || outcomeID,
-//         outcomeID,
-//         marketID,
-//         marketLink: selectMarketLink({ id: marketID, description }, dispatch)
-//       },
-//       message: `${action} ${shares.full} for ${formatEtherTokens(speedomatic.unfix(trade.type === TYPES.BUY ? totalCostPerShare : totalReturnPerShare)).full} / share`,
-//       numShares: shares,
-//       noFeePrice: formatEtherTokens(rawPrice),
-//       freeze: {
-//         verb: trade.inProgress ? 'freezing' : 'froze',
-//         noFeeCost: type === TYPES.ASK ? undefined : formatEtherTokens(speedomatic.unfix(noFeeCost)),
-//         tradingFees: formatEtherTokens(speedomatic.unfix(tradingFees))
-//       },
-//       avgPrice: price,
-//       timestamp: formatDate(new Date(trade.timestamp * 1000)),
-//       hash: trade.transactionHash,
-//       feePercent: formatPercent(speedomatic.unfix(tradingFees.dividedBy(totalCost).times(constants.ONE).floor()).times(100)),
-//       totalCost: type === TYPES.BID ? formatEtherTokens(speedomatic.unfix(totalCost)) : undefined,
-//       totalReturn: type === TYPES.ASK ? formatEtherTokens(speedomatic.unfix(totalReturn)) : undefined,
-//       gasFees: trade.gasFees && new BigNumber(trade.gasFees, 10).gt(ZERO) ? formatEther(trade.gasFees) : null,
-//       blockNumber: trade.blockNumber,
-//       orderId: trade.orderId
-//     }
-//   };
+
 };
 
 export const constructCancelOrderTransaction = (trade, marketID, marketType, description, outcomeID, outcomeName, status) => (dispatch, getState) => {
