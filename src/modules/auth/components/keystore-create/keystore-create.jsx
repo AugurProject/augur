fc576b576ac97a3dc5f33a4b28d3ac4e00a186e8
Stephen Sprinkle <stephen@sprinkle.io>
2017-09-22 15:29:07 -0700

    title suffix + reorg for keyword components

---
+++ src/modules/auth/components/keystore-create/keystore-create.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+export default function Keystore() {
+  return (
+    <section>
+      <Helmet>
+        <title>Keystore</title>
+      </Helmet>
+      <div>
+        <span>Keystore Create</span>
+      </div>
+    </section>
+  )
+}
--- src/modules/auth/components/keystore/keystore.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Keystore() {
-  return (
-    <span>keystuuuuurrre</span>
-  )
-}
