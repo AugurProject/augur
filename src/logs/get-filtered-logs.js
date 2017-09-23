dff25ce02c6728c17e18bf8687539a436d030714
Jack Peterson <jack@tinybike.net>
2017-09-22 18:17:24 -0700

    Removed sync path from getFilteredLogs

--- src/logs/get-filtered-logs.js
+++ src/logs/get-filtered-logs.js
@@ -3,16 +3,9 @@
 var eventsABI = require("../contracts").abi.events;
 var parametrizeFilter = require("./parametrize-filter");
 var ethrpc = require("../rpc-interface");
-var isFunction = require("../utils/is-function");
 
 function getFilteredLogs(label, filterParams, callback) {
-  var filter;
-  if (!callback && isFunction(filterParams)) {
-    callback = filterParams;
-    filterParams = null;
-  }
-  filter = parametrizeFilter(eventsABI[label], filterParams || {});
-  if (!isFunction(callback)) return ethrpc.getLogs(filter);
+  var filter = parametrizeFilter(eventsABI[label], filterParams || {});
   ethrpc.getLogs(filter, function (logs) {
     if (logs && logs.error) return callback(logs, null);
     if (!logs || !logs.length) return callback(null, []);
