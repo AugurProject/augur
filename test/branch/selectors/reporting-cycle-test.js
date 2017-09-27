12e0e5cd674ac4d72c5f4aa4aa3823f81de739ad
Tom Haile <tom.haile@cyone.com>
2017-09-27 12:10:15 -0500

    updated reporting-cycle, moment needed a number not a BigNumber. added tests to test the range of a reporting window.

--- test/branch/selectors/reporting-cycle-test.js
+++ test/branch/selectors/reporting-cycle-test.js
@@ -13,6 +13,7 @@ describe(`modules/branch/selectors/reporting-cycle.js`, () => {
     });
     t.assertions(selector.selectReportingCycle(t.state));
   });
+
   test({
     description: 'Reporting cycle is 51% complete',
     state: {
@@ -37,4 +38,58 @@ describe(`modules/branch/selectors/reporting-cycle.js`, () => {
       });
     }
   });
+
+
+  test({
+    description: 'Reporting cycle is 0% complete',
+    state: {
+      blockchain: {
+        currentBlockTimestamp: 123456789
+      },
+      branch: {
+        reportingPeriodDurationInSeconds: 2678400
+      }
+    },
+    stub: {
+      augur: {
+        reporting: {
+          getCurrentPeriodProgress: (reportingPeriodDurationInSeconds, timestamp) => 0
+        }
+      }
+    },
+    assertions: (reportingCycle) => {
+      assert.deepEqual(reportingCycle, {
+        currentReportingPeriodPercentComplete: 0,
+        reportingCycleTimeRemaining: 'in a month'
+      });
+    }
+  });
+
+
+
+  test({
+    description: 'Reporting cycle is 100% complete',
+    state: {
+      blockchain: {
+        currentBlockTimestamp: 123456789
+      },
+      branch: {
+        reportingPeriodDurationInSeconds: 2678400
+      }
+    },
+    stub: {
+      augur: {
+        reporting: {
+          getCurrentPeriodProgress: (reportingPeriodDurationInSeconds, timestamp) => 100
+        }
+      }
+    },
+    assertions: (reportingCycle) => {
+      assert.deepEqual(reportingCycle, {
+        currentReportingPeriodPercentComplete: 100,
+        reportingCycleTimeRemaining: 'a few seconds ago'
+      });
+    }
+  });
+
 });
