591f1a2354c79739bfb2c17223147a0cd8b6677b
Paul Gebheim <paul@onetaste.us>
2017-09-29 15:19:49 -0700

    Update worst case fees

--- test/unit/trading/simulation/simulate-buy.js
+++ test/unit/trading/simulation/simulate-buy.js
@@ -77,7 +77,7 @@ describe("trading/simulation/simulate-buy", function () {
     assertions: function (output) {
       assert.deepEqual(output, {
         settlementFees: ZERO,
-        worstCaseFees: new BigNumber("0.021", 10),
+        worstCaseFees: new BigNumber("0.009", 10),
         gasFees: ZERO,
         otherSharesDepleted: new BigNumber("3", 10),
         sharesDepleted: ZERO,
@@ -120,7 +120,7 @@ describe("trading/simulation/simulate-buy", function () {
     assertions: function (output) {
       assert.deepEqual(output, {
         settlementFees: new BigNumber("0.011", 10),
-        worstCaseFees: ZERO,
+        worstCaseFees: new BigNumber("0.011", 10),
         gasFees: ZERO,
         otherSharesDepleted: new BigNumber("3", 10),
         sharesDepleted: ZERO,
