08af1ceedad6a283a3b1eea5b52625fefd0d5b0d
Serena Randolph <serena.randolph@gmail.com>
2017-10-02 12:14:47 -0700

    Adding create market category outcome fields.

--- src/modules/create-market/components/create-market-form-outcome/create-market-form-outcome.jsx
+++ src/modules/create-market/components/create-market-form-outcome/create-market-form-outcome.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import Styles from 'modules/create-market/components/create-market-form-outcome/create-market-form-outcome.styles'
+import StylesForm from 'modules/create-market/components/create-market-form/create-market-form.styles'
 
 export default class CreateMarketDefine extends Component {
 
@@ -15,15 +16,15 @@ export default class CreateMarketDefine extends Component {
     const s = this.state
 
     return (
-      <ul className={Styles.CreateMarketOutcome__fields}>
+      <ul className={StylesForm.CreateMarketForm__fields}>
         <li>
-          <label htmlFor="cm__input--type-binary">
+          <label htmlFor="cm__radio--binary">
             <span>Market Type</span>
           </label>
           <ul className={Styles.CreateMarketOutcome__type}>
             <li>
               <input
-                id="cm__input--type-binary"
+                id="cm__radio--binary"
                 name="outcome"
                 type="radio"
                 value="binary"
@@ -32,23 +33,80 @@ export default class CreateMarketDefine extends Component {
             </li>
             <li>
               <input
-                id="cm__input--type-categorical"
+                id="cm__radio--categorical"
                 name="outcome"
                 type="radio"
                 value="categorical"
               />
-              <label htmlFor="cm__input--type-categorical">Multiple Choice</label>
+              <label htmlFor="cm__radio--categorical">Multiple Choice</label>
             </li>
             <li>
               <input
-                id="cm__input--type-scalar"
+                id="cm__radio--scalar"
                 name="outcome"
                 type="radio"
                 value="scalar"
               />
-              <label htmlFor="cm__input--type-scalar">Numerical Range</label>
+              <label htmlFor="cm__radio--scalar">Numerical Range</label>
             </li>
           </ul>
+        </li>
+        <li>
+          <label htmlFor="cm__input--outcome1">
+            <span>Additional Details</span>
+          </label>
+          <div className={Styles.CreateMarketOutcome__categorical}>
+            <input
+              id="cm__input--outcome1"
+              type="text"
+              placeholder="Outcome"
+            />
+            <input
+              id="cm__input--outcome1"
+              type="text"
+              placeholder="Outcome"
+            />
+            <input
+              id="cm__input--outcome1"
+              type="text"
+              placeholder="Optional Outcome"
+            />
+            <input
+              id="cm__input--outcome1"
+              type="text"
+              placeholder="Optional Outcome"
+            />
+            <input
+              id="cm__input--outcome1"
+              type="text"
+              placeholder="Optional Outcome"
+            />
+            <input
+              id="cm__input--outcome1"
+              type="text"
+              placeholder="Optional Outcome"
+            />
+            <input
+              id="cm__input--outcome1"
+              type="text"
+              placeholder="Optional Outcome"
+            />
+            <input
+              id="cm__input--outcome1"
+              type="text"
+              placeholder="Optional Outcome"
+            />
+          </div>
+        </li>
+        <li>
+          <label htmlFor="cm__input--details">
+            <span>Additional Details</span>
+          </label>
+          <input
+            id="cm__input--details"
+            type="text"
+            placeholder="Optional - Include any additional information that traders should know about this market."
+          />
         </li>
       </ul>
     )
