9934cbdf69bcce10bb87b23e9e84dd02add33791
Serena Randolph <serena.randolph@gmail.com>
2017-10-03 11:18:40 -0700

    Preserving create market fields and page when user navigates away.

--- src/modules/create-market/components/create-market-form-outcome/create-market-form-outcome.jsx
+++ src/modules/create-market/components/create-market-form-outcome/create-market-form-outcome.jsx
@@ -35,25 +35,25 @@ export default class CreateMarketOutcome extends Component {
           <ul className={Styles.CreateMarketOutcome__type}>
             <li>
               <label
-                className={classNames({ [`${Styles.active}`]: s.marketType === BINARY })}
+                className={classNames({ [`${Styles.active}`]: p.newMarket.type === BINARY })}
                 onClick={(e) => { this.setState({ marketType: BINARY }); p.updateNewMarket({ type : BINARY }) }}
               >Yes/No</label>
             </li>
             <li>
               <label
-                className={classNames({ [`${Styles.active}`]: s.marketType === CATEGORICAL })}
+                className={classNames({ [`${Styles.active}`]: p.newMarket.type === CATEGORICAL })}
                 onClick={(e) => { this.setState({ marketType: CATEGORICAL }); p.updateNewMarket({ type : CATEGORICAL }) }}
               >Multiple Choice</label>
             </li>
             <li>
               <label
-                className={classNames({ [`${Styles.active}`]: s.marketType === SCALAR })}
+                className={classNames({ [`${Styles.active}`]: p.newMarket.type === SCALAR })}
                 onClick={(e) => { this.setState({ marketType: SCALAR }); p.updateNewMarket({ type : SCALAR }) }}
               >Numerical Range</label>
             </li>
           </ul>
         </li>
-        { s.marketType === CATEGORICAL &&
+        { p.newMarket.type === CATEGORICAL &&
           <li>
             <label htmlFor="cm__input--outcome1">
               <span>Potential Outcomes</span>
@@ -62,47 +62,55 @@ export default class CreateMarketOutcome extends Component {
               <input
                 id="cm__input--outcome1"
                 type="text"
+                value={p.newMarket.outcomes[0]}
                 placeholder="Outcome"
               />
               <input
                 id="cm__input--outcome1"
                 type="text"
+                value={p.newMarket.outcomes[1]}
                 placeholder="Outcome"
               />
               <input
                 id="cm__input--outcome1"
                 type="text"
+                value={p.newMarket.outcomes[2]}
                 placeholder="Optional Outcome"
               />
               <input
                 id="cm__input--outcome1"
                 type="text"
+                value={p.newMarket.outcomes[3]}
                 placeholder="Optional Outcome"
               />
               <input
                 id="cm__input--outcome1"
                 type="text"
+                value={p.newMarket.outcomes[4]}
                 placeholder="Optional Outcome"
               />
               <input
                 id="cm__input--outcome1"
                 type="text"
+                value={p.newMarket.outcomes[5]}
                 placeholder="Optional Outcome"
               />
               <input
                 id="cm__input--outcome1"
                 type="text"
+                value={p.newMarket.outcomes[6]}
                 placeholder="Optional Outcome"
               />
               <input
                 id="cm__input--outcome1"
                 type="text"
+                value={p.newMarket.outcomes[7]}
                 placeholder="Optional Outcome"
               />
             </div>
           </li>
         }
-        { s.marketType === SCALAR &&
+        { p.newMarket.type === SCALAR &&
           <li>
             <label htmlFor="cm__input--min">
               <span>Range Values</span>
@@ -111,26 +119,28 @@ export default class CreateMarketOutcome extends Component {
               <input
                 id="cm__input--min"
                 type="number"
+                value={p.newMarket.scalarSmallNum}
                 placeholder="Min Value"
                 onChange={(e) => { p.updateNewMarket({ scalarSmallNum : e.target.value }) }}
               />
               <input
                 id="cm__input--max"
                 type="number"
+                value={p.newMarket.scalarBigNum}
                 placeholder="Max Value"
                 onChange={(e) => { p.updateNewMarket({ scalarBigNum : e.target.value }) }}
               />
             </div>
           </li>
         }
-        { s.marketType &&
+        { p.newMarket.type &&
           <li>
             <label htmlFor="cm__input--details">
               <span>Additional Details</span>
             </label>
-            <input
+            <textarea
               id="cm__input--details"
-              type="text"
+              value={p.newMarket.detailsText}
               placeholder="Optional - Include any additional information that traders should know about this market."
               onChange={(e) => { p.updateNewMarket({ detailsText : e.target.value }) }}
             />
