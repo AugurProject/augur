1721655164fe582b70c26fa8c0eef3e33f7d4ede
Jack Peterson <jack@tinybike.net>
2017-10-05 11:06:52 -0700

    Added package-lock.json

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ keythereum.constants.pbkdf2.c = ROUNDS;
 keythereum.constants.scrypt.n = ROUNDS;
 
 function Augur() {
-  this.version = "4.2.7";
+  this.version = "4.2.8";
   this.options = {
     debug: {
       broadcast: false, // broadcast debug logging in ethrpc
