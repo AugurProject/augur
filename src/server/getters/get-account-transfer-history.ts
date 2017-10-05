ae736aaeba2b67663c0e145fadfa178c383cf740
Paul Gebheim <paul@onetaste.us>
2017-10-04 20:49:07 -0700

    Fix transfer history getter

--- src/server/getters/get-account-transfer-history.ts
+++ src/server/getters/get-account-transfer-history.ts
@@ -24,19 +24,14 @@ interface TransferLog {
 type TransferHistory = TransferLog[];
 
 export function getAccountTransferHistory(db: Knex, account: Address, token: Address|null, callback: (err?: Error|null, result?: TransferHistory) => void): void {
-  let query: string;
   let dataToSelect: Address[];
-  if (token == null) {
-    query = `SELECT * FROM transfers WHERE sender = ? OR recipient = ?`;
-    dataToSelect = [account, account];
-  } else {
-    query = `SELECT * FROM transfers WHERE (sender = ? OR recipient = ?) AND token = ?`;
-    dataToSelect = [account, account, token];
-  }
-  db.all(query, dataToSelect, (err?: Error|null, transferRows?: TransferRow[]) => {
-    if (err) return callback(err);
-    if (!transferRows) return callback(null);
-    const transferHistory: TransferHistory = transferRows.map((transferRow: TransferRow): TransferLog => ({
+  let query: Knex.QueryBuilder = db("transfer")
+    .select()
+    .where({sender: account})
+    .orWhere({recipient: account});
+
+  (token === null ? query : query.andWhere({token: token}))
+    .map((transferRow: TransferRow): TransferLog => ({
       transactionHash: transferRow.transaction_hash,
       logIndex: transferRow.log_index,
       sender: transferRow.sender,
@@ -44,7 +39,10 @@ export function getAccountTransferHistory(db: Knex, account: Address, token: Add
       token: transferRow.token,
       value: transferRow.value,
       blockNumber: transferRow.block_number
-    }));
-    callback(null, transferHistory);
-  });
+    }))
+    .then((transferHistory: TransferLog[]) => {
+      if(!transferHistory) return callback(null);
+      callback(null, transferHistory);
+    })
+    .error(callback);
 }
