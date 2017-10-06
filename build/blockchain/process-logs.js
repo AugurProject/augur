d81cded8ef07bd6e0efdd6ef92a5118d9da00a72
Paul Gebheim <paul@onetaste.us>
2017-10-06 09:46:46 -0700

    Remove build from git

--- build/blockchain/process-logs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const async_1 = require("async");
-function processLogs(db, logs, processLog, callback) {
-    async_1.each(logs, (log, nextLog) => processLog(db, log, nextLog), callback);
-}
-exports.processLogs = processLogs;
-//# sourceMappingURL=process-logs.js.map
--- build/server/getters/get-user-trading-history.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Look up a user's trading history (should not include open orders, which will have their own endpoint). Should take market, outcome, and orderType as optional parameters. In addition to the "normal" trade data (i.e., the stuff stored in the OrderFilled log on-chain), the response should include the user's position after the trade, in both "raw" and "adjusted-for-user-intention" formats -- the latter meaning that short positions are shown as negative, rather than as positive positions in the other outcomes), realized and unrealized profit/loss, and max possible gain/loss (at market resolution).
-function getUserTradingHistory(db, account, marketID, outcome, orderType, callback) {
-}
-exports.getUserTradingHistory = getUserTradingHistory;
-//# sourceMappingURL=get-user-trading-history.js.map
