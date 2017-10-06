5a40f4a336063c0ce89dfa3c8be7457e60de3f62
Paul Gebheim <paul@onetaste.us>
2017-10-05 22:02:03 -0700

    Add test configuration for knex

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,22 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './augur.db',
+      filename: './augur.dev.db',
     },
     seeds: {
       directory: './build/seeds/dev'
+    },
+    migrations: {
+      directory: './build/migrations'
+    }
+  },
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './augur.test.db',
+    },
+    seeds: {
+      directory: './build/seeds/test'
     },
     migrations: {
       directory: './build/migrations'
