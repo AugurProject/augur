2a17e57f55ea50886a6331bc4ba1f0aac97a9ff9
Jack Peterson <jack@tinybike.net>
2017-10-07 10:54:44 -0700

    Removed unused constants

--- src/constants.js
+++ src/constants.js
@@ -5,12 +5,9 @@ var BigNumber = require("bignumber.js");
 var ten = new BigNumber(10, 10);
 var decimals = new BigNumber(4, 10);
 var multiple = ten.toPower(decimals);
-var ONE = ten.toPower(18);
 
 module.exports = {
   ZERO: new BigNumber(0),
-  ONE: ONE,
-  ETHER: ONE,
 
   PRECISION: {
     decimals: decimals.toNumber(),
@@ -23,13 +20,6 @@ module.exports = {
   DEFAULT_NETWORK_ID: "3",
   DEFAULT_GASPRICE: 20000000000,
 
-  // default branch info: "root branch", 1010101
-  DEFAULT_BRANCH_ID: "0xf69b5",
-  DEFAULT_BRANCH_PERIOD_LENGTH: 172800, // seconds
-
-  BID: 1,
-  ASK: 2,
-
   GETTER_CHUNK_SIZE: 100,
   BLOCKS_PER_CHUNK: 100,
 
@@ -37,14 +27,6 @@ module.exports = {
 
   GET_LOGS_DEFAULT_FROM_BLOCK: "0x1",
   GET_LOGS_DEFAULT_TO_BLOCK: "latest",
-
-  COST_PER_REPORTER: new BigNumber("0.0000000000035", 10),
-
-  // int256 type codes for log filters
-  LOG_TYPE_CODES: {
-    buy: "0x0000000000000000000000000000000000000000000000000000000000000001",
-    sell: "0x0000000000000000000000000000000000000000000000000000000000000002"
-  },
 
   // maximum number of transactions to auto-submit in parallel
   PARALLEL_LIMIT: 5,
@@ -54,18 +36,7 @@ module.exports = {
   CATEGORICAL_SCALAR_INDETERMINATE: new BigNumber("0x6f05b59d3b20000", 16),
   INDETERMINATE_PLUS_ONE: new BigNumber("0x6f05b59d3b20001", 16),
 
-  // gas needed for trade transactions (values from pyethereum tester)
-  MAKE_ORDER_GAS: {sell: 725202, buy: 725202},
-  TRADE_GAS: [
-    {sell: 756374, buy: 787421}, // first trade_id only
-    {sell: 615817, buy: 661894} // each additional trade_id
-  ],
-  CANCEL_GAS: {sell: 288060, buy: 230059},
-
   DUST_THRESHOLD: new BigNumber(1, 10), // placeholder value
-
-  // expected block interval
-  SECONDS_PER_BLOCK: 12,
 
   // keythereum crypto parameters
   KDF: "pbkdf2",
