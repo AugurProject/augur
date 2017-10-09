2830d17b42b94ac07deb97517e7dbe69f1fec0e6
Paul Gebheim <paul@onetaste.us>
2017-10-09 13:25:42 -0700

    Process logs in series

--- src/blockchain/download-augur-logs.ts
+++ src/blockchain/download-augur-logs.ts
@@ -1,5 +1,5 @@
 import Augur = require("augur.js");
-import { each } from "async";
+import { eachSeries } from "async";
 import * as Knex from "knex";
 import { AugurLogs, ErrorCallback } from "../types";
 import { processLogs } from "./process-logs";
@@ -8,8 +8,8 @@ import { logProcessors } from "./log-processors";
 export function downloadAugurLogs(db: Knex, augur: Augur, fromBlock: number, toBlock: number, callback: ErrorCallback): void {
   augur.events.getAllAugurLogs({ fromBlock, toBlock }, (err: string|Object, allAugurLogs?: AugurLogs) => {
     if (err) return callback(err instanceof Error ? err : new Error(JSON.stringify(err)));
-    each(Object.keys(allAugurLogs!), (contractName: string, nextContractName: ErrorCallback) => (
-      each(Object.keys(allAugurLogs![contractName]!), (eventName: string, nextEventName: ErrorCallback) => (
+    eachSeries(Object.keys(allAugurLogs!), (contractName: string, nextContractName: ErrorCallback) => (
+      eachSeries(Object.keys(allAugurLogs![contractName]!), (eventName: string, nextEventName: ErrorCallback) => (
         processLogs(db, allAugurLogs![contractName]![eventName]!, logProcessors[contractName][eventName], nextEventName)
       ), nextContractName)
     ), callback);
