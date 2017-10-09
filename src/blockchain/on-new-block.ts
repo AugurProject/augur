8283af0757e4357d1e437df0edb0c278bfeb6f0c
Paul Gebheim <paul@onetaste.us>
2017-10-09 11:05:45 -0700

    More transactions

--- src/blockchain/on-new-block.ts
+++ src/blockchain/on-new-block.ts
@@ -2,12 +2,12 @@ import Augur = require("augur.js");
 import * as Knex from "knex";
 import { logError } from "../utils/log-error";
 
-export function onNewBlock(db: Knex, augur: Augur, blockNumber: string) {
+export function onNewBlock(db: Knex, trx: Knex.Transaction, augur: Augur, blockNumber: string) {
   augur.rpc.eth.getBlockByNumber([blockNumber, false], (block: any): void => {
     if (!block || block.error || !block.timestamp) return logError(new Error(JSON.stringify(block)));
     console.log("new block:", parseInt(blockNumber, 16), parseInt(block.timestamp, 16));
     const dataToInsertOrReplace: (string|number)[] = [parseInt(blockNumber, 16), parseInt(block.timestamp, 16)];
     // PG NB: Does this work on pgsql?
-    db.raw(`INSERT OR REPLACE INTO blocks (block_number, block_timestamp) VALUES (?, ?)`, dataToInsertOrReplace).asCallback(logError);
+    trx.raw(`INSERT OR REPLACE INTO blocks (block_number, block_timestamp) VALUES (?, ?)`, dataToInsertOrReplace).asCallback(logError);
   });
 }
