513af464e336726fb7d0f292a0cea0e47b37ae7f
Paul Gebheim <paul@onetaste.us>
2017-10-08 20:41:44 -0700

    Replace .error with .asCallback

--- src/blockchain/on-new-block.ts
+++ src/blockchain/on-new-block.ts
@@ -8,7 +8,6 @@ export function onNewBlock(db: Knex, augur: Augur, blockNumber: string) {
     console.log("new block:", parseInt(blockNumber, 16), parseInt(block.timestamp, 16));
     const dataToInsertOrReplace: (string|number)[] = [parseInt(blockNumber, 16), parseInt(block.timestamp, 16)];
     // PG NB: Does this work on pgsql?
-    db.raw(`INSERT OR REPLACE INTO blocks (block_number, block_timestamp) VALUES (?, ?)`, dataToInsertOrReplace)
-      .error(logError);
+    db.raw(`INSERT OR REPLACE INTO blocks (block_number, block_timestamp) VALUES (?, ?)`, dataToInsertOrReplace).asCallback(logError);
   });
 }
