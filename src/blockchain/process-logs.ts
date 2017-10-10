81a1abb4fb531d24df99d15547b052eaf6260fbb
Paul Gebheim <paul@onetaste.us>
2017-10-10 13:45:42 -0700

    Require Array<T>

--- src/blockchain/process-logs.ts
+++ src/blockchain/process-logs.ts
@@ -2,6 +2,6 @@ import { each } from "async";
 import * as Knex from "knex";
 import { FormattedLog, LogProcessor, ErrorCallback } from "../types";
 
-export function processLogs(db: Knex, trx: Knex.Transaction, logs: FormattedLog[], processLog: LogProcessor, callback: ErrorCallback): void {
+export function processLogs(db: Knex, trx: Knex.Transaction, logs: Array<FormattedLog>, processLog: LogProcessor, callback: ErrorCallback): void {
   each(logs, (log: FormattedLog, nextLog: ErrorCallback) => processLog(db, trx, log, nextLog), callback);
 }
