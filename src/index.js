72c2f168c33bc4e04cc1fc333035d8cf37e910b7
Jack Peterson <jack@tinybike.net>
2017-10-10 00:53:00 -0700

    ethrpc@4.4.2

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ keythereum.constants.pbkdf2.c = ROUNDS;
 keythereum.constants.scrypt.n = ROUNDS;
 
 function Augur() {
-  this.version = "4.4.3";
+  this.version = "4.4.4";
   this.options = {
     debug: {
       broadcast: false, // broadcast debug logging in ethrpc
