33d211b7ba814731adc31e2b1a8d9c070c75d41d
Paul Gebheim <paul@onetaste.us>
2017-10-10 15:10:09 -0700

    Fix indentation

--- src/seeds/test/tokens.ts
+++ src/seeds/test/tokens.ts
@@ -1,16 +1,16 @@
 import * as Knex from "knex";
 
 exports.seed = async (knex: Knex): Promise<any> => {
-    // Deletes ALL existing entries
-    return knex("tokens").del()
-        .then(() => {
-          // Inserts seed entries
-          return knex.raw(`INSERT INTO tokens
+  // Deletes ALL existing entries
+  return knex("tokens").del()
+    .then(() => {
+      // Inserts seed entries
+      return knex.raw(`INSERT INTO tokens
             (contract_address, symbol, market, outcome)
             VALUES
             ('ETH', 'ETH', NULL, NULL),
             ('0x7a305d9b681fb164dc5ad628b5992177dc66aec8', 'REP', NULL, NULL),
             ('0x2000000000000000000000000000000000000000', 'shares', '0x0000000000000000000000000000000000000001', 1),
             ('0x1000000000000000000000000000000000000000', 'shares', '0x0000000000000000000000000000000000000001', 0)`);
-        });
+    });
 };
--- src/seeds/test/topics.ts
+++ src/seeds/test/topics.ts
@@ -1,17 +1,17 @@
 import * as Knex from "knex";
 
 exports.seed = async (knex: Knex): Promise<any> => {
-    // Deletes ALL existing entries
-    return knex("topics").del()
-        .then(() => {
-          // Inserts seed entries
-          return knex.raw(`INSERT INTO topics
-						(topic, popularity, universe)
+  // Deletes ALL existing entries
+  return knex("topics").del()
+    .then(() => {
+      // Inserts seed entries
+      return knex.raw(`INSERT INTO topics
+            (topic, popularity, universe)
             VALUES
             ('test topic', 0, '0x000000000000000000000000000000000000000b'),
             ('politics', 5000, '0x000000000000000000000000000000000000000b'),
             ('ethereum', 1000, '0x000000000000000000000000000000000000000b'),
             ('augur', 500, '0x000000000000000000000000000000000000000b'),
             ('finance', 12345, '0x000000000000000000000000000000000000000b')`);
-        });
+    });
 };
