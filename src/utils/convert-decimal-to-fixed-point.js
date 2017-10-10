a03718df5ff1936ed13e89b775c1a004c96c440e
Jack Peterson <jack@tinybike.net>
2017-10-10 01:13:15 -0700

    Added convertDecimalToFixedPoint step to placeTrade; augur.js@4.4.5

---
+++ src/utils/convert-decimal-to-fixed-point.js
@@ -0,0 +1,10 @@
+import BigNumber from 'bignumber.js';
+
+/**
+ * @param {string|number} decimalValue
+ * @param {number} conversionFactor
+ * @return {string}
+ */
+export default function (decimalValue, conversionFactor) {
+  return new BigNumber(decimalValue, 10).times(new BigNumber(conversionFactor, 10)).toFixed();
+}
