c3c84f351b8521bc2bcf5340642dfa736a5d30de
Paul Gebheim <paul@onetaste.us>
2017-10-09 23:36:28 -0700

    Sync all missing rows in initial sync
    Wait for the initial block to load from the blockchain and use this
    block number to calculate the range to bulk fetch.

--- src/blockchain/sync-augur-node-with-blockchain.ts
+++ src/blockchain/sync-augur-node-with-blockchain.ts
@@ -9,19 +9,22 @@ interface HighestBlockNumberRow {
 }
 
 export function syncAugurNodeWithBlockchain(db: Knex,  augur: Augur, ethereumNodeEndpoints: EthereumNodeEndpoints, uploadBlockNumbers: UploadBlockNumbers, callback: ErrorCallback): void {
-  augur.connect({ ethereumNode: ethereumNodeEndpoints }, () => startAugurListeners(db, augur, () => {
-    db.raw(`SELECT highest_block_number FROM blockchain_sync_history ORDER BY highest_block_number DESC LIMIT 1`)
-      .asCallback( (err: Error|null, rows?: HighestBlockNumberRow[]) => {
-        if(err) return callback(err);
-        const row: HighestBlockNumberRow = rows![0];
-        const fromBlock: number = (!row || !row.highest_block_number) ? uploadBlockNumbers[augur.rpc.getNetworkID()] : row.highest_block_number + 1;
-        const highestBlockNumber: number = parseInt(augur.rpc.getCurrentBlock().number, 16);
-        if (fromBlock >= highestBlockNumber) return callback(null); // augur-node is already up-to-date
+  augur.connect({ ethereumNode: ethereumNodeEndpoints }, () => {
+    console.log("Waiting for first block...");
+    startAugurListeners(db, augur, (blockNumber: string) => {
+      db.raw(`SELECT highest_block_number FROM blockchain_sync_history ORDER BY highest_block_number DESC LIMIT 1`)
+        .asCallback( (err: Error|null, rows?: HighestBlockNumberRow[]) => {
+          if(err) return callback(err);
+          const row: HighestBlockNumberRow = rows![0];
+          const fromBlock: number = (!row || !row.highest_block_number) ? uploadBlockNumbers[augur.rpc.getNetworkID()] : row.highest_block_number + 1;
+          const highestBlockNumber: number = parseInt(blockNumber, 16);
+          if (fromBlock >= highestBlockNumber) return callback(null); // augur-node is already up-to-date
 
-        downloadAugurLogs(db, augur, fromBlock, highestBlockNumber, (err?: Error|null) => {
-          if (err) return callback(err)
-          db.insert({highest_block_number: highestBlockNumber}).into("blockchain_sync_history").asCallback(callback);
+          downloadAugurLogs(db, augur, fromBlock, highestBlockNumber, (err?: Error|null) => {
+            if (err) return callback(err)
+            db.insert({highest_block_number: highestBlockNumber}).into("blockchain_sync_history").asCallback(callback);
+          });
         });
-      });
-  }));
+    });
+  });
 }
