8ec5c21be61ad266e3afff3b3b0af093bf88c508
Paul Gebheim <paul@onetaste.us>
2017-10-10 15:08:17 -0700

    Fix case in sql

--- src/server/getters/get-account-transfer-history.ts
+++ src/server/getters/get-account-transfer-history.ts
@@ -26,9 +26,9 @@ type TransferHistory = Array<TransferLog>;
 export function getAccountTransferHistory(db: Knex, account: Address, token: Address|null, callback: (err?: Error|null, result?: TransferHistory) => void): void {
   let query: Knex.Raw;
   if (token === null) {
-    query = db.raw("select * from transfers where sender = ? OR recipient = ?", [account, account]);
+    query = db.raw("SELECT * FROM transfers WHERE sender = ? OR recipient = ?", [account, account]);
   } else {
-    query = db.raw("select * from transfers where (sender = ? OR recipient = ?) AND token = ?", [account, account, token]);
+    query = db.raw("SELECT * FROM transfers WHERE (sender = ? OR recipient = ?) AND token = ?", [account, account, token]);
   }
 
   query.map((transferRow: TransferRow): TransferLog => ({
