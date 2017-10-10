AugurProject/augur-ui

semis linting rule + fixes

src/modules/app/actions/init-augur.js
@@ -1,74 +1,74 @@
-import { augur, connect } from 'services/augurjs';
-import { BRANCH_ID } from 'modules/app/constants/network';
-import { updateEnv } from 'modules/app/actions/update-env';
-import { updateConnectionStatus } from 'modules/app/actions/update-connection';
-import { updateAssets } from 'modules/auth/actions/update-assets';
-import { updateContractAddresses } from 'modules/contracts/actions/update-contract-addresses';
-import { updateFunctionsAPI, updateEventsAPI } from 'modules/contracts/actions/update-contract-api';
-import { loadChatMessages } from 'modules/chat/actions/load-chat-messages';
-import { setLoginAccount } from 'modules/auth/actions/set-login-account';
-import { loadBranch } from 'modules/app/actions/load-branch';
-import { registerTransactionRelay } from 'modules/transactions/actions/register-transaction-relay';
-import logError from 'utils/log-error';
-import noop from 'utils/noop';
+import { augur, connect } from 'services/augurjs'
+import { BRANCH_ID } from 'modules/app/constants/network'
+import { updateEnv } from 'modules/app/actions/update-env'
+import { updateConnectionStatus } from 'modules/app/actions/update-connection'
+import { updateAssets } from 'modules/auth/actions/update-assets'
+import { updateContractAddresses } from 'modules/contracts/actions/update-contract-addresses'
+import { updateFunctionsAPI, updateEventsAPI } from 'modules/contracts/actions/update-contract-api'
+import { loadChatMessages } from 'modules/chat/actions/load-chat-messages'
+import { setLoginAccount } from 'modules/auth/actions/set-login-account'
+import { loadBranch } from 'modules/app/actions/load-branch'
+import { registerTransactionRelay } from 'modules/transactions/actions/register-transaction-relay'
+import logError from 'utils/log-error'
+import noop from 'utils/noop'
 
 // for testing only
-import { reportingTestSetup } from 'modules/reports/actions/reporting-test-setup';
+import { reportingTestSetup } from 'modules/reports/actions/reporting-test-setup'
 
 // fixes Reflect not being recognized in test or node 4.2
-require('core-js/es6/reflect');
+require('core-js/es6/reflect')
 
 export function initAugur(cb) {
   return (dispatch, getState) => {
-    const xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest()
     xhttp.onreadystatechange = () => {
       if (xhttp.readyState === 4 && xhttp.status === 200) {
-        const env = JSON.parse(xhttp.responseText);
-        dispatch(updateEnv(env));
+        const env = JSON.parse(xhttp.responseText)
+        dispatch(updateEnv(env))
         connect(env, (err, vitals) => {
-          if (err) return console.error('connect failure:', err);
-          dispatch(updateConnectionStatus(true));
-          dispatch(updateContractAddresses(vitals.contracts));
-          dispatch(updateFunctionsAPI(vitals.api.functions));
-          dispatch(updateEventsAPI(vitals.api.events));
-          dispatch(registerTransactionRelay());
-          dispatch(loadChatMessages('augur'));
-          dispatch(setLoginAccount(env.autoLogin, vitals.coinbase));
+          if (err) return console.error('connect failure:', err)
+          dispatch(updateConnectionStatus(true))
+          dispatch(updateContractAddresses(vitals.contracts))
+          dispatch(updateFunctionsAPI(vitals.api.functions))
+          dispatch(updateEventsAPI(vitals.api.events))
+          dispatch(registerTransactionRelay())
+          dispatch(loadChatMessages('augur'))
+          dispatch(setLoginAccount(env.autoLogin, vitals.coinbase))
           if (env.reportingTest) {
 
             // 127.0.0.1 only: configure for follow-on (multi-user) reporting testing
             if (typeof window !== 'undefined' && window.location.hostname === '127.0.0.1' && env.reportingTest === true) {
               augur.api.Branches.getBranches((branches) => {
-                console.debug(window.location.hostname, branches[branches.length - 1]);
-                env.branchID = branches[branches.length - 1];
-                env.reportingTest = false;
+                console.debug(window.location.hostname, branches[branches.length - 1])
+                env.branchID = branches[branches.length - 1]
+                env.reportingTest = false
                 if (getState().loginAccount.address) {
                   augur.api.Faucets.fundNewAccount({
                     _signer: getState().loginAccount.privateKey,
                     branch: env.branchID || BRANCH_ID,
                     onSent: noop,
                     onSuccess: () => {
-                      dispatch(updateAssets());
-                      dispatch(loadBranch(env.branchID || BRANCH_ID));
+                      dispatch(updateAssets())
+                      dispatch(loadBranch(env.branchID || BRANCH_ID))
                     },
                     onFailed: logError
-                  });
+                  })
                 } else {
-                  dispatch(loadBranch(env.branchID || BRANCH_ID));
+                  dispatch(loadBranch(env.branchID || BRANCH_ID))
                 }
-              });
+              })
 
             } else {
-              dispatch(reportingTestSetup(env.branchID));
+              dispatch(reportingTestSetup(env.branchID))
             }
           } else {
-            dispatch(loadBranch(env.branchID || BRANCH_ID));
+            dispatch(loadBranch(env.branchID || BRANCH_ID))
           }
-          cb && cb();
-        });
+          cb && cb()
+        })
       }
-    };
-    xhttp.open('GET', '/config/env.json', true);
-    xhttp.send();
-  };
+    }
+    xhttp.open('GET', '/config/env.json', true)
+    xhttp.send()
+  }
 }

src/modules/auth/actions/register.js
@@ -1,23 +1,23 @@
-import { augur } from 'services/augurjs';
-import { base58Decode, base58Encode } from 'utils/base-58';
-import { loadAccountData } from 'modules/auth/actions/load-account-data';
-import { updateIsLogged } from 'modules/auth/actions/update-is-logged';
-import logError from 'utils/log-error';
+import { augur } from 'services/augurjs'
+import { base58Decode, base58Encode } from 'utils/base-58'
+import { loadAccountData } from 'modules/auth/actions/load-account-data'
+import { updateIsLogged } from 'modules/auth/actions/update-is-logged'
+import logError from 'utils/log-error'
 
 export const register = (password, callback = logError) => dispatch => (
   augur.accounts.register(password, (account) => {
     if (!account || !account.address) {
-      return callback({ code: 0, message: 'failed to register' });
+      return callback({ code: 0, message: 'failed to register' })
     } else if (account.error) {
-      return callback({ code: account.error, message: account.message });
+      return callback({ code: account.error, message: account.message })
     }
-    dispatch(updateIsLogged(true));
-    callback(null, base58Encode(account));
+    dispatch(updateIsLogged(true))
+    callback(null, base58Encode(account))
   })
-);
+)
 
 export const setupAndFundNewAccount = (password, loginID, callback = logError) => (dispatch, getState) => {
-  if (!loginID) return callback({ message: 'loginID is required' });
-  dispatch(loadAccountData({ loginID, ...base58Decode(loginID) }, true));
-  callback(null);
-};
+  if (!loginID) return callback({ message: 'loginID is required' })
+  dispatch(loadAccountData({ loginID, ...base58Decode(loginID) }, true))
+  callback(null)
+}

src/modules/reports/components/report-form.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import Checkbox from 'modules/common/components/checkbox';
-import OutcomeOptions from 'modules/reports/components/outcome-options';
-import { BINARY, SCALAR } from 'modules/markets/constants/market-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import Checkbox from 'modules/common/components/checkbox'
+import OutcomeOptions from 'modules/reports/components/outcome-options'
+import { BINARY, SCALAR } from 'modules/markets/constants/market-types'
 
 export default class ReportForm extends Component {
   // TODO -- Prop Validations
@@ -21,7 +21,7 @@ export default class ReportForm extends Component {
   };
 
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       type: props.type,
       minValue: props.minValue,
@@ -30,28 +30,28 @@ export default class ReportForm extends Component {
       isIndeterminate: props.isIndeterminate,
       isUnethical: props.isUnethical,
       isReported: props.isReported
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleOutcomeChange = this.handleOutcomeChange.bind(this);
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleOutcomeChange = this.handleOutcomeChange.bind(this)
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.isReported !== this.state.isReported) {
-      this.setState({ isReported: nextProps.isReported });
+      this.setState({ isReported: nextProps.isReported })
     }
   }
 
   handleOutcomeChange = e => this.setState({ reportedOutcomeID: e.target.value });
 
   handleSubmit() {
-    this.props.onClickSubmit(this.state.reportedOutcomeID, this.state.isUnethical, this.state.isIndeterminate, this.props.history);
-    this.setState({ reportedOutcomeID: '', isIndeterminate: undefined, isUnethical: undefined, isReported: false });
+    this.props.onClickSubmit(this.state.reportedOutcomeID, this.state.isUnethical, this.state.isIndeterminate, this.props.history)
+    this.setState({ reportedOutcomeID: '', isIndeterminate: undefined, isUnethical: undefined, isReported: false })
   }
 
   render() {
-    const p = this.props;
-    const s = this.state;
-    const indeterminateValue = p.type === BINARY ? '1.5' : '0.5';
+    const p = this.props
+    const s = this.state
+    const indeterminateValue = p.type === BINARY ? '1.5' : '0.5'
 
     return (
       <article className={classNames('report-form', { reported: s.isReported })}>
@@ -120,6 +120,6 @@ export default class ReportForm extends Component {
           }
         </div>
       </article>
-    );
+    )
   }
 }

AugurProject/augur

src/modules/app/actions/init-augur.js
@@ -1,43 +1,43 @@
-import * as AugurJS from 'services/augurjs';
-import { BRANCH_ID } from 'modules/app/constants/network';
-import { updateEnv } from 'modules/app/actions/update-env';
-import { updateConnectionStatus, updateAugurNodeConnectionStatus } from 'modules/app/actions/update-connection';
-import { updateContractAddresses } from 'modules/contracts/actions/update-contract-addresses';
-import { updateFunctionsAPI, updateEventsAPI } from 'modules/contracts/actions/update-contract-api';
-import { setLoginAccount } from 'modules/auth/actions/set-login-account';
-import { loadBranch } from 'modules/app/actions/load-branch';
-import { registerTransactionRelay } from 'modules/transactions/actions/register-transaction-relay';
-import logError from 'utils/log-error';
+import * as AugurJS from 'services/augurjs'
+import { BRANCH_ID } from 'modules/app/constants/network'
+import { updateEnv } from 'modules/app/actions/update-env'
+import { updateConnectionStatus, updateAugurNodeConnectionStatus } from 'modules/app/actions/update-connection'
+import { updateContractAddresses } from 'modules/contracts/actions/update-contract-addresses'
+import { updateFunctionsAPI, updateEventsAPI } from 'modules/contracts/actions/update-contract-api'
+import { setLoginAccount } from 'modules/auth/actions/set-login-account'
+import { loadBranch } from 'modules/app/actions/load-branch'
+import { registerTransactionRelay } from 'modules/transactions/actions/register-transaction-relay'
+import logError from 'utils/log-error'
 
 // fixes Reflect not being recognized in test or node 4.2
-require('core-js/es6/reflect');
+require('core-js/es6/reflect')
 
 export function initAugur(callback = logError) {
   return (dispatch, getState) => {
-    const xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest()
     xhttp.onreadystatechange = () => {
       if (xhttp.readyState === 4) {
         if (xhttp.status === 200) {
-          const env = JSON.parse(xhttp.responseText);
-          dispatch(updateEnv(env));
+          const env = JSON.parse(xhttp.responseText)
+          dispatch(updateEnv(env))
           AugurJS.connect(env, (err, ethereumNodeConnectionInfo) => {
-            if (err) return callback(err);
-            dispatch(updateConnectionStatus(true));
-            dispatch(updateContractAddresses(ethereumNodeConnectionInfo.contracts));
-            dispatch(updateFunctionsAPI(ethereumNodeConnectionInfo.abi.functions));
-            dispatch(updateEventsAPI(ethereumNodeConnectionInfo.abi.events));
-            if (env.augurNodeUrl) dispatch(updateAugurNodeConnectionStatus(true));
-            dispatch(registerTransactionRelay());
-            dispatch(setLoginAccount(env.autoLogin, ethereumNodeConnectionInfo.coinbase));
-            dispatch(loadBranch(env.branchID || BRANCH_ID));
-            callback();
-          });
+            if (err) return callback(err)
+            dispatch(updateConnectionStatus(true))
+            dispatch(updateContractAddresses(ethereumNodeConnectionInfo.contracts))
+            dispatch(updateFunctionsAPI(ethereumNodeConnectionInfo.abi.functions))
+            dispatch(updateEventsAPI(ethereumNodeConnectionInfo.abi.events))
+            if (env.augurNodeUrl) dispatch(updateAugurNodeConnectionStatus(true))
+            dispatch(registerTransactionRelay())
+            dispatch(setLoginAccount(env.autoLogin, ethereumNodeConnectionInfo.coinbase))
+            dispatch(loadBranch(env.branchID || BRANCH_ID))
+            callback()
+          })
         } else {
-          callback(xhttp.statusText);
+          callback(xhttp.statusText)
         }
       }
-    };
-    xhttp.open('GET', '/config/env.json', true);
-    xhttp.send();
-  };
+    }
+    xhttp.open('GET', '/config/env.json', true)
+    xhttp.send()
+  }
 }

src/modules/auth/actions/register.js
@@ -1,19 +1,19 @@
-import { augur } from 'services/augurjs';
-import { base58Decode, base58Encode } from 'utils/base-58';
-import { loadAccountData } from 'modules/auth/actions/load-account-data';
-import { updateIsLoggedIn } from 'modules/auth/actions/update-is-logged-in';
-import logError from 'utils/log-error';
+import { augur } from 'services/augurjs'
+import { base58Decode, base58Encode } from 'utils/base-58'
+import { loadAccountData } from 'modules/auth/actions/load-account-data'
+import { updateIsLoggedIn } from 'modules/auth/actions/update-is-logged-in'
+import logError from 'utils/log-error'
 
 export const register = (password, callback = logError) => dispatch => (
   augur.accounts.register({ password }, (err, account) => {
-    if (err) return callback(err);
-    dispatch(updateIsLoggedIn(true));
-    callback(null, base58Encode(account));
+    if (err) return callback(err)
+    dispatch(updateIsLoggedIn(true))
+    callback(null, base58Encode(account))
   })
-);
+)
 
 export const setupAndFundNewAccount = (password, loginID, callback = logError) => (dispatch, getState) => {
-  if (!loginID) return callback({ message: 'loginID is required' });
-  dispatch(loadAccountData({ loginID, ...base58Decode(loginID) }, true));
-  callback(null);
-};
+  if (!loginID) return callback({ message: 'loginID is required' })
+  dispatch(loadAccountData({ loginID, ...base58Decode(loginID) }, true))
+  callback(null)
+}

src/modules/reports/components/report-form.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import Checkbox from 'modules/common/components/checkbox';
-import OutcomeOptions from 'modules/reports/components/outcome-options';
-import { BINARY, SCALAR } from 'modules/markets/constants/market-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import Checkbox from 'modules/common/components/checkbox'
+import OutcomeOptions from 'modules/reports/components/outcome-options'
+import { BINARY, SCALAR } from 'modules/markets/constants/market-types'
 
 export default class ReportForm extends Component {
   // TODO -- Prop Validations
@@ -21,7 +21,7 @@ export default class ReportForm extends Component {
   };
 
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       type: props.type,
       minPrice: props.minPrice,
@@ -30,14 +30,14 @@ export default class ReportForm extends Component {
       amountToStake: props.amountToStake,
       isIndeterminate: props.isIndeterminate,
       isReported: props.isReported
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleOutcomeChange = this.handleOutcomeChange.bind(this);
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleOutcomeChange = this.handleOutcomeChange.bind(this)
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.isReported !== this.state.isReported) {
-      this.setState({ isReported: nextProps.isReported });
+      this.setState({ isReported: nextProps.isReported })
     }
   }
 
@@ -45,14 +45,14 @@ export default class ReportForm extends Component {
 
   handleSubmit() {
     // TODO add amount to stake to form
-    this.props.onClickSubmit(this.state.reportedOutcomeID, this.state.amountToStake, this.state.isIndeterminate, this.props.history);
-    this.setState({ reportedOutcomeID: '', isIndeterminate: undefined, isReported: false });
+    this.props.onClickSubmit(this.state.reportedOutcomeID, this.state.amountToStake, this.state.isIndeterminate, this.props.history)
+    this.setState({ reportedOutcomeID: '', isIndeterminate: undefined, isReported: false })
   }
 
   render() {
-    const p = this.props;
-    const s = this.state;
-    const indeterminateValue = p.type === BINARY ? '1.5' : '0.5';
+    const p = this.props
+    const s = this.state
+    const indeterminateValue = p.type === BINARY ? '1.5' : '0.5'
 
     return (
       <article className={classNames('report-form', { reported: s.isReported })}>
@@ -107,6 +107,6 @@ export default class ReportForm extends Component {
           }
         </div>
       </article>
-    );
+    )
   }
 }