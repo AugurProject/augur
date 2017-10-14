00fe246a536221b7998064b3f77c79adf63d7771
Paul Gebheim <paul@onetaste.us>
2017-10-13 17:57:00 -0700

    Fix quoting column names

--- src/migrations/20171005123329_transfers.ts
+++ src/migrations/20171005123329_transfers.ts
@@ -4,13 +4,13 @@ exports.up = async (knex: Knex): Promise<any> => {
   return knex.schema.dropTableIfExists("transfers").then((): PromiseLike<any> => {
     return knex.schema.raw(`CREATE TABLE transfers (
       "transactionHash" varchar(66) NOT NULL,
-      "logIndex" integer NOT NULL CONSTRAINT nonnegativeLogIndex CHECK (logIndex >= 0),
+      "logIndex" integer NOT NULL CONSTRAINT "nonnegativeLogIndex" CHECK ("logIndex" >= 0),
       sender varchar(66) NOT NULL,
       recipient varchar(66) NOT NULL,
       token varchar(66) NOT NULL,
-      value numeric NOT NULL CONSTRAINT positiveValue CHECK (value > 0),
-      "blockNumber" integer NOT NULL CONSTRAINT positiveBlockNumber CHECK (blockNumber > 0),
-      UNIQUE(transactionHash, logIndex)
+      value numeric NOT NULL CONSTRAINT "positiveValue" CHECK (value > 0),
+      "blockNumber" integer NOT NULL CONSTRAINT "positiveBlockNumber" CHECK ("blockNumber" > 0),
+      UNIQUE("transactionHash", "logIndex")
     )`);
   });
 };
