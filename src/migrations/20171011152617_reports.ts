499cc0aa3488242c8aff06321f4ca1d42c021da4
Paul Gebheim <paul@onetaste.us>
2017-10-16 14:44:42 -0700

    Apply column quotes in migrations

--- src/migrations/20171011152617_reports.ts
+++ src/migrations/20171011152617_reports.ts
@@ -4,9 +4,9 @@ exports.up = async (knex: Knex): Promise<any> => {
   return knex.schema.dropTableIfExists("reports").then( (): void  => {
     knex.schema.raw(`CREATE TABLE reports (
       reporter varchar(66) NOT NULL,
-      marketID varchar(66) NOT NULL,
-      reportingToken varchar(66) NOT NULL,
-      amountStaked numeric,
+      "marketID" varchar(66) NOT NULL,
+      "reportingToken" varchar(66) NOT NULL,
+      "amountStaked" numeric,
       payout0 numeric,
       payout1 numeric,
       payout2 numeric,
@@ -15,7 +15,7 @@ exports.up = async (knex: Knex): Promise<any> => {
       payout5 numeric,
       payout6 numeric,
       payout7 numeric,
-      isInvalid integer
+      "isInvalid" integer
     )`).then( (): void => {
       knex.schema.table("reports", (table: Knex.AlterTableBuilder): void => {
         table.increments("reportID").primary().notNullable();
