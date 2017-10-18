b7debb6b5e030d475a302b5b802fb9d4b569a333
Scott Bigelow <epheph@gmail.com>
2017-10-18 16:14:38 -0700

    change to non-raw createTable

--- src/migrations/20171017150457_market_state.ts
+++ src/migrations/20171017150457_market_state.ts
@@ -3,13 +3,13 @@ import * as Knex from "knex";
 // PRIMARY KEY AUTOINCREMENT pgsql compat issue? TODO: use .primary()
 exports.up = async (knex: Knex): Promise<any> => {
   return knex.schema.dropTableIfExists("market_state").then( (): PromiseLike<any> => {
-    return knex.schema.raw(`CREATE TABLE market_state (
-      "marketStateID" integer PRIMARY KEY AUTOINCREMENT,
-      "marketID" varchar(66) NOT NULL,
-      phase numeric,
-      "isDisputed" boolean,
-      "blockNumber" integer NOT NULL
-    )`);
+    return  knex.schema.createTable('market_state', function(table) {
+      table.increments("marketStateID");
+      table.string("marketID", 66).notNullable();
+      table.integer("phase").nullable();
+      table.boolean("isDisputed");
+      table.integer("blockNumber").notNullable();
+    })
   });
 };
 
