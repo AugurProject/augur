ab535a79f08f3103380b673ae1fcd233a554164f
Scott Bigelow <epheph@gmail.com>
2017-10-18 16:16:37 -0700

    lint adherence

--- src/migrations/20171017150457_market_state.ts
+++ src/migrations/20171017150457_market_state.ts
@@ -3,13 +3,13 @@ import * as Knex from "knex";
 // PRIMARY KEY AUTOINCREMENT pgsql compat issue? TODO: use .primary()
 exports.up = async (knex: Knex): Promise<any> => {
   return knex.schema.dropTableIfExists("market_state").then( (): PromiseLike<any> => {
-    return  knex.schema.createTable('market_state', function(table) {
+    return  knex.schema.createTable("market_state", (table) => {
       table.increments("marketStateID");
       table.string("marketID", 66).notNullable();
       table.integer("phase").nullable();
       table.boolean("isDisputed");
       table.integer("blockNumber").notNullable();
-    })
+    });
   });
 };
 
