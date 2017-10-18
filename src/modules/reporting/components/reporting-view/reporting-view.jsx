c4949f8785ec1b624f8a22cd19e3b357e24d96e8
Serena Randolph <serena.randolph@gmail.com>
2017-10-18 15:34:37 -0700

    Adding in reporting markets to Reporting view.

--- src/modules/reporting/components/reporting-view/reporting-view.jsx
+++ src/modules/reporting/components/reporting-view/reporting-view.jsx
@@ -2,60 +2,50 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 
-import Styles from 'modules/reporting/components/reporting-view/reporting-view.styles'
+import ReportingHeader from 'modules/reporting/components/reporting-header/reporting-header'
+import MarketsList from 'modules/markets/components/markets-list'
+
+// import Styles from 'modules/reporting/components/reporting-view/reporting-view.styles'
 
 export default class ReportingView extends Component {
   static propTypes = {
-    marketsReporting: PropTypes.array.isRequired,
+    markets: PropTypes.array.isRequired,
+    history: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+    scalarShareDenomination: PropTypes.object.isRequired,
+    toggleFavorite: PropTypes.func.isRequired,
+    loadMarketsInfo: PropTypes.func.isRequired,
+    isLogged: PropTypes.bool.isRequired,
   }
 
   constructor(props) {
     super(props)
 
     this.state = {
-      daysLeft: 9,
-      endDate: 'November 20, 2017',
-      totalStake: 1,
+      filteredMarkets: [0, 1],
     }
   }
 
   render() {
     const s = this.state
-
-    const currentPeriodStyle = {
-      width: `${((27 - s.daysLeft) / 27) * 100}%`
-    }
+    const p = this.props
 
     return (
       <section>
         <Helmet>
           <title>Reporting</title>
         </Helmet>
-        <article className={Styles.ReportingHeader}>
-          <div className={Styles.ReportingHeader__header}>
-            <div>
-              <h1 className={Styles.ReportingHeader__heading}>Reporting</h1>
-              <span className={Styles.ReportingHeader__endDate}>Reporting cycle ends { s.endDate }</span>
-            </div>
-            <div>
-              <span className={Styles.ReportingHeader__stake}>Total Stake: { s.totalStake } REP</span>
-            </div>
-          </div>
-          <div className={Styles['ReportingHeader__graph-wrapper']}>
-            <div className={Styles.ReportingHeader__graph}>
-              <div className={Styles['ReportingHeader__graph-current']}>
-                <div style={currentPeriodStyle}>
-                  <span>{ s.daysLeft } days left</span>
-                </div>
-              </div>
-              <div className={Styles['ReportingHeader__graph-dispute']} />
-            </div>
-            <div className={Styles.ReportingHeader__labels}>
-              <span>Current Cycle</span>
-              <span>Dispute</span>
-            </div>
-          </div>
-        </article>
+        <ReportingHeader />
+        <MarketsList
+          isLogged={p.isLogged}
+          markets={p.markets}
+          filteredMarkets={s.filteredMarkets}
+          location={p.location}
+          history={p.history}
+          scalarShareDenomination={p.scalarShareDenomination}
+          toggleFavorite={p.toggleFavorite}
+          loadMarketsInfo={p.loadMarketsInfo}
+        />
       </section>
     )
   }
