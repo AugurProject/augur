4b318281e1946556092b0704646e4555858a3d78
Scott Bigelow <epheph@gmail.com>
2017-10-18 15:56:24 -0700

    callback error handling, types

--- src/blockchain/log-processors/designated-report-submitted.ts
+++ src/blockchain/log-processors/designated-report-submitted.ts
@@ -19,16 +19,16 @@ export function processDesignatedReportSubmittedLog(db: Knex, augur: Augur, trx:
         blockNumber: log.blockNumber
     };
 
-    db.transacting(trx).insert(marketStateDataToInsert).returning("marketStateID").into("market_state").asCallback((err: Error|null, marketStateID: Array<number>): void => {
+    db.transacting(trx).insert(marketStateDataToInsert).returning("marketStateID").into("market_state").asCallback((err: Error|null, marketStateID?: Array<number>): void => {
         if (err) return callback(err);
-        if (!marketStateID) return callback(new Error("Failed to generate new marketStateID for marketID:" + log.market));
+        if (!marketStateID || !marketStateID.length) return callback(new Error("Failed to generate new marketStateID for marketID:" + log.market));
         const newMarketStateID = marketStateID[0];
         db("markets").transacting(trx).update({ marketStateID: newMarketStateID }).where("marketID", log.market).asCallback((err: Error|null): void => {
             if (err) return callback(err);
             const dataToInsert: { [index: string]: string|number } = {
-                marketID: log.market;
+                marketID: log.market
             };
-            log.payoutNumerators.forEach((value: number, i: number) => {
+            log.payoutNumerators.forEach((value: number, i: number): void => {
                 dataToInsert["payout" + i] = value;
             });
             db.transacting(trx).insert(dataToInsert).into("reports_designated").asCallback(callback);
