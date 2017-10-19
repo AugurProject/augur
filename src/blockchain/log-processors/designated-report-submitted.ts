ba9bde80d3f911ed8072c633688fe647365c9d27
Scott Bigelow <epheph@gmail.com>
2017-10-19 09:33:30 -0700

    Clean up comments

--- src/blockchain/log-processors/designated-report-submitted.ts
+++ src/blockchain/log-processors/designated-report-submitted.ts
@@ -4,14 +4,12 @@ import { FormattedLog, ErrorCallback } from "../../types";
 
 export function processDesignatedReportSubmittedLog(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedLog, callback: ErrorCallback): void {
     // @achapman: "The designated reporter purchases Stake Tokens in order to do the Designated Report"
-    // Should designatedReportSubmitted include StakedToken address?
+    // TODO: Should designatedReportSubmitted include StakedToken address?
 
-    // for phase, sqlite lacks an ENUM, but the idea here is that:
     //  phase:null = no report submitted
     //  phase:0 = designated report submitted
     //  phase:1 = round1
     //  phase:2 = round2
-    // if that seems reasonable, i'll update PR to import the enum and use name below
     const marketStateDataToInsert: { [index: string]: string|number|boolean } = {
         marketID: log.market,
         phase: 0,
