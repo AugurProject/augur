23783127ed6411d0d073d1354c395a8ea2f97fba
Jack Peterson <jack@tinybike.net>
2017-10-19 21:23:50 -0700

    Defined trades row interface instead of using any

--- src/server/getters/get-market-price-history.ts
+++ src/server/getters/get-market-price-history.ts
@@ -2,6 +2,12 @@ import * as Knex from "knex";
 import { Address, TimestampedPrice, MarketPriceHistory } from "../../types";
 import { sortDirection } from "../../utils/sort-direction";
 
+interface MarketPriceHistoryTradesRow {
+  outcome: number;
+  price: string|number;
+  tradeTime: number;
+}
+
 // Input: MarketID
 // Output: { outcome: [{ price, timestamp }] }
 export function getMarketPriceHistory(db: Knex, marketID: Address|null|undefined, sortBy: string|null|undefined, isSortDescending: boolean|null|undefined, limit: number|null|undefined, offset: number|null|undefined, callback: (err?: Error|null, result?: MarketPriceHistory) => void): void {
@@ -9,11 +15,11 @@ export function getMarketPriceHistory(db: Knex, marketID: Address|null|undefined
   let query: Knex.QueryBuilder = db.select(columnsToSelect).from("trades").where({ marketID }).orderBy(sortBy || "tradeTime", sortDirection(isSortDescending, "desc"));
   if (limit != null) query = query.limit(limit);
   if (offset != null) query = query.offset(offset);
-  query.asCallback((err?: Error|null, tradesRows?: Array<any>): void => {
+  query.asCallback((err?: Error|null, tradesRows?: Array<MarketPriceHistoryTradesRow>): void => {
     if (err) return callback(err);
     if (!tradesRows || !tradesRows.length) return callback(null);
     const marketPriceHistory: MarketPriceHistory = {};
-    tradesRows.forEach((trade: any): void => {
+    tradesRows.forEach((trade: MarketPriceHistoryTradesRow): void => {
       if (!marketPriceHistory[trade.outcome]) marketPriceHistory[trade.outcome] = [];
       marketPriceHistory[trade.outcome].push({
         price: trade.price,
