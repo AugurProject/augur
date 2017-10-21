16b60688ae3bdae6079ef0c75bf9949feed25d1d
John Danz <John.Danz87@gmail.com>
2017-10-21 12:03:42 -0500

    started working on account header, have the currencies scaling nicely, need to work with highcharts to add the charts, will need to do some reading/learning for that

--- src/modules/account/components/account-header/account-header.jsx
+++ src/modules/account/components/account-header/account-header.jsx
@@ -1,5 +1,66 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+// import Highcharts from 'highcharts'
+// import noData from 'highcharts/modules/no-data-to-display'
 
-const AccountHeader = () => (<div />)
+import Styles from 'modules/account/components/account-header/account-header.styles'
+
+// const AccountHeader = (p) => (
+
+class AccountHeader extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  // componentDidMount() {
+  //   noData(Highcharts)
+  //
+  //   Highcharts.setOptions({
+  //     lang: {
+  //       thousandsSep: ','
+  //     }
+  //   })
+
+  calcFontSize(value) {
+    // the container is 26.25rem, or 420 px, 16 px is the base, the 3 character
+    // label for the currency is roughly 44 px + another 8px for a space = 52px.
+    // 420 - 52 = 368, 368 / 16 = 23. so 23rem is our space to fill with text.
+    const CurrencyWidth = 23;
+    let size = (CurrencyWidth / value.length * 2)
+    size = size > 10 ? 10 : size
+    size = size + 'rem'
+    return size;
+  }
+
+  render () {
+    const p = this.props
+    console.log(p)
+    // NOTE: dummy data for now for easier testing/styling
+    const ethValue = '9,000'
+    const repValue = '47.932'
+
+    const ethSize = this.calcFontSize(ethValue);
+    const repSize = this.calcFontSize(repValue);
+
+    return (
+      <div
+        className={Styles.AccountHeader}
+      >
+        <div
+          title={p.stats[0].totalRealEth.title} className={Styles.AccountHeader__Currency}
+        >
+          <span className={Styles['AccountHeader__Currency-value']} style={{fontSize: ethSize}}>{ethValue}</ span>
+          <span className={Styles['AccountHeader__Currency-label']}>{p.stats[0].totalRealEth.label}</ span>
+        </ div>
+        <div
+          title={p.stats[0].totalRep.title} className={Styles.AccountHeader__Currency}
+        >
+          <span className={Styles['AccountHeader__Currency-value']} style={{fontSize: repSize}}>{repValue}</ span>
+          <span className={Styles['AccountHeader__Currency-label']}>{p.stats[0].totalRep.label}</ span>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default AccountHeader
--- src/modules/account/containers/account-header.js
+++ src/modules/account/containers/account-header.js
@@ -2,4 +2,18 @@ import { connect } from 'react-redux'
 
 import AccountHeader from 'modules/account/components/account-header/account-header'
 
-export default connect()(AccountHeader)
+import { selectCoreStats } from 'modules/account/selectors/core-stats'
+import { selectLoginAccount } from 'modules/auth/selectors/login-account'
+
+const mapStateToProps = state => {
+  const loginAccount = selectLoginAccount(state)
+  const coreStats = selectCoreStats(state)
+
+  return {
+    stats: coreStats
+  }
+}
+
+const AccountHeaderContainer = connect(mapStateToProps)(AccountHeader)
+
+export default AccountHeaderContainer
