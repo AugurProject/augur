578ff2df91bd2f2cb7e6b72ecf91d908ef56bf4b
Jack Peterson <jack@tinybike.net>
2017-10-21 16:51:06 -0700

    Replaced raw selects with querybuilder in MarketCreated processor

--- src/blockchain/log-processors/market-created.ts
+++ src/blockchain/log-processors/market-created.ts
@@ -16,7 +16,7 @@ interface MarketCreatedOnContractData {
 }
 
 export function processMarketCreatedLog(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedLog, callback: ErrorCallback): void {
-  trx.raw(`SELECT "blockTimestamp" FROM blocks WHERE "blockNumber" = ?`, [log.blockNumber]).asCallback((err?: Error|null, blocksRow?: {blockTimestamp: number}): void => {
+  trx.select("blockTimestamp").from("blocks").where({ blockNumber: log.blockNumber }).asCallback((err?: Error|null, blocksRow?: {blockTimestamp: number}): void => {
     if (err) return callback(err);
     if (!blocksRow) return callback(new Error("block timestamp not found"));
     const marketPayload: {} = { tx: { to: log.market } };
@@ -62,10 +62,10 @@ export function processMarketCreatedLog(db: Knex, augur: Augur, trx: Knex.Transa
         };
         db.transacting(trx).insert(dataToInsert).into("markets").asCallback((err?: Error|null): void => {
           if (err) return callback(err);
-          trx.raw(`SELECT "popularity" FROM categories WHERE "category" = ?`, [log.category]).asCallback((err?: Error|null, row?: {popularity: number}): void => {
+          trx.select("popularity").from("categories").where({ category: log.category }).asCallback((err?: Error|null, row?: {popularity: number}): void => {
             if (err) return callback(err);
             if (row) return callback(null);
-            db.transacting(trx).insert({category: log.category, universe: log.address}).into("categories").asCallback(callback);
+            db.transacting(trx).insert({ category: log.category, universe: log.address }).into("categories").asCallback(callback);
           });
         });
       });
