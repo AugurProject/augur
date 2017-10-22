122e2ae04aac8567909767a2f909b8ccd2f011c4
Jack Peterson <jack@tinybike.net>
2017-10-22 09:52:44 -0700

    Updated all seeds to use knex querybuilder

--- src/seeds/test/reports.ts
+++ src/seeds/test/reports.ts
@@ -2,33 +2,32 @@ import * as Knex from "knex";
 
 exports.seed = async (knex: Knex): Promise<any> => {
   // Deletes ALL existing entries
-  return knex("reports").del().then(() => {
-    return knex.raw(`INSERT INTO reports
-      (reporter, marketID, reportingToken, amountStaked, payout1, payout2, isInvalid)
-      VALUES (
-        '0x0000000000000000000000000000000000000021',
-        '0x0000000000000000000000000000000000000002',
-        '0x0000000000000000001000000000000000000001',
-        17,
-        0,
-        2,
-        0
-      ), (
-        '0x0000000000000000000000000000000000000022',
-        '0x0000000000000000000000000000000000000002',
-        '0x0000000000000000001000000000000000000002',
-        41,
-        2,
-        0,
-        0
-      ), (
-        '0x0000000000000000000000000000000000000023',
-        '0x0000000000000000000000000000000000000002',
-        '0x0000000000000000001000000000000000000002',
-        222,
-        1,
-        1,
-        1
-      )`);
+  return knex("reports").del().then((): void => {
+    // Inserts seed entries
+    knex.batchInsert("reports", [{
+      reporter: "0x0000000000000000000000000000000000000021",
+      marketID: "0x0000000000000000000000000000000000000002",
+      reportingToken: "0x0000000000000000001000000000000000000001",
+      amountStaked: 17,
+      payout1: 0,
+      payout2: 2,
+      isInvalid: 0
+    }, {
+      reporter: "0x0000000000000000000000000000000000000022",
+      marketID: "0x0000000000000000000000000000000000000002",
+      reportingToken: "0x0000000000000000001000000000000000000002",
+      amountStaked: 41,
+      payout1: 2,
+      payout2: 0,
+      isInvalid: 0
+    }, {
+      reporter: "0x0000000000000000000000000000000000000023",
+      marketID: "0x0000000000000000000000000000000000000002",
+      reportingToken: "0x0000000000000000001000000000000000000002",
+      amountStaked: 222,
+      payout1: 1,
+      payout2: 1,
+      isInvalid: 1
+    }], 1000);
   });
 };
--- src/seeds/test/transfers.ts
+++ src/seeds/test/transfers.ts
@@ -2,35 +2,32 @@ import * as Knex from "knex";
 
 exports.seed = async (knex: Knex): Promise<any> => {
   // Deletes ALL existing entries
-  return knex("transfers").del()
-    .then((): PromiseLike<any> => {
-      // Inserts seed entries
-      return knex.raw(`INSERT INTO transfers
-            (transactionHash, logIndex, sender, recipient, token, value, blockNumber)
-            VALUES (
-              '0x00000000000000000000000000000000000000000000000000000000deadbeef',
-              0,
-              '0x0000000000000000000000000000000000000b0b',
-              '0x000000000000000000000000000000000000d00d',
-              '0x1000000000000000000000000000000000000000',
-              10,
-              1400000
-            ), (
-              '0x00000000000000000000000000000000000000000000000000000000d3adb33f',
-              0,
-              '0x000000000000000000000000000000000000d00d',
-              '0x0000000000000000000000000000000000000b0b',
-              '0x1000000000000000000000000000000000000000',
-              2,
-              1400001
-            ), (
-              '0x00000000000000000000000000000000000000000000000000000000deadb33f',
-              1,
-              '0x0000000000000000000000000000000000000b0b',
-              '0x000000000000000000000000000000000000d00d',
-              '0x7a305d9b681fb164dc5ad628b5992177dc66aec8',
-              47,
-              1400001
-            )`);
-    });
+    return knex("transfers").del().then((): void => {
+    // Inserts seed entries
+    knex.batchInsert("transfers", [{
+      transactionHash: "0x00000000000000000000000000000000000000000000000000000000deadbeef",
+      logIndex: 0,
+      sender: "0x0000000000000000000000000000000000000b0b",
+      recipient: "0x000000000000000000000000000000000000d00d",
+      token: "0x1000000000000000000000000000000000000000",
+      value: 10,
+      blockNumber: 1400000
+    }, {
+      transactionHash: "0x00000000000000000000000000000000000000000000000000000000d3adb33f",
+      logIndex: 0,
+      sender: "0x000000000000000000000000000000000000d00d",
+      recipient: "0x0000000000000000000000000000000000000b0b",
+      token: "0x1000000000000000000000000000000000000000",
+      value: 2,
+      blockNumber: 1400001
+    }, {
+      transactionHash: "0x00000000000000000000000000000000000000000000000000000000deadb33f",
+      logIndex: 1,
+      sender: "0x0000000000000000000000000000000000000b0b",
+      recipient: "0x000000000000000000000000000000000000d00d",
+      token: "0x7a305d9b681fb164dc5ad628b5992177dc66aec8",
+      value: 47,
+      blockNumber: 1400001
+    }], 1000);
+  });
 };
