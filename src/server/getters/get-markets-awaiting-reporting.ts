c5af3b701fc5980dabdf43bad05cdaa3683d1a72
Scott Bigelow <epheph@gmail.com>
2017-10-23 15:35:04 -0700

    fixing up merge conflict

--- src/server/getters/get-markets-awaiting-reporting.ts
+++ src/server/getters/get-markets-awaiting-reporting.ts
@@ -4,7 +4,7 @@ import { reshapeMarketsRowToUIMarketInfo } from "./get-market-info";
 
 // Look up all markets that are currently available for limited reporting.
 // Must be able to sort by number of reports submitted for each market so far, and the response should include the number of reports already submitted -- as well as the payoutNumerator values of each of the reports + the amount staked on each -- as part of the response.
-export function getMarketsAwaitingReporting(db: Knex, reportingWindow: Address, reportingRound: number|null, callback: (err?: Error|null, result?: any) => void): void {
+export function getMarketsAwaitingReporting(db: Knex, reportingWindow: Address, reportingRound: number|null, sortBy: string|null|undefined, isSortDescending: boolean|null|undefined, limit: number|null|undefined, offset: number|null|undefined, callback: (err: Error|null, result?: any) => void): void {
     const queryData: {} = (reportingRound != null) ? { "market_state.round:": reportingRound } : {};
     // TODO: should we also consider a market_state's phase IS NULL?
     console.log(db.select().from("markets").join("market_state", "markets.marketStateID", "market_state.marketStateID").where(queryData).toSQL())
