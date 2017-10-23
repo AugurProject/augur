9e68f559f6db44584cbb5f6eaba8434e4570791a
Scott Bigelow <epheph@gmail.com>
2017-10-23 15:42:05 -0700

    populate the state to allow for phase lookup

---
+++ src/seeds/test/market_state.ts
@@ -0,0 +1,15 @@
+import * as Knex from "knex";
+
+exports.seed = async (knex: Knex): Promise<any> => {
+  // Deletes ALL existing entries
+  return knex("market_state").del().then((): void => {
+    // Inserts seed entries
+    knex.batchInsert("market_state", [{
+      marketID: "0x0000000000000000000000000000000000000001",
+      marketStateID: 1,
+      phase: 1,
+      isDisputed: false,
+      blockNumber: 50
+    }], 1000);
+  });
+};
