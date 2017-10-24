d5480a283e197b83e3cc08d06d90ae3dc87f091c
Scott Bigelow <epheph@gmail.com>
2017-10-24 15:20:57 -0700

    wire up those comments

--- src/server/getters/get-markets-awaiting-reporting.ts
+++ src/server/getters/get-markets-awaiting-reporting.ts
@@ -1,6 +1,7 @@
 import * as Knex from "knex";
 import { Address, MarketsRow, UIMarketInfo, UIMarketsInfo, ErrorCallback } from "../../types";
 import { reshapeMarketsRowToUIMarketInfo, getMarketsWithPhase } from "./get-market-info";
+import { sortDirection } from "../../utils/sort-direction";
 
 // Look up all markets that are currently available for limited reporting.
 // Must be able to sort by number of reports submitted for each market so far, and the response should include the number of reports already submitted -- as well as the payoutNumerator values of each of the reports + the amount staked on each -- as part of the response.
@@ -9,12 +10,14 @@ export function getMarketsAwaitingReporting(db: Knex, reportingWindow: Address|n
   if ( reportingWindow != null ) {
     queryData["markets.reportingWindow"] = reportingWindow;
   }
-  // query = query.orderBy(sortBy || "volume", sortDirection(isSortDescending, "desc"));
-  // if (limit != null) query = query.limit(limit);
-  // if (offset != null) query = query.offset(offset);
+  let query = getMarketsWithPhase(db).where(queryData);
+
+  query = query.orderBy(sortBy || "volume", sortDirection(isSortDescending, "desc"));
+  if (limit != null) query = query.limit(limit);
+  if (offset != null) query = query.offset(offset);
 
   // TODO: should we also consider a market_state's phase IS NULL?
-  getMarketsWithPhase(db).where(queryData).asCallback((err?: Error|null, marketsRows?: Array<MarketsRow>): void => {
+  query.asCallback((err?: Error|null, marketsRows?: Array<MarketsRow>): void => {
     if (err) return callback(err);
     if (!marketsRows) return callback(null);
     const marketsInfo: UIMarketsInfo = marketsRows.map((marketsRow) => reshapeMarketsRowToUIMarketInfo(marketsRow, []));
