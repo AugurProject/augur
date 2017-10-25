c55ec867a21d29060fc701bb3e93711e373afaf7
Paul Gebheim <paul@onetaste.us>
2017-10-25 12:12:58 -0700

    Simplify error handling logic

--- src/augur-node/dispatch-json-rpc-response.js
+++ src/augur-node/dispatch-json-rpc-response.js
@@ -6,18 +6,20 @@ var isObject = require("../utils/is-object");
 
 function dispatchJsonRpcResponse(err, jsonRpcResponse) {
   if (err) throw err;
-  if (!jsonRpcResponse || !isObject(jsonRpcResponse) || jsonRpcResponse.id === undefined || (jsonRpcResponse.id === null && (jsonRpcResponse.result === null && jsonRpcResponse.result.subscription === undefined))) {
+  if (!jsonRpcResponse || !isObject(jsonRpcResponse) || jsonRpcResponse.id === undefined ) {
     throw new Error("Bad JSON RPC response received:" + JSON.stringify(jsonRpcResponse));
   }
+
   var callback, result;
   if (jsonRpcResponse.id !== null) {
     callback = augurNodeState.getCallback(jsonRpcResponse.id);
     augurNodeState.removeCallback(jsonRpcResponse.id);
     result = jsonRpcResponse.result;
-  } else if (jsonRpcResponse.result.subscription !== null) {
+  } else if (jsonRpcResponse.result.subscription) {
     callback = augurNodeState.getCallback("event:" + jsonRpcResponse.result.subscription);
     result = jsonRpcResponse.result.result;
-    console.log(callback, result);
+  } else {
+    throw new Error("Bad JSON RPC response received:" + JSON.stringify(jsonRpcResponse));
   }
 
   if (!isFunction(callback)) {
