728fe928fb39e4e697088df32983519ebba9ae76
Jack Peterson <jack@tinybike.net>
2017-10-25 11:38:35 -0700

    Added process log removal callsites (important detail...)

--- src/blockchain/log-processors/index.ts
+++ src/blockchain/log-processors/index.ts
@@ -1,18 +1,18 @@
 import { LogProcessor } from "../../types";
-import { processMarketCreatedLog } from "./market-created";
-import { processTokensTransferredLog } from "./tokens-transferred";
-import { processOrderCanceledLog } from "./order-canceled";
-import { processOrderCreatedLog } from "./order-created";
-import { processOrderFilledLog } from "./order-filled";
-import { processProceedsClaimedLog } from "./proceeds-claimed";
-import { processDesignatedReportSubmittedLog } from "./designated-report-submitted";
-import { processReportSubmittedLog } from "./report-submitted";
-import { processWinningTokensRedeemedLog } from "./winning-tokens-redeemed";
-import { processReportsDisputedLog } from "./reports-disputed";
-import { processMarketFinalizedLog } from "./market-finalized";
-import { processUniverseForkedLog } from "./universe-forked";
-import { processTransferLog } from "./legacy-rep-contract/transfer";
-import { processApprovalLog } from "./legacy-rep-contract/approval";
+import { processMarketCreatedLog, processMarketCreatedLogRemoval } from "./market-created";
+import { processTokensTransferredLog, processTokensTransferredLogRemoval } from "./tokens-transferred";
+import { processOrderCanceledLog, processOrderCanceledLogRemoval } from "./order-canceled";
+import { processOrderCreatedLog, processOrderCreatedLogRemoval } from "./order-created";
+import { processOrderFilledLog, processOrderFilledLogRemoval } from "./order-filled";
+import { processProceedsClaimedLog, processProceedsClaimedLogRemoval } from "./proceeds-claimed";
+import { processDesignatedReportSubmittedLog, processDesignatedReportSubmittedLogRemoval } from "./designated-report-submitted";
+import { processReportSubmittedLog, processReportSubmittedLogRemoval } from "./report-submitted";
+import { processWinningTokensRedeemedLog, processWinningTokensRedeemedLogRemoval } from "./winning-tokens-redeemed";
+import { processReportsDisputedLog, processReportsDisputedLogRemoval } from "./reports-disputed";
+import { processMarketFinalizedLog, processMarketFinalizedLogRemoval } from "./market-finalized";
+import { processUniverseForkedLog, processUniverseForkedLogRemoval } from "./universe-forked";
+import { processTransferLog, processTransferLogRemoval } from "./legacy-rep-contract/transfer";
+import { processApprovalLog, processApprovalLogRemoval } from "./legacy-rep-contract/approval";
 
 export const logProcessors: {[contractName: string]: {[eventName: string]: LogProcessor}} = {
   Augur: {
@@ -32,5 +32,26 @@ export const logProcessors: {[contractName: string]: {[eventName: string]: LogPr
   LegacyRepContract: {
     Transfer: processTransferLog,
     Approval: processApprovalLog,
+  },
+};
+
+export const logRemovalProcessors: {[contractName: string]: {[eventName: string]: LogProcessor}} = {
+  Augur: {
+    MarketCreated: processMarketCreatedLogRemoval,
+    TokensTransferred: processTokensTransferredLogRemoval,
+    OrderCanceled: processOrderCanceledLogRemoval,
+    OrderCreated: processOrderCreatedLogRemoval,
+    OrderFilled: processOrderFilledLogRemoval,
+    ProceedsClaimed: processProceedsClaimedLogRemoval,
+    DesignatedReportSubmitted: processDesignatedReportSubmittedLogRemoval,
+    ReportSubmitted: processReportSubmittedLogRemoval,
+    WinningTokensRedeemed: processWinningTokensRedeemedLogRemoval,
+    ReportsDisputed: processReportsDisputedLogRemoval,
+    MarketFinalized: processMarketFinalizedLogRemoval,
+    UniverseForked: processUniverseForkedLogRemoval,
+  },
+  LegacyRepContract: {
+    Transfer: processTransferLogRemoval,
+    Approval: processApprovalLogRemoval,
   },
 };
--- src/blockchain/process-logs.ts
+++ src/blockchain/process-logs.ts
@@ -3,6 +3,6 @@ import { each } from "async";
 import * as Knex from "knex";
 import { FormattedLog, LogProcessor, ErrorCallback } from "../types";
 
-export function processLogs(db: Knex, augur: Augur, trx: Knex.Transaction, logs: Array<FormattedLog>, processLog: LogProcessor, callback: ErrorCallback): void {
-  each(logs, (log: FormattedLog, nextLog: ErrorCallback) => processLog(db, augur, trx, log, nextLog), callback);
+export function processLogs(db: Knex, augur: Augur, trx: Knex.Transaction, logs: Array<FormattedLog>, processLog: LogProcessor, processLogRemoval: LogProcessor, callback: ErrorCallback): void {
+  each(logs, (log: FormattedLog, nextLog: ErrorCallback) => !log.removed ? processLog(db, augur, trx, log, nextLog) : processLogRemoval(db, augur, trx, log, nextLog), callback);
 }
