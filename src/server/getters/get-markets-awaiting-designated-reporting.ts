78c8748b2a4bfb4161c04cbfbe40beb9052bec03
Scott Bigelow <epheph@gmail.com>
2017-10-24 20:42:52 -0700

    @tinybike feedback using querybuilder

--- src/server/getters/get-markets-awaiting-designated-reporting.ts
+++ src/server/getters/get-markets-awaiting-designated-reporting.ts
@@ -7,13 +7,15 @@ import { sortDirection } from "../../utils/sort-direction";
 // Look up all markets that are currently awaiting designated (automated) reporting.
 // Should accept a designatedReporterAddress parameter that filters by designated reporter address.
 export function getMarketsAwaitingDesignatedReporting(db: Knex, designatedReporter: Address|null, sortBy: string|null|undefined, isSortDescending: boolean|null|undefined, limit: number|null|undefined, offset: number|null|undefined, callback: (err?: Error|null, result?: any) => void): void {
-  const queryData: {} = (designatedReporter != null) ? { designatedReporter } : {};
-  let query = getMarketsWithReportingState(db).where(queryData).whereNull("markets.marketStateID");
+
+  // TODO: NULL isn't right here, we're going to an enumerated list to match the contracts
+  let query = getMarketsWithReportingState(db).whereNull("markets.marketStateID");
+  if (designatedReporter != null) query = query.where({ designatedReporter });
+
   query = query.orderBy(sortBy || "endTime", sortDirection(isSortDescending, "desc"));
   if (limit != null) query = query.limit(limit);
   if (offset != null) query = query.offset(offset);
-  // TODO: should we also consider a market_state's phase IS NULL?
-  query.asCallback((err?: Error|null, marketsRows?: Array<any>): void => {
+  query.asCallback((err?: Error|null, marketsRows?: Array<MarketsRow>): void => {
     if (err) return callback(err);
     if (!marketsRows || !marketsRows.length) return callback(null);
     const marketsInfo: UIMarketsInfo = marketsRows.map((marketsRow: any): UIMarketInfo => reshapeMarketsRowToUIMarketInfo(marketsRow, []));
--- src/server/getters/get-markets-awaiting-reporting.ts
+++ src/server/getters/get-markets-awaiting-reporting.ts
@@ -5,12 +5,10 @@ import { sortDirection } from "../../utils/sort-direction";
 
 // Look up all markets that are currently available for limited reporting.
 // Must be able to sort by number of reports submitted for each market so far, and the response should include the number of reports already submitted -- as well as the payoutNumerator values of each of the reports + the amount staked on each -- as part of the response.
-export function getMarketsAwaitingReporting(db: Knex, reportingWindow: Address|null, reportingRound: number|null, sortBy: string|null|undefined, isSortDescending: boolean|null|undefined, limit: number|null|undefined, offset: number|null|undefined, callback: (err: Error|null, result?: any) => void): void {
-  const queryData: { [id: string]: any } = (reportingRound != null) ? { "market_state.round:": reportingRound } : {};
-  if ( reportingWindow != null ) {
-    queryData["markets.reportingWindow"] = reportingWindow;
-  }
-  let query = getMarketsWithReportingState(db).where(queryData);
+export function getMarketsAwaitingReporting(db: Knex, reportingWindow: Address|null, reportingState: number|null, sortBy: string|null|undefined, isSortDescending: boolean|null|undefined, limit: number|null|undefined, offset: number|null|undefined, callback: (err: Error|null, result?: any) => void): void {
+  let query = getMarketsWithReportingState(db);
+  if (reportingWindow != null) query = query.where({ reportingWindow });
+  if (reportingState != null) query = query.where({ reportingState });
 
   query = query.orderBy(sortBy || "volume", sortDirection(isSortDescending, "desc"));
   if (limit != null) query = query.limit(limit);
