6b10aa51b5854c76e662c9fcace4fed879ddd5d0
Jack Peterson <jack@tinybike.net>
2017-10-26 19:09:22 -0700

    Added legacy rep contract Transfer and Approval tests

---
+++ test/blockchain/log-processors/legacy-rep-contract/transfer.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const assert = require("chai").assert;
+const setupTestDb = require("../../../test.database");
+const { processTransferLog, processTransferLogRemoval } = require("../../../../build/blockchain/log-processors/legacy-rep-contract/transfer");
+
+describe("blockchain/log-processors/legacy-rep-contract/transfer", () => {
+  const test = (t) => {
+    const getState = (db, params, callback) => db("transfers").where({ transactionHash: params.log.transactionHash, logIndex: params.log.logIndex }).asCallback(callback);
+    it(t.description, (done) => {
+      setupTestDb((err, db) => {
+        assert.isNull(err);
+        db.transaction((trx) => {
+          processTransferLog(db, t.params.augur, trx, t.params.log, (err) => {
+            assert.isNull(err);
+            getState(trx, t.params, (err, records) => {
+              t.assertions.onAdded(err, records);
+              processTransferLogRemoval(db, t.params.augur, trx, t.params.log, (err) => {
+                getState(trx, t.params, (err, records) => {
+                  t.assertions.onRemoved(err, records);
+                  done();
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  };
+  test({
+    description: "LegacyRepContract Transfer log and removal",
+    params: {
+      log: {
+        transactionHash: "TRANSACTION_HASH",
+        logIndex: 0,
+        from: "FROM_ADDRESS",
+        to: "TO_ADDRESS",
+        address: "LEGACY_REP_CONTRACT_ADDRESS",
+        value: "9000",
+        blockNumber: 1400101,
+      },
+      augur: {},
+    },
+    assertions: {
+      onAdded: (err, records) => {
+        assert.isNull(err);
+        assert.deepEqual(records, [{
+          transactionHash: "TRANSACTION_HASH",
+          logIndex: 0,
+          sender: "FROM_ADDRESS",
+          recipient: "TO_ADDRESS",
+          token: "LEGACY_REP_CONTRACT_ADDRESS",
+          value: 9000,
+          blockNumber: 1400101,
+        }]);
+      },
+      onRemoved: (err, records) => {
+        assert.isNull(err);
+        assert.deepEqual(records, []);
+      },
+    },
+  });
+});
