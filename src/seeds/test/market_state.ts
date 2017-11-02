3f27c3e9d8b7f72d58a6211a6ed157271ad4d409
Scott Bigelow <epheph@gmail.com>
2017-11-02 16:34:44 -0700

    lint

--- src/seeds/test/market_state.ts
+++ src/seeds/test/market_state.ts
@@ -9,17 +9,17 @@ exports.seed = async (knex: Knex): Promise<any> => {
       marketID: "0x0000000000000000000000000000000000000001",
       reportingState: "DESIGNATED_REPORTING",
       blockNumber: 1400000,
-    },{
+    }, {
       marketStateID: 2,
       marketID: "0x0000000000000000000000000000000000000002",
       reportingState: "DESIGNATED_REPORTING",
       blockNumber: 1400001,
-    },{
+    }, {
       marketStateID: 3,
       marketID: "0x0000000000000000000000000000000000000003",
       reportingState: "DESIGNATED_REPORTING",
       blockNumber: 1400002,
-    },{
+    }, {
       marketStateID: 4,
       marketID: "0x0000000000000000000000000000000000000011",
       reportingState: "FIRST_REPORTING",
