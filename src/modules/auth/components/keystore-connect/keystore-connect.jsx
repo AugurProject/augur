8e5f2cb3f6b6b3ebb98ace45c005f7469c6cab41
Stephen Sprinkle <stephen@sprinkle.io>
2017-11-03 10:51:41 -0700

    linting

--- src/modules/auth/components/keystore-connect/keystore-connect.jsx
+++ src/modules/auth/components/keystore-connect/keystore-connect.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import classNames from 'classnames'
 
 import { Export } from 'modules/common/components/icons/icons'
 
