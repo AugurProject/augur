1d6628609598f8876ac55164aa1d2329e24ac28b
Scott Bigelow <epheph@gmail.com>
2017-11-03 22:21:55 -0700

    lint

--- src/server/getters/get-user-trading-positions.ts
+++ src/server/getters/get-user-trading-positions.ts
@@ -7,7 +7,7 @@ import { queryModifier } from "./database";
 export function getUserTradingPositions(db: Knex, universe: Address|null, account: Address, marketID: Address|null|undefined, outcome: number|null|undefined, sortBy: string|null|undefined, isSortDescending: boolean|null|undefined, limit: number|null|undefined, offset: number|null|undefined, callback: (err: Error|null, result?: any) => void): void {
   if (universe == null && marketID == null ) return callback(new Error("Must provide reference to universe, specify universe or marketID"));
   let query = db.select(["positions.marketID", "outcome", "numShares", "numSharesAdjustedForUserIntention", "realizedProfitLoss", "unrealizedProfitLoss"]).from("positions").where({ account });
-  if (universe != null) query = query.join("markets", "markets.marketID","positions.marketID" ).where({ universe });
+  if (universe != null) query = query.join("markets", "markets.marketID", "positions.marketID" ).where({ universe });
   if (marketID != null) query = query.where({ marketID });
   if (outcome != null) query = query.where({ outcome });
   query = queryModifier(query, "outcome", "asc", sortBy, isSortDescending, limit, offset);
