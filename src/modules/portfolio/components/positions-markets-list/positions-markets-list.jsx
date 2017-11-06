c9ef73d2b3de98fe1761ca3d37cabbe8f3c5aea8
John Danz <John.Danz87@gmail.com>
2017-11-06 15:42:28 -0600

    removed overcomplicated filter drop down function

--- src/modules/portfolio/components/positions-markets-list/positions-markets-list.jsx
+++ src/modules/portfolio/components/positions-markets-list/positions-markets-list.jsx
@@ -6,37 +6,6 @@ import MarketPortfolioCard from 'modules/market/components/market-portfolio-card
 
 import Styles from 'modules/portfolio/components/positions-markets-list/positions-markets-list.styles'
 
-function createFilterObject(markets) {
-  // Used to generating the filter based on the markets passed tags. will only add each tag one time.
-  let filterType = 'none'
-  let defaultFilterType = 'none'
-  const filterOptions = []
-
-  markets.forEach((market) => {
-    market.tags.forEach((tag) => {
-      let taken = false
-      filterOptions.forEach((filter) => {
-        if (filter.value === tag) taken = true
-      })
-      // push a new tag/filter option to the filters Options
-      if (!taken) {
-        filterOptions.push({ label: tag, value: tag })
-      }
-      // as soon as we have a tag, replace none
-      if (defaultFilterType === 'none' && filterOptions.length > 0) {
-        defaultFilterType = tag
-        filterType = tag
-      }
-    })
-  })
-
-  return {
-    filterType,
-    defaultFilterType,
-    filterOptions
-  }
-}
-
 class PositionsMarketsList extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
@@ -52,11 +21,7 @@ class PositionsMarketsList extends Component {
 
   constructor(props) {
     super(props)
-    // NOTE: didn't make this a container because it is reused in 3 different ways, seemed easier to seperate markets in positions and pass market arrays and filter categories
-
-    // use the function above this class to calculate the filters for this group of markets
-    const filter = createFilterObject(this.props.markets)
-
+    // NOTE: didn't make this component a container because it is reused in 3 different ways, seemed easier to seperate markets in positions and pass market arrays and filter categories
     this.state = {
       sortType: 'volume',
       defaultSortType: 'volume',
@@ -66,9 +31,14 @@ class PositionsMarketsList extends Component {
         { label: 'Expiring Soon', value: 'expiring' },
         { label: 'Fees', value: 'fees' }
       ],
-      filterType: filter.filterType,
-      defaultFilterType: filter.defaultFilterType,
-      filterOptions: filter.filterOptions
+      filterType: 'cryptocurrency',
+      defaultFilterType: 'cryptocurrency',
+      filterOptions: [
+        { label: 'Cryptocurrency', value: 'cryptocurrency' },
+        { label: 'Blockchain', value: 'blockchain' },
+        { label: 'Bitcoin', value: 'bitcoin' },
+        { label: 'Ethereum', value: 'ethereum' }
+      ]
     }
 
     this.changeDropdown = this.changeDropdown.bind(this)
