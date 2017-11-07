cfa7d53c53a5f505ebde529aa751b55e3186e739
Stephen Sprinkle <stephen@sprinkle.io>
2017-11-07 10:55:25 -0800

    stake

--- src/modules/auth/components/ledger-connect/ledger-connect.jsx
+++ src/modules/auth/components/ledger-connect/ledger-connect.jsx
@@ -12,11 +12,17 @@ export default class Ledger extends Component {
       ENABLE_CONTRACT_SUPPORT: 'ENABLE_CONTRACT_SUPPORT'
     }
 
+    this.LedgerEthereum = null;
+
     this.state = {
       ledgerState: null
     }
 
     this.setupLedger = this.setupLedger.bind(this)
+    this.onConnectLedgerRequest = this.onConnectLedgerRequest.bind(this)
+    this.onOpenEthereumAppRequest = this.onOpenEthereumAppRequest.bind(this)
+    this.onSwitchLedgerModeRequest = this.onSwitchLedgerModeRequest.bind(this)
+    this.onEnableContractSupportRequest = this.onEnableContractSupportRequest.bind(this)
   }
 
   componentWillMount() {
@@ -24,70 +30,24 @@ export default class Ledger extends Component {
   }
 
   async setupLedger() {
-    console.log('setup!')
-
-    const onConnectLedgerRequest = async () => {
-      await new Promise(resolve => this.setState(
-        {
-          ledgerState: this.LEDGER_STATES.CONNECT_LEDGER
-        },
-        () => {
-          console.log('connect ledger')
-          resolve()
-        }
-      ))
-    }
-
-    const onOpenEthereumAppRequest = async () => {
-      await new Promise(resolve => this.setState(
-        {
-          ledgerState: this.LEDGER_STATES.OPEN_APP
-        },
-        () => {
-          console.log('open app')
-          resolve()
-        }
-      ))
-    }
-
-    const onSwitchLedgerModeRequest = async () => {
-      await new Promise(resolve => this.setState(
-        {
-          ledgerState: this.LEDGER_STATES.SWITCH_MODE
-        },
-        () => {
-          console.log('switch mode')
-          resolve()
-        }
-      ))
-    }
-
-    const onEnableContractSupportRequest = async () => {
-      await new Promise(resolve => this.setState(
-        {
-          ledgerState: this.LEDGER_STATES.ENABLE_CONTRACT_SUPPORT
-        },
-        () => {
-          console.log('enable contract support')
-          resolve()
-        }
-      ))
-    }
-
-    const ledgerEthereum = new LedgerEthereum(
-      Network.Main,
-      BrowserLedgerConnectionFactory,
-      onConnectLedgerRequest,
-      onOpenEthereumAppRequest,
-      onSwitchLedgerModeRequest,
-      onEnableContractSupportRequest
-    )
-
-    console.log('ledgerEthereum -- ', ledgerEthereum)
-
-    const address = await ledgerEthereum.getAddressByBip44Index(0)
-
-    console.log('address -- ', address)
+    // console.log('setup!')
+    //
+    //
+    //
+    // this.ledgerEthereum = new LedgerEthereum(
+    //   Network.Main,
+    //   BrowserLedgerConnectionFactory,
+    //   this.onConnectLedgerRequest,
+    //   this.onOpenEthereumAppRequest,
+    //   this.onSwitchLedgerModeRequest,
+    //   this.onEnableContractSupportRequest
+    // )
+    //
+    // console.log('ledgerEthereum -- ', ledgerEthereum)
+    //
+    // const address = await ledgerEthereum.getAddressByBip44Index(0)
+    //
+    // console.log('address -- ', address)
     // const firstSignedMessagePromise = ledgerEthereum.signTransactionByBip44Index("e8018504e3b292008252089428ee52a8f3d6e5d15f8b131996950d7f296c7952872bd72a2487400080", 7)
 	  // const secondSignedMessagePromise = ledgerEthereum.signTransactionByBip32Path("e8018504e3b292008252089428ee52a8f3d6e5d15f8b131996950d7f296c7952872bd72a2487400080", "m/44'/60'/0'/0/7")
 
@@ -101,13 +61,75 @@ export default class Ledger extends Component {
   	// assert.equal(firstSignedMessage, secondSignedMessage);
   }
 
+  async onConnectLedgerRequest() {
+    await new Promise(resolve => this.setState(
+      {
+        ledgerState: this.LEDGER_STATES.CONNECT_LEDGER
+      },
+      () => {
+        console.log('connect ledger')
+        resolve()
+      }
+    ))
+  }
+
+  async onOpenEthereumAppRequest() {
+    await new Promise(resolve => this.setState(
+      {
+        ledgerState: this.LEDGER_STATES.OPEN_APP
+      },
+      () => {
+        console.log('open app')
+        resolve()
+      }
+    ))
+  }
+
+  async onSwitchLedgerModeRequest() {
+    await new Promise(resolve => this.setState(
+      {
+        ledgerState: this.LEDGER_STATES.SWITCH_MODE
+      },
+      () => {
+        console.log('switch mode')
+        resolve()
+      }
+    ))
+  }
+
+  async onEnableContractSupportRequest() {
+    await new Promise(resolve => this.setState(
+      {
+        ledgerState: this.LEDGER_STATES.ENABLE_CONTRACT_SUPPORT
+      },
+      () => {
+        console.log('enable contract support')
+        resolve()
+      }
+    ))
+  }
+
   render() {
     const s = this.state
 
     console.log('state -- ', s)
 
     return (
-      <span>Ledger</span>
+      <button
+        onClick={() => {
+          new LedgerEthereum(
+            Network.Main,
+            BrowserLedgerConnectionFactory,
+            this.onConnectLedgerRequest,
+            this.onOpenEthereumAppRequest,
+            this.onSwitchLedgerModeRequest,
+            this.onEnableContractSupportRequest
+          ).getAddressByBip44Index(0)
+        }
+      }
+      >
+        CHECK
+      </button>
     )
   }
 }
