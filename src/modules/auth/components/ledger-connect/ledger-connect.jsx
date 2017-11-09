d4117f99fe9a2f5dbc14d5d69429b00c2a3ede58
Stephen Sprinkle <stephen@sprinkle.io>
2017-11-09 15:18:49 -0800

    rollback to failing state

--- src/modules/auth/components/ledger-connect/ledger-connect.jsx
+++ src/modules/auth/components/ledger-connect/ledger-connect.jsx
@@ -26,7 +26,7 @@ export default class Ledger extends Component {
   }
 
   componentWillMount() {
-    this.setupLedger()
+    // this.setupLedger()
   }
 
   async onConnectLedgerRequest() {
@@ -103,13 +103,13 @@ export default class Ledger extends Component {
   //   // const secondSignedMessagePromise = ledgerEthereum.signTransactionByBip32Path("e8018504e3b292008252089428ee52a8f3d6e5d15f8b131996950d7f296c7952872bd72a2487400080", "m/44'/60'/0'/0/7")
   //
   //   // this will block until both first and second messages are done because the library handles ordering internally
-  //   // const secondSignedMessage = await secondSignedMessage;
+  //   // const secondSignedMessage = await secondSignedMessage
   //
   //   // if the ledger isn't connected with the Ethereum app open in browser mode, the on*Request callbacks above will be called before the signing promises return
-  //   // const firstSignedMessage = await firstSignedMessage;
+  //   // const firstSignedMessage = await firstSignedMessage
   //
-  //   // BIP44 index 7 is the same as `m/44'/60'/0'/0/7`; it is strongly recommended to use index 0 if you don't support multi-address wallets
-  //   // assert.equal(firstSignedMessage, secondSignedMessage);
+  //   // BIP44 index 7 is the same as `m/44'/60'/0'/0/7` it is strongly recommended to use index 0 if you don't support multi-address wallets
+  //   // assert.equal(firstSignedMessage, secondSignedMessage)
   // }
 
   render() {
@@ -120,14 +120,11 @@ export default class Ledger extends Component {
     return (
       <button
         onClick={() => {
-          new LedgerEthereum(
-            Network.Main,
-            BrowserLedgerConnectionFactory,
-            this.onConnectLedgerRequest,
-            this.onOpenEthereumAppRequest,
-            this.onSwitchLedgerModeRequest,
-            this.onEnableContractSupportRequest
-          ).getAddressByBip44Index(0)
+          doStuff().then(() => {
+            console.log('done')
+          }).catch((error) => {
+            console.log(error)
+          })
         }
       }
       >
@@ -135,4 +132,21 @@ export default class Ledger extends Component {
       </button>
     )
   }
+}
+
+async function doStuff() {
+  const onConnectLedgerRequest = async () => { console.log('onConnectLedgerRequest') }
+  const onOpenEthereumAppRequest = async () => { console.log('onOpenEthereumAppRequest') }
+  const onSwitchLedgerModeRequest = async () => { console.log('onSwitchLedgerModeRequest') }
+  const onEnableContractSupportRequest = async () => { console.log('onEnableContractSupportRequest') }
+
+  const ledgerEthereum = new LedgerEthereum(Network.Main, BrowserLedgerConnectionFactory, onConnectLedgerRequest, onOpenEthereumAppRequest, onSwitchLedgerModeRequest, onEnableContractSupportRequest)
+  const address = await ledgerEthereum.getAddressByBip44Index(0)
+  console.log(address)
+  const firstSignedMessagePromise = ledgerEthereum.signTransactionByBip44Index('e8018504e3b292008252089428ee52a8f3d6e5d15f8b131996950d7f296c7952872bd72a2487400080', 7)
+  const secondSignedMessagePromise = ledgerEthereum.signTransactionByBip32Path('e8018504e3b292008252089428ee52a8f3d6e5d15f8b131996950d7f296c7952872bd72a2487400080', "m/44'/60'/0'/0/7")
+  const firstSignedMessage = await firstSignedMessagePromise
+  const secondSignedMessage = await secondSignedMessagePromise
+  console.log(firstSignedMessage)
+  console.log(secondSignedMessage)
 }
