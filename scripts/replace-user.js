e24c02e241223a5f46d8afc01514a05f1b9e141e
Tom Haile <bradford.haile@gmail.com>
2017-11-10 16:14:10 -0600

    added comment to script and move replace dep to dev dep.

--- scripts/replace-user.js
+++ scripts/replace-user.js
@@ -1,22 +1,26 @@
 #!/usr/bin/env node
+
 "use strict";
 const replace = require('replace-in-file');
+/*
+	usage: node replace-user <hash value>
+	run in augur-node root dir
+*/
 
-if ( process.argv.length < 3 ) throw new Error('Pass in user hash as arguments');
+if (process.argv.length < 3) throw new Error('Pass in user hash as arguments');
 
-process.argv.slice(2).map(function(user_hash) {
+process.argv.slice(2).map(function (user_hash) {
 	const options = {
-	  files: 'src/seeds/test/*',
-	  from: /0x0000000000000000000000000000000000000b0b/g,
-	  to: user_hash,
+		files: 'src/seeds/test/*',
+		from: /0x0000000000000000000000000000000000000b0b/g,
+		to: user_hash,
 	};
 
 	try {
-	  const changes = replace.sync(options);
-	  console.log('Modified files:', changes.join(', '));
-	}
-	catch (error) {
-	  console.error('Error occurred:', error);
+		const changes = replace.sync(options);
+		console.log('Modified files:', changes.join(', '));
+	} catch (error) {
+		console.error('Error occurred:', error);
 	}
 
 });
