a2bf54440a8e12e2519e6cee350258971bc52549
Scott Bigelow <epheph@gmail.com>
2017-11-10 15:27:45 -0800

    lint

--- src/server/getters/get-unfinalized-stake-tokens.ts
+++ src/server/getters/get-unfinalized-stake-tokens.ts
@@ -3,12 +3,11 @@ import * as _ from "lodash";
 import { Address, ReportingState, StakeTokensRowWithReportingState } from "../../types";
 import { getMarketsWithReportingState } from "./database";
 
-
 // Input: User Address
 // Output: Unfinalized Stake Tokens
 export function getUnfinalizedStakeTokens(db: Knex, universe: Address, account: Address, callback: (err: Error|null, result?: any) => void): void {
-    const query: Knex.QueryBuilder = getMarketsWithReportingState(db, ["stake_tokens.*", "reports.amountStaked", "market_state.reportingState"]).from("stake_tokens")
-    query.join("markets", "markets.marketID", "stake_tokens.marketID").join("reports","reports.stakeToken","stake_tokens.stakeToken");
+    const query: Knex.QueryBuilder = getMarketsWithReportingState(db, ["stake_tokens.*", "reports.amountStaked", "market_state.reportingState"]).from("stake_tokens");
+    query.join("markets", "markets.marketID", "stake_tokens.marketID").join("reports", "reports.stakeToken", "stake_tokens.stakeToken");
     query.whereNot("market_state.reportingState", ReportingState.FINALIZED);
     query.where("universe", universe).where("reports.reporter", account);
     query.asCallback((err: Error|null, unfinalizedStakeTokens?: Array<StakeTokensRowWithReportingState>): void => {
