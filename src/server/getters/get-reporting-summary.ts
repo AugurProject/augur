285016de8a958b6ac8fa8de0fe87c40ef7f16e17
Scott Bigelow <epheph@gmail.com>
2017-11-09 17:11:59 -0800

    unused import

--- src/server/getters/get-reporting-summary.ts
+++ src/server/getters/get-reporting-summary.ts
@@ -1,6 +1,5 @@
 import * as Knex from "knex";
 import { Address } from "../../types";
-import { sortDirection } from "../../utils/sort-direction";
 import { getMarketsWithReportingState } from "./database";
 
 // Look up reporting summary values. Should take reportingWindow (address) as a parameter and the response should include total number of markets up for reporting, total number of markets up for dispute, total number of markets undergoing and/or resolved via each reporting "tier" (automated, limited, full, fork), etc.
