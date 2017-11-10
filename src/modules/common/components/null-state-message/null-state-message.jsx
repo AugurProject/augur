230a029ae9b59a81ed4d8bd47c188f4813633935
Serena Randolph <serena.randolph@gmail.com>
2017-11-10 15:03:31 -0800

    Adjusting null state styles and moving component file to own folder.

--- src/modules/common/components/null-state-message/null-state-message.jsx
+++ src/modules/common/components/null-state-message/null-state-message.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import classNames from 'classnames'
+
+import Styles from 'modules/common/components/null-state-message/null-state-message.styles'
 
 const NullStateMessage = p => (
-  <article className="null-state-message">
+  <article className={classNames(p.className, Styles.NullState)}>
     {!p.message ?
       <span>No Data Available</span> :
       <span>{p.message}</span>
--- src/modules/my-positions/components/my-positions-market.jsx
+++ src/modules/my-positions/components/my-positions-market.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 import ComponentNav from 'modules/common/components/component-nav'
 import ValueDenomination from 'modules/common/components/value-denomination/value-denomination'
-import NullStateMessage from 'modules/common/components/null-state-message'
+import NullStateMessage from 'modules/common/components/null-state-message/null-state-message'
 
 import MyPosition from 'modules/my-positions/components/my-position'
 import MyPositionOverview from 'modules/my-positions/components/my-position-overview'
