b4accfc7371cf36cd302d579dbcd1e203c84eb38
John Danz <John.Danz87@gmail.com>
2017-11-11 18:28:38 -0600

    updated unit tests and removed some checks for ethTokens in the balances selector.

--- src/modules/auth/selectors/balances.js
+++ src/modules/auth/selectors/balances.js
@@ -1,17 +1,18 @@
 import { isZero } from 'utils/math'
 
 export const anyAccountBalancesZero = balances => (
-  (balances.ethTokens !== undefined && isZero(balances.ethTokens)) ||
   (balances.eth !== undefined && isZero(balances.eth)) ||
   (balances.rep !== undefined && isZero(balances.rep))
 )
 
 export const allAccountBalancesZero = balances => (
-  (balances.ethTokens !== undefined && isZero(balances.ethTokens)) &&
   (balances.eth !== undefined && isZero(balances.eth)) &&
   (balances.rep !== undefined && isZero(balances.rep))
 )
 
 export const allAssetsLoaded = assets => (
-    assets.ethTokens !== undefined && assets.eth !== undefined && assets.rep !== undefined
+    assets.eth !== undefined && assets.rep !== undefined
 )
+// assets.ethTokens !== undefined &&
+// (balances.ethTokens !== undefined && isZero(balances.ethTokens)) &&
+// (balances.ethTokens !== undefined && isZero(balances.ethTokens)) ||
