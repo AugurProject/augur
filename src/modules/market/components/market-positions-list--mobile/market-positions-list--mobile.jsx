8dc98141b984921cd9e2b62ade905d1fe35f1ec5
Serena Randolph <serena.randolph@gmail.com>
2017-11-13 11:14:29 -0800

    Mobile close order messaging.

--- src/modules/market/components/market-positions-list--mobile/market-positions-list--mobile.jsx
+++ src/modules/market/components/market-positions-list--mobile/market-positions-list--mobile.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
 import MobilePositions from 'modules/market/components/market-positions-list--mobile-positions/market-positions-list--mobile-positions'
-import MobileOrders from 'modules/market/components/market-positions-list--mobile-orders/market-positions-list--mobile-orders'
+import MobileOrder from 'modules/market/components/market-positions-list--mobile-order/market-positions-list--mobile-order'
 import MobileStats from 'modules/market/components/market-positions-list--mobile-stats/market-positions-list--mobile-stats'
 
 import Styles from 'modules/market/components/market-positions-list--mobile/market-positions-list--mobile.styles'
@@ -17,9 +17,14 @@ const MarketPositionsListMobile = p => (
       />
     }
     { p.openOrders.length > 0 &&
-      <MobileOrders
-        openOrders={p.openOrders}
-      />
+      <div className={Styles['MarketPositionsListMobile__wrapper--orders']}>
+        <h2>Open Orders</h2>
+        <div className={Styles.MarketPositionsListMobile__orders}>
+          { p.openOrders.map((order, i) => (
+            <MobileOrder key={i} order={order} />
+          )) }
+        </div>
+      </div>
     }
     <MobileStats
       outcome={p.outcome}
