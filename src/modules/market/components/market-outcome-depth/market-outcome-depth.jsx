4526c9ba99fccf08e0fe106abc31d5134815f972
Stephen Sprinkle <stephen@sprinkle.io>
2017-11-13 13:30:41 -0800

    depth lines

--- src/modules/market/components/market-outcome-depth/market-outcome-depth.jsx
+++ src/modules/market/components/market-outcome-depth/market-outcome-depth.jsx
@@ -46,6 +46,8 @@ export default class MarketOutcomeDepth extends Component {
 
       const marketDepth = this.props.marketDepth
 
+      console.log('marketDepth -- ', marketDepth)
+
       const margin = {
         top: 0,
         right: 0,
@@ -63,70 +65,31 @@ export default class MarketOutcomeDepth extends Component {
       chart.attr('width', width)
       chart.attr('height', height)
 
-      // const xDomain = priceHistory.reduce((p, dataPoint) => [...p, dataPoint.x], [])
-      // const yDomain = priceHistory.reduce((p, dataPoint) => [...p, dataPoint.high, dataPoint.low], [])
-      //
-      // const xScale = d3.scaleTime()
-      //   .domain(d3.extent(xDomain))
-      //   .range([margin.left, width - margin.right - 1])
-      //
-      // const yScale = d3.scaleLinear()
-      //   .domain(d3.extent(yDomain))
-      //   .range([height - margin.bottom, margin.top])
-      //
-      // chart.selectAll('line')
-      //   .data(new Array(4))
-      //   .enter()
-      //   .append('line')
-      //   .attr('class', 'tick-line')
-      //   .attr('x1', 0)
-      //   .attr('x2', width)
-      //   .attr('y1', (d, i) => ((height - margin.bottom) / 4) * i)
-      //   .attr('y2', (d, i) => ((height - margin.bottom) / 4) * i)
-      //
-      // chart.selectAll('text')
-      //   .data(new Array(4))
-      //   .enter()
-      //   .append('text')
-      //   .attr('class', 'tick-value')
-      //   .attr('x', 0)
-      //   .attr('y', (d, i) => ((height - margin.bottom) / 4) * i)
-      //   .attr('dy', margin.tickOffset)
-      //   .attr('dx', 0)
-      //   .text((d, i) => {
-      //     if (i) {
-      //       return parseFloat(yScale.invert(((height - margin.bottom) / 4) * i)).toFixed(2)
-      //     }
-      //   })
-      //
-      // chart.append('g')
-      //   .attr('class', 'outcomes-axis')
-      //   .attr('transform', `translate(0, ${height - margin.bottom})`)
-      //   .call(d3.axisBottom(xScale))
-      //
-      // // chart.append('g')
-      // //   .attr('class', 'outcomes-axis')
-      // //   .attr('transform', `translate(${margin.left}, 0)`)
-      // //   .call(d3.axisLeft(yScale))
-      //
-      // chart.selectAll('rect')
-      //   .data(priceHistory)
-      //   .enter().append('svg:rect')
-      //   .attr('x', d => xScale(d.x))
-      //   .attr('y', d => yScale(d3.max([d.open, d.close])))
-      //   .attr('height', d => yScale(d3.min([d.open, d.close])) - yScale(d3.max([d.open, d.close])))
-      //   .attr('width', d => (0.5 * (width - (2 * margin.stick))) / priceHistory.length)
-      //   .attr('class', d => d.close > d.open ? 'up-period' : 'down-period') // eslint-disable-line no-confusing-arrow
-      //
-      // chart.selectAll('line.stem')
-      //   .data(priceHistory)
-      //   .enter().append('svg:line')
-      //   .attr('class', 'stem')
-      //   .attr('x1', d => xScale(d.x) + (0.25 * ((width - (2 * margin.stick)) / priceHistory.length)))
-      //   .attr('x2', d => xScale(d.x) + (0.25 * ((width - (2 * margin.stick)) / priceHistory.length)))
-      //   .attr('y1', d => yScale(d.high))
-      //   .attr('y2', d => yScale(d.low))
-      //   .attr('class', d => d.close > d.open ? 'up-period' : 'down-period') // eslint-disable-line no-confusing-arrow
+      const xDomain = Object.keys(marketDepth).reduce((p, side) => [...p, ...marketDepth[side].reduce((p, item) => [...p, item[0]], [])], [])
+      const yDomain = Object.keys(marketDepth).reduce((p, side) => [...p, ...marketDepth[side].reduce((p, item) => [...p, item[1]], [])], [])
+
+      console.log('xDomain -- ', xDomain)
+
+      const xScale = d3.scaleTime()
+        .domain(d3.extent(xDomain))
+        .range([margin.left, width - margin.right - 1])
+
+      const yScale = d3.scaleLinear()
+        .domain(d3.extent(yDomain))
+        .range([height - margin.bottom, margin.top])
+
+      const depthLine = d3.line()
+        .x(d => xScale(d[0]))
+        .y(d => yScale(d[1]))
+
+      Object.keys(marketDepth).forEach((side) => {
+        chart.append('path')
+          .data([marketDepth[side]])
+          .attr('class', `depth-line outcome-line-${side}`)
+          .attr('d', depthLine)
+      })
+
+
 
       this.setState({ chart: fauxDiv.toReact() })
     }
