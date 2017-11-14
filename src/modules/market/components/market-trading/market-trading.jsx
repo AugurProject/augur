280531a5b2df83958d3ef12609862acaefe08899
Serena Randolph <serena.randolph@gmail.com>
2017-11-14 15:49:38 -0800

    Adding initial messages for trading form.

--- src/modules/market/components/market-trading/market-trading.jsx
+++ src/modules/market/components/market-trading/market-trading.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import { Link } from 'react-router-dom'
+
+import makePath from 'modules/routes/helpers/make-path'
+
+import getValue from 'utils/get-value'
+
+import { ACCOUNT_DEPOSIT } from 'modules/routes/constants/views'
 
 import Styles from 'modules/market/components/market-trading/market-trading.styles'
 
@@ -13,8 +20,36 @@ class MarketTrading extends Component {
   }
 
   render() {
+    const p = this.props
+
+    const hasFunds = getValue(p, 'market.tradeSummary.hasUserEnoughFunds')
+    const hasSelectedOutcome = p.selectedOutcomes.length > 0
+
+    let initialMessage = ''
+
+    switch(true) {
+      case !p.isLogged:
+        initialMessage = 'Log in to trade.'
+        break
+      case p.isLogged && !hasFunds:
+        initialMessage = 'Add funds to begin trading.'
+        break
+      case p.isLogged && hasFunds && !hasSelectedOutcome:
+        initialMessage = 'Select an outcome to begin placing an order.'
+        break
+      default:
+        initialMessage = false
+    }
+
     return (
-      <div>trading</div>
+      <section className={Styles.Trading}>
+        { initialMessage &&
+          <p className={Styles['Trading__initial-message']}>{ initialMessage }</p>
+        }
+        { p.isLogged && !hasFunds &&
+          <Link className={Styles['Trading__button--add-funds']} to={makePath(ACCOUNT_DEPOSIT)}>Add Funds</Link>
+        }
+      </section>
     )
   }
 }
--- src/modules/market/containers/market-trading.js
+++ src/modules/market/containers/market-trading.js
@@ -5,8 +5,18 @@ import MarketTrading from 'modules/market/components/market-trading/market-tradi
 
 const mapStateToProps = state => ({
   marketID: '0x7d9f26082539a7f9793b8c3b25f2a20374ab357d73ff6d6dc99cab6145b567a0',
+  market,
+  isLogged: state.isLogged,
 })
 
 const MarketTradingContainer = withRouter(connect(mapStateToProps)(MarketTrading))
 
 export default MarketTradingContainer
+
+const market = {
+  marketType: "categorical",
+  tradeSummary: {
+    hasUserEnoughFunds: true,
+  },
+  submitTrade: () => console.log('submit trade'),
+}
