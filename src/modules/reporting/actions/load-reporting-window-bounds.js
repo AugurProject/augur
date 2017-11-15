1855d6d9febf881e180593243ee178b0f7c5f842
Stephen Sprinkle <stephen@sprinkle.io>
2017-11-15 12:20:17 -0800

    notes for subsequent implementation

--- src/modules/reporting/actions/load-reporting-window-bounds.js
+++ src/modules/reporting/actions/load-reporting-window-bounds.js
@@ -11,3 +11,8 @@ export const loadReportingWindowBounds = () => (dispatch, getState) => {
   // TODO get endDate
   // TODO get stake (waiting on endpoint)
 }
+
+// > augur.api.Universe.getCurrentReportingWindow(console.log)
+// null '0x54d134699764375417e4b5dda1e2ac62f62e9725'
+// > augur.api.ReportingWindow.getStartTime({ tx: { to: "0x54d134699764375417e4b5dda1e2ac62f62e9725" } }, console.log)
+// null '1508544000'
