5af7479543b0d3bbca2f0c9e3d2a581ce48cbd88
Scott Bigelow <epheph@gmail.com>
2017-11-16 21:11:31 -0800

    convert create table to knex statements, use const query

--- src/server/getters/get-user-trading-history.ts
+++ src/server/getters/get-user-trading-history.ts
@@ -9,7 +9,7 @@ interface TradingHistoryRow extends Partial<TradesRow> {
 // Look up a user's trading history. Should take market, outcome, and orderType as optional parameters.
 export function getUserTradingHistory(db: Knex|Knex.Transaction, universe: Address|null, account: Address, marketID: Address|null|undefined, outcome: number|null|undefined, orderType: string|null|undefined, sortBy: string|null|undefined, isSortDescending: boolean|null|undefined, limit: number|null|undefined, offset: number|null|undefined, callback: (err: Error|null, result?: Array<UITrade>) => void): void {
   if (universe == null && marketID == null ) return callback(new Error("Must provide reference to universe, specify universe or marketID"));
-  let query = db.select([
+  const query = db.select([
     "trades.marketID",
     "trades.outcome",
     "trades.orderType",
@@ -22,11 +22,11 @@ export function getUserTradingHistory(db: Knex|Knex.Transaction, universe: Addre
     "trades.tradeGroupID",
     "blocks.timestamp",
   ]).from("trades").leftJoin("blocks", "trades.blockNumber", "blocks.blockNumber").where("trades.creator", account).orWhere("trades.filler", account);
-  if (universe != null) query = query.where("universe", universe);
-  if (marketID != null) query = query.where("trades.marketID", marketID);
-  if (outcome != null) query = query.where("trades.outcome", outcome);
-  if (orderType != null) query = query.where("trades.orderType", orderType);
-  query = queryModifier(query,  "trades.blockNumber", "desc", sortBy, isSortDescending, limit, offset);
+  if (universe != null) query.where("universe", universe);
+  if (marketID != null) query.where("trades.marketID", marketID);
+  if (outcome != null) query.where("trades.outcome", outcome);
+  if (orderType != null) query.where("trades.orderType", orderType);
+  queryModifier(query,  "trades.blockNumber", "desc", sortBy, isSortDescending, limit, offset);
   query.asCallback((err: Error|null, userTradingHistory?: Array<TradingHistoryRow>): void => {
     if (err) return callback(err);
     if (!userTradingHistory) return callback(new Error("Internal error retrieving trade history"));
