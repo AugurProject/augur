09844ec5c2208e85dbb25e5db330dcffe4383fa3
Scott Bigelow <epheph@gmail.com>
2017-11-17 15:55:41 -0800

    better docs

--- src/markets/get-markets-awaiting-reporting.js
+++ src/markets/get-markets-awaiting-reporting.js
@@ -6,9 +6,9 @@ var augurNode = require("../augur-node");
  * @param {Object} p Parameters object.
  * @param {string=} p.universe Look up markets within this universe.
  * @param {string=} p.reportingWindow Look up markets within this reportingWindow
- * @param {string=} p.reportingRound Look up markets within this reportingWindow
+ * @param {string=} p.reportingRound Look up markets in this reportingState: FIRST_REPORTING|LAST_REPORTING. See REPORTING_STATE enum
  * @param {function} callback Called when reporting history has been received and parsed.
- * @return {Object} Reporting history, keyed by market ID.
+ * @return {Object} Markets history, keyed by market ID.
  */
 function getMarketsAwaitingReporting(p, callback) {
   augurNode.submitRequest("getMarketsAwaitingReporting", p, callback);
