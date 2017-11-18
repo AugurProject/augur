ea443443bd65f828c4293ad653c7e4c889783649
Stephen Sprinkle <stephen@sprinkle.io>
2017-11-18 08:28:12 -0800

    removal of test data

--- src/modules/topics/container.js
+++ src/modules/topics/container.js
@@ -7,20 +7,8 @@ import { selectTopics } from 'modules/topics/selectors/topics'
 
 const mapStateToProps = state => ({
   universe: state.universe,
+  isMobile: state.isMobile,
   topics: selectTopics(state),
-  // TODO: please review using this dummy data, then remove this comment and the dummy data :)
-  // topics: [
-  //   { popularity: 1000, topic: 'Ethereum' },
-  //   { popularity: 900, topic: 'Bitcoin' },
-  //   { popularity: 800, topic: 'Sports' },
-  //   { popularity: 700, topic: 'Stocks' },
-  //   { popularity: 600, topic: 'Finance' },
-  //   { popularity: 500, topic: 'Fashion' },
-  //   { popularity: 400, topic: 'Art' },
-  //   { popularity: 300, topic: 'Politics' },
-  //   { popularity: 200, topic: 'News' },
-  //   { popularity: 100, topic: 'Games' },
-  // ],
   loginAccount: selectLoginAccount(state),
   isLogged: state.isLogged
 })
