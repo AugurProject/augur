f0950690494119e72e5909170b1843dcfe4ecc3b
Scott Bigelow <epheph@gmail.com>
2017-11-20 15:57:23 -0800

    quick fixes

--- src/server/getters/get-markets.ts
+++ src/server/getters/get-markets.ts
@@ -5,6 +5,7 @@ import { queryModifier, getMarketsWithReportingState } from "./database";
 
 // Returning marketIDs should likely be more generalized, since it is a single line change for most getters (awaiting reporting, by user, etc)
 export function getMarkets(db: Knex, universe: Address, sortBy: string|null|undefined, isSortDescending: boolean|null|undefined, limit: number|null|undefined, offset: number|null|undefined, callback: (err?: Error|null, result?: any) => void): void {
+  if (universe == null) return callback(new Error("Must provide universe"));
   let query = getMarketsWithReportingState(db, ["markets.marketID"]);
   if (universe != null) query = query.where({ universe });
 
