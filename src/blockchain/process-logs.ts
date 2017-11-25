1c429ec5bc93bc8f2ba95c43a8d7906de6c310d7
Jack Peterson <jack@tinybike.net>
2017-11-25 12:49:03 -0800

    Fixed categories insert race condition

--- src/blockchain/process-logs.ts
+++ src/blockchain/process-logs.ts
@@ -1,5 +1,5 @@
 import Augur from "augur.js";
-import { each } from "async";
+import { eachSeries } from "async";
 import * as Knex from "knex";
 import { FormattedLog, EventLogProcessor, ErrorCallback } from "../types";
 
@@ -8,5 +8,5 @@ export function processLog(db: Knex, augur: Augur, trx: Knex.Transaction, log: F
 }
 
 export function processLogs(db: Knex, augur: Augur, trx: Knex.Transaction, logs: Array<FormattedLog>, logProcessor: EventLogProcessor, callback: ErrorCallback): void {
-  each(logs, (log: FormattedLog, nextLog: ErrorCallback): void => processLog(db, augur, trx, log, logProcessor, nextLog), callback);
+  eachSeries(logs, (log: FormattedLog, nextLog: ErrorCallback): void => processLog(db, augur, trx, log, logProcessor, nextLog), callback);
 }
