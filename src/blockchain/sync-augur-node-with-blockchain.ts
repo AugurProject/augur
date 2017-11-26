accf22d1441cee448691106d45f9c8d9561c0e52
Scott Bigelow <epheph@gmail.com>
2017-11-26 07:00:48 -0800

    remove previous protection code

--- src/blockchain/sync-augur-node-with-blockchain.ts
+++ src/blockchain/sync-augur-node-with-blockchain.ts
@@ -47,19 +47,15 @@ export function syncAugurNodeWithBlockchain(db: Knex,  augur: Augur, ethereumNod
           if (err) return callback(err);
           if (!rows || !rows.length || !rows[0]) return callback(new Error("blockchain_sync_history lookup failed"));
           const row: HighestBlockNumberRow = rows[0];
-          if (row.highestBlockNumber === null) { // sync from scratch
-            const fromBlock = uploadBlockNumbers[networkID!];
-            const highestBlockNumber: number = parseInt(block.number, 16) - 1;
-            if (fromBlock >= highestBlockNumber) return callback(null); // augur-node is already up-to-date
-            downloadAugurLogs(db, augur, fromBlock, highestBlockNumber, (err?: Error|null): void => {
-              if (err) return callback(err);
-              db.insert({ highestBlockNumber }).into("blockchain_sync_history").asCallback(callback);
-              // TODO: Modify this function (and all calls down to blockstream) to take highestBlockNumber
-              startAugurListeners(db, augur);
-            });
-          } else {
-            callback(new Error("Please clear your augur.db and start over (must sync from scratch until issue #4386 is resolved)"));
-          }
+          const fromBlock: number = (!row || !row.highestBlockNumber) ? uploadBlockNumbers[networkID!] : row.highestBlockNumber + 1;
+          const highestBlockNumber: number = parseInt(block.number, 16) - 1;
+          if (fromBlock >= highestBlockNumber) return callback(null); // augur-node is already up-to-date
+          downloadAugurLogs(db, augur, fromBlock, highestBlockNumber, (err?: Error|null): void => {
+            if (err) return callback(err);
+            db.insert({ highestBlockNumber }).into("blockchain_sync_history").asCallback(callback);
+            // TODO: Modify this function (and all calls down to blockstream) to take highestBlockNumber
+            startAugurListeners(db, augur);
+          });
         });
       });
     });
