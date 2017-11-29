06d06259f828cbc3db18497bff90f9dfca2225ee
Scott Bigelow <epheph@gmail.com>
2017-11-28 21:50:49 -0800

    debug line

--- src/blockchain/log-processors/report-submitted.ts
+++ src/blockchain/log-processors/report-submitted.ts
@@ -3,12 +3,9 @@ import * as Knex from "knex";
 import { FormattedEventLog, ErrorCallback } from "../../types";
 import { insertStakeToken } from "./designated-report-submitted";
 
-// ReportSubmitted(address indexed universe, address indexed reporter, address indexed market, address stakeToken, uint256 amountStaked, uint256[] payoutNumerators);
-
 export function processReportSubmittedLog(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedEventLog, callback: ErrorCallback): void {
   insertStakeToken(db, trx, log.stakeToken, log.market, log.payoutNumerators, (err: Error|null) => {
     if (err) return callback(err);
-    // console.log(    trx("market_state").first("market_state.reportingState as reportingState").join("markets","markets.marketStateID","market_state.marketStateID").where({marketID: log.market}).toSQL());
     trx("market_state").first("market_state.reportingState as reportingState").join("markets", "markets.marketStateID", "market_state.marketStateID").where("markets.marketID", log.market).asCallback( (err: Error|null, { reportingState }: {reportingState: string}): void => {
       if (err) return callback(err);
       const reportDataToInsert: { [index: string]: string|number|boolean } = {
