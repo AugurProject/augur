9d9ef4cd7ec7f7d0687082b4ea7644f9da522cac
Scott Bigelow <epheph@gmail.com>
2017-12-01 16:13:31 -0800

    denormalized could be a number, too

--- src/server/getters/get-better-worse-orders.ts
+++ src/server/getters/get-better-worse-orders.ts
@@ -3,10 +3,10 @@ import * as Knex from "knex";
 import { Address, MarketsContractAddressRow } from "../../types";
 import { queryModifier, getMarketsWithReportingState } from "./database";
 
-export function getBetterWorseOrders(db: Knex, marketID: Address, outcome: number, amount: string, normalizedPrice: string, callback: (err?: Error|null, result?: any) => void): void {
+export function getBetterWorseOrders(db: Knex, marketID: Address, outcome: number, amount: string, normalizedPrice: string|number, callback: (err?: Error|null, result?: any) => void): void {
   if (marketID == null || outcome == null || normalizedPrice == null) return callback(new Error("Must provide marketID, outcome, and normalizedPrice"));
   callback(null, {immediateFill: true,
     betterOrderID: 0,
-    worseOrderID: 0,    
+    worseOrderID: 0,
     });
 }
