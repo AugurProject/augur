0e141d7992ec4662b5cfa813241a9c5fc9ebc8ea
Jack Peterson <jack@tinybike.net>
2017-12-03 11:08:57 -0800

    Removed spammy debug logging

--- src/create-market/get-market-from-create-market-receipt.js
+++ src/create-market/get-market-from-create-market-receipt.js
@@ -6,10 +6,8 @@ var isObject = require("../utils/is-object");
 
 function getMarketFromCreateMarketReceipt(transactionHash, callback) {
   ethrpc.getTransactionReceipt(transactionHash, function (receipt) {
-    console.log("receipt for", transactionHash, receipt);
     if (!isObject(receipt) || receipt.error) return callback(new Error("Transaction receipt not found for " + transactionHash));
     var marketCreatedLogs = findEventLogsInLogArray("Augur", "MarketCreated", receipt.logs);
-    console.log("marketCreatedLogs:", marketCreatedLogs);
     if (marketCreatedLogs == null || !marketCreatedLogs.length || marketCreatedLogs[0] == null || marketCreatedLogs[0].market == null) {
       return callback(new Error("MarketCreated log not found for " + transactionHash));
     }
