7821c4f79ad1b651b09c8f120dd352f1ac6c1f79
Jack Peterson <jack@tinybike.net>
2017-12-04 11:48:39 -0800

    Allow zero value transfers and approvals

--- src/migrations/20171005123329_transfers.ts
+++ src/migrations/20171005123329_transfers.ts
@@ -4,12 +4,12 @@ exports.up = async (knex: Knex): Promise<any> => {
   return knex.schema.dropTableIfExists("transfers").then((): PromiseLike<any> => {
     return knex.schema.raw(`CREATE TABLE transfers (
       "transactionHash" varchar(66) NOT NULL,
-      "logIndex" unsigned integer NOT NULL CONSTRAINT "nonnegativeLogIndex" CHECK ("logIndex" >= 0),
+      "logIndex" unsigned integer NOT NULL CONSTRAINT "nonnegativeTransferLogIndex" CHECK ("logIndex" >= 0),
       sender varchar(66),
       recipient varchar(66),
       token varchar(66) NOT NULL,
-      value numeric NOT NULL CONSTRAINT "positiveValue" CHECK (value > 0),
-      "blockNumber" integer NOT NULL CONSTRAINT "positiveBlockNumber" CHECK ("blockNumber" > 0),
+      value numeric NOT NULL CONSTRAINT "nonnegativeTransferValue" CHECK (value >= 0),
+      "blockNumber" integer NOT NULL CONSTRAINT "positiveTransferBlockNumber" CHECK ("blockNumber" > 0),
       UNIQUE("transactionHash", "logIndex")
     )`);
   });
