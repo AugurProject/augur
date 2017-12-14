e41a1a1393052fa666b617cf223bb04eb8ca4e31
Stephen Sprinkle <stephen@sprinkle.io>
2017-12-14 11:00:12 -0800

    order header + incrementers

--- src/modules/market/components/market-outcome-charts--header-depth/market-outcome-charts--header-depth.jsx
+++ src/modules/market/components/market-outcome-charts--header-depth/market-outcome-charts--header-depth.jsx
@@ -6,31 +6,36 @@ import Styles from 'modules/market/components/market-outcome-charts--header/mark
 import { ASKS } from 'modules/order-book/constants/order-book-order-types'
 
 const MarketOutcomeDepthHeader = p => (
-  <section className={Styles['MarketOutcomeChartsHeader__stats']}>
-    <span className={Styles['MarketOutcomeChartsHeader__stat']}>
-      <span className={Styles['MarketOutcomeChartsHeader__stat-title']}>
-        {p.hoveredDepth[3] === ASKS ? 'ask' : 'bid'} price
+  <section>
+    <div className={Styles.MarketOutcomeChartsHeader__Header} >
+      <span>Market Depth</span>
+    </div>
+    <div className={Styles['MarketOutcomeChartsHeader__stats']}>
+      <span className={Styles['MarketOutcomeChartsHeader__stat']}>
+        <span className={Styles['MarketOutcomeChartsHeader__stat-title']}>
+          {p.hoveredDepth[3] === ASKS ? 'ask' : 'bid'} price
+        </span>
+        <span className={Styles['MarketOutcomeChartsHeader__stat-value']}>
+          {p.hoveredDepth[1] ? p.hoveredDepth[1].toFixed(p.fixedPrecision).toString() : <span>&mdash;</span>}
+        </span>
       </span>
-      <span className={Styles['MarketOutcomeChartsHeader__stat-value']}>
-        {p.hoveredDepth[1] ? p.hoveredDepth[1].toFixed(p.fixedPrecision).toString() : <span>&mdash;</span>}
+      <span className={Styles[`MarketOutcomeChartsHeader__stat`]}>
+        <span className={Styles[`MarketOutcomeChartsHeader__stat-title`]}>
+          qty
+        </span>
+        <span className={Styles[`MarketOutcomeChartsHeader__stat-value`]}>
+          {p.hoveredDepth[2] ? p.hoveredDepth[2].toFixed(p.fixedPrecision).toString() : <span>&mdash;</span>}
+        </span>
       </span>
-    </span>
-    <span className={Styles[`MarketOutcomeChartsHeader__stat`]}>
-      <span className={Styles[`MarketOutcomeChartsHeader__stat-title`]}>
-        qty
+      <span className={Styles[`MarketOutcomeChartsHeader__stat`]}>
+        <span className={Styles[`MarketOutcomeChartsHeader__stat-title`]}>
+          depth
+        </span>
+        <span className={Styles[`MarketOutcomeChartsHeader__stat-value`]}>
+          {p.hoveredDepth[0] ? p.hoveredDepth[0].toFixed(p.fixedPrecision).toString() : <span>&mdash;</span>}
+        </span>
       </span>
-      <span className={Styles[`MarketOutcomeChartsHeader__stat-value`]}>
-        {p.hoveredDepth[2] ? p.hoveredDepth[2].toFixed(p.fixedPrecision).toString() : <span>&mdash;</span>}
-      </span>
-    </span>
-    <span className={Styles[`MarketOutcomeChartsHeader__stat`]}>
-      <span className={Styles[`MarketOutcomeChartsHeader__stat-title`]}>
-        depth
-      </span>
-      <span className={Styles[`MarketOutcomeChartsHeader__stat-value`]}>
-        {p.hoveredDepth[0] ? p.hoveredDepth[0].toFixed(p.fixedPrecision).toString() : <span>&mdash;</span>}
-      </span>
-    </span>
+    </div>
   </section>
 )
 
--- src/modules/market/components/market-outcome-charts--header-orders/market-outcome-charts--header-orders.jsx
+++ src/modules/market/components/market-outcome-charts--header-orders/market-outcome-charts--header-orders.jsx
@@ -4,21 +4,34 @@ import PropTypes from 'prop-types'
 import Styles from 'modules/market/components/market-outcome-charts--header/market-outcome-charts--header.styles'
 
 const MarketOutcomeChartsHeaderOrders = p => (
-  <section className={Styles['MarketOutcomeChartsHeader__stats']}>
-    <div className={Styles['MarketOutcomeChartsHeader__stat--right']}>
-      <span className={Styles['MarketOutcomeChartsHeader__stat-title']}>
-        ask qty
-      </span>
+  <section>
+    <div className={Styles.MarketOutcomeChartsHeader__Header} >
+      <span>Order Book</span>
+      <div className={Styles['MarketOutcomeChartsHeader__precision-selector']}>
+        <button>
+          -
+        </button>
+        <button>
+          +
+        </button>
+      </div>
     </div>
-    <div className={Styles['MarketOutcomeChartsHeader__stat--right']}>
-      <span className={Styles['MarketOutcomeChartsHeader__stat-title']}>
-        price
-      </span>
-    </div>
-    <div className={Styles['MarketOutcomeChartsHeader__stat--right']}>
-      <span className={Styles['MarketOutcomeChartsHeader__stat-title']}>
-        depth
-      </span>
+    <div className={Styles['MarketOutcomeChartsHeader__stats']}>
+      <div className={Styles['MarketOutcomeChartsHeader__stat--right']}>
+        <span className={Styles['MarketOutcomeChartsHeader__stat-title']}>
+          ask qty
+        </span>
+      </div>
+      <div className={Styles['MarketOutcomeChartsHeader__stat--right']}>
+        <span className={Styles['MarketOutcomeChartsHeader__stat-title']}>
+          price
+        </span>
+      </div>
+      <div className={Styles['MarketOutcomeChartsHeader__stat--right']}>
+        <span className={Styles['MarketOutcomeChartsHeader__stat-title']}>
+          depth
+        </span>
+      </div>
     </div>
   </section>
 )
