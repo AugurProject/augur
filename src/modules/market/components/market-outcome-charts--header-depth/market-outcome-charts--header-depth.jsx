858e9c1d8011406e6339888354f36db4c3b61f81
Stephen Sprinkle <stephen@sprinkle.io>
2017-12-14 09:25:44 -0800

    header styling + fixed precision implementation for depth chart

--- src/modules/market/components/market-outcome-charts--header-depth/market-outcome-charts--header-depth.jsx
+++ src/modules/market/components/market-outcome-charts--header-depth/market-outcome-charts--header-depth.jsx
@@ -1,34 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Styles from 'modules/market/components/market-outcome-charts--header-depth/market-outcome-charts--header-depth.styles'
+import Styles from 'modules/market/components/market-outcome-charts--header/market-outcome-charts--header.styles'
 
 import { ASKS } from 'modules/order-book/constants/order-book-order-types'
 
 const MarketOutcomeDepthHeader = p => (
-  <section className={Styles[`MarketOutcomeDepth__depth-hover-values`]}>
-    <span className={Styles[`MarketOutcomeDepth__hover-depth`]}>
-      <span className={Styles[`MarketOutcomeDepth__depth-hover-title`]}>
+  <section className={Styles['MarketOutcomeChartsHeader__stats']}>
+    <span className={Styles['MarketOutcomeChartsHeader__stat']}>
+      <span className={Styles['MarketOutcomeChartsHeader__stat-title']}>
         {p.hoveredDepth[3] === ASKS ? 'ask' : 'bid'} price
       </span>
-      <span className={Styles[`MarketOutcomeDepth__depth-hover-value`]}>
-        {p.hoveredDepth[1] || <span>&mdash;</span>}
+      <span className={Styles['MarketOutcomeChartsHeader__stat-value']}>
+        {p.hoveredDepth[1] ? p.hoveredDepth[1].toFixed(p.fixedPrecision).toString() : <span>&mdash;</span>}
       </span>
     </span>
-    <span className={Styles[`MarketOutcomeDepth__hover-depth`]}>
-      <span className={Styles[`MarketOutcomeDepth__depth-hover-title`]}>
+    <span className={Styles[`MarketOutcomeChartsHeader__stat`]}>
+      <span className={Styles[`MarketOutcomeChartsHeader__stat-title`]}>
         qty
       </span>
-      <span className={Styles[`MarketOutcomeDepth__depth-hover-value`]}>
-        {p.hoveredDepth[2] || <span>&mdash;</span>}
+      <span className={Styles[`MarketOutcomeChartsHeader__stat-value`]}>
+        {p.hoveredDepth[2] ? p.hoveredDepth[2].toFixed(p.fixedPrecision).toString() : <span>&mdash;</span>}
       </span>
     </span>
-    <span className={Styles[`MarketOutcomeDepth__hover-depth`]}>
-      <span className={Styles[`MarketOutcomeDepth__depth-hover-title`]}>
+    <span className={Styles[`MarketOutcomeChartsHeader__stat`]}>
+      <span className={Styles[`MarketOutcomeChartsHeader__stat-title`]}>
         depth
       </span>
-      <span className={Styles[`MarketOutcomeDepth__depth-hover-value`]}>
-        {p.hoveredDepth[0] || <span>&mdash;</span>}
+      <span className={Styles[`MarketOutcomeChartsHeader__stat-value`]}>
+        {p.hoveredDepth[0] ? p.hoveredDepth[0].toFixed(p.fixedPrecision).toString() : <span>&mdash;</span>}
       </span>
     </span>
   </section>
@@ -38,9 +38,5 @@ export default MarketOutcomeDepthHeader
 
 MarketOutcomeDepthHeader.propTypes = {
   hoveredDepth: PropTypes.array.isRequired,
-  side: PropTypes.string,
-  quantity: PropTypes.number,
-  depth: PropTypes.number,
-  price: PropTypes.number,
   fixedPrecision: PropTypes.number.isRequired
 }
