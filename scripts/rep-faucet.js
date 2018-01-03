d3daf2d54e6f33acf73921e135caf1ef3a1c98e1
Jack Peterson <jack@tinybike.net>
2018-01-03 11:58:26 -0800

    Only rep faucet if account does not have rep already

--- scripts/rep-faucet.js
+++ scripts/rep-faucet.js
@@ -9,44 +9,49 @@ var connectionEndpoints = require("./connection-endpoints");
 var debugOptions = require("./debug-options");
 
 function faucetInAndMigrate(augur, universe, auth, callback) {
-  augur.api.LegacyReputationToken.faucet({
-    meta: auth,
-    _amount: speedomatic.fix(100000, "hex"),
-    onSent: function (res) {
-      if (debugOptions.cannedMarkets) console.log("faucet sent:", res.hash);
-    },
-    onSuccess: function (res) {
-      if (debugOptions.cannedMarkets) console.log("faucet success:", res.callReturn);
-      augur.api.Universe.getReputationToken({ tx: { to: universe } }, function (err, reputationToken) {
-        if (err) return callback(err);
-        if (debugOptions.cannedMarkets) console.log("reputationToken:", reputationToken);
-        augur.api.LegacyReputationToken.approve({
-          meta: auth,
-          _spender: reputationToken,
-          _value: speedomatic.prefixHex(new BigNumber(2, 10).toPower(255).minus(1).toString(16)),
-          onSent: function (res) {
-            if (debugOptions.cannedMarkets) console.log("approve sent:", res.hash);
-          },
-          onSuccess: function (res) {
-            if (debugOptions.cannedMarkets) console.log("approve success:", res.callReturn);
-            augur.api.ReputationToken.migrateFromLegacyReputationToken({
-              meta: auth,
-              tx: { to: reputationToken },
-              onSent: function (res) {
-                if (debugOptions.cannedMarkets) console.log("migrateFromLegacyReputationToken sent:", res.hash);
-              },
-              onSuccess: function (res) {
-                if (debugOptions.cannedMarkets) console.log("migrateFromLegacyReputationToken success:", res.callReturn);
-                callback(null);
-              },
-              onFailed: callback,
-            });
-          },
-          onFailed: callback,
-        });
+  augur.api.Universe.getReputationToken({ tx: { to: universe } }, function (err, reputationToken) {
+    if (err) return callback(err);
+    if (debugOptions.cannedMarkets) console.log("reputationToken:", reputationToken);
+    augur.api.ReputationToken.balanceOf({ _owner: auth.address, tx: { to: reputationToken } }, function (err, reputationTokenBalance) {
+      if (err) return callback(err);
+      console.log("reputationTokenBalance:", reputationTokenBalance);
+      if (new BigNumber(reputationTokenBalance, 10).gt(new BigNumber("100000", 10))) return callback(null);
+      augur.api.LegacyReputationToken.faucet({
+        meta: auth,
+        _amount: speedomatic.fix(100000, "hex"),
+        onSent: function (res) {
+          if (debugOptions.cannedMarkets) console.log("faucet sent:", res.hash);
+        },
+        onSuccess: function (res) {
+          if (debugOptions.cannedMarkets) console.log("faucet success:", res.callReturn);
+          augur.api.LegacyReputationToken.approve({
+            meta: auth,
+            _spender: reputationToken,
+            _value: speedomatic.prefixHex(new BigNumber(2, 10).toPower(255).minus(1).toString(16)),
+            onSent: function (res) {
+              if (debugOptions.cannedMarkets) console.log("approve sent:", res.hash);
+            },
+            onSuccess: function (res) {
+              if (debugOptions.cannedMarkets) console.log("approve success:", res.callReturn);
+              augur.api.ReputationToken.migrateFromLegacyReputationToken({
+                meta: auth,
+                tx: { to: reputationToken },
+                onSent: function (res) {
+                  if (debugOptions.cannedMarkets) console.log("migrateFromLegacyReputationToken sent:", res.hash);
+                },
+                onSuccess: function (res) {
+                  if (debugOptions.cannedMarkets) console.log("migrateFromLegacyReputationToken success:", res.callReturn);
+                  callback(null);
+                },
+                onFailed: callback,
+              });
+            },
+            onFailed: callback,
+          });
+        },
+        onFailed: callback,
       });
-    },
-    onFailed: callback,
+    });
   });
 }
 
