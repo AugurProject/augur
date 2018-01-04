8ac007a3c568e8c1c86bac85757514c4bf10ce70
Jack Peterson <jack@tinybike.net>
2018-01-04 15:06:49 -0800

    Export getStakeRequiredForDesignatedReporter in reporting module

--- src/reporting/index.js
+++ src/reporting/index.js
@@ -5,6 +5,7 @@ module.exports = {
   getStakeTokens: require("./get-stake-tokens"),
   getReportingSummary: require("./get-reporting-summary"),
   getReportingWindowsWithUnclaimedFees: require("./get-reporting-windows-with-unclaimed-fees"),
+  getStakeRequiredForDesignatedReporter: require("./get-stake-required-for-designated-reporter"),
   getCurrentPeriodProgress: require("./get-current-period-progress"),
   finalizeMarket: require("./finalize-market"),
 };
