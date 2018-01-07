0975b4c8dcdddbb0e49634fa85c52bbb1cc5be16
Tom Haile <bradford.haile@gmail.com>
2018-01-07 17:29:47 -0600

    start up script works, not getting ls output from ipfs

--- ipfs-configure.sh
+++ ipfs-configure.sh
@@ -6,3 +6,4 @@ ipfs key gen --type=rsa --size=2048 augur-ui
 export HASH_KEY=$(ipfs add -r build/ | tail -n 1 | awk '{print $2}')
 echo "Using hash key of $HASH_KEY"
 ipfs name publish --key=augur-ui $HASH_KEY
+echo ipfs ls /ipfs/$HASH_KEY
--- ipfs-run.sh
+++ ipfs-run.sh
@@ -3,4 +3,5 @@
 ipfs daemon &
 sleep 10s
 # configure nginx to be proxy for ipfs
+export HASH_KEY=$(ipfs config show | grep ID | cut -c 16-61)
 echo ipfs ls /ipfs/$HASH_KEY
