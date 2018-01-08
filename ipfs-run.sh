8ab24aba1b40d97cf1f51574446762f863feac99
Tom Haile <bradford.haile@gmail.com>
2018-01-08 14:47:58 -0600

    update script

--- ipfs-run.sh
+++ ipfs-run.sh
@@ -5,5 +5,5 @@ sleep 10s
 
 # configure nginx to be proxy for ipfs
 cd /etc/nginx && rm -rf ./cache/* && mkdir ./cache && mkdir ./cache/temp && chown www-data ./cache/temp
-service nginx restart
 service cron start
+nginx
