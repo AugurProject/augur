e1ca4486442e39cb4e700692384aed4ac90b7f7a
Tom Haile <bradford.haile@gmail.com>
2018-01-09 15:49:39 -0600

    added build hash in nginx proxy_pass

--- support/ipfs-configure.sh
+++ support/ipfs-configure.sh
@@ -4,7 +4,7 @@ ipfs daemon &
 sleep 10s
 export NEW_BUILD_HASH=$(ipfs add -r build/ | tail -n 1 | awk '{print $2}')
 echo "Using hash key of $NEW_BUILD_HASH"
-export PUBLISHED_HASH=$(ipfs name publish $NEW_BUILD_HASH)
+ipfs name publish $NEW_BUILD_HASH
 mkdir /augur/ipfs-deploy
 echo $NEW_BUILD_HASH > /augur/ipfs-deploy/NEW_BUILD_HASH
-echo $PUBLISHED_HASH > /augur/ipfs-deploy/PUBLISHED_BUILD_HASH
+sed -i 's/NEW_BUILD_HASH/'"$NEW_BUILD_HASH"'/g' /etc/nginx/sites-available/default
