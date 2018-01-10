374cb8cad041ecf473a679ff2b7c0e5df127c089
Paul Gebheim <paul@onetaste.us>
2018-01-10 01:33:25 -0800

    Linthappiness

--- test/unit/connections/rpc.js
+++ test/unit/connections/rpc.js
@@ -3,16 +3,15 @@
 "use strict";
 
 var expect = require("chai").expect;
-var isFunction = require("../../../src/utils/is-function");
 var Augur = require("../../../src/");
 
-describe("connections/augur-node", function() {
+describe("connections/augur-node", function () {
   it("Has EventEmitter API on its interface", function (done) {
     var augur = new Augur();
 
-    expect(augur.rpc).to.respondTo('on');
-    expect(augur.rpc).to.respondTo('off');
-    expect(augur.rpc).to.respondTo('emit');
+    expect(augur.rpc).to.respondTo("on");
+    expect(augur.rpc).to.respondTo("off");
+    expect(augur.rpc).to.respondTo("emit");
     done();
   });
 });
