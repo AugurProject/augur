5500a10d0804e6f7b596a95d0ed1bf2a522227d4
Paul Gebheim <paul@onetaste.us>
2018-01-10 01:14:19 -0800

    Add basic mocha tests to verify interfaces

---
+++ test/unit/connections/augur-node.js
@@ -0,0 +1,18 @@
+/* eslint-env mocha */
+
+"use strict";
+
+var expect = require("chai").expect;
+var isFunction = require("../../../src/utils/is-function");
+var Augur = require("../../../src/");
+
+describe("connections/augur-node", function() {
+  it("Has EventEmitter API on its interface", function (done) {
+    var augur = new Augur();
+
+    expect(augur.augurNode).to.respondTo('on');
+    expect(augur.augurNode).to.respondTo('off');
+    expect(augur.augurNode).to.respondTo('emit');
+    done();
+  });
+});
