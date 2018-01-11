59f39ad04c6411cba71e5adb173bb184fd8b433b
Stephen Sprinkle <stephen@sprinkle.io>
2018-01-11 08:55:36 -0800

    expose sha3 method

--- src/rpc-interface.js
+++ src/rpc-interface.js
@@ -25,6 +25,7 @@ var createRpcInterface = function (ethrpc) {
     setDebugOptions: ethrpc.setDebugOptions,
     WsTransport: ethrpc.WsTransport,
     publish: ethrpc.publish,
+    sha3: ethrpc.sha3
   });
 };
 
