9b05ea65ad5422d2f132e31631a8924674a79659
Stephen Sprinkle <stephen@sprinkle.io>
2018-01-11 09:08:51 -0800

    linting

--- src/rpc-interface.js
+++ src/rpc-interface.js
@@ -25,7 +25,7 @@ var createRpcInterface = function (ethrpc) {
     setDebugOptions: ethrpc.setDebugOptions,
     WsTransport: ethrpc.WsTransport,
     publish: ethrpc.publish,
-    sha3: ethrpc.sha3
+    sha3: ethrpc.sha3,
   });
 };
 
