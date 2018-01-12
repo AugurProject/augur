878b9bec551cc2332901fbe2dfb670eb8e52773d
Stephen Sprinkle <stephen@sprinkle.io>
2018-01-12 09:47:22 -0800

    slight change to implementation

--- src/modules/auth/actions/login-with-airbitz.js
+++ src/modules/auth/actions/login-with-airbitz.js
@@ -15,7 +15,7 @@ export const loginWithAirbitzEthereumWallet = (airbitzAccount, ethereumWallet, h
     ...account,
     meta: {
       address: account.address,
-      signer: typeof privateKey === 'string' ? Buffer.from(privateKey, 'hex') : privateKey,
+      signer: Buffer.from(privateKey, 'hex'),
       accountType: augur.rpc.constants.ACCOUNT_TYPES.PRIVATE_KEY
     },
     name: airbitzAccount.username,
