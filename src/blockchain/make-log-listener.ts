d9d2abb7b2ea275025d9a82e9691d64f05f27a84
Scott Bigelow <epheph@gmail.com>
2018-01-12 16:27:53 -0800

    enqueue logs from the augur.js callbacks

--- src/blockchain/make-log-listener.ts
+++ src/blockchain/make-log-listener.ts
@@ -1,22 +1,27 @@
 import Augur from "augur.js";
 import * as Knex from "knex";
-import { FormattedEventLog } from "../types";
+import * as async from "async";
+import { ErrorCallback, FormattedEventLog } from "../types";
 import { logProcessors } from "./log-processors";
 import { processLog } from "./process-logs";
 import { augurEmitter } from "../events";
 import { logError } from "../utils/log-error";
+import { processQueue, LOG_PRIORITY } from "../blockchain/process-queue";
 
 export function makeLogListener(db: Knex, augur: Augur, contractName: string, eventName: string) {
   return (log: FormattedEventLog): void => {
-    console.log("log:", contractName, eventName, log);
-    const logProcessor = logProcessors[contractName][eventName];
-    if (!logProcessor.noAutoEmit) augurEmitter.emit(eventName, log);
-    db.transaction((trx: Knex.Transaction): void => processLog(db, augur, trx, log, logProcessors[contractName][eventName], (err: Error|null): void => {
-      if (err) {
-        trx.rollback(err);
-      } else {
-        trx.commit();
-      }
-    })).asCallback(logError);
+    console.log("log queued:", contractName, eventName, log);
+    processQueue.push( (callback: ErrorCallback) => {
+      console.log("log processing:", contractName, eventName, log);
+      const logProcessor = logProcessors[contractName][eventName];
+      if (!logProcessor.noAutoEmit) augurEmitter.emit(eventName, log);
+      db.transaction((trx: Knex.Transaction): void => processLog(db, augur, trx, log, logProcessors[contractName][eventName], (err: Error|null): void => {
+        if (err) {
+          trx.rollback(err);
+        } else {
+          trx.commit();
+        }
+      })).asCallback(callback);
+  }, LOG_PRIORITY);
   };
 }
