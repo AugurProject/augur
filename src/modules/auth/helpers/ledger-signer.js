88db94c571559c94722a6d03a5a0419d01037f69
Stephen Sprinkle <stephen@sprinkle.io>
2018-01-15 13:13:17 -0700

    updated signing method

--- src/modules/auth/helpers/ledger-signer.js
+++ src/modules/auth/helpers/ledger-signer.js
@@ -1,5 +1,7 @@
 import { updateModal } from 'modules/modal/actions/update-modal'
 import { closeModal } from 'modules/modal/actions/close-modal'
+import EthereumTX from 'ethereumjs-tx'
+import { prefixHex } from 'speedomatic'
 
 import { MODAL_LEDGER } from 'modules/modal/constants/modal-types'
 
@@ -8,11 +10,20 @@ const ledgerSigner = async (passedArguments, ledgerLib, dispatch) => {
     type: MODAL_LEDGER
   }))
 
-  return ledgerLib.signTransactionByBip44Index(...passedArguments)
+  const tx = new EthereumTX(passedArguments[0])
+
+  return ledgerLib.signTransactionByBip44Index(tx.serialize().toString('hex'), 7)
     .then((res) => {
+      tx.r = Buffer.from(res.r, 'hex')
+      tx.s = Buffer.from(res.s, 'hex')
+      tx.v = Buffer.from(res.v, 'hex')
+
+      passedArguments[1](null, prefixHex(tx.serialize().toString('hex')))
       dispatch(closeModal())
     })
     .catch((err) => {
+      passedArguments[1](err)
+
       dispatch(updateModal({
         type: MODAL_LEDGER,
         error: `Failed to Sign with "${err}"`,
