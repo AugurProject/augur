49a5f2cc9d5ac3391a105e7e256906dd5722bb99
Tom Haile <bradford.haile@gmail.com>
2018-01-16 13:15:53 -0600

    clean up script to get balance of creator mailbox

--- scripts/canned-markets/check-mailbox.js
+++ scripts/canned-markets/check-mailbox.js
@@ -5,6 +5,7 @@
 var Augur = require("../../src");
 var chalk = require("chalk");
 var connectionEndpoints = require("../connection-endpoints");
+var speedomatic = require("speedomatic");
 
 var marketID = process.argv[2];
 
@@ -12,18 +13,17 @@ var augur = new Augur();
 
 
 augur.connect(connectionEndpoints, function (err) {
-  console.log("getting connected");
   if (err) return console.error(err);
   augur.api.Market.getMarketCreatorMailbox({ tx: { to: marketID } }, function (err, marketMailboxAddress) {
-    console.log("getting creator mailbox");
     if (err) return console.log(err);
     console.log(chalk.green.dim("market mailbox address:"), chalk.green(marketMailboxAddress));
     augur.api.Cash.getBalance({ _address: marketMailboxAddress }, function (err, cashBal) {
       if (err) return console.log(err);
-      // var cashBalance = speedomatic.bignum(cashBal);
-      console.log(cashBal);
+      var cashBalance = speedomatic.bignum(cashBal);
       augur.rpc.eth.getBalance([marketMailboxAddress, "latest"], function (attoEthBalance) {
-        console.log("balance is ", attoEthBalance);
+        var ethBalance = speedomatic.bignum(attoEthBalance);
+        var combined = speedomatic.unfix(ethBalance.add(cashBalance), "string");
+        console.log(chalk.green.dim("Total balance:"), chalk.green(combined));
         process.exit();
       });
     });
