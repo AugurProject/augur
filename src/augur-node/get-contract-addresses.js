10fdb0c187d459d0a051e4715d74ad7cfcec52cb
Scott Bigelow <epheph@gmail.com>
2018-01-15 20:28:03 -0800

    adding augurNode.getContractAddresses function

---
+++ src/augur-node/get-contract-addresses.js
@@ -0,0 +1,17 @@
+/**
+ * @todo Query augur.js version and contract address set from augur-node.
+ */
+
+"use strict";
+
+var submitRequest = require("./submit-json-rpc-request");
+/**
+ * Returns the version and contract address set from augur-node. Requires an Augur Node connection.
+ * @param {function} callback Called after the version and contract addresses have been retrieved.
+ * @return {Object} Object containing version and addresses.
+ */
+function getContractAddresses(callback) {
+  submitRequest("getContractAddresses", null, callback);
+}
+
+module.exports = getContractAddresses;
