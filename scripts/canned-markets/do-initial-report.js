cf9dfb14d35d148aa1e713cf8e803086f8771c57
Tom Haile <bradford.haile@gmail.com>
2018-01-16 16:28:38 -0600

    follow docs and reformatted obj

--- scripts/canned-markets/do-initial-report.js
+++ scripts/canned-markets/do-initial-report.js
@@ -6,7 +6,6 @@ var Augur = require("../../src");
 var chalk = require("chalk");
 var connectionEndpoints = require("../connection-endpoints");
 var BigNumber = require("bignumber.js");
-var assign = require("lodash.assign");
 
 var marketID = process.argv[2];
 var outcome = process.argv[3];
@@ -20,18 +19,28 @@ augur.connect(connectionEndpoints, function (err) {
   augur.markets.getMarketsInfo({ marketIDs: [marketID] }, function (err, marketsInfo) {
     var market = marketsInfo[0];
     var numTicks = market.numTicks;
-    var payoutNumerators = [market.numOutcomes];
-    var i = 0;
-    for (i = 0; i < market.numOutcomes; i++) {
-      payoutNumerators[i] = new BigNumber(0, 10);
-    }
+    var payoutNumerators = Array(market.numOutcomes).fill(new BigNumber(0, 10));
     payoutNumerators[outcome] = new BigNumber(numTicks, 10);
-    var reportPayload = { tx: { to: marketID  }, _payoutNumerators: payoutNumerators, _invalid: invalid };
+
+
+    var reportPayload = { tx: { to: marketID  },
+      _payoutNumerators: payoutNumerators,
+      _invalid: invalid,
+      onSent: function (result) {
+        console.log(chalk.yellow.dim("Sent:"), chalk.yellow(JSON.stringify(result)));
+      },
+      onSuccess: function (result) {
+        console.log(chalk.green.dim("Success:"), chalk.green(JSON.stringify(result)));
+        process.exit(1);
+      },
+      onFailed: function (result) {
+        console.log(chalk.red.dim("Failed:"), chalk.red(JSON.stringify(result)));
+        process.exit(0);
+      },
+    };
+
     console.log(chalk.green.dim("reportPayload:"), chalk.green(JSON.stringify(reportPayload)));
-    augur.api.Market.doInitialReport(reportPayload, function (err, result) {
-      if (err) return console.log(err);
-      console.log(chalk.green.dim("doInitialReport:"), chalk.green(result));
-    });
+    augur.api.Market.doInitialReport(reportPayload);
   });
 });
 
