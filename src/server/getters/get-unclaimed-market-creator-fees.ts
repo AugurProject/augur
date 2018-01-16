603e63c2e40ff151c0c9bb89f282719afaa0f0ef
Scott Bigelow <epheph@gmail.com>
2018-01-16 14:06:31 -0800

    change to array of objects

--- src/server/getters/get-unclaimed-market-creator-fees.ts
+++ src/server/getters/get-unclaimed-market-creator-fees.ts
@@ -1,7 +1,7 @@
 import { parallel } from "async";
 import * as Knex from "knex";
 import * as _ from "lodash";
-import { Address, MarketsRowWithCreationTime, UIMarketCreatorFees, ErrorCallback, AsyncCallback, ReportingState} from "../../types";
+import { Address, MarketsRowWithCreationTime, UIMarketCreatorFee, ErrorCallback, AsyncCallback, ReportingState} from "../../types";
 import { reshapeOutcomesRowToUIOutcomeInfo, reshapeMarketsRowToUIMarketInfo, getMarketsWithReportingState } from "./database";
 
 interface MarketCreatorFeesRow {
@@ -11,18 +11,25 @@ interface MarketCreatorFeesRow {
   marketCreatorFeesClaimed: number;
 }
 
-export function getUnclaimedMarketCreatorFees(db: Knex, marketIDs: Array<Address>, callback: (err: Error|null, result?: UIMarketCreatorFees) => void): void {
+export function getUnclaimedMarketCreatorFees(db: Knex, marketIDs: Array<Address>, callback: (err: Error|null, result?: Array<UIMarketCreatorFee>) => void): void {
   if (marketIDs == null) return callback(new Error("must include marketIDs parameter"));
   let marketsQuery: Knex.QueryBuilder = getMarketsWithReportingState(db, ["markets.marketID", "market_state.reportingState", "markets.marketCreatorFeesCollected", "markets.marketCreatorFeesClaimed"]);
   marketsQuery = marketsQuery.whereIn("markets.marketID", marketIDs);
   marketsQuery.asCallback(( err: Error|null, marketCreatorFeeRows: Array<MarketCreatorFeesRow>): void => {
     if (err != null) return callback(err);
+    const feeRowsByMarket = _.keyBy(marketCreatorFeeRows, (r: MarketCreatorFeesRow): string => r.marketID);
+    const feeResult: Array<UIMarketCreatorFee> = _.map( marketIDs, (marketID: string): any|null => {
+      const market = feeRowsByMarket[marketID];
+      if ( !market ) {
+        return null;
+      } else {
+        return {
+          marketID,
+          unclaimedFee: market.reportingState === ReportingState.FINALIZED ? market.marketCreatorFeesCollected - market.marketCreatorFeesClaimed : 0,
+        };
+      }
+    });
     callback(null,
-      marketCreatorFeeRows.reduce( (acc: UIMarketCreatorFees, row) => {
-        acc[row.marketID] = row.reportingState === ReportingState.FINALIZED ? row.marketCreatorFeesCollected - row.marketCreatorFeesClaimed : 0;
-        return acc;
-      },
-      {}),
-    );
+      feeResult);
   });
 }
