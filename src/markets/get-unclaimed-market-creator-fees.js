7d561366364f1284354d9dc9a7712dd58f18797e
Scott Bigelow <epheph@gmail.com>
2018-01-16 17:37:26 -0800

    fixing jsdoc

--- src/markets/get-unclaimed-market-creator-fees.js
+++ src/markets/get-unclaimed-market-creator-fees.js
@@ -1,12 +1,18 @@
 "use strict";
 
+/**
+* @typedef {Object} FeeInfo
+* @property {string} marketID Address of a market, as a hexadecimal string.
+* @property {number|string} unclaimedFee Fee available to be claimed from the market, by the market creator
+*/
+
 var augurNode = require("../augur-node");
 
 /**
  * Returns information about unclaimed market creator fees. Fees are only available on finalized markets.
  * @param {Object} p Parameters object.
  * @param {string[]} p.marketIDs Contract addresses of the markets for which to get details, as hexadecimal strings.
- * @returns {Object.<string, number>}
+ * @returns {FeeInfo[]}
  */
 function getUnclaimedMarketCreatorFees(p, callback) {
   augurNode.submitRequest("getUnclaimedMarketCreatorFees", p, callback);
