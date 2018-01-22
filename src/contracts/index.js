30eb05cf062fe111e564a55a9342341af7410496
Paul Gebheim <paul@onetaste.us>
2018-01-22 12:11:01 -0800

    Happy Linter

--- src/contracts/index.js
+++ src/contracts/index.js
@@ -5,5 +5,5 @@ var generateAbiMap = require("./generate-abi-map");
 module.exports = {
   abi: generateAbiMap(require("./abi")),
   addresses: require("./addresses"),
-  uploadBlockNumbers: require("./upload-block-numbers")
+  uploadBlockNumbers: require("./upload-block-numbers"),
 };
