646ec1ac0383c03511aff2e5b12b2908db8d48e1
John Danz <John.Danz87@gmail.com>
2018-01-23 17:52:19 -0600

    linting

--- src/modules/modal/components/modal-network-disconnected/modal-network-disconnected.jsx
+++ src/modules/modal/components/modal-network-disconnected/modal-network-disconnected.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
 import Styles from 'modules/modal/components/modal-network-disconnected/modal-network-disconnected.styles'
 
-const ModalNetworkDisconnected = p => {
-  console.log('modalNetworkDisconnected p:', p)
-return (
+const ModalNetworkDisconnected = p => (
   <section className={Styles.ModalNetworkDisconnected}>
     <h1>Reconnecting to Augur Node</h1>
     <span>You have been disconnected from your Augur Node. Please wait while we try to reconnect you, or update your node address here.</span>
@@ -14,10 +12,10 @@ return (
     <span>http: {!!p.env['ethereum-node'] && p.env['ethereum-node'].http}</span>
     <span>ws: {!!p.env['ethereum-node'] && p.env['ethereum-node'].ws}</span>
   </section>
-)}
+)
 
-ModalNetworkDisconnected.propTypes = {
-  // expectedNetwork: PropTypes.number.isRequired
-}
+// ModalNetworkDisconnected.propTypes = {
+// expectedNetwork: PropTypes.number.isRequired
+// }
 
 export default ModalNetworkDisconnected
