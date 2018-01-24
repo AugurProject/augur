d6470e0debaa0197a0cdef078419d576934181ac
Scott Bigelow <epheph@gmail.com>
2018-01-24 15:23:14 -0800

    finalized test

--- src/blockchain/log-processors/market-finalized.ts
+++ src/blockchain/log-processors/market-finalized.ts
@@ -6,7 +6,6 @@ import { updateMarketState } from "./database";
 
 
 export function processMarketFinalizedLog(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedEventLog, callback: ErrorCallback): void {
-  console.log(log);
   updateMarketState( db, log.market, trx, log.blockNumber, augur.constants.REPORTING_STATE.FINALIZED, (err: Error|null): void => {
     augurEmitter.emit("MarketFinalized", log);
     callback(null);
@@ -14,8 +13,7 @@ export function processMarketFinalizedLog(db: Knex, augur: Augur, trx: Knex.Tran
 }
 
 export function processMarketFinalizedLogRemoval(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedEventLog, callback: ErrorCallback): void {
-  console.log(log);
-  db("market_state").transacting(trx).delete().where({marketID: log.market, reportingState: augur.constants.REPORTING_STATE.DESIGNATED_DISPUTE}).asCallback((err: Error|null): void => {
+  db("market_state").transacting(trx).delete().where({marketID: log.market, reportingState: augur.constants.REPORTING_STATE.FINALIZED}).asCallback((err: Error|null): void => {
     if (err) return callback(err);
     db("market_state").transacting(trx).max("marketStateID as previousMarketStateID").first().where({marketID: log.market}).asCallback((err: Error|null, {previousMarketStateID }: {previousMarketStateID: number}): void => {
       if (err) return callback(err);
