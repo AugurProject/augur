277aef5c4c8b506163f1ac86c3eae7bdfc95c3b3
Scott Bigelow <epheph@gmail.com>
2018-01-25 10:28:25 -0800

    test and removal

---
+++ src/blockchain/log-processors/universe-created.ts
@@ -0,0 +1,22 @@
+import Augur from "augur.js";
+import * as Knex from "knex";
+import { FormattedEventLog, ErrorCallback } from "../../types";
+import { augurEmitter } from "../../events";
+
+export function processUniverseCreatedLog(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedEventLog, callback: ErrorCallback): void {
+  const universeToInsert = {
+    universe: log.childUniverse,
+    parentUniverse: log.parentUniverse,
+  };
+  db.transacting(trx).insert(universeToInsert).into("universes").asCallback((err: Error|null): void => {
+    augurEmitter.emit("UniverseCreated", log);
+    callback(null);
+  });
+}
+
+export function processUniverseCreatedLogRemoval(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedEventLog, callback: ErrorCallback): void {
+  db("universes").transacting(trx).where({universe: log.childUniverse}).del().asCallback((err: Error|null): void => {
+    augurEmitter.emit("UniverseCreated", log);
+    callback(null);
+  });
+}
