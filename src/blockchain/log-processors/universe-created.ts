a9448169cf6e32b436ab0577c2dbac3155321b1e
Scott Bigelow <epheph@gmail.com>
2018-01-25 10:30:09 -0800

    error handling

--- src/blockchain/log-processors/universe-created.ts
+++ src/blockchain/log-processors/universe-created.ts
@@ -9,6 +9,7 @@ export function processUniverseCreatedLog(db: Knex, augur: Augur, trx: Knex.Tran
     parentUniverse: log.parentUniverse,
   };
   db.transacting(trx).insert(universeToInsert).into("universes").asCallback((err: Error|null): void => {
+    if (err) return callback(err);
     augurEmitter.emit("UniverseCreated", log);
     callback(null);
   });
@@ -16,6 +17,7 @@ export function processUniverseCreatedLog(db: Knex, augur: Augur, trx: Knex.Tran
 
 export function processUniverseCreatedLogRemoval(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedEventLog, callback: ErrorCallback): void {
   db("universes").transacting(trx).where({universe: log.childUniverse}).del().asCallback((err: Error|null): void => {
+    if (err) return callback(err);
     augurEmitter.emit("UniverseCreated", log);
     callback(null);
   });
