e70abe26a960eccd420756efdd118817d234c1b8
John Danz <John.Danz87@gmail.com>
2018-01-30 13:37:35 -0600

    linting, refactored slightly for dryness

--- src/modules/categories/components/category-list/category-list.jsx
+++ src/modules/categories/components/category-list/category-list.jsx
@@ -5,49 +5,52 @@ import Category from 'modules/categories/components/category/category'
 
 import Styles from 'modules/categories/components/category-list/category-list.styles'
 
-const CategoryList = p => {
+const CategoryList = (p) => {
   const isSingle = (p.categories.length === 1)
   const isDouble = (p.categories.length === 2)
 
-  return (<div>
-    {isDouble && <div
-      className={Styles['CategoryList__categorywrap-double']}
-      key={`${null}"null-0"`}
-    >
-      <Category
-        category="null-category"
-        popularity={0}
-      />
-    </div>}
-    {[...Array(p.boundedLength)].map((_, i) => {
-      let categoryIndex = (p.lowerBound - 1) + i
-      let categoryStyling = Styles.CategoryList__categorywrap
-
-      if (isSingle) {
-        categoryIndex--
-      }
-
-      const isLimitedCategories = ((p.categories.length <= 2) && (i <= 2))
-      const category = (p.categories && p.categories[categoryIndex]) ? p.categories[categoryIndex] : null
-
-
-      if (isLimitedCategories) {
-        categoryStyling = isSingle ? Styles['CategoryList__categorywrap-single'] : Styles['CategoryList__categorywrap-double']
-      }
-
-      return (
+  return (
+    <div>
+      {isDouble &&
         <div
-          className={categoryStyling}
-          key={category !== null ? JSON.stringify(category) : `${JSON.stringify(category)}${categoryIndex}`}
+          className={Styles['CategoryList__categorywrap-double']}
+          key={`${null}"null-0"`}
         >
           <Category
-            category={category !== null ? category.category : 'null-category'}
-            popularity={category !== null ? category.popularity : 0}
+            category="null-category"
+            popularity={0}
           />
         </div>
-      )
-    })}
-  </div>)
+      }
+      {[...Array(p.boundedLength)].map((_, i) => {
+        let categoryIndex = (p.lowerBound - 1) + i
+        let categoryStyling = Styles.CategoryList__categorywrap
+
+        if (isSingle) {
+          // put only cat in the 2nd position so it's centered
+          categoryIndex -= 1
+        }
+
+        const isLimitedCategories = ((p.categories.length <= 2) && (i <= 2))
+        const category = (p.categories && p.categories[categoryIndex]) ? p.categories[categoryIndex] : null
+
+        categoryStyling = (isDouble && isLimitedCategories) ? Styles['CategoryList__categorywrap-double'] : categoryStyling
+
+
+        return (
+          <div
+            className={categoryStyling}
+            key={category !== null ? JSON.stringify(category) : `${JSON.stringify(category)}${categoryIndex}`}
+          >
+            <Category
+              category={category !== null ? category.category : 'null-category'}
+              popularity={category !== null ? category.popularity : 0}
+            />
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 CategoryList.propTypes = {
