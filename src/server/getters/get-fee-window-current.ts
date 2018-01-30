150fa82e1098329f09fa1dfa4e1a9b86d843cc6b
Scott Bigelow <epheph@gmail.com>
2018-01-30 13:30:53 -0800

    lint-ish

--- src/server/getters/get-fee-window-current.ts
+++ src/server/getters/get-fee-window-current.ts
@@ -1,14 +1,14 @@
 import * as Knex from "knex";
 import {Address, FeeWindowRow} from "../../types";
 
-export function getFeeWindowCurrent(db: Knex, universe: Address, callback: (err?: Error | null, result?: any) => void): void {
+export function getFeeWindowCurrent(db: Knex, universe: Address, callback: (err?: Error|null, result?: any) => void): void {
   if (universe == null) return callback(new Error("Must provide universe"));
   const query = db.select("*").first().from("fee_windows")
     .whereNull("endBlockNumber")
     .where({universe})
     .orderBy("startTime", "ASC");
 
-  query.asCallback((err: Error | null, feeWindow?: FeeWindowRow): void => {
+  query.asCallback((err: Error|null, feeWindow?: FeeWindowRow): void => {
     if (err) return callback(err);
     if (!feeWindow) return callback(null, null);
     callback(null, feeWindow);
