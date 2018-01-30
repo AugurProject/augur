cc0a93c85bb7a032d318e7a76e71262513be68e5
John Danz <John.Danz87@gmail.com>
2018-01-30 13:16:23 -0600

    modified category list and category list styles to handle a different layout if only 1 or 2 categories are present.

--- src/modules/categories/components/category-list/category-list.jsx
+++ src/modules/categories/components/category-list/category-list.jsx
@@ -5,15 +5,39 @@ import Category from 'modules/categories/components/category/category'
 
 import Styles from 'modules/categories/components/category-list/category-list.styles'
 
-const CategoryList = p => (
-  <div>
+const CategoryList = p => {
+  const isSingle = (p.categories.length === 1)
+  const isDouble = (p.categories.length === 2)
+
+  return (<div>
+    {isDouble && <div
+      className={Styles['CategoryList__categorywrap-double']}
+      key={`${null}"null-0"`}
+    >
+      <Category
+        category="null-category"
+        popularity={0}
+      />
+    </div>}
     {[...Array(p.boundedLength)].map((_, i) => {
-      const categoryIndex = (p.lowerBound - 1) + i
+      let categoryIndex = (p.lowerBound - 1) + i
+      let categoryStyling = Styles.CategoryList__categorywrap
+
+      if (isSingle) {
+        categoryIndex--
+      }
+
+      const isLimitedCategories = ((p.categories.length <= 2) && (i <= 2))
       const category = (p.categories && p.categories[categoryIndex]) ? p.categories[categoryIndex] : null
+
+
+      if (isLimitedCategories) {
+        categoryStyling = isSingle ? Styles['CategoryList__categorywrap-single'] : Styles['CategoryList__categorywrap-double']
+      }
 
       return (
         <div
-          className={Styles.CategoryList__categorywrap}
+          className={categoryStyling}
           key={category !== null ? JSON.stringify(category) : `${JSON.stringify(category)}${categoryIndex}`}
         >
           <Category
@@ -23,8 +47,8 @@ const CategoryList = p => (
         </div>
       )
     })}
-  </div>
-)
+  </div>)
+}
 
 CategoryList.propTypes = {
   categories: PropTypes.array.isRequired,
