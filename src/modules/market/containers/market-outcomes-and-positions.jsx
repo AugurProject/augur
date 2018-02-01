2abd76ecdaf6d116099c77a84583ceebb8deccb5
Tom Haile <bradford.haile@gmail.com>
2018-02-01 11:30:49 -0600

    intermediate step to wiring up orders and positions

--- src/modules/market/containers/market-outcomes-and-positions.jsx
+++ src/modules/market/containers/market-outcomes-and-positions.jsx
@@ -7,10 +7,7 @@ import { selectMarket } from 'modules/market/selectors/market'
 // import getValue from 'utils/get-value'
 
 const mapStateToProps = state => ({
-  marketID: '0x7d9f26082539a7f9793b8c3b25f2a20374ab357d73ff6d6dc99cab6145b567a0',
   scalarShareDenomination: getScalarShareDenomination(),
-  positions,
-  openOrders,
   isMobile: state.isMobile,
 })
 
@@ -18,11 +15,18 @@ const mergeProps = (sP, dP, oP) => {
   const market = selectMarket(oP.marketId)
 
   console.log('market -- ', market)
-
+  let openOrders = []
+  let positions = []
+  if (market && market.outcomes && market.outcomes.length > 0) {
+    openOrders = market.outcomes[0].userOpenOrders ? market.outcomes[0].userOpenOrders : []
+    positions = market.outcomes[0].positions ? market.outcomes[0].positions : []
+  }
   return {
     ...sP,
     ...oP,
-    outcomes: market.outcomes
+    outcomes: market.outcomes,
+    openOrders,
+    positions
     // TODO -- need to rethink best shape for `userOpenOrders`
     // openOrder: getValue(market, 'outcomes')
   }
@@ -31,143 +35,3 @@ const mergeProps = (sP, dP, oP) => {
 const MarketOutcomesAndPositionsContainer = withRouter(connect(mapStateToProps, null, mergeProps)(MarketOutcomesAndPositions))
 
 export default MarketOutcomesAndPositionsContainer
-
-const openOrders = [
-  {
-    name: '1. HARTNELL (12)',
-    id: '1',
-    pending: false,
-    order: {
-      unmatchedShares: {
-        formatted: '10.942',
-        formattedValue: 10.942,
-      },
-      avgPrice: {
-        formatted: '0.3938',
-        formattedValue: 0.3938,
-      },
-      cancelOrder: () => console.log('cancel order'),
-    },
-  },
-  {
-    name: '1. HARTNELL (12)',
-    id: '1',
-    pending: true,
-    order: {
-      unmatchedShares: {
-        formatted: '4.942',
-        formattedValue: 4.942,
-      },
-      avgPrice: {
-        formatted: '0.6',
-        formattedValue: 0.6,
-      },
-      cancelOrder: () => console.log('cancel order'),
-    },
-  },
-  {
-    name: '1. HARTNELL (12)',
-    id: '1',
-    pending: false,
-    order: {
-      unmatchedShares: {
-        formatted: '7',
-        formattedValue: 7,
-      },
-      avgPrice: {
-        formatted: '0.38',
-        formattedValue: 0.38,
-      },
-      cancelOrder: () => console.log('cancel order'),
-    },
-  },
-  {
-    name: '1. HARTNELL (12)',
-    id: '1',
-    pending: false,
-    order: {
-      unmatchedShares: {
-        formatted: '15.203',
-        formattedValue: 15.203,
-      },
-      avgPrice: {
-        formatted: '0.3507',
-        formattedValue: 0.3507,
-      },
-      cancelOrder: () => console.log('cancel order'),
-    },
-  },
-  {
-    name: '1. HARTNELL (12)',
-    id: '1',
-    pending: false,
-    order: {
-      unmatchedShares: {
-        formatted: '2.942',
-        formattedValue: 2.942,
-      },
-      avgPrice: {
-        formatted: '0.35',
-        formattedValue: 0.35,
-      },
-      cancelOrder: () => console.log('cancel order'),
-    },
-  },
-  {
-    name: '2. ALMANDIN (14)',
-    id: '2',
-    pending: false,
-    order: {
-      unmatchedShares: {
-        formatted: '9.5',
-        formattedValue: 9.5,
-      },
-      avgPrice: {
-        formatted: '0.8',
-        formattedValue: 0.8,
-      },
-      cancelOrder: () => console.log('cancel order'),
-    },
-  },
-]
-
-const positions = [
-  {
-    name: '1. HARTNELL (12)',
-    id: '1',
-    position: {
-      marketId: 'dummy market id',
-      outcomeId: 'dummy outcome id',
-      qtyShares: {
-        formatted: '10',
-        formattedValue: 10,
-      },
-      avgPrice: {
-        formatted: '0.5',
-        formattedValue: 0.5,
-      },
-      realizedNet: { formatted: '+0' },
-      unrealizedNet: { formatted: '-1.2' },
-      closePosition: () => console.log('close position'),
-    },
-  },
-  {
-    id: '2',
-    name: '2. ALMANDIN (14)',
-    position: {
-      marketId: 'dummy market id',
-      outcomeId: 'dummy outcome id',
-      qtyShares: {
-        formatted: '5',
-        formattedValue: 5,
-      },
-      avgPrice: {
-        formatted: '0.75',
-        formattedValue: 0.75,
-      },
-      realizedNet: { formatted: '+0' },
-      unrealizedNet: { formatted: '+0.05' },
-      closePosition: () => console.log('close position'),
-    },
-  },
-]
