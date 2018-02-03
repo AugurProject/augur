523cfbf220e583caf89f79f539afaa4c6661c874
Tom Haile <bradford.haile@gmail.com>
2018-02-02 18:36:54 -0600

    stub in reporting dispute and setup routing for reporting.

--- src/modules/app/components/inner-nav/reporting-inner-nav.jsx
+++ src/modules/app/components/inner-nav/reporting-inner-nav.jsx
@@ -1,15 +1,31 @@
 import BaseInnerNav from 'modules/app/components/inner-nav/base-inner-nav'
-import { REPORTING_DISPUTE } from 'modules/routes/constants/views'
+import { REPORTING_OPEN, REPORTING_DISPUTE, REPORTING_CLOSED } from 'modules/routes/constants/views'
 
 export default class ReportingInnerNav extends BaseInnerNav {
   getMainMenuData() {
     return [
+      {
+        label: 'Reporting',
+        visible: true,
+        isSelected: (this.props.currentBasePath === REPORTING_OPEN),
+        link: {
+          pathname: REPORTING_OPEN
+        }
+      },
       {
         label: 'Dispute',
         visible: true,
         isSelected: (this.props.currentBasePath === REPORTING_DISPUTE),
         link: {
           pathname: REPORTING_DISPUTE
+        }
+      },
+      {
+        label: 'Resolved',
+        visible: true,
+        isSelected: (this.props.currentBasePath === REPORTING_CLOSED),
+        link: {
+          pathname: REPORTING_CLOSED
         }
       },
     ]
