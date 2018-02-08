cd5abe05363b7f8c1151e47fc8e1a8f6762be544
Justin Barry <328965+justinbarry@users.noreply.github.com>
2018-02-08 13:10:11 -0800

    Add JSX files to test pattern and set env in setup file
    Setting the env here is useful when using entrypoints other than scripts/test.js
    
    Signed-off-by: Justin Barry <328965+justinbarry@users.noreply.github.com>

--- test/setup.js
+++ test/setup.js
@@ -2,3 +2,6 @@ import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({ adapter: new Adapter() })
+
+process.env.BABEL_ENV = 'test'
+process.env.NODE_ENV = 'test'
