248cc8da713f90b1dd92f5ed3924678314b0fe3a
Jack Peterson <jack@tinybike.net>
2018-02-08 18:54:17 -0800

    Added outcome field to OrderCanceled emit

--- src/blockchain/log-processors/order-canceled.ts
+++ src/blockchain/log-processors/order-canceled.ts
@@ -3,16 +3,17 @@ import * as Knex from "knex";
 import { Bytes32, FormattedEventLog, ErrorCallback, OrderState } from "../../types";
 import { augurEmitter } from "../../events";
 
-interface MarketID {
+interface MarketIDAndOutcome {
   marketID: Bytes32;
+  outcome: number;
 }
 
 export function processOrderCanceledLog(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedEventLog, callback: ErrorCallback): void {
   db.transacting(trx).from("orders").where("orderID", log.orderId).update({ orderState: OrderState.CANCELED }).asCallback((err: Error|null): void => {
     if (err) return callback(err);
-    trx.first("marketID").from("orders").where("orderID", log.orderId).asCallback((err: Error|null, marketID?: MarketID): void => {
+    trx.first("marketID", "outcome").from("orders").where("orderID", log.orderId).asCallback((err: Error|null, ordersRow?: MarketIDAndOutcome): void => {
       if (err) return callback(err);
-      augurEmitter.emit("OrderCanceled", Object.assign({}, marketID, log));
+      augurEmitter.emit("OrderCanceled", Object.assign({}, ordersRow, log));
       callback(null);
     });
   });
@@ -21,9 +22,9 @@ export function processOrderCanceledLog(db: Knex, augur: Augur, trx: Knex.Transa
 export function processOrderCanceledLogRemoval(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedEventLog, callback: ErrorCallback): void {
   db.transacting(trx).from("orders").where("orderID", log.orderId).update({ orderState: OrderState.OPEN }).asCallback((err: Error|null): void => {
     if (err) return callback(err);
-    trx.first("marketID").from("orders").where("orderID", log.orderId).asCallback((err: Error|null, marketID?: MarketID): void => {
+    trx.first("marketID", "outcome").from("orders").where("orderID", log.orderId).asCallback((err: Error|null, ordersRow?: MarketIDAndOutcome): void => {
       if (err) return callback(err);
-      augurEmitter.emit("OrderCanceled", Object.assign({}, marketID, log));
+      augurEmitter.emit("OrderCanceled", Object.assign({}, ordersRow, log));
       callback(null);
     });
   });
