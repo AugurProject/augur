308865d14df88a3c3914356f445690bd634b4f0c
Tom Haile <bradford.haile@gmail.com>
2018-02-08 18:40:24 -0600

    Use augur-node convenient method to get fee window properties.

--- src/modules/reporting/actions/load-reporting-window-bounds.js
+++ src/modules/reporting/actions/load-reporting-window-bounds.js
@@ -3,30 +3,19 @@ import { updateReportingWindowStats } from 'modules/reporting/actions/update-rep
 
 export const loadReportingWindowBounds = () => (dispatch, getState) => {
   const { universe, loginAccount } = getState()
-
-  augur.api.Universe.getCurrentFeeWindow({ universe: universe.id }, (err, reportingWindow) => {
-    if (err) return
-
-    augur.api.FeeWindow.getStartTime({ tx: { to: reportingWindow } }, (err, startTime) => {
+  augur.augurNode.submitRequest(
+    'getFeeWindowCurrent',
+    {
+      universe: universe.id,
+      reporter: loginAccount.address
+    }, (err, result) => {
       if (err) return
-      dispatch(updateReportingWindowStats({ startTime }))
-    })
 
-    augur.api.FeeWindow.getEndTime({ tx: { to: reportingWindow } }, (err, endTime) => {
-      if (err) return
-      dispatch(updateReportingWindowStats({ endTime }))
-    })
-
-    augur.augurNode.submitRequest(
-      'getFeeWindowCurrent',
-      {
-        universe: universe.id,
-        reporter: loginAccount.address
-      }, (err, result) => {
-        if (err) return
-
-        dispatch(updateReportingWindowStats({ stake: result.totalStake }))
-      }
-    )
-  })
+      dispatch(updateReportingWindowStats({
+        startTime: result.startTime,
+        endTime: result.endTime,
+        stake: result.totalStake
+      }))
+    }
+  )
 }
