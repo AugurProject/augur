fa176b18877a1171b2eeef69b77fbf4e3112f3dd
Jack Peterson <jack@tinybike.net>
2018-02-09 14:31:36 -0800

    Do not sign to check isUnlocked for MetaMask accounts

--- src/modules/auth/actions/use-unlocked-account.js
+++ src/modules/auth/actions/use-unlocked-account.js
@@ -1,25 +1,35 @@
 import { augur } from 'services/augurjs'
 import { loadAccountData } from 'modules/auth/actions/load-account-data'
 import { updateIsLogged } from 'modules/auth/actions/update-is-logged'
+import logError from 'utils/log-error'
+
+const updateAccount = unlockedAddress => (dispatch) => {
+  augur.accounts.logout() // clear the client-side account
+  console.log('using unlocked account:', unlockedAddress)
+  dispatch(updateIsLogged(true))
+  dispatch(loadAccountData({
+    address: unlockedAddress,
+    meta: {
+      address: unlockedAddress,
+      signer: null,
+      accountType: augur.rpc.constants.ACCOUNT_TYPES.UNLOCKED_ETHEREUM_NODE
+    },
+    isUnlocked: true
+  }, true))
+}
 
 // Use unlocked local address (if actually unlocked)
-export const useUnlockedAccount = unlockedAddress => (dispatch) => {
-  if (!unlockedAddress) return console.error('no account address')
+export const useUnlockedAccount = (unlockedAddress, callback = logError) => (dispatch) => {
+  if (!unlockedAddress) return callback('no account address')
+  if (typeof window !== 'undefined' && (((window || {}).web3 || {}).currentProvider || {}).isMetaMask) {
+    return dispatch(updateAccount(unlockedAddress))
+  }
   augur.rpc.isUnlocked(unlockedAddress, (isUnlocked) => {
     if (!isUnlocked || isUnlocked.error) {
-      return console.warn('account is locked:', unlockedAddress, isUnlocked)
+      console.warn('account is locked:', unlockedAddress, isUnlocked)
+      return callback(null)
     }
-    augur.accounts.logout() // clear the client-side account
-    console.log('using unlocked account:', unlockedAddress)
-    dispatch(updateIsLogged(true))
-    dispatch(loadAccountData({
-      address: unlockedAddress,
-      meta: {
-        address: unlockedAddress,
-        signer: null,
-        accountType: augur.rpc.constants.ACCOUNT_TYPES.UNLOCKED_ETHEREUM_NODE
-      },
-      isUnlocked: true
-    }, true))
+    dispatch(updateAccount(unlockedAddress))
+    callback(null)
   })
 }
