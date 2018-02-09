d52c4c98779b0ec3c38304190eb6c6b48b8e70f8
Jack Peterson <jack@tinybike.net>
2018-02-08 18:49:24 -0800

    Added marketID field to OrderCanceled emitter

--- src/blockchain/log-processors/order-canceled.ts
+++ src/blockchain/log-processors/order-canceled.ts
@@ -1,20 +1,30 @@
 import Augur from "augur.js";
 import * as Knex from "knex";
-import { FormattedEventLog, ErrorCallback, OrderState } from "../../types";
+import { Bytes32, FormattedEventLog, ErrorCallback, OrderState } from "../../types";
 import { augurEmitter } from "../../events";
+
+interface MarketID {
+  marketID: Bytes32;
+}
 
 export function processOrderCanceledLog(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedEventLog, callback: ErrorCallback): void {
   db.transacting(trx).from("orders").where("orderID", log.orderId).update({ orderState: OrderState.CANCELED }).asCallback((err: Error|null): void => {
     if (err) return callback(err);
-    augurEmitter.emit("OrderCanceled", log);
-    callback(null);
+    trx.first("marketID").from("orders").where("orderID", log.orderId).asCallback((err: Error|null, marketID?: MarketID): void => {
+      if (err) return callback(err);
+      augurEmitter.emit("OrderCanceled", Object.assign({}, marketID, log));
+      callback(null);
+    });
   });
 }
 
 export function processOrderCanceledLogRemoval(db: Knex, augur: Augur, trx: Knex.Transaction, log: FormattedEventLog, callback: ErrorCallback): void {
   db.transacting(trx).from("orders").where("orderID", log.orderId).update({ orderState: OrderState.OPEN }).asCallback((err: Error|null): void => {
     if (err) return callback(err);
-    augurEmitter.emit("OrderCanceled", log);
-    callback(null);
+    trx.first("marketID").from("orders").where("orderID", log.orderId).asCallback((err: Error|null, marketID?: MarketID): void => {
+      if (err) return callback(err);
+      augurEmitter.emit("OrderCanceled", Object.assign({}, marketID, log));
+      callback(null);
+    });
   });
 }
