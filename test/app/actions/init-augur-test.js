3c3abaa7f855224290852791999827d138cbe7e0
unknown <achap5dk@gmail.com>
2018-02-11 14:42:56 -0800

    linting

--- test/app/actions/init-augur-test.js
+++ test/app/actions/init-augur-test.js
@@ -97,7 +97,7 @@ describe('init-augur', () => {
               getNetworkID: () => 4,
               eth: { accounts: cb => cb(['0xa11ce']) },
             },
-            api: { Controller: { stopped: () => {} }}
+            api: { Controller: { stopped: () => {} } }
           }
         })
 
@@ -152,7 +152,7 @@ describe('init-augur', () => {
               getNetworkID: () => 4,
               eth: { accounts: cb => cb([]) },
             },
-            api: { Controller: { stopped: () => {} }}
+            api: { Controller: { stopped: () => {} } }
           }
         })
 
@@ -212,7 +212,7 @@ describe('init-augur', () => {
               getNetworkID: () => 3,
               eth: { accounts: cb => cb([]) },
             },
-            api: { Controller: { stopped: () => {} }}
+            api: { Controller: { stopped: () => {} } }
           }
         })
 
@@ -314,7 +314,7 @@ describe('init-augur', () => {
               getNetworkID: () => 4,
               eth: { accounts: cb => cb(['0xa11ce']) },
             },
-            api: { Controller: { stopped: () => {} }}
+            api: { Controller: { stopped: () => {} } }
           }
         })
 
@@ -362,7 +362,7 @@ describe('init-augur', () => {
               getNetworkID: () => 4,
               eth: { accounts: cb => cb([]) },
             },
-            api: { Controller: { stopped: () => {} }}
+            api: { Controller: { stopped: () => {} } }
           }
         })
 
