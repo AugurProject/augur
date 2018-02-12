e3b951dfbebc0de50a6a9908dcc58dd941ea6311
Jack Peterson <jack@tinybike.net>
2018-02-12 11:13:54 -0800

    Updated docker scripts

--- scripts/geth-docker-persistent.sh
+++ scripts/geth-docker-persistent.sh
@@ -3,4 +3,4 @@
 set -ex
 
 docker pull augurproject/dev-node-geth:latest
-docker run -it -p 8545:8545 -p 8546:8546 -v $HOME/docker:/root/.ethereum augurproject/dev-node-geth:latest
+docker run -e GETH_VERBOSITY=4 -it -p 8545:8545 -p 8546:8546 -v $HOME/docker:/root/.ethereum augurproject/dev-node-geth:latest
