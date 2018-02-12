4364f3e4a435691b85eb3529b1445a2eac3b8887
Paul Gebheim <paul@onetaste.us>
2018-02-12 15:02:33 -0800

    Make dp ES5

--- scripts/dp/index.js
+++ scripts/dp/index.js
@@ -1,149 +1,181 @@
 #!/usr/bin/env node
 
-const { promisify } = require("util");
-const path = require("path");
-const Augur = require("../../src");
-const debugOptions = require("../debug-options");
-const { ContractDeployer, DeployerConfiguration, NetworkConfiguration } = require("augur-core");
-const { getPrivateKeyFromString } = require("./lib/get-private-key");
-const parrotSay = require("parrotsay-api");
-const chalk = require("chalk");
-const columnify = require("columnify");
+var path = require("path");
+var Augur = require("../../src");
+var debugOptions = require("../debug-options");
+var core = require("augur-core");
+var getPrivateKeyFromString = require("./lib/get-private-key").getPrivateKeyFromString;
+var parrotSay = require("parrotsay-api");
+var chalk = require("chalk");
+var columnify = require("columnify");
 
-const repFaucet = promisify(require("../rep-faucet"));
-const createMarkets = promisify(require("./create-markets"));
-const createOrders = promisify(require("./create-orders"));
+var repFaucet = require("../rep-faucet");
+var createMarkets = require("./create-markets");
+var createOrders = require("./create-orders");
 
-const COMMANDS = ["create-markets", "create-orders", "deploy", "rep-faucet", "upload"];
-const NETWORKS = ["aura", "clique", "environment", "rinkeby", "ropsten"];
+var COMMANDS = ["create-markets", "create-orders", "deploy", "rep-faucet", "upload"];
+var NETWORKS = ["aura", "clique", "environment", "rinkeby", "ropsten"];
 
-async function help() {
-  console.log(await parrotSay(" Augur Deployment a.k.a. Disco Parrot "));
-  console.log("Usage: dp <command> <network 1> <network 2> ... <network N>\n\n");
+function help() {
+  return parrotSay(" Augur Deployment Parrot ").then(function (say) {
+    console.log(say);
+    console.log("Usage: dp <command> <network 1> <network 2> ... <network N>\n\n");
 
-  console.log(chalk.underline("Commands"));
-  console.log(COMMANDS.join(", "), "or help for this message");
-  console.log("  NOTE: create-orders only supports " + chalk.bold("one network") + " at a time");
+    console.log(chalk.underline("Commands"));
+    console.log(COMMANDS.join(", "), "or help for this message");
+    console.log("  NOTE: create-orders only supports " + chalk.bold("one network") + " at a time");
 
-  console.log(chalk.underline("\nNetworks"));
-  console.log(NETWORKS.join(", "));
+    console.log(chalk.underline("\nNetworks"));
+    console.log(NETWORKS.join(", "));
 
-  console.log(chalk.underline("\nConfiguration"));
-  console.log("Set the following " + chalk.bold("environment variables") + " to modify the behavior of the deployment process");
-  console.log("ex: USE_NORMAL_TIME=false dp deploy aura");
+    console.log(chalk.underline("\nConfiguration"));
+    console.log("Set the following " + chalk.bold("environment variables") + " to modify the behavior of the deployment process");
+    console.log("ex: USE_NORMAL_TIME=false dp deploy aura");
 
-  console.log(chalk.underline("\nNetwork (when using `environment` for the network)"));
-  console.log(columnify([{
-    env: "ETHEREUM_HTTP",
-    Description: "The http(s) address of your ethereum endpoint (default: http://localhost:8545)",
-  }, {
-    env: "AUGUR_WS",
-    Description: "The websocket uri of your augur endpoint, only for " + chalk.bold("create-orders"),
-  }, {
-    env: "ETHEREUM_PRIVATE_KEY",
-    Description: "HEX Private Key used for transactions on this eth node",
-  }, {
-    env: "GAS_PRICE_IN_NANOETH",
-    Description: "The transaction gas price to use, specified in nanoeth (default: varies)",
-  }], {
-    columnSplitter: " - ",
-    minWidth: 20,
-    maxWidth: 80,
-    showHeaders: false,
-  }));
+    console.log(chalk.underline("\nNetwork (when using `environment` for the network)"));
+    console.log(columnify([{
+      env: "ETHEREUM_HTTP",
+      Description: "The http(s) address of your ethereum endpoint (default: http://localhost:8545)",
+    }, {
+      env: "AUGUR_WS",
+      Description: "The websocket uri of your augur endpoint, only for " + chalk.bold("create-orders"),
+    }, {
+      env: "ETHEREUM_PRIVATE_KEY",
+      Description: "HEX Private Key used for transactions on this eth node",
+    }, {
+      env: "GAS_PRICE_IN_NANOETH",
+      Description: "The transaction gas price to use, specified in nanoeth (default: varies)",
+    }], {
+      columnSplitter: " - ",
+      minWidth: 20,
+      maxWidth: 80,
+      showHeaders: false,
+    }));
 
-  console.log(chalk.underline("\nPrivate Keys (for any named environment)"));
-  console.log(columnify([{
-    env: "AURA_PRIVATE_KEY",
-    description: "Override key used to deploy to Aura, defaults to the dev key",
-  }, {
-    env: "CLIQUE_PRIVATE_KEY",
-    description: "Override key used to deploy to Clique, defaults to the dev key",
-  }, {
-    env: "RINKEBY_PRIVATE_KEY",
-    description: "Set key used to deploy to Rinkeby, default is blank and " + chalk.bold("will error if not set"),
-  }, {
-    env: "ROPSTEN_PRIVATE_KEY",
-    description: "Set key used to deploy to Ropsten, default is blank and " + chalk.bold("will error if not set"),
-  }], {
-    columnSplitter: " - ",
-    minWidth: 20,
-    maxWidth: 80,
-    showHeaders: false,
-  }));
-  console.log(chalk.underline("\nUpload Configs"));
-  console.log(columnify([{
-    env: "PRODUCTION",
-    description: "[true, false] If true force USE_NORMAL_TIME to true and potentially other optimizations. (default: false)",
-  }, {
-    env: "USE_NORMAL_TIME",
-    description: "[true, false] Should time flow normally or be adjusted using the custom time management (default: true)",
-  }], {
-    columnSplitter: " - ",
-    minWidth: 20,
-    maxWidth: 80,
-    showHeaders: false,
-  }));
+    console.log(chalk.underline("\nPrivate Keys (for any named environment)"));
+    console.log(columnify([{
+      env: "AURA_PRIVATE_KEY",
+      description: "Override key used to deploy to Aura, defaults to the dev key",
+    }, {
+      env: "CLIQUE_PRIVATE_KEY",
+      description: "Override key used to deploy to Clique, defaults to the dev key",
+    }, {
+      env: "RINKEBY_PRIVATE_KEY",
+      description: "Set key used to deploy to Rinkeby, default is blank and " + chalk.bold("will error if not set"),
+    }, {
+      env: "ROPSTEN_PRIVATE_KEY",
+      description: "Set key used to deploy to Ropsten, default is blank and " + chalk.bold("will error if not set"),
+    }], {
+      columnSplitter: " - ",
+      minWidth: 20,
+      maxWidth: 80,
+      showHeaders: false,
+    }));
+    console.log(chalk.underline("\nUpload Configs"));
+    console.log(columnify([{
+      env: "PRODUCTION",
+      description: "[true, false] If true force USE_NORMAL_TIME to true and potentially other optimizations. (default: false)",
+    }, {
+      env: "USE_NORMAL_TIME",
+      description: "[true, false] Should time flow normally or be adjusted using the custom time management (default: true)",
+    }], {
+      columnSplitter: " - ",
+      minWidth: 20,
+      maxWidth: 80,
+      showHeaders: false,
+    }));
+  });
 }
 
-async function runCannedData(command, networks) {
-  const deployerConfiguration = DeployerConfiguration.create(path.join(__dirname, "../../src/contracts"));
-  const networkConfigurations = networks.map(NetworkConfiguration.create);
-  for (const network of networkConfigurations) {
-    const augur = new Augur();
+function runCannedData(command, networks, callback) {
+  var deployerConfiguration = core.DeployerConfiguration.create(path.join(__dirname, "../../src/contracts"));
+  var networkConfigurations = networks.map(core.NetworkConfiguration.create);
+  // This is done in two steps on purpose, create validates the envs and will throw an error
+  // if it doesn't work
+  networkConfigurations.forEach(function (network) {
+    var augur = new Augur();
     augur.rpc.setDebugOptions(debugOptions);
 
-    const connect = promisify(augur.connect);
-    const reloadAddresses = promisify(augur.contracts.reloadAddresses);
-
-    const auth = getPrivateKeyFromString(network.privateKey);
+    var auth = getPrivateKeyFromString(network.privateKey);
     switch (command) {
       case "upload": {
-        await ContractDeployer.deployToNetwork(network, deployerConfiguration);
+        core.ContractDeployer.deployToNetwork(network, deployerConfiguration).then(function () {
+          callback(null);
+        });
         break;
       }
 
       case "rep-faucet": {
-        await connect({ ethereumNode: { http: network.http } });
-        await repFaucet(augur, auth);
+        augur.connect({ ethereumNode: { http: network.http } }, function (err) {
+          if (err) return callback(err);
+
+          repFaucet(augur, auth, callback);
+        });
         break;
       }
 
       case "create-markets": {
-        await connect({ ethereumNode: { http: network.http } });
-        await repFaucet(augur, auth);
-        await createMarkets(augur, auth);
+        augur.connect({ ethereumNode: { http: network.http } }, function (err) {
+          if (err) return callback(err);
+          repFaucet(augur, auth, function (err) {
+            if (err) return callback(err);
+            createMarkets(augur, auth, callback);
+          });
+        });
         break;
       }
 
       case "create-orders": {
         if (typeof process.env.AUGUR_WS === "undefined") {
           console.log("Error: Must pass augur node URI in AUGUR_WS for create-orders\n");
-          return help();
+          return help().then(function () {
+            callback(null);
+          });
         }
-        await connect({ ethereumNode: { http: network.http }, augurNode: process.env.AUGUR_WS });
-        await createOrders(augur, auth);
+        augur.connect({ ethereumNode: { http: network.http }, augurNode: process.env.AUGUR_WS }, function (err) {
+          if (err) return callback(err);
+          createOrders(augur, auth, callback);
+        });
         break;
       }
 
       case "deploy": {
-        await ContractDeployer.deployToNetwork(network, deployerConfiguration);
-        await reloadAddresses();
-        await connect({ ethereumNode: { http: network.http } });
-        await repFaucet(augur, auth);
-        await createMarkets(augur, auth);
+        core.ContractDeployer.deployToNetwork(network, deployerConfiguration).then(function () {
+          augur.contracts.reloadAddresses(function (err) {
+            if (err) return callback(err);
+
+            augur.connect({ ethereumNode: { http: network.http } }, function (err) {
+              if (err) return callback(err);
+              repFaucet(augur, auth, function (err) {
+                if (err) return callback(err);
+                createMarkets(augur, auth, callback);
+              });
+            });
+          });
+        });
         break;
       }
 
       default: {
-        console.log((await parrotSay("Hello")).length);
+        help().then(function () {
+          callback(null);
+        });
       }
     }
+  });
+}
+
+function showError(error) {
+  console.log("Failure!\n", error.message);
+  if (error.stack && debugOptions.cannedMarkets) {
+    console.log("-------- BACKTRACE ------");
+    console.log(error.stack);
   }
+  process.exit(1);
 }
 
 if (require.main === module) {
-  const command = process.argv[2];
+  var command = process.argv[2];
   var networks = process.argv.slice(3);
 
   if (networks.length === 0) {
@@ -155,16 +187,13 @@ if (require.main === module) {
       process.exit();
     });
   } else {
-    runCannedData(command, networks).then(() => {
-      process.exit();
-    }).catch((error) => {
-      console.log("Failure!\n", error.message);
-      if (error.stack && debugOptions.cannedMarkets) {
-        console.log("-------- BACKTRACE ------");
-        console.log(error.stack);
-      }
-      process.exit(1);
-    });
+    try {
+      runCannedData(command, networks).then(() => {
+        process.exit();
+      }).catch(showError);
+    } catch (error) {
+      showError(error);
+    }
   }
 }
 
