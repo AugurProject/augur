d8e19119b2c50a61d07955e1c686f731e8c096c5
Scott Bigelow <epheph@gmail.com>
2018-02-20 10:53:21 -0800

    extract the getPosition/updatePosition method

--- src/blockchain/log-processors/order-filled/update-orders-and-positions.ts
+++ src/blockchain/log-processors/order-filled/update-orders-and-positions.ts
@@ -5,31 +5,24 @@ import { Address, Bytes32, AsyncCallback, ErrorCallback } from "../../../types";
 import { upsertPositionInMarket } from "./upsert-position-in-market";
 import { convertOnChainSharesToHumanReadableShares } from "../../../utils/convert-fixed-point-to-decimal";
 import { formatOrderAmount } from "../../../utils/format-order";
+import { refreshPositionInMarket } from "./refresh-position-in-market";
 
 interface OrderFilledOnContractData {
   amount: string;
-  creatorPositionInMarket: Array<string>;
-  fillerPositionInMarket: Array<string>;
 }
 
 export function updateOrdersAndPositions(db: Knex, augur: Augur, trx: Knex.Transaction, marketID: Address, orderID: Bytes32, creator: Address, filler: Address, numTicks: string|number, tickSize: string, callback: ErrorCallback): void {
   parallel({
     amount: (next: AsyncCallback): void => augur.api.Orders.getAmount({ _orderId: orderID }, next),
-    creatorPositionInMarket: (next: AsyncCallback): void => augur.trading.getPositionInMarket({ market: marketID, address: creator, tickSize }, next),
-    fillerPositionInMarket: (next: AsyncCallback): void => augur.trading.getPositionInMarket({ market: marketID, address: filler, tickSize }, next),
+    creatorPositionInMarket: (next: AsyncCallback): void => refreshPositionInMarket(db, augur, trx, marketID, creator, next),
+    fillerPositionInMarket: (next: AsyncCallback): void => refreshPositionInMarket(db, augur, trx, marketID, filler, next),
   }, (err: Error|null, onContractData: OrderFilledOnContractData): void => {
     if (err) return callback(err);
-    const { amount, creatorPositionInMarket, fillerPositionInMarket } = onContractData!;
+    const { amount } = onContractData!;
     const fullPrecisionAmountRemainingInOrder = convertOnChainSharesToHumanReadableShares(amount, tickSize);
     const amountRemainingInOrder = formatOrderAmount(fullPrecisionAmountRemainingInOrder);
     const updateAmountsParams = { fullPrecisionAmount: fullPrecisionAmountRemainingInOrder, amount: amountRemainingInOrder };
     const updateParams = fullPrecisionAmountRemainingInOrder === "0" ? Object.assign({}, updateAmountsParams, { isRemoved: 1 }) : updateAmountsParams;
-    db("orders").transacting(trx).where({ orderID }).update(updateParams).asCallback((err: Error|null): void => {
-      if (err) return callback(err);
-      parallel([
-        (next: AsyncCallback): void => upsertPositionInMarket(db, augur, trx, creator, marketID, numTicks, creatorPositionInMarket, next),
-        (next: AsyncCallback): void => upsertPositionInMarket(db, augur, trx, filler, marketID, numTicks, fillerPositionInMarket, next),
-      ], callback);
-    });
+    db("orders").transacting(trx).where({ orderID }).update(updateParams).asCallback(callback);
   });
 }
