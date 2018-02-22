eab88d8442a3a28f83d81b0645a6e77d2ca5d1a1
nuevoalex <achap5dk@gmail.com>
2018-02-21 23:37:07 -0800

    tests

--- test/server/getters/get-initial-reporters.js
+++ test/server/getters/get-initial-reporters.js
@@ -9,7 +9,7 @@ describe("server/getters/get-initial-reporters", () => {
     it(t.description, (done) => {
       setupTestDb((err, db) => {
         assert.isNull(err);
-        getInitialReporters(db, t.params.reporter, t.params.redeemed, (err, initialReporters) => {
+        getInitialReporters(db, t.params.augur, t.params.reporter, t.params.redeemed, t.params.withRepBalance, (err, initialReporters) => {
           t.assertions(err, initialReporters);
           done();
         });
@@ -19,23 +19,44 @@ describe("server/getters/get-initial-reporters", () => {
   test({
     description: "get the initial reporter contracts owned by this reporter",
     params: {
+      augur: {
+        rpc: {
+          getNetworkID: () => {
+            return 1;
+          },
+        },
+        contracts: {
+          addresses: {
+            1: {
+              ReputationToken: "REP_TOKEN",
+            },
+          },
+        },
+      },
       reporter: "0x0000000000000000000000000000000000000b0b",
     },
     assertions: (err, initialReporters) => {
       assert.isNull(err);
-      assert.deepEqual(initialReporters, [{
-        marketId: "0x0000000000000000000000000000000000000011",
-        reporter: "0x0000000000000000000000000000000000000b0b",
-        amountStaked: 102,
-        initialReporter: "0x0000000000000000000000000000000000abe123",
-        redeemed: 0,
-      }, {
-        marketId: "0x0000000000000000000000000000000000000211",
-        reporter: "0x0000000000000000000000000000000000000b0b",
-        amountStaked: 102,
-        initialReporter: "0x0000000000000000000000000000000000abe321",
-        redeemed: 0,
-      }]);
+      assert.deepEqual(initialReporters, {
+        "0x0000000000000000000000000000000000abe123": {
+          marketId: "0x0000000000000000000000000000000000000011",
+          reporter: "0x0000000000000000000000000000000000000b0b",
+          amountStaked: 102,
+          initialReporter: "0x0000000000000000000000000000000000abe123",
+          redeemed: 0,
+          isDesignatedReporter: 0,
+          repBalance: 2000,
+        },
+        "0x0000000000000000000000000000000000abe321": {
+          marketId: "0x0000000000000000000000000000000000000211",
+          reporter: "0x0000000000000000000000000000000000000b0b",
+          amountStaked: 102,
+          initialReporter: "0x0000000000000000000000000000000000abe321",
+          redeemed: 0,
+          isDesignatedReporter: 1,
+          repBalance: 2000,
+        }
+      });
     },
   });
 });
