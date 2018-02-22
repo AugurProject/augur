af1bf9561b96c2d6fbf5da684f3c3d1f4a7ef996
nuevoalex <achap5dk@gmail.com>
2018-02-22 11:23:56 -0800

    argument fix

--- src/server/getters/get-initial-reporters.ts
+++ src/server/getters/get-initial-reporters.ts
@@ -4,7 +4,7 @@ import { Address } from "../../types";
 import { queryModifier } from "./database";
 import { InitialReportersRow, UIInitialReporters } from "../../types";
 
-export function getInitialReporters(db: Knex, augur: Augur, reporter: Address, redeemed: boolean|null|undefined, withRepBalance: false, callback: (err: Error|null, result?: any) => void): void {
+export function getInitialReporters(db: Knex, augur: Augur, reporter: Address, redeemed: boolean|null|undefined, withRepBalance: boolean|null|undefined, callback: (err: Error|null, result?: any) => void): void {
   let query = db.select(["marketID", "reporter", "amountStaked", "initialReporter", "redeemed", "isDesignatedReporter", "balances.balance AS repBalance"]).from("initial_reports")
     .join("balances", function () {
       this
