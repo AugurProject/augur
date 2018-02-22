0aba576322a29206503674c9a18112afd864078c
Stephen Sprinkle <stephen@sprinkle.io>
2018-02-22 09:04:43 -0700

    stake

--- src/modules/market/constants/permissible-periods.js
+++ src/modules/market/constants/permissible-periods.js
@@ -1,34 +1,3 @@
-// export const RANGE = {
-//   MINUTE: {
-//     value: 60000,
-//     label: 'Past minute'
-//   },
-//   HOUR: {
-//     value: 3600000,
-//     label: 'Past hour'
-//   },
-//   DAY: {
-//     value: 86400000,
-//     label: 'Past day'
-//   },
-//   WEEK: {
-//     value: 604800017,
-//     label: 'Past week'
-//   },
-//   MONTH: {
-//     value: 2629800000,
-//     label: 'Past month'
-//   },
-//   YEAR: {
-//     value: 31557600000,
-//     label: 'Past year'
-//   },
-//   FULL: {
-//     value: null,
-//     label: 'Full range'
-//   }
-// }
-
 export const RANGE_STEPS = [
   60000,
   3600000,
