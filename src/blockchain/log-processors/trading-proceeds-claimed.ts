daeb0891f89dd6fce6618853f57309c747abbaf8
Scott Bigelow <epheph@gmail.com>
2018-02-22 19:44:25 -0800

    simple test

--- src/blockchain/log-processors/trading-proceeds-claimed.ts
+++ src/blockchain/log-processors/trading-proceeds-claimed.ts
@@ -3,16 +3,28 @@ import * as Knex from "knex";
 import { FormattedEventLog, ErrorCallback } from "../../types";
 import { augurEmitter } from "../../events";
 
-// event TradingProceedsClaimed(address indexed universe, address indexed shareToken, address indexed sender, address market, uint256 numShares, uint256 numPayoutTokens, uint256 finalTokenBalance);
 export function processTradingProceedsClaimedLog(db: Knex, augur: Augur, log: FormattedEventLog, callback: ErrorCallback): void {
-  console.log("TODO: TradingProceedsClaimed");
-  console.log(log);
-  callback(null);
+  const tradingProceedsToInsert = {
+    marketId: log.market,
+    shareToken: log.shareToken,
+    account: log.sender,
+    numShares: log.numShares,
+    numPayoutTokens: log.numPayoutTokens,
+    blockNumber: log.blockNumber,
+    transactionHash: log.transactionHash,
+    logIndex: log.logIndex,
+  };
+  db("trading_proceeds").insert(tradingProceedsToInsert).asCallback((err?: Error|null) => {
+    if (err) return callback(err);
+    augurEmitter.emit("TradingProceedsClaimed", log);
+    callback(null);
+  });
 }
 
 export function processTradingProceedsClaimedLogRemoval(db: Knex, augur: Augur, log: FormattedEventLog, callback: ErrorCallback): void {
-  console.log("TODO: TradingProceedsClaimed removal");
-  console.log(log);
-  augurEmitter.emit("TradingProceedsClaimed", log);
-  callback(null);
+  db.from("trading_proceeds").where({ transactionHash: log.transactionHash, logIndex: log.logIndex }).del().asCallback((err?: Error|null) => {
+    if (err) return callback(err);
+    augurEmitter.emit("TradingProceedsClaimed", log);
+    callback(null);
+  });
 }
---
+++ test/blockchain/log-processors/trading-proceeds-claimed.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const assert = require("chai").assert;
+const setupTestDb = require("../../test.database");
+const { processTradingProceedsClaimedLog, processTradingProceedsClaimedLogRemoval } = require("../../../build/blockchain/log-processors/trading-proceeds-claimed");
+
+describe("blockchain/log-processors/trading-proceeds-claimed", () => {
+  const test = (t) => {
+    const getTradingProceeds = (db, params, callback) => db.select(["marketId", "shareToken", "account", "numShares", "numPayoutTokens"]).from("trading_proceeds").asCallback(callback);
+    it(t.description, (done) => {
+      setupTestDb((err, db) => {
+        assert.isNull(err);
+        db.transaction((trx) => {
+          processTradingProceedsClaimedLog(trx, t.params.augur, t.params.log, (err) => {
+            assert.isNull(err);
+            getTradingProceeds(trx, t.params, (err, records) => {
+              t.assertions.onAdded(err, records);
+              processTradingProceedsClaimedLogRemoval(trx, t.params.augur, t.params.log, (err) => {
+                assert.isNull(err);
+                getTradingProceeds(trx, t.params, (err, records) => {
+                  t.assertions.onRemoved(err, records);
+                  done();
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  };
+  test({
+    description: "Claim Trading Proceeds",
+    params: {
+      log: {
+        transactionHash: "TRANSACTION_HASH",
+        logIndex: 0,
+        blockNumber: 1400101,
+        market: "MARKET_ADDRESS",
+        shareToken: "SHARE_TOKEN_ADDRESS",
+        sender: "FROM_ADDRESS",
+        numShares: 140,
+        numPayoutTokens: 9000,
+      },
+    },
+    assertions: {
+      onAdded: (err, records) => {
+        assert.isNull(err);
+        assert.deepEqual(records, [{
+          account: "FROM_ADDRESS",
+          marketId: "MARKET_ADDRESS",
+          numPayoutTokens: 9000,
+          numShares: 140,
+          shareToken: "SHARE_TOKEN_ADDRESS",
+        }]);
+      },
+      onRemoved: (err, records) => {
+        assert.isNull(err);
+        assert.deepEqual(records, []);
+      },
+    },
+  });
+});
