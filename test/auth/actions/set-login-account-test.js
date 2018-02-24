8f34bd5842ed35f9a8b2037afc17a79761dfc67c
Jack Peterson <jack@tinybike.net>
2018-02-23 16:29:36 -0800

    Updated useUnlockedAccount tests

--- test/auth/actions/set-login-account-test.js
+++ test/auth/actions/set-login-account-test.js
@@ -8,110 +8,35 @@ import thunk from 'redux-thunk'
 describe(`modules/auth/actions/set-login-account.js`, () => {
   proxyquire.noPreserveCache()
   const mockStore = configureMockStore([thunk])
-  const test = (t) => {
-    it(t.description, () => {
-      const store = mockStore(t.state)
-      const AugurJS = {
-        augur: {
-          accounts: {
-            account: { ...t.state.augur.accounts.account }
-          },
-          rpc: {
-            clear: () => {}
-          },
-          from: t.state.augur.from
-        }
-      }
-      const UseUnlockedAccount = { useUnlockedAccount: () => {} }
-      const action = proxyquire('../../../src/modules/auth/actions/set-login-account.js', {
-        '../../../services/augurjs': AugurJS,
-        './use-unlocked-account': UseUnlockedAccount
-      })
-      sinon.stub(UseUnlockedAccount, 'useUnlockedAccount').callsFake(account => ({ type: 'USE_UNLOCKED_ACCOUNT', account }))
-      store.dispatch(action.setLoginAccount(t.params.autoLogin, t.params.account))
-      t.assertions(store.getActions())
-      store.clearActions()
+  const test = t => it(t.description, () => {
+    const store = mockStore()
+    const UseUnlockedAccount = { useUnlockedAccount: () => {} }
+    const action = proxyquire('../../../src/modules/auth/actions/set-login-account.js', {
+      './use-unlocked-account': UseUnlockedAccount
     })
-  }
+    sinon.stub(UseUnlockedAccount, 'useUnlockedAccount').callsFake(unlockedAccount => ({ type: 'USE_UNLOCKED_ACCOUNT', unlockedAccount }))
+    store.dispatch(action.setLoginAccount(t.params.autoLogin, t.params.account))
+    t.assertions(store.getActions())
+    store.clearActions()
+  })
   test({
-    description: 'no account available',
+    description: 'do not auto-login',
     params: {
       autoLogin: false,
       account: '0xtest'
-    },
-    state: {
-      augur: {
-        accounts: {
-          account: {}
-        },
-        from: null
-      }
     },
     assertions: (actions) => {
       assert.deepEqual(actions, [])
     }
   })
   test({
-    description: 'client-side account in augur.js, no from address',
-    params: {
-      autoLogin: false,
-      account: '0xtest'
-    },
-    state: {
-      augur: {
-        accounts: {
-          account: {
-            address: '0xb0b',
-            privateKey: Buffer.from('0x0c33')
-          },
-          from: null
-        }
-      }
-    },
-    assertions: (actions) => {
-      assert.deepEqual(actions, [])
-    }
-  })
-  test({
-    description: 'client-side account in augur.js, from address set',
-    params: {
-      autoLogin: false,
-      account: '0xtest'
-    },
-    state: {
-      augur: {
-        accounts: {
-          account: {
-            address: '0xb0b',
-            privateKey: Buffer.from('0x0c33')
-          },
-          from: '0xd00d'
-        }
-      }
-    },
-    assertions: (actions) => {
-      assert.deepEqual(actions, [])
-    }
-  })
-  test({
-    description: 'unlocked local account',
+    description: 'auto-login',
     params: {
       autoLogin: true,
       account: '0xtest'
     },
-    state: {
-      augur: {
-        accounts: {
-          account: {}
-        },
-        from: '0xd00d'
-      }
-    },
     assertions: (actions) => {
-      assert.deepEqual(actions, [{
-        type: 'USE_UNLOCKED_ACCOUNT',
-        account: '0xtest'
-      }])
+      assert.deepEqual(actions, [{ type: 'USE_UNLOCKED_ACCOUNT', unlockedAccount: '0xtest' }])
     }
   })
 })
