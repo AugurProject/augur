2f7e69e92bedbd70fb38eda9d14638be78924ce5
Scott Bigelow <epheph@gmail.com>
2018-02-27 06:18:09 -0800

    more informative error log

--- src/blockchain/log-processors/token/decrease-token-balance.ts
+++ src/blockchain/log-processors/token/decrease-token-balance.ts
@@ -9,7 +9,7 @@ interface BalanceResult {
 export function decreaseTokenBalance(db: Knex, augur: Augur, token: Address, owner: Address, amount: number, callback: ErrorCallback): void {
   db.first("balance").from("balances").where({ token, owner }).asCallback((err: Error|null, oldBalance?: BalanceResult): void => {
     if (err) return callback(err);
-    if (oldBalance == null) return callback(new Error("Could not find balance for token decrease"));
+    if (oldBalance == null) return callback(new Error(`Could not find balance for token decrease (token: ${token}, owner: ${owner})`));
     const balance = oldBalance.balance - amount;
     db.update({ balance }).into("balances").where({ token, owner }).asCallback(callback);
   });
--- src/blockchain/log-processors/token/decrease-token-supply.ts
+++ src/blockchain/log-processors/token/decrease-token-supply.ts
@@ -9,7 +9,7 @@ interface SupplyResult {
 export function decreaseTokenSupply(db: Knex, augur: Augur, token: Address, amount: number, callback: ErrorCallback): void {
   db.first("supply").from("token_supply").where({ token }).asCallback((err: Error|null, oldSupply?: SupplyResult): void => {
     if (err) return callback(err);
-    if (oldSupply == null) return callback(new Error("Could not find supply for token decrease"));
+    if (oldSupply == null) return callback(new Error(`Could not find supply for token decrease (token: ${token})`));
     const supply = oldSupply.supply - amount;
     db.update({ supply }).into("token_supply").where({ token }).asCallback(callback);
   });
