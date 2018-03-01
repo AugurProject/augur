ebe788bc96dca1dbc4e76c06817f1ce21ceedbab
John Danz <John.Danz87@gmail.com>
2018-03-01 09:33:10 -0600

    updating text by outstanding returns to have the value be in bold/capitalized as pointed out in the latest PR comment, confirmed font size was correct to mock (1.125rem / 18px)

--- src/modules/market/components/market-outstanding-returns/market-outstanding-returns.jsx
+++ src/modules/market/components/market-outstanding-returns/market-outstanding-returns.jsx
@@ -5,7 +5,12 @@ import Styles from 'modules/market/components/market-outstanding-returns/market-
 
 const OutstandingReturns = p => (
   <div className={Styles.MarketOutstandingReturns}>
-    Outstanding Returns {p.unclaimedCreatorFees.full}
+    <div className={Styles.MarketOutstandingReturns__text}>
+      Outstanding Returns
+      <span className={Styles.MarketOutstandingReturns__value}>
+        {p.unclaimedCreatorFees.full}
+      </span>
+    </div>
     <div className={Styles.MarketOutstandingReturns__actions}>
       <button
         className={Styles.MarketOutstandingReturns__collect}
