4ffb497a1f268efc7c03eabef1c6c8445a36ad31
Jack Peterson <jack@tinybike.net>
2018-03-01 10:54:27 -0800

    Fill both order types script in series

--- scripts/dp/lib/fill-both-order-types.js
+++ scripts/dp/lib/fill-both-order-types.js
@@ -4,7 +4,7 @@ var async = require("async");
 var fillOrder = require("./fill-order");
 
 function fillBothOrderTypes(augur, universe, fillerAddress, outcomeToTrade, sharesToTrade, auth, callback) {
-  async.parallel([
+  async.series([
     function (next) { fillOrder(augur, universe, fillerAddress, outcomeToTrade, sharesToTrade, "sell", auth, next); },
     function (next) { fillOrder(augur, universe, fillerAddress, outcomeToTrade, sharesToTrade, "buy", auth, next); },
   ], callback);
