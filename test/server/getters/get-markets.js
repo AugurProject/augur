770a9850a850e15660ae06556a69a4117d1946a0
Scott Bigelow <epheph@gmail.com>
2018-03-07 09:07:31 -0800

    fixing up tests

--- test/server/getters/get-markets.js
+++ test/server/getters/get-markets.js
@@ -264,4 +264,87 @@ describe("server/getters/get-markets", () => {
       ]);
     },
   });
+  test({
+    description: "get markets awaiting unknown designated reporter",
+    params: {
+      universe: "0x000000000000000000000000000000000000000b",
+      designatedReporter: "0xf0f0f0f0f0f0f0f0b0b0b0b0b0b0b0f0f0f0f0b0",
+    },
+    assertions: (err, marketsAwaitingDesignatedReporting) => {
+      assert.isNull(err);
+      assert.deepEqual(marketsAwaitingDesignatedReporting, []);
+    },
+  });
+  test({
+    description: "get all markets awaiting designated reporting, sorted ascending by volume",
+    params: {
+      universe: "0x000000000000000000000000000000000000000b",
+      sortBy: "volume",
+      isSortDescending: false,
+    },
+    assertions: (err, marketsInfo) => {
+      assert.isNull(err);
+      assert.deepEqual(marketsInfo, [
+        "0x0000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000002",
+        "0x0000000000000000000000000000000000000003",
+        "0x0000000000000000000000000000000000000012",
+        "0x0000000000000000000000000000000000000014",
+        "0x0000000000000000000000000000000000000015",
+        "0x0000000000000000000000000000000000000016",
+        "0x0000000000000000000000000000000000000017",
+      ]);
+    },
+  });
+  test({
+    description: "get all markets awaiting designated reporting by d00d",
+    params: {
+      universe: "0x000000000000000000000000000000000000000b",
+      designatedReporter: "0x000000000000000000000000000000000000d00d",
+    },
+    assertions: (err, marketsInfo) => {
+      assert.isNull(err);
+      assert.deepEqual(marketsInfo, [
+        "0x0000000000000000000000000000000000000003",
+      ]);
+    },
+  });
+  test({
+    description: "get markets upcoming, unknown designated reporter",
+    params: {
+      universe: "0x000000000000000000000000000000000000000b",
+      designatedReporter: "0xf0f0f0f0f0f0f0f0b0b0b0b0b0b0b0f0f0f0f0b0",
+    },
+    assertions: (err, marketsUpcomingDesignatedReporting) => {
+      assert.isNull(err);
+      assert.deepEqual(marketsUpcomingDesignatedReporting, []);
+    },
+  });
+  test({
+    description: "get all markets upcoming designated reporting, sorted ascending by volume",
+    params: {
+      universe: "0x000000000000000000000000000000000000000b",
+      sortBy: "volume",
+      isSortDescending: false,
+    },
+    assertions: (err, marketsUpcomingDesignatedReporting) => {
+      assert.isNull(err);
+      assert.deepEqual(marketsUpcomingDesignatedReporting, [
+        "0x0000000000000000000000000000000000000222",
+      ]);
+    },
+  });
+  test({
+    description: "get all markets upcoming designated reporting by b0b",
+    params: {
+      universe: "0x000000000000000000000000000000000000000b",
+      designatedReporter: "0x0000000000000000000000000000000000000b0b",
+    },
+    assertions: (err, marketsInfo) => {
+      assert.isNull(err);
+      assert.deepEqual(marketsInfo, [
+        "0x0000000000000000000000000000000000000222",
+      ]);
+    },
+  });
 });
