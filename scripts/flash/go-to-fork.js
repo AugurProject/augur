f1766d9e857ce677504aff887c2b7777613c4097
nuevoalex <achap5dk@gmail.com>
2018-03-09 19:30:23 -0800

    add fork script and make rep faucet take amount to mint

---
+++ scripts/flash/go-to-fork.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var chalk = require("chalk");
+var constants = require("../../src/constants");
+var doMarketContribute = require("./do-market-contribute");
+var doInitialReport = require("./do-initial-report");
+var setTimestamp = require("./set-timestamp");
+
+var ALL_THE_REP = 6000000000000000000000000;
+
+function goToFork(augur, marketId, payoutNumerators, timeAddress, auth, callback) {
+  augur.api.Market.getForkingMarket({tx: { to: marketId}}, (err, forkingMarket) => {
+    if (err) {
+      console.log(chalk.red(err));
+      return callback(err);
+    }
+    if (forkingMarket !== "0x0000000000000000000000000000000000000000") {
+      console.log(chalk.green("Successfully Forked"));
+      return callback(null);
+    }
+    augur.api.Market.getFeeWindow({tx: { to: marketId}}, (err, feeWindow) => {
+      if (err) {
+        console.log(chalk.red(err));
+        return callback(err);
+      }
+      if (feeWindow !== "0x0000000000000000000000000000000000000000") {
+        augur.api.FeeWindow.getStartTime({ tx: { to: feeWindow } }, (err, feeWindowStartTime) => {
+          if (err) {
+            console.log(chalk.red(err));
+            callback("Could not get Fee Window");
+          }
+          setTimestamp(augur, parseInt(feeWindowStartTime, 10) + 1, timeAddress, auth, function (err) {
+            if (err) {
+              console.log(chalk.red(err));
+              return callback(err);
+            }
+            doMarketContribute(augur, marketId, ALL_THE_REP, payoutNumerators, false, auth, (err, res) => {
+              if (err) {
+                console.log(chalk.red(err));
+                return callback(err);
+              }
+              goToFork(augur, marketId, payoutNumerators.reverse(), timeAddress, auth, callback);
+            });
+          });
+        });
+      } else {
+        doInitialReport(augur, marketId, payoutNumerators, false, auth, (err, res) => {
+          if (err) {
+            console.log(chalk.red(err));
+            return callback(err);
+          }
+          goToFork(augur, marketId, payoutNumerators.reverse(), timeAddress, auth, callback);
+        });
+      }
+    });
+  });
+}
+
+module.exports = goToFork;
