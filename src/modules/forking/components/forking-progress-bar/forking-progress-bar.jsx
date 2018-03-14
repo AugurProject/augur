0fa5a7f77386d21412f54730ff80682825134aad
nuevoalex <achap5dk@gmail.com>
2018-03-14 10:06:59 -0700

    simplify component & use forking period constant

--- src/modules/forking/components/forking-progress-bar/forking-progress-bar.jsx
+++ src/modules/forking/components/forking-progress-bar/forking-progress-bar.jsx
@@ -1,53 +1,48 @@
-import React, { Component } from 'react'
+import { augur } from 'services/augurjs'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { getDaysRemaining } from 'utils/format-date'
 
 import Styles from 'modules/forking/components/forking-progress-bar/forking-progress-bar.styles'
 
-class ForkingProgressBar extends Component {
-  constructor(props) {
-    super(props)
+const SECONDS_PER_DAY = 3600 * 24
 
-    this.state = {}
+const ForkingProgressBar = (p) => {
+  const forkingPeriodInDays = augur.rpc.constants.CONTRACT_INTERVAL.FORK_DURATION_SECONDS / SECONDS_PER_DAY
+
+  const daysRemaining = getDaysRemaining(this.props.forkEndTime, this.props.currentTime)
+
+  const percentagePassed = (100 * (forkingPeriodInDays - daysRemaining)) / forkingPeriodInDays
+
+  // Move the label to the left if the bar is pushing the label too far right
+  const daysLeftOnRightSide = percentagePassed < 85
+
+  const percentageCompleteWidth = {
+    width: `${percentagePassed}%`,
   }
 
-  render() {
-    const forkingPeriodInDays = 60
+  const percentageCompleteMarginLeft = {
+    marginLeft: `${percentagePassed}%`,
+  }
 
-    const daysRemaining = getDaysRemaining(this.props.forkEndTime, this.props.currentTime)
-
-    const percentagePassed = (100 * (forkingPeriodInDays - daysRemaining)) / forkingPeriodInDays
-
-    // Move the label to the left if the bar is pushing the label too far right
-    const daysLeftOnRightSide = percentagePassed < 85
-
-    const percentageCompleteWidth = {
-      width: `${percentagePassed}%`,
-    }
-
-    const percentageCompleteMarginLeft = {
-      marginLeft: `${percentagePassed}%`,
-    }
-
-    return (
-      <article>
-        <section className={Styles.ForkingProgressBar}>
-          <div className={Styles.ForkingProgressBar__background}>
-            <div
-              className={Styles.ForkingProgressBar__progress}
-              style={percentageCompleteWidth}
-            />
-            <div
-              className={Styles.ForkingProgressBar__days_left_label}
-              style={daysLeftOnRightSide ? percentageCompleteMarginLeft : { position: 'absolute' }}
-            >
-              {daysRemaining} days left
-            </div>
+  return (
+    <article>
+      <section className={Styles.ForkingProgressBar}>
+        <div className={Styles.ForkingProgressBar__background}>
+          <div
+            className={Styles.ForkingProgressBar__progress}
+            style={percentageCompleteWidth}
+          />
+          <div
+            className={Styles.ForkingProgressBar__days_left_label}
+            style={daysLeftOnRightSide ? percentageCompleteMarginLeft : { position: 'absolute' }}
+          >
+            {daysRemaining} days left
           </div>
-        </section>
-      </article>
-    )
-  }
+        </div>
+      </section>
+    </article>
+  )
 }
 
 ForkingProgressBar.propTypes = {
