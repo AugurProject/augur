36460e923070ea7358d565fb79bb8c8f5aa5504a
Tom Haile <bradford.haile@gmail.com>
2018-03-22 16:56:59 -0500

    simply removed 'No' from binary market liquidity in create market.

--- src/modules/create-market/components/create-market-form-liquidity/create-market-form-liquidity.jsx
+++ src/modules/create-market/components/create-market-form-liquidity/create-market-form-liquidity.jsx
@@ -404,13 +404,6 @@ export default class CreateMarketLiquidity extends Component {
                       >Yes
                       </button>
                     </li>
-                    <li>
-                      <button
-                        className={classNames({ [`${StylesForm.active}`]: s.selectedOutcome === 'No' })}
-                        onClick={() => this.setState({ selectedOutcome: 'No' })}
-                      >No
-                      </button>
-                    </li>
                   </ul>
                 </li>
               }
