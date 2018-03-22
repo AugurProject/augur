2737a1df6d621477ff68505c533ad063330d305b
Paul Gebheim <paul@onetaste.us>
2018-03-22 00:32:11 +0000

    Fix string numbers for token minting/burning

--- src/blockchain/log-processors/token/decrease-token-supply.ts
+++ src/blockchain/log-processors/token/decrease-token-supply.ts
@@ -1,16 +1,17 @@
 import { Augur } from "augur.js";
 import * as Knex from "knex";
+import { BigNumber } from "bignumber.js";
 import { Address, ErrorCallback } from "../../../types";
 
 interface SupplyResult {
-  supply: number;
+  supply: BigNumber;
 }
 
-export function decreaseTokenSupply(db: Knex, augur: Augur, token: Address, amount: number, callback: ErrorCallback): void {
+export function decreaseTokenSupply(db: Knex, augur: Augur, token: Address, amount: BigNumber, callback: ErrorCallback): void {
   db.first("supply").from("token_supply").where({ token }).asCallback((err: Error|null, oldSupply?: SupplyResult): void => {
     if (err) return callback(err);
     if (oldSupply == null) return callback(new Error(`Could not find supply for token decrease (token: ${token})`));
-    const supply = oldSupply.supply - amount;
-    db.update({ supply }).into("token_supply").where({ token }).asCallback(callback);
+    const supply = oldSupply.supply.minus(amount);
+    db.update({ supply: supply.toFixed() }).into("token_supply").where({ token }).asCallback(callback);
   });
 }
--- src/blockchain/log-processors/tokens-transferred.ts
+++ src/blockchain/log-processors/tokens-transferred.ts
@@ -11,22 +11,22 @@ import { decreaseTokenBalance } from "./token/decrease-token-balance";
 
 export function processTokensTransferredLog(db: Knex, augur: Augur, log: FormattedEventLog, callback: ErrorCallback): void {
   const token = log.token || log.address;
-  const value = new BigNumber(log.value || log.amount, 10).toFixed();
+  const value = new BigNumber(log.value || log.amount, 10);
   const tokenTransferDataToInsert = {
     transactionHash: log.transactionHash,
     logIndex: log.logIndex,
     sender: log.from,
     recipient: log.to,
-    token,
-    value,
+    value: value.toFixed(),
     blockNumber: log.blockNumber,
+    token
   };
   db.insert(tokenTransferDataToInsert).into("transfers").asCallback((err: Error|null): void => {
     if (err) return callback(err);
     augurEmitter.emit("TokensTransferred", tokenTransferDataToInsert);
     parallel([
-      (next: AsyncCallback): void => increaseTokenBalance(db, augur, token, log.to, Number(value), next),
-      (next: AsyncCallback): void => decreaseTokenBalance(db, augur, token, log.from, Number(value), next),
+      (next: AsyncCallback): void => increaseTokenBalance(db, augur, token, log.to, value, next),
+      (next: AsyncCallback): void => decreaseTokenBalance(db, augur, token, log.from, value, next),
     ], (err: Error|null): void => {
       if (err) return callback(err);
       handleShareTokenTransfer(db, augur, log, callback);
@@ -39,10 +39,10 @@ export function processTokensTransferredLogRemoval(db: Knex, augur: Augur, log: 
     if (err) return callback(err);
     augurEmitter.emit("TokensTransferred", log);
     const token = log.token || log.address;
-    const value = log.value || log.amount;
+    const value = new BigNumber(log.value || log.amount, 10);
     parallel([
-      (next: AsyncCallback): void => increaseTokenBalance(db, augur, token, log.from, Number(value), next),
-      (next: AsyncCallback): void => decreaseTokenBalance(db, augur, token, log.to, Number(value), next),
+      (next: AsyncCallback): void => increaseTokenBalance(db, augur, token, log.from, value, next),
+      (next: AsyncCallback): void => decreaseTokenBalance(db, augur, token, log.to, value, next),
     ], (err: Error|null): void => {
       if (err) return callback(err);
       handleShareTokenTransfer(db, augur, log, callback);
