60e55aa748bad0335dc5f2def1a89ccd687cdb4e
Scott Bigelow <epheph@gmail.com>
2018-03-22 16:35:33 -0700

    allow the system to work coming from clean or pre-populated

--- scripts/setup-local-dev.sh
+++ scripts/setup-local-dev.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 trap "kill 0" EXIT
 
-set -e;
+set -ex;
 
-export USE_NORMAL_TIME="false";
-export ETHEREUM_HOST="127.0.0.1";
-export ETHEREUM_GAS_PRICE_IN_NANOETH="1";
-export ETHEREUM_HTTP=http://127.0.0.1:8545;
-export ETHEREUM_WS=http://127.0.0.1:8546;
+export USE_POPULATED_CONTRACTS="${USE_POPULATED_CONTRACTS:-true}"
 
-export ETHEREUM_PRIVATE_KEY="fae42052f82bed612a724fec3632f325f377120592c75bb78adfcceae6470c5a";
+export USE_NORMAL_TIME=${USE_NORMAL_TIME:-false}
+export ETHEREUM_HOST=${ETHEREUM_HOST:-127.0.0.1}
+export ETHEREUM_GAS_PRICE_IN_NANOETH=${ETHEREUM_GAS_PRICE_IN_NANOETH:-1}
+export ETHEREUM_HTTP=${ETHEREUM_HTTP:-http://127.0.0.1:8545}
+export ETHEREUM_WS=${ETHEREUM_WS:-http://127.0.0.1:8546}
+
+export ETHEREUM_PRIVATE_KEY=${ETHEREUM_PRIVATE_KEY:-fae42052f82bed612a724fec3632f325f377120592c75bb78adfcceae6470c5a}
 
 CONTAINER_NAME="geth-node";
-DOCKER_IMAGE="augurproject/dev-pop-geth:core-$(npm view augur-core version)";
+if $USE_POPULATED_CONTRACTS; then
+  DOCKER_IMAGE="augurproject/dev-pop-geth:core-$(npm view augur-core version)";
+else
+  DOCKER_IMAGE="augurproject/dev-node-geth:latest";
+fi
 
 if [ "$(docker ps -a | grep $CONTAINER_NAME)" ]; then
   echo "Kill running container...";
@@ -32,7 +38,12 @@ yarn;
 yarn run build;
 yarn link;
 
-docker cp $CONTAINER_NAME:/augur.js/src/contracts/addresses.json src/contracts/addresses.json;
+if $USE_POPULATED_CONTRACTS; then
+  docker cp $CONTAINER_NAME:/augur.js/src/contracts/addresses.json src/contracts/addresses.json
+  docker cp $CONTAINER_NAME:/augur.js/src/contracts/upload-block-numbers.json src/contracts/upload-block-numbers.json
+else
+  yarn run deploy:environment
+fi
 
 popd;
 
