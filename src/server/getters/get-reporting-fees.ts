79737ba95abbbc292f09878daa5eebd234f7fadd
Scott Bigelow <epheph@gmail.com>
2018-03-23 14:13:11 -0700

    new format

--- src/server/getters/get-reporting-fees.ts
+++ src/server/getters/get-reporting-fees.ts
@@ -3,18 +3,24 @@ import * as Knex from "knex";
 import * as _ from "lodash";
 import { Address } from "../../types";
 
-interface FeeDetails {
-  ethFees: string;
-  repToReclaim: string;
-  repEarned: string;
+export interface FeeDetails {
+  unclaimedEth: string;
+  unclaimedRepStaked: string;
+  unclaimedRepEarned: string;
+  claimedEth: string;
+  claimedRepStaked: string;
+  claimedRepEarned: string;
 }
 
 export function getReportingFees(db: Knex, reporter: Address|null, universe: Address|null, feeWindow: Address|null, callback: (err: Error|null, result?: FeeDetails) => void): void {
   if (universe == null || feeWindow) return callback(new Error("Must provide universe or feeWindow"));
   const response = {
-    ethFees: "1",
-    repToReclaim: "2",
-    repEarned: "3",
+    unclaimedEth: "1",
+    unclaimedRepStaked: "2",
+    unclaimedRepEarned: "3",
+    claimedEth: "4",
+    claimedRepStaked: "5",
+    claimedRepEarned: "6",
   };
   callback(null, response);
 }
