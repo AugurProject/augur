dc962026e7eb4e483640aac68d50fed68e926c8b
Jack Peterson <jack@tinybike.net>
2018-03-24 11:46:03 -0700

    Added price to re-emitted OrderCanceled logs

--- src/blockchain/log-processors/order-canceled.ts
+++ src/blockchain/log-processors/order-canceled.ts
@@ -3,15 +3,16 @@ import * as Knex from "knex";
 import { Bytes32, FormattedEventLog, ErrorCallback, OrderState } from "../../types";
 import { augurEmitter } from "../../events";
 
-interface MarketIDAndOutcome {
+interface MarketIDAndOutcomeAndPrice {
   marketId: Bytes32;
   outcome: number;
+  price: string|number;
 }
 
 export function processOrderCanceledLog(db: Knex, augur: Augur, log: FormattedEventLog, callback: ErrorCallback): void {
   db.from("orders").where("orderId", log.orderId).update({ orderState: OrderState.CANCELED }).asCallback((err: Error|null): void => {
     if (err) return callback(err);
-    db.first("marketId", "outcome").from("orders").where("orderId", log.orderId).asCallback((err: Error|null, ordersRow?: MarketIDAndOutcome): void => {
+    db.first("marketId", "outcome", "price").from("orders").where("orderId", log.orderId).asCallback((err: Error|null, ordersRow?: MarketIDAndOutcomeAndPrice): void => {
       if (err) return callback(err);
       augurEmitter.emit("OrderCanceled", Object.assign({}, log, ordersRow));
       callback(null);
@@ -22,7 +23,7 @@ export function processOrderCanceledLog(db: Knex, augur: Augur, log: FormattedEv
 export function processOrderCanceledLogRemoval(db: Knex, augur: Augur, log: FormattedEventLog, callback: ErrorCallback): void {
   db.from("orders").where("orderId", log.orderId).update({ orderState: OrderState.OPEN }).asCallback((err: Error|null): void => {
     if (err) return callback(err);
-    db.first("marketId", "outcome").from("orders").where("orderId", log.orderId).asCallback((err: Error|null, ordersRow?: MarketIDAndOutcome): void => {
+    db.first("marketId", "outcome", "price").from("orders").where("orderId", log.orderId).asCallback((err: Error|null, ordersRow?: MarketIDAndOutcomeAndPrice): void => {
       if (err) return callback(err);
       augurEmitter.emit("OrderCanceled", Object.assign({}, log, ordersRow));
       callback(null);
