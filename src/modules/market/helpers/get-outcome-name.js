0e5321d8d63311f94ef0b576682ff090dbae7dd3
Jack Peterson <jack@tinybike.net>
2018-03-31 10:39:22 -0700

    Removed unused market helper getOutcomeName

--- src/modules/market/helpers/get-outcome-name.js
+++
@@ -1,6 +0,0 @@
-import { BINARY, SCALAR } from 'modules/markets/constants/market-types'
-
-export default function getOutcomeName(marketType, marketId, outcome, marketOutcomesData) {
-  if (marketType === BINARY || marketType === SCALAR) return null
-  return (marketOutcomesData ? marketOutcomesData[outcome] : {}).name
-}
