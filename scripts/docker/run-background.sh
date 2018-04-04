db0e1474703ed7bb0fc89635a8a074267d660162
Justin <328965+justinbarry@users.noreply.github.com>
2018-04-04 09:13:25 -0700

    Remove container upon process exit (#319)

--- scripts/docker/run-background.sh
+++ scripts/docker/run-background.sh
@@ -5,5 +5,5 @@ TAG=core-$(npm view augur-core version)
 
 echo "Running Geth with contracts deployed from augur-core@${TAG}"
 
-docker run -e GETH_VERBOSITY=4 -it -d -p 8545:8545 -p 8546:8546 --name ${CONTAINER_NAME:-geth-node} $IMAGE:$TAG
+docker run -e GETH_VERBOSITY=4 -it -d --rm -p 8545:8545 -p 8546:8546 --name ${CONTAINER_NAME:-geth-node} $IMAGE:$TAG
 
