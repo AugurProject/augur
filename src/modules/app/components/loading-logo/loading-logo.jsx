efa96d9deb7b23ccc07460cf14cc4079fe988776
Tom Haile <bradford.haile@gmail.com>
2018-04-04 20:09:28 -0500

    put in a time delay so animation can get done before static image shown when isLoading is false

--- src/modules/app/components/loading-logo/loading-logo.jsx
+++ src/modules/app/components/loading-logo/loading-logo.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { AugurLoadingLogo } from 'modules/common/components/icons'
 import Styles from 'modules/app/components/loading-logo/loading-logo.styles'
 
-const LoadingLogo = props => (
-  <div className={props.isLoading ? Styles.LoadingLogo__AugurLogo : Styles.LoadingLogo__Not__AugurLogo}>
-    <span>{ AugurLoadingLogo }</span>
-  </div>
-)
+export default class LoadingLogo extends Component {
+  static propTypes = {
+    isLoading: PropTypes.bool,
+  }
 
-LoadingLogo.propTypes = {
-  isLoading: PropTypes.bool,
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loading: props.isLoading,
+    }
+  }
+
+  componentWillUnmount = () => {
+    this.timeouts = null
+  };
+
+  render() {
+    const s = this.state
+    const { isLoading } = this.props
+    // put in lag so animation can finish
+    this.timeouts = setTimeout(() => {
+      s.loading = isLoading
+    }, 5000)
+
+    return (
+      <div className={s.loading ? Styles.LoadingLogo__AugurLogo : Styles.LoadingLogo__Not__AugurLogo}>
+        <span>{ AugurLoadingLogo }</span>
+      </div>
+    )
+  }
 }
 
-export default LoadingLogo
