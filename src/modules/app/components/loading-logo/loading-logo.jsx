d36b3b2e647339ac003c09442748172b96f8d0be
John Danz <John.Danz87@gmail.com>
2018-04-06 10:19:13 -0500

    removed unneeded span, cleaned up loadingLogo to make it a pure component, removed timeout, updated less

--- src/modules/app/components/loading-logo/loading-logo.jsx
+++ src/modules/app/components/loading-logo/loading-logo.jsx
@@ -1,40 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import { AugurLoadingLogo } from 'modules/common/components/icons'
 import Styles from 'modules/app/components/loading-logo/loading-logo.styles'
 
-export default class LoadingLogo extends Component {
-  static propTypes = {
-    isLoading: PropTypes.bool,
-  }
+const LoadingLogo = (p) => {
+  const { isLoading } = p
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      loading: props.isLoading,
-    }
-  }
-
-  componentWillUnmount = () => {
-    this.timeouts = null
-  };
-
-  render() {
-    const s = this.state
-    const { isLoading } = this.props
-    // put in lag so animation can finish
-    this.timeouts = setTimeout(() => {
-      s.loading = isLoading
-    }, 2500)
-
-    return (
-      <div
-        className={s.loading ? Styles.LoadingLogo__running : Styles.LoadingLogo__paused}
-      >
-        <span>{ AugurLoadingLogo }</span>
-      </div>
-    )
-  }
+  return (
+    <div
+      className={classNames(Styles.LoadingLogo, {
+        [Styles.running]: isLoading,
+        [Styles.paused]: !isLoading,
+      })}
+    >
+      { AugurLoadingLogo }
+    </div>
+  )
 }
 
+LoadingLogo.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+}
+
+export default LoadingLogo
