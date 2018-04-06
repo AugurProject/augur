b6cf76b9d172d6dadc84ee8ddc7c07852f25a96f
Tom Haile <bradford.haile@gmail.com>
2018-04-06 13:33:34 -0500

    wired up event on animation stop in order to change animation based on prop

--- src/modules/app/components/loading-logo/loading-logo.jsx
+++ src/modules/app/components/loading-logo/loading-logo.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import { AugurLoadingLogo } from 'modules/common/components/icons'
 import Styles from 'modules/app/components/loading-logo/loading-logo.styles'
 
-const LoadingLogo = (p) => {
-  const { isLoading } = p
+export default class LoadingLogo extends Component {
 
-  return (
-    <div
-      className={classNames(Styles.LoadingLogo, {
-        [Styles.running]: isLoading,
-        [Styles.paused]: !isLoading,
-      })}
-    >
-      { AugurLoadingLogo }
-    </div>
-  )
+  static propTypes = {
+    isLoading: PropTypes.bool.isRequired,
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loading: this.props.isLoading,
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.props.isLoading) {
+      this.state.loading = this.props.isLoading
+    }
+  }
+
+  animateEnd() {
+    this.state.loading = this.props.isLoading
+  }
+
+  render() {
+    const { loading } = this.state
+    return (
+      <div
+        className={classNames(Styles.LoadingLogo, {
+          [Styles.running]: loading,
+          [Styles.paused]: !loading,
+        })}
+        onAnimationEnd={() => (this.animateEnd())}
+      >
+        { AugurLoadingLogo }
+      </div>
+    )
+  }
 }
-
-LoadingLogo.propTypes = {
-  isLoading: PropTypes.bool.isRequired,
-}
-
-export default LoadingLogo
