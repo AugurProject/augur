e02caf44a8dcc75056927d18bcb0eaa81c36bb2a
Paul Gebheim <paul@onetaste.us>
2018-04-17 20:34:51 +0000

    Fix release script for :stable

--- scripts/release-stable.sh
+++ scripts/release-stable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 version=$(date -u +%Y-%m-%d-%H%M)
-docker build . --tag augurproject/augur-node:stable --tag augurprojet/augur:$version || exit 1
+docker build . --tag augurproject/augur-node:stable --tag augurprojet/augur-node:$version || exit 1
 
-docker push augurproject/augur:$version || exit 1
-docker push augurproject/augur:stable || exit 1
+docker push augurproject/augur-node:$version || exit 1
+docker push augurproject/augur-node:stable || exit 1
