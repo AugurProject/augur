c7219293d766ca6054c844e38a274d18d2b183de
Stephen Sprinkle <stephen@sprinkle.io>
2018-04-19 13:50:57 -0600

    initial refactor to make outcomes chart more functional + styling improvements

--- src/modules/market/components/market-outcomes-chart/market-outcomes-chart.jsx
+++ src/modules/market/components/market-outcomes-chart/market-outcomes-chart.jsx
@@ -30,56 +30,45 @@ export default class MarketOutcomesChart extends Component {
   }
 
   componentDidMount() {
-    this.drawChart()
+    this.drawChart(this.props.outcomes)
 
     window.addEventListener('resize', this.drawChart)
   }
 
-  componentDidUpdate(prevProps) {
-    const { outcomes } = this.props
-    if (!isEqual(prevProps.outcomes, outcomes)) this.drawChart()
+  componentWillUpdate(nextProps, nextState) {
+    const {
+      outcomes,
+    } = this.props
+
+    if (!isEqual(outcomes, nextProps.outcomes)) this.drawChart(nextProps.outcomes)
   }
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.drawChart)
   }
 
-  drawChart() {
+  drawChart(outcomes) {
     if (this.outcomesChart) {
+      const drawParams = determineDrawParams({
+        drawContainer: this.outcomesChart,
+        outcomes,
+      })
+
       const fauxDiv = new ReactFauxDOM.Element('div')
       const chart = d3.select(fauxDiv).append('svg')
+        .attr('id', 'outcomes_chart')
+        .attr('width', drawParams.width)
+        .attr('height', drawParams.height)
 
-      const { outcomes } = this.props
-
-      const margin = {
-        top: 20,
-        right: 0,
-        bottom: 30,
-        left: 50,
-      }
-
-      const width = this.outcomesChart.clientWidth
-      const height = this.outcomesChart.clientHeight
-
-      chart.attr('id', 'outcomes_chart')
-
-      chart.attr('width', width)
-      chart.attr('height', height)
-
-      const xDomain = outcomes.reduce((p, outcome) => [...p, ...outcome.priceTimeSeries.map(dataPoint => dataPoint.timestamp)], [])
-      const yDomain = outcomes.reduce((p, outcome) => [...p, ...outcome.priceTimeSeries.map(dataPoint => createBigNumber(dataPoint.price).toNumber())], [])
-
-      const xScale = d3.scaleTime()
-        .domain(d3.extent(xDomain))
-        .range([margin.left, width - margin.right - 1])
-
-      const yScale = d3.scaleLinear()
-        .domain(d3.extent(yDomain))
-        .range([height - margin.bottom, margin.top])
+      drawTicks({
+        outcomes,
+        drawParams,
+        chart,
+      })
 
       const outcomeLine = d3.line()
-        .x(d => xScale(d.timestamp))
-        .y(d => yScale(createBigNumber(d.price).toNumber()))
+        .x(d => drawParams.xScale(d.timestamp))
+        .y(d => drawParams.yScale(createBigNumber(d.price).toNumber()))
 
       // TODO -- refactor this to be more correct in d3 conventions, i.e. -- chart.select....
       outcomes.forEach((outcome, i) => {
@@ -92,13 +81,8 @@ export default class MarketOutcomesChart extends Component {
 
       chart.append('g')
         .attr('class', Styles['outcomes-axis'])
-        .attr('transform', `translate(0, ${height - margin.bottom})`)
-        .call(d3.axisBottom(xScale))
-
-      chart.append('g')
-        .attr('class', Styles['outcomes-axis'])
-        .attr('transform', `translate(${margin.left}, 0)`)
-        .call(d3.axisLeft(yScale))
+        .attr('transform', `translate(0, ${drawParams.containerHeight - drawParams.chartDim.bottom})`)
+        .call(d3.axisBottom(drawParams.xScale))
 
       this.setState({ chart: fauxDiv.toReact() })
     }
@@ -140,5 +124,103 @@ export default class MarketOutcomesChart extends Component {
   }
 }
 
-// NOTE -- this goes into the self closing div
-// <span >Filter (TODO)</span>
+function determineDrawParams(options) {
+  const {
+    drawContainer,
+    outcomes,
+  } = options
+
+  console.log('outcomes -- ', outcomes)
+
+  const chartDim = {
+    top: 20,
+    right: 0,
+    bottom: 30,
+    left: 50,
+  }
+
+  const containerWidth = drawContainer.clientWidth
+  const containerHeight = drawContainer.clientHeight
+
+  const xDomain = outcomes.reduce((p, outcome) => [...p, ...outcome.priceTimeSeries.map(dataPoint => dataPoint.timestamp)], [])
+  const yDomain = d3.extent(outcomes.reduce((p, outcome) => [...p, ...outcome.priceTimeSeries.map(dataPoint => createBigNumber(dataPoint.price).toNumber())], []))
+
+  const xScale = d3.scaleTime()
+    .domain(d3.extent(xDomain))
+    .range([chartDim.left, containerWidth - chartDim.right - 1])
+
+  const yScale = d3.scaleLinear()
+    .domain(yDomain)
+    .range([containerHeight - chartDim.bottom, chartDim.top])
+
+  return {
+    chartDim,
+    containerWidth,
+    containerHeight,
+    xDomain,
+    yDomain,
+    xScale,
+    yScale,
+  }
+}
+
+function drawTicks(options) {
+  const {
+    drawParams,
+    outcomes,
+    chart,
+  } = options
+
+  // Y axis
+  //  Bounds
+  //    Top
+  chart.append('line')
+    .attr('class', Styles['MarketOutcomesChart__bounding-line'])
+    .attr('x1', 0)
+    .attr('x2', drawParams.containerWidth)
+    .attr('y1', drawParams.chartDim.top)
+    .attr('y2', drawParams.chartDim.top)
+  //    Bottom
+  chart.append('line')
+    .attr('class', Styles['MarketOutcomesChart__bounding-line'])
+    .attr('x1', 0)
+    .attr('x2', drawParams.containerWidth)
+    .attr('y1', drawParams.containerHeight - drawParams.chartDim.bottom)
+    .attr('y2', drawParams.containerHeight - drawParams.chartDim.bottom)
+
+  const numberOfTicks = 5 // NOTE -- excludes bounds
+  const range = Math.abs(drawParams.yDomain[1] - drawParams.yDomain[0])
+  const interval = range / numberOfTicks
+
+  const ticks = [...new Array(5)].map((_item, i) => {
+    if (i === 0) return drawParams.yDomain[0] + interval
+    return drawParams.yDomain[0] + ((i + 1) * interval)
+  })
+
+  console.log('ticks -- ', drawParams.yDomain, range, ticks)
+
+  chart.append('g')
+    .selectAll('line')
+    .data(ticks)
+    .enter()
+    .append('line')
+    .classed(Styles['MarketOutcomesChart__tick-line'], true)
+    .classed(Styles['MarketOutcomesChart__tick-line--excluded'], (d, i) => i + 1 === ticks.length)
+    .attr('x1', 0)
+    .attr('x2', drawParams.containerWidth)
+    .attr('y1', d => drawParams.yScale(d))
+    .attr('y2', d => drawParams.yScale(d))
+
+  // chart.append('g')
+  //   .attr('class', Styles['outcomes-axis'])
+  //   .attr('transform', `translate(${drawParams.chartDim.left}, 0)`)
+  //   .call(d3.axisLeft(drawParams.yScale))
+}
+
+function drawAxisLabels() {
+
+}
+
+function drawSeries() {
+
+}
