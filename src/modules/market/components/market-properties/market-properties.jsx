67561c9e91fec36c09f64472e8d57708e6364a17
Tom Haile <bradford.haile@gmail.com>
2018-04-18 21:35:43 -0500

    don't let user dispute market if they don't have REP.

--- src/modules/market/components/market-properties/market-properties.jsx
+++ src/modules/market/components/market-properties/market-properties.jsx
@@ -21,6 +21,7 @@ const MarketProperties = (p) => {
   const isScalar = p.marketType === SCALAR
   const consensus = getValue(p, isScalar ? 'consensus.winningOutcome' : 'consensus.outcomeName')
   const linkType = (p.isForking && p.linkType === TYPE_DISPUTE) ? TYPE_VIEW : p.linkType
+  const disableDispute = p.loginAccount.rep === '0' && p.linkType === TYPE_DISPUTE
 
   return (
     <article>
@@ -68,7 +69,7 @@ const MarketProperties = (p) => {
           }
           { (linkType === undefined || (linkType && linkType !== TYPE_CLOSED)) &&
             <MarketLink
-              className={Styles.MarketProperties__trade}
+              className={disableDispute ? Styles.MarketProperties__trade__disabled : Styles.MarketProperties__trade}
               id={p.id}
               formattedDescription={p.formattedDescription}
               linkType={linkType}
