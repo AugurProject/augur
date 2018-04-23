9ee06b3da1dc94ba8755d8ff4787560a1ef35228
Jack Peterson <jack@tinybike.net>
2018-04-23 12:56:29 -0700

    Added accountType and network fields to uPortSigner

--- src/modules/auth/helpers/uport-signer.js
+++ src/modules/auth/helpers/uport-signer.js
@@ -1,34 +1,24 @@
 import { Connect } from 'uport-connect'
-
+import { augur } from 'services/augurjs'
 import { updateModal } from 'modules/modal/actions/update-modal'
 import { closeModal } from 'modules/modal/actions/close-modal'
-
 import { MODAL_UPORT } from 'modules/modal/constants/modal-types'
 
-export default function uPortSigner(txObj, dispatch) {
-  const uPortSigner = new Connect(
-    'AUGUR -- DEV',
-    {
-      clientId: '2ofGiHuZhhpDMAQeDxjoDhEsUQd1MayECgd',
-    },
-  )
-    .sendTransaction(txObj, (uri) => {
-      dispatch(updateModal({
-        type: MODAL_UPORT,
-        uri,
-      }))
-    })
-
-  uPortSigner.then((res) => {
-    dispatch(closeModal())
-  })
-    .catch((err) => {
-      dispatch(updateModal({
-        type: MODAL_UPORT,
-        error: `Failed to Sign with "${err}"`,
-        canClose: true,
-      }))
-    })
-
-  return uPortSigner
+const networks = {
+  1: 'mainnet',
+  3: 'ropsten',
+  4: undefined, // rinkeby (default)
+  42: 'kovan',
 }
+
+export const uPortSigner = transaction => dispatch => new Connect('AUGUR -- DEV', {
+  clientId: '2ofGiHuZhhpDMAQeDxjoDhEsUQd1MayECgd',
+  accountType: 'keypair',
+  network: networks[augur.rpc.getNetworkID()],
+}).sendTransaction(transaction, uri => (
+  dispatch(updateModal({ type: MODAL_UPORT, uri })))
+).then(() => (
+  dispatch(closeModal()))
+).catch(err => (
+  dispatch(updateModal({ type: MODAL_UPORT, error: `Failed to Sign with "${err}"`, canClose: true })))
+)
