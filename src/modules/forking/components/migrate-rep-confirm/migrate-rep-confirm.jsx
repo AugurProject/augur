a046892efdf11016c7dae1d847e140603ec6cc3c
phoebemirman <phoebe.mirman@gmail.com>
2018-04-27 13:11:02 -0700

    Revert "deleted extra div that was adding inconsistent padding"
    This reverts commit 4c48d88aacbef56cceb20ef8e8dc7726ec2a1055.

--- src/modules/forking/components/migrate-rep-confirm/migrate-rep-confirm.jsx
+++ src/modules/forking/components/migrate-rep-confirm/migrate-rep-confirm.jsx
@@ -1,30 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import FormStyles from 'modules/common/less/form'
 import ConfirmStyles from 'modules/common/less/confirm-table'
 
 const MigrateRepConfirm = p => (
-  <article className={ConfirmStyles.Confirm}>
-    <h2 className={ConfirmStyles.Confirm__heading}>Confirm Migration</h2>
-    <div className={ConfirmStyles['Confirm__wrapper--wide']}>
-      <div className={ConfirmStyles.Confirm__creation}>
-        <ul className={ConfirmStyles['Confirm__list--left-align']}>
-          <li>
-            <span>Outcome</span>
-            <span>{ p.selectedOutcomeName }</span>
-          </li>
-          <li>
-            <span>Migrating</span>
-            <span>{ p.repAmount } REP</span>
-          </li>
-          <li>
-            <span>Gas</span>
-            <span>{ p.gasEstimate } ETH</span>
-          </li>
-        </ul>
+  <article className={FormStyles.Form__fields}>
+    <div className={ConfirmStyles.Confirm}>
+      <h2 className={ConfirmStyles.Confirm__heading}>Confirm Migration</h2>
+      <div className={ConfirmStyles['Confirm__wrapper--wide']}>
+        <div className={ConfirmStyles.Confirm__creation}>
+          <ul className={ConfirmStyles['Confirm__list--left-align']}>
+            <li>
+              <span>Outcome</span>
+              <span>{ p.selectedOutcomeName }</span>
+            </li>
+            <li>
+              <span>Migrating</span>
+              <span>{ p.repAmount } REP</span>
+            </li>
+            <li>
+              <span>Gas</span>
+              <span>{ p.gasEstimate } ETH</span>
+            </li>
+          </ul>
+        </div>
       </div>
+      <div className={ConfirmStyles.Confirm__note_text}>After migrating your REP, select which universe to view on the Account: Universe page.</div>
     </div>
-    <div className={ConfirmStyles.Confirm__note_text}>After migrating your REP, select which universe to view on the Account: Universe page.</div>
   </article>
 )
 
