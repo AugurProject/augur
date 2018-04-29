376ccd17d0faded4f6b2cd789efe6af5b025b34b
Paul Gebheim <paul@onetaste.us>
2018-04-29 05:11:09 +0000

    Start of contraints

--- src/migrations/20171005123258_balances.ts
+++ src/migrations/20171005123258_balances.ts
@@ -2,11 +2,10 @@ import * as Knex from "knex";
 
 exports.up = async (knex: Knex): Promise<any> => {
   return knex.schema.dropTableIfExists("balances").then((): PromiseLike<any> => {
-    // TODO: re-add to balance CONSTRAINT nonnegativeBalance CHECK (balance >= 0)
     return knex.schema.raw(`CREATE TABLE balances (
       owner varchar(66) NOT NULL,
       token varchar(66) NOT NULL,
-      balance VARCHAR(255) NOT NULL DEFAULT "0",
+      balance varchar(255) NOT NULL DEFAULT "0" CONSTRAINT nonnegativeBalance CHECK (ltrim(balance, '-') = balance),
       UNIQUE(owner, token)
     )`);
   });
