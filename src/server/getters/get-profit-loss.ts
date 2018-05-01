6adfdcf1a8fc1790fd2335fe539925898a4cf870
Paul Gebheim <paul@onetaste.us>
2018-05-01 23:15:21 +0000

    Use PL types from augur.js

--- src/server/getters/get-profit-loss.ts
+++ src/server/getters/get-profit-loss.ts
@@ -1,12 +1,12 @@
 import * as Knex from "knex";
 import * as _ from "lodash";
-import Augur from "augur.js";
+import { Augur, CalculatedProfitLoss } from "augur.js";
 import { Address, TradingHistoryRow, GenericCallback } from "../../types";
 import { queryUserTradingHistory } from "./database";
 
 export interface PLResult {
-  realized: string;
-  unrealized: string;
+  timestamp: number;
+  profit_loss: CalculatedProfitLoss|null;
 };
 
 const getPL = async function(db: Knex, augur: Augur, universe: Address, account: Address, startTime: number, endTime: number, periodInterval: number): Promise<Array<PLResult>> {
@@ -14,22 +14,22 @@ const getPL = async function(db: Knex, augur: Augur, universe: Address, account:
 
   let bucketEndTime: number = startTime;
   const bucket: Array<TradingHistoryRow> = [];
-  const [basis, ...profit_losses] = trades.reduce((memo: Array<any>, trade: TradingHistoryRow): Array<any> => {
-    let profit_loss = null;
+  const [basis, ...profit_losses] = trades.reduce((memo: Array<PLResult>, trade: TradingHistoryRow): Array<PLResult> => {
+    let profit_loss: CalculatedProfitLoss|null = null;
     while(trade.timestamp >= bucketEndTime) {
       // Only calculate the PL once and if subsquent groups wont have this trade, re-use the result
       if (profit_loss === null && bucket.length > 0) {
         profit_loss = augur.trading.calculateProfitLoss({ bucket , lastPrice: _.last(bucket)!.price!});
       }
 
-      memo.push({ profit_loss, timestamp: bucketEndTime });
+      memo.push({ profit_loss, timestamp: bucketEndTime } );
       bucketEndTime += periodInterval;
     }
 
     bucket.push(trade);
 
     return memo;
-  }, [] as Array<any>);
+  }, [] as Array<PLResult>);
 
   return profit_losses; // Still need to adjust by "basis"
 }
