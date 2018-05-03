e17825fa4d25a40a866d9d74b82143c3a028a91e
phoebemirman <phoebe.mirman@gmail.com>
2018-05-02 17:58:48 -0700

    rm console log

--- src/modules/market/helpers/calculate-outstanding-returns.js
+++ src/modules/market/helpers/calculate-outstanding-returns.js
@@ -2,7 +2,6 @@ export const calculateOutstandingReturns = (marketId, positions) => {
   let totalReturns = 0
   // this is prob wrong, sometimes positions is empty?
   if (positions && positions[marketId]) {
-  	console.log('hi')
     for (let i = 0; i < positions[marketId].length; i++) {
       for (let j = 0; j < positions[marketId][i]; j++) {
         totalReturns += parseInt(positions[marketId][i][j].unrealizedProfitLoss, 10) // do fancy match
