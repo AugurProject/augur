1a4591b512e21b5589f2f34f37ea41e10464df96
phoebemirman <phoebe.mirman@gmail.com>
2018-05-02 17:56:52 -0700

    refactor calculate outstandings

--- src/modules/market/helpers/calculate-outstanding-returns.js
+++ src/modules/market/helpers/calculate-outstanding-returns.js
@@ -1,10 +1,14 @@
 export const calculateOutstandingReturns = (marketId, positions) => {
   let totalReturns = 0
-  // this is prob wrong
-  for (let i in positions[marketId]) {
-    for (let j in positions[marketId][i]) {
-      totalReturns += parseInt(positions[marketId][i][j].unrealizedProfitLoss) // do fancy match
+  // this is prob wrong, sometimes positions is empty?
+  if (positions && positions[marketId]) {
+  	console.log('hi')
+    for (let i = 0; i < positions[marketId].length; i++) {
+      for (let j = 0; j < positions[marketId][i]; j++) {
+        totalReturns += parseInt(positions[marketId][i][j].unrealizedProfitLoss, 10) // do fancy match
+      }
     }
   }
-  return totalReturns;
+
+  return totalReturns
 }
