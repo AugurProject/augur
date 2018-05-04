e5fea1d3ce55da2c65ec597b965147ba8781c855
Paul Gebheim <paul@onetaste.us>
2018-05-04 00:49:22 +0000

    Split functions so unit testable

--- src/server/getters/get-profit-loss.ts
+++ src/server/getters/get-profit-loss.ts
@@ -9,19 +9,14 @@ export interface PLResult {
   profitLoss: CalculatedProfitLoss|null;
 };
 
-const getPL = async function(db: Knex, augur: Augur, universe: Address, account: Address, startTime: number, endTime: number, periodInterval: number): Promise<Array<PLResult>> {
-  console.log(`Getting PL for ${startTime} to ${endTime}`);
-  const trades: Array<TradingHistoryRow> = await queryUserTradingHistory(db, universe, account, null, null, null, null, endTime); 
-  console.log(`Found ${trades.length} trades for account ${account}`);
-
+// Perhaps port this to Augur.js
+export function calculateBucketedProfitLoss(augur: Augur, trades: Array<TradingHistoryRow>, startTime: number, endTime: number, periodInterval: number) {
   // Pre make the buckets so its easy to parition trades
   const bucketEndTimes: Array<number> = [];
   for(let bucketEnd=startTime; bucketEnd < endTime; bucketEnd += periodInterval) {
     bucketEndTimes.push(bucketEnd);
   }
   bucketEndTimes.push(endTime);
-
-  console.log(bucketEndTimes);
 
   const [basis, ...profitLosses] = bucketEndTimes.map((bucketEndTime: number) => {
     // Get all the teades up to this endTime
@@ -32,12 +27,18 @@ const getPL = async function(db: Knex, augur: Augur, universe: Address, account:
     };
   });
 
-  console.log(basis);
-  console.log(profitLosses);
-
   return profitLosses; // Still need to adjust by "basis"
 }
 
 export function getProfitLoss(db: Knex, augur: Augur, universe: Address, account: Address, startTime: number, endTime: number, periodInterval: number, callback: GenericCallback<Array<PLResult>>) {
-  getPL(db, augur, universe, account, startTime, endTime, periodInterval).then((result) => callback(null, result)).catch(callback);
+  console.log(`Getting PL for ${startTime} to ${endTime}`);
+  try {
+    queryUserTradingHistory(db, universe, account, null, null, null, null, endTime).asCallback((err, trades: Array<TradingHistoryRow>) => {
+      if (err) return callback(err);
+
+      callback(null, calculateBucketedProfitLoss(augur, trades, startTime, endTime, periodInterval));
+    });
+  } catch(e) {
+    callback(e);
+  }
 }
