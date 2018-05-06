a836b2397454042114d1b827a474806aabb1c4f4
Peter Hicks <jph@jah.net>
2018-05-06 14:33:43 -0700

    set branch: true so we get branch information

--- webpack.config.js
+++ webpack.config.js
@@ -290,9 +290,7 @@ if (!process.env.DEBUG_BUILD && process.env.NODE_ENV === 'development') {
         sourceMap: true,
       }),
       new GitRevisionPlugin({
-        'VERSION': JSON.stringify(gitRevisionPlugin.version()),
-        'COMMITHASH': JSON.stringify(gitRevisionPlugin.commithash()),
-        'BRANCH': JSON.stringify(gitRevisionPlugin.branch()),
+        branch: true
       })
     ]
   });
