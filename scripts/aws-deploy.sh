673fc8bdd7c7efb9d29882f17cf0f4748868ba82
Peter Hicks <jph@jah.net>
2018-05-07 18:58:55 -0700

    pip install requests[security]

--- scripts/aws-deploy.sh
+++ scripts/aws-deploy.sh
@@ -3,8 +3,7 @@
 set -x
 
 # ssl requirements for aws
-sudo apt-get install python-service-identity pyasn1
-
-sudo pip install awscli
+pip install requests[security]
+pip install awscli
 
 aws --region=us-east-1 ecs update-service  --service dev-augur-node --cluster dev-augur-net --force-new-deployment
