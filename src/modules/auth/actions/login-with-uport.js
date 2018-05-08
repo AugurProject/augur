26435a5e6938118c4249a0a81ce7fc5c52fd09a6
John Danz <John.Danz87@gmail.com>
2018-05-08 15:26:43 -0500

    fixes uport and ledger account objects to also have a displayAddress (checksummed address)

--- src/modules/auth/actions/login-with-uport.js
+++ src/modules/auth/actions/login-with-uport.js
@@ -2,11 +2,13 @@ import { augur } from 'services/augurjs'
 import { updateIsLogged } from 'modules/auth/actions/update-is-logged'
 import { loadAccountData } from 'modules/auth/actions/load-account-data'
 import { uPortSigner } from 'modules/auth/helpers/uport-signer'
+import { toChecksumAddress } from 'ethereumjs-util'
 
 export const loginWithUport = account => (dispatch) => {
   dispatch(updateIsLogged(true))
   dispatch(loadAccountData({
     ...account,
+    displayAddress: toChecksumAddress(account.address),
     meta: {
       address: account.address,
       signer: transaction => dispatch(uPortSigner(transaction)),
