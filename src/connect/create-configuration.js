b46328a81c9126e188d71ed451850ba74c63c6e2
Scott Bigelow <epheph@gmail.com>
2018-05-08 11:11:48 -0700

    Properly clone the object so it does not get modified when configuration object does Also, check for blank strings and don't bother adding to array

--- src/connect/create-configuration.js
+++ src/connect/create-configuration.js
@@ -1,16 +1,17 @@
 "use strict";
 
 var assign = require("lodash.assign");
+var clonedeep = require("lodash.clonedeep");
 
 // upgrade from old config (single address per type) to new config (array of addresses per type)
 function createConfiguration(options) {
-  var configuration = assign({}, options, { contracts: options.contracts || {} });
+  var configuration = assign({}, clonedeep(options), { contracts: options.contracts || {} });
   if (!Array.isArray(configuration.httpAddresses)) configuration.httpAddresses = [];
   if (!Array.isArray(configuration.wsAddresses)) configuration.wsAddresses = [];
   if (!Array.isArray(configuration.ipcAddresses)) configuration.ipcAddresses = [];
-  if (typeof configuration.http === "string") configuration.httpAddresses.push(configuration.http);
-  if (typeof configuration.ws === "string") configuration.wsAddresses.push(configuration.ws);
-  if (typeof configuration.ipc === "string") configuration.ipcAddresses.push(configuration.ipc);
+  if (typeof configuration.http === "string" && configuration.http !== "") configuration.httpAddresses.push(configuration.http);
+  if (typeof configuration.ws === "string" && configuration.ws !== "") configuration.wsAddresses.push(configuration.ws);
+  if (typeof configuration.ipc === "string" && configuration.ipc !== "") configuration.ipcAddresses.push(configuration.ipc);
   return configuration;
 }
 
