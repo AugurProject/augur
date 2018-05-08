0e9bba8b377029da2c2f10725d8066751733ffaa
Peter Hicks <jph@jah.net>
2018-05-07 18:41:08 -0700

    use apt to install python ssl deps

--- scripts/aws-deploy.sh
+++ scripts/aws-deploy.sh
@@ -3,7 +3,8 @@
 set -x
 
 # ssl requirements for aws
-pip install pyopenssl ndg-httpsclient pyasn1
+apt install python-service-identity pyasn1
+
 pip install awscli
 
 aws --region=us-east-1 ecs update-service  --service dev-augur-node --cluster dev-augur-net --force-new-deployment
