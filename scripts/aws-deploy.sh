94e7de98af5f09a451fda3696af8a119143054ee
Peter Hicks <jph@jah.net>
2018-05-07 17:38:04 -0700

    update pip packages for aws

--- scripts/aws-deploy.sh
+++ scripts/aws-deploy.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+# ssl requirements for aws
+pip install pyopenssl ndg-httpsclient pyasn1
 pip install awscli
 
 aws --region=us-east-1 ecs update-service  --service dev-augur-node --cluster dev-augur-net --force-new-deployment
