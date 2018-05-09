ff49e37fb883ea2d64225dce000b089b5394712c
Tom Haile <bradford.haile@gmail.com>
2018-05-09 07:05:33 -0500

    add cash balance to market balance

--- scripts/flash/get-market-balance.js
+++ scripts/flash/get-market-balance.js
@@ -35,39 +35,47 @@ function getMarketBalance(augur, args, auth, callback) {
     var marketMailboxAddress = market.marketCreatorMailbox;
     console.log(chalk.green.dim("Mailbox owner:"), chalk.green(market.marketCreatorMailboxOwner));
     console.log(chalk.green.dim("market mailbox address:"), chalk.green(marketMailboxAddress));
-    augur.rpc.eth.getBalance([marketMailboxAddress, "latest"], function (err, attoEthBalance) {
+    augur.api.Cash.balanceOf({ _owner: marketMailboxAddress }, function (err, cashBalance) {
       if (err) {
         console.log(chalk.red(err));
         return callback(err);
       }
-      console.log(chalk.green.dim("Total balance:"), chalk.green(speedomatic.unfix(attoEthBalance, "string")));
-      console.log(chalk.yellow.dim("numTicks"), chalk.yellow(numTicks));
-      async.eachSeries(outcomes, function (outcomeId, nextOutcome) {
-        console.log(chalk.yellow.dim("Outcome: "), chalk.yellow(outcomeId));
-        augur.api.Market.getShareToken({tx: { to: marketId}, _outcome: outcomeId}, function (err, shareToken) {
-          if (err) {
-            console.log(chalk.red(err));
-            return callback(err);
-          }
-          var shareTokenPayload = { tx: { to: shareToken }, _owner: address };
-          augur.api.ShareToken.balanceOf(shareTokenPayload, function (err, balance) {
+      var bnCashBalance = speedomatic.bignum(cashBalance);
+      augur.rpc.eth.getBalance([marketMailboxAddress, "latest"], function (err, attoEthBalance) {
+        if (err) {
+          console.log(chalk.red(err));
+          return callback(err);
+        }
+        var bnAttoEthBalance = speedomatic.bignum(attoEthBalance);
+        var combined = speedomatic.unfix(bnAttoEthBalance.plus(bnCashBalance), "string");
+        console.log(chalk.green.dim("Total balance:"), chalk.green(combined));
+        console.log(chalk.yellow.dim("numTicks"), chalk.yellow(numTicks));
+        async.eachSeries(outcomes, function (outcomeId, nextOutcome) {
+          console.log(chalk.yellow.dim("Outcome: "), chalk.yellow(outcomeId));
+          augur.api.Market.getShareToken({tx: { to: marketId}, _outcome: outcomeId}, function (err, shareToken) {
             if (err) {
               console.log(chalk.red(err));
               return callback(err);
             }
-            var adjusted = new BigNumber(speedomatic.unfix(balance, "string")).times(new BigNumber(numTicks));
-            console.log("attoShare: " + chalk.green(balance));
-            console.log("Shares: " + chalk.green(speedomatic.unfix(balance, "string")));
-            console.log("Adjusted Shares: " + chalk.green(adjusted));
-            nextOutcome(null);
+            var shareTokenPayload = { tx: { to: shareToken }, _owner: address };
+            augur.api.ShareToken.balanceOf(shareTokenPayload, function (err, balance) {
+              if (err) {
+                console.log(chalk.red(err));
+                return callback(err);
+              }
+              var adjusted = new BigNumber(speedomatic.unfix(balance, "string")).times(new BigNumber(numTicks));
+              console.log("attoShare: " + chalk.green(balance));
+              console.log("Shares: " + chalk.green(speedomatic.unfix(balance, "string")));
+              console.log("Adjusted Shares: " + chalk.green(adjusted));
+              nextOutcome(null);
+            });
           });
+        }, function (err) {
+          if (err) console.log(chalk.red(err));
+          callback(null);
         });
-      }, function (err) {
-        if (err) console.log(chalk.red(err));
-        callback(null);
       });
     });
-
   });
 }
 
