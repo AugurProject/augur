84950dba4eade8ba91087cc6b0bf28715ad8904c
Justin Barry <328965+justinbarry@users.noreply.github.com>
2018-05-11 18:53:35 -0700

    Remove web worker stuff

--- src/modules/market/workers/derive-period-time-series.worker.js
+++
@@ -1,7 +0,0 @@
-import derivePeriodTimeSeries from 'modules/market/helpers/derive-period-time-series'
-
-onmessage = (event) => {
-  const periodTimeSeries = derivePeriodTimeSeries(event)
-
-  postMessage(periodTimeSeries)
-}
