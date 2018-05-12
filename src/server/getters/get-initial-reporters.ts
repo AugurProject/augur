188f5f5d978720876dd057c607b67e8fc3038286
Scott Bigelow <epheph@gmail.com>
2018-05-12 15:20:20 -0700

    update test data and fix tests

--- src/server/getters/get-initial-reporters.ts
+++ src/server/getters/get-initial-reporters.ts
@@ -7,8 +7,10 @@ import { InitialReportersRow, UIInitialReporters } from "../../types";
 export function getInitialReporters(db: Knex, augur: Augur, universe: Address, reporter: Address, redeemed: boolean|null|undefined, withRepBalance: boolean|null|undefined, callback: (err: Error|null, result?: any) => void): void {
   let query = db("initial_reports")
     .select(["marketID", "reporter", "amountStaked", "initialReporter", "redeemed", "isDesignatedReporter", "balances.balance AS repBalance"])
+    .select(["transactionHash", "initial_reports.blockNumber", "logIndex", "blocks.timestamp"])
     .join("balances", "balances.owner", "=", "initial_reports.initialReporter")
     .join("universes", "universes.reputationToken", "balances.token")
+    .join("blocks", "initial_reports.blockNumber", "blocks.blockNumber")
     .where({reporter})
     .where("universes.universe", universe);
 
