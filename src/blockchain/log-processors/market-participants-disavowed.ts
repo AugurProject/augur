0db0e0edb53da11eb7277f4eeff894129e6fcd58
nuevoalex <achap5dk@gmail.com>
2018-05-11 23:00:54 -0700

    update market needs disavowal on manual disavowal

--- src/blockchain/log-processors/market-participants-disavowed.ts
+++ src/blockchain/log-processors/market-participants-disavowed.ts
@@ -3,9 +3,19 @@ import * as Knex from "knex";
 import { FormattedEventLog, ErrorCallback } from "../../types";
 
 export function processMarketParticipantsDisavowedLog(db: Knex, augur: Augur, log: FormattedEventLog, callback: ErrorCallback): void {
-  db.from("crowdsourcers").where("marketId", log.market).update({ disavowed: db.raw("disavowed + 1")}).asCallback(callback);
+  db.update({
+    needsDisavowal: db.raw("needsDisavowal - 1"),
+  }).into("markets").where("marketId", log.market).asCallback((err) => {
+    if (err) return callback(err);
+    db.from("crowdsourcers").where("marketId", log.market).update({ disavowed: db.raw("disavowed + 1")}).asCallback(callback);
+  });
 }
 
 export function processMarketParticipantsDisavowedLogRemoval(db: Knex, augur: Augur, log: FormattedEventLog, callback: ErrorCallback): void {
-  db.from("crowdsourcers").where("marketId", log.market).update({ disavowed: db.raw("disavowed - 1")}).asCallback(callback);
+  db.update({
+    needsDisavowal: db.raw("needsDisavowal + 1"),
+  }).into("markets").where("marketId", log.market).asCallback((err) => {
+    if (err) return callback(err);
+    db.from("crowdsourcers").where("marketId", log.market).update({ disavowed: db.raw("disavowed - 1")}).asCallback(callback);
+  });
 }
