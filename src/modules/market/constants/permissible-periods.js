02d0566b88d1a6f1b7523b316559a6de1e1a4d38
Justin Barry <328965+justinbarry@users.noreply.github.com>
2018-05-12 17:30:16 -0700

    Add time interval to candlesticks to pretty up the x-axis

--- src/modules/market/constants/permissible-periods.js
+++ src/modules/market/constants/permissible-periods.js
@@ -1,29 +1,43 @@
+import {
+  timeDay,
+  timeHour,
+  timeMinute,
+  timeMonth,
+  timeSecond,
+} from 'd3'
+
 // Note: times are in seconds
 export const RANGES = [
   {
     duration: 60,
     label: 'Past minute',
+    tickInterval: timeSecond.every(30),
   },
   {
     duration: 3600,
     label: 'Past hour',
+    tickInterval: timeMinute.every(10),
   },
   {
     duration: 86400,
     label: 'Past day',
     isDefault: true,
+    tickInterval: timeHour.every(3),
   },
   {
     duration: 604800,
     label: 'Past week',
+    tickInterval: timeDay.every(1),
   },
   {
     duration: 2629800,
     label: 'Past month',
+    tickInterval: timeDay.every(6),
   },
   {
     duration: 31557600,
     label: 'Past year',
+    tickInterval: timeMonth.every(1),
   },
 ]
 
