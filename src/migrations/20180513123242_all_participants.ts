78ef396d2ad79defb637fabf315687a1e4817d6f
Scott Bigelow <epheph@gmail.com>
2018-05-13 20:21:48 -0700

    better quoting

--- src/migrations/20180513123242_all_participants.ts
+++ src/migrations/20180513123242_all_participants.ts
@@ -2,15 +2,15 @@ import * as Knex from "knex";
 
 exports.up = async (knex: Knex): Promise<any> => {
   return knex.raw("DROP VIEW IF EXISTS all_participants").then((): PromiseLike<any> => {
-    return knex.raw("CREATE VIEW all_participants AS\n" +
-      "SELECT markets.universe, markets.marketId, 'initial_report' as type, initialReporter as participantAddress, initial_reports.reporter as owner, CASE  WHEN redeemed = 0 THEN amountStaked ELSE 0 END as balance, initial_reports.payoutId, 1 as completed, reportingState, markets.forking, disavowed from initial_reports\n" +
-      " JOIN markets ON markets.marketId = initial_reports.marketId\n" +
-      " JOIN market_state ON markets.marketStateId = market_state.marketStateId\n" +
-      "union\n" +
-      "SELECT markets.universe, crowdsourcers.marketId, 'crowdsourcer' as type, crowdsourcers.crowdsourcerId as participantAddress , balances.owner, balances.balance, crowdsourcers.payoutId, crowdsourcers.completed, reportingState, markets.forking, crowdsourcers.disavowed from balances\n" +
-      "  JOIN crowdsourcers ON crowdsourcers.crowdsourcerId = balances.token\n" +
-      "  JOIN markets ON markets.marketId = crowdsourcers.marketId\n" +
-      "  JOIN market_state ON markets.marketStateId = market_state.marketStateId");
+    return knex.raw(`CREATE VIEW all_participants AS
+      SELECT markets.universe, markets.marketId, 'initial_report' as type, initialReporter as participantAddress, initial_reports.reporter as owner, CASE  WHEN redeemed = 0 THEN amountStaked ELSE 0 END as balance, initial_reports.payoutId, 1 as completed, reportingState, markets.forking, disavowed from initial_reports
+        JOIN markets ON markets.marketId = initial_reports.marketId
+        JOIN market_state ON markets.marketStateId = market_state.marketStateId
+      union
+      SELECT markets.universe, crowdsourcers.marketId, 'crowdsourcer' as type, crowdsourcers.crowdsourcerId as participantAddress , balances.owner, balances.balance, crowdsourcers.payoutId, crowdsourcers.completed, reportingState, markets.forking, crowdsourcers.disavowed from balances
+        JOIN crowdsourcers ON crowdsourcers.crowdsourcerId = balances.token
+        JOIN markets ON markets.marketId = crowdsourcers.marketId
+        JOIN market_state ON markets.marketStateId = market_state.marketStateId`);
   });
 };
 
