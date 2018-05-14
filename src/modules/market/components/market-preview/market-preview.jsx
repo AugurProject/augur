b3b92bdd040307cde4e8ccb51a6eff5fc35a6269
nuevoalex <achap5dk@gmail.com>
2018-05-14 15:41:29 -0700

    fix display

--- src/modules/market/components/market-preview/market-preview.jsx
+++ src/modules/market/components/market-preview/market-preview.jsx
@@ -15,7 +15,7 @@ const MarketPreview = p => (
     <div className={classNames(Styles.MarketPreview__footer, { [`${Styles['single-card']}`]: p.cardStyle === 'single-card' })}>
       <MarketProperties {...p} />
     </div>
-    { p.outstandingReturns && p.unclaimedCreatorFees.value > 0 &&
+    { p.unclaimedCreatorFees.value > 0 &&
       <div className={classNames(Styles.MarketPreview__returns, { [`${Styles['single-card']}`]: p.cardStyle === 'single-card' })}>
         <OutstandingReturns
           id={p.id}
