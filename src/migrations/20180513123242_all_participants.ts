4d999231a0570aae9888ca78ba600d17b6c6a52a
Scott Bigelow <epheph@gmail.com>
2018-05-13 21:24:49 -0700

    provide data both on an account's holdings as well as the participants reputation tokens and size

--- src/migrations/20180513123242_all_participants.ts
+++ src/migrations/20180513123242_all_participants.ts
@@ -3,12 +3,16 @@ import * as Knex from "knex";
 exports.up = async (knex: Knex): Promise<any> => {
   return knex.raw("DROP VIEW IF EXISTS all_participants").then((): PromiseLike<any> => {
     return knex.raw(`CREATE VIEW all_participants AS
-      SELECT markets.universe, markets.marketId, 'initial_report' as type, initialReporter as participantAddress, initial_reports.reporter as owner, CASE  WHEN redeemed = 0 THEN amountStaked ELSE 0 END as balance, initial_reports.payoutId, 1 as completed, reportingState, markets.forking, disavowed from initial_reports
+      SELECT markets.universe, markets.marketId, 'initial_report' as type, initialReporter as participantAddress, initial_reports.reporter as owner, amountStaked as size, CASE  WHEN redeemed = 0 THEN amountStaked ELSE 0 END as accountBalance, contractBalances.token as reputationToken, contractBalances.balance as reputationTokenBalance, initial_reports.payoutId, 1 as completed, reportingState, markets.forking, disavowed from initial_reports
+        JOIN balances as contractBalances ON (contractBalances.owner = initial_reports.initialReporter AND contractBalances.token = universes.reputationToken)
         JOIN markets ON markets.marketId = initial_reports.marketId
+        JOIN universes ON markets.universe = universes.universe
         JOIN market_state ON markets.marketStateId = market_state.marketStateId
       union
-      SELECT markets.universe, crowdsourcers.marketId, 'crowdsourcer' as type, crowdsourcers.crowdsourcerId as participantAddress , balances.owner, balances.balance, crowdsourcers.payoutId, crowdsourcers.completed, reportingState, markets.forking, crowdsourcers.disavowed from balances
-        JOIN crowdsourcers ON crowdsourcers.crowdsourcerId = balances.token
+      SELECT markets.universe, crowdsourcers.marketId, 'crowdsourcer' as type, crowdsourcers.crowdsourcerId as participantAddress, accountBalances.owner, crowdsourcers.size, accountBalances.balance as accountBalance, contractBalances.token as reputationToken, contractBalances.balance as reputationTokenBalance, crowdsourcers.payoutId, crowdsourcers.completed, reportingState, markets.forking, crowdsourcers.disavowed from crowdsourcers
+        JOIN balances as accountBalances ON (accountBalances.token = crowdsourcers.crowdsourcerId)
+        JOIN balances as contractBalances ON (contractBalances.owner = crowdsourcers.crowdsourcerId AND contractBalances.token = universes.reputationToken)
+        JOIN universes ON markets.universe = universes.universe
         JOIN markets ON markets.marketId = crowdsourcers.marketId
         JOIN market_state ON markets.marketStateId = market_state.marketStateId`);
   });
