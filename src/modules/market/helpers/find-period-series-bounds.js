9005b061cf5206af063c9694888989555e52b180
Justin Barry <328965+justinbarry@users.noreply.github.com>
2018-05-14 21:06:57 -0700

    Rework bit that finds price series bounds

--- src/modules/market/helpers/find-period-series-bounds.js
+++ src/modules/market/helpers/find-period-series-bounds.js
@@ -1,24 +1,22 @@
-import { isEmpty } from 'lodash'
+import { compose, defaultTo, map, max, min, toString } from 'lodash/fp'
+
+const getMinFn = marketMin => compose(
+  toString,
+  defaultTo(marketMin),
+  min,
+  map('low'),
+)
+
+const getMaxFn = marketMax => compose(
+  toString,
+  defaultTo(marketMax),
+  max,
+  map('high'),
+)
 
 export default function findPeriodSeriesBounds(periodTimeSeries = [], marketMin = 0, marketMax = 1) {
-  const DEFAULT_BOUNDS = {
-    min: marketMin,
-    max: marketMax,
+  return {
+    min: getMinFn(marketMin)(periodTimeSeries),
+    max: getMaxFn(marketMax)(periodTimeSeries),
   }
-
-  if (isEmpty(periodTimeSeries)) return DEFAULT_BOUNDS
-
-  return (periodTimeSeries).reduce((p, item, i) => {
-    if (i === 0) {
-      return {
-        min: item.low.toString(),
-        max: item.high.toString(),
-      }
-    }
-
-    return {
-      min: Math.min(item.low, p.min).toString(),
-      max: Math.max(item.high, p.max).toString(),
-    }
-  }, DEFAULT_BOUNDS)
 }
