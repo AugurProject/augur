5728d4b5b20034d67b8335b1785c5c6e2594410e
Tom Haile <bradford.haile@gmail.com>
2018-05-15 18:39:36 -0500

    remove filling orderbook when getting user orders for transactions view

--- src/modules/bids-asks/actions/load-account-orders.js
+++ src/modules/bids-asks/actions/load-account-orders.js
@@ -2,25 +2,16 @@ import { augur } from 'services/augurjs'
 import logError from 'utils/log-error'
 import { addOpenOrderTransactions } from 'modules/transactions/actions/add-transactions'
 import { loadMarketsInfoIfNotLoaded } from 'modules/markets/actions/load-markets-info-if-not-loaded'
-import { updateOrderBook } from 'modules/bids-asks/actions/update-order-book'
 
 export const loadAccountOrders = (options = {}, callback = logError) => (dispatch, getState) => {
   const { universe, loginAccount } = getState()
-  augur.trading.getOrders({ ...options, creator: loginAccount.address, universe: universe.id }, (err, orders) => {
+  augur.trading.getOrders({ ...options, orderState: augur.constants.ORDER_STATE.OPEN, creator: loginAccount.address, universe: universe.id }, (err, orders) => {
     if (err) return callback(err)
     if (orders == null || Object.keys(orders).length === 0) return callback(null)
     const marketIds = Object.keys(orders)
-    // TODO: consolidate all the getting of maket infos for load account history
     dispatch(loadMarketsInfoIfNotLoaded(marketIds, (err) => {
       if (err) return callback(err)
       dispatch(addOpenOrderTransactions(orders))
-      marketIds.forEach((marketId, id) => {
-        const outcomes = Object.keys(orders[marketId])
-        outcomes.forEach((outcome, id, nextOutcome) => {
-          const orderTypeLabels = Object.keys(orders[marketId][outcome])
-          orderTypeLabels.forEach(orderType => dispatch(updateOrderBook(marketId, outcome, orderType, orders[marketId][outcome][orderType])))
-        })
-      })
       callback(null, orders)
     }))
   })
