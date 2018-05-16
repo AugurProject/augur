7d9e66a3ecf8fec324f462a37ad83513e7117462
nuevoalex <achap5dk@gmail.com>
2018-05-16 09:12:06 -0700

    review nits

--- src/server/getters/get-all-orders.ts
+++ src/server/getters/get-all-orders.ts
@@ -6,11 +6,11 @@ export interface AllOrders {
   [orderId: string]: AllOrdersRow<string>;
 }
 
-export function getAllOrders(db: Knex, account: Address, callback: (err: Error|null, result?: any) => void): void {
+export function getAllOrders(db: Knex, account: Address, callback: (err: Error|null, result?: AllOrders) => void): void {
   const query = db.select(["orderId", "tokensEscrowed", "sharesEscrowed"]).from("orders").where("orderCreator", account);
   query.asCallback((err: Error|null, allOrders?: Array<AllOrdersRow<BigNumber>>): void => {
     if (err) return callback(err);
-    if (!allOrders) return callback(err, []);
+    if (!allOrders) return callback(err, {});
     callback(null, allOrders.reduce((acc: AllOrders, cur: AllOrdersRow<BigNumber>) => {
       acc[cur.orderId] = formatBigNumberAsFixed<AllOrdersRow<BigNumber>, AllOrdersRow<string>>({
         orderId: cur.orderId,
