f638f6f62419e45b6c395e9d1bc021c1d8b3a12e
nuevoalex <achap5dk@gmail.com>
2018-05-16 18:16:54 -0700

    fix bug

--- src/blockchain/log-processors/token/decrease-token-balance.ts
+++ src/blockchain/log-processors/token/decrease-token-balance.ts
@@ -10,7 +10,7 @@ interface BalanceResult {
 export function decreaseTokenBalance(db: Knex, augur: Augur, token: Address, owner: Address, amount: BigNumber, callback: ErrorCallback): void {
   db.first("balance").from("balances").where({ token, owner }).asCallback((err: Error|null, oldBalance?: BalanceResult): void => {
     if (err) return callback(err);
-    if (amount.isZero()) return;
+    if (amount.isZero()) return callback(null);
     if (oldBalance == null) return callback(new Error(`Could not find balance for token decrease (token: ${token}, owner: ${owner})`));
     const balance = oldBalance.balance.minus(amount);
     db.update({ balance: balance.toFixed() }).into("balances").where({ token, owner }).asCallback(callback);
