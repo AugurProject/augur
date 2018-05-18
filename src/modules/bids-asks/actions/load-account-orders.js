e78b7fa4b21fb04fa83a9344ff87d9ec07174656
Tom Haile <bradford.haile@gmail.com>
2018-05-17 20:27:25 -0500

    need to load orders for market so they show up in positions view on first load

--- src/modules/bids-asks/actions/load-account-orders.js
+++ src/modules/bids-asks/actions/load-account-orders.js
@@ -1,5 +1,7 @@
 import { augur } from 'services/augurjs'
 import logError from 'utils/log-error'
+import { forEach } from 'lodash'
+import loadBidsAsks from 'modules/bids-asks/actions/load-bids-asks'
 import { addOpenOrderTransactions } from 'modules/transactions/actions/add-transactions'
 import { loadMarketsInfoIfNotLoaded } from 'modules/markets/actions/load-markets-info-if-not-loaded'
 
@@ -12,6 +14,7 @@ export const loadAccountOrders = (options = {}, callback = logError) => (dispatc
     dispatch(loadMarketsInfoIfNotLoaded(marketIds, (err) => {
       if (err) return callback(err)
       dispatch(addOpenOrderTransactions(orders))
+      forEach(marketIds, id => dispatch(loadBidsAsks(id)))
       callback(null, orders)
     }))
   })
