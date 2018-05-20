efc54b8b1c6a12a1f3bdb594cbcd754121391618
Tom Haile <bradford.haile@gmail.com>
2018-05-19 20:40:53 -0500

    noticed that in higher dispute rounds all of the users rep wasn't use… (#394)
    * noticed that in higher dispute rounds all of the users rep wasn't used if amount wasn't provided and --noPush flash
    
    * set amount to inputted value or user balance
    
    * return callback, so it fall through
    
    * add return callbacks to be safe

--- scripts/flash/dispute-contribute.js
+++ scripts/flash/dispute-contribute.js
@@ -10,6 +10,7 @@ var displayTime = require("./display-time");
 var setTimestamp = require("./set-timestamp");
 var getPayoutNumerators = require("./get-payout-numerators");
 var doMarketContribute = require("./do-market-contribute");
+var getBalance = require("../dp/lib/get-balances");
 
 var day = 108000; // day
 
@@ -26,14 +27,12 @@ function disputeContribute(augur, args, auth, callback) {
     help();
     return callback(null);
   }
-  var amount = args.opt.amount || 10000;
+  var amount = args.opt.amount;
   var marketId = args.opt.marketId;
   var outcome = args.opt.outcome;
   var invalid = args.opt.invalid;
   var noPush = args.opt.noPush;
-  console.log(chalk.yellow.dim("amount"), amount);
-
-  getRepTokens(augur, amount, auth, function (err) {
+  getRepTokens(augur, amount || 10000, auth, function (err) {
     if (err) {
       console.log(chalk.red("Error"), chalk.red(err));
       return callback(err);
@@ -45,76 +44,96 @@ function disputeContribute(augur, args, auth, callback) {
         console.log(chalk.red(err));
         return callback("Could not get market info");
       }
+
       var market = marketsInfo[0];
       var payoutNumerators = getPayoutNumerators(market, outcome, invalid);
-      var attoREP = speedomatic.fix(amount, "hex");
+      var universe = augur.contracts.addresses[augur.rpc.getNetworkID()].Universe;
+      getBalance(augur, universe, auth.address, function (err, balances) {
+        if (err) {
+          console.log(chalk.red(err));
+          return callback(JSON.stringify(err));
+        }
 
-      if (noPush) {
-        doMarketContribute(augur, marketId, attoREP, payoutNumerators, invalid, auth, function (err) {
-          if (err) {
-            return callback("Market contribute Failed");
-          }
-          console.log(chalk.green("Market contribute Done"));
-          callback(null);
-        });
-      } else {
-        var marketPayload = { tx: { to: marketId } };
-        augur.api.Market.getFeeWindow(marketPayload, function (err, feeWindowId) {
-          if (err) {
-            console.log(chalk.red(err));
-            return callback("Could not get Fee Window");
-          }
-          if (feeWindowId === "0x0000000000000000000000000000000000000000") {
-            console.log(chalk.red("feeWindowId has not been created"));
-            return callback("Market doesn't have fee window, need to report");
-          }
-          console.log(chalk.yellow("Market Fee Window"), chalk.yellow(feeWindowId));
-          var feeWindowPayload = { tx: { to: feeWindowId } };
-          augur.api.FeeWindow.getStartTime(feeWindowPayload, function (err, feeWindowStartTime) {
+        var disputeAmount = amount || balances.reputation;
+        console.log(chalk.yellow.dim("amount"), disputeAmount);
+        var attoREP = speedomatic.fix(disputeAmount, "hex");
+        if (noPush) {
+          console.log(chalk.cyan("Balances:"));
+          console.log("Ether: " + chalk.green(balances.ether));
+          console.log("Rep:   " + chalk.green(balances.reputation));
+          doMarketContribute(augur, marketId, attoREP, payoutNumerators, invalid, auth, function (err) {
+            if (err) {
+              return callback("Market contribute Failed");
+            }
+            console.log(chalk.green("Market contribute Done"));
+            return callback(null);
+          });
+
+        } else {
+
+          var marketPayload = { tx: { to: marketId } };
+          augur.api.Market.getFeeWindow(marketPayload, function (err, feeWindowId) {
             if (err) {
               console.log(chalk.red(err));
-              callback("Could not get Fee Window");
+              return callback("Could not get Fee Window");
             }
-            getTime(augur, auth, function (err, timeResult) {
+
+            if (feeWindowId === "0x0000000000000000000000000000000000000000") {
+              console.log(chalk.red("feeWindowId has not been created"));
+              return callback("Market doesn't have fee window, need to report");
+            }
+            console.log(chalk.yellow("Market Fee Window"), chalk.yellow(feeWindowId));
+            var feeWindowPayload = { tx: { to: feeWindowId } };
+            augur.api.FeeWindow.getStartTime(feeWindowPayload, function (err, feeWindowStartTime) {
               if (err) {
                 console.log(chalk.red(err));
-                return callback(err);
+                return callback("Could not get Fee Window");
               }
 
-              var setTime = parseInt(feeWindowStartTime, 10) + day;
-              displayTime("Current timestamp", timeResult.timestamp);
-              displayTime("Fee Window end time", feeWindowStartTime);
-              displayTime("Set Time to", setTime);
-              setTimestamp(augur, setTime, timeResult.timeAddress, auth, function (err) {
+              getTime(augur, auth, function (err, timeResult) {
                 if (err) {
                   console.log(chalk.red(err));
                   return callback(err);
                 }
-                console.log(chalk.yellow("sending amount REP"), chalk.yellow(attoREP), chalk.yellow(amount));
-                augur.api.FeeWindow.isActive(feeWindowPayload, function (err, result) {
+
+                var setTime = parseInt(feeWindowStartTime, 10) + day;
+                displayTime("Current timestamp", timeResult.timestamp);
+                displayTime("Fee Window end time", feeWindowStartTime);
+                displayTime("Set Time to", setTime);
+                setTimestamp(augur, setTime, timeResult.timeAddress, auth, function (err) {
                   if (err) {
                     console.log(chalk.red(err));
                     return callback(err);
                   }
-                  console.log(chalk.green.dim("Few Window is active"), chalk.green(result));
-                  if (result) {
-                    doMarketContribute(augur, marketId, attoREP, payoutNumerators, invalid, auth, function (err) {
-                      if (err) {
-                        return callback("Market contribute Failed");
-                      }
-                      console.log(chalk.green("Market contribute Done"));
-                      callback(null);
-                    });
-                  } else {
-                    console.log(chalk.red("Fee Window isn't active"));
-                    callback("Fee Window isn't active");
-                  }
+
+                  console.log(chalk.yellow("sending amount REP"), chalk.yellow(attoREP), chalk.yellow(disputeAmount));
+                  augur.api.FeeWindow.isActive(feeWindowPayload, function (err, result) {
+                    if (err) {
+                      console.log(chalk.red(err));
+                      return callback(err);
+                    }
+
+                    console.log(chalk.green.dim("Few Window is active"), chalk.green(result));
+                    if (result) {
+                      doMarketContribute(augur, marketId, attoREP, payoutNumerators, invalid, auth, function (err) {
+                        if (err) {
+                          return callback("Market contribute Failed");
+                        }
+
+                        console.log(chalk.green("Market contribute Done"));
+                        return callback(null);
+                      });
+                    } else {
+                      console.log(chalk.red("Fee Window isn't active"));
+                      return callback("Fee Window isn't active");
+                    }
+                  });
                 });
               });
             });
           });
-        });
-      }
+        }
+      });
     });
   });
 }
