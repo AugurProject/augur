664ea6b1fd78b46dce566253583049ecd24360a9
phoebemirman <phoebe.mirman@gmail.com>
2018-05-21 13:35:40 -0700

    fixed variable name

--- src/modules/reporting/components/reporting-dispute-markets/reporting-dispute-markets.jsx
+++ src/modules/reporting/components/reporting-dispute-markets/reporting-dispute-markets.jsx
@@ -135,7 +135,7 @@ export default class ReportingDisputeMarkets extends Component {
                 outcomes={outcomes}
               />))
         }
-        { upcomingMarketsCount === 0 && nonForkingMarkets === 0 &&
+        { upcomingMarketsCount === 0 && nonForkingMarketsCount === 0 &&
           <NullStateMessage
             message="There are currently no markets slated for the upcoming dispute window."
           />
