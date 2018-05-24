5bfffbf2f17efc1ea7dfffc40f22fe45127e5280
nuevoalex <achap5dk@gmail.com>
2018-05-24 15:03:10 -0700

    adjust sim trade to not filter by user

--- src/trading/simulation/simulate-sell.js
+++ src/trading/simulation/simulate-sell.js
@@ -3,12 +3,12 @@
 var simulateCreateAskOrder = require("./simulate-create-ask-order");
 var simulateFillBidOrder = require("./simulate-fill-bid-order");
 var sumSimulatedResults = require("./sum-simulated-results");
-var filterByPriceAndUserSortByPrice = require("../filter-by-price-and-user-sort-by-price");
+var filterAndSortByPrice = require("../filter-and-sort-by-price");
 var constants = require("../../constants");
 var PRECISION = constants.PRECISION;
 var ZERO = constants.ZERO;
 
-function simulateSell(outcome, sharesToCover, shareBalances, tokenBalance, userAddress, minPrice, maxPrice, price, marketCreatorFeeRate, reportingFeeRate, shouldCollectReportingFees, buyOrderBook) {
+function simulateSell(outcome, sharesToCover, shareBalances, tokenBalance, minPrice, maxPrice, price, marketCreatorFeeRate, reportingFeeRate, shouldCollectReportingFees, buyOrderBook) {
   var simulatedSell = {
     sharesFilled: ZERO,
     settlementFees: ZERO,
@@ -18,7 +18,7 @@ function simulateSell(outcome, sharesToCover, shareBalances, tokenBalance, userA
     tokensDepleted: ZERO,
     shareBalances: shareBalances,
   };
-  var matchingSortedBids = filterByPriceAndUserSortByPrice({ singleOutcomeOrderBookSide: buyOrderBook, orderType: 1, price: price, userAddress: userAddress });
+  var matchingSortedBids = filterAndSortByPrice({ singleOutcomeOrderBookSide: buyOrderBook, orderType: 1, price: price });
 
   // if no matching bids, then user is asking: no settlement fees
   if (!matchingSortedBids.length && price !== null) {
