3002fb23958ffccbfb208599a40079658defe992
Tom Haile <bradford.haile@gmail.com>
2018-05-29 13:03:36 -0500

    need to toNumber() to get the correct depth range

--- src/modules/market/components/market-outcome-charts--depth/market-outcome-charts--depth.jsx
+++ src/modules/market/components/market-outcome-charts--depth/market-outcome-charts--depth.jsx
@@ -379,7 +379,7 @@ function determineDrawParams(options) {
   const containerHeight = depthChart.clientHeight
   const drawHeight = containerHeight - chartDim.top - chartDim.bottom
 
-  const xDomain = Object.keys(marketDepth).reduce((p, side) => [...p, ...marketDepth[side].reduce((p, item) => [...p, item[0]], [])], [])
+  const xDomain = Object.keys(marketDepth).reduce((p, side) => [...p, ...marketDepth[side].reduce((p, item) => [...p, item[0].toNumber()], [])], [])
 
   // Determine bounding diff
   const maxDiff = createBigNumber(marketMinMax.mid.minus(marketMinMax.max).toPrecision(15)).absoluteValue() // NOTE -- toPrecision to address an error when attempting to get the absolute value
