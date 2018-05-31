cf80bc2f0f44ce8ee1a27c54dffbcbd0c930c1fd
Jack Peterson <jack@tinybike.net>
2018-05-31 13:58:08 -0700

    Check for matching network IDs if using global web3 (#1757)
    * Removed getMetaMaskNetworkId auth helper
    
    * Added isGlobalWeb3 auth helper
    
    * Update verifyMatchingNetworkIds to use augur.rpc.net.version and isGlobalWeb3
    
    * Updated lockfiles

--- src/modules/auth/helpers/is-meta-mask.js
+++ src/modules/auth/helpers/is-meta-mask.js
@@ -1,8 +1,7 @@
+import isGlobalWeb3 from 'modules/auth/helpers/is-global-web3'
+
 export default function () {
-  if (typeof window === 'undefined') return false
-  if (!window) return false
-  if (!window.web3) return false
-  if (!window.web3.currentProvider) return false
+  if (!isGlobalWeb3()) return false
   if (!window.web3.currentProvider.isMetaMask) return false
   return true
 }
