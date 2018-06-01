c144ddd247eaf9eda050b2cb225247990261e81a
Tom Haile <bradford.haile@gmail.com>
2018-06-01 17:22:29 -0500

    don't remove order before the success callback is called

--- src/modules/bids-asks/actions/cancel-order.js
+++ src/modules/bids-asks/actions/cancel-order.js
@@ -13,12 +13,14 @@ export const cancelOrder = (orderId, marketId, outcome, orderTypeLabel, callback
   const order = selectOrder(orderId, marketId, outcome, orderTypeLabel, orderBooks)
   const market = marketsData[marketId]
   if (order && market && outcomesData[marketId] && outcomesData[marketId][outcome]) {
-    dispatch(updateOrderStatus(orderId, CLOSE_DIALOG_CLOSING, marketId, outcome, orderTypeLabel))
     augur.api.CancelOrder.cancelOrder({
       meta: loginAccount.meta,
       _orderId: orderId,
       onSent: () => dispatch(updateOrderStatus(orderId, CLOSE_DIALOG_PENDING, marketId, outcome, orderTypeLabel)),
-      onSuccess: () => callback(null),
+      onSuccess: () => {
+        dispatch(updateOrderStatus(orderId, CLOSE_DIALOG_CLOSING, marketId, outcome, orderTypeLabel))
+        callback(null)
+      },
       onFailed: (err) => {
         dispatch(updateOrderStatus(orderId, CLOSE_DIALOG_FAILED, marketId, outcome, orderTypeLabel))
         setTimeout(() => dispatch(updateOrderStatus(orderId, null, marketId, outcome, orderTypeLabel)), TIME_TO_WAIT_BEFORE_FINAL_ACTION_MILLIS)
