5486f4526f74e36ed53f76241eecb8f8ce417e34
John Danz <John.Danz87@gmail.com>
2018-06-04 14:18:04 -0500

    added updateAssets to load-account-data again

--- src/modules/auth/actions/load-account-data.js
+++ src/modules/auth/actions/load-account-data.js
@@ -2,6 +2,8 @@ import { loadAccountDataFromLocalStorage } from 'modules/auth/actions/load-accou
 import { updateLoginAccount } from 'modules/auth/actions/update-login-account'
 import { checkAccountAllowance } from 'modules/auth/actions/approve-account'
 import { loadAccountTrades } from 'modules/my-positions/actions/load-account-trades'
+import { updateAssets } from 'modules/auth/actions/update-assets'
+
 
 import getValue from 'utils/get-value'
 import logError from 'utils/log-error'
@@ -13,4 +15,5 @@ export const loadAccountData = (account, callback = logError) => (dispatch) => {
   dispatch(updateLoginAccount(account))
   dispatch(loadAccountTrades())
   dispatch(checkAccountAllowance())
+  dispatch(updateAssets())
 }
