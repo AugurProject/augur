bdbcda03052ab7253bd7f2d5686bd8c300fc0662
phoebemirman <phoebe.mirman@gmail.com>
2018-06-05 16:56:43 -0700

    change to zero

--- src/modules/market/components/market-positions-list--order/market-positions-list--order.jsx
+++ src/modules/market/components/market-positions-list--order/market-positions-list--order.jsx
@@ -103,10 +103,10 @@ export default class Order extends Component {
             {getValue(outcome, 'lastPrice.formatted') }
           </li>
         }
-        { !isMobile && <li> {isExtendedDisplay ? '-' : ''} </li> }
-        { !isMobile && <li> {isExtendedDisplay ? '-' : ''} </li> }
+        { !isMobile && <li> {isExtendedDisplay ? 0 : ''} </li> }
+        { !isMobile && <li> {isExtendedDisplay ? 0 : ''} </li> }
         { isExtendedDisplay &&
-          <li>-</li>
+          <li>0</li>
         }
         <li>
           { pending ?
