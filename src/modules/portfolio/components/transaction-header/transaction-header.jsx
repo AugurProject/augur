4661a1b00f83d5d96f73dcc244bd2c6abd5657e0
phoebemirman <phoebe.mirman@gmail.com>
2018-06-06 10:30:31 -0700

    lint

--- src/modules/portfolio/components/transaction-header/transaction-header.jsx
+++ src/modules/portfolio/components/transaction-header/transaction-header.jsx
@@ -6,21 +6,21 @@ import MarketLink from 'modules/market/components/market-link/market-link'
 import Styles from 'modules/portfolio/components/transaction-header/transaction-header.styles'
 
 const TransactionHeader = ({ transaction }) => {
-	const marketId = transaction.transactions && transaction.transactions[0] && transaction.transactions[0].marketId
+  const marketId = transaction.transactions && transaction.transactions[0] && transaction.transactions[0].marketId
 
-	return (
-	  <div>
-	    <h5 className={Styles.TransactionHeader__status}>{ transaction.status }</h5>
-	    <h3 className={Styles.TransactionHeader__message}>{ transaction.message || transaction.type }</h3>
-	    { marketId ? 
-			<MarketLink id={marketId} formattedDescription={transaction.description}>
-		    	<h4 className={Styles.TransactionHeader__description}>{ transaction.description }</h4>
-		    </MarketLink>
-		    : <h4 className={Styles.TransactionHeader__description}>{ transaction.description }</h4>
-		}
-	    <h4 className={Styles.TransactionHeader__date}>{ transaction.timestamp && transaction.timestamp.full }</h4>
-	  </div>
-	)
+  return (
+    <div>
+      <h5 className={Styles.TransactionHeader__status}>{ transaction.status }</h5>
+      <h3 className={Styles.TransactionHeader__message}>{ transaction.message || transaction.type }</h3>
+      { marketId ?
+        <MarketLink id={marketId} formattedDescription={transaction.description}>
+          <h4 className={Styles.TransactionHeader__description}>{ transaction.description }</h4>
+        </MarketLink>
+        : <h4 className={Styles.TransactionHeader__description}>{ transaction.description }</h4>
+      }
+      <h4 className={Styles.TransactionHeader__date}>{ transaction.timestamp && transaction.timestamp.full }</h4>
+    </div>
+  )
 }
 TransactionHeader.propTypes = {
   transaction: PropTypes.object.isRequired,
