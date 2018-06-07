5ed2be3819b44e6f8bbd8df633187dfe843ef446
Justin <328965+justinbarry@users.noreply.github.com>
2018-06-07 13:52:39 -0700

    Revert "Depth chart x-axis min is min market price"
    This reverts commit d73985e143df38f596b879217a431e20b21c3d58.

--- src/modules/market/components/market-outcome-charts--depth/market-outcome-charts--depth.jsx
+++ src/modules/market/components/market-outcome-charts--depth/market-outcome-charts--depth.jsx
@@ -378,7 +378,7 @@ function determineDrawParams(options) {
   const containerHeight = depthChart.clientHeight
   const drawHeight = containerHeight - chartDim.top - chartDim.bottom
 
-  const xDomain = Object.keys(marketDepth).reduce((p, side) => [...p, ...marketDepth[side].reduce((p, item) => [...p, item[0].toNumber()], [])], [marketMin.toNumber()])
+  const xDomain = Object.keys(marketDepth).reduce((p, side) => [...p, ...marketDepth[side].reduce((p, item) => [...p, item[0].toNumber()], [])], [0])
 
   // Determine bounding diff
   const maxDiff = createBigNumber(marketMinMax.mid.minus(marketMinMax.max).toPrecision(15)).absoluteValue() // NOTE -- toPrecision to address an error when attempting to get the absolute value
