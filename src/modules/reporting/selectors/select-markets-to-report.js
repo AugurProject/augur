6f213e3def103c9f43e9a8d3dc2d83420033ec25
phoebemirman <phoebe.mirman@gmail.com>
2018-06-07 10:44:24 -0700

    filter for forked markets

--- src/modules/reporting/selectors/select-markets-to-report.js
+++ src/modules/reporting/selectors/select-markets-to-report.js
@@ -2,12 +2,16 @@ import { createSelector } from 'reselect'
 import { selectMarketReportState } from 'src/select-state'
 import { selectMarket } from 'src/modules/market/selectors/market'
 
+function filterForkedMarket(market) {
+	return !(state.universe.isForkingMarketFinalized && state.universe.isForking && state.universe.forkingMarket === market.id)
+}
+
 export const selectMarketsToReport = createSelector(
   selectMarketReportState,
   reportData => ({
     designated: reportData.designated.map(selectMarket).sort((a, b) => a.endTime.timestamp - b.endTime.timestamp),
     open: reportData.open.map(selectMarket).sort((a, b) => a.endTime.timestamp - b.endTime.timestamp),
     upcoming: reportData.upcoming.map(selectMarket).sort((a, b) => a.endTime.timestamp - b.endTime.timestamp),
-    resolved: reportData.resolved.map(selectMarket).sort((a, b) => a.endTime.timestamp - b.endTime.timestamp),
+    resolved: reportData.resolved.map(selectMarket).filter(filterForkedMarket).sort((a, b) => a.endTime.timestamp - b.endTime.timestamp),
   }),
 )
