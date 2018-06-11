d9fcb30a0a671f587eb04e935e5bde678915ae6f
Jack Peterson <jack@tinybike.net>
2018-06-11 08:24:57 -0700

    Edge login no longer automatically pops up on account page

--- src/modules/auth/actions/prepare-edge-context.js
+++ src/modules/auth/actions/prepare-edge-context.js
@@ -1,32 +1,16 @@
 import { makeEdgeUiContext } from 'edge-login-ui-web'
-
-import { showEdgeLogin } from 'modules/auth/actions/show-edge-login'
 import { selectEdgeContextState } from 'src/select-state'
 
 export const UPDATE_EDGE_CONTEXT = 'UPDATE_EDGE_CONTEXT'
 
-const hasUsers = edgeContext => Object.keys(edgeContext.localUsers).length > 0
-
 export const prepareEdgeContext = history => (dispatch, getState) => {
-  const edgeContext = selectEdgeContextState(getState())
-
-  if (edgeContext == null) {
-    return makeEdgeUiContext({
+  if (selectEdgeContextState(getState()) == null) {
+    makeEdgeUiContext({
       apiKey: 'e239ec875955ec7474628a1dc3d449c8ea8e1b48',
       appId: 'net.augur.app',
       hideKeys: true,
       vendorName: 'Augur',
-      vendorImageUrl:
-        'https://airbitz.co/go/wp-content/uploads/2016/08/augur_logo_100.png',
-    }).then((edgeContext) => {
-      dispatch({ type: UPDATE_EDGE_CONTEXT, data: edgeContext })
-      if (hasUsers(edgeContext)) {
-        dispatch(showEdgeLogin(history))
-      }
-    })
-  }
-
-  if (hasUsers(edgeContext)) {
-    dispatch(showEdgeLogin(history))
+      vendorImageUrl: 'https://airbitz.co/go/wp-content/uploads/2016/08/augur_logo_100.png',
+    }).then(edgeContext => dispatch({ type: UPDATE_EDGE_CONTEXT, data: edgeContext }))
   }
 }
