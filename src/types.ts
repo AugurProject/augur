b55a952188bec3569e253525573fc0b99f4b5ed8
Scott Bigelow <epheph@gmail.com>
2018-06-12 10:40:43 -0700

    lint

--- src/types.ts
+++ src/types.ts
@@ -417,7 +417,7 @@ export interface OrdersRow<BigNumberType> extends BaseTransactionRow {
 export interface UITrade {
   transactionHash: string;
   logIndex: number;
-  orderId: string,
+  orderId: string;
   type: string;
   price: string;
   amount: string;
