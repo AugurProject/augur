385fb83b1ddc76fd3dbc039eb50ce19f6fe9c71e
Justin Barry <328965+justinbarry@users.noreply.github.com>
2018-06-12 12:21:31 -0700

    Wait for state updates

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,16 @@
 import { useUnlockedAccount } from 'src/modules/auth/actions/use-unlocked-account'
 
-export const helpers = (dispatch, getState) => ({
-  updateAccountAddress: account => new Promise(resolve => dispatch(useUnlockedAccount(account, resolve))),
-})
+
+export const helpers = (store) => {
+  const { dispatch, whenever } = store
+  return {
+    updateAccountAddress: account => new Promise((resolve) => {
+      dispatch(useUnlockedAccount(account, () => {
+        const unsubscribe = whenever('loginAccount.address', account, () => {
+          unsubscribe()
+          resolve()
+        })
+      }))
+    }),
+  }
+}
