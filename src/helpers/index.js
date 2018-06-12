2a537609156b072af5dee92b386690bb8d4a740b
phoebemirman <phoebe.mirman@gmail.com>
2018-06-12 15:26:23 -0700

    change keys to values

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -13,7 +13,7 @@ const findMarketByDesc = (marketDescription, callback = logError) => (dispatch) 
       if (err) return callback(err)
       dispatch(loadMarketsInfo(marketIds, (err, markets) => {
         if (err) return callback(err)
-        Object.keys(markets).forEach((market) => {
+        Object.values(markets).forEach((market) => {
           if (market.description === marketDescription) {
             return callback(market.id)
           }
