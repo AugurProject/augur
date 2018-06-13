b05dd5aec7ffb5647b1449dbb8e4c8bc622b76ca
phoebemirman <phoebe.mirman@gmail.com>
2018-06-13 16:33:25 -0700

    fix the bracket

--- integration/markets-list.test.ts
+++ integration/markets-list.test.ts
@@ -44,11 +44,9 @@ describe("Markets List", () => {
 
     yesNoMarketId = await page.evaluate((marketDescription) => window.integrationHelpers.findMarketId(marketDescription), yesNoMarketDesc);
 
-    await page.waitForSelector("div.modal-disclaimer-styles_ModalDisclaimer__ActionButtons")
     await expect(page).toClick("button", {
       text: "I have read and understand the above"
     });
-    await page.waitForSelector("aside.side-nav-styles_SideNav")
     await expect(page).toClick("a[href$='#/markets")
   });
 
@@ -65,81 +63,81 @@ describe("Markets List", () => {
     });
   });
 
-    describe("Market Cards", () => {
-      beforeAll(async () => {
-        await expect(page).toClick("a[href$='#/markets?category=agriculture']")
-        markets = await page.$$(".market-common-styles_MarketCommon__container")
-        yesNoMarket = markets[0] // this has to be the "Will antibiotics" market
-      });
+  describe("Market Cards", () => {
+    beforeAll(async () => {
+      await expect(page).toClick("a[href$='#/markets?category=agriculture']")
+      markets = await page.$$(".market-common-styles_MarketCommon__container")
+      yesNoMarket = markets[0] // this has to be the "Will antibiotics" market
+    });
 
-      it("should display market title", async () => {
-        expect(await yesNoMarket.$eval('a', node => node.innerText)).toBe(yesNoMarketDesc);
-      });
+    it("should display market title", async () => {
+      expect(await yesNoMarket.$eval('a', node => node.innerText)).toBe(yesNoMarketDesc);
+    });
 
-      it("display the min and max values accurately on either ends of the scale", async () => {
-        expect(await yesNoMarket.$eval('.market-outcomes-yes-no-scalar-styles_MarketOutcomes__min', node => node.innerText)).toBe('0 %');
-        expect(await yesNoMarket.$eval('.market-outcomes-yes-no-scalar-styles_MarketOutcomes__max', node => node.innerText)).toBe('100 %');
-      });
+    it("display the min and max values accurately on either ends of the scale", async () => {
+      expect(await yesNoMarket.$eval('.market-outcomes-yes-no-scalar-styles_MarketOutcomes__min', node => node.innerText)).toBe('0 %');
+      expect(await yesNoMarket.$eval('.market-outcomes-yes-no-scalar-styles_MarketOutcomes__max', node => node.innerText)).toBe('100 %');
+    });
 
-      it("should display stats about volume, settlement Fee, and Expiration Date", async () => {
-        expect(await yesNoMarket.$eval('.value_volume', node => node.innerText)).toBe('0');
-        expect(await yesNoMarket.$eval('.value_fee', node => node.innerText)).toBe('2.00');
-        expect(await yesNoMarket.$eval('.value_expires', node => node.innerText)).toBe('DEC 31, 2019 4:00 PM (UTC -8)');
-      });
+    it("should display stats about volume, settlement Fee, and Expiration Date", async () => {
+      expect(await yesNoMarket.$eval('.value_volume', node => node.innerText)).toBe('0');
+      expect(await yesNoMarket.$eval('.value_fee', node => node.innerText)).toBe('2.00');
+      expect(await yesNoMarket.$eval('.value_expires', node => node.innerText)).toBe('DEC 31, 2019 4:00 PM (UTC -8)');
+    });
 
-      it("should display a togglable favorites star to the left of the action button on the bottom right of the card", async () => {
-        await expect(yesNoMarket).toClick("button.market-properties-styles_MarketProperties__favorite")
-        await page.waitForSelector(".fa-star")
-      });
+    it("should display a togglable favorites star to the left of the action button on the bottom right of the card", async () => {
+      await expect(yesNoMarket).toClick("button.market-properties-styles_MarketProperties__favorite")
+      await page.waitForSelector(".fa-star")
+    });
 
-      it("should display an action button that reads 'trade' which when clicked brings you to the trade view for that market", async () => {
-        await expect(page).toClick("a.market-properties-styles_MarketProperties__trade")
-        const pageUrl = await page.evaluate(() => location.href);
-        expect(pageUrl).toEqual(`${process.env.AUGUR_URL}#/market?description=will_antibiotics_be_outlawed_for_agricultural_use_in_china_by_the_end_of_2019&id=${yesNoMarketId}`)
-      });
+    it("should display an action button that reads 'trade' which when clicked brings you to the trade view for that market", async () => {
+      await expect(page).toClick("a.market-properties-styles_MarketProperties__trade")
+      const pageUrl = await page.evaluate(() => location.href);
+      expect(pageUrl).toEqual(`${process.env.AUGUR_URL}#/market?description=will_antibiotics_be_outlawed_for_agricultural_use_in_china_by_the_end_of_2019&id=${yesNoMarketId}`)
+    });
 
-      it("should bring you to the trade view for that market when clicking on market title", async () => {
-        await page.goto(url + '#/markets?category=agriculture');
-        await expect(page).toClick("a.market-link")
-        const pageUrl = await page.evaluate(() => location.href);
-        expect(pageUrl).toEqual(`${process.env.AUGUR_URL}#/market?description=will_antibiotics_be_outlawed_for_agricultural_use_in_china_by_the_end_of_2019&id=${yesNoMarketId}`)
-      });
+    it("should bring you to the trade view for that market when clicking on market title", async () => {
+      await page.goto(url + '#/markets?category=agriculture');
+      await expect(page).toClick("a.market-link")
+      const pageUrl = await page.evaluate(() => location.href);
+      expect(pageUrl).toEqual(`${process.env.AUGUR_URL}#/market?description=will_antibiotics_be_outlawed_for_agricultural_use_in_china_by_the_end_of_2019&id=${yesNoMarketId}`)
+    });
 
-      it("should display categorical market outcomes correctly", async () => {
-        await page.goto(url + '#/markets?category=science&tags=mortality');
-        markets = await page.$$(".market-common-styles_MarketCommon__container")
-        const categoricalMarket = markets[0]
+    it("should display categorical market outcomes correctly", async () => {
+      await page.goto(url + '#/markets?category=science&tags=mortality');
+      markets = await page.$$(".market-common-styles_MarketCommon__container")
+      const categoricalMarket = markets[0]
 
-        // display the top 3 outcomes for a Categorical Market, with a "+ N More" where N is the number of remaining outcomes
-        expect(await categoricalMarket.$eval('.market-outcomes-categorical-styles_MarketOutcomesCategorical__show-more', node => node.innerText)).toBe('+ 3 MORE');
-        const outcomes = await page.$$(".market-outcomes-categorical-styles_MarketOutcomesCategorical__outcome")
-        expect(outcomes.length).toEqual(6)
+      // display the top 3 outcomes for a Categorical Market, with a "+ N More" where N is the number of remaining outcomes
+      expect(await categoricalMarket.$eval('.market-outcomes-categorical-styles_MarketOutcomesCategorical__show-more', node => node.innerText)).toBe('+ 3 MORE');
+      const outcomes = await page.$$(".market-outcomes-categorical-styles_MarketOutcomesCategorical__outcome")
+      expect(outcomes.length).toEqual(6)
 
-        // click show more button
-        await expect(categoricalMarket).toClick(".market-outcomes-categorical-styles_MarketOutcomesCategorical__show-more")
+      // click show more button
+      await expect(categoricalMarket).toClick(".market-outcomes-categorical-styles_MarketOutcomesCategorical__show-more")
 
-        // "+ N More" should change to "- N More" when expanded, should collapse again on click.
-        expect(await categoricalMarket.$eval('.market-outcomes-categorical-styles_MarketOutcomesCategorical__show-more', node => node.innerText)).toBe('- 3 LESS');
-      });
+      // "+ N More" should change to "- N More" when expanded, should collapse again on click.
+      expect(await categoricalMarket.$eval('.market-outcomes-categorical-styles_MarketOutcomesCategorical__show-more', node => node.innerText)).toBe('- 3 LESS');
     });
   });
 
   describe("Filtering", () => {
     beforeAll(async () => {
       await page.goto(url + '#/markets?category=politics');
+    });
+
+    it("should display both submenu bars", async () => {
       await page.waitForSelector(".inner-nav-styles_InnerNav__menu--main")
       await page.waitForSelector(".inner-nav-styles_InnerNav__menu--submenu")
     });
 
     it("should correctly redirect after category click", async () => {
-
       // check that url is correct
       const pageUrl = await page.evaluate(() => location.href);
       expect(pageUrl).toEqual(`${process.env.AUGUR_URL}#/markets?category=politics`)
     });
 
     it("should filter market cards", async () => {
-
       // check that header is correct
       const headerWrapper = await page.$('div.markets-header-styles_MarketsHeader__wrapper');
       expect(await headerWrapper.$eval('h1', node => node.innerText)).toBe('POLITICS');
