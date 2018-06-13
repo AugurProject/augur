59fb1e06658cb024c46441248e8a1add7f883ec1
phoebemirman <phoebe.mirman@gmail.com>
2018-06-13 12:13:27 -0700

    reformatting

--- integration/markets-list.test.ts
+++ integration/markets-list.test.ts
@@ -15,51 +15,99 @@ describe("Markets List", () => {
       width: 1200
     });
 
-    const marketId = await page.evaluate((marketDescription) => window.integrationHelpers.findMarketId(marketDescription), 'Will antibiotics be outlawed for agricultural use in China by the end of 2019?');
-
+    await page.evaluate((marketDescription) => window.integrationHelpers.findMarketId(marketDescription), 'Will Ethereum trade at $2000 or higher at any time before the end of 2018?');
     await page.waitForSelector("div.modal-disclaimer-styles_ModalDisclaimer__ActionButtons")
-
     await expect(page).toClick("button", {
       text: "I have read and understand the above"
     });
-
     await page.waitForSelector("aside.side-nav-styles_SideNav")
   });
 
-  it("should paginate in chunks of 10", async () => {
-    await page.waitForSelector(".markets-list")
-    const markets = await page.$$(".market-common-styles_MarketCommon__container")
-    expect(markets.length).toEqual(10);
+  describe("General View", () => {
 
-    // check that bottom equals 20
+    it("should paginate in chunks of 10", async () => {
+      await page.waitForSelector(".markets-list")
+      const markets = await page.$$(".market-common-styles_MarketCommon__container")
+      expect(markets.length).toEqual(10);
+    });
+
+    it("should display one submenu in the sidebar", async () => {
+    });
+
+    it("should display all categories for every loaded market in the sidebar", async () => {
+      await page.waitForSelector(".inner-nav-styles_InnerNav__menu--main")
+      const marketCategories = await page.$$(".inner-nav-styles_InnerNav__menu-item--visible")
+      expect(marketCategories.length).toEqual(12);
+    });
+
+    describe("Market Cards", () => {
+      beforeAll(async () => {
+        const markets = await page.$$(".market-common-styles_MarketCommon__container")
+        const yesNoMarket = markets[0] // yes/no market
+      });
+
+      it("should display market title", async () => {
+        expect(await yesNoMarket.$eval('a', node => node.innerText)).toBe('Will Ethereum trade at $2000 or higher at any time before the end of 2018?');
+
+        // display a scale with the current mid-price for the market.
+        // verify the mid-price display is accurate and that the midprice moves along the scale appropriately. Test this for binary and scalar markets, and with something other than the default value (place some trades).
+      });
+    });
   });
 
-  it("should display one submenu with categories", async () => {
+  describe("Filtering", () => {
+    beforeAll(async () => {
+      // click on crypto category
+      await page.waitForSelector(".inner-nav-styles_InnerNav__menu--main")
+      await expect(page).toClick("a[href$='#/markets?category=crypto']")
+    });
+
+    it("should correctly redirect after category click", async () => {
+
+      // check that url is correct
+      const pageUrl = await page.evaluate(() => location.href);
+      expect(pageUrl).toEqual(`${process.env.AUGUR_URL}#/markets?category=crypto`)
+    });
+
+    it("should slide out the second submenu bar", async () => {
+    });
+
+    it("should filter market cards", async () => {
+
+      // check that header is correct
+      const headerWrapper = await page.$('div.markets-header-styles_MarketsHeader__wrapper');
+      expect(await headerWrapper.$eval('h1', node => node.innerText)).toBe('CRYPTO');
+
+      // check that markets listed are correct
+      const markets = await page.$$(".market-common-styles_MarketCommon__container")
+      expect(markets.length).toEqual(2);
+
+      // check that markets listed are in the selected category
+    });
+
+    it("should filter out markets that don't match the selected tags", async () => {
+    });
+
+    it("should show all markets after clicking the market button", async () => {
+    });
   });
 
-  it("should display all categories for every loaded market in the sidebar submenu", async () => {
-    await page.waitForSelector(".inner-nav-styles_InnerNav__menu--main")
-    const marketCategories = await page.$$(".inner-nav-styles_InnerNav__menu-item--visible")
-    expect(marketCategories.length).toEqual(12);
+  describe("Search", () => {
+     beforeAll(async () => {
+      // click on search bar on top of the page
+      // enter in a keyword to search for
+    });
 
-    // check that categories are present
-  });
+    it("should filter markets to show only ones with searched keyword", async () => {
+    });
 
-  it("should correctly redirect when clicking on a category and filter market cards", async () => {
-    await page.waitForSelector(".inner-nav-styles_InnerNav__menu--main")
-    await expect(page).toClick("a[href$='#/markets?category=crypto']")
-    
-    // check that url is correct
-    const pageUrl = await page.evaluate(() => location.href);
-    expect(pageUrl).toEqual(`${process.env.AUGUR_URL}#/markets?category=crypto`)
+    it("should clear search and show all markets after clearing the search", async () => {
+    });
 
-    // check that header is correct
-    const headerWrapper = await page.$('div.markets-header-styles_MarketsHeader__wrapper');
-    expect(await headerWrapper.$eval('h1', node => node.innerText)).toBe('CRYPTO');
+    it("should not have case sensitive search", async () => {
+    });
 
-    // check that markets listed are correct
-  });
-
-  it("should display market cards correctly", async () => {
+    it("should have markets be searchable by title, tag, or category", async () => {
+    });
   });
 });
