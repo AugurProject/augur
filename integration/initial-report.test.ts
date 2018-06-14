45355e1a0031d59a36dea0b0dfb16b89e5f0bd52
Tom Haile <bradford.haile@gmail.com>
2018-06-14 16:53:10 -0500

    fixed finding report button to initial report on yes

--- integration/initial-report.test.ts
+++ integration/initial-report.test.ts
@@ -6,63 +6,44 @@ import { IFlash } from "./types/types"
 import {dismissDisclaimerModal} from "./helpers/dismiss-disclaimer-modal";
 
 const url = `${process.env.AUGUR_URL}`;
-
 const marketDesc = 'Will Ethereum trade at $2000 or higher at any time before the end of 2018?'
 jest.setTimeout(100000);
 
-describe.only("Initial Report", () => {
+describe.only("Initial Report -- Yes", () => {
   beforeAll(async () => {
-    await page.goto(url);
-    await dismissDisclaimerModal(page);
-    const marketId =  await page.evaluate((marketDescription) => window.integrationHelpers.findMarketId(marketDescription), marketDesc);
-
-    const flash: IFlash = new Flash();
-    await flash.setMarketEndTime(marketId)
-    console.log('pushing 100 seconds')
-    await flash.pushSeconds(100) // get in market in designated reporting state
-
-    const reportingUrl = url.concat('/#/report?id=' + marketId)
-    console.log('url', reportingUrl)
-    await page.goto(reportingUrl);
 
     // No idea what a 'typical' desktop resolution would be for our users.
     await page.setViewport({
       height: 1200,
       width: 1200
     });
+
+    await page.goto(url);
+    await dismissDisclaimerModal(page);
+    const marketId =  await page.evaluate((marketDescription) => window.integrationHelpers.findMarketId(marketDescription), marketDesc);
+
+    const flash: IFlash = new Flash();
+    await flash.setMarketEndTime(marketId)
+    await flash.pushSeconds(100) // get in market in designated reporting state
+
+    await expect(page).toClick("a[href$='#/reporting-report-markets']")
+
+    const card = await expect(page).toMatchElement(".market-common-styles_MarketCommon__container", { text: marketDesc })
+
+    await expect(card).toClick("a", {
+      text: "report"
+    })
+
+  });
+
+  afterAll(async () => {
+    // verify market was reported on
+
   });
 
   it("report on yes", async () => {
     await expect(page).toClick("button", {
       text: "Yes"
-    });
-
-    await expect(page).toClick("button", {
-      text: "Review"
-    });
-
-    await expect(page).toClick("button", {
-      text: "Submit"
-    });
-  })
-
-  it("report on no", async () => {
-    await expect(page).toClick("button", {
-      text: "No"
-    });
-
-    await expect(page).toClick("button", {
-      text: "Review"
-    });
-
-    await expect(page).toClick("button", {
-      text: "Submit"
-    });
-  })
-
-  it("report on Invalid", async () => {
-    await expect(page).toClick("button", {
-      text: "Market Is Invalid"
     });
 
     await expect(page).toClick("button", {
