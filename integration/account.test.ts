579b72ee9660bd033de5be0737ed5aa46a0c05e1
phoebemirman <phoebe.mirman@gmail.com>
2018-06-15 12:28:24 -0700

    account test improvements

--- integration/account.test.ts
+++ integration/account.test.ts
@@ -4,10 +4,17 @@ import {dismissDisclaimerModal} from "./helpers/dismiss-disclaimer-modal";
 import BigNumber from 'bignumber.js'
 
 const url = `${process.env.AUGUR_URL}`;
+const TIMEOUT = 20000 // this is big because loading account balances can take a while
 
 jest.setTimeout(100000);
 
+interface AccountData {
+  rep?: string,
+  eth?: string,
+}
 describe("Account", () => {
+  let originalAccountData:AccountData;
+
   beforeAll(async () => {
     await page.goto(url);
 
@@ -20,43 +27,55 @@ describe("Account", () => {
   });
 
   describe("Authentication", () => {
-    it("should only display two options in the sidebar when not logged in", async () => {
+
+    it("should correctly display 'Account' page", async () => {
       // logout
       await page.evaluate(() => window.integrationHelpers.logout());
 
+      await expect(page).toMatch("Bet on", { timeout: TIMEOUT }) 
+
+      // go to account page
+      await expect(page).toClick("span", { text: "Account", timeout: TIMEOUT })
+
+      await expect(page).toMatch("Link an ethereum account", { timeout: TIMEOUT }) 
+
+      // expect to be on authentication page 
+      const pageUrl = await page.url();
+      await expect(pageUrl).toEqual(`${process.env.AUGUR_URL}#/authentication`)
+    });
+
+    it("should only display two options in the sidebar when not logged in", async () => {
       // options available should be "Markets" and "Account"
       await page.waitForSelector("a[href$='#/markets']")
       await page.waitForSelector("a[href='#/deposit-funds']")
 
       // check that only those two options show up
-      const sidebarElements = await page.$$("li")
+      const sidebarElements = await page.$$("li#side-nav-items")
       await expect(sidebarElements.length).toEqual(2);
-
     });
     
-    it("should correctly display 'Account' page", async () => {
-      // click on account page
-      await expect(page).toClick("span", {
-        text: "Account"
-      });
-
-      // expect to be on authentication page 
-      const pageUrl = await page.evaluate(() => location.href);
-      await expect(pageUrl).toEqual(`${process.env.AUGUR_URL}#/authentication`)
-    });
 
     it("should display full sidebar when logged in", async () => {
       // log in
       await page.evaluate((account) => window.integrationHelpers.updateAccountAddress(account), UnlockedAccounts.CONTRACT_OWNER);
 
       // check that all sidebar options show up
-      const sidebarElements = await page.$$("li")
+      const sidebarElements = await page.$$("li#side-nav-items")
       await expect(sidebarElements.length).toEqual(5);
     });
   });
 
   describe("REP Faucet Page", () => {
     it("should have a working 'Get REP' button", async () => {
+      // keep track of original account data
+      originalAccountData = await page.evaluate(() => window.integrationHelpers.getAccountData());
+
+      // log in to secondary account - doing rep faucet on this account so that we always have rep to withdraw
+      await page.evaluate((account) => window.integrationHelpers.updateAccountAddress(account), UnlockedAccounts.SECONDARY_ACCOUNT);
+
+      // make sure logged in
+      await page.waitForSelector("#core-bar-eth", {timeout: TIMEOUT})
+
       // navigate to rep faucet
       await page.goto(url + '#/rep-faucet');
 
@@ -65,38 +84,46 @@ describe("Account", () => {
       const initialRep = await accountData.rep;
 
       // click 'Get REP' button 
-      await expect(page).toClick("button.account-rep-faucet-styles_AccountRepFaucet__button", {timeout: 50000})
+      await expect(page).toClick("button.account-rep-faucet-styles_AccountRepFaucet__button", {timeout: TIMEOUT})
 
       // verify you receieved a confirmed notification 
       await expect(page).toClick("button.top-bar-styles_TopBar__notification-icon")
-      await expect(page).toMatch("faucet - confirmed", {timeout: 50000})
+      await expect(page).toMatch("faucet - confirmed", {timeout: TIMEOUT})
 
       // balance should now have 47.00 more REP - compare old and new account balances
       const newRepPlus = await new BigNumber(initialRep).plus(47)
       const formatRep = await page.evaluate((value) => window.integrationHelpers.formatEth(value), newRepPlus);
-      await expect(page).toMatch(formatRep.formatted.split(".")[0], { timeout: 10000 }) // decimals may not equal be sometimes cause of rounding
+      await expect(page).toMatch(formatRep.formatted.split(".")[0], { timeout: TIMEOUT }) // decimals may not equal be sometimes cause of rounding
     });
   });
 
   describe("Withdraw Page", () => {
     it("should be able to send funds to another account using the form", async () => {
       // send eth from a second account to first account and check that the amount is right
-
-      // keep track of original account data
-      const originalAccountData = await page.evaluate(() => window.integrationHelpers.getAccountData());
-
-      // log in to secondary account
-      await page.evaluate((account) => window.integrationHelpers.updateAccountAddress(account), UnlockedAccounts.SECONDARY_ACCOUNT);
-
       // navigate to withdraw page
       await page.goto(url + '#/withdraw-funds');
       
       // withdraw eth
-      await expect(page).toFill("input#quantity", "100", {timeout: 5000});
+      await expect(page).toFill("input#quantity", "100", {timeout: TIMEOUT});
       await expect(page).toFill("input#address", UnlockedAccounts.CONTRACT_OWNER);
       await expect(page).toClick("button#withdraw-button");
 
-      // expect succesful withdraw 
+      // check for notification
+      await expect(page).toClick("button.top-bar-styles_TopBar__notification-icon")
+      await expect(page).toMatch("Transfer Ether -- Success", {timeout: TIMEOUT})
+      await expect(page).toClick("button.top-bar-styles_TopBar__notification-icon")
+
+      // withdraw rep
+      await expect(page).toClick(".input-dropdown-styles_InputDropdown")
+      await expect(page).toClick("button", {text: "REP", timeout: TIMEOUT})
+      await expect(page).toFill("input#quantity", "10", {timeout: TIMEOUT});
+      await expect(page).toFill("input#address", UnlockedAccounts.CONTRACT_OWNER);
+      await expect(page).toClick("button#withdraw-button");
+
+      // check for notification
+      await expect(page).toClick("button.top-bar-styles_TopBar__notification-icon")
+      await expect(page).toMatch("Transfer REP -- Success", {timeout: TIMEOUT})
+      await expect(page).toClick("button.top-bar-styles_TopBar__notification-icon")
 
       // log into original account
       await page.evaluate((account) => window.integrationHelpers.updateAccountAddress(account), UnlockedAccounts.CONTRACT_OWNER);
@@ -104,9 +131,14 @@ describe("Account", () => {
 
       // compare old and new account balances
       const eth = await originalAccountData.eth // sometimes null for newAccountData
-      const newEth = await new BigNumber(eth).plus(100)
+      const newEth = await new BigNumber(eth || '').plus(100)
       const formatEth = await page.evaluate((value) => window.integrationHelpers.formatEth(value), newEth);
-      await expect(page).toMatch(formatEth.formatted.split(".")[0], { timeout: 10000 }) // decimals may not equal be sometimes cause of rounding
+      await expect(page).toMatch(formatEth.formatted.split(".")[0], { timeout: TIMEOUT }) // decimals may not equal be sometimes cause of rounding
+
+      const rep = await originalAccountData.rep // sometimes null for newAccountData
+      const newRep = await new BigNumber(rep || '').plus(10)
+      const formatRep = await page.evaluate((value) => window.integrationHelpers.formatRep(value), newRep);
+      await expect(page).toMatch(formatRep.formatted.split(".")[0], { timeout: TIMEOUT }) // decimals may not equal be sometimes cause of rounding
     });
   });
 
