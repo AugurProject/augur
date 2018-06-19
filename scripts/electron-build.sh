72e995b4f3f0a32a76c077b21ffd0054a3d3973a
Peter Hicks <jph@jah.net>
2018-06-19 14:14:07 -0700

    change nvm installation order

--- scripts/electron-build.sh
+++ scripts/electron-build.sh
@@ -16,10 +16,10 @@ else
     rm -rf node_modules/*
     apt update
     apt install -y libusb-{dev,1.0-0-dev} rpm curl
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
     id
     env
     export NVM_DIR="$HOME/.nvm"
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
     nvm install v9.11.2
     nvm use v9.11.2
