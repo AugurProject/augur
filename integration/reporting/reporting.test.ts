e73852eb2b3af2fa56bd98d137e7a3fca7db1abd
Tom Haile <bradford.haile@gmail.com>
2018-06-18 22:38:16 -0500

    change sequence of tests, have categorical markets run first

--- integration/reporting/reporting.test.ts
+++ integration/reporting/reporting.test.ts
@@ -12,141 +12,6 @@ jest.setTimeout(30000);
 
 let flash: IFlash = new Flash();
 
-describe("YesNo Initial Report", () => {
-  beforeAll(async () => {
-    await toDefaultView()
-  });
-
-  afterAll(async () => {
-    flash.dispose()
-  })
-
-  beforeEach(async () => {
-    await toReporting()
-
-    const market: IMarket = await createYesNoMarket()
-
-    await flash.setMarketEndTime(market.id)
-    await flash.pushDays(1) // put market in designated reporting state
-
-    await waitNextBlock()
-    await toInitialReporting(market.id)
-  });
-
-  it("report on yes", async () => {
-    await expect(page).toClick("button", {
-      text: "Yes",
-      timeout: 1000,
-    });
-
-    await expect(page).toClick("button", {
-      text: "Review"
-    });
-
-    await expect(page).toClick("button", {
-      text: "Submit"
-    });
-  })
-
-  it("report on No", async () => {
-    await expect(page).toClick("button", {
-      text: "No",
-      timeout: 1000,
-    });
-
-    await expect(page).toClick("button", {
-      text: "Review"
-    });
-
-    await expect(page).toClick("button", {
-      text: "Submit"
-    });
-  })
-
-  it("report on Invalid", async () => {
-    await expect(page).toClick("button", {
-      text: "Market is invalid",
-      timeout: 1000,
-    });
-
-    await expect(page).toClick("button", {
-      text: "Review"
-    });
-
-    await expect(page).toClick("button", {
-      text: "Submit"
-    });
-  })
-})
-
-describe("YesNo Open Report", () => {
-  beforeAll(async () => {
-    await toDefaultView()
-  });
-
-  afterAll(async () => {
-    flash.dispose()
-  })
-
-  beforeEach(async () => {
-    await page.evaluate((account) => window.integrationHelpers.updateAccountAddress(account), UnlockedAccounts.CONTRACT_OWNER);
-
-    const market: IMarket = await createYesNoMarket()
-    await page.evaluate((account) => window.integrationHelpers.updateAccountAddress(account), UnlockedAccounts.SECONDARY_ACCOUNT);
-    await toReporting()
-
-    await flash.setMarketEndTime(market.id)
-    await flash.pushDays(5) // put market in open reporting state
-    await waitNextBlock(2)
-    await toInitialReporting(market.id)
-  });
-
-  it("report on yes", async () => {
-    await expect(page).toClick("button", {
-      text: "Yes",
-      timeout: 1000,
-    });
-
-    await expect(page).toClick("button", {
-      text: "Review"
-    });
-
-    await expect(page).toClick("button", {
-      text: "Submit"
-    });
-  })
-
-  it("report on No", async () => {
-    await expect(page).toClick("button", {
-      text: "No",
-      timeout: 1000,
-    });
-
-    await expect(page).toClick("button", {
-      text: "Review"
-    });
-
-    await expect(page).toClick("button", {
-      text: "Submit"
-    });
-  })
-
-  it("report on Invalid", async () => {
-    await expect(page).toClick("button", {
-      text: "Market is invalid",
-      timeout: 1000,
-    });
-
-    await expect(page).toClick("button", {
-      text: "Review"
-    });
-
-    await expect(page).toClick("button", {
-      text: "Submit"
-    });
-  })
-})
-
 describe("Categorical Initial Report", () => {
   beforeAll(async () => {
     await toDefaultView()
@@ -478,9 +343,142 @@ describe("Categorical Open Report", () => {
       timeout: 1000,
     });
   })
-
 })
 
+describe("YesNo Initial Report", () => {
+  beforeAll(async () => {
+    await toDefaultView()
+  });
+
+  afterAll(async () => {
+    flash.dispose()
+  })
+
+  beforeEach(async () => {
+    await toReporting()
+
+    const market: IMarket = await createYesNoMarket()
+
+    await flash.setMarketEndTime(market.id)
+    await flash.pushDays(1) // put market in designated reporting state
+
+    await waitNextBlock()
+    await toInitialReporting(market.id)
+  });
+
+  it("report on yes", async () => {
+    await expect(page).toClick("button", {
+      text: "Yes",
+      timeout: 1000,
+    });
+
+    await expect(page).toClick("button", {
+      text: "Review"
+    });
+
+    await expect(page).toClick("button", {
+      text: "Submit"
+    });
+  })
+
+  it("report on No", async () => {
+    await expect(page).toClick("button", {
+      text: "No",
+      timeout: 1000,
+    });
+
+    await expect(page).toClick("button", {
+      text: "Review"
+    });
+
+    await expect(page).toClick("button", {
+      text: "Submit"
+    });
+  })
+
+  it("report on Invalid", async () => {
+    await expect(page).toClick("button", {
+      text: "Market is invalid",
+      timeout: 1000,
+    });
+
+    await expect(page).toClick("button", {
+      text: "Review"
+    });
+
+    await expect(page).toClick("button", {
+      text: "Submit"
+    });
+  })
+})
+
+describe("YesNo Open Report", () => {
+  beforeAll(async () => {
+    await toDefaultView()
+  });
+
+  afterAll(async () => {
+    flash.dispose()
+  })
+
+  beforeEach(async () => {
+    await page.evaluate((account) => window.integrationHelpers.updateAccountAddress(account), UnlockedAccounts.CONTRACT_OWNER);
+
+    const market: IMarket = await createYesNoMarket()
+    await page.evaluate((account) => window.integrationHelpers.updateAccountAddress(account), UnlockedAccounts.SECONDARY_ACCOUNT);
+    await toReporting()
+
+    await flash.setMarketEndTime(market.id)
+    await flash.pushDays(5) // put market in open reporting state
+    await waitNextBlock(2)
+    await toInitialReporting(market.id)
+  });
+
+  it("report on yes", async () => {
+    await expect(page).toClick("button", {
+      text: "Yes",
+      timeout: 1000,
+    });
+
+    await expect(page).toClick("button", {
+      text: "Review"
+    });
+
+    await expect(page).toClick("button", {
+      text: "Submit"
+    });
+  })
+
+  it("report on No", async () => {
+    await expect(page).toClick("button", {
+      text: "No",
+      timeout: 1000,
+    });
+
+    await expect(page).toClick("button", {
+      text: "Review"
+    });
+
+    await expect(page).toClick("button", {
+      text: "Submit"
+    });
+  })
+
+  it("report on Invalid", async () => {
+    await expect(page).toClick("button", {
+      text: "Market is invalid",
+      timeout: 1000,
+    });
+
+    await expect(page).toClick("button", {
+      text: "Review"
+    });
+
+    await expect(page).toClick("button", {
+      text: "Submit"
+    });
+  })
+})
 
 describe("Scalar Initial Report", () => {
   beforeAll(async () => {
