4c84a952a1df02ee7d425149e9f925089e89a5a8
Peter Hicks <jph@jah.net>
2018-06-19 13:48:00 -0700

    switch to use electron-build

---
+++ scripts/electron-build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+set -x
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    rm -rf node_modules/*
+    brew install yarn --without-node
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    nvm install v9.11.2
+    nvm use v9.11.2
+    npm install
+    npm run make -- --mac
+else
+    rm -rf node_modules/*
+    apt update
+    apt install -y libusb-{dev,1.0-0-dev} rpm curl
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    nvm install v9.11.2
+    nvm use v9.11.2
+    npm install
+    npm run make -- --win --linux
+fi
+
