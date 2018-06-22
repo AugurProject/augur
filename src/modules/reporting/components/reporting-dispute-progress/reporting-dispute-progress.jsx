47d004f95d0bf5d6dd3c24f7a43caea95d75648c
phoebemirman <phoebe.mirman@gmail.com>
2018-06-22 14:31:42 -0700

    switch to use formatted value

--- src/modules/reporting/components/reporting-dispute-progress/reporting-dispute-progress.jsx
+++ src/modules/reporting/components/reporting-dispute-progress/reporting-dispute-progress.jsx
@@ -13,7 +13,7 @@ const ReportingDisputeProgress = (p) => {
   let repStakedFormatted = formatAttoRep(p.stakeCurrent, { decimals: 4, roundUp: true }).formatted
 
   if (userStaked) {
-    const accountStakeCurrentFormatted = (p.accountStakeCurrent > 0 ? formatAttoRep(p.accountStakeCurrent, { decimals: 4, roundUp: true }).formatted : 0)
+    const accountStakeCurrentFormatted = formatAttoRep(p.accountStakeCurrent, { decimals: 4, roundUp: true }).formattedValue
     userPercentage = calculateAddedStakePercentage(bondSizeCurrent.fullPrecision, accountStakeCurrentFormatted, p.tentativeStake)
     repStakedFormatted = calculateTentativeCurrentRep(p.stakeCurrent, p.tentativeStake)
     totalPercentageComplete = currentPercentageComplete + userPercentage
