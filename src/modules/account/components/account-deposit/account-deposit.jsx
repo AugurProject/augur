182829dd4866c2c341e8761791db2f26054d23bc
Graham Perich <graham.perich@gmail.com>
2018-06-26 15:59:43 -0700

    change env name to 'production' instead of 'mainnet'

--- src/modules/account/components/account-deposit/account-deposit.jsx
+++ src/modules/account/components/account-deposit/account-deposit.jsx
@@ -10,7 +10,7 @@ import { Deposit as DepositIcon, Copy as CopyIcon } from 'modules/common/compone
 import Styles from 'modules/account/components/account-deposit/account-deposit.styles'
 
 function airSwapOnClick(e) {
-  const env = parseInt(augur.rpc.getNetworkID(), 10) === 1 ? 'mainnet' : 'sandbox'
+  const env = parseInt(augur.rpc.getNetworkID(), 10) === 1 ? 'production' : 'sandbox'
   e.preventDefault()
 
   // The widget will offer swaps for REP <-> ETH on mainnet
@@ -18,7 +18,7 @@ function airSwapOnClick(e) {
   window.AirSwap.Trader.render({
     env,
     mode: 'buy',
-    token: env === 'mainnet' ? '0xe94327d07fc17907b4db788e5adf2ed424addff6' : '0xcc1cbd4f67cceb7c001bd4adf98451237a193ff8',
+    token: env === 'production' ? '0xe94327d07fc17907b4db788e5adf2ed424addff6' : '0xcc1cbd4f67cceb7c001bd4adf98451237a193ff8',
     onCancel() {
       console.info('AirSwap trade cancelled')
     },
