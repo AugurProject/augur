bdbe1000f093578122cb655b92c95de4d56b7e8e
phoebemirman <phoebe.mirman@gmail.com>
2018-06-26 11:06:04 -0700

    check that outcome list isn't empty - this happens during loading

--- src/modules/market/components/market-outcomes-and-positions/market-outcomes-and-positions.jsx
+++ src/modules/market/components/market-outcomes-and-positions/market-outcomes-and-positions.jsx
@@ -22,7 +22,7 @@ const MarketOutcomesAndPositions = p => (
         closePositionStatus={p.closePositionStatus}
       />
     }
-    { p.isMobile && p.selectedOutcome &&
+    { p.isMobile && p.selectedOutcome && p.outcomes.length > 0 &&
       <MarketPositionsListMobile
         outcome={p.outcomes.filter(outcome => outcome.id === p.selectedOutcome)[0]}
         positions={p.positions.filter(position => parseInt(position.outcomeId, 10) === parseInt(p.selectedOutcome, 10))}
