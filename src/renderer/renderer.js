787ca4adfbe0efb758a8a734fef70076bb96cab0
nuevoalex <achap5dk@gmail.com>
2018-07-02 17:48:17 -0700

    override network config of UI with AN config. Better Syncing behavior

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -44,7 +44,8 @@ Renderer.prototype.onWindowError = function (errorMsg, url, lineNumber) {
 }
 
 Renderer.prototype.openAugurUI = function () {
-    opn('http://localhost:8080');
+    const networkConfig = this.config.networks[this.config.network];
+    opn(`http://localhost:8080?augur_node=ws://localhost:9001&ethereum_node_http=${networkConfig.http}&ethereum_node_ws=${networkConfig.ws}`);
 }
 
 Renderer.prototype.saveNetworkConfig = function (event) {
@@ -127,9 +128,12 @@ Renderer.prototype.renderNetworkOptions = function () {
 Renderer.prototype.onLatestSyncedBlock = function (event, data) {
     let progress = 0;
     let progressMsg = "Downloading Augur Logs" + ".".repeat(this.progressDots);
-    if (data.lastSyncBlockNumber !== null) {
+    if (data.lastSyncBlockNumber !== null && data.lastSyncBlockNumber !== 0) {
         progress = (100 * (data.lastSyncBlockNumber - data.uploadBlockNumber) / (data.highestBlockNumber - data.uploadBlockNumber)).toFixed(0);
-        this.isSynced = data.lastSyncBlockNumber === data.highestBlockNumber;
+        this.isSynced = data.lastSyncBlockNumber >= data.highestBlockNumber - 3;
+        if (!this.isSynced && progress === "100") {
+            progress = "99";
+        }
         this.clearNotice();
     } else {
         this.isSynced = false;
