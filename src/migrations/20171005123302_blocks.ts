6d583254d2dcc3b6afbf241b4b979a895ab35a23
Jonny Burger <jonathanburger11@gmail.com>
2018-07-06 16:37:18 +0200

    More SQL migrated to Postgres

--- src/migrations/20171005123302_blocks.ts
+++ src/migrations/20171005123302_blocks.ts
@@ -3,9 +3,9 @@ import * as Knex from "knex";
 exports.up = async (knex: Knex): Promise<any> => {
   return knex.schema.dropTableIfExists("blocks").then((): PromiseLike<any> => {
     return knex.schema.raw(`CREATE TABLE blocks (
-      "blockNumber" integer PRIMARY KEY NOT NULL,
-      "blockHash" varchar(66) NOT NULL,
-      "timestamp" integer NOT NULL
+      blockNumber integer PRIMARY KEY NOT NULL,
+      blockHash varchar(66) NOT NULL,
+      timestamp integer NOT NULL
     )`);
   });
 };
