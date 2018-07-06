ae3abe9d2bcaec4a3e18a7fa3ef42ff62f0d0b58
Jonny Burger <jonathanburger11@gmail.com>
2018-07-07 00:42:19 +0200

    Explicitly cast `supply` to BigNumber because it is string in Postgres

--- src/blockchain/log-processors/token/increase-token-balance.ts
+++ src/blockchain/log-processors/token/increase-token-balance.ts
@@ -13,7 +13,7 @@ export function increaseTokenBalance(db: Knex, augur: Augur, token: Address, own
     if (oldBalance == null) {
       db.insert({ owner, token, balance: amount.toFixed() }).into("balances").asCallback(callback);
     } else {
-      const balance = oldBalance.balance.plus(amount);
+      const balance = new BigNumber(oldBalance.balance).plus(amount);
       db.update({ balance: balance.toFixed() }).into("balances").where({ token, owner }).asCallback(callback);
     }
   });
