1ef540ddb05ed82980b779fa566a7d9b605e305d
JohnDanz <John.Danz87@gmail.com>
2018-07-11 10:59:11 -0500

    adjustment due to augur-node change

--- src/modules/transactions/actions/add-transactions.js
+++ src/modules/transactions/actions/add-transactions.js
@@ -200,7 +200,7 @@ export function addCompleteSetsSoldLogs(completeSetsSoldLogs) {
     const { marketsData } = getState()
     const completeSetsData = {}
     each(completeSetsSoldLogs, (completeSetLog) => {
-      const { marketId } = completeSetLog
+      const marketId = completeSetLog.market
       const market = marketsData[marketId]
       const transaction = Object.assign({ marketId, ...completeSetLog }, {
         id: completeSetLog.transactionHash,
