c69958f5ee237911eccd2628165505c5183849ea
Peter Hicks <jph@jah.net>
2018-07-13 13:12:50 -0700

    upload asset

--- scripts/post_build.py
+++ scripts/post_build.py
@@ -4,22 +4,11 @@ import hashlib
 import json
 import os
 import requests
+import pprint
 
 # headers
 GH_TOKEN = os.environ['GH_TOKEN']
 headers = {"Authorization": "token " + GH_TOKEN}
-
-# get tag
-#if os.environ['TRAVIS_TAG']:
-#    RELEASE_TAG = os.environ['TRAVIS_TAG']
-#if os.environ['APPVEYOR_REPO_TAG_NAME']:
-#    RELEASE_TAG = os.environ['APPVEYOR_REPO_TAG_NAME']
-RELEASE_TAG = '1.0.3'
-
-# set up build dir paths
-build_dir = 'dist/'
-full_path = os.path.abspath(build_dir)
-file_extensions = ['dmg', 'deb', 'exe']
 
 
 def get_github_release_info():
@@ -37,36 +26,45 @@ def get_current_version():
 
 def get_version_release_info(result, version):
     for project in result:
-        #print(project)
-        print(project['tag_name'])
         if version in project['tag_name']:
             return project
 
 
+# upload asset
+# https://uploads.github.com/repos/AugurProject/augur-app/releases/11907294/assets{?name,label}
+def upload_release_asset(id, name):
+    headers['Content-Type'] = 'text/plain'
+    request = requests.put('https://uploads.github.com/repos/AugurProject/augur-app/releases/%s/assets?name=%s' % (id, name),
+              file={'file': open(name, 'rb')},
+              headers={headers}
+              )
+
+
 current_version = get_current_version()
-print(current_version)
-
-
 result = get_github_release_info()
 release_info = get_version_release_info(result, current_version)
-print(release_info)
+release_id = release_info['id']
+print(release_id)
 
-exit(0)
 
+# set up build dir paths
+build_dir = 'dist/'
+full_path = os.path.abspath(build_dir)
+file_extensions = ['dmg', 'deb', 'exe']
 
-exit(0)
 # change to build dir
 os.chdir(full_path)
 
 # gather sha256 sums of all files in package dir
 shasums = ''
 for fname in os.listdir(full_path):
-    if any(x in fname for x in file_extensions):
+    if any(fname.endswith(x) for x in file_extensions):
         print(fname)
         sha = hashlib.sha256(open(fname, 'rb').read()).hexdigest()
         shasums_file = fname + '.sha256'
         with open(shasums_file, "w") as shafile:
             shasums = '{} {}'.format(sha, fname)
             shafile.write(shasums)
+        upload_release_asset(release_id, shasums_file)
 
 
