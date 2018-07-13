fd8540256fa60f2e2438c51114dd551798eb4b07
Peter Hicks <jph@jah.net>
2018-07-13 10:42:47 -0700

    check GH release info and our current version

--- scripts/post_build.py
+++ scripts/post_build.py
@@ -1,13 +1,60 @@
 #!/usr/bin/env python
 
-import os
 import hashlib
+import json
+import os
+import requests
+
+# headers
+GH_TOKEN = os.environ['GH_TOKEN']
+headers = {"Authorization": "token " + GH_TOKEN}
+
+# get tag
+#if os.environ['TRAVIS_TAG']:
+#    RELEASE_TAG = os.environ['TRAVIS_TAG']
+#if os.environ['APPVEYOR_REPO_TAG_NAME']:
+#    RELEASE_TAG = os.environ['APPVEYOR_REPO_TAG_NAME']
+RELEASE_TAG = '1.0.3'
 
 # set up build dir paths
 build_dir = 'dist/'
 full_path = os.path.abspath(build_dir)
 file_extensions = ['dmg', 'deb', 'exe']
 
+
+def get_github_release_info():
+    request = requests.get('https://api.github.com/repos/AugurProject/augur-app/releases', headers=headers)
+    if request.status_code == 200:
+        return request.json()
+
+
+def get_current_version():
+    data = {}
+    with open('package.json') as json_file:
+        data = json.load(json_file)
+    return data['version']
+
+
+def get_version_release_info(result, version):
+    for project in result:
+        #print(project)
+        print(project['tag_name'])
+        if version in project['tag_name']:
+            return project
+
+
+current_version = get_current_version()
+print(current_version)
+
+
+result = get_github_release_info()
+release_info = get_version_release_info(result, current_version)
+print(release_info)
+
+exit(0)
+
+
+exit(0)
 # change to build dir
 os.chdir(full_path)
 
@@ -21,3 +68,5 @@ for fname in os.listdir(full_path):
         with open(shasums_file, "w") as shafile:
             shasums = '{} {}'.format(sha, fname)
             shafile.write(shasums)
+
+
