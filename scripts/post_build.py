0130b943c241a2f7d5a113fcbe1308e865ef2164
Peter Hicks <jph@jah.net>
2018-07-14 13:20:19 -0700

    make sure to set header in post to github

--- scripts/post_build.py
+++ scripts/post_build.py
@@ -41,6 +41,7 @@ def get_version_release_info(result, version):
 # https://uploads.github.com/repos/AugurProject/augur-app/releases/11907294/assets{?name,label}
 def upload_release_asset(id, data, name):
     try:
+        headers['Content-Type'] = 'application/octet-stream, multipart/form-data'
         request = requests.post('https://uploads.github.com/repos/AugurProject/augur-app/releases/%s/assets?name=%s' % (id, name),
                   data=data,
                   headers=headers
