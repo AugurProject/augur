e0ec3fba15b2153f83183b8c42e3876b2da2b1f2
Jack Peterson <jack@tinybike.net>
2018-07-18 08:23:47 -0700

    Removed infura websocket endpoint

--- src/main/augurNodeServer.js
+++ src/main/augurNodeServer.js
@@ -43,7 +43,7 @@ const defaultConfig = {
     'mainnet': {
       'http': 'https://mainnet.infura.io/augur',
       'name': 'Mainnet',
-      'ws': 'wss://mainnet.infura.io/ws',
+      'ws': null,
       'id': '1'
     },
     'custom': {
