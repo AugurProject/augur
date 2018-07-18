a91ad41bfbd8a5068ef096485df6fda52927b178
nuevoalex <achap5dk@gmail.com>
2018-07-18 11:11:21 -0700

    move to constant

--- src/blockchain/log-processors/order-created.ts
+++ src/blockchain/log-processors/order-created.ts
@@ -8,6 +8,8 @@ import { formatOrderAmount, formatOrderPrice } from "../../utils/format-order";
 import { BN_WEI_PER_ETHER} from "../../constants";
 import { QueryBuilder } from "knex";
 
+const MAX_LOGS_PER_BLOCK = 100000;
+
 export function processOrderCreatedLog(db: Knex, augur: Augur, log: FormattedEventLog, callback: ErrorCallback): void {
   const amount: BigNumber = new BigNumber(log.amount, 10);
   const price: BigNumber = new BigNumber(log.price, 10);
@@ -92,9 +94,9 @@ function checkForOrphanedOrders(db: Knex, orderData: OrdersRow<string>, callback
   db.first([db.raw("count(*) as numOrders"), db.raw("count(distinct(price)) as numPrices")]).from("orders").where(queryData).where("amount", "!=", "0").asCallback((err: Error|null, results: { numOrders: number, numPrices: number }): void => {
     if (err) return callback(err);
     if (results.numPrices === 1 && results.numOrders > 1) {
-      db.from("orders").first(db.raw("MAX(blockNumber * 100000 + logIndex) as maxLog")).where(queryData).asCallback((err: Error|null, result: {maxLog: number}): void => {
+      db.from("orders").first(db.raw("MAX(blockNumber * MAX_LOGS_PER_BLOCK + logIndex) as maxLog")).where(queryData).asCallback((err: Error|null, result: {maxLog: number}): void => {
         if (err) return callback(err);
-        db.from("orders").where(db.raw("(blockNumber * 100000 + logIndex) == ??", [result.maxLog])).update({orphaned: true}).asCallback((err) => {
+        db.from("orders").where(db.raw("(blockNumber * MAX_LOGS_PER_BLOCK + logIndex) == ??", [result.maxLog])).update({orphaned: true}).asCallback((err) => {
           if (err) return callback(err);
           return callback(null);
         });
