ad318be63e0bcb7405421610a93995d900e52db7
Scott Bigelow <epheph@gmail.com>
2018-07-19 15:33:17 -0700

    Change of parameters for WsTransport

--- src/augur-node/connect.js
+++ src/augur-node/connect.js
@@ -5,7 +5,7 @@ var dispatchJsonRpcResponse = require("./dispatch-json-rpc-response");
 var WsTransport = require("../rpc-interface").WsTransport;
 
 function connect(augurNodeUrl, callback) {
-  new WsTransport(augurNodeUrl, 100, {}, dispatchJsonRpcResponse, function (err, transport) {
+  new WsTransport(augurNodeUrl, 100, 0, {}, dispatchJsonRpcResponse, function (err, transport) {
     if (err) return callback(err);
     augurNodeState.setTransport(transport);
     callback(null, transport);
