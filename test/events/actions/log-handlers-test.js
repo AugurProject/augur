2c5ba80fa134764797fd196ef356952f98ed298f
Justin Barry <328965+justinbarry@users.noreply.github.com>
2018-07-20 01:59:07 -0700

    Fix test that was failing when run individually

--- test/events/actions/log-handlers-test.js
+++ test/events/actions/log-handlers-test.js
@@ -19,18 +19,26 @@ describe('modules/events/actions/log-handlers.js', () => {
       UPDATE_LOGGED_TRANSACTIONS: 'UPDATE_LOGGED_TRANSACTIONS',
     }
 
-    __RewireAPI__.__Rewire__('updateLoggedTransactions', log => ({
-      type: ACTIONS.UPDATE_LOGGED_TRANSACTIONS,
-      data: {
-        log,
-      },
-    }))
-    __RewireAPI__.__Rewire__('loadAccountTrades', options => ({
-      type: ACTIONS.LOAD_ACCOUNT_TRADES,
-      data: {
-        marketId: options.marketId,
-      },
-    }))
+    beforeEach(() => {
+      __RewireAPI__.__Rewire__('updateLoggedTransactions', log => ({
+        type: ACTIONS.UPDATE_LOGGED_TRANSACTIONS,
+        data: {
+          log,
+        },
+      }))
+      __RewireAPI__.__Rewire__('loadAccountTrades', options => ({
+        type: ACTIONS.LOAD_ACCOUNT_TRADES,
+        data: {
+          marketId: options.marketId,
+        },
+      }))
+    })
+
+    afterEach(() => {
+      __RewireAPI__.__ResetDependency__('loadAccountTrades')
+      __RewireAPI__.__ResetDependency__('updateLoggedTransactions')
+    })
+
 
     test({
       description: `Should fire off update and load account trades if the sell complete set log includes the account address`,
@@ -50,13 +58,18 @@ describe('modules/events/actions/log-handlers.js', () => {
 
         const expected = [{
           type: ACTIONS.UPDATE_LOGGED_TRANSACTIONS,
-          log,
+          data: {
+            log,
+          },
         }, {
           type: ACTIONS.LOAD_ACCOUNT_TRADES,
           data: {
             marketId: '0xdeadbeef',
           },
         }]
+
+
+        console.log(actual)
 
         assert.deepEqual(actual, expected, `Dispatched unexpected actions.`)
       },
