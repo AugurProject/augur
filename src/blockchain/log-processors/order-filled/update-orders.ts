1ed9f0dcc2799d9f8a39da6c121e644e4b13b297
Scott Bigelow <epheph@gmail.com>
2018-07-20 11:00:20 -0700

    start reading orders to see which is best

--- src/blockchain/log-processors/order-filled/update-orders.ts
+++ src/blockchain/log-processors/order-filled/update-orders.ts
@@ -6,20 +6,38 @@ import { Address, Bytes32, ErrorCallback, OrderState } from "../../../types";
 import { formatOrderAmount } from "../../../utils/format-order";
 import { formatBigNumberAsFixed } from "../../../utils/format-big-number-as-fixed";
 
-interface OrderFilledOnContractData {
-  amount: { fullPrecisionAmount?: BigNumber };
+interface OrderFilledRow {
+  fullPrecisionAmount: BigNumber;
+  outcome: number;
+  price: BigNumber;
+}
+
+interface OrderFilledUpdateParams {
+  orderState: OrderState,
+  price?: BigNumber,
+}
+
+function isBestOrder(db: Knex, marketId: Address, outcome: number, price: BigNumber, callback: (err: Error|null, isBest?: boolean) => void) {
+  callback(null, true);
 }
 
 export function updateOrders(db: Knex, augur: Augur, marketId: Address, orderId: Bytes32, amount: BigNumber, creator: Address, filler: Address, tickSize: BigNumber, callback: ErrorCallback): void {
-  // If the user is taking their own order we don't refresh twice
-  db("orders").first("fullPrecisionAmount").where({ orderId }).asCallback((err: Error|null, onContractData: OrderFilledOnContractData): void => {
+  db("orders").first("fullPrecisionAmount", "outcome", "price").where({ orderId }).asCallback((err: Error|null, orderRow?: OrderFilledRow): void => {
     if (err) return callback(err);
-    if (onContractData.amount.fullPrecisionAmount == null) return callback(new Error(`Could not fetch order amount for order ${orderId}`));
-    const fullPrecisionAmountRemainingInOrder = onContractData.amount.fullPrecisionAmount.minus(amount);
+    if (orderRow == null) return callback(new Error(`Could not fetch order amount for order ${orderId}`));
+    const fullPrecisionAmountRemainingInOrder = orderRow.fullPrecisionAmount.minus(amount);
     const amountRemainingInOrder = formatOrderAmount(fullPrecisionAmountRemainingInOrder);
     const updateAmountsParams = { fullPrecisionAmount: fullPrecisionAmountRemainingInOrder, amount: amountRemainingInOrder };
     const orderState = fullPrecisionAmountRemainingInOrder.eq(ZERO) ? OrderState.FILLED : OrderState.OPEN;
-    const updateParams = Object.assign({ orderState }, updateAmountsParams);
-    db("orders").where({ orderId }).update(formatBigNumberAsFixed(updateParams)).asCallback(callback);
+    const updateParams: OrderFilledUpdateParams = Object.assign({ orderState }, updateAmountsParams);
+    isBestOrder(db, marketId, orderRow.outcome, orderRow.price, (err: Error|null, isBest?: boolean) => {
+      if (err) return callback(err);
+      if (isBest) {
+        updateParams.price = orderRow.price;
+      }
+      console.log(updateParams);
+      db("orders").where({ orderId }).update(formatBigNumberAsFixed(updateParams)).asCallback(callback);
+    });
   });
 }
+
