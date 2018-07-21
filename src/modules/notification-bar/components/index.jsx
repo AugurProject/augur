e6832f55ee0751aad1c495cb5f20bb59082d01cb
phoebemirman <phoebe.mirman@gmail.com>
2018-07-20 18:25:21 -0700

    style notification bar

--- src/modules/notification-bar/components/index.jsx
+++ src/modules/notification-bar/components/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import Styles from 'modules/notification-bar/components/notification-bar'
-import AppStyles from 'modules/app/components/app/app.styles'
 
 import classNames from 'classnames'
 
 export const NotificationBar = () => (
-  <div className={Styles.notificationBar}>Notification
-    <div className={classNames(Styles.notificationBar_dismiss, AppStyles.fa, AppStyles['fa-window-close'], AppStyles['fa-3x'])} />
+  <div className={Styles.notificationBar}>
+    <div className={Styles.notificationBar_textContainer}>
+      <p className={Styles.notificationBar_text}>
+	  		Je déteste faire du sport, cependant je vais à tout mes entraînements. J'en ai besoin parce que la vie Américaine m'a transformé
+      </p>
+    </div>
+    <div className={Styles.notificationBar_button}>
+  		SIGN TRANSACTION
+    </div>
+    <div className={Styles.notificationBar_dismiss}>
+  		Dismiss
+      <div className={Styles.notificationBar_dismissIcon}>
+        <img
+          className={Styles.notificationBar_dismissIconImg}
+          alt="Alert"
+          src="../../assets/images/augur-logo.svg"
+        />
+      </div>
+    </div>
   </div>
 )
