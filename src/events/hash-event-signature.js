b35e7233357e006998e751e7877b632d837c5d26
Paul Gebheim <paul@onetaste.us>
2018-07-24 16:59:33 +0000

    Export as `Augur` and fix Buffer

--- src/events/hash-event-signature.js
+++ src/events/hash-event-signature.js
@@ -2,6 +2,7 @@
 
 var speedomatic = require("speedomatic");
 var keccak256 = require("../utils/keccak256");
+var Buffer = require("safe-buffer").Buffer;
 
 function hashEventSignature(eventName) {
   return speedomatic.formatInt256(keccak256(Buffer.from(eventName, "utf8")).toString("hex"));
