fb9bfa09489584fcd701d01a892bf1010d710a22
Aaron Drake <aaron@augur.net>
2018-07-30 15:05:33 -0700

    Added better error handling for startAugurNodeEventListeners (#492)
    * Added better error handling for startAugurNodeEventListeners
    
    * Modified description of `onSetupComplete`
    
    * Do not set up listeners in startAugurNodeEventListeners if error

--- src/events/start-augur-node-event-listeners.js
+++ src/events/start-augur-node-event-listeners.js
@@ -9,14 +9,24 @@ var noop = require("../utils/noop");
 /**
  * Start listening for events emitted by augur-node.
  * @param {Object.<function>} eventCallbacks Callbacks to fire when events are received, keyed by event name.
- * @param {function=} onSetupComplete Called when all listeners are successfully set up.
+ * @param {function=} onSetupComplete Called when all listeners are successfully set up, or if `eventCallbacks` is improperly formatted.
  */
 function startAugurNodeEventListeners(eventCallbacks, onSetupComplete) {
   if (!isFunction(onSetupComplete)) onSetupComplete = noop;
   if (!isObject(eventCallbacks)) return onSetupComplete(new Error("No callbacks found"));
+
+  var startListeners = function (err) {
+    if (err) return onSetupComplete(err);
+    async.forEachOf(eventCallbacks, function (callback, eventName, nextEvent) {
+      augurNode.subscribeToEvent(eventName, callback, nextEvent);
+    }, onSetupComplete);
+  };
+
+  // Verify that all callbacks in `eventCallbacks` are functions before starting listeners
   async.forEachOf(eventCallbacks, function (callback, eventName, nextEvent) {
-    augurNode.subscribeToEvent(eventName, callback, nextEvent);
-  }, onSetupComplete);
+    if (typeof callback !== "function") return nextEvent(new Error("Callback for " + eventName + " is not a function"));
+    nextEvent();
+  }, startListeners);
 }
 
 module.exports = startAugurNodeEventListeners;
