358f1d2283ced6cf63b816840fc8447c58f6b333
Scott Bigelow <epheph@gmail.com>
2018-07-30 15:26:13 -0700

    lint

--- src/events.ts
+++ src/events.ts
@@ -3,7 +3,7 @@ import { EventEmitter } from "events";
 // Some events, like MarketState, are not always sourced from logs and do not have an "eventName"
 // Always make sure it is present without having to specify in every .emit call
 class EventNameEmitter extends EventEmitter {
-  emit(type: string|symbol, ...args: any[]): boolean {
+  public emit(type: string|symbol, ...args: Array<any>): boolean {
     const eventsWithName = args.map((event) => {
       return Object.assign(
         { eventName: type },
