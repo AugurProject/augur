e8f4c52d44002552768457f539ebfebf2a8c275d
Paul Gebheim <paul@onetaste.us>
2018-07-31 22:24:46 +0000

    Fix lint

--- src/migrations/20180731214011_populate_search_en.ts
+++ src/migrations/20180731214011_populate_search_en.ts
@@ -4,8 +4,8 @@ import { contentSearchBuilder } from "../utils/content-search-builder";
 exports.up = async (knex: Knex): Promise<any> => {
   await knex("search_en").delete();
 
-  const markets = await knex.select('*').from('markets');
-  for (let market of markets) {
+  const markets = await knex.select("*").from("markets");
+  for (const market of markets) {
     await knex("search_en").insert({marketId: market.marketId, content: contentSearchBuilder(market)});
   }
 };
