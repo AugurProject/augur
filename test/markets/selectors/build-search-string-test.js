83889168b97eb153d11856fb81b3e51526f8883b
Tom Haile <bradford.haile@gmail.com>
2018-08-01 18:53:01 -0500

    add more code coverage to beat coveralls

--- test/markets/selectors/build-search-string-test.js
+++ test/markets/selectors/build-search-string-test.js
@@ -5,38 +5,38 @@ import { buildSearchString } from '../../../src/modules/markets/selectors/build-
 describe('modules/markets/selectors/build-search-string.js', () => {
 
   it('should return nothing', () => {
-    const actual = buildSearchString(undefined, undefined, [])
+    const actual = buildSearchString(undefined, [])
     const expected = undefined
     assert.deepEqual(actual, expected, `Didn't return the expected default value`)
   })
 
   it('should return the existing value', () => {
-    const actual = buildSearchString(undefined, undefined, ['tag1', 'tag2'])
-    const expected = 'tag1 AND tag2'
+    const actual = buildSearchString(undefined, ['tag1', 'tag2'])
+    const expected = 'tag1 OR tag2'
     assert.deepEqual(actual, expected, `Didn't return the expected existing value`)
   })
 
   it('should return the existing value', () => {
-    const actual = buildSearchString(undefined, undefined, ['tag1'])
+    const actual = buildSearchString(undefined, ['tag1'])
     const expected = 'tag1'
     assert.deepEqual(actual, expected, `Didn't return the expected existing value`)
   })
 
   it('keyword is too short to search on', () => {
-    const actual = buildSearchString('category', 'sue', [])
-    const expected = 'category'
+    const actual = buildSearchString('sue', [])
+    const expected = undefined
     assert.deepEqual(actual, expected, `Didn't return the expected updated value`)
   })
 
   it('add keyword in search', () => {
-    const actual = buildSearchString('category', 'suez', [])
-    const expected = 'category AND suez'
+    const actual = buildSearchString('suez', [])
+    const expected = 'suez'
     assert.deepEqual(actual, expected, `Didn't return the expected updated value`)
   })
 
   it('add keyword and tag', () => {
-    const actual = buildSearchString('category', 'bobo', ['tag1'])
-    const expected = 'category AND tag1 AND bobo'
+    const actual = buildSearchString('bobo', ['tag1'])
+    const expected = 'tag1 OR bobo'
     assert.deepEqual(actual, expected, `Didn't return the expected updated value`)
   })
 })
