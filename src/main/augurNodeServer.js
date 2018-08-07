c2ad7f93d75524da9e00e2e52603aee8f2388ff9
Tom Haile <bradford.haile@gmail.com>
2018-08-07 16:53:10 -0500

    add linting and format files

--- src/main/augurNodeServer.js
+++ src/main/augurNodeServer.js
@@ -6,17 +6,17 @@ const fs = require('fs')
 const path = require('path')
 const { ipcMain } = require('electron')
 const appData = require('app-data-folder')
-const debounce = require('debounce');
+const debounce = require('debounce')
 
-const REMOTE_DELAY_WAIT = 60*1000;
-const LOCAL_DELAY_WAIT = 1*1000;
+const REMOTE_DELAY_WAIT = 60*1000
+const LOCAL_DELAY_WAIT = 1*1000
 
-const REMOTE_MAX_RETRIES = 5;
-const LOCAL_MAX_RETRIES = 3;
+const REMOTE_MAX_RETRIES = 5
+const LOCAL_MAX_RETRIES = 3
 
-const AUGUR_NODE_RESTART_RETRIES = 1;
-const AUGUR_NODE_RESTART_WAIT = 5*1000;
-const MAX_BLOCKS_BEHIND_BEFORE_RESTART = 1000;
+const AUGUR_NODE_RESTART_RETRIES = 1
+const AUGUR_NODE_RESTART_WAIT = 5*1000
+const MAX_BLOCKS_BEHIND_BEFORE_RESTART = 1000
 const LIGHT_CLIENT = 'lightclient'
 
 const defaultConfig = {
@@ -68,19 +68,20 @@ const defaultConfig = {
 }
 
 function AugurNodeServer() {
+  this.window = null
   this.appDataPath = appData('augur')
   if (!fs.existsSync(this.appDataPath)) {
     fs.mkdirSync(this.appDataPath)
   }
   this.configPath = path.join(this.appDataPath, 'config.json')
   if (!fs.existsSync(this.configPath)) {
-    this.config = JSON.parse(JSON.stringify(defaultConfig));
+    this.config = JSON.parse(JSON.stringify(defaultConfig))
     fs.writeFileSync(this.configPath, JSON.stringify(this.config, null, 4))
   } else {
     this.config = JSON.parse(fs.readFileSync(this.configPath))
-    if (this.config.version === "1.0.0") {
+    if (this.config.version === '1.0.0') {
       this.config.networks.lightclient = defaultConfig.networks.lightclient
-      this.config.version = "1.0.1"
+      this.config.version = '1.0.1'
       this.config.network = LIGHT_CLIENT
       fs.writeFileSync(this.configPath, JSON.stringify(this.config, null, 4))
     }
@@ -88,9 +89,8 @@ function AugurNodeServer() {
   this.networkConfig = this.config.networks[this.config.network]
   this.augur = new Augur()
   this.augurNodeController = new AugurNodeController(this.augur, this.networkConfig, this.appDataPath)
-  this.window = null
-  this.retriesRemaining = AUGUR_NODE_RESTART_RETRIES;
-  this.bulkSyncing = false;
+  this.retriesRemaining = AUGUR_NODE_RESTART_RETRIES
+  this.bulkSyncing = false
   ipcMain.on('requestLatestSyncedBlock', this.requestLatestSyncedBlock.bind(this))
   ipcMain.on('requestConfig', this.onRequestConfig.bind(this))
   ipcMain.on('saveNetworkConfig', this.onSaveNetworkConfig.bind(this))
@@ -103,75 +103,79 @@ function AugurNodeServer() {
 // We wait until the window is provided so that if it fails we can send an error message to the renderer
 AugurNodeServer.prototype.setWindow = function (window) {
   this.window = window
+  window.once('closed', () => {
+    this.window = null
+  })
 }
 
-AugurNodeServer.prototype.startServer = function (event) {
+AugurNodeServer.prototype.startServer = function () {
   try {
-    log.info("Starting Server");
-    var propagationDelayWaitMillis = REMOTE_DELAY_WAIT;
-    var maxRetries = REMOTE_MAX_RETRIES;
-    this.bulkSyncing = false;
-    if (this.networkConfig.http.indexOf("localhost") > -1 || this.networkConfig.http.indexOf("127.0.0.1") > -1) {
-      propagationDelayWaitMillis = LOCAL_DELAY_WAIT;
-      maxRetries = LOCAL_MAX_RETRIES;
+    log.info('Starting Server')
+    var propagationDelayWaitMillis = REMOTE_DELAY_WAIT
+    var maxRetries = REMOTE_MAX_RETRIES
+    this.bulkSyncing = false
+    log.info('here looking at networkConfig')
+    if (this.networkConfig.http.indexOf('localhost') > -1 || this.networkConfig.http.indexOf('127.0.0.1') > -1) {
+      propagationDelayWaitMillis = LOCAL_DELAY_WAIT
+      maxRetries = LOCAL_MAX_RETRIES
     }
     this.augurNodeController = new AugurNodeController(this.augur, Object.assign({}, this.networkConfig, { propagationDelayWaitMillis, maxRetries }), this.appDataPath)
-    this.augurNodeController.clearLoggers();
-    this.augurNodeController.addLogger(log);
+    this.augurNodeController.clearLoggers()
+    this.augurNodeController.addLogger(log)
 
-    this.augurNodeController.augur.events.nodes.ethereum.on("disconnect", this.onEthereumDisconnect.bind(this));
-    this.augurNodeController.augur.events.nodes.ethereum.on("reconnect", this.onEthereumReconnect.bind(this));
+    this.augurNodeController.augur.events.nodes.ethereum.on('disconnect', this.onEthereumDisconnect.bind(this))
+    this.augurNodeController.augur.events.nodes.ethereum.on('reconnect', this.onEthereumReconnect.bind(this))
     this.augurNodeController.controlEmitter.on(ControlMessageType.ServerError, this.onError.bind(this))
     this.augurNodeController.controlEmitter.on(ControlMessageType.WebsocketError, this.onError.bind(this))
     this.augurNodeController.controlEmitter.on(ControlMessageType.BulkSyncStarted, this.onBulkSyncStarted.bind(this))
     this.augurNodeController.controlEmitter.on(ControlMessageType.BulkSyncFinished, this.onBulkSyncFinished.bind(this))
 
-    event.sender.send('augurNodeStatus', true)
+    this.sendMsgToWindowContents('augurNodeStatus', true)
     this.augurNodeController.start(function (err) {
       if (this.retriesRemaining > 0) {
-        event.sender.send('error', {
+        this.sendMsgToWindowContents('error', {
           error: `ERROR: ${err.message}. RESTARTING.`
         })
-        this.retriesRemaining--;
-        this.restartOnFailure(event);
+        this.retriesRemaining--
+        this.restartOnFailure()
       } else {
-        event.sender.send('error', {
+        this.sendMsgToWindowContents('error', {
           error: `ERROR: ${err.message}.`
         })
       }
     }.bind(this))
   } catch (err) {
-    event.sender.send('augurNodeStop')
+    this.sendMsgToWindowContents('augurNodeStop')
     log.error(err)
-    event.sender.send('error', {
-      error: message
+    this.sendMsgToWindowContents('error', {
+      error: err.message
     })
   }
 }
 
-AugurNodeServer.prototype.onEthereumDisconnect = function (event) {
+AugurNodeServer.prototype.onEthereumDisconnect = function () {
   if (this.window) this.window.webContents.send('error', {
-    error: "Disconnected from Ethereum Node. Attempting to reconnect..."
+    error: 'Disconnected from Ethereum Node. Attempting to reconnect...'
   })
 }
 
-AugurNodeServer.prototype.onEthereumReconnect = function (event) {
+AugurNodeServer.prototype.onEthereumReconnect = function () {
   if (this.window) this.window.webContents.send('showNotice', {
-    message: "Reconnected",
-    class: "success"
-  });
+    message: 'Reconnected',
+    class: 'success'
+  })
 }
 
-AugurNodeServer.prototype.restart = function (event) {
+AugurNodeServer.prototype.restart = function () {
   try {
-    log.info("Restarting Server");
-    this.shutDownServer(event)
+    log.info('Restarting Server')
+    this.shutDownServer()
     setTimeout(() => {
-      this.startServer(event)
+      this.startServer()
     }, 2000)
   } catch (err) {
     log.error(err)
-    event.sender.send('error', {
+    this.sendMsgToWindowContents('error', {
       error: err
     })
   }
@@ -184,23 +188,23 @@ AugurNodeServer.prototype.onError = function (err) {
   })
 }
 
-AugurNodeServer.prototype.restartOnFailure = debounce(function (event) {
-  this.restart(event)
+AugurNodeServer.prototype.restartOnFailure = debounce(function () {
+  this.restart()
 }, AUGUR_NODE_RESTART_WAIT)
 
 AugurNodeServer.prototype.onBulkSyncStarted = function () {
   log.info('Sync with blockchain started.')
   if (this.window) this.window.webContents.send('bulkSyncStarted')
-  this.bulkSyncing = true;
+  this.bulkSyncing = true
 }
 
 AugurNodeServer.prototype.onBulkSyncFinished = function () {
   log.info('Sync with blockchain complete.')
   if (this.window) this.window.webContents.send('bulkSyncFinished')
-  this.bulkSyncing = false;
+  this.bulkSyncing = false
 }
 
-AugurNodeServer.prototype.onRequestConfig = function (event, data) {
+AugurNodeServer.prototype.onRequestConfig = function (event) {
   event.sender.send('config', this.config)
 }
 
@@ -219,14 +223,14 @@ AugurNodeServer.prototype.onSaveNetworkConfig = function (event, data) {
   }
 }
 
-AugurNodeServer.prototype.onResetConfig = function (event) {
+AugurNodeServer.prototype.onResetConfig = function () {
   try {
-    this.config = JSON.parse(JSON.stringify(defaultConfig));
+    this.config = JSON.parse(JSON.stringify(defaultConfig))
     fs.writeFileSync(this.configPath, JSON.stringify(this.config, null, 4))
-    event.sender.send('config', this.config)
+    this.sendMsgToWindowContents('config', this.config)
   } catch (err) {
     log.error(err)
-    event.sender.send('error', {
+    this.sendMsgToWindowContents('error', {
       error: err
     })
   }
@@ -235,7 +239,7 @@ AugurNodeServer.prototype.onResetConfig = function (event) {
 AugurNodeServer.prototype.onReset = function (event, data) {
   try {
     if (this.augurNodeController.isRunning()) {
-      return event.sender.send('noResetDatabase')
+      return this.sendMsgToWindowContents('noResetDatabase')
     } else {
       const network = data.network
       const id = this.config.networks[network].id || defaultConfig.networks[network].id
@@ -243,11 +247,11 @@ AugurNodeServer.prototype.onReset = function (event, data) {
     }
   } catch (err) {
     log.error(err)
-    event.sender.send('error', {
+    this.sendMsgToWindowContents('error', {
       error: err
     })
   }
-  event.sender.send('resetResponse', {})
+  this.sendMsgToWindowContents('resetResponse', {})
 }
 
 AugurNodeServer.prototype.onStartNetwork = function (event, data) {
@@ -256,8 +260,8 @@ AugurNodeServer.prototype.onStartNetwork = function (event, data) {
     this.config.network = data.network
     this.config.networks[data.network] = data.networkConfig
     this.networkConfig = this.config.networks[this.config.network]
-    this.retriesRemaining = AUGUR_NODE_RESTART_RETRIES;
-    this.restart(event)
+    this.retriesRemaining = AUGUR_NODE_RESTART_RETRIES
+    this.restart()
 
     const waiting = setInterval(() => {
       if (this.augurNodeController && this.augurNodeController.isRunning()) {
@@ -274,63 +278,71 @@ AugurNodeServer.prototype.onStartNetwork = function (event, data) {
   }
 }
 
-AugurNodeServer.prototype.onSaveConfiguration = function (event, data) {
+AugurNodeServer.prototype.onSaveConfiguration = function () {
   try {
     fs.writeFileSync(this.configPath, JSON.stringify(this.config, null, 4))
   } catch (err) {
     log.error(err)
-    event.sender.send('error', {
+    this.sendMsgToWindowContents('error', {
       error: err
     })
   }
 }
 
-AugurNodeServer.prototype.requestLatestSyncedBlock = function (event, data) {
+AugurNodeServer.prototype.requestLatestSyncedBlock = function () {
   if (this.augurNodeController == null || !this.augurNodeController.isRunning()) return
   this.augurNodeController.requestLatestSyncedBlock()
     .then((syncedBlockInfo) => {
-      event.sender.send('latestSyncedBlock', syncedBlockInfo)
-      const blocksBehind = syncedBlockInfo.highestBlockNumber - syncedBlockInfo.lastSyncBlockNumber;
+      this.sendMsgToWindowContents('latestSyncedBlock', syncedBlockInfo)
+      const blocksBehind = syncedBlockInfo.highestBlockNumber - syncedBlockInfo.lastSyncBlockNumber
       if (!this.bulkSyncing && (blocksBehind > MAX_BLOCKS_BEHIND_BEFORE_RESTART)) {
         const message = `Behind by ${blocksBehind}. Restarting to bulk sync.`
         log.info(message)
-        event.sender.send('error', {
+        this.sendMsgToWindowContents('error', {
           error: message
         })
-        this.restart(event);
+        this.restart()
       }
     }).catch((err) => {
       log.error(err)
-      event.sender.send('error', {
+      this.sendMsgToWindowContents('error', {
         error: err
       })
     })
 }
 
-AugurNodeServer.prototype.disconnectServerMessage = function (event) {
+AugurNodeServer.prototype.disconnectServerMessage = function () {
   try {
-    event.sender.send('onServerDisconnected', {})
+    this.sendMsgToWindowContents('onServerDisconnected', {})
   } catch (err) {
     log.error(err)
   }
 }
 
-AugurNodeServer.prototype.shutDownServer = function (event) {
+AugurNodeServer.prototype.shutDownServer = function () {
   try {
-    this.bulkSyncing = false;
+    this.bulkSyncing = false
     if (this.augurNodeController == null || !this.augurNodeController.isRunning()) return
     log.info('Calling Augur Node Controller Shutdown')
     this.augurNodeController.shutdown()
-    this.disconnectServerMessage(event)
-    event.sender.send('augurNodeStatus', false)
+    this.disconnectServerMessage()
+    this.sendMsgToWindowContents('augurNodeStatus', false)
   } catch (err) {
     log.error(err)
     if (this.augurNodeController && !this.augurNodeController.isRunning()) {
       this.disconnectServerMessage()
     }
-    event.sender.send('error', {
+    this.sendMsgToWindowContents('error', {
       error: err
     })
+  }
+}
+
+AugurNodeServer.prototype.sendMsgToWindowContents = function(msg, payload) {
+  try {
+    if(this.window) this.window.webContents.send(msg, payload)
+  } catch (err) {
+    log.error(err)
   }
 }
 
--- src/main/gethNodeController.js
+++ src/main/gethNodeController.js
@@ -1,145 +1,160 @@
-const { ipcMain } = require('electron');
-const { spawn } = require('child_process');
-const { request } = require('http');
+const { ipcMain } = require('electron')
+const { spawn } = require('child_process')
+const { request } = require('http')
 const fs = require('fs')
 const path = require('path')
 const appData = require('app-data-folder')
 
-const STATUS_LOOP_INTERVAL = 5000;
+const STATUS_LOOP_INTERVAL = 5000
 
-const SYNCING_POST_DATA = '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}';
+const SYNCING_POST_DATA = '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}'
 const PEER_COUNT_POST_DATA = '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}'
 
 const PEER_NODES = [
-    "enode://19b5013d24243a659bda7f1df13933bb05820ab6c3ebf6b5e0854848b97e1f7e308f703466e72486c5bc7fe8ed402eb62f6303418e05d330a5df80738ac974f6@163.172.138.100:30303?discport=30301",
-    "enode://03f178d5d4511937933b50b7af683b467abaef8cfc5f7c2c9b271f61e228578ae192aaafc7f0d8035dfa994e734c2c2f72c229e383706be2f4fa43efbe9f94f4@163.172.149.200:30303",
-    "enode://31b5db1136a0ebceeb0ab6879e95dc66e8c52bcce9c8de50e2f722b5868f782aa0306b6b137b9e0c6271a419c5562a194d7f2abd78e22dcd1f55700dfc30c46a@35.165.17.127:30303",
-    "enode://3afdfd40713a8b188a94e4c7a9ddc61bc6ef176c3abbb13d1dd35eb367725b95329a7570039044dbffa49c50d4aa65f0a1f99ee68e46b8e2f09100d11d4fc85a@31.16.0.92:30303",
-    "enode://8fcd039bb514ccac1f207d9b23efbea79a1ba9ed559768109b9b3fc9f7f89cfc3a6cd3e11ec1d92a93bdbfe2322e43f3bb3d9519530e8b503c92294116c38c32@108.232.148.241:30303",
-    "enode://95176fe178be55d40aae49a5c11f21aa58968e13c681a6b1f571b2bb3e45927a7fb3888361bef85c0e28a52ea0e4afa17dcaa9d6c61baf504b3559f056f78581@163.172.145.241:30303",
-    "enode://a18cc2a6ad4b00cae57b77ef26276eaceffb2593a196df105ce06caa25300636369767430fa21f82b12ed431bca1f61742b8ec7e4db1a58f1b6421d280752b96@148.251.67.86:30303",
-    "enode://bfad505cbb2bde72e161a7cff044d66d20ceb85c8a61047b50037881f289bd2dcc064189ade2077daddd5b20fd2fc6dee7208f227ae2a34361bf51751d225e8e@51.15.220.91:30303",
-    "enode://e70d9a9175a2cd27b55821c29967fdbfdfaa400328679e98ed61060bc7acba2e1ddd175332ee4a651292743ffd26c9a9de8c4fce931f8d7271b8afd7d221e851@104.197.99.24:30303",
-    "enode://e908af3f69a120bbb08e22a7b6e1810ec36b4bf71028d6e8640f08e8b34851ac8e19c5dc6ae6b9f49d541677a41ba97375017be6ba02f8a61f38e3c2e460171c@89.247.76.110:35698",
-    "enode://ea1737bf696928b4b686a2ccf61a6f2295d149281a80b0d83a9bce242e7bb084434c0837a2002d4cc2840663571ecf3e45517545499c466e4373c69951d090fe@163.172.181.92:30303",
-    "enode://5f0b5cf15a2d470bbadca2a4c4656d1c1918e2079241427b64ebc9fe55f47932466a89fd01c66b9568c5307c01cf21f479681624161fc7565ff075326633f2c3@75.24.193.161:30303",
-    "enode://d17489a8fbdb16587fce5cea0383a3b2e87c53d01eddbd4b2a8ba1b4af353c1bdd9551f8894acfa0d770aaa7eca9e334e15a304a356b4ec214a274f60f2435d6@107.188.141.56:30303"
+  'enode://19b5013d24243a659bda7f1df13933bb05820ab6c3ebf6b5e0854848b97e1f7e308f703466e72486c5bc7fe8ed402eb62f6303418e05d330a5df80738ac974f6@163.172.138.100:30303?discport=30301',
+  'enode://03f178d5d4511937933b50b7af683b467abaef8cfc5f7c2c9b271f61e228578ae192aaafc7f0d8035dfa994e734c2c2f72c229e383706be2f4fa43efbe9f94f4@163.172.149.200:30303',
+  'enode://31b5db1136a0ebceeb0ab6879e95dc66e8c52bcce9c8de50e2f722b5868f782aa0306b6b137b9e0c6271a419c5562a194d7f2abd78e22dcd1f55700dfc30c46a@35.165.17.127:30303',
+  'enode://3afdfd40713a8b188a94e4c7a9ddc61bc6ef176c3abbb13d1dd35eb367725b95329a7570039044dbffa49c50d4aa65f0a1f99ee68e46b8e2f09100d11d4fc85a@31.16.0.92:30303',
+  'enode://8fcd039bb514ccac1f207d9b23efbea79a1ba9ed559768109b9b3fc9f7f89cfc3a6cd3e11ec1d92a93bdbfe2322e43f3bb3d9519530e8b503c92294116c38c32@108.232.148.241:30303',
+  'enode://95176fe178be55d40aae49a5c11f21aa58968e13c681a6b1f571b2bb3e45927a7fb3888361bef85c0e28a52ea0e4afa17dcaa9d6c61baf504b3559f056f78581@163.172.145.241:30303',
+  'enode://a18cc2a6ad4b00cae57b77ef26276eaceffb2593a196df105ce06caa25300636369767430fa21f82b12ed431bca1f61742b8ec7e4db1a58f1b6421d280752b96@148.251.67.86:30303',
+  'enode://bfad505cbb2bde72e161a7cff044d66d20ceb85c8a61047b50037881f289bd2dcc064189ade2077daddd5b20fd2fc6dee7208f227ae2a34361bf51751d225e8e@51.15.220.91:30303',
+  'enode://e70d9a9175a2cd27b55821c29967fdbfdfaa400328679e98ed61060bc7acba2e1ddd175332ee4a651292743ffd26c9a9de8c4fce931f8d7271b8afd7d221e851@104.197.99.24:30303',
+  'enode://e908af3f69a120bbb08e22a7b6e1810ec36b4bf71028d6e8640f08e8b34851ac8e19c5dc6ae6b9f49d541677a41ba97375017be6ba02f8a61f38e3c2e460171c@89.247.76.110:35698',
+  'enode://ea1737bf696928b4b686a2ccf61a6f2295d149281a80b0d83a9bce242e7bb084434c0837a2002d4cc2840663571ecf3e45517545499c466e4373c69951d090fe@163.172.181.92:30303',
+  'enode://5f0b5cf15a2d470bbadca2a4c4656d1c1918e2079241427b64ebc9fe55f47932466a89fd01c66b9568c5307c01cf21f479681624161fc7565ff075326633f2c3@75.24.193.161:30303',
+  'enode://d17489a8fbdb16587fce5cea0383a3b2e87c53d01eddbd4b2a8ba1b4af353c1bdd9551f8894acfa0d770aaa7eca9e334e15a304a356b4ec214a274f60f2435d6@107.188.141.56:30303'
 ]
 
 const SYNCING_REQUEST_OPTIONS = {
-    port: 8545,
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(SYNCING_POST_DATA)
-    }
-};
+  port: 8545,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(SYNCING_POST_DATA)
+  }
+}
 
 const PEER_COUNT_REQUEST_OPTIONS = {
-    port: 8545,
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(PEER_COUNT_POST_DATA)
-    }
-};
+  port: 8545,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(PEER_COUNT_POST_DATA)
+  }
+}
 
 function GethNodeController() {
-    this.window = null;
-    let os = 'linux';
-    if (process.platform === 'win32') os = 'win';
-    if (process.platform === 'darwin') os = 'mac';
-    this.gethExecutablePath = `resources/${os}/geth`;
-    this.gethProcess = null;
-    this.statusLoop = null;
-    ipcMain.on('toggleGeth', this.toggle.bind(this));
-    ipcMain.on('startGeth', this.start.bind(this));
-    ipcMain.on('stopGeth', this.stop.bind(this));
+  this.window = null
+  let os = 'linux'
+  if (process.platform === 'win32') os = 'win'
+  if (process.platform === 'darwin') os = 'mac'
+  this.gethExecutablePath = `resources/${os}/geth`
+  this.gethProcess = null
+  this.statusLoop = null
+  ipcMain.on('toggleGeth', this.toggle.bind(this))
+  ipcMain.on('startGeth', this.start.bind(this))
+  ipcMain.on('stopGeth', this.stop.bind(this))
 }
 
 GethNodeController.prototype.setWindow = function (window) {
-    this.window = window;
+  this.window = window
+  window.once('closed', () => {
+    this.window = null
+  })
 }
 
 GethNodeController.prototype.start = function (event) {
-    const appDataPath = appData('augur')
-    const gethPath = path.join(appDataPath, 'geth')
-    if (!fs.existsSync(gethPath)) {
-        fs.mkdirSync(gethPath)
-    }
-    const staticNodesPath = path.join(gethPath, 'static-nodes.json')
-    fs.writeFileSync(staticNodesPath, JSON.stringify(PEER_NODES, null, 4))
-    this.gethProcess = spawn(this.gethExecutablePath, [
-            '--syncmode=light',
-            '--cache=512',
-            '--rpc',
-            '--ws',
-            '--wsorigins=127.0.0.1,http://127.0.0.1:8080,https://127.0.0.1:8080',
-            `--datadir=${gethPath}`
-        ], {
-            stdio: ['ignore', 'ignore', 'pipe']
-        });
+  const appDataPath = appData('augur')
+  const gethPath = path.join(appDataPath, 'geth')
+  if (!fs.existsSync(gethPath)) {
+    fs.mkdirSync(gethPath)
+  }
+  const staticNodesPath = path.join(gethPath, 'static-nodes.json')
+  fs.writeFileSync(staticNodesPath, JSON.stringify(PEER_NODES, null, 4))
+  this.gethProcess = spawn(this.gethExecutablePath, [
+    '--syncmode=light',
+    '--cache=512',
+    '--rpc',
+    '--ws',
+    '--wsorigins=127.0.0.1,http://127.0.0.1:8080,https://127.0.0.1:8080',
+    `--datadir=${gethPath}`
+  ], {
+    stdio: ['ignore', 'ignore', 'pipe']
+  })
 
-    console.log("STARTED GETH");
+  console.log('STARTED GETH')
 
-    this.gethProcess.stderr.on('data', this.log.bind(this));
-    this.gethProcess.on('close', this.onGethClose.bind(this));
+  this.gethProcess.stderr.on('data', this.log.bind(this))
+  this.gethProcess.on('close', this.onGethClose.bind(this))
 
-    this.statusLoop = setInterval(this.checkStatus.bind(this), STATUS_LOOP_INTERVAL);
-    event.sender.send("onServerConnected");
+  this.statusLoop = setInterval(this.checkStatus.bind(this), STATUS_LOOP_INTERVAL)
+  event.sender.send('onServerConnected')
 }
 
 GethNodeController.prototype.log = function (data) {
-    console.log(`GETH NODE: ${data}`);
+  console.log(`GETH NODE: ${data}`)
 }
 
 GethNodeController.prototype.onGethClose = function (code) {
-    console.log(`GETH child process exited with code ${code}`);
+  console.log(`GETH child process exited with code ${code}`)
 }
 
 GethNodeController.prototype.stop = function () {
-    console.log(`Stopping geth process`);
-    if (this.gethProcess) this.gethProcess.kill('SIGINT');
-    if (this.statusLoop) clearInterval(this.statusLoop);
+  console.log('Stopping geth process')
+  if (this.gethProcess) this.gethProcess.kill('SIGINT')
+  if (this.statusLoop) clearInterval(this.statusLoop)
 }
 
 GethNodeController.prototype.toggle = function (event) {
-    console.log(`Toggling geth process`);
-    if (this.gethProcess && !this.gethProcess.killed) return this.stop();
-    this.start(event);
+  console.log('Toggling geth process')
+  if (this.gethProcess && !this.gethProcess.killed) return this.stop()
+  this.start(event)
 }
 
-GethNodeController.prototype.checkStatus = function (window) {
-    this.makeRequest(PEER_COUNT_REQUEST_OPTIONS, PEER_COUNT_POST_DATA, this.updatePeerCount.bind(this));
+GethNodeController.prototype.checkStatus = function () {
+  this.makeRequest(PEER_COUNT_REQUEST_OPTIONS, PEER_COUNT_POST_DATA, this.updatePeerCount.bind(this))
 }
 
 GethNodeController.prototype.makeRequest = function (options, data, callback) {
+  try {
     const req = request(options, function (res) {
-        res.setEncoding('utf8');
-        res.on('data', function (data) {
-            const syncData = JSON.parse(data).result;
-            callback(syncData);
-        }.bind(this));
-    }.bind(this));
+      res.setEncoding('utf8')
+      res.on('data', function (data) {
+        const syncData = JSON.parse(data).result
+        callback(syncData)
+      }.bind(this))
+    }.bind(this))
 
-    req.on('error', console.error);
-    req.write(data);
-    req.end();
+    req.on('error', console.error)
+    req.write(data)
+    req.end()
+  } catch (err) {
+    this.sendMsgToWindowContents('error', {message: err.message})
+  }
 }
 
 GethNodeController.prototype.updatePeerCount = function (peerCountHex) {
-    const peerCount = parseInt(peerCountHex, 16);
-    this.window.webContents.send('peerCountData', { peerCount });
-    if (peerCount > 0) this.makeRequest(SYNCING_REQUEST_OPTIONS, SYNCING_POST_DATA, this.updateSyncData.bind(this));
+  try {
+    const peerCount = parseInt(peerCountHex, 16)
+    this.sendMsgToWindowContents('peerCountData', { peerCount })
+    if (peerCount > 0) this.makeRequest(SYNCING_REQUEST_OPTIONS, SYNCING_POST_DATA, this.updateSyncData.bind(this))
+  } catch (err) {
+    this.sendMsgToWindowContents('error', {message: err.message})
+  }
 }
 
 GethNodeController.prototype.updateSyncData = function (syncData) {
-    if (syncData === false) return this.window.webContents.send('gethFinishedSyncing');
-    this.window.webContents.send('latestSyncedGethBlock', {
-        lastSyncBlockNumber: parseInt(syncData.currentBlock, 16),
-        highestBlockNumber: parseInt(syncData.highestBlock, 16),
-        uploadBlockNumber: parseInt(syncData.startingBlock, 16),
-    });
+  if (syncData === false) return this.sendMsgToWindowContents('gethFinishedSyncing')
+  this.sendMsgToWindowContents('latestSyncedGethBlock', {
+    lastSyncBlockNumber: parseInt(syncData.currentBlock, 16),
+    highestBlockNumber: parseInt(syncData.highestBlock, 16),
+    uploadBlockNumber: parseInt(syncData.startingBlock, 16),
+  })
+}
+
+GethNodeController.prototype.sendMsgToWindowContents = function(msg, payload) {
+  if(this.window) this.window.webContents.send(msg, payload)
 }
 
 module.exports = GethNodeController
