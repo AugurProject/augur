b35675b050a18fd30c314a1254870fc63619b4e7
Justin Barry <328965+justinbarry@users.noreply.github.com>
2018-08-10 16:02:26 -0700

    Fix css Loader issues

--- packages/augur-ui/src/modules/create-market/components/create-market-form-resolution/create-market-form-resolution.jsx
+++ packages/augur-ui/src/modules/create-market/components/create-market-form-resolution/create-market-form-resolution.jsx
@@ -6,7 +6,7 @@ import classNames from 'classnames'
 import moment from 'moment'
 
 import 'react-dates/initialize'
-import 'react-dates/lib/css/_datepicker.css'
+
 import { SingleDatePicker } from 'react-dates'
 
 import { formatDate } from 'utils/format-date'
