58374256b1a1c2f7f5c84701a6432e40fad5c981
Tom Haile <bradford.haile@gmail.com>
2018-08-13 11:43:02 -0500

    fixed/added tests, fixed issue with adding wildcard to double quoted search phrases

--- src/modules/markets/selectors/build-search-string.js
+++ src/modules/markets/selectors/build-search-string.js
@@ -2,7 +2,7 @@ export const buildSearchString = (keywords, tags) => {
   const MIN_KEYWORDS_LENGTH = 1
 
   let keywordSearch = keywords && keywords.length > MIN_KEYWORDS_LENGTH ? keywords : undefined
-  if (keywordSearch && !keywordSearch.endsWith(' ')) keywordSearch += '*'
+  if (keywordSearch && (!keywordSearch.endsWith(' ') && !keywordSearch.endsWith('"'))) keywordSearch += '*'
   const terms = [];
   [...tags, keywordSearch].forEach((i) => {
     if (i) terms.push(i)
