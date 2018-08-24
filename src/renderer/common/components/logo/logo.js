1ff8357dd0158d14af0235f7f36209879e84206e
Tom Haile <bradford.haile@gmail.com>
2018-08-24 14:19:31 -0500

    fixed augur.svg image, now shows up

--- src/renderer/common/components/logo/logo.js
+++ src/renderer/common/components/logo/logo.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Component } from 'react'
 import PropTypes from "prop-types";
 import Styles from './logo.styles.less'
-import logoPng from '../../../../assets/images/logo@3x.png'
 
 export class Logo extends Component {
   render() {
   	return (
   		<section className={Styles.Logo}>
-            <img src={logoPng} alt="augur" width="100" />
+       <div className={Styles.Logo_image} />
 		</section>
   	)
   }
