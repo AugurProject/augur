9bb9f370d8d009470941b65cc05c553a33036ec6
Tom Haile <bradford.haile@gmail.com>
2018-08-24 13:41:06 -0500

    add latest block informaiton to start for augur node and geth node

--- src/renderer/handle-events.js
+++ src/renderer/handle-events.js
@@ -1,7 +1,8 @@
 const {ipcRenderer} = require('electron')
 import { each } from 'lodash'
-import { CONFIG } from '../utils/constants'
+import { CONFIG, LATEST_SYNCED_BLOCK, LATEST_SYNCED_GETH_BLOCK } from '../utils/constants'
 import { addUpdateConnection } from './app/actions/connections'
+import { augurNodeUpdateBlockInfo, gethNodeUpdateBlockInfo } from './app/actions/blockInfo'
 import store from './store'
 
 export const handleEvents = () => {
@@ -12,6 +13,14 @@ export const handleEvents = () => {
 
   ipcRenderer.on(CONFIG, (event, config) => {
     each(config.networks, network => store.dispatch(addUpdateConnection(network)))
+  })
+
+  ipcRenderer.on(LATEST_SYNCED_BLOCK, (event, info) => {
+    store.dispatch(augurNodeUpdateBlockInfo(info))
+  })
+
+  ipcRenderer.on(LATEST_SYNCED_GETH_BLOCK, (event, info) => {
+    store.dispatch(gethNodeUpdateBlockInfo(info))
   })
 
 }
