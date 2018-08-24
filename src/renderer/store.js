02c1cf784f539cc73bdec0fe7e377fa616714973
Tom Haile <bradford.haile@gmail.com>
2018-08-23 20:17:00 -0500

    put state on window for debugging

--- src/renderer/store.js
+++ src/renderer/store.js
@@ -7,4 +7,6 @@ const store = createStore(combineReducers({
   ...createReducer(),
 }), middleware)
 
+Object.defineProperty(window, 'state', { get: store.getState, enumerable: true })
+
 export default store
