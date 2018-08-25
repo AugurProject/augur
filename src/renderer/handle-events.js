6f3d6b2d5605276c3e4f34dfedccd44382e1d022
Tom Haile <bradford.haile@gmail.com>
2018-08-25 10:00:13 -0500

    store ssl in redux store as serverStatus attribute

--- src/renderer/handle-events.js
+++ src/renderer/handle-events.js
@@ -1,6 +1,6 @@
 const {ipcRenderer} = require('electron')
 import { each } from 'lodash'
-import { CONFIG, LATEST_SYNCED_BLOCK, LATEST_SYNCED_GETH_BLOCK, ON_SERVER_CONNECTED, ON_SERVER_DISCONNECTED, PEER_COUNT_DATA, GETH_FINISHED_SYNCING, CONNECTED } from '../utils/constants'
+import { SSL, CONFIG, LATEST_SYNCED_BLOCK, LATEST_SYNCED_GETH_BLOCK, ON_SERVER_CONNECTED, ON_SERVER_DISCONNECTED, PEER_COUNT_DATA, GETH_FINISHED_SYNCING } from '../utils/constants'
 import { addUpdateConnection } from './app/actions/connections'
 import { updateBlockInfo } from './app/actions/blockInfo'
 import { updateServerAttrib } from './app/actions/serverStatus'
@@ -35,8 +35,13 @@ export const handleEvents = () => {
   ipcRenderer.on(PEER_COUNT_DATA, (event, data) => {
     store.dispatch(updateServerAttrib({ PEER_COUNT_DATA: data.peerCount }))
   })
+
   ipcRenderer.on(GETH_FINISHED_SYNCING, () => {
     store.dispatch(updateServerAttrib({ GETH_FINISHED_SYNCING: true }))
+  })
+
+  ipcRenderer.on(SSL, (event, enabled) => {
+    store.dispatch(updateServerAttrib({ SSL: enabled }))
   })
 
 }
