77c40d0762b25c5b84a45d7a070964831c0a7967
phoebemirman <phoebe.mirman@gmail.com>
2018-08-28 13:47:03 -0700

    add loading indicator

--- src/renderer/app/components/processing-view/processing-view.js
+++ src/renderer/app/components/processing-view/processing-view.js
@@ -4,6 +4,18 @@ import PropTypes from "prop-types";
 import Styles from './processing-view.styles.less'
 import classNames from "classnames";
 
+function addCommas(number) {
+  if (!number || isNaN(number)) {
+    return number
+  }
+  let sides = []
+
+  sides = number.toString().split('.')
+  sides[0] = sides[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+
+  return sides.join('.')
+}
+
 export class ProcessingView extends Component {
  	static propTypes = {
 	    processing: PropTypes.bool,
@@ -28,13 +40,26 @@ export class ProcessingView extends Component {
   		const { 
   			processing, blockInfo 
   		} = this.props
-  		// let processing = true
-  		console.log(blockInfo)
+
+		const pct = blockInfo.lastSyncBlockNumber ? ((blockInfo.lastSyncBlockNumber - blockInfo.uploadBlockNumber) / (blockInfo.highestBlockNumber - blockInfo.uploadBlockNumber) * 100) : 0
+  		const pctLbl = Math.floor(pct * Math.pow(10, 2)) / Math.pow(10, 2)
+  		const percent = pctLbl
+
+  		const blocksBehind = addCommas(parseInt(blockInfo.highestBlockNumber, 10) - parseInt(blockInfo.lastSyncBlockNumber, 10)) || 0
+
+  		const blocksProcessed = addCommas(blockInfo.lastSyncBlockNumber) || 0
+  		const highestBlocks = addCommas(blockInfo.highestBlockNumber) || 0
+
+  		const currentPercentStyle = {
+	      width: `${percent}%`
+	    };
+
+	    console.log(currentPercentStyle)
 
 	  	return (
 	  		<section className={classNames(Styles.ProcessingView, {
 		               			[Styles['ProcessingView-processing']]: processing,
-		               			[Styles['ProcessingView-tall']]: !this.state.hideDetails,
+		               			[Styles['ProcessingView-tall']]: processing && !this.state.hideDetails,
 		           			})}
 	  		>
 	  			<div className={Styles.ProcessingView__connectingContainer}>
@@ -47,7 +72,7 @@ export class ProcessingView extends Component {
 		               			[Styles['ProcessingView__processingText-processing']]: processing,
 		           			})}
 				    >
-				    	0 <span className={Styles['ProcessingView__processingText-percent']}>%</span>
+				    	{percent} <span className={Styles['ProcessingView__processingText-percent']}>%</span>
 				    	{ processing && 
 				    		<div className={Styles['ProcessingView__showDetails']} onClick={this.toggleHideDetails}>
 				    			{this.state.hideDetails ? 'Show details' : 'Hide details'}
@@ -59,11 +84,39 @@ export class ProcessingView extends Component {
 				    		</div>
 				    	}
 				    </div>
+				    { processing && !this.state.hideDetails && 
+			    		<div className={Styles.ProcessingView__blocksInfo}>
+			    			<div className={Styles.ProcessingView__blocksBehind}>
+			    				{blocksBehind}
+			    			</div>
+			    			<div className={Styles.ProcessingView__blocksLabel}>
+			    				blocks behind
+			    			</div>
+			    			<div className={Styles.ProcessingView__blocksInfoBreak} />
+			    			<div className={Styles.ProcessingView__blocksFraction}>
+			    				<div className={Styles.ProcessingView__blocksProcessed}>
+			    					{blocksProcessed}
+			    				</div>
+			    				<div className={Styles.ProcessingView__highestBlocks}>
+			    					/ {highestBlocks}
+			    				</div>
+			    			</div>
+			    			<div className={Styles.ProcessingView__blocksLabel}>
+			    				blocks processed
+			    			</div>
+			    		</div>
+				    }
 			    </div>
 			    <div className={classNames(Styles.ProcessingView__loadingIndicator, {
 		               			[Styles['ProcessingView__loadingIndicator-processing']]: processing,
 		           			})}
-	  			/>
+	  			>
+	  				<div className={Styles.ProcessingView__graph}>
+                      <div className={Styles["ProcessingView__graph-current"]}>
+                        <div style={currentPercentStyle} />
+                      </div>
+                    </div>
+	  			</div>
 			</section>
 	  	)
 	}
