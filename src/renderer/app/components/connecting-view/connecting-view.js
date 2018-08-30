28b33ea775643d40e43e69d0a69b1e68f11b63a3
phoebemirman <phoebe.mirman@gmail.com>
2018-08-30 16:35:22 -0700

    rm border

--- src/renderer/app/components/connecting-view/connecting-view.js
+++ src/renderer/app/components/connecting-view/connecting-view.js
@@ -35,21 +35,18 @@ export class ConnectingView extends Component {
 	      backgroundColor: 'transparent',
 	    };
 
-		let syncing = false
+		let syncing = serverStatus.GETH_SYNCING
 		// for light node processing is looking for peers
 		// syncing is syncing
 
   		if (isLocalLightNode) {
-  			if (serverStatus.GETH_SYNCING) {
-  				syncing = true
-  			}
   			if (syncing) {
   				showDisconnected = false
   			}
 
   			const pct = blockInfo.lastSyncBlockNumber ? ((blockInfo.lastSyncBlockNumber - blockInfo.uploadBlockNumber) / (blockInfo.highestBlockNumber - blockInfo.uploadBlockNumber) * 100) : 0
   			let percent = Math.floor(pct * Math.pow(10, 2)) / Math.pow(10, 2)
-  			if (!syncing) {
+  			if (connected) {
   				percent = 0
   			}
   			currentPercentStyle = {
