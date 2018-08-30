a221d1f9169e90559cb2ace97be4bc0bed5a4bde
Paul Gebheim <paul@onetaste.us>
2018-08-30 21:45:17 +0000

    Fix condition for running sqlite based migration

--- src/migrations/20180724123707_search_en.ts
+++ src/migrations/20180724123707_search_en.ts
@@ -1,7 +1,7 @@
 import * as Knex from "knex";
 
 exports.up = async (knex: Knex): Promise<any> => {
-  if (knex.client.config.client !== "pg") return;
+  if (knex.client.config.client !== "sqlite3") return;
 
   return knex.schema.dropTableIfExists("search_en").then((): PromiseLike<any> => {
     return knex.schema.raw(`CREATE VIRTUAL TABLE search_en USING fts4(marketId, content)`);
@@ -9,7 +9,7 @@ exports.up = async (knex: Knex): Promise<any> => {
 };
 
 exports.down = async (knex: Knex): Promise<any> => {
-  if (knex.client.config.client !== "pg") return;
+  if (knex.client.config.client !== "sqlite3") return;
 
   return knex.schema.dropTableIfExists("search_en");
 };
