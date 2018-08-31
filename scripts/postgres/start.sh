825d3eb739fa2e7a35d62d99b6464e3777cadd27
Paul Gebheim <paul@onetaste.us>
2018-08-31 17:56:11 +0000

    Add postgres start script

---
+++ scripts/postgres/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker run \
+  --name augur-postgres \
+  -e POSTGRES_USER=augur \
+  -e POSTGRES_PASSWORD=augur \
+  -e POSTGRES_DATABASE=augur \
+  -p 5432:5432 \
+  -it --rm postgres
+
+export DATABASE_URL="postgresql://augur:augur@localhost:5432/augur" 
