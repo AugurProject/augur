2cb7b8a4a2c62bdeb93c9088abf16ede982f93f8
phoebemirman <phoebe.mirman@gmail.com>
2018-09-04 13:15:51 -0700

    rm notification

--- src/renderer/reducers/notifications.js
+++ src/renderer/reducers/notifications.js
@@ -5,7 +5,7 @@ import { ADD_NOTIFICATION, REMOVE_NOTIFICATION } from '../app/actions/notificati
 
 const DEFAULT_STATE = {
   INFO_NOTIFICATION: [],
-  ERROR_NOTIFICATION: [{desc:'hihi'}]
+  ERROR_NOTIFICATION: []
 }
 
 const MAX_NOTIFICATIONS = 5
