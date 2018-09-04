58e3e1e8b30eb026356187bf3b2ac6b6a204ab33
JohnDanz <John.Danz87@gmail.com>
2018-09-04 09:33:19 -0500

    removed some extra files from modal-network-mismatch

--- src/modules/modal/components/modal-network-mismatch/modal-network-mismatch.jsx
+++ src/modules/modal/components/modal-network-mismatch/modal-network-mismatch.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import isMetaMask from "modules/auth/helpers/is-meta-mask";
-import NetworkMismatchMessage from "modules/modal/components/modal-network-mismatch/network-mismatch-message";
-import MetaMaskNetworkMismatchMessage from "modules/modal/components/modal-network-mismatch/meta-mask-network-mismatch-message";
 import Styles from "modules/modal/components/modal-network-mismatch/modal-network-mismatch.styles";
 
 const ModalNetworkMismatch = p => (
   <section className={Styles.ModalNetworkMismatch}>
     <h1>Network Mismatch</h1>
     {isMetaMask() ? (
-      <MetaMaskNetworkMismatchMessage
-        {...p}
-        expectedNetwork={p.expectedNetwork}
-      />
+      <section className={Styles.NetworkMismatchMessage}>
+        <span>MetaMask is connected to the wrong Ethereum network.</span>
+        <span>Please set the MetaMask network to: {p.expectedNetwork}</span>
+      </section>
     ) : (
-      <NetworkMismatchMessage {...p} expectedNetwork={p.expectedNetwork} />
+      <section className={Styles.NetworkMismatchMessage}>
+        <span>
+          Your Ethereum node and Augur node are connected to different networks.
+        </span>
+        <span>Please connect to a {p.expectedNetwork} Ethereum node.</span>
+      </section>
     )}
   </section>
 );
