6b903cad1cfdba5d82d6d32ffd9cd3aa8d823420
Peter Hicks <petong@jah.net>
2018-09-12 16:16:04 -0700

    remove docker from linux env, add node_js

--- scripts/electron-build.sh
+++ scripts/electron-build.sh
@@ -24,12 +24,6 @@ else
     rm -rf node_modules/*
     sudo apt update
     sudo apt install -y libusb-{dev,1.0-0-dev} rpm curl tzdata python-pip
-    export NVM_DIR="$HOME/.nvm"
-    mkdir -p $HOME/.nvm
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    nvm install v9.11.2
-    nvm use v9.11.2
     npm install
     npm run make-linux
     sudo pip install requests
