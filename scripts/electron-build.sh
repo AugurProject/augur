824275e8a42bd774918ef02327c148783cbe3fcc
Peter Hicks <petong@jah.net>
2018-09-12 16:42:39 -0700

    /dev/null isn't working, revert

--- scripts/electron-build.sh
+++ scripts/electron-build.sh
@@ -24,7 +24,7 @@ else
     rm -rf node_modules/*
     sudo apt-get update
     sudo apt-get install -y libusb-{dev,1.0-0-dev} rpm curl tzdata python-pip
-    npm install 2>&1 /dev/null
+    npm install
     npm run make-linux
     sudo pip install requests
 fi
