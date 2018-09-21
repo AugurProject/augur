74aa0092acbf70a8cb6dba80fe7eb3992c7a10ea
Tom Haile <bradford.haile@gmail.com>
2018-09-21 13:52:38 -0500

    more linting

--- src/modules/auth/actions/login-with-metamask.js
+++ src/modules/auth/actions/login-with-metamask.js
@@ -3,11 +3,7 @@ import { augur } from "services/augurjs";
 import { useUnlockedAccount } from "modules/auth/actions/use-unlocked-account";
 import { first } from "lodash";
 
-export const loginWithMetaMask = (callback = logError) => (
-  dispatch,
-  getState
-) => {
-  const { env, authStatus } = getState();
+export const loginWithMetaMask = (callback = logError) => dispatch => {
   augur.rpc.eth.accounts((err, accounts) => {
     const account = first(accounts);
     if (err || !account) return callback("NOT_SIGNED_IN");
