d1a3335140254f5d628d62c6bc639ba01e3c301f
phoebemirman <phoebe.mirman@gmail.com>
2018-09-21 08:47:01 -0700

    add logouts if account isnt same

--- src/modules/app/actions/init-augur.js
+++ src/modules/app/actions/init-augur.js
@@ -85,11 +85,17 @@ function loadAccount(dispatch, existing, env, callback) {
       account = accounts[0];
       if (account && (env.useWeb3Transport || process.env.AUTO_LOGIN)) {
         dispatch(useUnlockedAccount(account));
-      } else if (loggedInAccount && account === loggedInAccount) {
+      } else if (loggedInAccount && accounts.includes(loggedInAccount)) {
         dispatch(useUnlockedAccount(loggedInAccount));
       } else {
         dispatch(logout());
       }
+
+      if (loggedInAccount && account !== loggedInAccount) {
+        dispatch(logout());
+      }
+    } else if (existing !== loggedInAccount) {
+      dispatch(logout());
     }
     if (!existing && loggedInAccount && accounts.includes(loggedInAccount)) {
       dispatch(useUnlockedAccount(loggedInAccount));
