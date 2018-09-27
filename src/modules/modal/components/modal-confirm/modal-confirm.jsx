d1f7705c043a987b341f8dc2a531f4f493c2619d
JohnDanz <John.Danz87@gmail.com>
2018-09-27 11:39:18 -0500

    removed modal-approval components since they are no longer required, started redesiging the terms and conditions modal

--- src/modules/modal/components/modal-confirm/modal-confirm.jsx
+++ src/modules/modal/components/modal-confirm/modal-confirm.jsx
@@ -6,39 +6,36 @@ import ModalDescription from "modules/modal/components/common/modal-description"
 
 import Styles from "modules/modal/components/modal-confirm/modal-confirm.styles";
 
-const ModalConfirm = p => {
-  console.log("modalconfirmprops", p);
-  return (
-    <section className={Styles.ModalConfirm}>
-      <h1>{p.title}</h1>
-      {p.description.map(text => (
-        <ModalDescription text={text} key={text} />
-      ))}
-      <div className={Styles.ModalConfirm__ActionButtons}>
-        <button
-          className={Styles.ModalConfirm__cancel}
-          onClick={e => {
-            if (!p.continueDefault) e.preventDefault();
-            p.cancelAction();
-            p.closeModal();
-          }}
-        >
-          {p.cancelButtonText}
-        </button>
-        <button
-          className={Styles.ModalConfirm__submit}
-          onClick={e => {
-            if (!p.continueDefault) e.preventDefault();
-            p.submitAction();
-            p.closeModal();
-          }}
-        >
-          {p.submitButtonText}
-        </button>
-      </div>
-    </section>
-  );
-};
+const ModalConfirm = p => (
+  <section className={Styles.ModalConfirm}>
+    <h1>{p.title}</h1>
+    {p.description.map(text => (
+      <ModalDescription text={text} key={text} />
+    ))}
+    <div className={Styles.ModalConfirm__ActionButtons}>
+      <button
+        className={Styles.ModalConfirm__cancel}
+        onClick={e => {
+          if (!p.continueDefault) e.preventDefault();
+          p.cancelAction();
+          p.closeModal();
+        }}
+      >
+        {p.cancelButtonText}
+      </button>
+      <button
+        className={Styles.ModalConfirm__submit}
+        onClick={e => {
+          if (!p.continueDefault) e.preventDefault();
+          p.submitAction();
+          p.closeModal();
+        }}
+      >
+        {p.submitButtonText}
+      </button>
+    </div>
+  </section>
+);
 
 ModalConfirm.propTypes = {
   description: PropTypes.arrayOf(PropTypes.string).isRequired,
