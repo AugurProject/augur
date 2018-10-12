406d506ddcf9b7102eedffa69bd397d42b66360f
Paul Gebheim <paul@onetaste.us>
2018-10-12 00:28:34 +0000

    Fix ref to augur-core

--- packages/augur.js/src/contracts/index.js
+++ packages/augur.js/src/contracts/index.js
@@ -4,7 +4,7 @@ var generateAbiMap = require("./generate-abi-map");
 var readJsonFile = require("../utils/read-json-file");
 
 var e = module.exports = {
-  abi: generateAbiMap(require("augur-core").abi),
+  abi: generateAbiMap(require("augur-artifacts/abi")),
   addresses: require("augur-artifacts/addresses"),
   uploadBlockNumbers: require("augur-artifacts/upload-block-numbers"),
 };
