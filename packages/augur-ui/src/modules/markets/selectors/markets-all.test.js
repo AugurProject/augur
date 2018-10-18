f62d8586a93042995900c3cd360ae7c9650f78a8
Tom Haile <bradford.haile@gmail.com>
2018-10-18 11:45:28 -0500

    added another test, use beforeEach()

--- packages/augur-ui/src/modules/markets/selectors/markets-all.test.js
+++ packages/augur-ui/src/modules/markets/selectors/markets-all.test.js
@@ -1,46 +1,61 @@
 describe(`modules/markets/selectors/markets-all.js`, () => {
-  jest.mock("./market");
-  jest.mock("../../../select-state");
-  const { selectMarket } = require("./market");
-  const { selectMarketsDataState } = require("../../../select-state");
+  beforeEach(() => {
+    jest.mock("./market");
+    jest.mock("../../../select-state");
+    const { selectMarket } = require("./market");
 
-  selectMarket.mockImplementation(value => value);
-  selectMarketsDataState.mockImplementationOnce(() => ({
-    test: {
-      endTime: parseInt(new Date("01/01/3000").getTime() / 1000, 10),
-      outcomes: {
-        test: {}
-      },
-      volume: {
-        value: 5
-      }
-    },
-    test2: {
-      endTime: parseInt(new Date("01/01/3000").getTime() / 1000, 10),
-      outcomes: {
-        test2: {}
-      },
-      volume: {
-        value: 10
-      }
-    },
-    test3: {
-      endTime: parseInt(new Date("01/01/3000").getTime() / 1000, 10),
-      outcomes: {
-        test3: {}
-      },
-      volume: {
-        value: 7
-      }
-    }
-  }));
-
-  const { selectMarkets } = require("./markets-all");
+    selectMarket.mockImplementation(value => value);
+  });
 
   test(`should return the correct selectedMarket function`, () => {
+    const { selectMarketsDataState } = require("../../../select-state");
+    selectMarketsDataState.mockImplementationOnce(() => ({
+      test: {
+        endTime: parseInt(new Date("01/01/3000").getTime() / 1000, 10),
+        outcomes: {
+          test: {}
+        },
+        volume: {
+          value: 5
+        }
+      },
+      test2: {
+        endTime: parseInt(new Date("01/01/3000").getTime() / 1000, 10),
+        outcomes: {
+          test2: {}
+        },
+        volume: {
+          value: 10
+        }
+      },
+      test3: {
+        endTime: parseInt(new Date("01/01/3000").getTime() / 1000, 10),
+        outcomes: {
+          test3: {}
+        },
+        volume: {
+          value: 7
+        }
+      }
+    }));
+
+    const { selectMarkets } = require("./markets-all");
+
     const markets = selectMarkets();
 
     expect(markets).toBeDefined();
     expect(markets).toHaveLength(3);
+  });
+
+  test(`should return zero markets`, () => {
+    const { selectMarketsDataState } = require("../../../select-state");
+    selectMarketsDataState.mockImplementationOnce(() => ({}));
+
+    const { selectMarkets } = require("./markets-all");
+
+    const markets = selectMarkets();
+
+    expect(markets).toBeDefined();
+    expect(markets).toHaveLength(0);
   });
 });
