7ef549acb51fcb203f173230b90ee460fa6ecaff
Scott Bigelow <epheph@gmail.com>
2018-10-18 09:54:30 -0700

    better ordering

---
+++ src/utils/retryable-error.ts
@@ -0,0 +1,6 @@
+//
+// export function convertDivisorToRate(divisor: string, base?: number) {
+//   const d = new BigNumber(divisor, base || 10);
+//   if (d.isEqualTo(ZERO)) return "0";
+//   return ONE.dividedBy(d).toFixed();
+// }
