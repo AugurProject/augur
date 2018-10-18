4a7444b172bb0d68cd5536746a6fe5cb6645b7e9
Scott Bigelow <epheph@gmail.com>
2018-10-18 13:47:13 -0700

    accidental commit

--- src/utils/retryable-error.ts
+++
@@ -1,6 +0,0 @@
-//
-// export function convertDivisorToRate(divisor: string, base?: number) {
-//   const d = new BigNumber(divisor, base || 10);
-//   if (d.isEqualTo(ZERO)) return "0";
-//   return ONE.dividedBy(d).toFixed();
-// }
