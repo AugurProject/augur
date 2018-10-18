35005b16ab51853ab711cdb091a37fe466227639
JohnDanz <John.Danz87@gmail.com>
2018-10-18 17:17:11 -0500

    fixed failing test

--- src/modules/universe/actions/load-universe-info.test.js
+++ src/modules/universe/actions/load-universe-info.test.js
@@ -57,7 +57,8 @@ describe("modules/account/actions/load-universe-info.js", () => {
         ]
       };
 
-      loadUniverseInfo(actual => {
+      loadUniverseInfo((err, actual) => {
+        expect(err).toStrictEqual(null);
         expect(actual).toStrictEqual(expected);
       })(null, getState);
     });
