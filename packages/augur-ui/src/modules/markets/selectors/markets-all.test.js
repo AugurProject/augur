bbae93a00e5770f6fd92cc59d1418e877d442df8
Tom Haile <bradford.haile@gmail.com>
2018-10-18 21:11:56 -0500

    removed mocks for select-state, not needed

--- packages/augur-ui/src/modules/markets/selectors/markets-all.test.js
+++ packages/augur-ui/src/modules/markets/selectors/markets-all.test.js
@@ -1,54 +1,52 @@
 jest.mock("modules/markets/selectors/market");
-jest.mock("src/select-state");
 
 describe(`modules/markets/selectors/markets-all.js`, () => {
   test(`should return the correct selectedMarket function`, () => {
-    const { selectMarketsDataState } = require("src/select-state");
-    selectMarketsDataState.__setSelectMarketsDataState(() => ({
-      test: {
-        endTime: parseInt(new Date("01/01/3000").getTime() / 1000, 10),
-        outcomes: {
-          test: {}
+    const state = {
+      marketsData: {
+        test: {
+          endTime: parseInt(new Date("01/01/3000").getTime() / 1000, 10),
+          outcomes: {
+            test: {}
+          },
+          volume: {
+            value: 5
+          }
         },
-        volume: {
-          value: 5
-        }
-      },
-      test2: {
-        endTime: parseInt(new Date("01/01/3000").getTime() / 1000, 10),
-        outcomes: {
-          test2: {}
+        test2: {
+          endTime: parseInt(new Date("01/01/3000").getTime() / 1000, 10),
+          outcomes: {
+            test2: {}
+          },
+          volume: {
+            value: 10
+          }
         },
-        volume: {
-          value: 10
-        }
-      },
-      test3: {
-        endTime: parseInt(new Date("01/01/3000").getTime() / 1000, 10),
-        outcomes: {
-          test3: {}
-        },
-        volume: {
-          value: 7
+        test3: {
+          endTime: parseInt(new Date("01/01/3000").getTime() / 1000, 10),
+          outcomes: {
+            test3: {}
+          },
+          volume: {
+            value: 7
+          }
         }
       }
-    }));
+    };
 
-    const { selectMarkets } = require("./markets-all");
+    const { selectMarkets } = require("modules/markets/selectors/markets-all");
 
-    const markets = selectMarkets();
+    const markets = selectMarkets(state);
 
     expect(markets).toBeDefined();
     expect(markets).toHaveLength(3);
   });
 
   test(`should return zero markets`, () => {
-    const { selectMarketsDataState } = require("src/select-state");
-    selectMarketsDataState.__setSelectMarketsDataState(() => ({}));
+    const state = { marketsData: {} };
+    const { selectMarkets } = require("modules/markets/selectors/markets-all");
 
-    const { selectMarkets } = require("./markets-all");
-
-    const markets = selectMarkets();
+    const markets = selectMarkets(state);
 
     expect(markets).toBeDefined();
     expect(markets).toHaveLength(0);
