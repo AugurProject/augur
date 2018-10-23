3c1b4a8387e6c635013ec1f152dca93a2d95423a
JohnDanz <John.Danz87@gmail.com>
2018-10-23 12:03:32 -0500

    updated isGlobalWeb3 to look for window.ethereum to meet new metamask breaking changes, updated login-with-metamask to ask for permission if needed

--- src/modules/auth/helpers/is-global-web3.js
+++ src/modules/auth/helpers/is-global-web3.js
@@ -1,7 +1,6 @@
 export default function() {
   if (typeof window === "undefined") return false;
   if (!window) return false;
-  if (!window.web3) return false;
-  if (!window.web3.currentProvider) return false;
+  if (!window.ethereum) return false;
   return true;
 }
