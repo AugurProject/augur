ddab121fee1ae3c38367dc7afabb735ad6f86954
JohnDanz <John.Danz87@gmail.com>
2018-10-23 12:03:32 -0500

    updated isGlobalWeb3 to look for window.ethereum to meet new metamask breaking changes, updated login-with-metamask to ask for permission if needed

--- src/modules/auth/actions/login-with-metamask.js
+++ src/modules/auth/actions/login-with-metamask.js
@@ -2,12 +2,23 @@ import logError from "utils/log-error";
 import { augur } from "services/augurjs";
 import { useUnlockedAccount } from "modules/auth/actions/use-unlocked-account";
 import { first } from "lodash";
+import { windowRef } from "utils/window-ref";
 
 export const loginWithMetaMask = (callback = logError) => dispatch => {
-  augur.rpc.eth.accounts((err, accounts) => {
-    const account = first(accounts);
-    if (err || !account) return callback("NOT_SIGNED_IN");
+  const failure = () => callback("NOT_SIGNED_IN");
+  const success = account => {
     dispatch(useUnlockedAccount(account));
     callback(null, account);
+  };
+  augur.rpc.eth.accounts((err, accounts) => {
+    const account = first(accounts);
+    if (err) return failure();
+    if (account) {
+      success(account);
+    } else {
+      windowRef.ethereum
+        .enable()
+        .then(resolve => success(first(resolve)), failure);
+    }
   });
 };
