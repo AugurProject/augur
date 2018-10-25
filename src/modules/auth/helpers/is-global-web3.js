5bd77801f8162486db16d46c3a44b090f6999c15
JohnDanz <John.Danz87@gmail.com>
2018-10-25 12:30:52 -0500

    updated isGlobalWeb3 to check for fallback/legacy web3

--- src/modules/auth/helpers/is-global-web3.js
+++ src/modules/auth/helpers/is-global-web3.js
@@ -1,6 +1,9 @@
 export default function() {
   if (typeof window === "undefined") return false;
   if (!window) return false;
-  if (!window.ethereum) return false;
+  if (!window.ethereum) {
+    if (window.web3 && window.web3.currentProvider) return true;
+    return false;
+  }
   return true;
 }
