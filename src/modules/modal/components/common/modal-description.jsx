7c552836c58b35f38119a8f997e89eedc0cad54b
phoebemirman <phoebe.mirman@gmail.com>
2018-10-29 11:19:28 -0700

    use html parser to bold passed in strings, remove space between lines (#2436)

--- src/modules/modal/components/common/modal-description.jsx
+++ src/modules/modal/components/common/modal-description.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import ReactHtmlParser from "react-html-parser";
 
 import Styles from "modules/modal/components/common/common.styles";
 
 const ModalDescription = ({ text }) => (
-  <p className={Styles.Description}>{text}</p>
+  <p className={Styles.Description}>{ReactHtmlParser(text)}</p>
 );
 
 ModalDescription.propTypes = {
--- src/modules/modal/containers/modal-mismatch.js
+++ src/modules/modal/containers/modal-mismatch.js
@@ -14,12 +14,16 @@ const mergeProps = (sP, dP, oP) => {
   const description = [];
   if (isMetaMask()) {
     description.push(`MetaMask is connected to the wrong Ethereum network.`);
-    description.push(`Please set the MetaMask network to: ${expectedNetwork}.`);
+    description.push(
+      `Please set the MetaMask network to: <strong>${expectedNetwork}</strong>.`
+    );
   } else {
     description.push(
       `Your Ethereum node and Augur node are connected to different networks.`
     );
-    description.push(`Please connect to a ${expectedNetwork} Ethereum node.`);
+    description.push(
+      `Please connect to a <strong>${expectedNetwork}</strong> Ethereum node.`
+    );
   }
 
   return {
