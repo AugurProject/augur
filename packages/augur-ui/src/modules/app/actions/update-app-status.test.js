9a3e7e4a614cca9b2f61739c2b8cb8a8c5b455b2
unparallel <juliya@augur.net>
2018-11-05 15:36:33 -0600

    Migrated update-app-status test to jest

--- packages/augur-ui/src/modules/app/actions/update-app-status.test.js
+++ packages/augur-ui/src/modules/app/actions/update-app-status.test.js
@@ -9,96 +9,86 @@ import {
 } from "modules/app/actions/update-app-status";
 
 describe("modules/app/actions/update-app-status.js", () => {
-  const test = t => {
-    it(t.description, () => {
-      t.assertions(updateAppStatus(t.statusKey, t.value));
-    });
-  };
-
-  test({
+  const t1 = {
     description: "should return the expected object for updating isAnimating",
     statusKey: IS_ANIMATING,
     value: true,
     assertions: action => {
-      const expected = {
+      expect(action).toEqual({
         type: UPDATE_APP_STATUS,
         data: {
           statusKey: IS_ANIMATING,
           value: true
         }
-      };
-
-      assert.deepEqual(action, expected, `Didn't return the expected object`);
+      });
     }
-  });
+  };
 
-  test({
+  const t2 = {
     description: "should return the expected object for updating isMobile",
     statusKey: IS_MOBILE,
     value: true,
     assertions: action => {
-      const expected = {
+      expect(action).toEqual({
         type: UPDATE_APP_STATUS,
         data: {
           statusKey: IS_MOBILE,
           value: true
         }
-      };
-
-      assert.deepEqual(action, expected, `Didn't return the expected object`);
+      });
     }
-  });
+  };
 
-  test({
+  const t3 = {
     description: "should return the expected object for updating isMobileSmall",
     statusKey: IS_MOBILE_SMALL,
     value: true,
     assertions: action => {
-      const expected = {
+      expect(action).toEqual({
         type: UPDATE_APP_STATUS,
         data: {
           statusKey: IS_MOBILE_SMALL,
           value: true
         }
-      };
-
-      assert.deepEqual(action, expected, `Didn't return the expected object`);
+      });
     }
-  });
+  };
 
-  test({
+  const t4 = {
     description:
       "should return the expected object for updating hasLoadedMarkets",
     statusKey: HAS_LOADED_MARKETS,
     value: true,
     assertions: action => {
-      const expected = {
+      expect(action).toEqual({
         type: UPDATE_APP_STATUS,
         data: {
           statusKey: HAS_LOADED_MARKETS,
           value: true
         }
-      };
-
-      assert.deepEqual(action, expected, `Didn't return the expected object`);
+      });
     }
-  });
+  };
 
-  test({
+  const t5 = {
     description:
       "should return the expected object for updating transactionsLoading",
     statusKey: TRANSACTIONS_LOADING,
     value: true,
     assertions: action => {
-      const expected = {
+      expect(action).toEqual({
         type: UPDATE_APP_STATUS,
         data: {
           statusKey: TRANSACTIONS_LOADING,
           value: true
         }
-      };
-
-      assert.deepEqual(action, expected, `Didn't return the expected object`);
+      });
     }
+  };
+
+  describe.each([t1, t2, t3, t4, t5])("Update app status tests", t => {
+    test(t.description, () => {
+      t.assertions(updateAppStatus(t.statusKey, t.value));
+    });
   });
 });
