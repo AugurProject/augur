35a50caa2dc4e7f828281a0c4c858baf89d11073
JohnDanz <John.Danz87@gmail.com>
2018-11-07 13:58:28 -0600

    mods

--- src/utils/create-big-number.js
+++ src/utils/create-big-number.js
@@ -4,8 +4,7 @@ import { BigNumber } from "bignumber.js";
 export const createBigNumber = (value, ...args) => {
   let newBigNumber;
   try {
-    const bn = typeof value === "number" ? value.toString() : value;
-    newBigNumber = new BigNumber(bn, ...args);
+    newBigNumber = new BigNumber(`${value}`, ...args);
   } catch (e) {
     logError("Error instantiating WrappedBigNumber", e);
   }
