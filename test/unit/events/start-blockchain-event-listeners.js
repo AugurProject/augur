a9a0aebf39ee8697be3fba71d2d421ccf8bc8d3b
Scott Bigelow <epheph@gmail.com>
2018-11-07 13:59:07 -0800

    Forgot to offset arguments in test

--- test/unit/events/start-blockchain-event-listeners.js
+++ test/unit/events/start-blockchain-event-listeners.js
@@ -37,7 +37,7 @@ describe("events/start-blockchain-event-listeners", function () {
         "../contracts": t.mock.contracts,
         "../rpc-interface": t.stub.rpcInterface,
       });
-      startBlockchainEventListeners(t.params, undefined, undefined, function (err) {
+      startBlockchainEventListeners(t.params, undefined, undefined, undefined, function (err) {
         t.assertions(err);
         done();
       });
