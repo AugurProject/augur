2940f5f7588f0a9a1dff4917d5174e3a9f9fa176
nuevoalex <achap5dk@gmail.com>
2018-11-16 20:01:45 -0800

    the block number given by rpc is static. Need to actually make request

--- src/blockchain/bulk-sync-augur-node-with-blockchain.ts
+++ src/blockchain/bulk-sync-augur-node-with-blockchain.ts
@@ -28,7 +28,7 @@ export async function bulkSyncAugurNodeWithBlockchain(db: Knex, augur: Augur): P
   const row: HighestBlockNumberRow|null = await db("blocks").max("blockNumber as highestBlockNumber").first();
   const lastSyncBlockNumber: number|null|undefined = row!.highestBlockNumber;
   const uploadBlockNumber: number = augur.contracts.uploadBlockNumbers[augur.rpc.getNetworkID()] || 0;
-  let highestBlockNumber: number = parseInt(augur.rpc.getCurrentBlock().number, 16);
+  let highestBlockNumber: number = await getHighestBlockNumber(augur);
   let fromBlock: number;
   if (uploadBlockNumber > highestBlockNumber) {
     logger.info(`Synchronization started at (${uploadBlockNumber}), which exceeds the current block from the ethereum node (${highestBlockNumber}), starting from 0 instead`);
@@ -37,15 +37,30 @@ export async function bulkSyncAugurNodeWithBlockchain(db: Knex, augur: Augur): P
     fromBlock = lastSyncBlockNumber == null ? uploadBlockNumber : lastSyncBlockNumber + 1;
   }
   let handoffBlockNumber = highestBlockNumber - BLOCKSTREAM_HANDOFF_BLOCKS;
+  let skipBulkDownload = false;
   while (handoffBlockNumber < fromBlock) {
+    skipBulkDownload = true;
     logger.warn(`Not enough blocks to start blockstream reliably, waiting at least ${BLOCKSTREAM_HANDOFF_BLOCKS} from ${fromBlock}. Current Block: ${highestBlockNumber}`);
     await delay(BLOCKSTREAM_HANDOFF_WAIT_TIME_MS);
-    highestBlockNumber = parseInt(augur.rpc.getCurrentBlock().number, 16);
+    highestBlockNumber = await getHighestBlockNumber(augur);
     handoffBlockNumber = highestBlockNumber - BLOCKSTREAM_HANDOFF_BLOCKS;
+  }
+  if (skipBulkDownload) {
+    logger.info(`Skipping batch load`);
+    return fromBlock;
   }
   await promisify(downloadAugurLogs)(db, augur, fromBlock, handoffBlockNumber);
   setSyncFinished();
   await db.insert({ highestBlockNumber }).into("blockchain_sync_history");
   logger.info(`Finished batch load from ${fromBlock} to ${handoffBlockNumber}`);
   return handoffBlockNumber;
+}
+
+async function getHighestBlockNumber(augur: Augur): Promise<number> {
+  return new Promise<number>((resolve, reject) => {
+    augur.rpc.eth.blockNumber("latest", (error, blockNumber: string) => {
+      if (error) reject("Couldn't get block number");
+      resolve(parseInt(blockNumber.toString(), 16));
+    });
+  });
 }
