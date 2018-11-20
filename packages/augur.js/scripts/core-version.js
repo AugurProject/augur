537306092f775b2eed9c2f034799f239969c4d9b
Tom Haile <bradford.haile@gmail.com>
2018-11-20 12:48:12 -0600

    remove all scripts from augur.js

--- packages/augur.js/scripts/core-version.js
+++
@@ -1,9 +0,0 @@
-var package = require("../package.json");
-var version = package.dependencies["augur-core"];
-
-if (!version) {
-  console.log("Could not find version of augur-core in package.json");
-  process.exit(1);
-}
-
-console.log(version);
--- packages/augur.js/scripts/get-version.js
+++
@@ -1,9 +0,0 @@
-var package = require(process.argv[2]);
-var version = package.version;
-
-if (!version) {
-  console.log("Could not find version in package.json");
-  process.exit(1);
-}
-
-console.log(version);
