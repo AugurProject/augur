50abfafe0bd3c025b0451508bdd770d33754c86b
Justin Barry <328965+justinbarry@users.noreply.github.com>
2018-12-07 01:42:39 -0800

    Name multiple returns to satisfy typescript interface generator

--- packages/augur-core/source/contracts/trading/IOrdersFetcher.sol
+++ packages/augur-core/source/contracts/trading/IOrdersFetcher.sol
@@ -6,5 +6,5 @@ import 'reporting/IMarket.sol';
 
 
 contract IOrdersFetcher {
-    function findBoundingOrders(Order.Types _type, uint256 _price, bytes32 _bestOrderId, bytes32 _worstOrderId, bytes32 _betterOrderId, bytes32 _worseOrderId) public returns (bytes32, bytes32);
+    function findBoundingOrders(Order.Types _type, uint256 _price, bytes32 _bestOrderId, bytes32 _worstOrderId, bytes32 _betterOrderId, bytes32 _worseOrderId) public returns (bytes32 betterOrderId, bytes32 worseOrderId);
 }
