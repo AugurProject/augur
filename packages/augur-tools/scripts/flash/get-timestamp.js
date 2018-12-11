def890b95da75779873ecc6965d2bf0e8a68c644
Tom Haile <bradford.haile@gmail.com>
2018-12-11 16:40:07 -0600

    Update flash timestamp (#628)
    * rm yarn.lock
    
    * update flash to push timestamp
    
    * Update package.json
    
    revert adding web3
    
    * Update bind-contract-function.js
    
    remove console.log
    
    * add back in yarn.lock file
    
    * revert to connect to augur-node, it will run
    
    * Update bind-contract-function.js
    
    remove extra space
    
    * update package.json for augur-ui needed lib

--- packages/augur-tools/scripts/flash/get-timestamp.js
+++ packages/augur-tools/scripts/flash/get-timestamp.js
@@ -7,16 +7,22 @@ var chalk = require("chalk");
 function getTime(augur, auth, callback) {
   var controller =
     augur.contracts.addresses[augur.rpc.getNetworkID()].Controller;
+  var _key = "0x54696d6500000000000000000000000000000000000000000000000000000000"; // "Time"
+  console.log(_key, controller);
   augur.api.Controller.lookup(
     {
       meta: auth,
       tx: { to: controller },
-      _key: augur.format.tag.encodeTag("Time")
+      _key
     },
     function(err, timeAddress) {
+      console.log("get time address", err, timeAddress);
       if (err) {
         console.log(chalk.red(err));
         return callback(err);
+      }
+      if (timeAddress === "0x0000000000000000000000000000000000000000") {
+        return callback("time contract address not found");
       }
       augur.api.Controller.getTimestamp(function(err, timestamp) {
         if (err) {
