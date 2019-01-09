ae9c4a145fe0c98e7d30d72a9afd23163d508b45
Joseph Krug <joeykrug@gmail.com>
2019-01-09 12:08:54 -0800

    Update babel.config.js (#2602)

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ["@babel/preset-react", "@babel/preset-env"],
+  presets: [["@babel/preset-react"], ["@babel/preset-env", {"useBuiltIns":"entry", "targets":"> 0.25%, not dead, Chrome >= 41"}]],
   plugins: [
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-syntax-dynamic-import",
