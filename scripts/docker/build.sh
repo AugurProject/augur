de8d486d6525c3cc64a182eb19a4dd371780efd4
Peter Hicks <petong@jah.net>
2019-01-11 09:21:34 -0800

    changes node version in build.sh

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -3,7 +3,7 @@
 TAG=core-$(node scripts/core-version.js)
 
 # Make sure you're up to date
-docker pull node:8
+docker pull node:10.12
 
 IMAGE_NAME=augurproject/augur-node
 docker build --no-cache . -t $IMAGE_NAME:$TAG -t $IMAGE_NAME:dev
