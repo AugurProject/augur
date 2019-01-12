d0e91b586bc95727329115c3406588d164ee638d
unknown <achap5dk@gmail.com>
2019-01-11 16:48:49 -0800

    export interfaces

--- packages/augur-core/source/index.ts
+++ packages/augur-core/source/index.ts
@@ -2,3 +2,9 @@ export { NetworkConfiguration } from "./libraries/NetworkConfiguration";
 export { DeployerConfiguration } from "./libraries/DeployerConfiguration";
 export { ContractDeployer } from "./libraries/ContractDeployer";
 
+import * as GenericAugurInterfaces from "./libraries/GenericContractInterfaces";
+import * as EthersAugurInterfaces from "./libraries/ContractInterfaces";
+export {
+    GenericAugurInterfaces,
+    EthersAugurInterfaces
+}
