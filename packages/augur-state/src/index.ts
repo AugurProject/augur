a0178e61b789e3c8c02bb4ff44357fff30294bd2
unknown <achap5dk@gmail.com>
2019-01-14 15:42:45 -0800

    initial project commit

---
+++ packages/augur-state/src/index.ts
@@ -0,0 +1,4 @@
+export { AugurStateSyncController } from "./sync/AugurStateSyncController";
+export { AugurStateAPIController } from "./api/AugurStateAPIController";
+export { AugurStateServerController } from "./server/AugurStateServerController";
+export { AugurStateController } from "AugurStateController";
---
+++ packages/augur-state/src/repl.ts
@@ -0,0 +1,11 @@
+import * as repl from "repl";
+import { augurEmitter } from "./events";
+import "./runServer";
+
+const replServer = repl.start();
+
+Object.defineProperty(replServer.context, "augurEmitter", {
+  configurable: false,
+  enumerable: true,
+  value: augurEmitter,
+});
