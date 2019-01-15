d04f50791a0e720604b2574c0f62f82342fbce0c
unknown <achap5dk@gmail.com>
2019-01-14 18:40:32 -0800

    add AN example too

--- packages/typings-example/src/SomeFile.ts
+++ packages/typings-example/src/SomeFile.ts
@@ -1,4 +1,10 @@
 import { ContractDeployer } from "augur-core";
+import { AugurNodeController } from "augur-node";
 
 export class Test extends ContractDeployer {
+    private controller:AugurNodeController;
+
+    public doThing(): boolean {
+        return this.controller.dasdasd();
+    }
 }
