80290ed68c3eb05f1ba87dadc667f0b1a2dc41ff
Paul Gebheim <paul@onetaste.us>
2019-01-16 17:29:19 +0000

    Update tsconfig to include packages as part of the stuff to build

--- packages/typings-example/src/SomeFile.ts
+++ packages/typings-example/src/SomeFile.ts
@@ -2,7 +2,9 @@ import { ContractDeployer } from "augur-core";
 import { AugurNodeController } from "augur-node";
 
 export class Test extends ContractDeployer {
-    public doThing(): boolean {
-        return this.controller.dasdasd();
-    }
+  private controller: AugurNodeController;
+
+  public doThing(): boolean {
+    return this.controller.dasdasd();
+  }
 }
