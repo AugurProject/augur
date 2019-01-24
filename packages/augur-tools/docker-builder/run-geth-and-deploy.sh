73b6a52f00d3de4fbf30348ee8d667237d98b112
Peter Hicks <petong@jah.net>
2019-01-24 14:14:56 -0800

    checks to see if nvm is installed and uses that version of node if so

--- packages/augur-tools/docker-builder/run-geth-and-deploy.sh
+++ packages/augur-tools/docker-builder/run-geth-and-deploy.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-cd /
-./start.sh &
+# comment out the commands below since the geth docker image
+# already is running
+#cd /
+#./start.sh &
+
+export NVM_DIR="/usr/local/nvm"
+if [[ -d $NVM_DIR ]]; then
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    nvm use --delete-prefix $NODE_VERSION
+fi
+
+which node
 
 echo "Resting for a bit"
 sleep 10s
