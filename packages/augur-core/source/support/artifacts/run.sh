6d0930634fae90001789a747c57c386877a676f7
Peter Hicks <petong@jah.net>
2019-01-24 14:12:52 -0800

    adds some debug output while running the artifacts script

--- packages/augur-core/source/support/artifacts/run.sh
+++ packages/augur-core/source/support/artifacts/run.sh
@@ -2,5 +2,10 @@
 
 set -e
 
+# generate abi.json
+echo 'generating abi.json'
 docker run --rm --entrypoint cat augurproject/augur-core:latest /app/output/contracts/abi.json > ../augur-artifacts/abi.json
+
+# generate contracts.json'
+echo 'generating contracts.json'
 docker run --rm --entrypoint cat augurproject/augur-core:latest /app/output/contracts/contracts.json > ../augur-artifacts/contracts.json
