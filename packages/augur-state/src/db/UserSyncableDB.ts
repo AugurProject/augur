59385f1ac989f58fad5215b8cd3e0960a0edca71
Justin Barry <328965+justinbarry@users.noreply.github.com>
2019-01-30 17:05:22 -0800

    Fix type def

--- packages/augur-state/src/db/UserSyncableDB.ts
+++ packages/augur-state/src/db/UserSyncableDB.ts
@@ -5,7 +5,7 @@ import { DB } from './DB';
 
 export class UserSyncableDB<TBigNumber> extends SyncableDB<TBigNumber> {
     public user: string;
-    private additionalTopics: Array<null|string>;
+    private additionalTopics: Array<string | Array<string>>;
 
     constructor(dbController: DB<TBigNumber>, eventName: string, user: string, numAdditionalTopics: number, userTopicIndex: number) {
         const dbName = `${eventName}-${user}`;
@@ -13,7 +13,7 @@ export class UserSyncableDB<TBigNumber> extends SyncableDB<TBigNumber> {
         this.user = user;
         const bytes32User = `0x000000000000000000000000${this.user.substr(2)}`;
         this.additionalTopics = [];
-        this.additionalTopics.fill(null, numAdditionalTopics);
+        this.additionalTopics.fill("", numAdditionalTopics);
         this.additionalTopics[userTopicIndex] = bytes32User;
         console.log(JSON.stringify(this.additionalTopics));
     }
