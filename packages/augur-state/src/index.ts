049887cf532ef646ed56186c834ab322145a6326
adrake33 <aarondrake@gmail.com>
2019-02-13 12:53:58 -0800

    Remove deprecated export

--- packages/augur-state/src/index.ts
+++ packages/augur-state/src/index.ts
@@ -1,4 +1,3 @@
-export { SyncController } from "./sync/SyncController";
 export { API } from "./api/API";
 export { ServerController } from "./server/ServerController";
 export { Controller } from "./Controller";
