3723aefb5729e62e691fcbee6e8a8c670ac080aa
adrake33 <aarondrake@gmail.com>
2019-03-01 17:37:24 -0800

    Update workspace names

--- scripts/docker-build-all.sh
+++ scripts/docker-build-all.sh
@@ -4,13 +4,13 @@ echo "docker:build:augur - build base augur image"
 yarn docker:build:augur
 
 # build augur core
-echo "yarn workspace augur-core docker:build - building augur-core image"
-yarn workspace augur-core docker:build
+echo "yarn workspace @augurproject/core docker:build - building augur-core image"
+yarn workspace @augurproject/core docker:build
 
 # augur-core artifacts
-echo " yarn workspace augur-core artifacts - building artifacts"
-yarn workspace augur-core artifacts
+echo " yarn workspace @augurproject/core artifacts - building artifacts"
+yarn workspace @augurproject/core artifacts
 
 # augur-tools
-echo "building augur-tools image"
-yarn workspace augur-tools docker:build
+echo "building @augurproject/tools image"
+yarn workspace @augurproject/tools docker:build
