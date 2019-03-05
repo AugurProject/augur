af8c36f53c34beffa0004276f3de68b45ae3fb95
robert <robert@augur.net>
2019-03-05 15:27:54 -0800

    compile and deploy Augur contracts to ganache node running in the tests

--- packages/augur-state/src/run.ts
+++ packages/augur-state/src/run.ts
@@ -6,13 +6,14 @@ import { ethers } from "ethers";
 import { EthersJsonRpcProvider as EthersProvider } from "@augurproject/ethers-provider";
 import { ContractDependenciesEthers } from "contract-dependencies-ethers";
 import { PouchDBFactory } from "./db/AbstractDB";
+import { addresses } from "@augurproject/artifacts";
 
 const TEST_NETWORK_ID = 4;
 
 export async function start() {
   const provider = new EthersProvider(settings.ethNodeURLs[TEST_NETWORK_ID]);
   const contractDependencies = new ContractDependenciesEthers(provider, undefined, settings.testAccounts[0]);
-  const augur = await Augur.create(provider, contractDependencies);
+  const augur = await Augur.create(provider, contractDependencies, addresses[TEST_NETWORK_ID]);
   const pouchDBFactory = PouchDBFactory({});
   const controller = new Controller<ethers.utils.BigNumber>(augur, TEST_NETWORK_ID, settings.blockstreamDelay, uploadBlockNumbers[TEST_NETWORK_ID], [settings.testAccounts[0]], pouchDBFactory);
   controller.run();
