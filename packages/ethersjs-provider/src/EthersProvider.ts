aa08c5a46c234b842a5f928e005c35fc26efa515
adrake33 <aarondrake@gmail.com>
2019-03-13 11:51:47 -0700

    Use async.retry instead of async.queue + retryable

--- packages/ethersjs-provider/src/EthersProvider.ts
+++ packages/ethersjs-provider/src/EthersProvider.ts
@@ -5,96 +5,95 @@ import { EthersProvider as EProvider } from "contract-dependencies-ethers";
 import { ethers } from "ethers";
 import { Abi } from "ethereum";
 import * as _ from "lodash";
-import { AsyncQueue, queue, retryable } from "async";
+import { retry } from "async";
 
 interface ContractMapping {
-    [contractName: string]: ethers.utils.Interface;
-}
-
-interface PerformQueueTask {
-  message: any;
-  params: any;
-  resolve: (res: any) => void;
-  reject: (err?: Error | null) => void;
+  [contractName: string]: ethers.utils.Interface;
 }
 
 export class EthersProvider extends ethers.providers.BaseProvider implements EProvider {
-    private contractMapping: ContractMapping = {};
-    private performQueue: AsyncQueue<PerformQueueTask>;
-    readonly provider: ethers.providers.JsonRpcProvider;
+  private contractMapping: ContractMapping = {};
+  private opts: number | {
+    times: number,
+    interval: number | ((retryCount: number) => number)
+  };
 
-    constructor(provider: ethers.providers.JsonRpcProvider, times: number, interval: number, concurrency: number) {
-      super(provider.getNetwork());
-      this.provider = provider;
-      this.performQueue = queue(
-        retryable(
-          { times, interval},
-          (item: PerformQueueTask, callback: () => void): void => {
-            this.provider.perform(item.message, item.params).then((res) => {
-              item.resolve(res);
-              callback();
-            }).catch((err: Error) => {
-              item.reject(err);
-              callback();
-            });
+  constructor(readonly provider: ethers.providers.JsonRpcProvider, times: number, interval: number, readonly concurrency: number) {
+    super(provider.getNetwork());
+    this.opts = { times, interval };
+  }
+
+  public async listAccounts(): Promise<Array<string>> {
+    return this.provider.listAccounts();
+  }
+
+  public async call(transaction: Transaction<ethers.utils.BigNumber>): Promise<string> {
+    return await super.call(transaction);
+  }
+
+  public async getNetworkId(): Promise<NetworkId> {
+    return <NetworkId>(await this.getNetwork()).chainId.toString();
+  }
+
+  public async getBlockNumber(): Promise<number> {
+    return await super.getBlockNumber();
+  }
+
+  public storeAbiData(abi: Abi, contractName: string): void {
+    this.contractMapping[contractName] = new ethers.utils.Interface(abi);
+  }
+
+  public getEventTopic(contractName: string, eventName: string): string {
+    const contractInterface = this.contractMapping[contractName];
+    if (!contractInterface) {
+      throw new Error(`Contract name ${contractName} not found in EthersJSProvider. Call 'storeAbiData' first with this name and the contract abi`);
+    }
+    return contractInterface.events[eventName].topic;
+  }
+
+  public parseLogValues(contractName: string, log: Log): LogValues {
+    const contractInterface = this.contractMapping[contractName];
+    if (!contractInterface) {
+      throw new Error(`Contract name ${contractName} not found in EthersJSProvider. Call 'storeAbiData' first with this name and the contract abi`);
+    }
+    const parsedLog = contractInterface.parseLog(log);
+    let omittedValues = _.map(_.range(parsedLog.values.length), (n) => n.toString());
+    omittedValues.push('length');
+    let logValues = _.omit(parsedLog.values, omittedValues);
+    logValues = _.mapValues(logValues, (val) => {
+      if (val._hex) {
+        return val._hex;
+      }
+      return val;
+    });
+    return logValues;
+  }
+
+  public async getLogs(filter: Filter): Promise<Array<Log>> {
+    return super.getLogs(filter);
+  }
+
+  public async perform(message: any, params: any): Promise<any> {
+    const _this = this;
+    return new Promise((resolve, reject) => {
+      retry(
+        this.opts,
+        async function (callback) {
+          let results: any;
+          try {
+            results = await _this.provider.perform(message, params);
+          } catch (err) {
+            callback(err, []);
           }
-        ),
-        concurrency
+          callback(null, results);
+        },
+        function (err: Error, results: any) {
+          if (err || typeof results === "undefined") {
+            reject(err);
+          }
+          resolve(results);
+        }
       );
-    }
-
-    public async listAccounts(): Promise<Array<string>> {
-      return this.provider.listAccounts();
-    }
-    public async call(transaction: Transaction<ethers.utils.BigNumber>): Promise<string> {
-        return await super.call(transaction);
-    }
-
-    public async getNetworkId(): Promise<NetworkId> {
-        return <NetworkId>(await this.getNetwork()).chainId.toString();
-    }
-
-    public async getBlockNumber(): Promise<number> {
-        return await super.getBlockNumber();
-    }
-
-    public storeAbiData(abi: Abi, contractName: string): void {
-        this.contractMapping[contractName] = new ethers.utils.Interface(abi);
-    }
-
-    public getEventTopic(contractName: string, eventName: string): string {
-        const contractInterface = this.contractMapping[contractName];
-        if (!contractInterface) {
-            throw new Error(`Contract name ${contractName} not found in EthersJSProvider. Call 'storeAbiData' first with this name and the contract abi`);
-        }
-        return contractInterface.events[eventName].topic;
-    }
-
-    public parseLogValues(contractName: string, log: Log): LogValues {
-        const contractInterface = this.contractMapping[contractName];
-        if (!contractInterface) {
-            throw new Error(`Contract name ${contractName} not found in EthersJSProvider. Call 'storeAbiData' first with this name and the contract abi`);
-        }
-        const parsedLog = contractInterface.parseLog(log);
-        let omittedValues = _.map(_.range(parsedLog.values.length), (n) => n.toString());
-        omittedValues.push('length');
-        let logValues = _.omit(parsedLog.values, omittedValues);
-        logValues = _.mapValues(logValues, (val) => {
-            if (val._hex) {
-            return val._hex;
-            }
-            return val;
-        });
-        return logValues;
-    }
-
-    public async getLogs(filter: Filter): Promise<Array<Log>> {
-        return super.getLogs(filter);
-    }
-
-    public async perform(message: any, params: any): Promise<any> {
-      return new Promise((resolve, reject) => {
-        this.performQueue.push({ message, params, resolve, reject });
-      });
-    }
+    });
+  }
 }
