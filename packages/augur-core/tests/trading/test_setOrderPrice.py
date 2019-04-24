55fc255c4adf75caa9da5f7f6dfbcca14239f2f1
unknown <achap5dk@gmail.com>
2019-04-24 15:53:30 -0700

    year market dur. OI mult to 5x. numTicks to 100

--- packages/augur-core/tests/trading/test_setOrderPrice.py
+++ packages/augur-core/tests/trading/test_setOrderPrice.py
@@ -14,39 +14,39 @@ def test_orders_set_order_price_all_tokens(contractsFixture, market, cash):
     tradeGroupID = "42"
 
     # create order
-    with BuyWithCash(cash, fix('10', '5000'), tester.k0, "create order"):
-        orderId = createOrder.publicCreateOrder(BID, fix(10), 5000, market.address, YES, longTo32Bytes(0), longTo32Bytes(0), tradeGroupID, False, nullAddress)
+    with BuyWithCash(cash, fix('10', '50'), tester.k0, "create order"):
+        orderId = createOrder.publicCreateOrder(BID, fix(10), 50, market.address, YES, longTo32Bytes(0), longTo32Bytes(0), tradeGroupID, False, nullAddress)
 
     assert orders.getAmount(orderId) == fix('10')
-    assert orders.getPrice(orderId) == 5000
+    assert orders.getPrice(orderId) == 50
     assert orders.getOrderCreator(orderId) == bytesToHexString(tester.a0)
-    assert orders.getOrderMoneyEscrowed(orderId) == fix('10', '5000')
+    assert orders.getOrderMoneyEscrowed(orderId) == fix('10', '50')
     assert orders.getOrderSharesEscrowed(orderId) == 0
 
-    # Change the price to 6000
+    # Change the price to 60
     # We have to provide the extra tokens required by the increase in our BID
     with raises(TransactionFailed):
-        orders.setOrderPrice(orderId, 6000, nullOrder, nullOrder)
+        orders.setOrderPrice(orderId, 60, nullOrder, nullOrder)
 
-    with BuyWithCash(cash, fix('10', '1000'), tester.k0, "set order price higher"):
-        assert orders.setOrderPrice(orderId, 6000, nullOrder, nullOrder)
+    with BuyWithCash(cash, fix('10', '10'), tester.k0, "set order price higher"):
+        assert orders.setOrderPrice(orderId, 60, nullOrder, nullOrder)
 
     # See that the order price and money escrowed has changed
     assert orders.getAmount(orderId) == fix('10')
-    assert orders.getPrice(orderId) == 6000
+    assert orders.getPrice(orderId) == 60
     assert orders.getOrderCreator(orderId) == bytesToHexString(tester.a0)
-    assert orders.getOrderMoneyEscrowed(orderId) == fix('10', '6000')
+    assert orders.getOrderMoneyEscrowed(orderId) == fix('10', '60')
     assert orders.getOrderSharesEscrowed(orderId) == 0
 
-    # Now if we set the price lower again to 5000 we'll receive a refund for the difference
-    with TokenDelta(cash, fix(10, 1000), tester.a0, "Did not recieve a refund for lowering order price"):
-        assert orders.setOrderPrice(orderId, 5000, nullOrder, nullOrder)
+    # Now if we set the price lower again to 50 we'll receive a refund for the difference
+    with TokenDelta(cash, fix(10, 10), tester.a0, "Did not recieve a refund for lowering order price"):
+        assert orders.setOrderPrice(orderId, 50, nullOrder, nullOrder)
 
     # See that the order price and money escrowed has changed
     assert orders.getAmount(orderId) == fix('10')
-    assert orders.getPrice(orderId) == 5000
+    assert orders.getPrice(orderId) == 50
     assert orders.getOrderCreator(orderId) == bytesToHexString(tester.a0)
-    assert orders.getOrderMoneyEscrowed(orderId) == fix('10', '5000')
+    assert orders.getOrderMoneyEscrowed(orderId) == fix('10', '50')
     assert orders.getOrderSharesEscrowed(orderId) == 0
 
 def test_orders_set_order_price_all_shares(contractsFixture, market, cash):
@@ -57,24 +57,24 @@ def test_orders_set_order_price_all_shares(contractsFixture, market, cash):
     tradeGroupID = "42"
 
     # create order using only shares
-    with BuyWithCash(cash, fix('10', '10000'), tester.k0, "buy complete set"):
+    with BuyWithCash(cash, fix('10', '100'), tester.k0, "buy complete set"):
         assert completeSets.publicBuyCompleteSets(market.address, fix(10))
 
-    orderId = createOrder.publicCreateOrder(ASK, fix(10), 5000, market.address, YES, longTo32Bytes(0), longTo32Bytes(0), tradeGroupID, False, nullAddress)
+    orderId = createOrder.publicCreateOrder(ASK, fix(10), 50, market.address, YES, longTo32Bytes(0), longTo32Bytes(0), tradeGroupID, False, nullAddress)
 
     assert orders.getAmount(orderId) == fix('10')
-    assert orders.getPrice(orderId) == 5000
+    assert orders.getPrice(orderId) == 50
     assert orders.getOrderCreator(orderId) == bytesToHexString(tester.a0)
     assert orders.getOrderMoneyEscrowed(orderId) == 0
     assert orders.getOrderSharesEscrowed(orderId) == fix(10)
 
-    # Change the price to 6000
+    # Change the price to 60
     # We don't need to provide any additional tokens since we're covering this order entirely with shares
-    assert orders.setOrderPrice(orderId, 6000, nullOrder, nullOrder)
+    assert orders.setOrderPrice(orderId, 60, nullOrder, nullOrder)
 
     # See that the order price has changed and the money escrowed has not changed
     assert orders.getAmount(orderId) == fix('10')
-    assert orders.getPrice(orderId) == 6000
+    assert orders.getPrice(orderId) == 60
     assert orders.getOrderCreator(orderId) == bytesToHexString(tester.a0)
     assert orders.getOrderMoneyEscrowed(orderId) == 0
     assert orders.getOrderSharesEscrowed(orderId) == fix(10)
@@ -87,29 +87,29 @@ def test_orders_set_order_price_partial_shares(contractsFixture, market, cash):
     tradeGroupID = "42"
 
     # create order using partial shares along with tokens escrowed
-    with BuyWithCash(cash, fix('5', '10000'), tester.k0, "buy complete set"):
+    with BuyWithCash(cash, fix('5', '100'), tester.k0, "buy complete set"):
         assert completeSets.publicBuyCompleteSets(market.address, fix(5))
 
-    with BuyWithCash(cash, fix('5', '5000'), tester.k0, "create order"):
-        orderId = createOrder.publicCreateOrder(ASK, fix(10), 5000, market.address, YES, longTo32Bytes(0), longTo32Bytes(0), tradeGroupID, False, nullAddress)
+    with BuyWithCash(cash, fix('5', '50'), tester.k0, "create order"):
+        orderId = createOrder.publicCreateOrder(ASK, fix(10), 50, market.address, YES, longTo32Bytes(0), longTo32Bytes(0), tradeGroupID, False, nullAddress)
 
     assert orders.getAmount(orderId) == fix('10')
-    assert orders.getPrice(orderId) == 5000
+    assert orders.getPrice(orderId) == 50
     assert orders.getOrderCreator(orderId) == bytesToHexString(tester.a0)
-    assert orders.getOrderMoneyEscrowed(orderId) == fix('5', '5000')
+    assert orders.getOrderMoneyEscrowed(orderId) == fix('5', '50')
     assert orders.getOrderSharesEscrowed(orderId) == fix(5)
 
-    # Change the price to 4000
+    # Change the price to 40
     # We have to provide the extra tokens required by the decrease in our ASK
     with raises(TransactionFailed):
-        orders.setOrderPrice(orderId, 4000, nullOrder, nullOrder)
+        orders.setOrderPrice(orderId, 40, nullOrder, nullOrder)
 
-    with BuyWithCash(cash, fix('5', '1000'), tester.k0, "set order price higher"):
-        assert orders.setOrderPrice(orderId, 4000, nullOrder, nullOrder)
+    with BuyWithCash(cash, fix('5', '10'), tester.k0, "set order price higher"):
+        assert orders.setOrderPrice(orderId, 40, nullOrder, nullOrder)
 
     # See that the order price and money escrowed has changed
     assert orders.getAmount(orderId) == fix('10')
-    assert orders.getPrice(orderId) == 4000
+    assert orders.getPrice(orderId) == 40
     assert orders.getOrderCreator(orderId) == bytesToHexString(tester.a0)
-    assert orders.getOrderMoneyEscrowed(orderId) == fix('5', '6000')
+    assert orders.getOrderMoneyEscrowed(orderId) == fix('5', '60')
     assert orders.getOrderSharesEscrowed(orderId) == fix(5)
