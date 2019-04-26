4693973ccd0df093b22d4ffd1258875355199202
Tom Haile <bradford.haile@gmail.com>
2019-04-26 18:31:05 -0500

    linting

--- src/modules/orders/selectors/filled-orders.js
+++ src/modules/orders/selectors/filled-orders.js
@@ -64,7 +64,10 @@ function findOrders(
       const priceBN = createBigNumber(price);
       let typeOp = type;
 
-      const outcomeName = getOutcomeName(marketsData, (outcomesData || {})[outcome]);
+      const outcomeName = getOutcomeName(
+        marketsData,
+        (outcomesData || {})[outcome]
+      );
 
       let originalQuantity = amountBN;
       if (accountId === creator && !foundOrder) {
