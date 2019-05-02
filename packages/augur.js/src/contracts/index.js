525503947a6633abc5551387076a1a2898ae1347
Justin Barry <328965+justinbarry@users.noreply.github.com>
2019-05-02 15:16:18 -0700

    Fix export reference capitalization

--- packages/augur.js/src/contracts/index.js
+++ packages/augur.js/src/contracts/index.js
@@ -3,23 +3,12 @@
 var generateAbiMap = require("./generate-abi-map");
 var readJsonFile = require("../utils/read-json-file");
 
-var e = module.exports = {
-  abi: generateAbiMap(require("@augurproject/artifacts").abi),
-  addresses: require("@augurproject/artifacts").addresses,
-  uploadBlockNumbers: require("@augurproject/artifacts").uploadBlockNumbers
+module.exports = {
+   abi: generateAbiMap(require("@augurproject/artifacts").abi),
+   addresses: require("@augurproject/artifacts").Addresses,
+   uploadBlockNumbers: require("@augurproject/artifacts").UploadBlockNumbers
 };
 
 module.exports.reloadAddresses = function (callback) {
-  readJsonFile(require.resolve("@augurproject/artifacts/build/addresses.json"), function (err, data) {
-    if (err) return callback(err);
-
-    e.addresses = data;
-
-    readJsonFile(require.resolve("@augurproject/artifacts/build/upload-block-numbers.json"), function (err, data) {
-      if (err) return callback(err);
-      e.uploadBlockNumbers = data;
-
-      callback(null, e);
-    });
-  });
+  callback(null, e);
 };
