90802441f11492eab2678ea212bd2a347b8dd82f
Tom Haile <bradford.haile@gmail.com>
2019-05-03 17:30:46 -0500

    remove copy files from this run.sh

--- packages/augur-node/scripts/docker/run.sh
+++ packages/augur-node/scripts/docker/run.sh
@@ -2,6 +2,5 @@
 
 IMAGE=$1
 TAG=$(node ./scripts/get-contract-hashes.js)
-../scripts/copy-docker-files.sh $IMAGE:$TAG
 docker run -it -e ETHEREUM_WS=${ETHEREUM_WS} --rm -p 9001:9001 -p 9002:9002 --name ${CONTAINER_NAME:-augur-node} $IMAGE:$TAG
 
