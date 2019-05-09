b8d21230f561267ff2d6e897e3a34cf9f86f2c31
Tom Haile <bradford.haile@gmail.com>
2019-05-09 12:51:15 -0500

    added remove from when sending transaction to ethers

--- packages/augur-ui/src/modules/account/actions/get-dai.js
+++ packages/augur-ui/src/modules/account/actions/get-dai.js
@@ -1,52 +1,29 @@
-import { augur } from "services/augurjs";
 import { updateAlert } from "modules/alerts/actions/alerts";
-import { updateAssets } from "modules/auth/actions/update-assets";
 import { selectCurrentTimestampInSeconds } from "src/select-state";
 import logError from "utils/log-error";
-import noop from "utils/noop";
 import { augurApi } from "services/augurapi";
 import { BigNumber } from "ethers/utils";
 
 export default function(callback = logError) {
   return async (dispatch, getState) => {
     const { contracts } = augurApi.get();
-    contracts.cash.faucet(new BigNumber(100000)).then(
+    contracts.cash.faucet(new BigNumber("100000000000000000000000")).then(
       success => {
-        console.log("success", success);
+        // TODO: SDK will management pending tx queue
+        console.log("get-dai was success");
+        callback(null);
       },
       err => {
         console.log("failure", err);
-      }
-    );
-
-    /*
-      {
-      tx: { to: augur.contracts.addresses[augur.rpc.getNetworkID()].Cash },
-      _amount: 1000000000000000000000,
-      meta: loginAccount.meta,
-      onSent: noop,
-      onSuccess: res => {
         dispatch(
-          updateAlert(res.hash, {
-            id: res.hash,
-            status: "Confirmed",
-            timestamp: selectCurrentTimestampInSeconds(getState())
-          })
-        );
-        dispatch(updateAssets());
-        callback(null);
-      },
-      onFailed: res => {
-        dispatch(
-          updateAlert(res.hash, {
-            id: res.hash,
+          updateAlert("blah-blah", {
+            id: "blah-blah",
             status: "Failed",
             timestamp: selectCurrentTimestampInSeconds(getState())
           })
         );
-        logError(res);
+        logError("blah-blah");
       }
-    });
-    */
+    );
   };
 }
