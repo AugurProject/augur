33e7b4ffb3525321e61e468919ba9e5aad0c92fe
Brian Woods <me@brian.biz>
2019-05-09 12:05:33 -0700

    Closes #2086 - checkpoint

---
+++ packages/augur-sdk/src/connector/connector.ts
@@ -0,0 +1,22 @@
+import { ContractEvents } from "@augurproject/types/ethereum/events";
+
+export type Callback = (data: any) => Promise<unknown>;
+
+export abstract class Connector {
+  private callbacks: { [key in ContractEvents]?: Callback } = {};
+
+  public abstract async connect(params: any): Promise<any>;
+  public abstract async disconnect(): Promise<any>;
+  public abstract async send(data: any): Promise<any>;
+  public abstract async subscribe(event: string, callback: Callback): Promise<any>;
+  public abstract async unsubscribe(event: string): Promise<any>;
+
+  // is ts-io being used with this connector?
+  public decode?(): boolean {
+    return false;
+  }
+
+  public async unsubscribeAll(): Promise<Array<Promise<void>>> {
+    return Object.keys(this.callbacks).map((event) => this.unsubscribe(event));
+  }
+}
