1c6b1200ef36f4cdc5c7473622de912ec115c6f0
Tom Haile <bradford.haile@gmail.com>
2019-05-13 16:25:42 -0500

    fix mis merge, controller no longer exists.

--- packages/augur-ui/src/modules/app/actions/sync-blockchain.ts
+++ packages/augur-ui/src/modules/app/actions/sync-blockchain.ts
@@ -3,51 +3,47 @@ import { updateBlockchain } from "modules/app/actions/update-blockchain";
 import { updateAssets } from "modules/auth/actions/update-assets";
 import { createBigNumber } from "utils/create-big-number";
 import { loadGasPriceInfo } from "modules/app/actions/load-gas-price-info";
+import { getNetworkId, getTimestamp, getCurrentBlock } from "modules/contracts/actions/contractCalls";
 
 const GET_GAS_BLOCK_LIMIT = 100;
 const MAINNET_ID = "1";
 
 export const syncBlockchain = (cb: Function) => (
   dispatch: Function,
-  getState: Function
+  getState: Function,
 ) => {
-  const networkId = augur.rpc.getNetworkID();
+  const networkId = getNetworkId();
   const { gasPriceInfo } = getState();
-  const blockNumber = parseInt(augur.rpc.getCurrentBlock().number, 16);
-  augur.api.Controller.getTimestamp((err: any, augurTimestamp: any) => {
-    if (err) console.error(err);
-    dispatch(
+  const currentBlockNumber = getCurrentBlock();
+  const currentAugurTimestamp = getTimestamp();
+
+  dispatch(
       updateBlockchain({
-        currentBlockNumber: blockNumber,
-        currentBlockTimestamp: parseInt(
-          augur.rpc.getCurrentBlock().timestamp,
-          16
-        ),
-        currentAugurTimestamp: parseInt(augurTimestamp, 10)
-      })
-    );
-    const BNblockNumber = createBigNumber(blockNumber);
-    const BNGasBlockNumberLimit = createBigNumber(
-      gasPriceInfo.blockNumber || "0"
+        currentBlockNumber,
+        currentAugurTimestamp,
+      }));
+
+  const BNblockNumber = createBigNumber(currentBlockNumber);
+  const BNGasBlockNumberLimit = createBigNumber(
+      gasPriceInfo.blockNumber || "0",
     ).plus(GET_GAS_BLOCK_LIMIT);
 
-    if (
+  if (
       (!gasPriceInfo.blockNumber || BNblockNumber.gte(BNGasBlockNumberLimit)) &&
       networkId === MAINNET_ID
     ) {
       dispatch(loadGasPriceInfo());
     }
 
-    cb && cb();
-  });
+  cb && cb();
 
   augur.augurNode.getSyncData((err: any, res: any) => {
     if (!err && res) {
       dispatch(
         updateBlockchain({
           highestBlock: res.highestBlock.number,
-          lastProcessedBlock: res.lastProcessedBlock.number
-        })
+          lastProcessedBlock: res.lastProcessedBlock.number,
+        }),
       );
     }
   });
