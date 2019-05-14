b91a29e051434df0504b7edd7d6536361a162013
Justin Barry <328965+justinbarry@users.noreply.github.com>
2019-05-14 14:21:19 -0700

    Add account types to constants file

--- packages/augur-sdk/src/constants.ts
+++ packages/augur-sdk/src/constants.ts
@@ -1,3 +1,13 @@
 import {BigNumber} from "ethers/utils";
 
-export const ETHER = new BigNumber(10).pow(18);
+export enum ACCOUNT_TYPES {
+U_PORT= "uPort",
+LEDGER = "ledger",
+PRIVATE_KEY = "privateKey",
+UNLOCKED_ETHEREUM_NODE = "unlockedEthereumNode",
+META_MASK = "metaMask",
+TREZOR = "trezor",
+EDGE = "edge",
+};
+
+export const ETHER = new BigNumber(10).pow(18)
