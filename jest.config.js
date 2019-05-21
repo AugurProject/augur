b35aa39d87377930cc83da63be905ab18edd8c6a
robert <robert@augur.net>
2019-05-21 16:28:55 -0700

    run tests at toplevel. ignore UI tho

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   projects: [
     "<rootDir>/packages/augur-sdk",
     "<rootDir>/packages/augur-ui",
+    "<rootDir>/packages/augur-test",
   ],
   reporters: ["default", "jest-junit"],
 };
