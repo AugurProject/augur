dc8bfc723492af42d26dac00ca37650cc03dd732
JohnDanz <John.Danz87@gmail.com>
2019-05-22 12:41:55 -0500

    refactored terms and conditions

--- packages/augur-ui/src/modules/app/containers/terms-and-conditions.js
+++
@@ -1,8 +0,0 @@
-import { connect } from "react-redux";
-import TermsAndConditions from "modules/app/components/terms-and-conditions/terms-and-conditions";
-
-const mapStateToProps = state => ({
-  versions: state.versions
-});
-
-export default connect(mapStateToProps)(TermsAndConditions);
