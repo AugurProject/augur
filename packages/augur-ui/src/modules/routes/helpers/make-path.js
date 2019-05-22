f7852d3e008425827f88ea620b9cb8493568efe1
Tom Haile <bradford.haile@gmail.com>
2019-05-22 11:32:07 -0500

    error in converting String to sting when object was needed

--- packages/augur-ui/src/modules/routes/helpers/make-path.js
+++ packages/augur-ui/src/modules/routes/helpers/make-path.js
@@ -1,10 +1,10 @@
 // TODO -- clean up doc here
-// (string|Array) string to individual path OR array of deep paths for linking OR array of individual paths to match when routing
+// (String|Array) string to individual path OR array of deep paths for linking OR array of individual paths to match when routing
 // (Bool) whether return value of paths array should be formatted as string path or matching regex
 export default function makePath(paths, match) {
   // Invalid, return root
   if (
-    (paths.constructor !== string && paths.constructor !== Array) ||
+    (paths.constructor !== String && paths.constructor !== Array) ||
     (match && paths.constructor !== Array)
   )
     return "/";
@@ -12,9 +12,9 @@ export default function makePath(paths, match) {
   // Matching Regex for Route Component
   if (match) return `^/(${paths.join("|")})/`;
 
-  // string Path for Link from Array
+  // String Path for Link from Array
   if (paths.constructor === Array) return `/${paths.join("/")}/`;
 
-  // string Path for Link from string
+  // String Path for Link from String
   return `/${paths}`;
 }
