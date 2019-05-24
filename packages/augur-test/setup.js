0059922df1f7b0c6c85350acc275485fe8d82464
robert <robert@augur.net>
2019-05-24 12:06:03 -0700

    setup.js runs dp correctly from augur-test

--- packages/augur-test/setup.js
+++ packages/augur-test/setup.js
@@ -2,5 +2,5 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
 module.exports = () => {
-  return exec(`/usr/bin/env yarn dp create-seed-file`);
+  return exec(`/usr/bin/env yarn workspace @augurproject/tools dp create-seed-file`);
 };
