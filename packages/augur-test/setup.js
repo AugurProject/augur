dc7572bada87e4b35db18ce4319880946ffc369e
robert <robert@augur.net>
2019-05-23 17:20:30 -0700

    ganache pre-seeded

--- packages/augur-test/setup.js
+++ packages/augur-test/setup.js
@@ -2,5 +2,5 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
 module.exports = () => {
-  return exec(`/usr/bin/env ts-node ${__dirname}/build/scripts/build-ganache.js`);
+  return exec(`/usr/bin/env yarn dp create-seed-file`);
 };
--- packages/augur-tools/src/dp/index.ts
+++ packages/augur-tools/src/dp/index.ts
@@ -23,6 +23,7 @@ import { ContractDependenciesEthers } from "contract-dependencies-ethers";
 import { repFaucet } from "./lib/rep-faucet";
 import { UploadBlockNumbers } from "@augurproject/artifacts";
 import { createCannedMarketsAndOrders} from "@augurproject/test";
+import { createSeedFile } from "@augurproject/test/build/scripts/GenerateGanacheSeed";
 
 const COMMANDS = [
   "create-markets",
@@ -32,7 +33,8 @@ const COMMANDS = [
   "gas-limit",
   "rep-faucet",
   "upload",
-  "all-logs"
+  "all-logs",
+  "create-seed-file",
 ] as const;
 
 type COMMANDS = typeof COMMANDS[number];
@@ -207,13 +209,17 @@ async function runCommandForNetwork(networkConfiguration: NetworkConfiguration, 
           secretKey: networkConfiguration.privateKey,
           publicKey: computeAddress(`0x${networkConfiguration.privateKey!}`),
           balance: 0
-        }]
-
+        }];
 
         console.log(JSON.stringify(accounts, null, 2));
 
         await createCannedMarketsAndOrders(accounts, ethersProvider, addresses);
       }
+      break;
+    }
+    case "create-seed-file": {
+      await createSeedFile("../augur-test/seed.json");
+      break;
     }
     case "gas-limit": {
       const provider = new providers.JsonRpcProvider(networkConfiguration.http);
