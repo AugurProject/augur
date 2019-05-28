17d56064a63d3017deaa164bfe93e4fe39c915d0
Brian Woods <brianosaurus@users.noreply.github.com>
2019-05-28 11:05:20 -0700

    2213 webworker connector (#2253)
    * Closes #2213 - web worker and API working
    
    * minor cleanup
    
    * removing test

--- packages/augur-sdk/src/state/index.ts
+++ packages/augur-sdk/src/state/index.ts
@@ -1,14 +1,3 @@
-import RunWorker from "./Sync.worker";
+import { WebWorkerConnector } from "../connector/ww-connector";
+import { Markets } from "./api/Markets";
 
-console.log("Starting web worker");
-
-// assumption is this will fail if the browser doesn't support web workers
-try {
-  const worker = new RunWorker();
-
-  worker.onmessage = (event: MessageEvent) => {
-    console.log(event.data);
-  };
-} catch (error) {
-  console.log("Your browser does not support web workers");
-}
