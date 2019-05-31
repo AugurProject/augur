00a9e6830755fdddcacff6fb1f23206954fc2ff9
Brian Woods <brianosaurus@users.noreply.github.com>
2019-05-31 08:59:47 -0700

    Events for connectors (#2291)
    * Closes #2183 - events for connectors
    
    * Finishes events and connectors

--- packages/augur-sdk/src/connector/http-connector.ts
+++ packages/augur-sdk/src/connector/http-connector.ts
@@ -1,4 +1,5 @@
 import { Connector, Callback } from "./connector";
+import { SubscriptionEventNames } from "../constants";
 import fetch from "cross-fetch";
 
 export class HTTPConnector extends Connector {
@@ -18,18 +19,13 @@ export class HTTPConnector extends Connector {
   public bindTo<R, P>(f: (db: any, augur: any, params: P) => R) {
     return async (params: P): Promise<R> => {
       return <R>(await (await fetch(this.endpoint, {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify({ id: 42, method: f.name, params, jsonrpc: "2.0" }),
-        headers: { 'Content-Type': 'application/json' },
+        headers: { "Content-Type": "application/json" },
       })).json());
-    }
+    };
   }
 
-  public async subscribe(event: string, callback: Callback): Promise<any> {
-
-  }
-
-  public async unsubscribe(event: string): Promise<any> {
-
-  }
+  public on(eventName: SubscriptionEventNames | string, callback: Callback): void { }
+  public off(eventName: SubscriptionEventNames | string): void { }
 }
--- packages/augur-sdk/src/state/index.ts
+++ packages/augur-sdk/src/state/index.ts
@@ -1,3 +1,37 @@
-import { SEOConnector } from "../connector/seo-connector";
+import { Addresses } from "@augurproject/artifacts";
+import { Augur } from "../Augur"
+import { ContractDependenciesEthers } from "contract-dependencies-ethers";
+import { EthersProvider } from "@augurproject/ethersjs-provider";
+import { JsonRpcProvider } from "ethers/providers";
 import { Markets } from "./api/Markets";
+import { SubscriptionEventNames } from "../constants";
+import { WebsocketConnector } from "../connector/ws-connector";
 
+const settings = require("@augurproject/sdk/src/state/settings.json");
+
+console.log("Starting web worker");
+
+(async function() {
+  try {
+    const ethersProvider = new EthersProvider(new JsonRpcProvider(settings.ethNodeURLs[4]), 10, 0, 40);
+    const contractDependencies = new ContractDependenciesEthers(ethersProvider, undefined, settings.testAccounts[0]);
+    const augur = await Augur.create(ethersProvider, contractDependencies, Addresses[4], new WebsocketConnector("ws://localhost:9001"));
+    await augur.connect();
+
+    augur.on(SubscriptionEventNames.CompleteSetsPurchased, (data: any): void => {
+      console.log(data);
+      augur.off(SubscriptionEventNames.CompleteSetsPurchased);
+    });
+
+    setTimeout(async () => {
+      console.log("getMarkets");
+      const getMarkets = augur.bindTo(Markets.getMarkets);
+      console.log(await getMarkets({
+        universe: "0x02149d40d255fceac54a3ee3899807b0539bad60",
+      }));
+    }, 1000);
+    console.log("Done");
+  } catch (e) {
+    console.log(e);
+  }
+})();
