2279996351c47a55886ca393438c75a889dd48cb
Justin Barry <328965+justinbarry@users.noreply.github.com>
2019-06-03 00:14:34 -0700

    Export connectors from top level of SDK

---
+++ packages/augur-sdk/src/connector/index.ts
@@ -0,0 +1,6 @@
+export * from "./empty-connector";
+export * from "./http-connector";
+export * from "./seo-connector";
+export * from "./ws-connector";
+export * from "./ww-connector";
+
--- packages/augur-sdk/src/index.ts
+++ packages/augur-sdk/src/index.ts
@@ -4,3 +4,5 @@ export * from "@augurproject/types";
 export { Provider } from "./ethereum/Provider";
 export * from "./utils";
 export * from "./constants";
+
+export * from "./connector";
