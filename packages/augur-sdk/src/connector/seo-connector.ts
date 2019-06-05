97117103b4ec457a589f36229a4e764621b73a70
Brian Woods <me@brian.biz>
2019-06-05 14:45:00 -0700

    removing console.log

--- packages/augur-sdk/src/connector/seo-connector.ts
+++ packages/augur-sdk/src/connector/seo-connector.ts
@@ -46,7 +46,6 @@ export class SEOConnector extends Connector {
   }
 
   public on(eventName: SubscriptionEventNames | string, callback: Callback): void {
-    console.log("On called with eventName: " + eventName);
     const subscription: string = this.events.subscribe(eventName, callback);
     this.subscriptions[eventName] = { id: subscription, callback };
   }
