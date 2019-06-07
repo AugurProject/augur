ddd1a6a674660cbe0c9c6c51b2c33354459bcadb
Justin Barry <328965+justinbarry@users.noreply.github.com>
2019-06-06 17:02:38 -0700

    Separate out db creation from controller
    We need a "readonly"  version of the DBs in the webworker connector

--- packages/augur-sdk/src/state/Controller.ts
+++ packages/augur-sdk/src/state/Controller.ts
@@ -1,20 +1,14 @@
 import {Augur} from "../Augur";
-import {PouchDBFactoryType} from "./db/AbstractDB";
 import {DB} from "./db/DB";
 import {BlockAndLogStreamerListener} from "./db/BlockAndLogStreamerListener";
 
 const settings = require("./settings.json");
 
 export class Controller {
-  public db: Promise<DB>;
 
   public constructor(
     private augur: Augur,
-    private networkId: number,
-    private blockstreamDelay: number,
-    private defaultStartSyncBlockNumber: number,
-    private trackedUsers: Array<string>,
-    private pouchDBFactory: PouchDBFactoryType,
+    private db: Promise<DB>,
     private blockAndLogStreamerListener: BlockAndLogStreamerListener,
   ) {
   }
@@ -24,33 +18,8 @@ export class Controller {
     return db.fullTextSearch(eventName, query);
   }
 
-  public createDb() {
-    this.db = DB.createAndInitializeDB(
-      this.networkId,
-      this.blockstreamDelay,
-      this.defaultStartSyncBlockNumber,
-      this.trackedUsers,
-      this.augur.genericEventNames,
-      this.augur.customEvents,
-      this.augur.userSpecificEvents,
-      this.pouchDBFactory,
-      this.blockAndLogStreamerListener,
-    );
-  }
-
   public async run(): Promise<void> {
     try {
-      this.db =  DB.createAndInitializeDB(
-        this.networkId,
-        this.blockstreamDelay,
-        this.defaultStartSyncBlockNumber,
-        this.trackedUsers,
-        this.augur.genericEventNames,
-        this.augur.customEvents,
-        this.augur.userSpecificEvents,
-        this.pouchDBFactory,
-        this.blockAndLogStreamerListener,
-      );
       const db = await this.db;
       db.sync(
         this.augur,
