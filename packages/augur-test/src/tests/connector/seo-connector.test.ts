ab8d36b957a2324c82c5592af8bb6f2f587681f0
Brian Woods <me@brian.biz>
2019-06-11 09:02:15 -0700

    checkpoint

--- packages/augur-test/src/tests/connector/seo-connector.test.ts
+++ packages/augur-test/src/tests/connector/seo-connector.test.ts
@@ -14,41 +14,34 @@ let provider: EthersProvider;
 let john: ContractAPI;
 let addresses: any;
 
-describe("seo-connector", () => {
-  beforeAll(async () => {
-    jest.setTimeout(10000);
-    connector = new SEOConnector();
+beforeAll(async () => {
+  connector = new SEOConnector();
 
-    const contractData = await deployContracts(ACCOUNTS, compilerOutput);
-    provider = contractData.provider;
-    addresses = contractData.addresses;
+  const contractData = await deployContracts(ACCOUNTS, compilerOutput);
+  provider = contractData.provider;
+  addresses = contractData.addresses;
 
-    john = await ContractAPI.userWrapper(ACCOUNTS, 0, provider, addresses);
-    await john.approveCentralAuthority();
+  john = await ContractAPI.userWrapper(ACCOUNTS, 0, provider, addresses);
+  await john.approveCentralAuthority();
+}, 120000);
+
+test("Should route the message correctly and return a response", async (done) => {
+  console.log(john.augur.contracts.universe);
+  console.log(john.augur);
+
+  await connector.connect({ provider });
+
+  console.log(john.augur.contracts.universe);
+  console.log(john.augur);
+
+  connector.on(SubscriptionEventNames.NewBlock, async (...args: Array<any>): Promise<void> => {
+    const getMarkets = connector.bindTo(Markets.getMarkets);
+    // await getMarkets({
+    //   universe: john.augur.contracts.universe;
+    // });
+
+    connector.off(SubscriptionEventNames.NewBlock);
+    await connector.disconnect();
+    done();
   });
-
-  afterEach(() => {
-    jest.setTimeout(5000);
-  });
-
-  it("Should route the message correctly and return a response", async (done) => {
-    console.log(john.augur.contracts.universe);
-    console.log(john.augur);
-
-    await connector.connect({ provider });
-
-    console.log(john.augur.contracts.universe);
-    console.log(john.augur);
-
-    connector.on(SubscriptionEventNames.NewBlock, async (...args: Array<any>): Promise<void> => {
-      const getMarkets = connector.bindTo(Markets.getMarkets);
-      // await getMarkets({
-      //   universe: john.augur.contracts.universe;
-      // });
-
-      connector.off(SubscriptionEventNames.NewBlock);
-      await connector.disconnect();
-      done();
-    });
-  });
-});
+}, 15000);
