e9277130147f003049281553ff0dc8cf752f1f99
Andrew D'Amelio <dev@andrewdamel.io>
2019-06-12 10:08:16 -0400

    Convert existing MM/web3 login flow to use SDK

---
+++ packages/augur-ui/src/modules/auth/actions/update-sdk.ts
@@ -0,0 +1,30 @@
+import logError from "utils/log-error";
+import { LoginAccount } from "modules/types";
+import { augurSdk } from "services/augursdk";
+import { JsonRpcProvider } from "ethers/providers";
+
+export function updateSdk(loginAccount: LoginAccount, injectedProvider: JsonRpcProvider) {
+  return async () => {
+    const { address, meta }  = loginAccount;
+    if (!meta || !address) return;
+
+    if (!augurSdk.sdk) return;
+
+    let { provider } = augurSdk.sdk.dependencies;
+
+    if (injectedProvider) {
+      provider = injectedProvider;
+    }
+
+    try {
+      await augurSdk.makeApi(
+        provider,
+        address,
+        meta.signer,
+        meta.isWeb3,
+      );
+    } catch (error) {
+      logError(error);
+    }
+  };
+}
--- packages/augur-ui/src/services/augurjs.ts
+++ packages/augur-ui/src/services/augurjs.ts
@@ -2,38 +2,62 @@ import Augur from "@augurproject/augur.js";
 import logError from "utils/log-error";
 import { augurSdk } from "services/augursdk";
 import { JsonRpcProvider, Web3Provider } from "ethers/providers";
-import { LoginAccount, NodeStyleCallback, WindowApp, Connection } from "modules/types";
+import { LoginAccount, NodeStyleCallback, Connection, EnvObject } from "modules/types";
+import { windowRef } from "utils/window-ref";
 
-export const connect = async (env: any, loginAccount: LoginAccount, callback: NodeStyleCallback = logError) => {
+// Access a User’s MetaMask/Dapper Account
+async function initInjectedWeb3() {
+  if (typeof window.ethereum === "undefined") {
+    // Handle case where user hasn't installed MetaMask/Dapper.
+    return false;
+  }
+  try {
+    // If a user is logged in to MetaMask/Dapper and has previously approved the dapp,
+    // `ethereum.enable` will return the result of `eth_accounts`.
+    const accounts = await window.ethereum.enable();
+    return accounts;
+  } catch (error) {
+    // Handle error. If the user rejects the request for access, then
+    // `ethereum.enable` will throw an error.
+    return false;
+  }
+}
+
+export const connect = async (env: EnvObject, loginAccount: LoginAccount, callback: NodeStyleCallback = logError) => {
   const connectOptions = {
     augurNode: env["augur-node"],
     ethereumNode: env["ethereum-node"],
     useWeb3Transport: env.useWeb3Transport,
   };
 
-  let provider;
+  let provider = new JsonRpcProvider(env["ethereum-node"].http);;
   let isWeb3 = false;
-  let account;
-  const win: WindowApp = window as WindowApp;
-  if (win.web3 && win.web3.currentProvider) {
-    provider = new Web3Provider(win.web3.currentProvider);
-    account = win.web3.currentProvider.selectedAddress
+  let account = "";
+
+  const bootstrap = async (provider, account, isWeb3) => {
+    await augurSdk.makeApi(provider, account, provider.getSigner(), isWeb3);
+
+    augur.connect(
+      connectOptions,
+      (err: any, connectionInfo: Connection) => {
+        if (err) return callback(err);
+        console.log("connected:", connectionInfo);
+        callback(null, connectionInfo);
+      },
+    );
+  };
+
+  const injectedAccount = await initInjectedWeb3();
+  const loggedInAccount = windowRef.localStorage.getItem("loggedInAccount");
+
+  // Use injected provider if returning User has a unlocked injected account that matches the current logged in account
+  if (injectedAccount && (loggedInAccount === injectedAccount[0])) {
+    provider = new Web3Provider(windowRef.web3.currentProvider);
+    account = windowRef.web3.currentProvider.selectedAddress,
     isWeb3 = true;
-  } else {
-    provider = new JsonRpcProvider(env["ethereum-node"].http);
-    account = loginAccount.address;
   }
 
-  await augurSdk.makeApi(provider, account, provider.getSigner(), isWeb3);
-
-  augur.connect(
-    connectOptions,
-    (err: any, connectionInfo: Connection) => {
-      if (err) return callback(err);
-      console.log("connected:", connectionInfo);
-      callback(null, connectionInfo);
-    },
-  );
+  await bootstrap(provider, account, isWeb3);
 };
 
 export const augur = new Augur();
