f31d4bf2110e6c52749135f2fde9013cc443cf27
nuevoalex <achap5dk@gmail.com>
2019-06-24 13:11:43 -0700

    remove unused interface

--- packages/augur-core/source/contracts/IRepOracle.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.5.4;
-
-
-// TODO when the Uniswap oracle is available modify this. It will need to take a specific REP token as an argument to accomodate forks
-contract IRepOracle {
-    function getRepPriceInAttoCash() external view returns (uint256);
-}
