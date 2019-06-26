f73007128110b63c644200611d40fb88662a77ab
nuevoalex <achap5dk@gmail.com>
2019-06-25 18:28:54 -0700

    gas optimization on math

--- packages/augur-core/source/contracts/libraries/math/SafeMathUint256.sol
+++ packages/augur-core/source/contracts/libraries/math/SafeMathUint256.sol
@@ -7,8 +7,16 @@ pragma solidity 0.5.4;
  */
 library SafeMathUint256 {
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
         uint256 c = a * b;
-        require(a == 0 || c / a == b);
+        require(c / a == b, "SafeMath: multiplication overflow");
+
         return c;
     }
 
