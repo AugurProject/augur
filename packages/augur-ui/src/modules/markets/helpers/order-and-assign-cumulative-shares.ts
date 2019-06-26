818576f57126c5e61b75910a1d868542b80655cd
Tom Haile <bradford.haile@gmail.com>
2019-06-25 21:54:12 -0500

    fixed ordering of orders in order book after calc quantityScale

--- packages/augur-ui/src/modules/markets/helpers/order-and-assign-cumulative-shares.ts
+++ packages/augur-ui/src/modules/markets/helpers/order-and-assign-cumulative-shares.ts
@@ -1,74 +1,64 @@
-import memoize from "memoizee";
-import { createBigNumber } from "utils/create-big-number";
-import { BIDS, ASKS } from "modules/common/constants";
+import memoize from 'memoizee';
+import { createBigNumber } from 'utils/create-big-number';
+import { BIDS, ASKS } from 'modules/common/constants';
 
 function calculateQuantityScale(outOfBN, sharesBN) {
   return createBigNumber(100).minus(
-    sharesBN
-      .dividedBy(outOfBN)
-      .times(createBigNumber(100))
+    sharesBN.dividedBy(outOfBN).times(createBigNumber(100))
   );
 }
 
 function calculateMaxValues(mostShares) {
-  return mostShares &&
-  createBigNumber(mostShares).plus(
-    createBigNumber(mostShares).times(createBigNumber(0.15))
+  return (
+    mostShares &&
+    createBigNumber(mostShares).plus(
+      createBigNumber(mostShares).times(createBigNumber(0.15))
+    )
   );
 }
-const orderAndAssignCumulativeShares = memoize(
-  (orderBook) => {
-    const rawBids = ((orderBook || {})[BIDS] || []).slice();
-    const rawAsks = ((orderBook || {})[ASKS] || []).slice();
+const orderAndAssignCumulativeShares = memoize(orderBook => {
+  const rawBids = ((orderBook || {})[BIDS] || []).slice();
+  const rawAsks = ((orderBook || {})[ASKS] || []).slice();
 
-    const mostBidShares =
-      Math.max(...rawBids.map(bid => createBigNumber(bid.shares).toNumber()))
-    const mostAskShares =
-      Math.max(...rawAsks.map(ask => createBigNumber(ask.shares).toNumber()))
-    const outOfBids = calculateMaxValues(mostBidShares);
-    const outOfAsks = calculateMaxValues(mostAskShares);
+  const mostBidShares = Math.max(
+    ...rawBids.map(bid => createBigNumber(bid.shares).toNumber())
+  );
+  const mostAskShares = Math.max(
+    ...rawAsks.map(ask => createBigNumber(ask.shares).toNumber())
+  );
+  const outOfBids = calculateMaxValues(mostBidShares);
+  const outOfAsks = calculateMaxValues(mostAskShares);
 
-    const bids = rawBids.reduce(
-      (p, order) => [
-        ...p,
-        {
-          price: order.price,
-          shares: order.shares,
-          quantityScale: calculateQuantityScale(
-            outOfBids,
-            createBigNumber(order.shares)
-          ).toString(),
-          cumulativeShares: order.cumulativeShares,
-          mySize: order.mySize
-        }
-      ],
-      []
-    );
+  const bids = rawBids
+    .map(order => ({
+      price: order.price,
+      shares: order.shares,
+      quantityScale: calculateQuantityScale(
+        outOfBids,
+        createBigNumber(order.shares)
+      ).toString(),
+      cumulativeShares: order.cumulativeShares,
+      mySize: order.mySize,
+    }))
+    .sort((a, b) => createBigNumber(b.price).minus(createBigNumber(a.price)));
 
-    const asks = rawAsks
-      .reduce(
-        (p, order) => [
-          ...p,
-          {
-            price: order.price,
-            shares: order.shares,
-            quantityScale: calculateQuantityScale(
-              outOfAsks,
-              createBigNumber(order.shares)
-            ).toString(),
-            cumulativeShares: order.cumulativeShares,
-            mySize: order.mySize
-          }
-        ],
-        []
-      )
-      .sort((a, b) => createBigNumber(b.price).minus(createBigNumber(a.price)));
+  const asks = rawAsks
+    .map(order => ({
+      price: order.price,
+      shares: order.shares,
+      quantityScale: calculateQuantityScale(
+        outOfAsks,
+        createBigNumber(order.shares)
+      ).toString(),
+      cumulativeShares: order.cumulativeShares,
+      mySize: order.mySize,
+    }))
+    .sort((a, b) => createBigNumber(b.price).minus(createBigNumber(a.price)));
 
-    return {
-      bids,
-      asks
-    };
-  }
-);
+  return {
+    bids,
+    asks,
+  };
+});
 
 export default orderAndAssignCumulativeShares;
