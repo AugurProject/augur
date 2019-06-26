b801bf46e86866a02bfb352492d804bf5b030b2a
John Danz <10524630+JohnDanz@users.noreply.github.com>
2019-06-26 11:32:32 -0400

    Numbered Lists Component (#2579)
    * added numbered list and numberedListItem to create-market common components, added PlusIcon to icons, added an option icon prop to SecondaryButton
    
    * fixed comment

--- packages/augur-ui/src/modules/create-market/components/common.tsx
+++ packages/augur-ui/src/modules/create-market/components/common.tsx
@@ -1,8 +1,11 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import classNames from "classnames";
+import React, { Component } from 'react';
+import classNames from 'classnames';
 
-import Styles from "modules/create-market/components/common.styles";
+import { SecondaryButton } from 'modules/common/buttons';
+import { TextInput } from 'modules/common/form';
+import { XIcon, AddIcon } from 'modules/common/icons';
+
+import Styles from 'modules/create-market/components/common.styles';
 
 export interface HeaderProps {
   text: string;
@@ -23,7 +26,131 @@ export const Subheaders = (props: SubheadersProps) => (
     <span>{props.header}</span>
     <span>
       {props.subheader}
-      {props.link && <a target="blank" href="https://docs.augur.net">Learn more</a>}
+      {props.link && (
+        <a target="blank" href="https://docs.augur.net">
+          Learn more
+        </a>
+      )}
     </span>
-  </div>        
+  </div>
 );
+
+export interface NumberedInputProps {
+  value: string;
+  removable: boolean;
+  number: number;
+  placeholder: string;
+  onChange: Function;
+  onRemove?: Function;
+}
+
+export interface NumberedListProps {
+  initialList: Array<string>;
+  minShown: number;
+  maxList: number;
+  placeholder: string;
+  updateList: Function;
+}
+
+export interface NumberedListState {
+  list: Array<string>;
+  isFull: boolean;
+  isMin: boolean;
+}
+
+export const NumberedInput = ({
+  number,
+  value,
+  placeholder,
+  onChange,
+  removable,
+  onRemove,
+}: NumberedInputProps) => (
+  <li key={number} className={Styles.NumberedInput}>
+    <span>{`${number + 1}.`}</span>
+    <TextInput
+      onChange={value => onChange(value, number)}
+      value={value}
+      placeholder={placeholder}
+    />
+    {removable && <button onClick={e => onRemove(number)}>{XIcon}</button>}
+  </li>
+);
+
+export class NumberedList extends Component<
+  NumberedListProps,
+  NumberedListState
+> {
+  state: NumberedListState = {
+    list: this.props.initialList,
+    isFull: this.props.initialList.length === this.props.maxList,
+    isMin: this.props.initialList.length === this.props.minShown,
+  };
+
+  onChange = (value, index) => {
+    const { updateList } = this.props;
+    const { list } = this.state;
+    list[index] = value;
+    this.setState({ list }, updateList(list));
+  };
+
+  addItem = () => {
+    const { isFull, list } = this.state;
+    const { maxList, minShown, updateList } = this.props;
+    if (!isFull) {
+      list.push('');
+      this.setState(
+        {
+          list,
+          isFull: list.length === maxList,
+          isMin: list.length === minShown,
+        },
+        updateList(list)
+      );
+    }
+  };
+
+  removeItem = index => {
+    const { isMin, list } = this.state;
+    const { minShown, maxList, updateList } = this.props;
+    if (!isMin) {
+      list.splice(index, 1);
+      this.setState(
+        {
+          list,
+          isMin: list.length === minShown,
+          isFull: list.length === maxList,
+        },
+        updateList(list)
+      );
+    }
+  };
+
+  render() {
+    const { list, isFull } = this.state;
+    const { placeholder, minShown } = this.props;
+
+    return (
+      <ul className={Styles.NumberedList}>
+        {list.map((item, index) => (
+          <NumberedInput
+            value={item}
+            placeholder={placeholder}
+            onChange={this.onChange}
+            number={index}
+            removable={index >= minShown}
+            onRemove={this.removeItem}
+          />
+        ))}
+        <li>
+          <SecondaryButton
+            disabled={isFull}
+            text="Add"
+            action={e => this.addItem()}
+            icon={AddIcon}
+          />
+        </li>
+      </ul>
+    );
+  }
+}
