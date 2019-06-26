303b0e89a79a0d55083e1aa06f1cdc7b45996455
nuevoalex <achap5dk@gmail.com>
2019-06-26 14:02:20 -0700

    add addition require validations

--- packages/augur-core/source/contracts/factories/DisputeCrowdsourcerFactory.sol
+++ packages/augur-core/source/contracts/factories/DisputeCrowdsourcerFactory.sol
@@ -8,6 +8,7 @@ import 'ROOT/IAugur.sol';
 
 contract DisputeCrowdsourcerFactory is CloneFactory {
     function createDisputeCrowdsourcer(IAugur _augur, IMarket _market, uint256 _size, bytes32 _payoutDistributionHash, uint256[] memory _payoutNumerators) public returns (IDisputeCrowdsourcer) {
+        require(_augur.isKnownMarket(_market));
         IDisputeCrowdsourcer _disputeCrowdsourcer = IDisputeCrowdsourcer(createClone(_augur.lookup("DisputeCrowdsourcer")));
         _disputeCrowdsourcer.initialize(_augur, _market, _size, _payoutDistributionHash, _payoutNumerators, _augur.lookup("ERC1820Registry"));
         return _disputeCrowdsourcer;
