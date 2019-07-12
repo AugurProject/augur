8e46e83a8824be54d6fc461c87082bbb84931b38
Andrew D'Amelio <dev@andrewdamel.io>
2019-07-12 16:05:43 -0400

    update Depth Chart

--- packages/augur-ui/src/modules/markets/helpers/order-for-market-depth.ts
+++ packages/augur-ui/src/modules/markets/helpers/order-for-market-depth.ts
@@ -1,60 +1,78 @@
-import memoize from "memoizee";
+import memoize from 'memoizee';
 
-import { BIDS, ASKS } from "modules/common/constants";
-import { createBigNumber } from "utils/create-big-number";
+import { BIDS, ASKS } from 'modules/common/constants';
+import { createBigNumber } from 'utils/create-big-number';
+
+type BidAskShape = [string, string, string, boolean];
+
+export interface MarketDepth {
+  bids: BidAskShape[];
+  asks: BidAskShape[];
+}
+
+export interface BidAsk {
+  price: string;
+  shares: string;
+  cumulativeShares: string;
+  quantityScale: string;
+}
+
+export interface OrderBookData {
+  asks: [] | BidAsk[];
+  bids: [] | BidAsk[];
+  spread: null | string;
+}
 
 // The last entry in the order array is a boolean denoting whether the order is selectable or not.
-const orderForMarketDepth = orderBook => {
-  const rawBids = ((orderBook || {})[BIDS] || []).slice();
-  const bids = rawBids.reduce(
+const orderForMarketDepth = (orderBook: OrderBookData): MarketDepth => {
+  const rawBids: BidAsk[] = ((orderBook || {})[BIDS] || []).slice();
+
+  const bids: BidAskShape[] = rawBids.reduce(
     (p, order) => [
       ...p,
-      [order.cumulativeShares, order.price, order.shares, true]
+      [order.cumulativeShares, order.price, order.shares, true],
     ],
     []
   );
-  const rawAsks = ((orderBook || {})[ASKS] || []).slice();
-  const asks = rawAsks
-    .sort((a, b) => createBigNumber(a.price).minus(createBigNumber(b.price)))
+
+  const rawAsks: BidAsk[] = ((orderBook || {})[ASKS] || []).slice();
+
+  const asks: BidAskShape[] = rawAsks
+    .sort((a, b) =>
+      createBigNumber(a.price)
+        .minus(createBigNumber(b.price))
+        .toNumber()
+    )
     .reduce(
       (p, order) => [
         ...p,
-        [order.cumulativeShares, order.price, order.shares, true]
+        [order.cumulativeShares, order.price, order.shares, true],
       ],
       []
     );
 
   if (asks.length > 0) {
-    const minAsksDepthOrder = asks.reduce(
+    const minAsksDepthOrder: BidAskShape = asks.reduce(
       (lastValue, nextValue) =>
         createBigNumber(lastValue[0] || 0).lte(createBigNumber(nextValue[0] || 0)) ? lastValue : nextValue,
       asks[0]
     );
-    asks.unshift([
-      createBigNumber(0),
-      minAsksDepthOrder[1],
-      minAsksDepthOrder[2],
-      false
-    ]);
+
+    asks.unshift(['0', minAsksDepthOrder[1], minAsksDepthOrder[2], false]);
   }
 
   if (bids.length > 0) {
-    const minBidDepthOrder = bids.reduce(
+    const maxBidDepthOrder: BidAskShape = bids.reduce(
       (lastValue, nextValue) =>
-        createBigNumber(lastValue[0] || 0).gte(createBigNumber(nextValue[0] || 0)) ? lastValue : nextValue,
+        createBigNumber(lastValue[0] || 0).lte(createBigNumber(nextValue[0] || 0)) ? lastValue : nextValue,
       bids[0]
     );
-    bids.unshift([
-      createBigNumber(0),
-      minBidDepthOrder[1],
-      minBidDepthOrder[2],
-      false
-    ]);
+    bids.unshift(['0', maxBidDepthOrder[1], maxBidDepthOrder[2], false]);
   }
 
   return {
     [BIDS]: bids,
-    [ASKS]: asks
+    [ASKS]: asks,
   };
 };
 
