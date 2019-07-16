e502670799d6f4354c73fb14d71e66a501a20535
Justin Barry <328965+justinbarry@users.noreply.github.com>
2019-07-16 12:36:34 -0700

    Fix tmux docker container exit issue

--- packages/augur-tools/docker-builder/run.sh
+++ packages/augur-tools/docker-builder/run.sh
@@ -4,5 +4,5 @@ IMAGE=$1
 DOCKER_TAG=${DOCKER_TAG:-$(node ./scripts/get-contract-hashes.js)};
 ./scripts/copy-docker-files.sh $IMAGE:$DOCKER_TAG
 echo "Running Geth with contracts deployed from ${IMAGE}:${DOCKER_TAG}"
-docker run -e GETH_VERBOSITY=4 -it --rm -p 8545:8545 -p 8546:8546 --name ${CONTAINER_NAME:-geth-node} $IMAGE:$DOCKER_TAG
+docker run --init -e GETH_VERBOSITY=4 --rm -p 8545:8545 -p 8546:8546 --name ${CONTAINER_NAME:-geth-node} $IMAGE:$DOCKER_TAG
 
