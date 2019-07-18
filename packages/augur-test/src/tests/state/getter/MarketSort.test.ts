8ecd6a9973701ecb44b58a1a657def5b9ff30c62
nuevoalex <achap5dk@gmail.com>
2019-07-17 21:15:49 -0700

    horizontal liquidity

--- packages/augur-test/src/tests/state/getter/MarketSort.test.ts
+++ packages/augur-test/src/tests/state/getter/MarketSort.test.ts
@@ -6,7 +6,7 @@ import { stringTo32ByteHex } from '../../../libs/Utils';
 
 const mock = makeDbMock();
 
-describe('State API :: Invalid Filter', () => {
+describe('State API :: Market Sorts', () => {
   let db: Promise<DB>;
   let john: ContractAPI;
 
@@ -18,7 +18,8 @@ describe('State API :: Invalid Filter', () => {
     db = mock.makeDB(john.augur, ACCOUNTS);
   }, 120000);
 
-  test(':getTradingHistory', async () => {
+  /*
+  test(':invalidFilter', async () => {
     await john.approveCentralAuthority();
 
     // Create a market
@@ -53,6 +54,40 @@ describe('State API :: Invalid Filter', () => {
 
     // The Invalid filter is now hit because this Bid would be profitable for a filler assuming the market were actually Valid
     await expect(marketData[0].invalidFilter).toEqual(true);
+
+  }, 60000);
+  */
+
+  test(':liquidity', async () => {
+    await john.approveCentralAuthority();
+
+    // Create a market
+    const market = await john.createReasonableMarket([stringTo32ByteHex('A'), stringTo32ByteHex('B'), stringTo32ByteHex('C')]);
+    const outcomeA = new BigNumber(1);
+    const outcomeB = new BigNumber(2);
+    const outcomeC = new BigNumber(3);
+
+    // With no orders on the book the liquidity scores won't exist
+    await (await db).sync(john.augur, mock.constants.chunkSize, 0);
+    let marketData = await (await db).findMarkets({selector: { market: market.address }});
+    await expect(marketData[0].liquidity).toEqual(undefined);
+
+    // Place a Bid on A and an Ask on A
+    const bid = new BigNumber(0);
+    const ask = new BigNumber(1);
+    let numShares = new BigNumber(10**18);
+    let price = new BigNumber(51);
+
+    await john.simplePlaceOrder(market.address, bid, numShares, price, outcomeA);
+
+    price = new BigNumber(49);
+
+    await john.simplePlaceOrder(market.address, ask, numShares, price, outcomeA);
+
+    await (await db).sync(john.augur, mock.constants.chunkSize, 0);
+    marketData = await (await db).findMarkets({selector: { market: market.address }});
+
+    await expect(marketData[0].liquidity[10]).toEqual(102000000000000000000);
 
   }, 60000);
 
