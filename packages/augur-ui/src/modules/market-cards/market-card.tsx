983c3433b6fd63b8bd4f2f1ad65694df8e684b94
phoebemirman <phoebe.mirman@gmail.com>
2019-07-30 16:09:33 -0700

    loading view

--- packages/augur-ui/src/modules/market-cards/market-card.tsx
+++ packages/augur-ui/src/modules/market-cards/market-card.tsx
@@ -31,6 +31,7 @@ interface MarketCardProps {
   condensed?: Boolean;
   expandedView?: Boolean;
   address: string;
+  loading?: Boolean;
 }
 
 interface MarketCardState {
@@ -60,7 +61,8 @@ export default class MarketCard extends React.Component<
       reportingWindowStatsEndTime,
       condensed,
       address,
-      expandedView
+      expandedView,
+      loading
     } = this.props;
 
     const s = this.state;
@@ -107,135 +109,150 @@ export default class MarketCard extends React.Component<
  
     return (
       <div 
-        className={Styles.MarketCard}
+        className={classNames(Styles.MarketCard, {[Styles.Loading]: loading})}
       >
-        <div>
-          {address && address.toUpperCase() === author.toUpperCase() &&
-            <HoverIcon
-              label="marketCreator"
-              icon={MarketCreator}
-              hoverText="Market Creator"
-            />
-          }
-          <HoverIcon
-            label="reporter"
-            icon={DesignatedReporter}
-            hoverText="Designated Reporter"
-          />
-          <HoverIcon
-            label="Position"
-            icon={PositionIcon}
-            hoverText="Position"
-          />
-          <HoverIcon
-            label="dispute"
-            icon={DisputeStake}
-            hoverText="Dispute Stake"
-          />
-        </div>
-        <div>
-          <InReportingLabel
-            marketStatus={marketStatus}
-            reportingState={reportingState}
-            disputeInfo={disputeInfo}
-            endTime={endTimeFormatted}
-            currentAugurTimestamp={currentAugurTimestamp}
-            reportingWindowStatsEndTime={reportingWindowStatsEndTime}
-          />
-          <MarketTypeLabel marketType={marketType} />
-          <CategoryTagTrail
-            categories={categoriesWithClick}
-            tags={tagsWithClick}
-          />
-          <MarketProgress
-            reportingState={reportingState}
-            currentTime={currentAugurTimestamp}
-            endTime={endTime}
-            reportingWindowEndtime={reportingWindowStatsEndTime}
-            alignRight
-          />
-          {toggleFavorite && (
-            <div>
-              <FavoritesButton
-                action={() => toggleFavorite()}
-                isFavorite={isFavorite}
-                hideText
-                disabled={!isLogged}
-              />
-            </div>
-          )}
-          <DotSelection>
-            <div
-              id="copy_marketId"
-              data-clipboard-text={id}
-            >
-              {PaperClip} {COPY_MARKET_ID}
-            </div>
-            <div
-              id="copy_author"
-              data-clipboard-text={author}
-            >
-              {Person} {COPY_AUTHOR}
-            </div>
-          </DotSelection>
-        </div>
-        <div>
-          {reportingState === REPORTING_STATE.PRE_REPORTING &&
-            <>
-              <LabelValue
-                label="VOL"
-                value={volumeFormatted.formatted}
-              />
-              {!condensed && 
-                <LabelValue
-                  label="OI"
-                  value={openInterestFormatted.formatted}
-                />
-              }
-            </>
-          }
-          {reportingState !== REPORTING_STATE.PRE_REPORTING &&
-            <LabelValue
-              condensed
-              label="Total Dispute Stake"
-              value={disputeInfo.stakeCompletedTotal}
-            />
-          }
-        </div>
-        
-        <MarketLink id={id}>
-          {description}
-        </MarketLink>
-        {!condensed && !marketResolved &&
+        {loading &&
           <>
-            <OutcomeGroup 
-              outcomes={outcomesFormatted} 
-              marketType={marketType}
-              scalarDenomination={scalarDenomination}
-              min={minPrice}
-              max={maxPrice}
-              lastPrice={0}
-              expanded={expandedView ? true : s.expanded}
-            />
-            {marketType === CATEGORICAL && outcomesFormatted.length > 3 && !expandedView &&
-              <button onClick={this.expand}>
-                <ChevronFlip
-                  stroke="#fff"
-                  pointDown={s.expanded}
-                  quick
-                  filledInIcon
-                  hover
-                />
-                {s.expanded ? "show less" : "view all outcomes"}
-              </button>
-            }
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
           </>
         }
-        {marketResolved &&
-          <ResolvedOutcomes
-            outcomes={o}
-            expanded={expandedView}
-          />
+        {!loading && 
+          <>
+            <div>
+              {address && address.toUpperCase() === author.toUpperCase() &&
+                <HoverIcon
+                  label="marketCreator"
+                  icon={MarketCreator}
+                  hoverText="Market Creator"
+                />
+              }
+              <HoverIcon
+                label="reporter"
+                icon={DesignatedReporter}
+                hoverText="Designated Reporter"
+              />
+              <HoverIcon
+                label="Position"
+                icon={PositionIcon}
+                hoverText="Position"
+              />
+              <HoverIcon
+                label="dispute"
+                icon={DisputeStake}
+                hoverText="Dispute Stake"
+              />
+            </div>
+            <div>
+              <InReportingLabel
+                marketStatus={marketStatus}
+                reportingState={reportingState}
+                disputeInfo={disputeInfo}
+                endTime={endTimeFormatted}
+                currentAugurTimestamp={currentAugurTimestamp}
+                reportingWindowStatsEndTime={reportingWindowStatsEndTime}
+              />
+              <MarketTypeLabel marketType={marketType} />
+              <CategoryTagTrail
+                categories={categoriesWithClick}
+                tags={tagsWithClick}
+              />
+              <MarketProgress
+                reportingState={reportingState}
+                currentTime={currentAugurTimestamp}
+                endTime={endTime}
+                reportingWindowEndtime={reportingWindowStatsEndTime}
+                alignRight
+              />
+              {toggleFavorite && (
+                <div>
+                  <FavoritesButton
+                    action={() => toggleFavorite()}
+                    isFavorite={isFavorite}
+                    hideText
+                    disabled={!isLogged}
+                  />
+                </div>
+              )}
+              <DotSelection>
+                <div
+                  id="copy_marketId"
+                  data-clipboard-text={id}
+                >
+                  {PaperClip} {COPY_MARKET_ID}
+                </div>
+                <div
+                  id="copy_author"
+                  data-clipboard-text={author}
+                >
+                  {Person} {COPY_AUTHOR}
+                </div>
+              </DotSelection>
+            </div>
+            <div>
+              {reportingState === REPORTING_STATE.PRE_REPORTING &&
+                <>
+                  <LabelValue
+                    label="VOL"
+                    value={volumeFormatted.formatted}
+                  />
+                  {!condensed && 
+                    <LabelValue
+                      label="OI"
+                      value={openInterestFormatted.formatted}
+                    />
+                  }
+                </>
+              }
+              {reportingState !== REPORTING_STATE.PRE_REPORTING &&
+                <LabelValue
+                  condensed
+                  label="Total Dispute Stake"
+                  value={disputeInfo.stakeCompletedTotal}
+                />
+              }
+            </div>
+            
+            <MarketLink id={id}>
+              {description}
+            </MarketLink>
+            {!condensed && !marketResolved &&
+              <>
+                <OutcomeGroup 
+                  outcomes={outcomesFormatted} 
+                  marketType={marketType}
+                  scalarDenomination={scalarDenomination}
+                  min={minPrice}
+                  max={maxPrice}
+                  lastPrice={0}
+                  expanded={expandedView ? true : s.expanded}
+                />
+                {marketType === CATEGORICAL && outcomesFormatted.length > 3 && !expandedView &&
+                  <button onClick={this.expand}>
+                    <ChevronFlip
+                      stroke="#fff"
+                      pointDown={s.expanded}
+                      quick
+                      filledInIcon
+                      hover
+                    />
+                    {s.expanded ? "show less" : "view all outcomes"}
+                  </button>
+                }
+              </>
+            }
+            {marketResolved &&
+              <ResolvedOutcomes
+                outcomes={o}
+                expanded={expandedView}
+              />
+            }
+          </>
         }
       </div>
     );
