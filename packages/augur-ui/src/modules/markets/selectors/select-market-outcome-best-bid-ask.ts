bc9212c60cda35c59e9aa2e697237956fdbeb4b8
Andrew D'Amelio <dev@andrewdamel.io>
2019-08-02 12:55:25 -0400

    fix best bid ask missing on trading page (#3046)
    * fix best bid ask missing on trading page
    
    * minor update

---
+++ packages/augur-ui/src/modules/markets/selectors/select-market-outcome-best-bid-ask.ts
@@ -0,0 +1,39 @@
+import { formatNone, formatShares, formatDai } from 'utils/format-number';
+
+export const selectMarketOutcomeBestBidAsk = orderBook => {
+  const none = { price: formatNone(), shares: formatNone() };
+  let topAsk = none;
+  let topBid = none;
+
+  const formatData = item => {
+    return {
+      price: formatDai(item.price),
+      shares: formatShares(item.shares),
+    };
+  };
+
+  if (orderBook) {
+    topAsk =
+      orderBook.asks && orderBook.asks.length > 0
+        ? orderBook.asks
+            // Ascending order for asks
+            .sort((a, b) => Number(a.price) - Number(b.price))
+            .map(item => formatData(item))
+            .shift()
+        : topAsk;
+
+    topBid =
+      orderBook.bids && orderBook.bids.length > 0
+        ? orderBook.bids
+            // Decreasing order for bids
+            .sort((a, b) => Number(b.price) - Number(a.price))
+            .map(item => formatData(item))
+            .shift()
+        : topBid;
+  }
+
+  return {
+    topAsk,
+    topBid,
+  };
+};
