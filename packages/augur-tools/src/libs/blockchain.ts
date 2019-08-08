40587848083bd268442d33f25bfb43a6e76c0139
Robert E. Davidson III <45945043+robert-e-davidson3@users.noreply.github.com>
2019-08-08 10:11:34 -0700

    DeployerConfiguration is now an interface (#3111)
    * DeployerConfiguration is now an interface

--- packages/augur-tools/src/libs/blockchain.ts
+++ packages/augur-tools/src/libs/blockchain.ts
@@ -2,7 +2,7 @@ import * as path from "path";
 import { CompilerOutput } from "solc";
 
 import { EthersProvider } from "@augurproject/ethersjs-provider";
-import { DeployerConfiguration, EthersFastSubmitWallet } from "@augurproject/core";
+import { DeployerConfigurationOverwrite, CreateDeployerConfiguration, EthersFastSubmitWallet } from "@augurproject/core";
 import { ContractAddresses } from "@augurproject/artifacts";
 import { ContractDependenciesEthers } from "contract-dependencies-ethers";
 import { ContractDeployer } from "@augurproject/core";
@@ -10,12 +10,12 @@ import { ContractDeployer } from "@augurproject/core";
 import { Account } from "../constants";
 
 const augurCorePath = path.join(__dirname, "../../../augur-core/");
-
-export function makeDeployerConfiguration(writeArtifacts = true) {
-  const contractInputRoot = path.join(augurCorePath, "../augur-artifacts/src");
-  const artifactOutputRoot = writeArtifacts ? path.join(augurCorePath, "../augur-artifacts/src") : null;
-  return DeployerConfiguration.create(contractInputRoot, artifactOutputRoot);
-}
+const root = path.join(augurCorePath, "../augur-artifacts/src");
+const flashDeployerConfigurationDefaults: DeployerConfigurationOverwrite = {
+  contractInputPath: path.join(root, 'contracts.json'),
+  contractAddressesOutputPath: path.join(root, 'addresses.json'),
+  uploadBlockNumbersOutputPath: path.join(root, 'upload-block-numbers.json'),
+};
 
 export interface UsefulContractObjects {
   provider: EthersProvider;
@@ -24,11 +24,13 @@ export interface UsefulContractObjects {
   addresses: ContractAddresses;
 }
 
-export async function deployContracts(provider: EthersProvider, accounts: Account[], compiledContracts: CompilerOutput, writeArtifacts = false): Promise<UsefulContractObjects> {
+export async function deployContracts(provider: EthersProvider,  accounts: Account[], compiledContracts: CompilerOutput, config: DeployerConfigurationOverwrite): Promise<UsefulContractObjects> {
+  config = Object.assign({}, flashDeployerConfigurationDefaults, config);
+  const deployerConfiguration = CreateDeployerConfiguration(config);
+
   const signer = await makeSigner(accounts[0], provider);
   const dependencies = makeDependencies(accounts[0], provider, signer);
 
-  const deployerConfiguration = makeDeployerConfiguration(writeArtifacts);
   const contractDeployer = new ContractDeployer(deployerConfiguration, dependencies, provider.provider, signer, compiledContracts);
   const addresses = await contractDeployer.deploy();
 
