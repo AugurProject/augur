46bd54bdfc27410c01b343ccacf930380b3d2a4d
Tom Haile <bradford.haile@gmail.com>
2019-08-09 17:25:28 -0500

    pad end so string is correct length

--- packages/augur-ui/src/utils/generate-tx-parameter-id.ts
+++ packages/augur-ui/src/utils/generate-tx-parameter-id.ts
@@ -3,6 +3,6 @@ import { TransactionMetadataParams } from 'contract-dependencies-ethers/build';
 
 export function generateTxParameterId(parameters: TransactionMetadataParams): string {
   if (!parameters) return null;
-  const json = JSON.stringify(parameters);
+  let json = JSON.stringify(parameters).padEnd(32, '0');
   return ethers.utils.formatBytes32String(json).substring(1, 32);
 }
