cbcef4766646c38e7642c8854c76e4c008f06fd9
Tom Haile <bradford.haile@gmail.com>
2019-08-09 17:37:39 -0500

    fixed gen tx id

--- packages/augur-ui/src/utils/generate-tx-parameter-id.ts
+++ packages/augur-ui/src/utils/generate-tx-parameter-id.ts
@@ -3,6 +3,6 @@ import { TransactionMetadataParams } from 'contract-dependencies-ethers/build';
 
 export function generateTxParameterId(parameters: TransactionMetadataParams): string {
   if (!parameters) return null;
-  let json = JSON.stringify(parameters).padEnd(32, '0');
-  return ethers.utils.formatBytes32String(json).substring(1, 32);
+  let json = JSON.stringify(parameters).padEnd(32, '0').substring(1, 32);
+  return ethers.utils.formatBytes32String(json);
 }
