dbd51e0efe113aaab736076a847f7b0b09b7af8a
phoebemirman <phoebe.mirman@gmail.com>
2019-08-13 14:02:19 -0700

    chkpnt

--- packages/augur-ui/src/modules/market-cards/common.tsx
+++ packages/augur-ui/src/modules/market-cards/common.tsx
@@ -55,7 +55,7 @@ export interface ScalarOutcomeProps {
 }
 
 function calculatePosition(min, max, lastPrice) {
-	const range = max.minus(min);
+	const range = max.abs().minus(min.abs());
 	const pricePercentage = createBigNumber(lastPrice || 0)
 	  .minus(min)
 	  .dividedBy(range)
