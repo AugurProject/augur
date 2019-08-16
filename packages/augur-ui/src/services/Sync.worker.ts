304933cf2355f034279ac4cbd6781f6dc0061311
adrake33 <aarondrake@gmail.com>
2019-08-16 15:52:29 -0700

    Checkpoint

--- packages/augur-ui/src/services/Sync.worker.ts
+++ packages/augur-ui/src/services/Sync.worker.ts
@@ -18,27 +18,18 @@ const subscriptions = new Subscriptions(Events.augurEmitter);
 ctx.addEventListener('message', async (message: any) => {
   const messageData = message.data;
 
-  try {
-    if (!IsJsonRpcRequest(messageData)) {
-      return console.error('Bad JSON RPC message received:', messageData);
-    }
-  } catch (err) {
-    ctx.postMessage(
-      MakeJsonRpcError('-1', JsonRpcErrorCode.ParseError, 'Bad JSON RPC message received', { originalText: messageData as string })
-    );
-  }
-
-  try {
-    if (messageData.method === 'subscribe') {
-console.log("In Sync.worker.addEventListener (subscribe)");
+console.log("MessageData");
 console.log(messageData);
-      const eventName: string = messageData.params.shift();
+  try {
+    if (messageData.subscribe) {
+console.log("In Sync.worker.addEventListener (subscribe)");
+
       try {
         const subscription: string = subscriptions.subscribe(
-          eventName,
+          messageData.subscribe,
           (data: {eventName: string}): void => {
             ctx.postMessage(
-              MakeJsonRpcResponse(null, { eventName: data.eventName, subscription, result: data })
+              MakeJsonRpcResponse(null, { eventName: data.eventName, result: data })
             );
           }
         );
@@ -50,24 +41,21 @@ console.log(messageData);
           MakeJsonRpcError(messageData.id, JsonRpcErrorCode.MethodNotFound, err.toString(), false)
         );
       }
-    } else if (messageData.method === 'unsubscribe') {
+    } else if (messageData.unsubscribe) {
 console.log("In Sync.worker.addEventListener (unsubscribe)");
 console.log(messageData);
-      const subscription: string = messageData.params.shift();
-      subscriptions.unsubscribe(subscription);
+      subscriptions.unsubscribe(messageData.unsubscribe);
       ctx.postMessage(
         MakeJsonRpcResponse(messageData.id, true)
       );
     } else if (messageData.method === 'start') {
 console.log("In Sync.worker.addEventListener (start)");
-console.log(messageData);
       api = await Sync.start(messageData.ethNodeUrl, messageData.account);
       ctx.postMessage(
         MakeJsonRpcResponse(messageData.id, null)
       );
     } else {
 console.log("In Sync.worker.addEventListener (else)");
-console.log(messageData);
       try {
         const request = messageData as JsonRpcRequest;
         const result = await api.route(request.method, request.params);
--- packages/augur-ui/src/services/ww-connector.ts
+++ packages/augur-ui/src/services/ww-connector.ts
@@ -20,6 +20,7 @@ export class WebWorkerConnector extends Connectors.BaseConnector {
   subscriptions: { [event: string]: { id: string, callback: Callback } } = {};
 
   async connect(ethNodeUrl: string, account?: string): Promise<any> {
+// console.log("In WebWorkerConnector.connect");
     this.worker = new RunWorker();
 
     this.worker.postMessage({
@@ -30,23 +31,18 @@ export class WebWorkerConnector extends Connectors.BaseConnector {
       account,
       jsonrpc: '2.0',
     });
-console.log("In WebWorkerConnector.connect");
 
     this.worker.onmessage = (event: MessageEvent) => {
 console.log("In WebWorkerConnector.onMessage");
 console.log(event);
       try {
         const eventData = JSON.parse(event.data);
-console.log(eventData);
-        if (eventData.result && eventData.result.subscription) {
-console.log("in if");
-          if (this.subscriptions[eventData.result.eventName]) {
-            this.subscriptions[eventData.result.eventName]['id'] = eventData.result.subscription;
-          }
+        if (eventData.result && eventData.result.subscribed) {
+console.log("Subscribed to", eventData.result.subscribed);
+          this.subscriptions[eventData.result.subscribed].id = eventData.result.subscription;
         } else {
           this.messageReceived(eventData);
         }
-console.error(this.subscriptions);
       } catch (error) {
         console.error('Bad Web Worker response: ' + error);
       }
@@ -65,8 +61,8 @@ console.log("In WebWorkerConnector.messageReceived");
 console.log(message);
     if (message.result && message.result.result) {
       if (this.subscriptions[message.result.eventName]) {
-console.log(this.subscriptions[message.result.eventName].callback);
-        this.subscriptions[message.result.eventName].callback(message.result.result);
+console.log("Calling callback");
+        this.subscriptions[message.result.eventName].callback(message.result);
       }
     }
 console.log(this.subscriptions);
@@ -79,9 +75,7 @@ console.log(this.subscriptions);
   bindTo<R, P>(
     f: (db: any, augur: any, params: P) => Promise<R>
   ): (params: P) => Promise<R> {
-console.log("In WebWorkerConnector.bindTo");
     return async (params: P): Promise<R> => {
-console.log(params);
       return this.worker.postMessage({
         id: iterator.next().value,
         method: f.name,
@@ -95,34 +89,25 @@ console.log(params);
     eventName: SubscriptionEventName | string,
     callback: Events.Callback
   ): Promise<void> {
-console.log("In WebWorkerConnector.on");
-console.log(eventName);
-console.log(callback);
     this.subscriptions[eventName] = {
       id: '',
       callback: this.callbackWrapper(callback),
     };
     this.worker.postMessage({
       id: iterator.next().value,
-      method: 'subscribe',
-      params: [eventName],
-      eventName,
+      subscribe: eventName,
       jsonrpc: '2.0',
     });
-console.error(this.subscriptions);
   }
 
   async off(eventName: SubscriptionEventName | string): Promise<void> {
-console.log("In WebWorkerConnector.off");
     const subscription = this.subscriptions[eventName];
 
     if (subscription) {
       delete this.subscriptions[eventName];
       this.worker.postMessage({
         id: iterator.next().value,
-        method: 'unsubscribe',
-        params: [subscription.id],
-        subscription: subscription.id,
+        unsubscribe: subscription.id,
         jsonrpc: '2.0',
       });
     }
