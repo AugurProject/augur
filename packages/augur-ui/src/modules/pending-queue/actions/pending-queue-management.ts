769434562b3e33a7cbffa8319b1bb47bbee32c75
Tom Haile <bradford.haile@gmail.com>
2019-08-16 17:05:14 -0500

    add failures back in to pending queue, user has to manually dismiss failed tx to clear them

--- packages/augur-ui/src/modules/pending-queue/actions/pending-queue-management.ts
+++ packages/augur-ui/src/modules/pending-queue/actions/pending-queue-management.ts
@@ -1,6 +1,6 @@
-import { BaseAction, UIOrder, CreateMarketData } from "modules/types";
+import { BaseAction, CreateMarketData } from "modules/types";
 import { isTransactionConfirmed } from 'modules/contracts/actions/contractCalls';
-
+import { TXEventName } from '@augurproject/sdk';
 export const ADD_PENDING_DATA = "ADD_PENDING_DATA";
 export const REMOVE_PENDING_DATA = "REMOVE_PENDING_DATA";
 
@@ -14,6 +14,7 @@ export const loadPendingQueue = (pendingQueue: any) => (
     Object.keys(data).map(async (d: any) => {
       const pendingData = data[d];
       if (!pendingData.pendingId) return;
+      if (pendingData.status === TXEventName.Failure) dispatch(addPendingData(d, queue, pendingData.status, pendingData.data));;
       const confirmed = await isTransactionConfirmed(pendingData.pendingId);
       confirmed
         ? dispatch(removePendingData(d, queue))
