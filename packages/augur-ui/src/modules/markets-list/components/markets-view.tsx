8d7dab7c084d14fe1ef685ea99019162ab79ffc0
JohnDanz <John.Danz87@gmail.com>
2019-08-20 11:22:43 -0500

    fixed a divide by 0 situation that caused a crash

--- packages/augur-ui/src/modules/markets-list/components/markets-view.tsx
+++ packages/augur-ui/src/modules/markets-list/components/markets-view.tsx
@@ -102,8 +102,8 @@ export default class MarketsView extends Component<
   }
 
   updateFilter(params) {
-    const { filter, sort, limit, offset } = params;
-    this.setState({ filter, sort, limit, offset }, this.updateFilteredMarkets);
+    const { filter, sort, offset } = params;
+    this.setState({ filter, sort, offset }, this.updateFilteredMarkets);
   }
 
   updateFilteredMarkets() {
