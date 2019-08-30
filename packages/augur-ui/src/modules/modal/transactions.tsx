424dcf4abfef88e3f64379ab6ce46da480a9ea91
Andrew D'Amelio <dev@andrewdamel.io>
2019-08-30 11:05:58 -0400

    clean up UI routes

--- packages/augur-ui/src/modules/modal/transactions.tsx
+++ packages/augur-ui/src/modules/modal/transactions.tsx
@@ -106,7 +106,7 @@ const actionOptions = [
   },
   {
     label: "Dispute",
-    value: "DISPUTE",
+    value: "DISPUTING",
   },
   {
     label: "Initial Report",
--- packages/augur-ui/src/modules/routes/components/routes/routes.tsx
+++ packages/augur-ui/src/modules/routes/components/routes/routes.tsx
@@ -1,50 +1,26 @@
-import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
-import AuthenticatedRoute from "modules/routes/components/authenticated-route/authenticated-route";
-import makePath from "modules/routes/helpers/make-path";
+import AuthenticatedRoute from 'modules/routes/components/authenticated-route/authenticated-route';
+import makePath from 'modules/routes/helpers/make-path';
 
-import * as VIEWS from "modules/routes/constants/views";
-import * as COMPONENTS from "modules/routes/constants/components";
+import * as VIEWS from 'modules/routes/constants/views';
+import * as COMPONENTS from 'modules/routes/constants/components';
 
-// NOTE --  Routes are declarative, meaning ONLY top level views should be declared here.
-//          Sub-views should be declared as needed at their respective inclusion points.
-
-// TODO -- due to side-bar matching constraints, the below does not conform to the above.
 const Routes = (p) => (
   <Switch>
     <Route path={makePath(VIEWS.MARKETS)} component={COMPONENTS.Markets} />
     <Route path={makePath(VIEWS.MARKET)} component={COMPONENTS.Market} />
     <AuthenticatedRoute
-      path={makePath(VIEWS.DISPUTE)}
-      component={COMPONENTS.Dispute}
-    />
-    <AuthenticatedRoute
-      path={makePath(VIEWS.REPORT)}
-      component={COMPONENTS.Report}
-    />
-    <AuthenticatedRoute
       path={makePath(VIEWS.MY_POSITIONS)}
       component={COMPONENTS.Portfolio}
     />
     <AuthenticatedRoute
-      path={makePath(VIEWS.ACCOUNT)}
+      path={makePath(VIEWS.ACCOUNT_SUMMARY)}
       component={COMPONENTS.Account}
     />
     <AuthenticatedRoute
-      path={makePath(VIEWS.ACCOUNT_DEPOSIT)}
-      component={COMPONENTS.Account}
-    />
-    <AuthenticatedRoute
-      path={makePath(VIEWS.ACCOUNT_WITHDRAW)}
-      component={COMPONENTS.Account}
-    />
-    <AuthenticatedRoute
-      path={makePath(VIEWS.ACCOUNT_REP_FAUCET)}
-      component={COMPONENTS.Account}
-    />
-    <AuthenticatedRoute
-      path={makePath(VIEWS.ACCOUNT_UNIVERSES)}
+      path={makePath(VIEWS.TRANSACTIONS)}
       component={COMPONENTS.Account}
     />
     <AuthenticatedRoute
@@ -52,11 +28,11 @@ const Routes = (p) => (
       component={COMPONENTS.CreateMarket}
     />
     <Route
-      path={makePath(VIEWS.REPORTING_DISPUTE_MARKETS)}
-      component={COMPONENTS.Reporting}
+      path={makePath(VIEWS.DISPUTING)}
+      component={COMPONENTS.Disputing}
     />
     <Route
-      path={makePath(VIEWS.REPORTING_REPORT_MARKETS)}
+      path={makePath(VIEWS.REPORTING)}
       component={COMPONENTS.Reporting}
     />
     <Redirect to={makePath(VIEWS.MARKETS)} />
