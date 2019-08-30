f6f1947ac77a023e3721cfd0a3dea6a09e2d637d
adrake33 <aarondrake@gmail.com>
2019-08-30 14:10:40 -0700

    Emit trackedUsers

--- packages/augur-sdk/src/state/db/DB.ts
+++ packages/augur-sdk/src/state/db/DB.ts
@@ -283,6 +283,7 @@ export class DB {
 
     augurEmitter.emit(SubscriptionEventName.UserDataSynced, {
       eventName: SubscriptionEventName.UserDataSynced,
+      trackedUsers: this.trackedUsers,
     });
   }
 
