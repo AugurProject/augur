5ba508031aad983a73365f00e9cb967d9f25b64a
JohnDanz <John.Danz87@gmail.com>
2019-09-05 16:32:40 -0500

    switched to quad box, started filling in some dummy values

--- packages/augur-ui/src/modules/reporting/components/markets-in-dispute.tsx
+++ packages/augur-ui/src/modules/reporting/components/markets-in-dispute.tsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
-import FilterBox from "modules/portfolio/components/common/filter-box";
+import QuadBox from "modules/portfolio/components/common/quad-box";
+import EmptyDisplay from "modules/portfolio/components/common/empty-display";
+import { SwitchLabelsGroup } from "modules/common/switch-labels-group";
 
 interface MarketsInDisputeProps {
   markets: any;
   sortByOptions: any;
+}
+
+interface MarketsInDisputeState {
+  search: string;
+  // sortBy: string;
+  selectedTab: string;
+  tabs: Array<Tab>;
+  // filteredData: Array<Market>;
 }
 
 function filterComp(input, market) {
@@ -11,23 +21,65 @@ function filterComp(input, market) {
   return true;
 }
 
-export default class MarketsInDispute extends Component<MarketsInDisputeProps> {
+export default class MarketsInDispute extends Component<MarketsInDisputeProps, MarketsInDisputeState> {
   constructor(props) {
     super(props);
+    this.state = {
+      selectedTab: "dispute",
+      tabs: [
+        {
+          key: "dispute",
+          label: "dispute",
+          num: 0,
+        },
+        {
+          key: "disputeSoon",
+          label: "dispute soon",
+          num: 1,
+        }
+      ],
+      search: "dispute"
+    }
   }
 
-  render() {
-    
-    const { markets, sortByOptions } = this.props;
+  selectTab = (tab: string) => {
+    this.setState({ selectedTab: tab });
+    // const { data } = this.props;
+    // let dataFiltered = this.applySearch(data, this.state.search, data[tab]);
+    // dataFiltered = this.applySortBy(this.state.sortBy, dataFiltered);
 
+    // @ts-ignore
+    // this.setState({ tab });
+  };
+
+  render() {
+    const { selectedTab, tabs, search } = this.state;
+    const { markets, sortByOptions } = this.props;
+    
     return (
-      <FilterBox
+      <QuadBox
         title="Markets In Dispute"
+        switchHeaders={true}
+        showFilterSearch={true}
+        onSearchChange={filterComp}
         sortByOptions={sortByOptions}
-        markets={[markets]}
-        filterComp={filterComp}
-        filterLabel="hello world"
-        currentAugurTimestamp={Date.now() / 1000}
+        updateDropdown={filterComp}
+        bottomBarContent={
+          <SwitchLabelsGroup
+            tabs={tabs}
+            selectedTab={selectedTab}
+            selectTab={this.selectTab}
+          />
+        }
+        content={
+          <>
+            <EmptyDisplay
+              selectedTab="disputes tab"
+              filterLabel={"hello world"}
+              search={search}
+            />
+          </>
+        }
       />
     );
   }
