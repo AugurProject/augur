0036794b8a73883d4ee5f4b06881db80ff367991
Andrew D'Amelio <dev@andrewdamel.io>
2019-09-06 19:32:27 -0400

    rebase and pr feedback

--- packages/augur-ui/src/modules/common/constants.ts
+++ packages/augur-ui/src/modules/common/constants.ts
@@ -239,10 +239,6 @@ export const SORT_OPTIONS = [
   { value: MARKET_SORT_PARAMS.CREATION_TIME, header: 'Recently created' },
   { value: MARKET_SORT_PARAMS.END_DATE, header: 'Ending soon ' },
   { value: MARKET_SORT_PARAMS.RECENTLY_TRADED, header: 'Recently Traded' },
-  {
-    value: MARKET_SORT_PARAMS.LAST_LIQUIDITY_DEPLETED,
-    header: 'Recently depleted liquidity',
-  },
 ];
 
 export enum MARKET_CARD_FORMATS {
