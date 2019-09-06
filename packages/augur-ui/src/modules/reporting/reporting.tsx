e25a97de3e48997f558b7841ebd34d2bfbfa2e24
phoebemirman <phoebe.mirman@gmail.com>
2019-09-06 12:22:30 -0700

    chpnt

--- packages/augur-ui/src/modules/reporting/reporting.tsx
+++ packages/augur-ui/src/modules/reporting/reporting.tsx
@@ -6,7 +6,7 @@ import { ButtonActionType, MarketData } from 'modules/types';
 import { ReportingModalButton } from 'modules/reporting/common';
 import { ReportingList } from 'modules/reporting/reporting-list';
 import UserRepDisplay from 'modules/reporting/containers/user-rep-display';
-import { TABLET } from "modules/common/constants";
+import { TEMP_TABLET, REPORTING_STATE } from "modules/common/constants";
 import ModuleTabs from "modules/market/components/common/module-tabs/module-tabs";
 import ModulePane from "modules/market/components/common/module-tabs/module-pane";
 
@@ -34,7 +34,7 @@ export default class Reporting extends React.Component<ReportingProps> {
         <Helmet>
           <title>Reporting</title>
         </Helmet>
-        <Media query={TABLET}>
+        <Media query={TEMP_TABLET}>
           {(matches) =>
             matches ? (
               <ModuleTabs selected={0} fillWidth noBorder>
@@ -73,25 +73,25 @@ export default class Reporting extends React.Component<ReportingProps> {
             ) : (
             <>
               <div>
-                <ReportingList 
-                  markets={designatedReporterMarkets}
-                  title={"Designated Reporting"}
+                <ReportingList
+                  reportingType={REPORTING_STATE.DESIGNATED_REPORTING}
+                  title={'Designated Reporting'}
+                  showLoggedOut={showLoggedOut}
                   loggedOutMessage="Connect a wallet to see your markets that are ready for Reporting."
-                  showLoggedOut={!isLogged}
-                  emptyHeader="There are no markets available for you to Report on." 
+                  emptyHeader="There are no markets available for you to Report on."
                   emptySubheader=" Check your Upcoming Designated Reporting to see Markets that will soon be availble to Report on."
                 />
-                <ReportingList 
-                  markets={upcomingMarkets}
-                  title={"Upcoming Designated Reporting"}
+                <ReportingList
+                  reportingType={REPORTING_STATE.PRE_REPORTING}
+                  title={'Upcoming Designated Reporting'}
+                  showLoggedOut={showLoggedOut}
                   loggedOutMessage="Connect a wallet to see your markets that will soon be ready to Report on."
-                  showLoggedOut={!isLogged}
                   emptyHeader="There are no markets coming up in the next week for you to Report on. "
                   emptySubheader="Check your Upcoming Designated Reporting to see Markets that will soon be availble to Report on."
-                />
-                <ReportingList 
-                  markets={openMarkets}
-                  title={"Open Reporting"}
+                /> 
+                <ReportingList
+                  reportingType={REPORTING_STATE.OPEN_REPORTING}
+                  title={'Open Reporting'}
                   emptyHeader="There are currently no markets in Open Reporting. "
                   emptySubheader=" Markets appear here once if a Designated Reporter fails to show up."
                 />
