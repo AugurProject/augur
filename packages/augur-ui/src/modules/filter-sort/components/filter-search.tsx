4eb2d77c1983e5bc517691b29a63503235db18dc
Tom Haile <bradford.haile@gmail.com>
2019-09-05 19:43:16 -0500

    remove hint next to search

--- packages/augur-ui/src/modules/filter-sort/components/filter-search.tsx
+++ packages/augur-ui/src/modules/filter-sort/components/filter-search.tsx
@@ -132,50 +132,6 @@ export default class FilterSearch extends Component<
           onBlur={this.onBlur}
           isLoading={Boolean(isSearchingMarkets && search && search !== '')}
         />
-        <label
-          className={classNames(TooltipStyles.TooltipHint, Styles.Tooltip)}
-          data-tip
-          data-for='tooltip--search-input'
-        >
-          {Hint}
-        </label>
-        <ReactTooltip
-          id='tooltip--search-input'
-          className={TooltipStyles.Tooltip}
-          effect='solid'
-          place='bottom'
-          type='light'
-        >
-          <h4>Search Syntax</h4>
-          <u>Specific field search:</u> prepend the field name:
-          <b> category</b>, <b>title</b>, <b>details</b>, <b>source</b>,{' '}
-          <b>tags</b>
-          <p style={{ color: '#372e4b' }} />
-          <p>
-            Example: <b>title: ethereum</b>, returns markets with ethereum in
-            market title.
-          </p>
-          <p>
-            <u>Exact match:</u> Use a <b>double quoted string</b> to search
-            exact phrase.
-          </p>
-          <p>
-            Example: <b>&#34;price of bitcoin&#34;</b>
-          </p>
-          <p>
-            <u>Either/Or:</u> Use capitalized <b>OR</b> between word(s) and/or
-            phrase(s) to get matches for either word(s) or phrase(s)
-          </p>
-          <p>
-            Example: <b>bitcoin OR ethereum OR litecoin</b>
-          </p>
-          <p>
-            <u>All words:</u> Markets will contain all word(s) and phrase(s)
-          </p>
-          <p>
-            Example: <b>bitcoin ethereum litecoin</b>
-          </p>
-        </ReactTooltip>
       </article>
     );
   }
