eeafff88a7b66c86296dea55fcd99e6442654551
Andrew D'Amelio <dev@andrewdamel.io>
2019-09-07 08:40:15 -0400

    add markets empty state

--- packages/augur-ui/src/modules/common/null-state-message.tsx
+++ packages/augur-ui/src/modules/common/null-state-message.tsx
@@ -1,26 +1,31 @@
-import React from "react";
-import classNames from "classnames";
-import Styles from "modules/common/null-state-message.styles.less";
+import React from 'react';
+import classNames from 'classnames';
+import Styles from 'modules/common/null-state-message.styles.less';
 
 interface NullStateProps {
   message?: string;
-  addNullPadding: Boolean;
+  subMessage?: string;
+  icon?: JSX.Element;
+  addNullPadding?: boolean;
   className?: string;
 }
 const NullStateMessage = (p: NullStateProps) => {
-    return (
-      <article
+  return (
+    <article
       className={classNames(p.className, Styles.NullState, {
-        [`${Styles.Padding}`]: p.addNullPadding
+        [`${Styles.Padding}`]: p.addNullPadding,
       })}
     >
-    <span>{p.message}</span>
+      {p.icon && <span>{p.icon}</span>}
+      <span>{p.message}</span>
+      {p.subMessage && <span>{p.subMessage}</span>}
     </article>
-    )
+  );
 };
 
 NullStateMessage.defaultProps = {
-  message: "No Data Available"
+  message: 'No Data Available',
+  addNullPadding: false,
 };
 
 export default NullStateMessage;
