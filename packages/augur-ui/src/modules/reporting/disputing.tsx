8581f46382c4e7cb2f651bbf28eaa7b615d8705c
JohnDanz <John.Danz87@gmail.com>
2019-09-10 19:39:55 -0500

    started to compose dispute layout

--- packages/augur-ui/src/modules/reporting/disputing.tsx
+++ packages/augur-ui/src/modules/reporting/disputing.tsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import MarketsInDispute from "modules/reporting/containers/markets-in-dispute";
+import { WindowProgress } from "modules/common/progress";
+import UserRepDisplay from "modules/reporting/containers/user-rep-display";
 
 import ParticipationTokensView from 'modules/reporting/containers/participation-tokens-view';
 
 interface DisputingProps {
+  account: object;
+  isLogged: boolean;
   isConnected: boolean;
   loadDisputeWindow: Function;
+  disputeWindow: object;
+  currentTime: number;
 }
-
+// TODO: containerize the WindowProgress for disputing.
+// TODO: fix fitlers dispute Markets List
 export default class Disputing extends Component<DisputingProps> {
   constructor(props) {
     super(props);
@@ -28,8 +35,20 @@ export default class Disputing extends Component<DisputingProps> {
   }
 
   render() {
+    const { disputeWindow, currentTime, isLogged, account } = this.props;
+    const { startTime, endTime } = disputeWindow;
+
     return (
       <section>
+        <UserRepDisplay />
+        <WindowProgress 
+          startTime={startTime}
+          endTime={endTime}
+          currentTime={currentTime}
+          title="Current Dispute Window"
+          description="A few lines of information explaing the purpose of the Dispute Window"
+          countdownLabel="Time Remaining in Window"
+        />
         <ParticipationTokensView />
         <MarketsInDispute />
       </section>
