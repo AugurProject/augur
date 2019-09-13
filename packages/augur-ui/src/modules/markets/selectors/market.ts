f83b3169d87089a03006e25f847cd9d6e8c1fc85
Andrew D'Amelio <dev@andrewdamel.io>
2019-09-13 18:49:25 -0400

    remove unused tag/category code (#3632)

--- packages/augur-ui/src/modules/markets/selectors/market.ts
+++ packages/augur-ui/src/modules/markets/selectors/market.ts
@@ -34,9 +34,6 @@ const assembleMarket = createSelector(
   (marketData): MarketData => {
   const market: MarketData = convertMarketInfoToMarketData(marketData);
 
-  // TODO: tags will get removed with new nav redesign
-  market.tags = (market.tags || []).filter(tag => !!tag);
-
   // TODO: currently where this data comes from is unknown, need to have discussion about architecture.
   market.unclaimedCreatorFees = formatDai(marketData.unclaimedCreatorFees);
   market.marketCreatorFeesCollected = formatDai(
