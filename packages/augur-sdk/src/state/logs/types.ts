50581e752046d405f3680f7d711ab4728b536ca2
Justin Barry <328965+justinbarry@users.noreply.github.com>
2019-09-14 15:00:49 -0700

    Add TokensMinted event UserSyncableDB

--- packages/augur-sdk/src/state/logs/types.ts
+++ packages/augur-sdk/src/state/logs/types.ts
@@ -1,3 +1,5 @@
+import { FormattedEventLog } from "../../event-handlers";
+
 export type Address = string;
 export type Bytes32 = string;
 export type PayoutNumerators = string[];
@@ -311,11 +313,30 @@ export interface TimestampSetLog extends Log, Doc {
   newTimestamp: Timestamp;
 }
 
+export enum TokenType {
+  ReputationToken,
+  ShareToken,
+  DisputeCrowdsourcer,
+  FeeWindow, // No longer a valid type but here for backward compat with Augur Node processing
+  FeeToken, // No longer a valid type but here for backward compat with Augur Node processing
+  ParticipationToken,
+}
+
+export interface TokensMinted extends Log, Doc {
+  universe: Address;
+  token: Address;
+  target: Address;
+  amount: string;
+  tokenType: TokenType;
+  market: Address;
+  totalSupply: string;
+}
+
 export interface TokenBalanceChangedLog extends Log, Doc {
   universe: Address;
   owner: Address;
   token: string;
-  tokenType: string;
+  tokenType: TokenType;
   market: Address;
   balance: string;
 }
