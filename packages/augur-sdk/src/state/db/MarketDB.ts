2fd996372639deb5e7ebadead4a4a67f3d330172
nuevoalex <achap5dk@gmail.com>
2019-09-16 09:22:23 -0700

    add second

--- packages/augur-sdk/src/state/db/MarketDB.ts
+++ packages/augur-sdk/src/state/db/MarketDB.ts
@@ -281,7 +281,7 @@ export class MarketDB extends DerivedDB {
     for (const marketData of eligibleMarketsData) {
       let reportingState: MarketReportingState = null;
       const marketEnd = new BigNumber(marketData.endTime, 16);
-      const openReportingStart = marketEnd.plus(SECONDS_IN_A_DAY);
+      const openReportingStart = marketEnd.plus(SECONDS_IN_A_DAY).plus(1);
 
       if (marketData.nextWindowEndTime && timestamp >= new BigNumber(marketData.nextWindowEndTime, 16).toNumber()) {
         reportingState = MarketReportingState.AwaitingFinalization;
