d7fde125c1a7a5de18012c7c1f68a64fb5178bc0
Justin Barry <328965+justinbarry@users.noreply.github.com>
2019-09-17 17:19:33 -0700

    Market getter disputeInfo fixes
    * Add DisputeCrowdSourcerCreated and DisputeCrowdSourcerContribution to market derived db
    * Procress DisputeCrowdsourcerCreated log in dispute db
    * Fix identification of tentativeWinning outcome
    * Fix initial report subitted dispute round

--- packages/augur-sdk/src/state/db/DisputeDB.ts
+++ packages/augur-sdk/src/state/db/DisputeDB.ts
@@ -6,7 +6,6 @@ import { ParsedLog } from '@augurproject/types';
  * DB to store current outcome stake and dispute related information
  */
 export class DisputeDatabase extends DerivedDB {
-
   constructor(
     db: DB,
     networkId: number,
@@ -18,21 +17,21 @@ export class DisputeDatabase extends DerivedDB {
 
     this.db.createIndex({
       index: {
-        ddoc: "marketIndex",
-        fields: ["market"],
+        ddoc: 'marketIndex',
+        fields: ['market'],
       },
     });
   }
 
   protected processDoc(log: ParsedLog): ParsedLog {
     if (log.name === 'InitialReportSubmitted') {
-        return this.processInitialReportSubmitted(log);
+      return this.processInitialReportSubmitted(log);
+    } else if (log.name === 'DisputeCrowdsourcerCreated') {
+      return this.processDisputeCrowdsourcerCreated(log);
+    } else if (log.name === 'DisputeCrowdsourcerContribution') {
+      return this.processDisputeCrowdsourcerContribution(log);
     } else if (log.name === 'DisputeCrowdsourcerCompleted') {
-        return this.processDisputeCrowdsourcerCompleted(log);
-    } else if (log.name === 'InitialDisputeCrowdsourcerContribution') {
-        return this.processDisputeCrowdsourcerContribution(log);
-    } else if (log.name === 'DisputeCrowdsourcerCompleted') {
-        return this.processDisputeCrowdsourcerCompleted(log);
+      return this.processDisputeCrowdsourcerCompleted(log);
     }
     return log;
   }
@@ -40,8 +39,8 @@ export class DisputeDatabase extends DerivedDB {
   private processInitialReportSubmitted(log: ParsedLog): ParsedLog {
     log['stakeCurrent'] = '0x0';
     log['stakeRemaining'] = '0x0';
-    log['tentativeWinningOnRound'] = '0x1';
     log['totalRepStakedInPayout'] = log['amountStaked'];
+    log['disputeRound'] = '0x01';
     return log;
   }
 
@@ -53,14 +52,13 @@ export class DisputeDatabase extends DerivedDB {
   }
 
   private processDisputeCrowdsourcerContribution(log: ParsedLog): ParsedLog {
-    log['stakeCurrent'] = log['currentStake']
+    log['stakeCurrent'] = log['currentStake'];
     return log;
   }
 
   private processDisputeCrowdsourcerCompleted(log: ParsedLog): ParsedLog {
     log['stakeCurrent'] = '0x0';
     log['stakeRemaining'] = '0x0';
-    log['tentativeWinningOnRound'] = log['disputeRound'];
     return log;
   }
 }
