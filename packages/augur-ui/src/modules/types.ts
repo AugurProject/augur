56469885b459e963493a617393167508e729d0ce
Tom Haile <bradford.haile@gmail.com>
2019-09-18 19:18:54 -0500

    convert to big number, serialization issue

--- packages/augur-ui/src/modules/types.ts
+++ packages/augur-ui/src/modules/types.ts
@@ -317,8 +317,10 @@ export interface NewMarket {
   affiliateFee: number;
   orderBook: {[outcome: number]: Array<LiquidityOrder> };
   orderBookSorted: {[outcome: number]: Array<LiquidityOrder> };
-  initialLiquidityDai: any; // TODO: big number type
-  initialLiquidityGas: any; // TODO: big number type
+  minPriceBigNumber: BigNumber;
+  maxPriceBigNumber: BigNumber;
+  initialLiquidityDai: BigNumber;
+  initialLiquidityGas: BigNumber;
   creationError: string;
   offsetName: string;
   offset: number;
