aa98a947873a3997749664b3a1e7679ece9b23af
adrake33 <aarondrake@gmail.com>
2019-09-19 17:44:13 -0700

    Update CLAIM_MARKETS_PROCEEDS_GAS_ESTIMATE

--- packages/augur-ui/src/modules/common/constants.ts
+++ packages/augur-ui/src/modules/common/constants.ts
@@ -658,8 +658,7 @@ export const TEN_TO_THE_EIGHTEENTH_POWER = TEN.exponentiatedBy(18);
 export const MIN_QUANTITY = createBigNumber('0.00000001');
 export const NEW_ORDER_GAS_ESTIMATE = createBigNumber(700000);
 export const NEW_MARKET_GAS_ESTIMATE = createBigNumber(2000000);
-// TODO: Get actual gas cost from augur-core
-export const CLAIM_MARKETS_PROCEEDS_GAS_ESTIMATE = createBigNumber(1000000); // Estimate for claiming proceeds from a categorical market (worst-case gas cost)
+export const CLAIM_MARKETS_PROCEEDS_GAS_ESTIMATE = createBigNumber(1121349); // Gas cost for claiming proceeds from a categorical market with 8 outcomes (worst-case gas cost)
 export const CLAIM_MARKETS_PROCEEDS_GAS_LIMIT = createBigNumber(3000000);
 export const MAX_BULK_CLAIM_MARKETS_PROCEEDS_COUNT = Math.floor(
   createBigNumber(CLAIM_MARKETS_PROCEEDS_GAS_LIMIT)
