64860c6a96c5910810c7cc7a237ac7396dbb5874
phoebemirman <phoebe.mirman@gmail.com>
2019-09-23 12:32:04 -0700

    outcomes list and mobile order form

--- packages/augur-ui/src/modules/market/components/market-outcomes-list/market-outcomes-list.tsx
+++ packages/augur-ui/src/modules/market/components/market-outcomes-list/market-outcomes-list.tsx
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import classNames from "classnames";
+import React, { Component } from 'react';
+import classNames from 'classnames';
 
-import { SCALAR } from "modules/common/constants";
-import MarketOutcomesListOutcome from "modules/market/containers/market-outcome";
-import MarketScalarOutcomeDisplay from "modules/market/components/market-scalar-outcome-display/market-scalar-outcome-display";
+import { SCALAR } from 'modules/common/constants';
+import MarketOutcomesListOutcome from 'modules/market/containers/market-outcome';
+import MarketScalarOutcomeDisplay from 'modules/market/components/market-scalar-outcome-display/market-scalar-outcome-display';
 
-import Styles from "modules/market/components/market-outcomes-list/market-outcomes-list.styles.less";
-import SharedStyles from "modules/market/components/market-orders-positions-table/open-orders-table.styles.less";
-import HeaderStyles from "modules/portfolio/components/common/data-table-header.styles.less";
-import { OutcomeFormatted } from "modules/types";
+import Styles from 'modules/market/components/market-outcomes-list/market-outcomes-list.styles.less';
+import SharedStyles from 'modules/market/components/market-orders-positions-table/open-orders-table.styles.less';
+import HeaderStyles from 'modules/portfolio/components/common/data-table-header.styles.less';
+import { OutcomeFormatted } from 'modules/types';
 
 interface MarketOutcomesListProps {
   outcomesFormatted: OutcomeFormatted[];
@@ -22,7 +22,9 @@ interface MarketOutcomesListProps {
   popUp: boolean;
 }
 
-export default class MarketOutcomesList extends Component<MarketOutcomesListProps> {
+export default class MarketOutcomesList extends Component<
+  MarketOutcomesListProps
+> {
   static defaultProps = {
     selectedOutcomeId: 2,
     scalarDenomination: null,
@@ -46,16 +48,23 @@ export default class MarketOutcomesList extends Component<MarketOutcomesListProp
 
     return (
       <section className={Styles.OutcomesList}>
-        {!popUp && (
-          <h3 className={Styles.Heading}>Outcomes</h3>
-        )}
+        {!popUp && <h3 className={Styles.Heading}>Outcomes</h3>}
         <div className={classNames(SharedStyles.Table, SharedStyles.Outcomes)}>
-          <ul className={classNames(HeaderStyles.DataTableHeader, HeaderStyles.OutcomesHeader)}>
+          <ul
+            className={classNames(
+              HeaderStyles.DataTableHeader,
+              HeaderStyles.OutcomesHeader
+            )}
+          >
             <li>Outcome</li>
-            <li>Bid Qty</li>
+            <li>
+              Bid Qty
+            </li>
             <li>Best Bid</li>
             <li>Best Ask</li>
-            <li>Ask Qty</li>
+            <li>
+              Ask Qty
+            </li>
             <li>Last</li>
           </ul>
           {outcomesFormatted.map(outcome => (
@@ -66,12 +75,9 @@ export default class MarketOutcomesList extends Component<MarketOutcomesListProp
               selectedOutcomeId={selectedOutcomeId}
               updateSelectedOutcome={updateSelectedOutcome}
               marketType={marketType}
-              scalarDenomination={
-                marketType === SCALAR && scalarDenomination
-              }
+              scalarDenomination={marketType === SCALAR && scalarDenomination}
             />
-          ))
-          }
+          ))}
         </div>
         {marketType === SCALAR && (
           <MarketScalarOutcomeDisplay
