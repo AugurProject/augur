05b1cde0f25d53a8674ac9afb3d4992a70aa7999
adrake33 <aarondrake@gmail.com>
2019-10-02 09:55:35 -0700

    Remove commented code from action

--- packages/augur-ui/src/modules/forking/actions/migrate-market-through-one-fork.ts
+++ packages/augur-ui/src/modules/forking/actions/migrate-market-through-one-fork.ts
@@ -8,29 +8,6 @@ export const migrateMarketThroughOneFork = (
   estimateGas = false,
   callback = logError
 ) => (dispatch, getState) => {
-  const { loginAccount } = getState();
-console.log('in migrateThroughFork');
-  // augur.api.Market.migrateThroughOneFork({
-  //   tx: {
-  //     to: marketId,
-  //     estimateGas,
-  //   },
-  //   meta: loginAccount.meta,
-  //   onSent: () => {
-  //     // if we aren't estimatingGas, close the modal once the transaction is sent.
-  //     if (!estimateGas) dispatch(closeModal());
-  //   },
-  //   onSuccess: res => {
-  //     if (estimateGas) {
-  //       // if just a gas estimate, return the gas cost.
-  //       const gasPrice = getGasPrice(getState());
-  //       return callback(
-  //         null,
-  //         formatGasCostToEther(res, { decimalsRounded: 4 }, gasPrice)
-  //       );
-  //     }
-  //     return callback(null, res);
-  //   },
-  //   onFailed: err => callback(err),
-  // });
+  // TBD
+  console.log('in migrateThroughFork');
 };
