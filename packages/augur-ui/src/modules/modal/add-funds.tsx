eaa47187c1fbf88e689bc599ad91be2d9be95e93
Andrew D'Amelio <dev@andrewdamel.io>
2019-10-04 16:47:18 -0400

    add funds updates

--- packages/augur-ui/src/modules/modal/add-funds.tsx
+++ packages/augur-ui/src/modules/modal/add-funds.tsx
@@ -7,19 +7,51 @@ import {
   BackButton,
 } from 'modules/common/buttons';
 import { AccountAddressDisplay, FundsHelp } from 'modules/modal/common';
-import formatAddress from 'modules/auth/helpers/format-address';
 
 import Styles from 'modules/modal/modal.styles.less';
 import { RadioTwoLineBarGroup, TextInput } from 'modules/common/form';
 import classNames from 'classnames';
+import { ACCOUNT_TYPES } from 'modules/common/constants';
 
 interface AddFundsProps {
   closeAction: Function;
   address: string;
+  accountMeta: LoginAccount['meta'];
 }
 
-export const AddFunds = ({ closeAction, address }: AddFundsProps) => {
+export const AddFunds = ({
+  closeAction,
+  accountMeta,
+  address,
+}: AddFundsProps) => {
   const [selectedOption, setSelectedOption] = useState(null);
+  console.log('accountMeta', accountMeta);
+
+  let addFundsOptions = [
+    {
+      header: 'Credit/debit card',
+      description: 'Add Funds instantly using a credit/debit card',
+      value: '0',
+    },
+    {
+      header: 'Coinbase',
+      description: 'Add funds using a Coinbase account',
+      value: '1',
+    },
+    {
+      header: 'Transfer',
+      description: 'Transfer funds to your account address',
+      value: '2',
+    },
+  ];
+
+  if (
+    accountMeta.accountType !== ACCOUNT_TYPES.TORUS &&
+    accountMeta.accountType !== ACCOUNT_TYPES.PORTIS
+  ) {
+    addFundsOptions = addFundsOptions.slice(1, 3);
+  }
+
   return (
     <div
       className={classNames(Styles.AddFunds, {
@@ -34,23 +66,7 @@ export const AddFunds = ({ closeAction, address }: AddFundsProps) => {
           <h1>Add Funds</h1>
           <h2>Choose a method</h2>
           <RadioTwoLineBarGroup
-            radioButtons={[
-              {
-                header: 'Credit/debit card',
-                description: 'Add Funds instantly using a credit/debit card',
-                value: '0',
-              },
-              {
-                header: 'Coinbase',
-                description: 'Add funds using a Coinbase account',
-                value: '1',
-              },
-              {
-                header: 'Transfer',
-                description: 'Transfer funds to your account address',
-                value: '2',
-              },
-            ]}
+            radioButtons={addFundsOptions}
             defaultSelected={selectedOption}
             hideRadioButton
             onChange={value => {
@@ -69,15 +85,47 @@ export const AddFunds = ({ closeAction, address }: AddFundsProps) => {
           {selectedOption === '0' && (
             <>
               <h1>Credit/debit card</h1>
-              <h2>Add up to $1,000 worth of DAI instantly</h2>
-              <h3>Amount</h3>
-              <TextInput placeholder="0" onChange={null} innerLabel="REP" />
-              <PrimaryButton action={null} text="Buy with Portis" />
-              <h4>
-                You will be taken to Portis’ website to finalise the purchase
-                process. The funds will appear in your Augur account address
-                when complete.
-              </h4>
+              {accountMeta.accountType === ACCOUNT_TYPES.PORTIS && (
+                <h2>Add up to $250 worth of DAI/ETH instantly</h2>
+              )}
+              {accountMeta.accountType === ACCOUNT_TYPES.TORUS && (
+                <h2>Add DAI/ETH instantly</h2>
+              )}
+
+              {accountMeta.accountType === ACCOUNT_TYPES.PORTIS && (
+                <h3>Amount</h3>
+              )}
+              {accountMeta.accountType === ACCOUNT_TYPES.PORTIS && (
+                <TextInput placeholder="0" onChange={null} innerLabel="DAI" />
+              )}
+              {accountMeta.accountType === ACCOUNT_TYPES.PORTIS && (
+                <a href="https://wallet.portis.io/buy/" target="_blank">
+                  <PrimaryButton
+                    action={null}
+                    text={`Buy with ${accountMeta.accountType}`}
+                  />
+                </a>
+              )}
+              {accountMeta.accountType === ACCOUNT_TYPES.TORUS && (
+                <PrimaryButton
+                  action={() => accountMeta.openWallet('topup')}
+                  text={`Buy with ${accountMeta.accountType}`}
+                />
+              )}
+              {accountMeta.accountType === ACCOUNT_TYPES.PORTIS && (
+                <h4>
+                  You will be taken to Portis’ website to finalise the purchase
+                  process. The funds will appear in your Augur account address
+                  when complete.
+                </h4>
+              )}
+
+              {accountMeta.accountType === ACCOUNT_TYPES.TORUS && (
+                <h4>
+                  Open the Tor.us wallet to start the purchase process. The
+                  funds will appear in your Augur account address when complete.
+                </h4>
+              )}
             </>
           )}
           {selectedOption === '1' && (
@@ -95,10 +143,7 @@ export const AddFunds = ({ closeAction, address }: AddFundsProps) => {
                 <li>Send the DAI to your account address</li>
               </ol>
               <h3>Your Account Address</h3>
-              <AccountAddressDisplay
-                copyable
-                address={address ? formatAddress(address) : '-'}
-              />
+              <AccountAddressDisplay copyable address={address} />
             </>
           )}
           {selectedOption === '2' && (
@@ -112,10 +157,7 @@ export const AddFunds = ({ closeAction, address }: AddFundsProps) => {
                 <li>Transfer the DAI to your account address</li>
               </ol>
               <h3>Your Account Address</h3>
-              <AccountAddressDisplay
-                copyable
-                address={address ? formatAddress(address) : '-'}
-              />
+              <AccountAddressDisplay copyable address={address} />
               <ExternalLinkButton label="popular services for buying dai" />
             </>
           )}
