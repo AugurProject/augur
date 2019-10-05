07e6bd14c8ebec39b2a6fc1b409282ec3a7214aa
adrake33 <aarondrake@gmail.com>
2019-10-05 14:53:17 -0700

    Fix condition

--- packages/augur-ui/src/modules/auth/containers/connect-dropdown.ts
+++ packages/augur-ui/src/modules/auth/containers/connect-dropdown.ts
@@ -18,7 +18,7 @@ const mapStateToProps = state => {
   return {
     universeOutcomeName: state.universe.outcomeName ? state.universe.outcomeName : null,
     parentUniverseId: state.universe.parentId ? state.universe.parentId : null,
-    universeHasChildren: state.universe.children.length > 0 ? true : false,
+    universeHasChildren: state.universe.children && state.universe.children.length > 0 ? true : false,
     loginAccountAddress: state.loginAccount.address,
     userDefinedGasPrice: userDefined,
     gasPriceSpeed,
