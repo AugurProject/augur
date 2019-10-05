7948331a34046f6289d1683e1a5e1d0c8ca58b22
adrake33 <aarondrake@gmail.com>
2019-10-05 13:49:44 -0700

    Fix loading of universe details

--- packages/augur-ui/src/modules/auth/containers/connect-account.ts
+++ packages/augur-ui/src/modules/auth/containers/connect-account.ts
@@ -4,7 +4,6 @@ import {
   updateAuthStatus,
   IS_CONNECTION_TRAY_OPEN,
 } from 'modules/auth/actions/auth-status';
-import { loadUniverseDetails } from 'modules/universe/actions/load-universe-details';
 import { updateMobileMenuState } from 'modules/app/actions/update-sidebar-status';
 
 const mapStateToProps = state => ({
@@ -19,23 +18,9 @@ const mapDispatchToProps = dispatch => ({
   updateMobileMenuState: data => dispatch(updateMobileMenuState(data)),
   updateConnectionTray: value =>
     dispatch(updateAuthStatus(IS_CONNECTION_TRAY_OPEN, value)),
-  loadUniverseDetails: (universe, account) =>
-    dispatch(loadUniverseDetails(universe, account))
 });
-
-const mergeProps = (sP: any, dP: any, oP: any) => {
-  if (sP.isLogged) {
-    dP.loadUniverseDetails(sP.universeId, sP.userInfo.address);
-  }
-  return {
-    ...oP,
-    ...sP,
-    ...dP,
-  };
-}
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-  mergeProps
 )(ConnectAccount);
