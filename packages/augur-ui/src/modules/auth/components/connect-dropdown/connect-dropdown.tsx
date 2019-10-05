62ea77d943169a78ae3be2663fdda355c95c1ec4
adrake33 <aarondrake@gmail.com>
2019-10-05 14:27:50 -0700

    Fix var name

--- packages/augur-ui/src/modules/auth/components/connect-dropdown/connect-dropdown.tsx
+++ packages/augur-ui/src/modules/auth/components/connect-dropdown/connect-dropdown.tsx
@@ -46,7 +46,7 @@ const ConnectDropdown = (props: ConnectDropdownProps) => {
     showAddFundsModal,
     universeSelectorModal,
     universeOutcomeName,
-    parentUniverse,
+    parentUniverseId,
     universeHasChildren,
   } = props;
 
