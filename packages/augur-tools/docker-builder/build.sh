3716a83033e537d73c373ad8ad03897279b70e18
Peter Hicks <petong@jah.net>
2019-10-08 12:24:49 -0700

    docker fixes to deploy in docker

--- packages/augur-tools/docker-builder/build.sh
+++ packages/augur-tools/docker-builder/build.sh
@@ -8,17 +8,19 @@ CONTRACT_SHA=$(scripts/get-contract-hashes.js)
 # Build and copy to merge with current address.json/upload-block-numbers.json
 IMAGE_NAME=augurproject/dev-pop-geth-v2
 docker build --no-cache=true -f docker-builder/Dockerfile --build-arg normal_time=false --build-arg network_id=102 -t $IMAGE_NAME $MONO_ROOT
-scripts/copy-docker-files.sh $IMAGE_NAME
+#scripts/copy-docker-files.sh $IMAGE_NAME
 docker tag $IMAGE_NAME $IMAGE_NAME:$CONTRACT_SHA
+
+exit 0
 
 # Build and copy to merge with updated address.json/block_numbers.json
 IMAGE_NAME=augurproject/dev-pop-normtime-geth-v2
 docker build --no-cache=true -f docker-builder/Dockerfile --build-arg normal_time=true --build-arg network_id=103 -t $IMAGE_NAME $MONO_ROOT
-scripts/copy-docker-files.sh $IMAGE_NAME
+#scripts/copy-docker-files.sh $IMAGE_NAME
 docker tag $IMAGE_NAME $IMAGE_NAME:$CONTRACT_SHA
 
 # Build and copy to merge with current address.json/block_numbers.json
 IMAGE_NAME=augurproject/dev-pop-geth-15-v2
 docker build --no-cache=true -f docker-builder/Dockerfile --build-arg normal_time=false --build-arg network_id=104 --build-arg period_time=15 -t $IMAGE_NAME $MONO_ROOT
-scripts/copy-docker-files.sh $IMAGE_NAME
+#scripts/copy-docker-files.sh $IMAGE_NAME
 docker tag $IMAGE_NAME $IMAGE_NAME:$CONTRACT_SHA
