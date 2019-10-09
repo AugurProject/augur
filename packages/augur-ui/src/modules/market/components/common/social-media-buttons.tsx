8f14b68456cc6535d438647c0fbacfc647fbfe34
Tom Haile <bradford.haile@gmail.com>
2019-10-08 20:08:08 -0500

    converted component to functional, no lifecycle methods are used it can be a functional

--- packages/augur-ui/src/modules/market/components/common/social-media-buttons.tsx
+++ packages/augur-ui/src/modules/market/components/common/social-media-buttons.tsx
@@ -1,48 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import {
-  FacebookButton,
-  TwitterButton
-} from 'modules/common/icons';
-
-import Styles from 'modules/market/components/common/market-common.styles.less';
+import { FacebookButton, TwitterButton } from 'modules/common/icons';
 
 interface SocialMediaButtonsProps {
   marketDescription: string;
   marketAddress: string;
 }
 
-export class SocialMediaButtons extends Component<SocialMediaButtonsProps> {
-  showFacebookShare(encodedMarketUrl, encodedMarketDescription) {
+export const SocialMediaButtons = (props: SocialMediaButtonsProps) => {
+  const showFacebookShare = (encodedMarketUrl, encodedMarketDescription) => {
     const url = `https://www.facebook.com/sharer/sharer.php?t=${encodedMarketDescription}&u=${encodedMarketUrl}`;
-    window.open(url, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+    window.open(
+      url,
+      '',
+      'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600'
+    );
     return false;
-  }
+  };
 
-  showTwitterShare(encodedMarketUrl, encodedMarketDescription) {
+  const showTwitterShare = (encodedMarketUrl, encodedMarketDescription) => {
     const url = `https://twitter.com/intent/tweet?text=${encodedMarketDescription}&url=${encodedMarketUrl}`;
     window.open(url, '', 'width=600,height=300');
     return false;
-  }
+  };
 
-  render() {
-    const encodedMarketUrl = encodeURIComponent(`${window.location.origin}/#!/market?id=${this.props.marketAddress}`);
-    const encodedMarketDescription = encodeURI(this.props.marketDescription);
-    return (
-      <>
-        <button
-          id='facebookButton'
-          onClick={() => this.showFacebookShare(encodedMarketUrl, encodedMarketDescription)}
-        >
-          {FacebookButton}
-        </button>
-        <button
-          id='twitterButton'
-          onClick={() => this.showTwitterShare(encodedMarketUrl, encodedMarketDescription)}
-        >
-          {TwitterButton}
-        </button>
-      </>
-    );
-  }
-}
+  const encodedMarketUrl = encodeURIComponent(
+    `${window.location.origin}/#!/market?id=${props.marketAddress}`
+  );
+  const encodedMarketDescription = encodeURI(props.marketDescription);
+  return (
+    <>
+      <button
+        id="facebookButton"
+        onClick={() =>
+          showFacebookShare(encodedMarketUrl, encodedMarketDescription)
+        }
+      >
+        {FacebookButton}
+      </button>
+      <button
+        id="twitterButton"
+        onClick={() =>
+          showTwitterShare(encodedMarketUrl, encodedMarketDescription)
+        }
+      >
+        {TwitterButton}
+      </button>
+    </>
+  );
+};
