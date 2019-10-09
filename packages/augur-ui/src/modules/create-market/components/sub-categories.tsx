9992357ecd417be215508fd2a2fb41fe75a7aaf5
Tom Haile <bradford.haile@gmail.com>
2019-10-09 10:46:30 -0500

    remove teriary dropdown in templates, auto forward when user selects template sub category

--- packages/augur-ui/src/modules/create-market/components/sub-categories.tsx
+++ packages/augur-ui/src/modules/create-market/components/sub-categories.tsx
@@ -1,34 +1,18 @@
 import React from 'react';
 import {
-  CategorySingleSelect,
-  createGroups,
-} from 'modules/common/form';
-import {
   LargeSubheaders,
   SmallHeaderLink,
 } from 'modules/create-market/components/common';
-import { MARKET_SUB_TEMPLATES } from 'modules/create-market/constants';
 import { RadioCardGroup } from 'modules/common/form';
 import Styles from 'modules/create-market/components/sub-categories.styles.less';
-import { CUSTOM } from 'modules/common/constants';
-import { setCategories } from 'modules/create-market/set-categories';
 import { getTemplateRadioCards } from '../get-template';
 
 export const SubCategories = ({
     newMarket,
     updateNewMarket,
+    nextPage
   }) => {
   const { categories } = newMarket;
-  const {
-    tertiaryOptions,
-    tertiaryAutoComplete,
-  } = createGroups(setCategories, categories, categories);
-
-  const tertiarySelected = categories[2].length > 0;
-  const categorySelected = tertiaryOptions.map(options => options.value).includes(categories[2]);
-  const isCustomSelected = tertiaryOptions.length === 0 || !categorySelected && tertiaryOptions.length > 0;
-  const customOption = { label: CUSTOM, value: CUSTOM };
-
   return (
     <section className={Styles.SubCategories}>
       <LargeSubheaders
@@ -43,6 +27,7 @@ export const SubCategories = ({
             updatedNewMarket.categories[1] = value;
             updatedNewMarket.categories[2] = '';
             updateNewMarket(updatedNewMarket);
+            nextPage();
           }}
           radioButtons={getTemplateRadioCards({
             primary: newMarket.categories[0],
@@ -52,33 +37,6 @@ export const SubCategories = ({
         >
           <SmallHeaderLink text="Don't see your category?" link ownLine />
         </RadioCardGroup>
-      </section>
-      <section>
-        <LargeSubheaders
-          header="Choose a further sub-category"
-          subheader="Optionally select another sub-category to help users find your market."
-        />
-        <CategorySingleSelect
-          options={
-            tertiaryOptions.length > 0 ? tertiaryOptions : [customOption]
-          }
-          autoCompleteList={tertiaryAutoComplete}
-          initialSelected={isCustomSelected ? CUSTOM : categories[2]}
-          initialValue={
-            isCustomSelected
-              ? tertiarySelected
-                ? categories[2]
-                : ''
-              : categories[2]
-          }
-          staticLabel="Tertiary Category (optional)"
-          placeholder="Custom Tertiary Category"
-          updateSelection={(value: string) => {
-            const updatedNewMarket = { ...newMarket };
-            updatedNewMarket.categories[2] = value;
-            updateNewMarket(updatedNewMarket);
-          }}
-        />
       </section>
     </section>
   );
