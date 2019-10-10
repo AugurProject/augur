b307e7b22efda4684de82eb6dd8cf5c703f9da3b
adrake33 <aarondrake@gmail.com>
2019-10-09 17:27:56 -0700

    Checkpoint

--- packages/augur-ui/src/modules/common/global-chat.tsx
+++ packages/augur-ui/src/modules/common/global-chat.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import * as React from "react";
 
 import { SecondaryButton } from 'modules/common/buttons';
-import { Chevron } from 'modules/common/icons';
+import { Chevron, Close } from 'modules/common/icons';
 
 import Styles from 'modules/common/global-chat.styles.less';
 
-interface GlobalChatProps {
+export interface GlobalChatProps {
+  toggleGlobalChat: Function;
 }
 
-export const GlobalChat = (props: GlobalChatProps) => {
-  const toggleGlobalChat = () => {
+export interface GlobalChatState {
+  isOpen: boolean;
+}
+
+export class GlobalChat extends React.Component<GlobalChatProps, GlobalChatState> {
+  state: GlobalChatState = {
+    isOpen: false
   };
 
-  return (
-    <div className={Styles.GlobalChat}>
-      <SecondaryButton
-        action={() => toggleGlobalChat()}
-        text='GLOBAL CHAT'
-        icon={Chevron}
-      />
-   </div>
-  );
+  toggleGlobalChat = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
+  render() {
+    const numberOfPeers = 15; // Hard-code this for now
+    return (
+      <div ref='global-chat' className={Styles.GlobalChat}>
+        {!this.state.isOpen &&
+          <SecondaryButton
+            action={() => this.toggleGlobalChat()}
+            text='Global Chat'
+            icon={Chevron}
+          />
+        }
+        {this.state.isOpen &&
+          <div>
+            <div>
+              <span>Global Chat</span>
+              <span>{numberOfPeers} Peers</span>
+              <button onClick={() => this.toggleGlobalChat()}>
+                {Close}
+              </button>
+            </div>
+            <div>
+              Chat content goes here.
+            </div>
+            <div>
+              <textarea placeholder='Type your message...' rows='2'></textarea>
+            </div>
+          </div>
+        }
+      </div>
+    );
+  }
 };
