4a0fb837f3209b03cd730212aaa39f27d69ec335
phoebemirman <phoebe.mirman@gmail.com>
2019-10-16 12:46:37 -0700

    push up stuff

--- packages/augur-ui/src/modules/create-market/components/common.tsx
+++ packages/augur-ui/src/modules/create-market/components/common.tsx
@@ -304,6 +304,52 @@ interface TimeSelectorParams {
   meridiem?: string;
 }
 
+interface DatePickerSelectorProps {
+  setEndTime?: number;
+  onChange: Function;
+  currentTimestamp: number;
+  errrorMessage?: string;
+  placeholder?: string;
+}
+
+export const DatePickerSelector = (props: DatePickerSelectorProps) => {
+    const {
+      setEndTime,
+      onChange,
+      currentTimestamp,
+      errorMessage,
+      placeholder
+    } = props;
+  
+    const [dateFocused, setDateFocused] = useState(false);
+  
+    return (
+      <DatePicker
+        date={setEndTime ? moment(setEndTime * 1000) : null}
+        placeholder={placeholder}
+        displayFormat="MMM D, YYYY"
+        id="input-date"
+        onDateChange={(date: Moment) => {
+          if (!date) return onChange('setEndTime', '');
+          onChange(date.startOf('day').unix());
+        }}
+        isOutsideRange={day =>
+          day.isAfter(moment(currentTimestamp * 1000).add(6, 'M')) ||
+          day.isBefore(moment(currentTimestamp * 1000))
+        }
+        numberOfMonths={1}
+        onFocusChange={({ focused }) => {
+          if (setEndTime === null) {
+            onChange(currentTimestamp);
+          }
+          setDateFocused(() => focused);
+        }}
+        focused={dateFocused}
+        errorMessage={errorMessage}
+      />
+  };
+}
+
 export const DateTimeSelector = (props: DateTimeSelectorProps) => {
   const {
     setEndTime,
@@ -630,10 +676,11 @@ interface InputFactoryProps {
   inputIndex: number;
   onChange: Function;
   newMarket: NewMarket;
+  currentTimestamp: string;
 }
 
 export const InputFactory = (props: InputFactoryProps) => {
-  const { input, inputIndex, onChange, newMarket } = props;
+  const { input, inputIndex, onChange, newMarket, currentTimestamp } = props;
 
   const { template, outcomes, marketType, validations } = newMarket;
 
@@ -693,6 +740,18 @@ export const InputFactory = (props: InputFactoryProps) => {
           onChange('outcomes', newOutcomes);
         }}
         value={input.userInput}
+      />
+    );
+  } else if (input.type === TemplateInputType.DATEYEAR) {
+    return (
+      <DatePickerSelector 
+        onChange={(value) => {
+          updateData(value);
+        }} 
+        currentTimestamp={currentTimestamp} 
+        placeholder={input.placeholder} 
+        setEndTime={input.userInput} 
+        errorMessage={validations.inputs[inputIndex]} 
       />
     );
   } else if (input.type === TemplateInputType.DATETIME) {
@@ -873,7 +932,7 @@ export interface QuestionBuilderProps {
 }
 
 export const QuestionBuilder = (props: QuestionBuilderProps) => {
-  const { onChange, newMarket } = props;
+  const { onChange, newMarket, currentTimestamp } = props;
   const { template, outcomes, marketType, validations } = newMarket;
   const question = template.question.split(' ');
   const inputs = template.inputs;
@@ -909,6 +968,7 @@ export const QuestionBuilder = (props: QuestionBuilderProps) => {
                   inputIndex={inputIndex}
                   onChange={onChange}
                   newMarket={newMarket}
+                  currentTimestamp={currentTimestamp}
                 />
               );
             }
@@ -920,7 +980,7 @@ export const QuestionBuilder = (props: QuestionBuilderProps) => {
           newMarket={newMarket}
           onChange={onChange}
           input={inputs[dateTimeIndex]}
-          currentTime={props.currentTime}
+          currentTime={currentTimestamp}
           template={template}
           inputIndex={dateTimeIndex}
         />
