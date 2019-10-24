647c9e4459ab98b351448b0d66f4d7fbcd26b42a
phoebemirman <phoebe.mirman@gmail.com>
2019-10-24 11:29:24 -0700

    add new input types

--- packages/augur-ui/src/modules/common/validations.ts
+++ packages/augur-ui/src/modules/common/validations.ts
@@ -182,7 +182,9 @@ export function checkForUserInputFilled(inputs) {
       return 'Must enter a valid number';
     } else if (
       (input.type === TemplateInputType.TEXT ||
+        input.type === TemplateInputType.DROPDOWN_TEXT ||
         input.type === TemplateInputType.USER_DESCRIPTION_OUTCOME ||
+        input.type === TemplateInputType.USER_DESCRIPTION_DROPDOWN_OUTCOME ||
         input.type === TemplateInputType.DATEYEAR ||
         input.type === TemplateInputType.DROPDOWN ||
         input.type === TemplateInputType.DENOMINATION_DROPDOWN) &&
