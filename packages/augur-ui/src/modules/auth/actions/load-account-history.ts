543ec312a9847516717f88c3bbff6e20a09c7f77
Tom Haile <bradford.haile@gmail.com>
2019-10-25 16:51:16 -0500

    the start of user data getter

--- packages/augur-ui/src/modules/auth/actions/load-account-history.ts
+++ packages/augur-ui/src/modules/auth/actions/load-account-history.ts
@@ -1,7 +1,7 @@
 import { loadAllAccountPositions } from 'modules/positions/actions/load-account-positions';
 import { loadAccountOpenOrders } from 'modules/orders/actions/load-account-open-orders';
 import { loadCreateMarketHistory } from 'modules/markets/actions/load-create-market-history';
-import { loadUserFilledOrders } from 'modules/markets/actions/market-trading-history-management';
+import { loadUserFilledOrders, updateUserFilledOrders, bulkMarketTradingHistory } from 'modules/markets/actions/market-trading-history-management';
 import { clearTransactions } from 'modules/transactions/actions/update-transactions-data';
 import { loadMarketsInfoIfNotLoaded } from 'modules/markets/actions/load-markets-info';
 import { ThunkDispatch, ThunkAction } from 'redux-thunk';
@@ -10,6 +10,8 @@ import { NodeStyleCallback } from 'modules/types';
 import { AppState } from 'store';
 import { loadAccountReportingHistory } from 'modules/auth/actions/load-account-reporting';
 import { loadDisputeWindow } from 'modules/auth/actions/load-dispute-window';
+import { augurSdk } from 'services/augursdk';
+import { Getters } from '@augurproject/sdk';
 
 export const loadAccountHistory = (): ThunkAction<any, any, any, any> => (
   dispatch: ThunkDispatch<void, any, Action>,
@@ -19,18 +21,22 @@ export const loadAccountHistory = (): ThunkAction<any, any, any, any> => (
   loadTransactions(dispatch, getState(), () => {});
 };
 
-function loadTransactions(
+async function loadTransactions(
   dispatch: ThunkDispatch<void, any, Action>,
   appState: AppState,
   callback: NodeStyleCallback
 ) {
+  const { universe, loginAccount } = appState;
+  const { address } = loginAccount;
   dispatch(loadDisputeWindow()); // need to load dispute window for user to claim reporting fees
   dispatch(loadCreateMarketHistory()); // uses getMarkets so marketInfo objects are already returned
   const options = {};
   const promises = [];
-  promises.push(
-    new Promise(resolve => dispatch(loadUserFilledOrders(options, resolve)))
-  );
+
+  const Augur = augurSdk.get();
+  const userData: Getters.Users.UserAccountDataResult = await Augur.getUserAccountData({universe: universe.id, account: address})
+  dispatch(updateUserFilledOrders(address, userData.userTradeHistory));
+  dispatch(bulkMarketTradingHistory(userData.marketTradeHistory));
 
   promises.push(
     new Promise(resolve =>
