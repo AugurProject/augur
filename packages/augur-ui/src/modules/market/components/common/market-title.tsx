1309c502e52789f6b3b169e6eb1215feaa69e465
Tom Haile <bradford.haile@gmail.com>
2019-10-31 17:30:55 -0500

    remvoed isTemplate and show icon, design changed

--- packages/augur-ui/src/modules/market/components/common/market-title.tsx
+++ packages/augur-ui/src/modules/market/components/common/market-title.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import MarketLink from 'modules/market/components/market-link/market-link';
-import { TemplateIcon } from 'modules/common/icons';
-import Styles from 'modules/market/components/common/market-common.styles.less';
 
 interface MarketTitleProps {
   id: string;
   description: string;
   isWrapped?: boolean;
-  isTemplate?: boolean;
-  showIcon?: boolean;
 }
 
 const wrapMarketName = (marketName: string) => <span>{`"${marketName}"`}</span>;
