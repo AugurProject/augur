0e40a33576064a40c4e1788ebb45296ec52f6371
Tom Haile <bradford.haile@gmail.com>
2019-10-31 18:37:32 -0500

    checkpoint, template validation structure

---
+++ packages/augur-ui/src/modules/create-market/template-validations.ts
@@ -0,0 +1,6 @@
+export const TEMPLATE_VALIDATIONS = {
+  '918ee03a826a8a9c51198f9eb3896fef5f5c8e95d19bb5814c2647c75d48ba91': {
+    templateValidation: ``,
+    resolutionRulesValidation: ``,
+  },
+};
