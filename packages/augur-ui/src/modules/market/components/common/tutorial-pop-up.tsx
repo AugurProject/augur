663dee35dc703ecf89fde0b97f67a8e6725f0223
phoebemirman <phoebe.mirman@gmail.com>
2019-11-01 13:52:50 -0700

    position tooltip and arrow

--- packages/augur-ui/src/modules/market/components/common/tutorial-pop-up.tsx
+++ packages/augur-ui/src/modules/market/components/common/tutorial-pop-up.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import Styles from 'modules/market/components/common/tutorial-pop-up.styles.less';
-import { PrimaryButton } from 'modules/common/buttons';
+import { SecondaryButton } from 'modules/common/buttons';
+import classNames from 'classnames';
 
 export interface TutorialPopUpProps {
   next: Function;
   back: Function;
+  left?: boolean;
+  right?: boolean;
+  top?: boolean;
+  bottom?: boolean;
 }
 
 export const TutorialPopUp = (props: TutorialPopUpProps) => (
-  <section className={Styles.TutorialPopUp}>
-    pop up
-    <PrimaryButton
-      text="back"
-      action={() => props.back()}
-    />
-    <PrimaryButton
-      text="next"
-      action={() => props.next()}
-    />
+  <section
+    className={classNames(Styles.TutorialPopUp, {
+      [Styles.Left]: props.left,
+      [Styles.Top]: props.top,
+      [Styles.Bottom]: props.bottom,
+      [Styles.Right]: props.right,
+    })}
+  >
+    <div>
+      <span>My Fills</span>
+      <span>
+        Once an order is partially or completley filled, you'll get a
+        notification in the top right. 'My Fills' are where you can track all
+        filled or partially-filled orders.
+      </span>
+      <div>
+        <SecondaryButton text="Back" action={() => props.back()} />
+        <span>
+          <span>11</span>/ 12
+        </span>
+        <SecondaryButton text="Next" action={() => props.next()} />
+      </div>
+    </div>
   </section>
 );
