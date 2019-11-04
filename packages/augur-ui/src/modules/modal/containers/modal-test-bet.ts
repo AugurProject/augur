cfd70d76698cd62b6beff9a4fe15ce04ebad82db
phoebemirman <phoebe.mirman@gmail.com>
2019-11-04 15:46:44 -0800

    hook up place bet, remove old modal, start using local state for ordeer

--- packages/augur-ui/src/modules/modal/containers/modal-test-bet.ts
+++ packages/augur-ui/src/modules/modal/containers/modal-test-bet.ts
@@ -5,8 +5,15 @@ import { closeModal } from 'modules/modal/actions/close-modal';
 import { AppState } from 'store';
 import { ThunkDispatch } from 'redux-thunk';
 import { Action } from 'redux';
-import { ONBOARDING_SEEN_KEY } from 'modules/common/constants';
+import {
+  ONBOARDING_SEEN_KEY,
+  TRADING_TUTORIAL,
+} from 'modules/common/constants';
 import { windowRef } from 'utils/window-ref';
+import makePath from 'modules/routes/helpers/make-path';
+import { MARKET } from 'modules/routes/constants/views';
+import makeQuery from 'modules/routes/helpers/make-query';
+import { MARKET_ID_PARAM_NAME } from 'modules/routes/constants/param-names';
 
 const mapStateToProps = (state: AppState) => ({
   isMobile: state.appStatus.isMobile,
@@ -22,23 +29,31 @@ const mapDispatchToProps = (dispatch: ThunkDispatch<void, any, Action>) => ({
 });
 
 const mergeProps = (sP: any, dP: any, oP: any) => ({
-  largeHeader: sP.isMobile ? '2. Learn how to bet on Augur' : '2. Run a test bet',
-  smallHeader: sP.isMobile ? 'Watch a 30 second video' : 'Learn how to place a bet using Augur Trade',
+  largeHeader: sP.isMobile
+    ? '2. Learn how to bet on Augur'
+    : '2. Run a test bet',
+  smallHeader: sP.isMobile
+    ? 'Watch a 30 second video'
+    : 'Learn how to place a bet using Augur Trade',
   testBet: true,
   currentStep: 3,
   linkContent: [
     {
-      content:
-        sP.isMobile
-          ? 'Watch our quick start video to learn how to place a bet using our trading app.'
-          : 'See how betting works on Augur. You’ll get guidance and tips to help you get started.',
+      content: sP.isMobile
+        ? 'Watch our quick start video to learn how to place a bet using our trading app.'
+        : 'See how betting works on Augur. You’ll get guidance and tips to help you get started.',
     },
   ],
   buttons: [
     {
       text: sP.isMobile ? 'Watch video' : 'Place test bet',
-      disabled: true,
       action: () => {
+        oP.history.push({
+          pathname: makePath(MARKET),
+          search: makeQuery({
+            [MARKET_ID_PARAM_NAME]: TRADING_TUTORIAL,
+          }),
+        });
         dP.setOnboardingSeen();
         dP.closeModal();
       },
