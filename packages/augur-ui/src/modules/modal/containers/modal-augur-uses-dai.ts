9aec22983b487d7bd7f851d2cc6347093de7d94a
Andrew D'Amelio <dev@andrewdamel.io>
2019-11-05 16:12:58 -0500

    update onboarding modals (#4584)

---
+++ packages/augur-ui/src/modules/modal/containers/modal-augur-uses-dai.ts
@@ -0,0 +1,48 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { Onboarding } from 'modules/modal/onboarding';
+import { updateModal } from 'modules/modal/actions/update-modal';
+import { AppState } from 'store';
+import { ThunkDispatch } from 'redux-thunk';
+import { Action } from 'redux';
+import { MODAL_BUY_DAI } from 'modules/common/constants';
+import { OnboardingDollarDaiIcon } from 'modules/common/icons';
+
+const mapStateToProps = (state: AppState) => ({});
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<void, any, Action>) => ({
+  buyDaiModal: () => dispatch(updateModal({ type: MODAL_BUY_DAI })),
+});
+
+const mergeProps = (sP: any, dP: any, oP: any) => ({
+  icon: OnboardingDollarDaiIcon,
+  largeHeader: 'Augur uses DAI',
+  currentStep: 2,
+  linkContent: [
+    {
+      content:
+        'DAI is a pegged currency that mirrors the value of the US dollar. This means that 1 DAI is equivalent to 1 USD. For ease of use we refer to DAI using the $ symbol.',
+    },
+    {
+      content: 'Learn more about DAI',
+      link: 'https://docs.augur.net',
+    },
+  ],
+  buttons: [
+    {
+      text: 'Continue',
+      disabled: false,
+      action: () => {
+        dP.buyDaiModal();
+      },
+    },
+  ],
+});
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+    mergeProps
+  )(Onboarding)
+);
