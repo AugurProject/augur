96e34f4622ac539aab07275c08609b6fe09ba23b
Andrew D'Amelio <dev@andrewdamel.io>
2019-11-06 13:38:03 -0500

    market-view fix

--- packages/augur-ui/src/modules/market/components/market-view/market-view.tsx
+++ packages/augur-ui/src/modules/market/components/market-view/market-view.tsx
@@ -618,12 +618,12 @@ export default class MarketView extends Component<
                         updateSelectedOrderProperties={
                           this.updateSelectedOrderProperties
                         }
-                        marketId={tradingTutorial && marketId}
+                        marketId={marketId}
                         market={preview && market}
                         preview={preview}
                         tradingTutorial={tradingTutorial}
                         orders={orders}
-                        fills={fillls}
+                        fills={fills}
                       />
                     </div>
                   </ModulePane>
