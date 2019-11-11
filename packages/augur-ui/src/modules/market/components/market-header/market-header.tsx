75489d916d35717f91287b555bef91870a31a104
Tom Haile <bradford.haile@gmail.com>
2019-11-11 12:18:51 -0600

    fixed missing market title on preview

--- packages/augur-ui/src/modules/market/components/market-header/market-header.tsx
+++ packages/augur-ui/src/modules/market/components/market-header/market-header.tsx
@@ -31,6 +31,7 @@ import { MarketData, QueryEndpoints } from 'modules/types';
 import Clipboard from 'clipboard';
 import { TutorialPopUp } from '../common/tutorial-pop-up';
 import MarketTitle from 'modules/market/containers/market-title';
+import PreviewMarketTitle from 'modules/market/components/common/PreviewMarketTitle';
 
 const OVERFLOW_DETAILS_LENGTH = 25; // in px, overflow limit to trigger MORE details
 
@@ -146,7 +147,7 @@ export default class MarketHeader extends Component<
       toggleFavorite,
       showTutorial,
       next,
-      back
+      back,
     } = this.props;
     let { details } = this.props;
     const { headerCollapsed } = this.state;
@@ -226,7 +227,7 @@ export default class MarketHeader extends Component<
             </div>
             <div className={Styles.MainValues}>
               <div>
-                <MarketTitle id={market.marketId} noLink />
+                {preview ? <PreviewMarketTitle market={market} /> : <MarketTitle id={market.marketId} noLink />}
                 {details.length > 0 && (
                   <div className={Styles.Details}>
                     <h4>Additional Details</h4>
