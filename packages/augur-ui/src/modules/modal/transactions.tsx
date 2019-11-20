0129692eb10ac8308bbf997c59bdad39ce4df987
JohnDanz <John.Danz87@gmail.com>
2019-11-20 14:27:34 -0600

    more styling work and some cleaning on the transactions.tsx file that have changed since converting to a pure component

--- packages/augur-ui/src/modules/modal/transactions.tsx
+++ packages/augur-ui/src/modules/modal/transactions.tsx
@@ -1,23 +1,28 @@
-import React, { useState, useRef } from "react";
-import moment, { Moment } from "moment";
+import React, { useState, useRef } from 'react';
+import moment, { Moment } from 'moment';
 import {
   PrimaryButton,
   SecondaryButton,
   ExportButton,
   ViewTransactionDetailsButton,
   SortButton,
-} from "modules/common/buttons";
+} from 'modules/common/buttons';
 import {
   ASCENDING,
   DESCENDING,
   NEUTRAL,
-} from "modules/common/constants";
-import { Pagination } from "modules/common/pagination";
-import { ValueLabel, TextLabel } from "modules/common/labels";
-import { DatePicker, FormDropdown } from "modules/common/form";
-import { Title } from "modules/modal/common";
-import { formatEther, formatShares } from "utils/format-number";
-import Styles from "modules/modal/modal.styles.less";
+  ETH,
+  REP,
+  DAI,
+} from 'modules/common/constants';
+import { convertAttoValueToDisplayValue } from '@augurproject/sdk';
+import { Pagination } from 'modules/common/pagination';
+import { ValueLabel, TextLabel } from 'modules/common/labels';
+import { DatePicker, FormDropdown } from 'modules/common/form';
+import { Title } from 'modules/modal/common';
+import { formatEther, formatShares, formatDai } from 'utils/format-number';
+import Styles from 'modules/modal/modal.styles.less';
+import { createBigNumber } from 'utils/create-big-number';
 
 interface TransactionsProps {
   closeAction: Function;
@@ -58,127 +63,135 @@ interface TransactionsState {
 
 const coinOptions = [
   {
-    label: "All",
-    value: "ALL",
+    label: 'All',
+    value: 'ALL',
   },
   {
-    label: "ETH",
-    value: "ETH",
+    label: DAI,
+    value: DAI,
   },
   {
-    label: "REP",
-    value: "REP",
+    label: REP,
+    value: REP,
+  },
+  {
+    label: ETH,
+    value: ETH,
   },
 ];
 
 const actionOptions = [
   {
-    label: "All",
-    value: "ALL",
+    label: 'All',
+    value: 'ALL',
   },
   {
-    label: "Buy",
-    value: "BUY",
+    label: 'Buy',
+    value: 'BUY',
   },
   {
-    label: "Sell",
-    value: "SELL",
+    label: 'Sell',
+    value: 'SELL',
   },
   {
-    label: "Cancelled",
-    value: "CANCEL",
+    label: 'Cancelled',
+    value: 'CANCEL',
   },
   {
-    label: "Claim Market Creator Fees",
-    value: "CLAIM_MARKET_CREATOR_FEES",
+    label: 'Claim Market Creator Fees',
+    value: 'CLAIM_MARKET_CREATOR_FEES',
   },
   {
-    label: "Claim Participation Tokens",
-    value: "CLAIM_PARTICIPATION_TOKENS",
+    label: 'Claim Participation Tokens',
+    value: 'CLAIM_PARTICIPATION_TOKENS',
   },
   {
-    label: "Claim Staked REP & Reporting Fees",
-    value: "CLAIM_WINNING_CROWDSOURCERS",
+    label: 'Claim Staked REP & Reporting Fees',
+    value: 'CLAIM_WINNING_CROWDSOURCERS',
   },
   {
-    label: "Claim Trading Proceeds",
-    value: "CLAIM_TRADING_PROCEEDS",
+    label: 'Claim Trading Proceeds',
+    value: 'CLAIM_TRADING_PROCEEDS',
   },
   {
-    label: "Dispute",
-    value: "DISPUTE",
+    label: 'Dispute',
+    value: 'DISPUTE',
   },
   {
-    label: "Initial Report",
-    value: "INITIAL_REPORT",
+    label: 'Initial Report',
+    value: 'INITIAL_REPORT',
   },
   // { // Removed until V2
   //   label: "Finalize Market",
   //   value: "FINALIZE_MARKET",
   // },
   {
-    label: "Market Creation",
-    value: "MARKET_CREATION",
+    label: 'Market Creation',
+    value: 'MARKET_CREATION',
   },
   {
-    label: "Complete Sets",
-    value: "COMPLETE_SETS",
+    label: 'Complete Sets',
+    value: 'COMPLETE_SETS',
   },
 ];
 
 const paginationOptions = [
   {
-    label: "10 per page",
+    label: '10 per page',
     value: 10,
   },
   {
-    label: "20 per page",
+    label: '20 per page',
     value: 20,
   },
   {
-    label: "30 per page",
+    label: '30 per page',
     value: 30,
   },
   {
-    label: "40 per page",
+    label: '40 per page',
     value: 40,
   },
   {
-    label: "50 per page",
+    label: '50 per page',
     value: 50,
   },
 ];
 
-
 export const Transactions: React.FC<TransactionsProps> = props => {
   //Default states
-  const [coin, setCoin] = useState<string>("ALL");
-  const [action, setAction] = useState<string>("ALL");
+  const [coin, setCoin] = useState<string>('ALL');
+  const [action, setAction] = useState<string>('ALL');
   const [itemsPerPage, setItemsPerPage] = useState<number>(20);
   const [page, setPage] = useState<number>(1);
-  const [priceSort, setPriceSort] = useState<string>(NEUTRAL)
-  const [quantitySort, setQuantitySort] = useState<string>(NEUTRAL)
+  const [priceSort, setPriceSort] = useState<string>(NEUTRAL);
+  const [quantitySort, setQuantitySort] = useState<string>(NEUTRAL);
   //Component states
-  const [startDate, setStartDate] = useState<Moment>(moment(props.currentTimestamp * 1000).subtract(6, "M"))
-  const [endDate, setEndDate] = useState<Moment>(moment(props.currentTimestamp * 1000));
+  const [startDate, setStartDate] = useState<Moment>(
+    moment(props.currentTimestamp * 1000).subtract(6, 'M')
+  );
+  const [endDate, setEndDate] = useState<Moment>(
+    moment(props.currentTimestamp * 1000)
+  );
   const [startFocused, setStartFocused] = useState<boolean>(false);
   const [endFocused, setEndFocused] = useState<boolean>(false);
   const [AllTransactions, setAllTransactions] = useState<TransactionInfo[]>([]);
-  const [filteredTransactions, setFilteredTransactions] = useState<TransactionInfo[]>([])
+  const [filteredTransactions, setFilteredTransactions] = useState<
+    TransactionInfo[]
+  >([]);
   //Refs
   const tableHeaderRef = useRef<HTMLDivElement>(null);
   const tableBodyRef = useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
     triggerSearch();
-    tableBodyRef.current.addEventListener("scroll", handleScroll);
-    tableHeaderRef.current.addEventListener("scroll", handleScroll);
+    tableBodyRef.current.addEventListener('scroll', handleScroll);
+    tableHeaderRef.current.addEventListener('scroll', handleScroll);
     return () => {
-      tableBodyRef.current.removeEventListener("scroll", handleScroll);
-      tableHeaderRef.current.removeEventListener("scroll", handleScroll);
+      tableBodyRef.current.removeEventListener('scroll', handleScroll);
+      tableHeaderRef.current.removeEventListener('scroll', handleScroll);
     };
   }, []);
-  
 
   const handleScroll = () => {
     const body = tableBodyRef.current.scrollLeft;
@@ -186,23 +199,24 @@ export const Transactions: React.FC<TransactionsProps> = props => {
     if (body !== head) {
       tableHeaderRef.current.scrollTo(body, 0);
     }
-  }
+  };
 
   const cyclePriceSort = (e: Event) => {
     let updatedPriceSort = NEUTRAL;
+    console.log('cyclePriceSort', e, priceSort, filteredTransactions);
     switch (priceSort) {
       case ASCENDING:
         // if ascending cycle to descending
         updatedPriceSort = DESCENDING;
         filteredTransactions.sort(
-          (a, b) => parseFloat(b.price) - parseFloat(a.price),
+          (a, b) => parseFloat(b.price) - parseFloat(a.price)
         );
         break;
       case NEUTRAL:
         // if neutral cycle to ascending
         updatedPriceSort = ASCENDING;
         filteredTransactions.sort(
-          (a, b) => parseFloat(a.price) - parseFloat(b.price),
+          (a, b) => parseFloat(a.price) - parseFloat(b.price)
         );
         break;
       default:
@@ -212,8 +226,8 @@ export const Transactions: React.FC<TransactionsProps> = props => {
     }
     // @ts-ignore
     setPriceSort(updatedPriceSort);
-    setFilteredTransactions(filteredTransactions)
-  }
+    setFilteredTransactions(filteredTransactions);
+  };
 
   const cycleQuantitySort = (e: Event) => {
     let updatedQuantitySort = NEUTRAL;
@@ -222,7 +236,7 @@ export const Transactions: React.FC<TransactionsProps> = props => {
         // if ascending cycle to descending
         updatedQuantitySort = DESCENDING;
         filteredTransactions.sort(
-          (a, b) => parseFloat(b.quantity) - parseFloat(a.quantity),
+          (a, b) => parseFloat(b.quantity) - parseFloat(a.quantity)
         );
         break;
       case NEUTRAL:
@@ -249,23 +263,20 @@ export const Transactions: React.FC<TransactionsProps> = props => {
       endDate.unix().valueOf(),
       coin,
       action,
-      (err: any, AllTransactions: Array<TransactionInfo>) => {
-        if (!err) {
-          const filteredTransactions = filterTransactions(
-            AllTransactions,
-            coin,
-            action
-          );
-          if (tableHeaderRef)
-            setAllTransactions(AllTransactions);
-            setFilteredTransactions(filteredTransactions);
-        }
+      (AllTransactions: Array<TransactionInfo>) => {
+        const filteredTransactions = filterTransactions(
+          AllTransactions,
+          coin,
+          action
+        );
+        if (tableHeaderRef) setAllTransactions(AllTransactions);
+        setFilteredTransactions(filteredTransactions);
       }
     );
   };
 
   const resetSearch = () => {
-    setStartDate(moment(currentTimestamp * 1000).subtract(6, "M"));
+    setStartDate(moment(currentTimestamp * 1000).subtract(6, 'M'));
     setEndDate(moment(currentTimestamp * 1000));
     setFilteredTransactions(AllTransactions);
     triggerSearch();
@@ -274,74 +285,84 @@ export const Transactions: React.FC<TransactionsProps> = props => {
   const filterTransactions = (
     transactions: Array<TransactionInfo>,
     coin: string,
-    action: string,
+    action: string
   ) => {
     const filteredTransactions = transactions.filter(
       (Transaction: TransactionInfo) =>
-        (Transaction.coin === coin || coin === "ALL") &&
-        (Transaction.action === action || action === "ALL")
+        (Transaction.coin === coin || coin === 'ALL') &&
+        (Transaction.action === action || action === 'ALL')
     );
     return filteredTransactions;
   };
 
   const triggerTransactionsExport = () => {
+    if (AllTransactions.length === 0) return;
     const items = AllTransactions;
-    const replacer = (key: string, value: any) => (value === null ? "" : value);
+    const replacer = (key: string, value: any) => (value === null ? '' : value);
     const header = Object.keys(items[0]);
     const csv = items.map((row: any) =>
       header
         .map((fieldName: any) => JSON.stringify(row[fieldName], replacer))
-        .join(","),
+        .join(',')
     );
-    csv.unshift(header.join(","));
-    const exportCSV = csv.join("\r\n");
+    csv.unshift(header.join(','));
+    const exportCSV = csv.join('\r\n');
     const transactionsDataString =
-      "data:text/plain;charset=utf-8," + encodeURIComponent(exportCSV);
-    const a = document.createElement("a");
+      'data:text/plain;charset=utf-8,' + encodeURIComponent(exportCSV);
+    const a = document.createElement('a');
 
-    a.setAttribute("href", transactionsDataString);
-    a.setAttribute("download", "AugurTransactions.csv");
+    a.setAttribute('href', transactionsDataString);
+    a.setAttribute('download', 'AugurTransactions.csv');
     a.click();
   };
 
   const addTransactionRow = (tx: TransactionInfo) => {
-    const timestamp = moment(tx.timestamp * 1000).format("D MMM YYYY HH:mm:ss");
-    const key = `${tx.transactionHash}-${tx.timestamp}-${tx.outcome}-${
-      tx.quantity
-    }-${tx.price}-${tx.total}-${tx.action}-${tx.marketDescription}-${
-      tx.outcomeDescription
-    }`;
+    const timestamp = moment(tx.timestamp * 1000).format('D MMM YYYY HH:mm:ss');
+    const key = `${tx.transactionHash}-${tx.timestamp}-${tx.action}-${tx.outcomeDescription}`;
     // we never show the coin type outside of tx.coin so we can just format by shares always here.
-    const quantity = formatShares(tx.quantity);
+    const quantity = formatShares(
+      convertAttoValueToDisplayValue(createBigNumber(tx.quantity))
+    );
     const actionLabel = actionOptions.find((option: any) => {
       if (option.value === tx.action) return true;
       return false;
     });
-    return (
-      <React.Fragment key={key}>
-        <span>{timestamp}</span>
-        <span>
-          <TextLabel text={tx.marketDescription} />
-        </span>
-        <span>
-          <TextLabel text={tx.outcomeDescription || ""} />
-        </span>
-        <span>
-          <TextLabel
-            text={
-              (actionLabel && actionLabel.label) || tx.action.replace(/_/g, " ").toLowerCase()
-            }
-          />
-        </span>
-        <ValueLabel value={formatEther(Number(tx.price))} showDenomination={false} showEmptyDash={false} />
-        <ValueLabel value={quantity} showDenomination={false} showEmptyDash={false} />
-        <span>{tx.coin}</span>
-        <ValueLabel value={formatEther(Number(tx.fee))} showDenomination={false} showEmptyDash={false} />
-        <ValueLabel value={formatEther(Number(tx.total))} showDenomination={false} showEmptyDash={false} />
-        <ViewTransactionDetailsButton transactionHash={tx.transactionHash} />
-      </React.Fragment>
-    );
-  }
+    return [
+      <span>{timestamp}</span>,
+      <TextLabel text={tx.marketDescription} />,
+      <TextLabel text={tx.outcomeDescription || ''} />,
+      <TextLabel
+        text={
+          (actionLabel && actionLabel.label) ||
+          tx.action.replace(/_/g, ' ').toLowerCase()
+        }
+      />,
+      <ValueLabel
+        value={formatEther(Number(tx.price))}
+        showDenomination={false}
+        showEmptyDash={false}
+      />,
+      <ValueLabel
+        value={quantity}
+        showDenomination={false}
+        showEmptyDash={false}
+      />,
+      <span>{tx.coin}</span>,
+      <ValueLabel
+        value={formatEther(Number(tx.fee))}
+        showDenomination={false}
+        showEmptyDash={false}
+      />,
+      <ValueLabel
+        value={formatEther(
+          Number(convertAttoValueToDisplayValue(createBigNumber(tx.total)))
+        )}
+        showDenomination={false}
+        showEmptyDash={false}
+      />,
+      <ViewTransactionDetailsButton transactionHash={tx.transactionHash} />,
+    ];
+  };
 
   const { title, closeAction, currentTimestamp } = props;
   const pageInfo = {
@@ -352,18 +373,18 @@ export const Transactions: React.FC<TransactionsProps> = props => {
   };
   const pageTransactions = filteredTransactions.slice(
     page * itemsPerPage - itemsPerPage,
-    page * itemsPerPage,
+    page * itemsPerPage
   );
 
   const startDatePicker = {
-    id: "startDatePicker",
+    id: 'startDatePicker',
     date: startDate,
-    placeholder: "Start Date",
-    onDateChange: (startDate: string) =>{
+    placeholder: 'Start Date',
+    onDateChange: (startDate: string) => {
       setStartDate(moment(startDate));
       triggerSearch();
     },
-    onFocusChange: ({ focused }: { focused: boolean; }) => {
+    onFocusChange: ({ focused }: { focused: boolean }) => {
       if (startDate == null) {
         const startDate = moment(currentTimestamp * 1000);
         setStartDate(startDate);
@@ -371,19 +392,19 @@ export const Transactions: React.FC<TransactionsProps> = props => {
       setStartFocused(focused);
     },
     focused: startFocused,
-    displayFormat: "D MMM YYYY",
+    displayFormat: 'D MMM YYYY',
     numberOfMonths: 1,
   };
 
   const endDatePicker = {
-    id: "endDatePicker",
+    id: 'endDatePicker',
     date: endDate,
-    placeholder: "End Date",
+    placeholder: 'End Date',
     onDateChange: (endDate: string) => {
       setEndDate(moment(endDate));
       triggerSearch();
     },
-    onFocusChange: ({ focused }: { focused: boolean; }) => {
+    onFocusChange: ({ focused }: { focused: boolean }) => {
       if (endDate == null) {
         const endDate = moment(currentTimestamp * 1000);
         setEndDate(endDate);
@@ -391,13 +412,16 @@ export const Transactions: React.FC<TransactionsProps> = props => {
       setEndFocused(focused);
     },
     isOutsideRange: (day: Moment) =>
-      day.isAfter(moment(currentTimestamp * 1000).add(1, "hour")) ||
+      day.isAfter(moment(currentTimestamp * 1000).add(1, 'hour')) ||
       day.isBefore(startDate),
     focused: endFocused,
-    displayFormat: "D MMM YYYY",
+    displayFormat: 'D MMM YYYY',
     numberOfMonths: 1,
   };
 
+  const transactionsRows = pageTransactions.map(
+    (transaction: TransactionInfo) => addTransactionRow(transaction)
+  );
   return (
     <div className={Styles.Transactions}>
       <Title title={title} closeAction={closeAction} />
@@ -420,10 +444,10 @@ export const Transactions: React.FC<TransactionsProps> = props => {
             const filteredTransactions = filterTransactions(
               AllTransactions,
               coin,
-              action,
+              action
             );
-            setFilteredTransactions(filteredTransactions)
-            setAction(action)
+            setFilteredTransactions(filteredTransactions);
+            setAction(action);
           }}
         />
         <FormDropdown
@@ -433,10 +457,10 @@ export const Transactions: React.FC<TransactionsProps> = props => {
             const filteredTransactions = filterTransactions(
               AllTransactions,
               coin,
-              action,
+              action
             );
-            setFilteredTransactions(filteredTransactions)
-            setCoin(coin)
+            setFilteredTransactions(filteredTransactions);
+            setCoin(coin);
           }}
         />
         <div>
@@ -445,9 +469,7 @@ export const Transactions: React.FC<TransactionsProps> = props => {
         </div>
         <ExportButton action={triggerTransactionsExport} />
       </section>
-      <div
-        ref={tableHeaderRef}
-      >
+      <div ref={tableHeaderRef}>
         <span>Date</span>
         <span>Market</span>
         <span>Outcome</span>
@@ -471,18 +493,16 @@ export const Transactions: React.FC<TransactionsProps> = props => {
         <span>Total</span>
         <span>Etherscan</span>
       </div>
-      <section
-        ref={tableBodyRef}
-      >
-        {pageTransactions.length === 0 && (
-          <span className={Styles.NullTransactionsRow}>No Transactions</span>
-        )}
-        {pageTransactions.map((transaction: TransactionInfo) =>
-          addTransactionRow(transaction),
-        )}
+      <section ref={tableBodyRef}>
+        <>
+          {pageTransactions.length === 0 && (
+            <span className={Styles.NullTransactionsRow}>No Transactions</span>
+          )}
+          {transactionsRows}
+        </>
       </section>
       <div>
-        <Pagination {...pageInfo} updateLimit={() => {}}/>
+        <Pagination {...pageInfo} updateLimit={() => {}} />
         <FormDropdown
           options={paginationOptions}
           defaultValue={itemsPerPage}
@@ -492,4 +512,4 @@ export const Transactions: React.FC<TransactionsProps> = props => {
       </div>
     </div>
   );
-}
+};
