dbf7bf4373c91ed9af74e45e110057276c2d4be8
Peter Hicks <petong@jah.net>
2019-11-20 16:08:34 -0800

    adds docker:all
    one script to fire up pop-geth and the relayer/0x images

---
+++ scripts/one-docker-to-rule-them-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# make sure we have the latest images
+# (workaround until next version of docker supports the --pull flag)
+docker pull augurproject/safe-relay-service_web:latest
+docker pull 0xorg/mesh:latest
+
+yarn docker:geth:pop
+export CUSTOM_CONTRACT_ADDRESSES=`yarn --silent flash run get-all-contract-addresses --ugly | sed '1d'`
+#yarn docker:gnosis
+yarn workspace @augurproject/gnosis-relay-api run-relay
