42bac84639d20994842f697d4d3f2c58323979cc
phoebemirman <phoebe.mirman@gmail.com>
2019-11-22 12:50:13 -0800

    update analytics string

--- packages/augur-ui/src/services/analytics/helpers.ts
+++ packages/augur-ui/src/services/analytics/helpers.ts
@@ -28,12 +28,12 @@ export const addedDaiEvent = (dai: Number): ThunkAction<any, any, any, any> => (
 }
 
 // Onboarding event names
-export const START_TEST_TRADE = 'Onboarding::StartTestTrade';
-export const SKIPPED_TEST_TRADE = 'Onboarding::SkippedTestTrade';
-export const FINISHED_TEST_TRADE = 'Onboarding::FinishedTestTrade';
-export const DO_A_TEST_BET = 'Onboarding::DoATestBet';
-export const BUY_DAI = 'Onboarding::BuyDai';
-export const AUGUR_USES_DAI = 'Onboarding::AugurUsesDai';
-export const ADD_FUNDS = 'AddFunds';
-export const ACCOUNT_CREATED = 'Onboarding::AccountCreated';
-export const ADDED_DAI = 'AddedDai';
+export const START_TEST_TRADE = 'Onboarding - Started Test Trade';
+export const SKIPPED_TEST_TRADE = 'Onboarding - Skipped Test Trade';
+export const FINISHED_TEST_TRADE = 'Onboarding - Finished Test Trade';
+export const DO_A_TEST_BET = 'Onboarding - Do A Test Bet';
+export const BUY_DAI = 'Onboarding - Buy Dai';
+export const AUGUR_USES_DAI = 'Onboarding - Augur Uses Dai';
+export const ADD_FUNDS = 'Add Funds Modal';
+export const ACCOUNT_CREATED = 'Onboarding - Account Created';
+export const ADDED_DAI = 'Added Dai';
