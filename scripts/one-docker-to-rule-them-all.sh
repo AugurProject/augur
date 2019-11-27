838c641ba488af13d787e6f186d413e4f535dcf8
Peter Hicks <petong@jah.net>
2019-11-27 09:46:37 -0800

    changes 0x tag from latest to 0xV3

--- scripts/one-docker-to-rule-them-all.sh
+++ scripts/one-docker-to-rule-them-all.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # make sure we have the latest images
 # (workaround until next version of docker supports the --pull flag)
 docker pull augurproject/safe-relay-service_web:latest
-docker pull 0xorg/mesh:latest
+docker pull 0xorg/mesh:0xV3
 
 yarn docker:geth:pop
 export CUSTOM_CONTRACT_ADDRESSES=`yarn --silent flash run get-all-contract-addresses --ugly | sed '1d'`
