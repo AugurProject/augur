6d5546e81cfcd517d56afa6e7c6fa8bd2bcc79f7
Peter Hicks <petong@jah.net>
2019-12-02 14:13:27 -0800

    cleans up docker:geth

--- scripts/one-docker-to-rule-them-all.sh
+++ scripts/one-docker-to-rule-them-all.sh
@@ -2,6 +2,13 @@
 
 set -euo pipefail
 
+cleanup() {
+  echo "stopping geth docker image"
+  docker stop geth
+}
+
+trap cleanup SIGINT SIGTERM
+
 # make sure we have the latest images
 # (workaround until next version of docker supports the --pull flag)
 docker pull augurproject/safe-relay-service_web:latest
@@ -11,3 +18,4 @@ yarn docker:geth:pop
 export CUSTOM_CONTRACT_ADDRESSES=`yarn --silent flash run get-all-contract-addresses --ugly | sed '1d'`
 #yarn docker:gnosis
 yarn workspace @augurproject/gnosis-relay-api run-relay
+
