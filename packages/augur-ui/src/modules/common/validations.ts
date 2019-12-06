05182c24b42e223bd8e84300d0105264d6581cdc
phoebemirman <phoebe.mirman@gmail.com>
2019-12-06 10:27:48 -0800

    add more trimming

--- packages/augur-ui/src/modules/common/validations.ts
+++ packages/augur-ui/src/modules/common/validations.ts
@@ -131,15 +131,15 @@ export function checkOutcomesArray(value) {
   } else {
     const errors = Array(value.length).fill('');
     const invalid = value.findIndex(
-      outcome => outcome && outcome.toLowerCase() === INVALID_OUTCOME.toLowerCase()
+      outcome => outcome && outcome.trim().toLowerCase() === INVALID_OUTCOME.toLowerCase()
     );
     if (invalid !== -1)
       errors[invalid] = ['Can\'t enter "Market is Invalid" as an outcome'];
 
     let dupes = {};
     value.forEach((outcome, index) => {
-      dupes[outcome.toLowerCase()] = dupes[outcome.toLowerCase()] || [];
-      dupes[outcome.toLowerCase()].push(index);
+      dupes[outcome.trim().toLowerCase()] = dupes[outcome.trim().toLowerCase()] || [];
+      dupes[outcome.trim().toLowerCase()].push(index);
     });
     Object.keys(dupes).map(key => {
       if (dupes[key].length > 1) {
