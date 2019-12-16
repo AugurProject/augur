b5896958fc75d07eeb88e2ae258e70dccb4d3707
robert <robert@augur.net>
2019-12-16 14:06:25 -0800

    also --no-cache instead of -no-cache

--- packages/augur-tools/docker-builder/build.sh
+++ packages/augur-tools/docker-builder/build.sh
@@ -10,9 +10,9 @@ docker build --progress=plain --no-cache=true -f docker-builder/Dockerfile --bui
 docker tag $IMAGE_NAME $IMAGE_NAME:$CONTRACT_SHA
 
 IMAGE_NAME=augurproject/dev-pop-normtime-geth-v2
-docker build --progress=plain -no-cache=true -f docker-builder/Dockerfile --build-arg normal_time=true --build-arg network_id=103 -t $IMAGE_NAME $MONO_ROOT
+docker build --progress=plain --no-cache=true -f docker-builder/Dockerfile --build-arg normal_time=true --build-arg network_id=103 -t $IMAGE_NAME $MONO_ROOT
 docker tag $IMAGE_NAME $IMAGE_NAME:$CONTRACT_SHA
 
 IMAGE_NAME=augurproject/dev-pop-geth-15-v2
-docker build --progress=plain -no-cache=true -f docker-builder/Dockerfile --build-arg normal_time=false --build-arg network_id=104 --build-arg period_time=15 -t $IMAGE_NAME $MONO_ROOT
+docker build --progress=plain --no-cache=true -f docker-builder/Dockerfile --build-arg normal_time=false --build-arg network_id=104 --build-arg period_time=15 -t $IMAGE_NAME $MONO_ROOT
 docker tag $IMAGE_NAME $IMAGE_NAME:$CONTRACT_SHA
