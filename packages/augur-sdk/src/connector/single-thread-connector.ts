bf4c84c84d50de9ff4eed191c43765de902ab269
Paul Gebheim <pgebheim@gmail.com>
2019-12-27 09:43:15 +0000

    Remove SEO Connector and move functionality into BaseConnector

--- packages/augur-sdk/src/connector/single-thread-connector.ts
+++ packages/augur-sdk/src/connector/single-thread-connector.ts
@@ -3,16 +3,16 @@ import { API } from '../state/getter/API';
 import { BaseConnector } from './baseConnector';
 import { SubscriptionEventName } from '../constants';
 import { Subscriptions } from '../subscriptions';
-import { Callback } from '../events';
+import { Callback, EventNameEmitter } from '../events';
 
 export class SingleThreadConnector extends BaseConnector {
-  protected api: Promise<API>;
-  protected events;
+  protected api: API;
+  protected events: Subscriptions;
   subscriptions: { [event: string]: { id: string; callback: Callback } } = {};
 
   connect = async (ethNodeUrl: string, account?: string): Promise<any> => {
-    this.api = Sync.start(ethNodeUrl, account, true);
-    this.events = (await this.api).augur.getAugurEventEmitter();
+    this.api = await Sync.start(ethNodeUrl, account, true);
+    this.events = this.api.augur.events;
 
     return this.api;
   }
@@ -24,22 +24,30 @@ export class SingleThreadConnector extends BaseConnector {
 
   bindTo<R, P>(f: (db: any, augur: any, params: P) => Promise<R>): (params: P) => Promise<R> {
     return async (params: P): Promise<R> => {
-      return (await this.api).route(f.name, params);
+      return this.api.route(f.name, params);
     };
   }
 
   async on(eventName: SubscriptionEventName | string, callback: Callback): Promise<void> {
-    const wrappedCallack = super.callbackWrapper(callback);
+    const wrappedCallack = this.callbackWrapper(eventName, callback);
+    const id: string = this.events.subscribe(eventName, wrappedCallack);
+    this.subscriptions[eventName] = { id, callback: wrappedCallack };
 
-    const subscription: string = (await this.events).subscribe(eventName, wrappedCallack);
-    this.subscriptions[eventName] = { id: subscription, callback: wrappedCallack };
+    // NB: This is a hack and I wanna remove it
+    // controller.run() is called before the SEOConnector is subscribed to SDKReady, so SDKReady is never triggered,
+    // that's why we need to re-emit it here.
+    if (eventName === SubscriptionEventName.SDKReady) {
+      await this.events.emit(SubscriptionEventName.SDKReady, {
+        eventName: SubscriptionEventName.SDKReady,
+      });
+    }
   }
 
   async off(eventName: SubscriptionEventName | string): Promise<void> {
     const subscription = this.subscriptions[eventName];
     if (subscription) {
       delete this.subscriptions[eventName];
-      return (await this.events).unsubscribe(subscription.id);
+      return this.events.unsubscribe(subscription.id);
     }
   }
 }
