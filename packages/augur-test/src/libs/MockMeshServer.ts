674b3e49730164743a69ad89b9703f13209e8936
Paul Gebheim <pgebheim@gmail.com>
2020-01-17 18:22:45 +0000

    MockMoshServer support for handling port conflicts

--- packages/augur-test/src/libs/MockMeshServer.ts
+++ packages/augur-test/src/libs/MockMeshServer.ts
@@ -29,6 +29,7 @@ export interface StoredOrder {
  * @return A WS server
  */
 export async function setupServerAsync(): Promise<WebSocket.server> {
+    let port = SERVER_PORT;
     return new Promise<WebSocket.server>((resolve, reject) => {
         server = http.createServer((_request, response) => {
             response.writeHead(DEFAULT_STATUS_CODE);
@@ -36,7 +37,6 @@ export async function setupServerAsync(): Promise<WebSocket.server> {
         });
 
         wsServer = new WebSocket.server({
-            port: SERVER_PORT,
             httpServer: server,
             autoAcceptConnections: true,
             maxReceivedFrameSize: sixtyFourMB,
@@ -46,9 +46,16 @@ export async function setupServerAsync(): Promise<WebSocket.server> {
             disableNagleAlgorithm: false,
         });
 
-        server.listen(SERVER_PORT, () => {
-            resolve(wsServer);
+        server.on('listening', () => {
+            resolve({port, wsServer});
+        })
+
+        server.on('error', (e) => {
+            port++;
+            server.listen(port);
         });
+
+        server.listen(port);
     });
 }
 
@@ -74,10 +81,10 @@ export class MockMeshServer {
         this.initialize();
     }
 
-    static async create(): Promise<MockMeshServer> {
-        const wsServer = await setupServerAsync();
+    static async create(): Promise<{port: number, meshServer: MockMeshServer }> {
+        const { port, wsServer } = await setupServerAsync();
         const meshServer = new MockMeshServer(wsServer);
-        return meshServer;
+        return { port, meshServer };
     }
 
     initialize(): void {
