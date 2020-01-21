5d84a6f534b34d343bd0bec9e2dc00b23586b604
Bruno Confortin <brunoconfortin@gmail.com>
2020-01-21 01:58:36 -0300

    Checkpoint

--- packages/augur-ui/src/modules/reporting/reporting.tsx
+++ packages/augur-ui/src/modules/reporting/reporting.tsx
@@ -6,7 +6,7 @@ import { ButtonActionType, MarketData } from 'modules/types';
 import { ReportingModalButton } from 'modules/reporting/common';
 import ReportingList from 'modules/reporting/containers/reporting-list';
 import UserRepDisplay from 'modules/reporting/containers/user-rep-display';
-import { TEMP_TABLET, REPORTING_STATE } from "modules/common/constants";
+import { TEMP_TABLET, REPORTING_STATE, SMALL_MOBILE, TABLET, DESKTOP, LARGE_DESKTOP } from "modules/common/constants";
 import ModuleTabs from "modules/market/components/common/module-tabs/module-tabs";
 import ModulePane from "modules/market/components/common/module-tabs/module-pane";
 
@@ -26,75 +26,123 @@ export default class Reporting extends React.Component<ReportingProps> {
         <Helmet>
           <title>Reporting</title>
         </Helmet>
-        <Media query={TEMP_TABLET}>
-          {(matches) =>
-            matches ? (
-              <ModuleTabs selected={0} fillWidth noBorder>
-                <ModulePane label="designated reporting">
-                  <UserRepDisplay />
-                  <ReportingList
-                    reportingType={REPORTING_STATE.DESIGNATED_REPORTING}
-                    title={'Designated Reporting'}
-                    showLoggedOut={showLoggedOut}
-                    loggedOutMessage="Connect a wallet to see your markets that are ready for Reporting."
-                    emptyHeader="There are no markets available for you to Report on."
-                    emptySubheader=" Check your Upcoming Designated Reporting to see Markets that will soon be availble to Report on."
-                  />
-                  <ReportingList
-                    reportingType={REPORTING_STATE.PRE_REPORTING}
-                    title={'Upcoming Designated Reporting'}
-                    showLoggedOut={showLoggedOut}
-                    loggedOutMessage="Connect a wallet to see your markets that will soon be ready to Report on."
-                    emptyHeader="There are no markets coming up in the next week for you to Report on."
-                    emptySubheader="Check your Upcoming Designated Reporting to see Markets that will soon be availble to Report on."
-                  />
-                  <ReportingModalButton
-                    text="Designated Reporting Quick Guide"
-                    action={openReportingModal}
-                  />
-                </ModulePane>
-                <ModulePane label="Open reporting">
-                  <ReportingList
-                    reportingType={REPORTING_STATE.OPEN_REPORTING}
-                    title={'Open Reporting'}
-                    emptyHeader="There are currently no markets in Open Reporting."
-                    emptySubheader=" Markets appear here once if a Designated Reporter fails to show up."
-                  />
-                </ModulePane>
-              </ModuleTabs>
-            ) : (
+        <Media queries={{
+          smallMobile: SMALL_MOBILE,
+          tablet: TABLET,
+          desktop: DESKTOP,
+          largeDesktop: LARGE_DESKTOP
+        }}>
+          {matches => (
             <>
-              <div>
-                <ReportingList
-                  reportingType={REPORTING_STATE.DESIGNATED_REPORTING}
-                  title={'Designated Reporting'}
-                  showLoggedOut={showLoggedOut}
-                  loggedOutMessage="Connect a wallet to see your markets that are ready for Reporting."
-                  emptyHeader="There are no markets available for you to Report on."
-                  emptySubheader=" Check your Upcoming Designated Reporting to see Markets that will soon be availble to Report on."
-                />
-                <ReportingList
-                  reportingType={REPORTING_STATE.PRE_REPORTING}
-                  title={'Upcoming Designated Reporting'}
-                  showLoggedOut={showLoggedOut}
-                  loggedOutMessage="Connect a wallet to see your markets that will soon be ready to Report on."
-                  emptyHeader="There are no markets coming up in the next week for you to Report on. "
-                  emptySubheader="Check your Upcoming Designated Reporting to see Markets that will soon be availble to Report on."
-                />
-                <ReportingList
-                  reportingType={REPORTING_STATE.OPEN_REPORTING}
-                  title={'Open Reporting'}
-                  emptyHeader="There are currently no markets in Open Reporting. "
-                  emptySubheader=" Markets appear here once if a Designated Reporter fails to show up."
-                />
-              </div>
-              <div>
-                <UserRepDisplay />
-                <ReportingModalButton
-                  text="Designated Reporting Quick Guide"
-                  action={this.props.openReportingModal}
-                />
-              </div>
+              {matches.smallMobile && (
+                <ModuleTabs selected={0} fillWidth noBorder>
+                  <ModulePane label="designated reporting">
+                    <ReportingModalButton
+                      text='Designated Reporting Quick Guide'
+                      action={openReportingModal}
+                    />
+                    <UserRepDisplay />
+                    <ReportingList
+                      reportingType={REPORTING_STATE.DESIGNATED_REPORTING}
+                      title={'Designated Reporting'}
+                      showLoggedOut={showLoggedOut}
+                      loggedOutMessage='Connect a wallet to see your markets that are ready for Reporting.'
+                      emptyHeader='There are no markets available for you to Report on.'
+                      emptySubheader=' Check your Upcoming Designated Reporting to see Markets that will soon be availble to Report on.'
+                    />
+                    <ReportingList
+                      reportingType={REPORTING_STATE.PRE_REPORTING}
+                      title={'Upcoming Designated Reporting'}
+                      showLoggedOut={showLoggedOut}
+                      loggedOutMessage='Connect a wallet to see your markets that will soon be ready to Report on.'
+                      emptyHeader='There are no markets coming up in the next week for you to Report on.'
+                      emptySubheader='Check your Upcoming Designated Reporting to see Markets that will soon be availble to Report on.'
+                    />
+                  </ModulePane>
+                  <ModulePane label='Open reporting'>
+                    <ReportingModalButton
+                      text='Designated Reporting Quick Guide'
+                      action={openReportingModal}
+                    />
+                    <ReportingList
+                      reportingType={REPORTING_STATE.OPEN_REPORTING}
+                      title={'Open Reporting'}
+                      emptyHeader='There are currently no markets in Open Reporting.'
+                      emptySubheader=' Markets appear here once if a Designated Reporter fails to show up.'
+                    />
+                  </ModulePane>
+                </ModuleTabs>
+              )}
+              {matches.tablet && (
+                <>
+                  <div>
+                    <ReportingList
+                      reportingType={REPORTING_STATE.DESIGNATED_REPORTING}
+                      title={'Designated Reporting'}
+                      showLoggedOut={showLoggedOut}
+                      loggedOutMessage='Connect a wallet to see your markets that are ready for Reporting.'
+                      emptyHeader='There are no markets available for you to Report on.'
+                      emptySubheader=' Check your Upcoming Designated Reporting to see Markets that will soon be availble to Report on.'
+                    />
+                    <ReportingList
+                      reportingType={REPORTING_STATE.PRE_REPORTING}
+                      title={'Upcoming Designated Reporting'}
+                      showLoggedOut={showLoggedOut}
+                      loggedOutMessage='Connect a wallet to see your markets that will soon be ready to Report on.'
+                      emptyHeader='There are no markets coming up in the next week for you to Report on. '
+                      emptySubheader='Check your Upcoming Designated Reporting to see Markets that will soon be availble to Report on.'
+                    />
+                  </div>
+                  <div>
+                    <ReportingModalButton
+                      text='Designated Reporting Quick Guide'
+                      action={this.props.openReportingModal}
+                    />
+                    <UserRepDisplay />
+                    <ReportingList
+                      reportingType={REPORTING_STATE.OPEN_REPORTING}
+                      title={'Open Reporting'}
+                      emptyHeader='There are currently no markets in Open Reporting. '
+                      emptySubheader=' Markets appear here once if a Designated Reporter fails to show up.'
+                    />
+                  </div>
+                </>
+              )}
+              {(matches.desktop || matches.largeDesktop) && (
+                <>
+                  <div>
+                    <ReportingList
+                      reportingType={REPORTING_STATE.DESIGNATED_REPORTING}
+                      title={'Designated Reporting'}
+                      showLoggedOut={showLoggedOut}
+                      loggedOutMessage='Connect a wallet to see your markets that are ready for Reporting.'
+                      emptyHeader='There are no markets available for you to Report on.'
+                      emptySubheader=' Check your Upcoming Designated Reporting to see Markets that will soon be availble to Report on.'
+                    />
+                    <ReportingList
+                      reportingType={REPORTING_STATE.PRE_REPORTING}
+                      title={'Upcoming Designated Reporting'}
+                      showLoggedOut={showLoggedOut}
+                      loggedOutMessage='Connect a wallet to see your markets that will soon be ready to Report on.'
+                      emptyHeader='There are no markets coming up in the next week for you to Report on. '
+                      emptySubheader='Check your Upcoming Designated Reporting to see Markets that will soon be availble to Report on.'
+                    />
+                    <ReportingList
+                      reportingType={REPORTING_STATE.OPEN_REPORTING}
+                      title={'Open Reporting'}
+                      emptyHeader='There are currently no markets in Open Reporting. '
+                      emptySubheader=' Markets appear here once if a Designated Reporter fails to show up.'
+                    />
+                  </div>
+                  <div>
+                    <ReportingModalButton
+                      text='Designated Reporting Quick Guide'
+                      action={this.props.openReportingModal}
+                    />
+                    <UserRepDisplay />
+                  </div>
+                </>
+              )}
             </>
           )
         }
