0cfc8f0bf90f2549a4f317fb341601f01540da8b
Paul Gebheim <pgebheim@gmail.com>
2020-01-26 09:58:03 +0000

    Pass gasPrice in atto to backend

--- packages/contract-dependencies-gnosis/src/ContractDependenciesGnosis.ts
+++ packages/contract-dependencies-gnosis/src/ContractDependenciesGnosis.ts
@@ -326,7 +326,7 @@ export class ContractDependenciesGnosis extends ContractDependenciesEthers {
       relayTransaction.operation,
       new ethers.utils.BigNumber(Number(relayTransaction.safeTxGas).toFixed()),
       new ethers.utils.BigNumber(Number(relayTransaction.dataGas).toFixed()),
-      new ethers.utils.BigNumber(Number(gasPrice).toFixed()),
+      gasPrice,
       relayTransaction.gasToken,
       relayTransaction.refundReceiver,
       signatures,
