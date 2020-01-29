a0447588c6068774d29df931327aee6fc0fb8c70
JohnDanz <John.Danz87@gmail.com>
2020-01-29 16:22:54 -0600

    removed unusued utils helper, updated candlestick.tsx to become a pure component

--- packages/augur-ui/src/modules/market-charts/components/candlestick/candlestick.tsx
+++ packages/augur-ui/src/modules/market-charts/components/candlestick/candlestick.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { loadCandleStickData } from 'modules/markets/actions/load-candlestick-data';
 import logError from 'utils/log-error';
-import { checkPropsChange } from 'utils/check-props-change';
 import OutcomeCandlestick from 'modules/market-charts/components/candlestick/outcome-candlestick';
 import { BigNumber } from 'bignumber.js';
 import {
   DEFAULT_SHORT_PERIODS_VALUE,
   DEFAULT_PERIODS_VALUE,
 } from 'modules/common/constants';
-import { PriceTimeSeriesData } from 'modules/types';
 
 interface CandlestickProps {
   currentTimeInSeconds: number;
@@ -20,52 +18,20 @@ interface CandlestickProps {
   daysPassed: number;
 }
 
-interface CandlestickState {
-  priceTimeSeries: PriceTimeSeriesData[];
-  selectedPeriod: number;
-}
-export class Candlestick extends React.Component<
-  CandlestickProps,
-  CandlestickState
-> {
-  constructor(props) {
-    super(props);
+export const Candlestick = ({
+  currentTimeInSeconds,
+  marketId,
+  maxPrice,
+  minPrice,
+  selectedOutcomeId,
+  daysPassed,
+}: CandlestickProps) => {
+  const [priceTimeSeries, setPriceTimeSeries] = useState([]);
+  const [selectedPeriod, setSelectedPeriod] = useState(
+    daysPassed < 1 ? DEFAULT_SHORT_PERIODS_VALUE : DEFAULT_PERIODS_VALUE
+  );
 
-    const defPeriod =
-      props.daysPassed < 1
-        ? DEFAULT_SHORT_PERIODS_VALUE
-        : DEFAULT_PERIODS_VALUE;
-
-    this.state = {
-      priceTimeSeries: [],
-      selectedPeriod: defPeriod,
-    };
-
-    this.getData = this.getData.bind(this);
-    this.updateSelectedPeriod = this.updateSelectedPeriod.bind(this);
-  }
-
-  componentDidMount() {
-    this.getData();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      checkPropsChange(prevProps, this.props, [
-        'currentTimeInSeconds',
-        'selectedOutcomeId',
-        'marketId',
-      ]) ||
-      checkPropsChange(prevState, this.state, ['selectedPeriod'])
-    ) {
-      this.getData();
-    }
-  }
-
-  getData() {
-    const { currentTimeInSeconds, marketId, selectedOutcomeId } = this.props;
-    const { selectedPeriod } = this.state;
-
+  useEffect(() => {
     loadCandleStickData(
       {
         marketId,
@@ -76,34 +42,21 @@ export class Candlestick extends React.Component<
       },
       (err, data) => {
         if (err) return logError(err);
-        const priceTimeSeries = data[selectedOutcomeId] || [];
-        this.setState({
-          priceTimeSeries,
-        });
+        const updatedPriceTimeSeries = data[selectedOutcomeId] || [];
+        setPriceTimeSeries(updatedPriceTimeSeries);
       }
     );
-  }
+  }, [marketId, selectedPeriod, currentTimeInSeconds, selectedOutcomeId]);
 
-  updateSelectedPeriod(selectedPeriod) {
-    this.setState({
-      selectedPeriod,
-    });
-  }
-
-  render() {
-    const { maxPrice, minPrice } = this.props;
-    const { priceTimeSeries, selectedPeriod } = this.state;
-
-    return (
-      <OutcomeCandlestick
-        priceTimeSeries={priceTimeSeries}
-        fixedPrecision={2}
-        pricePrecision={2}
-        selectedPeriod={selectedPeriod}
-        updateSelectedPeriod={this.updateSelectedPeriod}
-        marketMax={maxPrice}
-        marketMin={minPrice}
-      />
-    );
-  }
-}
+  return (
+    <OutcomeCandlestick
+      priceTimeSeries={priceTimeSeries}
+      fixedPrecision={2}
+      pricePrecision={2}
+      selectedPeriod={selectedPeriod}
+      updateSelectedPeriod={v => setSelectedPeriod(v)}
+      marketMax={maxPrice}
+      marketMin={minPrice}
+    />
+  );
+};
