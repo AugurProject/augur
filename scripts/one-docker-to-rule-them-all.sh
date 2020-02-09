9a023df813313a81d9422611482bafca8a822a7e
Joey Krug <joeykrug@gmail.com>
2020-02-09 01:41:53 -0800

    fixed one docker script

--- scripts/one-docker-to-rule-them-all.sh
+++ scripts/one-docker-to-rule-them-all.sh
@@ -54,12 +54,11 @@ fi
 
 
 # run docker ecosystem with variables derived from geth node
-export ETHEREUM_CHAIN_ID=`yarn --silent flash network-id`
-export CUSTOM_CONTRACT_ADDRESSES=`yarn --silent flash get-all-contract-addresses --ugly`
-export GNOSIS_SAFE_CONTRACT_ADDRESS=`yarn --silent flash get-contract-address --name GnosisSafe`
-export PROXY_FACTORY_CONTRACT_ADDRESS=`yarn --silent flash get-contract-address --name ProxyFactory`
-export ZEROX_CONTRACT_ADDRESS=`yarn --silent flash get-contract-address --name ZeroXTrade -r -l`
-
+export ETHEREUM_CHAIN_ID=`yarn --silent flash network-id | sed '1d'`
+export CUSTOM_CONTRACT_ADDRESSES=`yarn --silent flash get-all-contract-addresses --ugly | sed '1d'`
+export GNOSIS_SAFE_CONTRACT_ADDRESS=`yarn --silent flash get-contract-address --name GnosisSafe | sed '1d'`
+export PROXY_FACTORY_CONTRACT_ADDRESS=`yarn --silent flash get-contract-address --name ProxyFactory | sed '1d'`
+export ZEROX_CONTRACT_ADDRESS=`yarn --silent flash get-contract-address --name ZeroXTrade -r -l | sed '1d'`
 #yarn docker:gnosis
 
 if [[ "${DETACH-false}" == "true" ]]; then
