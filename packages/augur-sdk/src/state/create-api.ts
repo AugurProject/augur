3d9a6cd8a03b05588e53931f9ef2534eb1fda2e6
nuevoalex <achap5dk@gmail.com>
2020-02-18 18:18:05 -0800

    just make our provider compliant

--- packages/augur-sdk/src/state/create-api.ts
+++ packages/augur-sdk/src/state/create-api.ts
@@ -17,7 +17,6 @@ import { LogFilterAggregator } from './logs/LogFilterAggregator';
 import { BlockAndLogStreamerSyncStrategy } from './sync/BlockAndLogStreamerSyncStrategy';
 import { BulkSyncStrategy } from './sync/BulkSyncStrategy';
 import { WarpSyncStrategy } from './sync/WarpSyncStrategy';
-import { Web3Provider } from 'ethers/providers';
 
 export interface SDKConfiguration {
   networkId: NetworkId,
@@ -124,8 +123,7 @@ export async function createClient(
       // interface instead of actually import @0x/mesh-browser -- since
       // that would attempt to start the wasm client in nodejs and cause
       // everything to die.
-      const web3Provider = (ethersProvider.provider as Web3Provider)._web3Provider as SupportedProvider;
-      createBrowserMesh(config, web3Provider, zeroX);
+      createBrowserMesh(config, ethersProvider, zeroX);
     }
   }
 
