08ae0e0c8ee58a3a7566e5f343a0c0a917fa58cd
phoebemirman <phoebe.mirman@gmail.com>
2020-02-25 13:10:39 -0800

    add processing on tx pop up

--- packages/augur-ui/src/modules/account/actions/convert-v1-rep-to-v2.ts
+++ packages/augur-ui/src/modules/account/actions/convert-v1-rep-to-v2.ts
@@ -14,7 +14,6 @@ import { TXEventName } from '@augurproject/sdk';
 export default function(callback: NodeStyleCallback = logError) {
   return async (dispatch: ThunkDispatch<void, any, Action>) => {
     await convertV1ToV2Approve();
-    dispatch(addPendingData(MIGRATE_V1_V2, MIGRATE_V1_V2, TXEventName.Pending, MIGRATE_V1_V2))
     await convertV1ToV2().catch((err: Error) => {
       console.log('error could not migrate v1 REP', err);
       logError(new Error('convertV1ToV2'));
