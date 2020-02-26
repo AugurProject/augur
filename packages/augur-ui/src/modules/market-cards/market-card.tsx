628a15e656f8380e4dd9b97a88f7cfb60f2528d4
Tom Haile <bradford.haile@gmail.com>
2020-02-25 19:09:38 -0600

    add REP at the end of staked rep on market cards

--- packages/augur-ui/src/modules/market-cards/market-card.tsx
+++ packages/augur-ui/src/modules/market-cards/market-card.tsx
@@ -233,7 +233,7 @@ export default class MarketCard extends React.Component<
         : undefined;
 
     const restOfOutcomes = isScalar && inDispute ? disputeInfo.stakes.length - showOutcomeNumber - 1 : outcomesFormatted.length - showOutcomeNumber;
-  
+
     return (
       <div
         className={classNames(Styles.MarketCard, {
@@ -264,7 +264,7 @@ export default class MarketCard extends React.Component<
               <LabelValue
                 condensed
                 label="Total Dispute Stake"
-                value={formatAttoRep(disputeInfo.stakeCompletedTotal).formatted}
+                value={formatAttoRep(disputeInfo.stakeCompletedTotal).full}
               />
             )}
             <div className={Styles.hoverIconTray}>{InfoIcons}</div>
