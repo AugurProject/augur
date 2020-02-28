9c8bdb3b710f81700eeba74447c0b05c2e39d532
Tom Haile <bradford.haile@gmail.com>
2020-02-28 15:41:23 -0600

    fix issue where wraper was sending single logs

--- packages/augur-ui/src/modules/events/actions/wrap-log-handler.ts
+++ packages/augur-ui/src/modules/events/actions/wrap-log-handler.ts
@@ -10,18 +10,8 @@ export const wrapLogHandler = (logHandler: Function) => (
 ) => {
   if (log) {
     const universeId: string = getState().universe.id;
-    console.log("event name", Array.isArray(log) ? log.length : log.eventName);
     const isInCurrentUniverse = true;
-    // TODO: process universe when Events have universe propety, for now assume all events are good
-    // const isInCurrentUniverse = Object.values(log).find(
-    //   value => universeId === value
-    // );
-    if (Array.isArray(log) && isInCurrentUniverse) {
-      log.forEach(log => {
-        if (logHandler) dispatch(logHandler(log));
-      });
-    }
-    // TODO: will need to filter out some redundent events like token transfers in some instances
+    // TODO: need to filter based on current selected universe
     if (isInCurrentUniverse) return dispatch(logHandler(log));
   }
 };
