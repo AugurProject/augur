cb7b72946d24d756f95ab0213c9b4128332262da
nuevoalex <achap5dk@gmail.com>
2020-03-06 15:21:22 -0800

    convenience functions

--- packages/augur-sdk/src/api/GSN.ts
+++ packages/augur-sdk/src/api/GSN.ts
@@ -2,6 +2,7 @@ import { abi } from '@augurproject/artifacts';
 import { Abi } from 'ethereum';
 import { Provider } from '..';
 import { Augur } from '../Augur';
+import { NULL_ADDRESS } from '../constants';
 
 export class GSN {
   constructor(
@@ -21,5 +22,27 @@ export class GSN {
     owner: string
   ): Promise<string> {
     return this.augur.contracts.augurWalletRegistry.getCreate2WalletAddress_(owner);
+  }
+
+  /**
+   * @desc Sees if the user actually has a wallet on chain already
+   * @param {string} owner
+   * @returns {Promise<string>}
+   */
+  async userHasInitializedWallet(
+    owner: string
+  ): Promise<boolean> {
+    return (await this.augur.contracts.augurWalletRegistry.getWallet_(owner)) !== NULL_ADDRESS;
+  }
+
+  /**
+   * @desc Manually initialize a wallet for the signer. The main use case for this is to enable the signing of off chain orders
+   * @returns {Promise<void>}
+   */
+  async initializeWallet(
+  ): Promise<void> {
+    // TODO: Any transaction will do for the purposes of this function as they all initialize a wallet when the useWallet flag is true. In that spirit we have users do light gas fee ops that have no incentives here. Could cycle them randomly even.
+    await this.augur.contracts.universe.sweepInterest();
+    return;
   }
 }
