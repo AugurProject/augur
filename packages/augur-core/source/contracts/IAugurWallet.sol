4c2b22d4f888bcaa8c673a45f0b119529312f29b
nuevoalex <achap5dk@gmail.com>
2020-03-06 15:16:59 -0800

    contract payment hardening

--- packages/augur-core/source/contracts/IAugurWallet.sol
+++ packages/augur-core/source/contracts/IAugurWallet.sol
@@ -8,5 +8,7 @@ import 'ROOT/ISimpleDex.sol';
 
 interface IAugurWallet {
     function initialize(address _owner, address _referralAddress, bytes32 _fingerprint, address _augur, IERC20 _cash, IAffiliates _affiliates, IERC1155 _shareToken, address _createOrder, address _fillOrder, address _zeroXTrade) external;
-    function executeTransaction(address _to, bytes calldata _data, uint256 _value) external;
+    function transferCash(address _to, uint256 _amount) external;
+    function giveRegistryEth(uint256 _amount) external;
+    function executeTransaction(address _to, bytes calldata _data, uint256 _value) external returns (bool);
 }
