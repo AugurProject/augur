2f61114a27504c870c942b05d195287db80482a9
Tom Haile <bradford.haile@gmail.com>
2020-03-10 08:01:26 -0500

    remove displaying incorrect date for cancelled orders

--- packages/augur-ui/src/modules/modal/transactions.tsx
+++ packages/augur-ui/src/modules/modal/transactions.tsx
@@ -306,7 +306,7 @@ export const Transactions: React.FC<TransactionsProps> = props => {
   };
 
   const addTransactionRow = (tx: TransactionInfo) => {
-    const timestamp = moment(tx.timestamp * 1000).format('D MMM YYYY HH:mm:ss');
+    const timestamp = tx.timestamp ? moment(tx.timestamp * 1000).format('D MMM YYYY HH:mm:ss') : null;
     const key = `${tx.transactionHash}-${tx.timestamp}-${tx.action}-${tx.outcomeDescription}`;
     // we never show the coin type outside of tx.coin so we can just format by shares always here.
     const quantity = formatShares(createBigNumber(tx.quantity));
@@ -490,7 +490,7 @@ export const Transactions: React.FC<TransactionsProps> = props => {
           </>
         </section>
       </div>
-      
+
       <div>
         <Pagination {...pageInfo} updateLimit={() => {}} />
         <FormDropdown
