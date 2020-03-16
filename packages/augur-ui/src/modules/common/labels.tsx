b109708485a851e92bf34490e94aa0382379e073
rubyweb3 <ruby.web3@gmail.com>
2020-03-16 18:52:47 +0800

    Update tooltip.styles.less

--- packages/augur-ui/src/modules/common/labels.tsx
+++ packages/augur-ui/src/modules/common/labels.tsx
@@ -262,7 +262,7 @@ export const RedFlag = ({ market }: RedFlagProps) => market.mostLikelyInvalid ? 
       className={TooltipStyles.Tooltip}
       effect="solid"
       place="right"
-      type="light"
+      type={getTheme() === THEMES.TRADING ? "light" : null}
     >
       {PROBABLE_INVALID_MARKET}
     </ReactTooltip>
@@ -292,7 +292,7 @@ export const TemplateShield = ({ market }: TemplateShieldProps) => {
         className={TooltipStyles.Tooltip}
         effect="solid"
         place="right"
-        type="light"
+        type={getTheme() === THEMES.TRADING ? "light" : null}
       >
         {yellowShield
           ? "Templated market question, contains market creator text. This text should match to highlighted section's tooltip"
@@ -521,7 +521,7 @@ export class TextLabel extends React.Component<TextLabelProps, TextLabelState> {
             className={TooltipStyles.Tooltip}
             effect="solid"
             place="top"
-            type="light"
+            type={getTheme() === THEMES.TRADING ? "light" : null}
             data-event="mouseover"
             data-event-off="blur scroll"
           >
@@ -644,7 +644,7 @@ export const InvalidLabel = ({
         )}
         effect="solid"
         place={tooltipPositioning || 'left'}
-        type="dark"
+        type={getTheme() === THEMES.TRADING ? "light" : null}
         data-event="mouseover"
         data-event-off="blur scroll"
       >
