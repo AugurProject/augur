7947abb09701223cd0de5ce5bcb3dbf9965bd1da
Tom Haile <bradford.haile@gmail.com>
2020-03-20 18:12:13 -0500

    pass along from get markets order book to get zerox orders getter

--- packages/augur-sdk/src/state/getter/Markets.ts
+++ packages/augur-sdk/src/state/getter/Markets.ts
@@ -284,6 +284,7 @@ export class Markets {
       outcomeId: t.union([outcomeIdType, t.array(outcomeIdType)]),
       account: t.string,
       onChain: t.boolean, // if false or not present, use 0x orderbook
+      expirationCutoffSeconds: t.number,
     }),
   ]);
 
@@ -652,11 +653,13 @@ export class Markets {
       orders = await OnChainTrading.getOpenOnChainOrders(augur, db, {
         marketId: params.marketId,
         orderState: OrderState.OPEN,
+        expirationCutoffSeconds: params.expirationCutoffSeconds,
       });
     } else {
       orders = await OnChainTrading.getOpenOrders(augur, db, {
         marketId: params.marketId,
         orderState: OrderState.OPEN,
+        expirationCutoffSeconds: params.expirationCutoffSeconds,
       });
     }
 
