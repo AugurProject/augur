ab893354f79f023b571c7a8ee93b2f7c66a91a0a
phoebemirman <phoebe.mirman@gmail.com>
2020-03-24 13:43:36 -0700

    put categories on correct row for template cards

--- packages/augur-ui/src/modules/market-cards/market-card.tsx
+++ packages/augur-ui/src/modules/market-cards/market-card.tsx
@@ -288,6 +288,7 @@ export default class MarketCard extends React.Component<
               [Styles.scalar]: isScalar,
               [Styles.template]: isTemplate,
               [Styles.invalid]: mostLikelyInvalid,
+              [Styles.nonOpen]: marketType !== REPORTING_STATE.PRE_REPORTING
             })}
           >
             {marketStatus === MARKET_REPORTING && (
