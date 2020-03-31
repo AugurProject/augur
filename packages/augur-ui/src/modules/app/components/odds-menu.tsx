4a3e2e914a4e55bee8831fa7c98062385082bdda
JohnDanz <John.Danz87@gmail.com>
2020-03-31 13:50:01 -0500

    styling of odds table

--- packages/augur-ui/src/modules/app/components/odds-menu.tsx
+++ packages/augur-ui/src/modules/app/components/odds-menu.tsx
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
 import classNames from 'classNames';
 
-import { Gear } from 'modules/common/icons';
+import { Gear, CheckMark } from 'modules/common/icons';
 
 import Styles from 'modules/app/components/odds-menu.styles';
 
 export const OddsMenu = () => {
   const [open, setOpen] = useState(false);
+
   return (
     <div
       className={classNames(Styles.OddsMenu, { [Styles.Open]: open })}
-      onClick={() => setOpen(!open)}
     >
-      {Gear}
+      <button onClick={() => setOpen(!open)}>{Gear}</button>
+      <ul>
+        <li><h4>Odds Settings</h4></li>
+        <li><button onClick={() => null}>Decimal {CheckMark}</button></li>
+        <li><button onClick={() => null}>Fractional {CheckMark}</button></li>
+        <li><button onClick={() => null}>American {CheckMark}</button></li>
+        <li><button onClick={() => null}>0% - 100% {CheckMark}</button></li>
+      </ul>
     </div>
   );
 };
