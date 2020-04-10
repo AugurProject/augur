6339f30b62814922cab1a56e29c837f59ee3d48b
phoebemirman <phoebe.mirman@gmail.com>
2020-04-10 13:20:41 -0700

    use deep clone

--- packages/augur-ui/src/modules/pending-queue/reducers/pending-queue.ts
+++ packages/augur-ui/src/modules/pending-queue/reducers/pending-queue.ts
@@ -6,6 +6,7 @@ import {
 import { PendingQueue, BaseAction } from "modules/types";
 import { CLEAR_LOGIN_ACCOUNT } from "modules/account/actions/login-account";
 import { RESET_STATE } from "modules/app/actions/reset-state";
+import deepClone from "utils/deep-clone";
 
 const DEFAULT_STATE: PendingQueue = {};
 
@@ -13,16 +14,17 @@ export default function(pendingQueue: PendingQueue = DEFAULT_STATE, { type, data
   switch (type) {
     case ADD_PENDING_DATA: {
       const { pendingId, queueName, status, blockNumber, hash, info } = data;
+      let pending = deepClone<PendingQueue>(pendingQueue);
       if (pendingQueue[queueName]) {
-        pendingQueue[queueName][pendingId] = {
+        pending[queueName][pendingId] = {
           status,
           data: info,
           hash,
           blockNumber,
         };
       } else {
-        pendingQueue[queueName] = {};
-        pendingQueue[queueName][pendingId] = {
+        pending[queueName] = {};
+        pending[queueName][pendingId] = {
           status,
           data: info,
           hash,
@@ -31,14 +33,14 @@ export default function(pendingQueue: PendingQueue = DEFAULT_STATE, { type, data
       }
 
       return {
-        ...pendingQueue,
+        ...pending,
       };
     }
     case REMOVE_PENDING_DATA_BY_HASH: {
       const { hash, queueName } = data;
-      let pending = pendingQueue;
+      let pending = deepClone<PendingQueue>(pendingQueue);
       if (pendingQueue[queueName]) {
-        const queue = pendingQueue[queueName];
+        const queue = deepClone<PendingQueue>(pendingQueue);
         pending[queueName] = Object.keys(queue).reduce(
           (p, o) => (queue[o].hash !== hash ? {...p, [o]: queue[o]} : p),
           {}
@@ -50,11 +52,12 @@ export default function(pendingQueue: PendingQueue = DEFAULT_STATE, { type, data
     }
     case REMOVE_PENDING_DATA: {
       const { pendingId, queueName } = data;
+      let pending = deepClone<PendingQueue>(pendingQueue);
       if (pendingQueue[queueName] && pendingQueue[queueName][pendingId]) {
-        delete pendingQueue[queueName][pendingId];
+        delete pending[queueName][pendingId];
       }
       return {
-        ...pendingQueue,
+        ...pending,
       };
     }
     case RESET_STATE:
