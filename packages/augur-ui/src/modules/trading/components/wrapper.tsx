dc9caff7d7fe48dd3753f9d18eae35198469472d
phoebemirman <phoebe.mirman@gmail.com>
2020-04-14 11:21:57 -0700

    fix position selecting to populate order form

--- packages/augur-ui/src/modules/trading/components/wrapper.tsx
+++ packages/augur-ui/src/modules/trading/components/wrapper.tsx
@@ -178,6 +178,7 @@ class Wrapper extends Component<WrapperProps, WrapperState> {
           this.setState(
             {
               orderPrice: '',
+              orderQuantity: selectedOrderProperties.orderQuantity,
             },
             () =>
               this.updateTradeTotalCost(
@@ -191,14 +192,21 @@ class Wrapper extends Component<WrapperProps, WrapperState> {
               )
           );
         } else {
-          this.updateTradeTotalCost(
+          this.setState(
             {
-              ...selectedOrderProperties,
-              orderQuantity: convertExponentialToDecimal(
-                selectedOrderProperties.orderQuantity
-              ),
+              orderPrice: selectedOrderProperties.orderPrice,
+              orderQuantity: selectedOrderProperties.orderQuantity,
             },
-            true
+            () =>
+              this.updateTradeTotalCost(
+                {
+                  ...selectedOrderProperties,
+                  orderQuantity: convertExponentialToDecimal(
+                    selectedOrderProperties.orderQuantity
+                  ),
+                },
+                true
+              )
           );
         }
       }
