d2e9728401d3a661b9eaa2c417ede70c4db2ab7e
phoebemirman <phoebe.mirman@gmail.com>
2020-05-27 13:42:34 -0700

    update selector

--- packages/augur-ui/src/modules/markets/selectors/markets-all.ts
+++ packages/augur-ui/src/modules/markets/selectors/markets-all.ts
@@ -1,17 +1,8 @@
-import store from "appStore";
 import { selectMarket } from "modules/markets/selectors/market";
-import {
-  selectMarketTradingHistoryState,
-  selectCancelingOrdersState,
-  selectLoginAccountAddress,
-  selectAccountPositionsState
-} from "appStore/select-state";
-import { createSelector } from "reselect";
-import { MarketData } from "modules/types";
 import { Markets } from "../store/markets";
 
 export default function() {
-  return selectMarkets(store.getState());
+  return selectMarkets();
 }
 
 export const getMarkets = () => {
@@ -23,22 +14,11 @@ export const getMarkets = () => {
   }, []);
 };
 
-export const selectMarkets = createSelector(
-  selectMarketTradingHistoryState,
-  selectCancelingOrdersState,
-  selectLoginAccountAddress,
-  selectAccountPositionsState,
-  (
-    marketPriceHistory,
-    orderCancellation,
-    accountAddress,
-    accountPositions
-  ): Array<MarketData> => {
-    const { marketInfos } = Markets.get();
-    if (!marketInfos) return [];
-    return Object.keys(marketInfos).reduce((p, marketId) => {
-      if (!marketId || !marketInfos[marketId]) return p;
-      return [...p, selectMarket(marketId)];
-    }, []);
-  }
-);
+export const selectMarkets = () => {
+      const { marketInfos } = Markets.get();
+      if (!marketInfos) return [];
+      return Object.keys(marketInfos).reduce((p, marketId) => {
+        if (!marketId || !marketInfos[marketId]) return p;
+        return [...p, selectMarket(marketId)];
+      }, []);
+    }
