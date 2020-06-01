a4dec4936b503033cfb344a3b5558b10cb549b67
Bruno Confortin <brunoconfortin@gmail.com>
2020-06-01 19:36:34 -0300

    Restyled and fixed the migrate rep button for mobile

--- packages/augur-ui/src/modules/app/components/top-nav/top-nav.tsx
+++ packages/augur-ui/src/modules/app/components/top-nav/top-nav.tsx
@@ -103,13 +103,12 @@ const TopNav = ({
                     <label
                       className={classNames(TooltipStyles.TooltipHint)}
                       data-tip
-                      data-for={'migrateRep'}
-                      data-iscapture={true}
+                      data-for={'tooltip--migrateRep'}
                     >
                       {helpIcon}
                     </label>
                     <ReactTooltip
-                      id={'migrateRep'}
+                      id={'tooltip--migrateRep'}
                       className={TooltipStyles.Tooltip}
                       effect="solid"
                       place="top"
@@ -119,7 +118,7 @@ const TopNav = ({
                     >
                       <p>
                         {
-                          walletBalances.legacyRep > 0
+                          Number(walletBalances.legacyRep) > 0
                             ? 'You have V1 REP in your trading account. Migrate it to V2 REP to use it in Augur V2.'
                             : 'You have V1 REP in your wallet. Migrate it to V2 REP to use it in Augur V2.'
                         }
