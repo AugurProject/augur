a1b5de3182466a7d3bb7455a6be8397fd61062d6
Tom Haile <bradford.haile@gmail.com>
2020-06-05 08:34:03 -0500

    tweak to new federally charged template

--- packages/augur-tools/src/templates-source.ts
+++ packages/augur-tools/src/templates-source.ts
@@ -7503,26 +7503,25 @@ export const TEMPLATES = {
           },
           {
             marketType: YES_NO,
-            question: `Will [0] be federal charged by [1]?`,
+            question: `Will [0] be federally charged by [1]?`,
             example: `Will Al Capone be federal charged by December 31, 2020 11:59 pm EST`,
             inputs: [
               {
                 id: 0,
                 type: TemplateInputType.TEXT,
-                placeholder: TEXT_PLACEHOLDERS.SINGLE_PERSON_FULL_NAME,
+                placeholder: TEXT_PLACEHOLDERS.SINGLE_PERSON_NAME,
               },
               {
                 id: 1,
-                type: TemplateInputType.DATETIME,
-                placeholder: `Specific Datetime`,
-                label: `Specific Datetime`,
-                sublabel: `Specify date time for event`,
+                type: TemplateInputType.DATESTART,
+                daysAfterDateStart: 1,
+                placeholder: `Day of Year`,
               },
             ],
             resolutionRules: {
               [REQUIRED]: [
                 {
-                  text: `Person named shall be indicted or otherwise formally charged with a U.S. federal crime, as publicly confirmed on or before date in market question by an authorized representative of the charging agency(ies) or judicial venue(s).`,
+                  text: `Person named shall be indicted or otherwise formally charged with a U.S. federal crime, as publicly confirmed on or before date by 11:59PM eastern time in market question by an authorized representative of the charging agency(ies) or judicial venue(s).`,
                 },
               ],
             },
