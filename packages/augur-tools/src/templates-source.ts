f1d28d2db3e52ab2a6b51f9747af9bc760dde69c
J. Farid <jrab227@gmail.com>
2020-06-04 21:30:19 -0400

    Changing suggested market resolution text to socialblade.com. fixes #7970

--- packages/augur-tools/src/templates-source.ts
+++ packages/augur-tools/src/templates-source.ts
@@ -8591,8 +8591,9 @@ export const TEMPLATES = {
         templates: [
           {
             marketType: YES_NO,
-            question: `Twitter: Will @[0] have [1] [2] or more twitter followers on [3], according to www.trackalytics.com?`,
-            example: `Twitter: Will @elonmusk have 50 million or more twitter followers on September 12, 2020, according to www.trackalytics.com?`,
+            question: `Twitter: Will @[0] have [1] [2] or more twitter followers on [3], according to www.socialblade.com?\n` + \
+                      'To find the results: In the header of the page. Select "Twitter" from the drop down menu. The enter the twitter handle in the search bar. Scroll down the page until you reach the section that says "Twitter Stas Summary/ User Statistics for (named person). Find the date named in the market question and get the larger number under the "Followers" section (number will not have a +/-). Settlement should be based off of this number.',
+            example: `Twitter: Will @elonmusk have 50 million or more twitter followers on September 12, 2020, according to www.socialblade.com?`,
             inputs: [
               {
                 id: 0,
@@ -8622,7 +8623,7 @@ export const TEMPLATES = {
               [REQUIRED]: [
                 {
                   text:
-                    'If www.trackalytics.com is down or not available use inspectsocial.com/ to determine total twitter followers for the social media account stated in market question.',
+                    'If www.socialblade.com is down or not available use inspectsocial.com/ to determine total twitter followers for the social media account stated in market question.',
                 },
               ],
             },
