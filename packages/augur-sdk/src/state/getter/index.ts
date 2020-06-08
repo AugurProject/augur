1f1da9d21fbb8db3af4ceb9ce93314fb63e12edb
Tom Haile <bradford.haile@gmail.com>
2020-06-08 13:53:29 -0500

    revert files, getting werid type error

--- packages/augur-sdk/src/state/getter/index.ts
+++ packages/augur-sdk/src/state/getter/index.ts
@@ -1,12 +1,11 @@
-import * as Accounts from './Accounts';
 import * as API from './API';
-
+import * as Accounts from './Accounts';
 import * as Markets from './Markets';
 import * as Trading from './OnChainTrading';
-import * as Platform from './Platform';
+import * as Users from './Users';
 import * as Status from './status';
 import * as Universe from './Universe';
-import * as Users from './Users';
+import * as Platform from './Platform';
 
 export {
   API,
@@ -18,3 +17,4 @@ export {
   Status,
   Platform,
 };
+
