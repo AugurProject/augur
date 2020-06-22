ec614b8490d11b730280e390ecc23b50e65ab8f7
phoebemirman <phoebe.mirman@gmail.com>
2020-06-22 11:48:49 -0700

    move 3 box to app status

--- packages/augur-ui/src/reducers/index.ts
+++ packages/augur-ui/src/reducers/index.ts
@@ -1,19 +1,12 @@
 // import newMarket from 'modules/markets/reducers/new-market';
-import initialized3box from 'modules/global-chat/reducers/initialized-3box'
-import {
-  Initialized3box
-} from 'modules/types';
 import { SDKConfiguration } from '@augurproject/artifacts';
 
 export function createReducer() {
-  return {
-    initialized3box
-  };
+  return {};
 }
 
 // TODO: couldn't use concreat type form `createReducer` so hardcoding structure here
 // keeping with reducers for easier maintenance.
 export interface AppStateInterface {
   config: SDKConfiguration;
-  initialized3box: Initialized3box;
 }
