5ea09936ba591679467eea48b84a3ac5e3b18e03
Andrew D'Amelio <dev@andrewdamel.io>
2020-06-24 08:51:55 -0400

    fix order expiration bug and update non Mainnet buckets

--- packages/augur-sdk-lite/src/constants.ts
+++ packages/augur-sdk-lite/src/constants.ts
@@ -143,6 +143,17 @@ export enum SubscriptionEventName {
   DBUpdatedZeroXOrders = 'DB:updated:ZeroXOrders',
 }
 
+export const NETWORK_IDS = {
+  Mainnet: '1',
+  Ropsten: '3',
+  Rinkeby: '4',
+  Kovan: '42',
+  Private1: '101',
+  Private2: '102',
+  Private3: '103',
+  Private4: '104',
+};
+
 export enum TXEventName {
   AwaitingSigning = 'AwaitingSigning',
   Pending = 'Pending',
