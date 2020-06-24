59acd70f38ea4f09f4286878f70acf81d2320e69
Alex <achap5dk@gmail.com>
2020-06-24 04:51:25 -0700

    slight improvement in getMarkets initial load (#8148)

--- packages/augur-sdk/src/state/getter/Markets.ts
+++ packages/augur-sdk/src/state/getter/Markets.ts
@@ -337,14 +337,6 @@ export class Markets {
     params.limit = typeof params.limit === 'undefined' ? 10 : params.limit;
     params.offset = typeof params.offset === 'undefined' ? 0 : params.offset;
 
-    // Get Market docs for all markets with the specified filters
-    const numMarketDocs = params.reportingStates
-      ? await db.Markets.where('reportingState')
-          .anyOf(params.reportingStates)
-          .and(item => !params.includeWarpSyncMarkets && !item.isWarpSync)
-          .count()
-      : await db.Markets.count();
-
     let marketIds: string[] = [];
     const useMarketIds =
       params.search ||
@@ -436,6 +428,13 @@ export class Markets {
       );
       marketsCollection = db.Markets.toCollection();
     }
+
+    // Get Market docs for all markets with the specified filters
+    const numMarketDocs = usedReportingStates
+    ? await marketsCollection
+        .and(item => !params.includeWarpSyncMarkets && !item.isWarpSync)
+        .count()
+    : await db.Markets.count();
 
     // Prepare filter values before loop
     let feePercent = 0;
