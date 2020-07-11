64fc36d907b0ba524a12bf54ae81a9679229d7b6
Tom Haile <bradford.haile@gmail.com>
2020-07-10 19:07:30 -0500

    simplification of network mismatch check

--- packages/augur-ui/src/modules/app/actions/init-augur.ts
+++ packages/augur-ui/src/modules/app/actions/init-augur.ts
@@ -80,29 +80,21 @@ async function loadAccountIfStored() {
   }
 }
 
-async function web3CorrectNetwork(config) {
-  if(windowRef.web3) {
-    const web3Provider = new Web3Provider('ethereum' in window ? windowRef.ethereum : windowRef.web3.currentProvider);
-    const web3NetworkId = web3Provider && web3Provider._web3Provider.networkVersion || web3Provider._network && web3Provider._network.chainId;
-    if (web3NetworkId && config.networkId !== web3NetworkId) {
-      // bring up modal, how ever modals are popped up. TODO
-        /* pop up modal Method({
-          type: MODAL_NETWORK_MISMATCH,
-          expectedNetwork: NETWORK_NAMES[Number(config.networkId)],
-        })
-        */
-      return false;
-    }
-    return true;
-  }
-  return false;
+const isNetworkMismatch = config => {
+  if (!windowRef.ethereum && !windowRef.web3) return false;
+  const web3Provider = getWeb3Provider(windowRef);
+  const web3NetworkId =
+    web3Provider?._web3Provider?.networkVersion ||
+    windowRef.web3?.currentProvider?.networkVersion ||
+    web3Provider?._network?.chainId;
+  return web3NetworkId && config.networkId !== web3NetworkId ? false : true;
 }
 
 async function createDefaultProvider(config: SDKConfiguration) {
   if (config.networkId && isDevNetworkId(config.networkId)) {
     // In DEV, use local ethereum node
     return new JsonRpcProvider(config.ethereum.http);
-  } else if (windowRef.web3 && web3CorrectNetwork(config)) {
+  } else if (windowRef.web3) {
     // Use the provider on window if it exists, otherwise use torus provider
     return getWeb3Provider(windowRef);
   } else if (config.ui?.fallbackProvider === "jsonrpc" && config.ethereum.http) {
@@ -187,22 +179,6 @@ export const connectAugur = async (
       AppStatus.actions.updateLoginAccount(accountObject);
       break;
     }
-
-
-    // Optimize for the case where we can just use a JSON endpoint.
-    // If things aren't configured for that we'll create the default
-    // provider which may be slow.
-    let provider = config.ui?.liteProvider === "jsonrpc" ?
-      new JsonRpcProvider(config.ethereum.http) :
-      await createDefaultProvider(config);
-
-    await augurSdkLite.makeLiteClient(
-      provider,
-      config.addresses,
-      config.networkId
-    );
-
-   // TODO: turn on hotloading dispatch(updateCanHotload(true)); // Hotload now!
 
   // Disable mesh/gsn for googleBot
   if (isGoogleBot()) {
