b7c804726fdc26904d1ef4e56a20a63cc057aa89
Tom Haile <bradford.haile@gmail.com>
2020-07-18 21:57:47 -0500

    Track market invalid bids (#8530)
    * track market invalid bids to be displayed on market cards in market list page.
    
    * remove code not needed
    
    * better naming of method
    
    * remove console log
    
    * clean up and use get market orderbook getter to get shaped invalid bids. thread into orderbook UI state.
    
    * remove clearing orderbook when user nav off market trading page. fix orderbook structure so that newly loaded ordersbooks don't wipe state.
    
    * show best bid as percentage for invalid outcome in market card.
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/augur-ui/src/modules/market-cards/common.tsx
+++ packages/augur-ui/src/modules/market-cards/common.tsx
@@ -1,6 +1,7 @@
 import type { Getters } from '@augurproject/sdk';
 import { MarketReportingState } from '@augurproject/sdk-lite';
 import classNames from 'classnames';
+import { connect } from 'react-redux';
 import { ProcessingButton } from 'modules/common/buttons';
 import {
   INVALID_OUTCOME_ID,
@@ -33,8 +34,10 @@ import {
 import React, { Fragment } from 'react';
 import ReactTooltip from 'react-tooltip';
 import { BigNumber, createBigNumber } from 'utils/create-big-number';
-import { formatAttoRep, formatDai, formatNumber } from 'utils/format-number';
+import { formatAttoRep, formatDai, formatNumber, formatNone } from 'utils/format-number';
 import { getOutcomeNameWithOutcome } from 'utils/get-outcome';
+import { AppState } from 'appStore';
+import { getBestInvalidBid } from 'modules/orders/actions/load-market-orderbook';
 
 export interface PercentProps {
   percent: number;
@@ -58,7 +61,7 @@ export interface OutcomeProps {
   outcomeId: string;
 }
 
-export const Outcome = (props: OutcomeProps) => {
+export const OutcomeCmp = (props: OutcomeProps) => {
   const percent = props.lastPricePercent
     ? calculatePosition(props.min, props.max, props.lastPricePercent)
     : 0;
@@ -92,6 +95,28 @@ export const Outcome = (props: OutcomeProps) => {
     </MarketLink>
   );
 };
+
+const mapStateToPropsOutcome = (state: AppState, ownProps) => {
+  let lastPricePercent = ownProps.lastPricePercent;
+  if (ownProps.invalid) {
+    const price = getBestInvalidBid(ownProps.marketId, state.orderBooks);
+    lastPricePercent = price ? formatNumber(price, {
+        decimals: 2,
+        decimalsRounded: 1,
+        positiveSign: false,
+        zeroStyled: true,
+      })
+      : formatNone();
+  }
+  return {
+    ...ownProps,
+    lastPricePercent,
+  }
+};
+
+export const Outcome = connect(
+  mapStateToPropsOutcome
+)(OutcomeCmp);
 
 export interface DisputeOutcomeProps {
   description: string;
