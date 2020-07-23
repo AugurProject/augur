494860f48a04a3bad369b93a5ff80db7c32ca91d
Paul Gebheim <pgebheim@gmail.com>
2020-07-23 05:42:21 +0000

    Use printf to handle cross platform output

--- support/install.sh
+++ support/install.sh
@@ -3,49 +3,50 @@
 BUFFER_TEXT="\n###########################";
 DEFAULT_ETH_NODE_URL="http://localhost:8545";
 
-echo -e "$BUFFER_TEXT\n  Welcome friend of Augur$BUFFER_TEXT";
+printf "$BUFFER_TEXT\n  Welcome friend of Augur$BUFFER_TEXT\n";
 
 # Check docker is present
 if [ -x "$(command -v docker)" ]
 then
-  echo -e "Your system already has access to Docker, you're good to go!\n";
+  printf "Your system already has access to Docker, you're good to go!\n\n";
 else
-  echo -e "You need Docker installed and configured in order to run the augur services. See: https://docs.docker.com/get-docker/ for instructions.\n";
+  printf "You need Docker installed and configured in order to run the augur services. See: https://docs.docker.com/get-docker/ for instructions.\n\n";
   exit 1;
 fi
 
 docker info > /dev/null 2>&1;
 if [ $? != 0 ]
 then
-  echo -e "Docker daemon not running. Cannot install.\n";
+  printf "Docker daemon not running. Cannot install.\n\n";
   exit 1;
 else
-  echo -e "Docker daemon is running!\n";
+  printf "Docker daemon is running!\n\n";
 fi
 
 # Get eth node url
-echo -e "$BUFFER_TEXT\nNOTE: You need to have access to an Ethereum Mainnet server.\nIf you don't have one or don't know what this is, \nregister one at https://infura.io/register and past the Mainnet URL here.$BUFFER_TEXT";
+printf "$BUFFER_TEXT\nNOTE: You need to have access to an Ethereum Mainnet server.\nIf you don't have one or don't know what this is, \nregister one at https://infura.nio/register and past the Mainnet URL here.$BUFFER_TEXT\n";
 
 printf "Enter an ethereum RPC URL (default: $DEFAULT_ETH_NODE_URL): ";
 read ETH_NODE_URL;
 
 print_menu() {
-	echo -e "~~~~~~~~~~~~~~~~~~~~~"
-	echo -e " Select Augur Environment"
-	echo -e "~~~~~~~~~~~~~~~~~~~~~"
-	echo -e "1. mainnet"
-	echo -e "2. v2 (kovan)"
-	echo -e "3. local"
+	printf "~~~~~~~~~~~~~~~~~~~~~\n"
+	printf " Select Augur Environmen\n"
+	printf "~~~~~~~~~~~~~~~~~~~~\n"
+	printf "1. mainnet\n"
+	printf "2. v2 (kovan)\n"
+	printf "3. local\n"
 }
 
 read_options(){
 	local choice
-	read -p "Enter choice [ 1 - 3] " choice
+	read -p "Enter choice [ 1 - 3] (Default is v2): " choice
 	case $choice in
-		1) echo -e "mainnet";;
-		2) echo -e "v2" ;;
-		3) echo -e "local" ;;
-		*) echo -e -e "${RED}Error...${STD}" && sleep 2
+		1) printf "mainnet";;
+		2) printf "v2" ;;
+		3) printf "local" ;;
+		# The Default
+		*) printf "v2"
 	esac
 }
 
