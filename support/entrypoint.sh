0a3fe9e9bf38df6ed0cbd35ebf6588819ab0b113
Justin Barry <328965+justinbarry@users.noreply.github.com>
2020-07-24 10:57:47 -0700

    Call webpack directly

--- support/entrypoint.sh
+++ support/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 
-yarn ui build;
+set -e;
+
+printf "\nBuilding client....\n\n";
+
+# This is an attempt to avoid memeory limitation in processes spawned from node.
+cd packages/augur-ui;
+./node_modules/.bin/webpack;
+
+cd -;
 
 yarn flash generate-wallet --keyfileOutputLocation /keys/priv.key
 yarn flash sdk-server --keyfile /keys/priv.key -p -w -a "${@:2}"
