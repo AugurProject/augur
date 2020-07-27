640080ee1219a623c6ca4f36b517f63fb28f3095
JohnDanz <John.Danz87@gmail.com>
2020-07-27 14:49:14 -0500

    fixed an issue in form.tsx causing a warning no-op, working on optimizing betting-market-view data

--- packages/augur-ui/src/modules/market/components/market-view/betting-market-view.tsx
+++ packages/augur-ui/src/modules/market/components/market-view/betting-market-view.tsx
@@ -24,6 +24,7 @@ import parsePath from 'modules/routes/helpers/parse-path';
 import { convertInputs, findStartTime } from '../common/market-title';
 import { convertUnixToFormattedDate } from 'utils/format-date';
 import { bulkLoadMarketTradingHistory } from 'modules/markets/actions/market-trading-history-management';
+import { useEffect } from 'react';
 
 export const isMarketView = location =>
   parsePath(location.pathname)[0] === MARKET;
@@ -40,46 +41,62 @@ const BettingMarketView = () => {
   const sportsGroup = useRef(null);
   const marketIds = useRef([]);
   const sportsGroupTradeHistory = useRef([]);
-  const [loadHistory, setLoadHistory] = useState(false);
   const queryId = parseQuery(location.search)[MARKET_ID_PARAM_NAME];
   const marketId = getAddress(queryId);
   const market = selectMarket(marketId);
   const markets = getMarkets();
-  if (!market.sportsBook) {
-    updateMarketsData(null, loadMarketsInfo([marketId]));
-  } else if (sportsGroup.current === null) {
-    sportsGroup.current = getSportsGroupsFromSportsIDs(
-      [market.sportsBook.groupId],
-      markets
-    )[0];
-    marketIds.current = sportsGroup.current.markets.map(market => market.id);
-    if (!loadHistory && marketIds.current.length > 0) {
-      bulkLoadMarketTradingHistory(marketIds.current, (err, tradeHistory) => {
-        bulkMarketTradingHistory(tradeHistory)
-      });
-      setLoadHistory(true);
+
+  useEffect(() => {
+    console.log('use effect 1 -- make sure we have sportsBook');
+    if (!market.sportsBook) {
+      console.log('use effect 1 -- going to load sportsBook');
+      updateMarketsData(null, loadMarketsInfo([marketId]));
     }
-  }
-  sportsGroupTradeHistory.current = marketIds.current.map(marketId => {
-    if (marketTradingHistory[marketId]) return marketTradingHistory[marketId];
-  });
-  sportsGroupTradeHistory.current = sportsGroupTradeHistory.current.filter(item => !!item);
-  if (sportsGroupTradeHistory.current.length > 0) {
-    let tradeCount = 0;
-    sportsGroupTradeHistory.current.forEach(MarketTradeHistoryArray => {
-      tradeCount = tradeCount + MarketTradeHistoryArray.length;
-      MarketTradeHistoryArray.forEach(({ creator, filler }) => {
-        if (!uniqueAddresses.current.includes(creator)) {
-          uniqueAddresses.current.push(creator);
-        }
-        if (!uniqueAddresses.current.includes(filler)) {
-          uniqueAddresses.current.push(filler);
-        }
-      })
-    });
-    totalBets.current = tradeCount;
-    totalBettors.current = uniqueAddresses.current.length;
-  }
+  }, []);
+
+  useEffect(() => {
+    console.log('use effect 2 -- initial bulk trade history load');
+    if (sportsGroup.current === null && market?.sportsBook?.groupId) {
+      console.log("use effect 2 -- code triggered");
+      sportsGroup.current = getSportsGroupsFromSportsIDs(
+        [market.sportsBook.groupId],
+        markets
+      )[0];
+      marketIds.current = sportsGroup.current.markets.map(market => market.id);
+      if (marketIds.current.length > 0) {
+        console.log('use effect 2 -- Actually bulk load history');
+        bulkLoadMarketTradingHistory(marketIds.current, (err, tradeHistory) => {
+          bulkMarketTradingHistory(tradeHistory)
+        });
+      }
+    }
+  }, [market.sportsBook])
+
+  useEffect(() => {
+    console.log("use effect 3 -- update trade history on history update -- updates totalBets, bettors, sportsGroupHistory, UniqueAddresses");
+    sportsGroupTradeHistory.current = marketIds.current.map(marketId => {
+      if (marketTradingHistory[marketId]) return marketTradingHistory[marketId];
+    }).filter(item => !!item);
+    console.log("use effect 3 -- part 1", sportsGroupTradeHistory.current);
+    // sportsGroupTradeHistory.current = sportsGroupTradeHistory.current;
+    if (sportsGroupTradeHistory.current.length > 0) {
+      console.log("use effect 3 -- sportsGroupTradeHistory length > 0")
+      let tradeCount = 0;
+      sportsGroupTradeHistory.current.forEach(MarketTradeHistoryArray => {
+        tradeCount = tradeCount + MarketTradeHistoryArray.length;
+        MarketTradeHistoryArray.forEach(({ creator, filler }) => {
+          if (!uniqueAddresses.current.includes(creator)) {
+            uniqueAddresses.current.push(creator);
+          }
+          if (!uniqueAddresses.current.includes(filler)) {
+            uniqueAddresses.current.push(filler);
+          }
+        })
+      });
+      totalBets.current = tradeCount;
+      totalBettors.current = uniqueAddresses.current.length;
+    }
+  }, [marketTradingHistory[marketId]])
 
   if (!market) {
     return <div />;
