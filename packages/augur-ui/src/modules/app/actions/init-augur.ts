78ca08ea916d4b071e65ef4de10ebae62a8e5e15
Andrew D'Amelio <dev@andrewdamel.io>
2020-07-28 13:01:08 -0400

    update default provider

--- packages/augur-ui/src/modules/app/actions/init-augur.ts
+++ packages/augur-ui/src/modules/app/actions/init-augur.ts
@@ -30,6 +30,7 @@ import {
   NETWORK_NAMES,
   SIGNIN_SIGN_WALLET,
   MODAL_REPORTING_ONLY,
+  PORTIS_API_KEY,
 } from 'modules/common/constants';
 import { getNetworkId } from 'modules/contracts/actions/contractCalls';
 import { listenForStartUpEvents } from 'modules/events/actions/listen-to-updates';
@@ -135,27 +136,21 @@ async function createDefaultProvider(config: SDKConfiguration, canUseWeb3) {
   } else if ((config.ui.primaryProvider === 'jsonrpc' || config.ui?.fallbackProvider === "jsonrpc") && config.ethereum.http) {
     return new JsonRpcProvider(config.ethereum.http);
   } else {
-    // Use torus provider
+    // Use portis provider
 
     // Use import instead of import for wallet SDK packages
     // to conditionally load web3 into the DOM.
     //
     // Note: This also creates a split point in webpack
-    const { default: Torus } = await import( /*webpackChunkName: 'torus'*/ '@toruslabs/torus-embed');
-    const torus = new Torus({});
+    const { default: Portis } = await import( /*webpackChunkName: 'portis'*/ '@portis/web3');
 
-    const host = getNetwork(config.networkId);
-    await torus.init({
-      network: { host },
-      showTorusButton: false,
-    });
+    const portisNetwork = getNetwork(config.networkId);
+    const portis = new Portis(
+      PORTIS_API_KEY,
+      portisNetwork,
+    );
 
-    // Tor.us cleanup
-    const torusWidget = document.querySelector('#torusWidget');
-    if (torusWidget) {
-      torusWidget.remove();
-    }
-    return new Web3Provider(torus.provider);
+    return new Web3Provider(portis.provider);
   }
 }
 
