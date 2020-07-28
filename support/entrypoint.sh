9dccd006996b143481eb86a9c28bf33e2e44b069
robert <robert@augur.net>
2020-07-28 00:25:46 -0700

    flag order

--- support/entrypoint.sh
+++ support/entrypoint.sh
@@ -16,4 +16,4 @@ cd -;
 
 yarn flash generate-wallet --keyfileOutputLocation /keys/priv.key
 echo
-yarn flash sdk-server --keyfile /keys/priv.key -p -w -a -r "${@:2}"
+yarn flash sdk-server --keyfile /keys/priv.key -p -a -w -a "${@:2}"
