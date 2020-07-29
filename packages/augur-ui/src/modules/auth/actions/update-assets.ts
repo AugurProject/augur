b2a0ec93677f874714ce79173eb3ded88b0a9c47
Tom Haile <bradford.haile@gmail.com>
2020-07-28 21:08:28 -0500

    fload calc for activating fee reserve

--- packages/augur-ui/src/modules/auth/actions/update-assets.ts
+++ packages/augur-ui/src/modules/auth/actions/update-assets.ts
@@ -13,15 +13,16 @@ import {
   WALLET_STATUS,
 } from 'modules/app/actions/update-app-status';
 import { createBigNumber } from 'utils/create-big-number';
-import { ETHER, WALLET_STATUS_VALUES, FIVE } from 'modules/common/constants';
+import { ETHER, WALLET_STATUS_VALUES, FIVE, ZERO, GWEI_CONVERSION } from 'modules/common/constants';
 import { addEthIncreaseAlert } from 'modules/alerts/actions/alerts';
 import { formatAttoDai } from 'utils/format-number';
+import { augurSdk } from 'services/augursdk';
 
 export const updateAssets = (): ThunkAction<any, any, any, any> => async (
   dispatch: ThunkDispatch<void, any, Action>,
   getState: () => AppState
 ) => {
-  const { loginAccount, appStatus } = getState();
+  const { loginAccount, appStatus, env, gasPriceInfo } = getState();
   const { meta } = loginAccount;
   const nonSafeWallet = await meta.signer?.getAddress();
 
@@ -40,21 +41,22 @@ export const updateAssets = (): ThunkAction<any, any, any, any> => async (
       walletREP,
       walletLegacyREP,
     } = values;
-    dispatch(updateAppStatus(ETH_TO_DAI_RATE, formatAttoDai(attoDAIperETH)));
+    const dai2Eth = formatAttoDai(attoDAIperETH);
+    dispatch(updateAppStatus(ETH_TO_DAI_RATE, dai2Eth));
     dispatch(updateAppStatus(REP_TO_DAI_RATE, formatAttoDai(attoDAIperREP)));
-    const dai = String(createBigNumber(String(walletDAI)).dividedBy(ETHER));
+    const daiBalance = String(createBigNumber(String(walletDAI)).dividedBy(ETHER));
     const signerEthBalance = String(
       createBigNumber(String(signerETH)).dividedBy(ETHER)
     );
-    dispatch(addedDaiEvent(dai));
+    dispatch(addedDaiEvent(daiBalance));
     const ethNonSafeBalance = loginAccount.balances.signerBalances?.eth;
-    dispatch(addEthIncreaseAlert(dai, ethNonSafeBalance, signerEthBalance));
+    dispatch(addEthIncreaseAlert(daiBalance, ethNonSafeBalance, signerEthBalance));
     dispatch(
       updateLoginAccount({
         balances: {
           attoRep: String(walletREP),
           rep: String(createBigNumber(walletREP).dividedBy(ETHER)),
-          dai,
+          dai: daiBalance,
           eth: String(createBigNumber(String(walletETH)).dividedBy(ETHER)),
           legacyAttoRep: String(walletLegacyREP),
           legacyRep: String(
@@ -72,8 +74,29 @@ export const updateAssets = (): ThunkAction<any, any, any, any> => async (
       })
     );
     let status = appStatus[WALLET_STATUS];
-    if (createBigNumber(dai).gt(FIVE) && (status !== WALLET_STATUS_VALUES.CREATED)) {
+    if (createBigNumber(daiBalance).gte(FIVE) && (status !== WALLET_STATUS_VALUES.CREATED)) {
       dispatch(updateAppStatus(WALLET_STATUS, WALLET_STATUS_VALUES.FUNDED_NEED_CREATE));
     }
+
+    const feeReserveAmount = createBigNumber(env.gsn.desiredSignerBalanceInETH);
+    const desiredEthBalance = feeReserveAmount.minus(signerEthBalance);
+    let activateFeeReserve = true;
+    if (desiredEthBalance.gt(ZERO)) {
+      // gasPrice
+      const gasPriceGwei = gasPriceInfo.userDefinedGasPrice || gasPriceInfo.average
+      const gasPriceWei = createBigNumber(gasPriceGwei).times(createBigNumber(GWEI_CONVERSION));
+
+      // convert worst cost tx in dai
+      const dai2EthConvert = createBigNumber(dai2Eth.value);
+      const desiredEthReserveInDai = feeReserveAmount.times(dai2EthConvert);
+      const totalEstimatedMaxDai = dai2EthConvert.times((gasPriceWei.times(createBigNumber(6000000))).div(ETHER)); // Estimate of a very large tx with current gas price
+      const totalWorstCaseDai = totalEstimatedMaxDai.plus(desiredEthReserveInDai);
+
+      // calc if user has dai balance over desired dai balance after worst case tx dai costs
+      const userBalAfterWorstCase = createBigNumber(daiBalance).minus(totalWorstCaseDai);
+      const minDaiNeeded = createBigNumber(env.gsn.minDaiForSignerETHBalanceInDAI);
+      activateFeeReserve = userBalAfterWorstCase.gte(minDaiNeeded);
+    }
+    augurSdk.client.setUseDesiredEthBalance(activateFeeReserve);
   }
 };
