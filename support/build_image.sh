4bc2e9302149520758cd98f7de2cfde028dd9d59
Justin <328965+justinbarry@users.noreply.github.com>
2020-07-29 20:37:00 -0700

    Trim down docker image (#8746)
    * Black hole puppeteer npm package
    
    * Build orbitz-web locally and copy artifact to docker image
    
    This will save some space.
    
    * Allow client COMMIT_HASH to be populated by an ENVVAR
    
    Also no longer outputting branch in client
    
    * gitRevisionPlugin not needed as a plugin
    
    Co-authored-by: robert <robert@augur.net>

---
+++ support/build_image.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e;
+
+VERSION=$(node -e 'process.stdout.write(require("./lerna.json").version);');
+printf "Building docker runner for version %s\n" "$VERSION";
+
+yarn;
+
+# Pre-build this to avoid the
+yarn workspace orbit-web build;
+
+docker build . \
+  -f support/Dockerfile \
+  --build-arg CURRENT_COMMITHASH="$(git rev-parse HEAD)" \
+  -t augurproject/augur:runner \
+  -t "augurproject/augur:v$VERSION";
