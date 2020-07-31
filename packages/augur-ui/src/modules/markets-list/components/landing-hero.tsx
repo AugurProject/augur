869f76d0ee91eef43d648926ca26fe96d779954a
Andrew D'Amelio <dev@andrewdamel.io>
2020-07-31 13:33:41 -0400

    update hero styles

--- packages/augur-ui/src/modules/markets-list/components/landing-hero.tsx
+++ packages/augur-ui/src/modules/markets-list/components/landing-hero.tsx
@@ -11,7 +11,7 @@ export const LandingHero = ({showSignup}: LandingHeroProps) => {
   return (
     <section className={Styles.LandingHero}>
       <div>
-        <h1>Augur<br />Your global, no-limit betting platform</h1>
+        <h1>Augur<br /><span>Your global, no-limit betting platform</span></h1>
         <span>
           Bet how much you want on sports, economics, world events and more.
         </span>
