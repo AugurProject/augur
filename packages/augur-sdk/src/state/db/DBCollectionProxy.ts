31719bd6ce06a19aabff6059910abf1278c8b915
Justin Barry <328965+justinbarry@users.noreply.github.com>
2020-08-05 16:51:50 -0700

    Add timeout to read queue

--- packages/augur-sdk/src/state/db/DBCollectionProxy.ts
+++ packages/augur-sdk/src/state/db/DBCollectionProxy.ts
@@ -1,16 +1,24 @@
 import Dexie, {
+  Collection, IndexableType,
   PromiseExtended,
+  ThenShortcut,
 } from 'dexie';
-import { queue } from 'async';
+import { queue, timeout } from 'async';
+
+interface ToArrayQueueTask<T, R> {
+  cb?: ThenShortcut<T[], R>;
+  collection: Collection<T>;
+};
 
 export function CollectionQueueAddOn (db: Dexie) {
   const originalToArray = db.Collection.prototype.toArray;
-  const toArrayQueue = queue(async ({collection, cb}) => {
-    console.log('Reading Query');
-    return originalToArray.call(collection, cb)
+  const toArrayQueue = queue<ToArrayQueueTask<any, any>>(({collection, cb}, callback) => {
+    return timeout(async (args) => {
+      return originalToArray.call(collection, cb);
+    }, 1000)(callback);
   }, 1);
 
-  db.Collection.prototype.toArray = function toArray(cb?): PromiseExtended<any[]> {
+  db.Collection.prototype.toArray = function toArray<R>(cb?): PromiseExtended<any[]> {
     return new Dexie.Promise((resolve, reject) => {
       toArrayQueue.push({ collection: this, cb }, (err, result:any[]) => {
         if (err) {
