bcb6e1ada93ccecf99ebc8b159df4b089ada4795
Andrew D'Amelio <dev@andrewdamel.io>
2020-08-10 16:06:12 -0400

    add dropdowns to swap

--- packages/augur-ui/src/modules/types.ts
+++ packages/augur-ui/src/modules/types.ts
@@ -638,6 +638,8 @@ export interface AccountBalances {
   eth: string;
   rep: string;
   dai: string;
+  usdt: string;
+  usdc: string;
   legacyRep: string;
   attoRep: string;
   legacyAttoRep?: string;
