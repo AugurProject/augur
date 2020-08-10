29d42d39ee0bc8ea5dea6ea5000f227ec944b201
JohnDanz <John.Danz87@gmail.com>
2020-08-10 15:25:35 -0500

    fixed a few issues chwy noticed in the create market flow for certain templates

--- packages/augur-ui/src/utils/format-date.ts
+++ packages/augur-ui/src/utils/format-date.ts
@@ -204,10 +204,11 @@ export function buildformattedDate(
 export function timestampComponents(timestamp: number, offset: number = 0, timezone: string = null): Partial<DateTimeComponents> {
   // using local mode with moment, manually adjusting for offset
   const date = moment.unix(timestamp).add(offset, 'hours');
+
   return {
     setEndTime: timestamp,
     hour: String(date.utc().format('h')),
-    minute: String(date.utc().format('m')),
+    minute: String(date.utc().format('mm')),
     meridiem: String(date.utc().format('A')),
     timezone
   }
