479d42735c2754f09216679dd250b06dc17106b4
JohnDanz <John.Danz87@gmail.com>
2020-08-11 13:25:50 -0500

    fixed syntax error

--- packages/augur-ui/src/modules/market-charts/sports-group-charts.tsx
+++ packages/augur-ui/src/modules/market-charts/sports-group-charts.tsx
@@ -3,14 +3,13 @@ import classNames from 'classnames';
 import PriceHistory from 'modules/market-charts/components/price-history/price-history';
 import Styles from 'modules/market-charts/sports-group-charts.styles';
 import { selectMarket } from 'modules/markets/selectors/market';
-import { outcome } from 'modules/market-cards/common.styles.less';
 
 export const SportsGroupCharts = ({ sportsGroup }) => {
   const market = selectMarket(sportsGroup.markets[0].id);
   const outcomes = market.outcomesFormatted;
   const invalid = outcomes.shift();
   outcomes.push(invalid);
-  
+
   return (
     <section className={Styles.Container}>
       <div className={Styles.ChartArea}>
@@ -25,7 +24,7 @@ export const SportsGroupCharts = ({ sportsGroup }) => {
         <ul>
           {outcomes.map(outcome => {
             return (
-              <li key={outcome.id} className={classNames({[`Styles.${outcome.id}`]: true}}>
+              <li key={outcome.id} className={classNames({[`Styles.${outcome.id}`]: true})}>
                 <span>{outcome.description}</span>
                 <span>{outcome.lastPricePercent.full}</span>
               </li>
