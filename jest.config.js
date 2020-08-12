05bdbe971270c681b74690c10168d7362b9a9694
Justin Barry <328965+justinbarry@users.noreply.github.com>
2020-08-12 14:47:18 -0700

    Add jest config to augur-utils and set explicit jest deps in sdk

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   projects: [
     '<rootDir>/packages/augur-sdk',
     '<rootDir>/packages/augur-ui',
+    '<rootDir>/packages/augur-utils',
     '<rootDir>/packages/augur-test',
     '<rootDir>/packages/ethersjs-provider',
   ],
