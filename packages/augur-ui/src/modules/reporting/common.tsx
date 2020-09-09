895acf29f27e3b8364f4bfdf6b687c58248b35f8
phoebemirman <phoebe.mirman@gmail.com>
2020-09-09 11:28:55 -0700

    fix stake error not showing up in reporting form

--- packages/augur-ui/src/modules/reporting/common.tsx
+++ packages/augur-ui/src/modules/reporting/common.tsx
@@ -756,7 +756,6 @@ export const ReportingBondsView = ({
     showInput: false,
     disabled: market.marketType === SCALAR && migrateRep ? true : false,
     scalarError: '',
-    stakeError: '',
     isScalar: market.marketType === SCALAR,
     threshold: userAttoRep.toString(),
     readAndAgreedCheckbox: false,
@@ -766,12 +765,12 @@ export const ReportingBondsView = ({
       gasPrice
     ),
   });
+  const [stakeError, setStakeError] = useState('');
 
   const {
     showInput,
     disabled,
     scalarError,
-    stakeError,
     isScalar,
     threshold,
     readAndAgreedCheckbox,
@@ -832,25 +831,19 @@ export const ReportingBondsView = ({
     let disabled = false;
     if (isNaN(Number(inputStakeValue))) {
       disabled = true;
-      setState({ ...state, stakeError: 'Enter a valid number' });
+      setStakeError('Enter a valid number');
     } else if (
       createBigNumber(userAttoRep).lt(createBigNumber(inputStakeValue))
     ) {
       disabled = true;
-      setState({
-        ...state,
-        stakeError: 'Value is bigger than user REP balance',
-      });
+      setStakeError('Value is bigger than user REP balance');
     } else if (
       createBigNumber(threshold).lt(createBigNumber(inputStakeValue))
     ) {
       disabled = true;
-      setState({
-        ...state,
-        stakeError: `Value is bigger than the REP threshold: ${threshold}`,
-      });
+      setStakeError(`Value is bigger than the REP threshold: ${threshold}`);
     } else {
-      setState({ ...state, stakeError: '' });
+      setStakeError('');
     }
     let inputToAttoRep = '0';
     if (!isNaN(Number(inputStakeValue)) && inputStakeValue !== '') {
