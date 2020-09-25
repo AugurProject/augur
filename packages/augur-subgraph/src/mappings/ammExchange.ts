f5173db7b6ebe9c5e00b91a26fd424a0cf40e748
Justin Barry <328965+justinbarry@users.noreply.github.com>
2020-09-25 16:08:12 -0700

    Add ammExchange add/remove handler stubs

---
+++ packages/augur-subgraph/src/mappings/ammExchange.ts
@@ -0,0 +1,12 @@
+import {
+  AddLiquidityCall,
+  RemoveLiquidityCall,
+} from '../../generated/templates/AMMExchange/AMMExchange';
+
+export function handleAddLiquidity(call: AddLiquidityCall) {
+
+}
+
+export function handleRemoveLiquidity(call: RemoveLiquidityCall) {
+
+}
