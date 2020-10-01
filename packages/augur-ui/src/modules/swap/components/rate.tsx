48dbb571a6a18c77659808d43dffe15c65f7a87b
Andrew D'Amelio <dev@andrewdamel.io>
2020-10-01 16:16:42 -0400

    update addfunds, swap

--- packages/augur-ui/src/modules/swap/components/rate.tsx
+++ packages/augur-ui/src/modules/swap/components/rate.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { DAI, REP, ETH, USDC, USDT } from 'modules/common/constants';
 import { BigNumber, createBigNumber } from 'utils/create-big-number';
-import { formatEther, formatRep, formatDai } from 'utils/format-number';
+import { formatEther, formatDai } from 'utils/format-number';
+import { FormattedNumber } from 'modules/types';
 
 import Styles from 'modules/swap/components/rate.styles.less';
 
@@ -10,6 +11,10 @@ interface RateProps {
   swapForToken: string;
   repRate: BigNumber;
   ethRate: BigNumber;
+  ethToDaiRate: FormattedNumber;
+  repToDaiRate: FormattedNumber;
+  usdcToDaiRate: FormattedNumber;
+  usdtToDaiRate: FormattedNumber;
 }
 
 export const Rate = ({
@@ -17,25 +22,58 @@ export const Rate = ({
   swapForToken,
   repRate,
   ethRate,
+  ethToDaiRate,
+  repToDaiRate,
+  usdcToDaiRate,
+  usdtToDaiRate,
 }: RateProps) => {
   let displayRate = null;
-  if (swapForToken === REP) {
-    const rate =
-      baseToken === DAI || baseToken === USDC || baseToken === USDT
-        ? formatDai(repRate).formattedValue + ' DAI'
-        : formatEther(repRate.multipliedBy(ethRate)).formattedValue + ' ETH';
 
-    displayRate = `1 ${swapForToken} = ${rate}`;
-  } else {
-    const rate =
-      baseToken === USDC || baseToken === USDT
-        ? `1 ${baseToken}`
-        : baseToken === ETH
-        ? formatEther(ethRate).formattedValue + ' ETH'
-        : formatRep(createBigNumber(1).dividedBy(repRate)).formattedValue +
-          ' REP';
+  const usdtDaiRate = usdtToDaiRate.value / 10**12;
+  const usdcDaiRate = usdcToDaiRate.value / 10**12;
+  // Rates for converting TO DAI
+  if (swapForToken === DAI) {
+    if (baseToken === REP) {
+      displayRate = `1 REPv2 = $${formatDai(repToDaiRate.value).formattedValue}`;
+    }
+    else if (baseToken === USDT) {
 
-    displayRate = `1 ${swapForToken} = ${rate}`;
+      displayRate = `1 USDT = ${formatEther(usdtDaiRate).formattedValue} DAI`;
+    }
+    else if (baseToken === USDC) {
+      displayRate = `1 USDC = ${formatEther(usdcDaiRate).formattedValue} DAI`;
+    }
+    else if (baseToken === ETH) {
+      displayRate = `1 ETH = $${formatDai(ethToDaiRate.value).formattedValue}`;
+    }
+  }
+
+  // Rates for converting TO REPv2
+  else if (swapForToken === REP) {
+    const repInDai = repRate.multipliedBy(ethToDaiRate.value);
+
+    if (baseToken === ETH) {
+      displayRate = `1 REPv2 = ${formatEther(repRate).formattedValue} ETH`;
+    }
+    else if (baseToken === DAI) {
+      displayRate = `1 REPv2 = ${formatDai(repInDai).formattedValue} DAI`;
+    }
+  }
+
+  // Rates for converting TO ETH
+  else if (swapForToken === ETH) {
+    if (baseToken === REP) {
+      displayRate = `1 REPv2 = ${formatEther(repRate).formattedValue} ETH`;
+    }
+    else if (baseToken === DAI) {
+      displayRate = `1 DAI = ${formatEther(ethRate).formattedValue} ETH`;
+    }
+    else if (baseToken === USDC) {
+      displayRate = `1 USDC = ${formatEther((createBigNumber(usdcDaiRate)).multipliedBy(ethRate)).formattedValue} ETH`;
+    }
+    else if (baseToken === USDT) {
+      displayRate = `1 USDT = ${formatEther((createBigNumber(usdtDaiRate)).multipliedBy(ethRate)).formattedValue} ETH`;
+    }
   }
 
   return (
