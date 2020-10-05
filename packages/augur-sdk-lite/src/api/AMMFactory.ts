c59bda6348f00bce6f33da152862bf7ae2ff1b66
Justin Barry <328965+justinbarry@users.noreply.github.com>
2020-10-05 16:36:46 -0700

    Add getAMMExchange method to lite AMMFactory

--- packages/augur-sdk-lite/src/api/AMMFactory.ts
+++ packages/augur-sdk-lite/src/api/AMMFactory.ts
@@ -1,5 +1,6 @@
 import { ethers } from 'ethers';
 import { AMMFactoryAbi } from '../abi/AMMFactoryAbi';
+import { AMMExchange } from './AMMExchange';
 
 export class AMMFactory {
   private readonly provider: ethers.providers.Provider;
@@ -9,6 +10,17 @@ export class AMMFactory {
     this.contract = new ethers.Contract(address, AMMFactoryAbi, provider);
 
     this.provider = provider;
+  }
+
+  async getAMMExchange(marketAddress: string, collateralAddress: string): Promise<AMMExchange> {
+    const paras = await this.contract.exchanges(marketAddress) || {};
+
+    const amm = paras[collateralAddress];
+    if(typeof amm === 'undefined') {
+      throw new Error(`Market/Collateral pair ${marketAddress}/${collateralAddress} does not exist.`);
+    }
+
+    return new AMMExchange(this.provider, amm);
   }
 
 }
