9457e283fb2e8a2f7b13e6b808b3fef635d65532
JohnDanz <John.Danz87@gmail.com>
2020-10-15 15:16:22 -0500

    updated view market details link to show for any futures markets regardless of outcomes amount. was only showing on greater than 4 outcomes markets previously.

--- packages/augur-ui/src/modules/market-cards/common.tsx
+++ packages/augur-ui/src/modules/market-cards/common.tsx
@@ -963,7 +963,7 @@ export const SportsMarketContainer = ({
         ): null}
       </header>
       <div>{innerContent}</div>
-      {isGrid && <OutcomeGroupFooter market={market} />}
+      {isFutures && <OutcomeGroupFooter market={market} />}
     </section>
   );
 };
