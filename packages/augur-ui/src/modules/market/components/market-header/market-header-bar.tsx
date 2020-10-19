f61251f867a228c02f839d7fb355bfa6cab0566a
JohnDanz <John.Danz87@gmail.com>
2020-10-19 11:40:59 -0500

    fixed styling of backButton for trading ui when collapsed bar on trading page

--- packages/augur-ui/src/modules/market/components/market-header/market-header-bar.tsx
+++ packages/augur-ui/src/modules/market/components/market-header/market-header-bar.tsx
@@ -1,10 +1,9 @@
-import type { Getters } from '@augurproject/sdk';
 import { REPORTING_STATE } from 'modules/common/constants';
 import { Archived, InReportingLabel } from 'modules/common/labels';
 import { MarketProgress } from 'modules/common/progress';
 import Styles
   from 'modules/market/components/market-header/market-header-bar.styles.less';
-import { DateFormattedObject, MarketData } from 'modules/types';
+import { MarketData } from 'modules/types';
 import React from 'react';
 
 export interface MarketHeaderBarProps {
