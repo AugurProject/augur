65f83a8d805953611ba2f0b9c1c70472f614e520
Paul Gebheim <pgebheim@gmail.com>
2020-10-29 17:56:12 +0000

    Update docker stuff to work w/ para

--- support/entrypoint.sh
+++ support/entrypoint.sh
@@ -7,10 +7,16 @@ printf "\nBuilding client....\n\n";
 # This is an attempt to avoid memeory limitation in processes spawned from node.
 cd packages/augur-ui;
 
-ENABLE_MAINNET=true REPORTING_ONLY=true ./node_modules/.bin/webpack;
-mv build reporting-only-build;
+export ENABLE_MAINNET=true
 
-ENABLE_MAINNET=true ./node_modules/.bin/webpack;
+#REPORTING_ONLY=true ./node_modules/.bin/webpack;
+#mv build reporting-only-build;
+mkdir reporting-only-build
+
+PARA_AUGUR_DEPLOY_TOKEN_NAME=WETH ./node_modules/.bin/webpack;
+#mv build para-augur-weth;
+
+#./node_modules/.bin/webpack;
 
 cd -;
 
