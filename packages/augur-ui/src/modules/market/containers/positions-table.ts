2aa899a124276e0051004b99eb34c5bd056d6333
Tom Haile <bradford.haile@gmail.com>
2020-11-05 18:02:00 -0600

    hack to get user's external token balances in positions, they don't show up in portfolio only in market position section

--- packages/augur-ui/src/modules/market/containers/positions-table.ts
+++ packages/augur-ui/src/modules/market/containers/positions-table.ts
@@ -3,17 +3,18 @@ import { withRouter } from "react-router-dom";
 import { AppState } from "appStore";
 import { MarketPositionsTable } from "modules/portfolio/components/common/market-positions-table";
 import { selectUserMarketPositions } from "modules/markets/selectors/select-user-market-positions";
+import { createBigNumber } from "utils/create-big-number";
+import { formatDai } from "utils/format-number";
+import { SHORT } from "modules/common/constants";
 
 const mapStateToProps = (state: AppState, ownProps: any) => {
   let marketId = ownProps.marketId;
   if (!marketId) {
     marketId = ownProps.market.id;
   }
-  let positions = [];
+  let  positions = selectUserMarketPositions(state, marketId);
   if (ownProps.positions) {
-    positions = ownProps.positions;
-  } else if (marketId) {
-    positions = selectUserMarketPositions(state, marketId);
+    positions = mergePositions(ownProps.positions, positions);
   }
 
   return {
@@ -28,3 +29,17 @@ const PositionsTable = withRouter(
 );
 
 export default PositionsTable;
+
+const mergePositions = (propertyPos, selectedPos) => {
+  const hasShort = selectedPos.find(s => s.type === SHORT)
+  if (hasShort) return selectedPos;
+  return propertyPos.reduce((p, r) => {
+    const sPosition = p.find(pos => pos.outcomeId === r.outcomeId)
+    if (!sPosition) return [...p, r]
+    if (createBigNumber(sPosition.quantity.value).lt(createBigNumber(r.quantity.value))) {
+      sPosition.quantity = formatDai(createBigNumber(sPosition.quantity.value).plus(createBigNumber(r.quantity.value)))
+    }
+    return p
+  }, selectedPos)
+
+}
