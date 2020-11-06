1b67dc6d0d43abb86f0357421a46964c96f07f58
Justin Barry <328965+justinbarry@users.noreply.github.com>
2020-11-05 22:39:12 -0800

    Add augur subgraph base package

---
+++ packages/augur-subgraph-base/src/AugurTrading.ts
@@ -0,0 +1,128 @@
+import { Address, BigInt, Bytes } from '@graphprotocol/graph-ts';
+import {
+  CancelZeroXOrder as CancelZeroXOrderEvent,
+  MarketVolumeChanged as MarketVolumeChangedEvent,
+  OrderEvent as OrderEventEvent,
+  ProfitLossChanged as ProfitLossChangedEvent,
+} from '../generated/AugurTrading/AugurTrading';
+
+import {
+  CancelZeroXOrder as CancelZeroXOrderEntity,
+  MarketVolumeChanged as MarketVolumeChangedEntity,
+  OrderEvent as OrderEventEntity,
+  ProfitLossChanged as ProfitLossChangedEntity,
+} from '../generated/schema';
+
+
+function mapAddressArray(arr:Address[]):string[] {
+  let result = new Array<string>(arr.length);
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i].toHexString());
+  }
+
+  return result;
+}
+
+function mapByteArray(arr:Bytes[]):string[] {
+  let result = new Array<string>(arr.length);
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i].toHexString());
+  }
+
+  return result;
+}
+
+function mapArray(arr: BigInt[]):string[] {
+  let result = new Array<string>(arr.length);
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i].toHexString());
+  }
+
+  return result;
+}
+
+
+export function handleCancelZeroXOrder(event: CancelZeroXOrderEvent): void {
+  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let entity = new CancelZeroXOrderEntity(id);
+
+  entity.blockHash = event.block.hash.toHexString();
+  entity.logIndex = event.logIndex.toString();
+  entity.name = "CancelZeroXOrder";
+  entity.transactionHash = event.transaction.hash.toHexString();
+
+  entity.universe = event.params.universe.toHexString();
+  entity.market = event.params.market.toHexString();
+  entity.account = event.params.account.toHexString();
+  entity.outcome = event.params.outcome.toHexString();
+  entity.price = event.params.price.toHexString();
+  entity.amount = event.params.amount.toHexString();
+  entity.orderType = BigInt.fromI32(event.params.orderType).toHexString();
+  entity.orderHash = event.params.orderHash;
+
+  entity.save();
+}
+
+export function handleMarketVolumeChanged(event: MarketVolumeChangedEvent): void {
+  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let entity = new MarketVolumeChangedEntity(id);
+
+  entity.blockHash = event.block.hash.toHexString();
+  entity.logIndex = event.logIndex.toString();
+  entity.name = "MarketVolumeChanged";
+  entity.transactionHash = event.transaction.hash.toHexString();
+
+  entity.universe = event.params.universe.toHexString();
+  entity.market = event.params.market.toHexString();
+  entity.volume = event.params.volume.toHexString();
+  entity.outcomeVolumes = mapArray(event.params.outcomeVolumes);
+  entity.totalTrades = event.params.totalTrades.toHexString();
+  entity.timestamp = event.params.timestamp.toHexString();
+
+  entity.save();
+}
+
+export function handleOrderEvent(event: OrderEventEvent): void {
+  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let entity = new OrderEventEntity(id);
+
+  entity.blockHash = event.block.hash.toHexString();
+  entity.logIndex = event.logIndex.toString();
+  entity.name = "OrderEvent";
+  entity.transactionHash = event.transaction.hash.toHexString();
+
+  entity.universe = event.params.universe.toHexString();
+  entity.market = event.params.market.toHexString();
+  entity.eventType = BigInt.fromI32(event.params.eventType).toHexString();
+  entity.orderType = BigInt.fromI32(event.params.orderType).toHexString();
+  entity.orderId = event.params.orderId;
+  entity.tradeGroupId = event.params.tradeGroupId;
+  entity.addressData = mapAddressArray(event.params.addressData);
+  entity.uint256Data = mapArray(event.params.uint256Data);
+
+  entity.save();
+}
+
+export function handleProfitLossChanged(event: ProfitLossChangedEvent): void {
+  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let entity = new ProfitLossChangedEntity(id);
+
+  entity.blockHash = event.block.hash.toHexString();
+  entity.logIndex = event.logIndex.toString();
+  entity.name = "ProfitLossChanged";
+  entity.transactionHash = event.transaction.hash.toHexString();
+
+  entity.universe = event.params.universe.toHexString();
+  entity.market = event.params.market.toHexString();
+  entity.account = event.params.account.toHexString();
+  entity.outcome = event.params.outcome.toHexString();
+  entity.netPosition = event.params.netPosition.toHexString();
+  entity.avgPrice = event.params.avgPrice.toHexString();
+  entity.realizedProfit = event.params.realizedProfit.toHexString();
+  entity.frozenFunds = event.params.frozenFunds.toHexString();
+  entity.realizedCost = event.params.realizedCost.toHexString();
+  entity.timestamp = event.params.timestamp.toHexString();
+
+  entity.save();
+}
+
---
+++ packages/augur-subgraph-base/src/ShareToken.ts
@@ -0,0 +1,101 @@
+import { BigInt, Bytes } from '@graphprotocol/graph-ts';
+import {
+  ApprovalForAll as ApprovalForAllEvent,
+  TransferBatch as TransferBatchEvent,
+  TransferSingle as TransferSingleEvent,
+  URI as URIEvent,
+} from '../generated/ShareToken/ShareToken';
+
+import {
+  ApprovalForAll as ApprovalForAllEntity,
+  TransferBatch as TransferBatchEntity,
+  TransferSingle as TransferSingleEntity,
+  URI as URIEntity,
+} from '../generated/schema';
+
+function mapByteArray(arr:Bytes[]):string[] {
+  let result = new Array<string>(arr.length);
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i].toHexString());
+  }
+
+  return result;
+}
+
+function mapArray(arr: BigInt[]):string[] {
+  let result = new Array<string>(arr.length);
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i].toHexString());
+  }
+
+  return result;
+}
+
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let entity = new ApprovalForAllEntity(id);
+
+  entity.blockHash = event.block.hash.toHexString();
+  entity.logIndex = event.logIndex.toString();
+  entity.name = "ApprovalForAll";
+  entity.transactionHash = event.transaction.hash.toHexString();
+
+  entity.owner = event.params.owner.toHexString();
+  entity.operator = event.params.operator.toHexString();
+  entity.approved = event.params.approved;
+
+  entity.save();
+}
+
+export function handleTransferBatch(event: TransferBatchEvent): void {
+  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let entity = new TransferBatchEntity(id);
+
+  entity.blockHash = event.block.hash.toHexString();
+  entity.logIndex = event.logIndex.toString();
+  entity.name = "TransferBatch";
+  entity.transactionHash = event.transaction.hash.toHexString();
+
+  entity.operator = event.params.operator.toHexString();
+  entity.from = event.params.from.toHexString();
+  entity.to = event.params.to.toHexString();
+  entity.ids = mapArray(event.params.ids);
+  entity.values = mapArray(event.params.values);
+
+  entity.save();
+}
+
+export function handleTransferSingle(event: TransferSingleEvent): void {
+  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let entity = new TransferSingleEntity(id);
+
+  entity.blockHash = event.block.hash.toHexString();
+  entity.logIndex = event.logIndex.toString();
+  entity.name = "TransferSingle";
+  entity.transactionHash = event.transaction.hash.toHexString();
+
+  entity.operator = event.params.operator.toHexString();
+  entity.from = event.params.from.toHexString();
+  entity.to = event.params.to.toHexString();
+  entity.id = event.params.id.toHexString();
+  entity.value = event.params.value.toHexString();
+
+  entity.save();
+}
+
+export function handleURI(event: URIEvent): void {
+  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let entity = new URIEntity(id);
+
+  entity.blockHash = event.block.hash.toHexString();
+  entity.logIndex = event.logIndex.toString();
+  entity.name = "URI";
+  entity.transactionHash = event.transaction.hash.toHexString();
+
+  entity.value = event.params.value;
+  entity.id = event.params.id.toHexString();
+
+  entity.save();
+}
+
