d3042e5379c5af20ce52e2b7c1625ac68bfab3c7
JohnDanz <John.Danz87@gmail.com>
2020-11-17 10:39:41 -0600

    removed logs

--- packages/augur-ui/src/modules/app/store/app-status-hooks.ts
+++ packages/augur-ui/src/modules/app/store/app-status-hooks.ts
@@ -298,7 +298,6 @@ export function AppStatusReducer(state, action) {
       break;
     }
     case SET_CATEGORY_STATS: {
-      console.log("set Category Stats", action.categoryStats);
       updatedState[CATEGORY_STATS] = action.categoryStats;
       break;
     }
@@ -665,7 +664,6 @@ export function AppStatusReducer(state, action) {
       break;
     }
     case UPDATE_MARKETS_LIST: {
-      console.log("update Markets List", action.data);
       updatedState[MARKETS_LIST] = {
         ...updatedState[MARKETS_LIST],
         ...action.data,
