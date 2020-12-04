e46e274f3cbf2b6f0563aec1a1f4f95cd7622b6e
JohnDanz <John.Danz87@gmail.com>
2020-12-04 12:26:27 -0600

    some initial setup to more closely match what we are doing in augur-ui. still needs work to enable less.

--- packages/augur-simplified/src/modules/App.test.ts
+++ packages/augur-simplified/src/modules/App.test.ts
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from 'modules/App';
 
 test('renders learn react link', () => {
   render(<App />);
--- packages/augur-simplified/src/modules/App.tsx
+++ packages/augur-simplified/src/modules/App.tsx
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// @ts-ignore
+import logo from '../logo.svg';
+import 'modules/App.styles.css';
 
 function App() {
   return (
@@ -7,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload!
         </p>
         <a
           className="App-link"
