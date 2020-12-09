b3fdfc8cc8933f44c654b71202fe8c8257884b33
phoebemirman <phoebe.mirman@gmail.com>
2020-12-09 11:07:18 -0800

    market pg dropdown

--- packages/augur-simplified/src/modules/common/selection.tsx
+++ packages/augur-simplified/src/modules/common/selection.tsx
@@ -1,252 +1,251 @@
-// import React, { Component, useState, useEffect, useRef } from 'react';
-// import classNames from 'classnames';
-// import Styles from 'modules/common/logo.styles.less';
+import React, { Component, useState, useEffect, useRef } from 'react';
+import classNames from 'classnames';
+import Styles from 'modules/common/selection.styles.less';
+import { SimpleChevron } from './icons';
 
-// export interface NameValuePair {
-//   label: string;
-//   value: string | number;
-// }
+export interface NameValuePair {
+  label: string;
+  value: string | number;
+}
 
-// export interface DropdownProps {
-//   id?: string;
-//   onChange: any;
-//   className?: string;
-//   defaultValue?: string | number;
-//   options: NameValuePair[];
-//   large?: boolean;
-//   staticLabel?: string;
-//   staticMenuLabel?: string;
-//   stretchOutOnMobile?: boolean;
-//   sortByStyles?: object;
-//   openTop?: boolean;
-//   stretchOut?: boolean;
-//   activeClassName?: string;
-//   showColor?: boolean;
-//   disabled?: boolean;
-//   sort?: boolean;
-//   preLabel?: string;
-//   preLabelClean?: boolean;
-//   dontCheckInvalid?: boolean;
-// }
+export interface DropdownProps {
+  id?: string;
+  onChange: any;
+  className?: string;
+  defaultValue?: string | number;
+  options: NameValuePair[];
+  large?: boolean;
+  staticLabel?: string;
+  staticMenuLabel?: string;
+  stretchOutOnMobile?: boolean;
+  sortByStyles?: object;
+  openTop?: boolean;
+  stretchOut?: boolean;
+  activeClassName?: string;
+  showColor?: boolean;
+  disabled?: boolean;
+  sort?: boolean;
+  preLabel?: string;
+  preLabelClean?: boolean;
+  dontCheckInvalid?: boolean;
+}
 
-// interface DropdownState {
-//   selected: NameValuePair;
-//   showList: boolean;
-//   sortedList: NameValuePair[];
-//   scrollWidth?: number;
-//   clientWidth?: number;
-//   isDisabled?: boolean;
-// }
+interface DropdownState {
+  selected: NameValuePair;
+  showList: boolean;
+  sortedList: NameValuePair[];
+  scrollWidth?: number;
+  clientWidth?: number;
+  isDisabled?: boolean;
+}
 
-// function findSelected(options, defaultVal) {
-//   const foundOption = options.find(
-//     o => o.value === defaultVal
-//   );
-//   const defaultValue = defaultVal ? {
-//     label: defaultVal.toString(),
-//     value: defaultVal,
-//   } : null;
+function findSelected(options, defaultVal) {
+  const foundOption = options.find(
+    o => o.value === defaultVal
+  );
+  const defaultValue = defaultVal ? {
+    label: defaultVal.toString(),
+    value: defaultVal,
+  } : null;
 
-//   return foundOption ? foundOption : defaultValue;
-// }
+  return foundOption ? foundOption : defaultValue;
+}
 
-// class Dropdown extends Component<DropdownProps, DropdownState> {
-//   state: DropdownState = {
-//     selected: this.props.defaultValue !== null
-//         ? findSelected(this.props.options, this.props.defaultValue)
-//         : null,
-//     showList: false,
-//     scrollWidth: null,
-//     clientWidth: null,
-//     isDisabled: true,
-//     sortedList:
-//       this.props.sort && this.props.options
-//         ? this.props.options.sort((a, b) => (a.label > b.label ? 1 : -1))
-//         : this.props.options,
-//   };
-//   labelRef: any;
+class Dropdown extends Component<DropdownProps, DropdownState> {
+  state: DropdownState = {
+    selected: this.props.defaultValue !== null
+        ? findSelected(this.props.options, this.props.defaultValue)
+        : null,
+    showList: false,
+    scrollWidth: null,
+    clientWidth: null,
+    isDisabled: true,
+    sortedList:
+      this.props.sort && this.props.options
+        ? this.props.options.sort((a, b) => (a.label > b.label ? 1 : -1))
+        : this.props.options,
+  };
+  labelRef: any;
 
-//   componentDidMount() {
-//     this.measure();
-//    // window.addEventListener('click', this.handleWindowOnClick);
-//   }
+  componentDidMount() {
+    this.measure();
+//    window.addEventListener('click', this.handleWindowOnClick);
+  }
 
-//   componentWillUnmount() {
-//    // window.removeEventListener('click', this.handleWindowOnClick);
-//   }
+  componentWillUnmount() {
+//    window.removeEventListener('click', this.handleWindowOnClick);
+  }
 
-//   componentDidUpdate(prevProps: DropdownProps, prevState: DropdownState) {
-//     this.measure();
-//     if (prevProps.defaultValue !== this.props.defaultValue) {
-//       this.setState({
-//         selected: findSelected(this.props.options, this.props.defaultValue)
-//       })
-//     }
-//     if (
-//       JSON.stringify(this.props.options) !==
-//         JSON.stringify(prevProps.options) ||
-//       this.props.sort !== prevProps.sort
-//     ) {
-//       const sortedList =
-//         this.props.sort && this.props.options
-//           ? this.props.options.sort((a, b) => (a.label > b.label ? 1 : -1))
-//           : this.props.options;
-//       this.setState({
-//         sortedList,
-//       });
-//     }
-//   }
+  componentDidUpdate(prevProps: DropdownProps, prevState: DropdownState) {
+    this.measure();
+    if (prevProps.defaultValue !== this.props.defaultValue) {
+      this.setState({
+        selected: findSelected(this.props.options, this.props.defaultValue)
+      })
+    }
+    if (
+      JSON.stringify(this.props.options) !==
+        JSON.stringify(prevProps.options) ||
+      this.props.sort !== prevProps.sort
+    ) {
+      const sortedList =
+        this.props.sort && this.props.options
+          ? this.props.options.sort((a, b) => (a.label > b.label ? 1 : -1))
+          : this.props.options;
+      this.setState({
+        sortedList,
+      });
+    }
+  }
 
-//   shouldComponentUpdate(nextProps: DropdownProps, nextState: DropdownState) {
-//     if (
-//       nextState.selected !== this.state.selected ||
-//       nextState.showList !== this.state.showList ||
-//       this.props.disabled !== nextProps.disabled ||
-//       this.props.staticLabel !== nextProps.staticLabel ||
-//       this.props.defaultValue !== nextProps.defaultValue ||
-//       JSON.stringify(this.props.options) !== JSON.stringify(nextProps.options) ||
-//       this.props.className !== nextProps.className
-//     ) {
-//       return true;
-//     }
+  shouldComponentUpdate(nextProps: DropdownProps, nextState: DropdownState) {
+    if (
+      nextState.selected !== this.state.selected ||
+      nextState.showList !== this.state.showList ||
+      this.props.disabled !== nextProps.disabled ||
+      this.props.staticLabel !== nextProps.staticLabel ||
+      this.props.defaultValue !== nextProps.defaultValue ||
+      JSON.stringify(this.props.options) !== JSON.stringify(nextProps.options) ||
+      this.props.className !== nextProps.className
+    ) {
+      return true;
+    }
 
-//     return (
-//       this.state.scrollWidth !== nextState.scrollWidth ||
-//       this.state.clientWidth !== nextState.clientWidth
-//     );
-//   }
+    return (
+      this.state.scrollWidth !== nextState.scrollWidth ||
+      this.state.clientWidth !== nextState.clientWidth
+    );
+  }
 
-//   measure = () => {
-//     if (!this.labelRef) return;
-//     const { clientWidth, scrollWidth } = this.labelRef;
+  measure = () => {
+    if (!this.labelRef) return;
+    const { clientWidth, scrollWidth } = this.labelRef;
 
-//     this.setState({
-//       scrollWidth,
-//       clientWidth,
-//       isDisabled: !(scrollWidth > clientWidth),
-//     });
-//   };
+    this.setState({
+      scrollWidth,
+      clientWidth,
+      isDisabled: !(scrollWidth > clientWidth),
+    });
+  };
 
-//   refDropdown: any = null;
+  refDropdown: any = null;
 
-//   dropdownSelect = (selected: NameValuePair) => {
-//     const { onChange } = this.props;
-//     if (selected !== this.state.selected) {
-//       this.setState({
-//         selected,
-//       });
+  dropdownSelect = (selected: NameValuePair) => {
+    const { onChange } = this.props;
+    if (selected !== this.state.selected) {
+      this.setState({
+        selected,
+      });
 
-//       if (onChange) {
-//         onChange(selected.value);
-//       }
+      if (onChange) {
+        onChange(selected.value);
+      }
 
-//       this.toggleList();
-//     }
-//   };
+      this.toggleList();
+    }
+  };
 
-//   toggleList = () => {
-//     this.setState({ showList: !this.state.showList });
-//   };
+  toggleList = () => {
+    this.setState({ showList: !this.state.showList });
+  };
 
-//   handleWindowOnClick = (event: React.MouseEvent<HTMLElement>) => {
-//     if (this.refDropdown && !this.refDropdown.contains(event.target)) {
-//       this.setState({ showList: false });
-//     }
-//   };
+  handleWindowOnClick = (event: React.MouseEvent<HTMLElement>) => {
+    if (this.refDropdown && !this.refDropdown.contains(event.target)) {
+      this.setState({ showList: false });
+    }
+  };
 
-//   render() {
-//     const {
-//       sortByStyles,
-//       large,
-//       stretchOutOnMobile,
-//       openTop,
-//       className,
-//       activeClassName,
-//       staticLabel,
-//       id,
-//       showColor,
-//       disabled,
-//       preLabel,
-//       dontCheckInvalid,
-//       preLabelClean,
-//     } = this.props;
-//     const { selected, showList, isDisabled, sortedList } = this.state;
-//     return (
-//       <div
-//         style={sortByStyles}
-//         className={classNames(className, {
-//           [Styles.Large]: large,
-//           [Styles.Normal]: !large,
-//           [Styles.stretchOut]: stretchOutOnMobile,
-//           [Styles.isOpen]: showList,
-//           [Styles.openTop]: openTop,
-//           [`${activeClassName}`]: showList,
-//           [Styles.showColor]: showColor,
-//           [Styles.Disabled]: disabled,
-//         })}
-//         ref={dropdown => {
-//           this.refDropdown = dropdown;
-//         }}
-//         role="button"
-//         tabIndex={0}
-//         onClick={this.toggleList}
-//         data-tip
-//         data-for={'dropdown-' + id + staticLabel}
-//         data-iscapture={true}
-//       >
-//         {preLabel && <span>{`${preLabel}${preLabelClean ? '' : ':'}`}</span>}
-//         <button
-//           className={classNames(Styles.label, {
-//             [Styles.SelectedLabel]: selected,
-//             [Styles.invalidColor]: !dontCheckInvalid && selected?.value === INVALID_OUTCOME_ID,
-//           })}
-//         >
-//           <span ref={ref => (this.labelRef = ref)}>
-//             {selected ? selected.label : staticLabel}
-//           </span>
-//         </button>
-//         <div>
-//           <div
-//             className={classNames(Styles.list, {
-//               [`${Styles.active}`]: showList,
-//             })}
-//           >
-//             {sortedList.map(option => (
-//               <button
-//                 key={`${option.value}${option.label}`}
-//                 value={option.value}
-//                 onClick={() => this.dropdownSelect(option)}
-//                 className={classNames({
-//                   [Styles.Selected]: option?.value === selected?.value,
-//                   [Styles.invalidColor]: !dontCheckInvalid && option?.value === INVALID_OUTCOME_ID,
-//                 })}
-//               >
-//                 {option.label}
-//               </button>
-//             ))}
-//           </div>
-//         </div>
-//         {selected && (
-//           <select
-//             onChange={e => {
-//               this.dropdownSelect(e.target.options[e.target.selectedIndex]);
-//             }}
-//             value={selected.value}
-//           >
-//             {sortedList.map(option => (
-//               <option
-//                 key={`${option.value}${option.label}`}
-//                 value={option.value}
-//               >
-//                 {option.label}
-//               </option>
-//             ))}
-//           </select>
-//         )}
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    const {
+      sortByStyles,
+      large,
+      stretchOutOnMobile,
+      openTop,
+      className,
+      activeClassName,
+      staticLabel,
+      id,
+      showColor,
+      disabled,
+      preLabel,
+      preLabelClean,
+    } = this.props;
+    const { selected, showList, isDisabled, sortedList } = this.state;
+    return (
+      <div
+        style={sortByStyles}
+        className={classNames(className, {
+          [Styles.Large]: large,
+          [Styles.Normal]: !large,
+          [Styles.stretchOut]: stretchOutOnMobile,
+          [Styles.isOpen]: showList,
+          [Styles.openTop]: openTop,
+          [`${activeClassName}`]: showList,
+          [Styles.showColor]: showColor,
+          [Styles.Disabled]: disabled,
+        })}
+        ref={dropdown => {
+          this.refDropdown = dropdown;
+        }}
+        role="button"
+        tabIndex={0}
+        onClick={this.toggleList}
+        data-tip
+        data-for={'dropdown-' + id + staticLabel}
+        data-iscapture={true}
+      >
+        {preLabel && <span>{`${preLabel}${preLabelClean ? '' : ':'}`}</span>}
+        <button
+          className={classNames(Styles.label, {
+            [Styles.SelectedLabel]: selected,
+          })}
+        >
+          <span ref={ref => (this.labelRef = ref)}>
+            {selected ? selected.label : staticLabel}
+          </span>
+          {SimpleChevron}
+        </button>
+        <div>
+          <div
+            className={classNames(Styles.list, {
+              [`${Styles.active}`]: showList,
+            })}
+          >
+            {sortedList.map(option => (
+              <button
+                key={`${option.value}${option.label}`}
+                value={option.value}
+                onClick={() => this.dropdownSelect(option)}
+                className={classNames({
+                  [Styles.Selected]: option?.value === selected?.value,
+                })}
+              >
+                {option.label}
+              </button>
+            ))}
+          </div>
+        </div>
+        {selected && (
+          <select
+            onChange={e => {
+              this.dropdownSelect(e.target.options[e.target.selectedIndex]);
+            }}
+            value={selected.value}
+          >
+            {sortedList.map(option => (
+              <option
+                key={`${option.value}${option.label}`}
+                value={option.value}
+              >
+                {option.label}
+              </option>
+            ))}
+          </select>
+        )}
+      </div>
+    );
+  }
+}
 
-// export const SquareDropdown = (props: DropdownProps) => <Dropdown {...props} />;
+export const SquareDropdown = (props: DropdownProps) => <Dropdown {...props} />;
 
--- packages/augur-simplified/src/modules/markets/markets-view.tsx
+++ packages/augur-simplified/src/modules/markets/markets-view.tsx
@@ -8,12 +8,14 @@ import { formatDai } from 'utils/format-number';
 import { EthIcon, PillIcon, UsdIcon } from 'modules/common/icons';
 import classNames from 'classnames';
 import { PrimaryButton } from 'modules/common/buttons';
+import { SquareDropdown } from 'modules/common/selection';
 
 const fakeMarketData = [
   {
-    id: "0",
+    id: '0',
     category: 'Covid-19',
-    description: "Will Pfizer's COVID-19 vaccine be the first to receive FDA approval or Emergency Use Authorization (EUA)?",
+    description:
+      "Will Pfizer's COVID-19 vaccine be the first to receive FDA approval or Emergency Use Authorization (EUA)?",
     volume: 350019,
     outcomes: [
       {
@@ -27,7 +29,7 @@ const fakeMarketData = [
     ],
   },
   {
-    id: "1",
+    id: '1',
     category: 'Us Politics',
     description: 'How many electoral college votes will be cast for Joe Biden?',
     volume: 350019,
@@ -43,7 +45,7 @@ const fakeMarketData = [
     ],
   },
   {
-    id: "2",
+    id: '2',
     category: 'Covid-19',
     description: "Will Pfizer's thing happen?",
     volume: 350019,
@@ -59,7 +61,7 @@ const fakeMarketData = [
     ],
   },
   {
-    id: "3",
+    id: '3',
     category: 'Us Politics',
     inUsd: true,
     description: 'How many electoral college votes?',
@@ -76,7 +78,7 @@ const fakeMarketData = [
     ],
   },
   {
-    id: "4",
+    id: '4',
     category: 'Covid-19',
     description: "Will Pfizer's thing happen?",
     volume: 350019,
@@ -92,7 +94,7 @@ const fakeMarketData = [
     ],
   },
   {
-    id: "5",
+    id: '5',
     category: 'Us Politics',
     inUsd: true,
     description: 'How many electoral college votes?',
@@ -109,7 +111,7 @@ const fakeMarketData = [
     ],
   },
   {
-    id: "6",
+    id: '6',
     category: 'Us Politics',
     description: 'How many electoral college votes?',
     volume: 0,
@@ -200,7 +202,7 @@ const MarketCard = ({ market }) => {
           {market.noLiquidity ? (
             <div>
               <span>Market requires Initial liquidity</span>
-              <PrimaryButton text='Earn fees as a liquidity provider' />
+              <PrimaryButton text="Earn fees as a liquidity provider" />
             </div>
           ) : (
             <>
@@ -222,10 +224,26 @@ const MarketsView = () => {
     <div className={Styles.MarketsView}>
       <MarketViewStats />
       <ul>
-        <li>filter dropdown</li>
-        <li>filter dropdown</li>
-        <li>filter dropdown</li>
-        <li>filter dropdown</li>
+        <SquareDropdown
+          onChange={() => null}
+          options={[{ label: 'Open', value: 0 }, { label: 'Closed', value: 1 }]}
+          defaultValue='All Markets'
+        />
+         <SquareDropdown
+          onChange={() => null}
+          options={[{ label: 'Open', value: 0 }, { label: 'Closed', value: 1 }]}
+          defaultValue='Volume'
+        />
+         <SquareDropdown
+          onChange={() => null}
+          options={[{ label: 'Open', value: 0 }, { label: 'Closed', value: 1 }]}
+          defaultValue='Open'
+        />
+         <SquareDropdown
+          onChange={() => null}
+          options={[{ label: 'Open', value: 0 }, { label: 'Closed', value: 1 }]}
+          defaultValue='All Currencies'
+        />
       </ul>
       <section>
         {fakeMarketData.map((market) => (
