0f2f35d6357e5c989b4c1bc65c4f40690c0272dc
JohnDanz <John.Danz87@gmail.com>
2020-12-09 11:34:44 -0600

    more work for setup of portfolio and market pages, updated topnav for twotone, updated site wide layout to allow for the proper scrolling and framing of content

--- packages/augur-simplified/src/modules/common/top-nav.tsx
+++ packages/augur-simplified/src/modules/common/top-nav.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useLocation } from 'react-router';
 import Styles from 'modules/common/top-nav.styles.less';
 import { Link } from 'react-router-dom';
 import { MARKETS, PORTFOLIO } from 'modules/constants';
 import makePath from 'modules/routes/helpers/make-path';
 import Logo from 'modules/common/logo';
+import parsePath from 'modules/routes/helpers/parse-path';
+import classNames from 'classnames';
 
 export const TopNav = () => {
+  const location = useLocation();
+  const path = parsePath(location.pathname)[0];
   return (
-    <nav className={Styles.TopNav}>
+    <nav className={classNames(Styles.TopNav, {
+      [Styles.TwoTone]: path !== MARKETS,
+    })}>
       <section>
         <Logo />
         <ol>
