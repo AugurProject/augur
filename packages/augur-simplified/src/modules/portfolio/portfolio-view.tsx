c528cb96858c5c443fbac50f97373ba831f153af
phoebemirman <phoebe.mirman@gmail.com>
2020-12-09 12:49:06 -0800

    update buttons

--- packages/augur-simplified/src/modules/portfolio/portfolio-view.tsx
+++ packages/augur-simplified/src/modules/portfolio/portfolio-view.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Styles from 'modules/portfolio/portfolio-view.styles.less';
 import { AppViewStats } from 'modules/common/labels';
 import Activity from './activity';
+import { SecondaryButton } from 'modules/common/buttons';
 
 export const PortfolioView = () => (
   <div className={Styles.PortfolioView}>
     <section>
       <AppViewStats />
+      <SecondaryButton text={'$24.00 in Winnings to claim'}/>
     </section>
     <section>
       <Activity />
