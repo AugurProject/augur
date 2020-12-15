6ca4527dbd52eefe565194f9f96830be27a2ea35
Tom Haile <bradford.haile@gmail.com>
2020-12-15 16:42:02 -0600

    fix blow up when no liquidity and calc slippage

--- packages/augur-amm-ui/src/components/swap/FormattedPriceImpact.tsx
+++ packages/augur-amm-ui/src/components/swap/FormattedPriceImpact.tsx
@@ -8,10 +8,9 @@ import { ErrorText } from './styleds'
  * Formatted version of price impact text with warning colors
  */
 export default function FormattedPriceImpact({ priceImpact }: { priceImpact?: Percent }) {
-  const slippage = priceImpact.toFixed(2);
   return (
     <ErrorText fontWeight={500} fontSize={14} severity={warningSeverity(priceImpact)}>
-      {priceImpact ? (priceImpact.lessThan(ONE_BIPS) ? '<0.01%' : `${slippage}%`) : '-'}
+      {priceImpact ? (priceImpact.lessThan(ONE_BIPS) ? '<0.01%' : `${priceImpact.toFixed(2)}%`) : '-'}
     </ErrorText>
   )
 }
