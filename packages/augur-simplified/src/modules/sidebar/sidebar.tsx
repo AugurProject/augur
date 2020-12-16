17a92e33cad8c854d59ab5008f6f621c5b46cf14
phoebemirman <phoebe.mirman@gmail.com>
2020-12-16 15:02:56 -0800

    style side nav

--- packages/augur-simplified/src/modules/sidebar/sidebar.tsx
+++ packages/augur-simplified/src/modules/sidebar/sidebar.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Styles from 'modules/sidebar/sidebar.styles.less';
-import { CloseIcon } from 'modules/common/icons';
+import { CloseIcon, GearIcon } from 'modules/common/icons';
 import { useAppStatusStore } from 'modules/stores/app-status';
 import { PrimaryButton, SecondaryButton } from 'modules/common/buttons';
 import { CheckboxGroup, RadioBarGroup } from 'modules/common/selection';
-import { SIDEBAR_TYPES } from 'modules/constants';
+import { MARKETS, PORTFOLIO, SIDEBAR_TYPES } from 'modules/constants';
+import Logo from 'modules/common/logo';
+import classNames from 'classnames';
+import makePath from 'modules/routes/helpers/make-path';
+import parsePath from 'modules/routes/helpers/parse-path';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router';
 
 const categoryItems = [
   {
@@ -92,13 +98,15 @@ const currencyItems = [
 
 interface SideBarHeaderProps {
   header?: string;
+  showLogo?: boolean;
 }
-const SideBarHeader = ({ header }: SideBarHeaderProps) => {
+const SideBarHeader = ({ header, showLogo }: SideBarHeaderProps) => {
   const {
     actions: { setSidebar },
   } = useAppStatusStore();
   return (
     <div className={Styles.Header}>
+      {showLogo && <Logo />}
       <span>{header}</span>
       <span onClick={() => setSidebar(null)}>{CloseIcon}</span>
     </div>
@@ -124,13 +132,29 @@ const FilterSideBar = () => {
 };
 
 const NavigationSideBar = () => {
+  const {
+    actions: { setSidebar },
+  } = useAppStatusStore();
+  const location = useLocation();
+  const path = parsePath(location.pathname)[0];
   return (
     <>
-      <SideBarHeader />
+      <SideBarHeader showLogo />
       <div className={Styles.Body}>
+        <ol>
+          <li className={classNames({ [Styles.Active]: path === MARKETS })}>
+            <Link onClick={() => setSidebar(null)} to={makePath(MARKETS)}>
+              Markets
+            </Link>
+          </li>
+          <li className={classNames({ [Styles.Active]: path === PORTFOLIO })}>
+            <Link onClick={() => setSidebar(null)} to={makePath(PORTFOLIO)}>
+              Portfolio
+            </Link>
+          </li>
+        </ol>
       </div>
-      <div className={Styles.Footer}>
-      </div>
+      <div className={Styles.NavigationFooter}>{GearIcon}</div>
     </>
   );
 };
