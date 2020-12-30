cc30131d84e8164ab3119ad376d384abadcb85c8
Paul Gebheim <pgebheim@users.noreply.github.com>
2020-12-29 16:39:40 -0800

    Update sidebars.js

--- augur.sh/sidebars.js
+++ augur.sh/sidebars.js
@@ -25,7 +25,7 @@ module.exports = {
       label: 'Architecture',
       items: [
         'contracts/overview',
-        'architecture/layer'
+        'architecture/layer2'
       ],
     },
     {
