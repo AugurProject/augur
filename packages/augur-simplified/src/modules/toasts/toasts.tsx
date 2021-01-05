ee62672561e63f2163c0607007e40fba5f11cee9
JohnDanz <John.Danz87@gmail.com>
2021-01-05 11:27:32 -0600

    added mocked up versions of toasts, will eventually need to integrate with state but for now this shows the basic idea.

--- packages/augur-simplified/src/modules/toasts/toasts.tsx
+++ packages/augur-simplified/src/modules/toasts/toasts.tsx
@@ -1,12 +1,68 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 
 import Styles from './toasts.styles.less';
 import { CloseIcon, FailedX, ConfirmedCheck } from '../common/icons';
 import { ReceiptLink } from '../routes/helpers/links';
 
+const MockToasts = [
+  {
+    txId: '0xdeadbeef',
+    status: 'CONFIRMED',
+    message: 'Claimed $24.32 in winnings.',
+    lastUpdated: new Date().getTime(),
+    seen: false,
+  },
+  {
+    txId: '0xdeadbeef2',
+    status: 'FAILED',
+    message: 'Claimed $85.90 in winnings.',
+    lastUpdated: new Date().getTime(),
+    seen: false,
+  }
+];
+
 export const Toasts = () => {
-  const [confirmed, setConfirmed] = useState(true);
+  const [toasts, updateToasts] = useState(MockToasts.sort((a, b) => a.lastUpdated - b.lastUpdated).filter(t => !t.seen));
+  const numToastsToSee = toasts.length;
+  console.log("rendering toasts", numToastsToSee, toasts);
+  return (
+    <>
+      {numToastsToSee > 0 && (
+        <Toast
+          toast={toasts.find(t => !t.seen)}
+          key={`toast-4-${toasts.find(t => !t.seen).txId}`}
+          markAsSeen={(toast) => {
+            let updates = toasts;
+            const toastIndex= updates.findIndex(t => t.txId === toast.txId);
+            updates[toastIndex].seen = true;
+            updates = updates.filter(t => !t.seen);
+            updateToasts(updates);
+          }}
+        />
+      )}
+    </>
+  );
+};
+
+const Toast = ({ toast, markAsSeen }) => {
+  const confirmed = toast.status === 'CONFIRMED';
+
+  useEffect(() => {
+    const curToast = toast;
+    if (!curToast.seen) {
+      const hide = setTimeout(() => {
+        markAsSeen(curToast);
+      }, 3000);
+
+      return () => {
+        clearTimeout(hide);
+      }
+    }
+    return undefined;
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <article
       className={classNames(Styles.Toast, {
@@ -14,10 +70,13 @@ export const Toasts = () => {
         [Styles.Failed]: !confirmed,
       })}
     >
-      <span>{confirmed ? ConfirmedCheck : FailedX} {confirmed ? 'Confirmed' : 'Failed'}</span>
-      <p>Claimed $24.32 in winnings.</p>
-      <button onClick={() => setConfirmed(!confirmed)}>{CloseIcon}</button>
-      <ReceiptLink txId="0xdeadbeef" />
+      <span>
+        {confirmed ? ConfirmedCheck : FailedX}{' '}
+        {confirmed ? 'Confirmed' : 'Failed'}
+      </span>
+      <p>{toast.message}</p>
+      <button onClick={() => markAsSeen(toast)}>{CloseIcon}</button>
+      <ReceiptLink txId={toast.txId} />
     </article>
   );
 };
