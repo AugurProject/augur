3101b5f2e09fd9bf019c04172fbb5397d43003f2
JohnDanz <John.Danz87@gmail.com>
2021-01-18 15:58:30 -0600

    made some adjustments to portfolio page, started claimWinningSection, updated some types

--- packages/augur-simplified/src/modules/portfolio/portfolio-view.tsx
+++ packages/augur-simplified/src/modules/portfolio/portfolio-view.tsx
@@ -2,21 +2,34 @@ import React, { useState, useEffect } from 'react';
 import Styles from 'modules/portfolio/portfolio-view.styles.less';
 import { AppViewStats } from 'modules/common/labels';
 import Activity from './activity';
-import { PositionsLiquidityViewSwitcher } from 'modules/common/tables';
-import { useAppStatusStore } from 'modules/stores/app-status';
+import { PositionsLiquidityViewSwitcher } from '../common/tables';
+import { useAppStatusStore } from '../stores/app-status';
 import { PrimaryButton } from '../common/buttons';
 import { NetworkMismatchBanner } from '../common/labels';
+import { EthIcon, UsdIcon } from '../common/icons';
 
 const TABLES = 'TABLES';
 const ACTIVITY = 'ACTIVITY';
 
+export const ClaimWinningsSection = () => {
+  const { isLogged} = useAppStatusStore();
+  // userInfo: { balances: { claimableWinnings }}
+  // console.log(isLogged, claimableWinnings);
+  return (
+    <div className={Styles.ClaimableWinningsSection}>
+      {isLogged && <PrimaryButton text="Claim Winnings ($24.00)" icon={UsdIcon} />}
+      {isLogged && <PrimaryButton text="Approve to Claim Winnings (0.87 ETH)" icon={EthIcon} />}
+    </div>
+  );
+};
+
 export const PortfolioView = () => {
-  const { isLogged, isMobile } = useAppStatusStore();
+  const { isMobile } = useAppStatusStore();
   const [view, setView] = useState(TABLES);
 
   useEffect(() => {
     // initial render only.
-    document.getElementById("mainContent")?.scrollTo(0, 0);
+    document.getElementById('mainContent')?.scrollTo(0, 0);
     window.scrollTo(0, 1);
   }, []);
 
@@ -27,7 +40,7 @@ export const PortfolioView = () => {
           <section>
             <NetworkMismatchBanner />
             <AppViewStats />
-            {isLogged && <PrimaryButton text="Claim Winnings ($24.00)" />}
+            <ClaimWinningsSection />
             <PositionsLiquidityViewSwitcher />
           </section>
           <section>
@@ -35,15 +48,19 @@ export const PortfolioView = () => {
           </section>
         </>
       )}
-      {isMobile &&
-      <>
-        <NetworkMismatchBanner />
-        <AppViewStats />
-        {isLogged && <PrimaryButton text="Claim Winnings ($24.00)" />}
-        <PositionsLiquidityViewSwitcher showActivityButton setTables={() => setView(TABLES)} setActivity={() => setView(ACTIVITY)}/>
-        {view === ACTIVITY && <Activity />}
-      </>
-      }
+      {isMobile && (
+        <>
+          <NetworkMismatchBanner />
+          <AppViewStats />
+          <ClaimWinningsSection />
+          <PositionsLiquidityViewSwitcher
+            showActivityButton
+            setTables={() => setView(TABLES)}
+            setActivity={() => setView(ACTIVITY)}
+          />
+          {view === ACTIVITY && <Activity />}
+        </>
+      )}
     </div>
   );
 };
