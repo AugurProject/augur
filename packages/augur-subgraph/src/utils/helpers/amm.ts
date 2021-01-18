55b2472c7e7fc0083a51688e7b961aad09a7f724
Justin <328965+justinbarry@users.noreply.github.com>
2021-01-18 04:03:50 -0800

    Skip AMM exchanges created against an unknown para deploy (#10188)

--- packages/augur-subgraph/src/utils/helpers/amm.ts
+++ packages/augur-subgraph/src/utils/helpers/amm.ts
@@ -2,78 +2,10 @@ import {
   AMMExchange,
   Market
 } from '../../../generated/schema';
-import { Address, BigInt, BigDecimal, log } from '@graphprotocol/graph-ts'
+import { Address, BigInt, BigDecimal } from '@graphprotocol/graph-ts'
 import { ERC20 } from '../../../generated/templates/Cash/ERC20';
 import { ParaShareToken } from '../../../generated/templates/ParaShareToken/ParaShareToken';
 import { AMMExchange as AMMExchangeContract } from "../../../generated/templates/AMMExchange/AMMExchange";
-import { AMMExchange as AMMExchangeTemplate } from '../../../generated/templates';
-
-import { getOrCreateParaShareToken } from './paraDeployer';
-import { getOrCreateCash } from './cash';
-
-export function getOrCreateAMMExchange(
-  id: string,
-  createIfNotFound: boolean = true
-): AMMExchange {
-  log.error('checkpoint1', []);
-  let amm = AMMExchange.load(id);
-
-  log.error('checkpoint1', []);
-
-  if (amm == null && createIfNotFound) {
-    let ZERO = BigDecimal.fromString('0');
-    log.error('checkpoint2', [])
-    amm = new AMMExchange(id);
-    amm.cashBalance = BigInt.fromI32(0);
-    amm.liquidity = ZERO;
-    amm.liquidityCash = ZERO;
-    amm.liquidityInvalid = ZERO;
-    amm.liquidityNo = ZERO;
-    amm.liquidityYes = ZERO;
-    amm.percentageNo = BigDecimal.fromString('0');
-    amm.percentageYes = BigDecimal.fromString('0');
-    amm.totalSupply =  BigInt.fromI32(0);
-    amm.volumeNo = BigDecimal.fromString('0');
-    amm.volumeYes = BigDecimal.fromString('0');
-    amm.fee = BigInt.fromI32(0);
-    amm.feePercent = BigDecimal.fromString('0');
-
-    log.error('checkpoint3', [])
-
-    AMMExchangeTemplate.create(Address.fromString(id));
-  }
-
-  return amm as AMMExchange;
-}
-
-export function createAndSaveAMMExchange(
-  id: string,
-  market: string,
-  shareTokenId: string,
-  cashId: string,
-  fee: BigInt
-): AMMExchange {
-  // This just create the entity.
-
-  log.error('1checkpoint1', []);
-  let cash = getOrCreateCash(cashId);
-  let shareToken = getOrCreateParaShareToken(shareTokenId);
-  shareToken.cash = cashId;
-  shareToken.save();
-
-
-  let amm = getOrCreateAMMExchange(id);
-  amm.market = market;
-  amm.shareToken = shareTokenId;
-  amm.cash = cashId;
-  amm.fee = fee;
-  amm.feePercent = fee.divDecimal(BigInt.fromI32(100).toBigDecimal());
-  amm.save()
-
-
-
-  return updateAMM(id);
-}
 
 export function updateAMM(id: string):AMMExchange {
   let amm = AMMExchange.load(id);
