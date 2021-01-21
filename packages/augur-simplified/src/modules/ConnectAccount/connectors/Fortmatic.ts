f2315cb546d145432a991f69e69b47328d5ec97a
Andrew D'Amelio <dev@andrewdamel.io>
2021-01-21 14:33:44 -0500

    fortmatic, dont wait for overlay

--- packages/augur-simplified/src/modules/ConnectAccount/connectors/Fortmatic.ts
+++ packages/augur-simplified/src/modules/ConnectAccount/connectors/Fortmatic.ts
@@ -1,8 +1,6 @@
 import { ChainId } from '@uniswap/sdk'
 import { FortmaticConnector as FortmaticConnectorCore } from '@web3-react/fortmatic-connector'
 
-export const OVERLAY_READY = 'OVERLAY_READY'
-
 type FormaticSupportedChains = Extract<ChainId, ChainId.MAINNET | ChainId.ROPSTEN | ChainId.RINKEBY | ChainId.KOVAN>
 
 const CHAIN_ID_NETWORK_ARGUMENT: { readonly [chainId in FormaticSupportedChains]: string | undefined } = {
@@ -26,22 +24,9 @@ export class FortmaticConnector extends FortmaticConnectorCore {
     }
 
     const provider = this.fortmatic.getProvider()
-
-    const pollForOverlayReady = new Promise(resolve => {
-      const interval = setInterval(() => {
-        if (provider.overlayReady) {
-          clearInterval(interval)
-          this.emit(OVERLAY_READY)
-          resolve(null)
-        }
-      }, 200)
-    })
-
     const [account] = await Promise.all([
       provider.enable().then((accounts: string[]) => accounts[0]),
-      pollForOverlayReady
     ])
-
     return { provider: this.fortmatic.getProvider(), chainId: (this as any).chainId, account }
   }
 }
--- packages/augur-simplified/src/modules/modal/modal-connect-wallet.tsx
+++ packages/augur-simplified/src/modules/modal/modal-connect-wallet.tsx
@@ -7,7 +7,6 @@ import { AbstractConnector } from '@web3-react/abstract-connector';
 import { SUPPORTED_WALLETS } from '../ConnectAccount/constants';
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
 import { injected, portis } from '../ConnectAccount/connectors';
-import { isMobile } from 'react-device-detect';
 import MetamaskIcon from '../ConnectAccount/assets/metamask.png';
 import { ErrorBlock } from '../common/labels';
 import Loader from '../ConnectAccount/components/Loader';
@@ -217,10 +216,6 @@ const ModalConnectWallet = ({
   useEffect(() => {
     setPendingError(false);
     setWalletView(WALLET_VIEWS.ACCOUNT);
-    // fortmatic.on(OVERLAY_READY, () => {
-    //   // closeModal();
-    //   toggleModal();
-    // });
   }, [toggleModal]);
 
   // close modal when a connection is successful
