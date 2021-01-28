225efa3624e1de7e23c2db4e3c477d7a08d2b947
Justin Barry <328965+justinbarry@users.noreply.github.com>
2021-01-28 14:40:24 -0800

    Handle missing graph events

--- packages/augur-subgraph/src/mappings/wethWrapperForAMMExchange.ts
+++ packages/augur-subgraph/src/mappings/wethWrapperForAMMExchange.ts
@@ -15,6 +15,9 @@ import { getOrCreateUser } from '../utils/helpers';
 export function wethWrapperHandleAddLiquidity(event: AddLiquidityEvent): void {
   let id = event.transaction.hash.toHexString() + "-" + event.logIndex.minus(BigInt.fromI32(1)).toString();
   let addLiquidity = AddLiquidity.load(id);
+  if(addLiquidity == null) {
+    return;
+  }
 
   getOrCreateUser(event.params.sender.toHexString());
   addLiquidity.sender = event.params.sender.toHexString();
@@ -24,6 +27,9 @@ export function wethWrapperHandleAddLiquidity(event: AddLiquidityEvent): void {
 export function wethWrapperHandleEnterPosition(event: EnterPositionEvent): void {
   let id = event.transaction.hash.toHexString() + "-" + event.logIndex.minus(BigInt.fromI32(1)).toString();
   let enterPosition = EnterPosition.load(id);
+  if(enterPosition == null) {
+    return;
+  }
 
   getOrCreateUser(event.params.sender.toHexString());
   enterPosition.sender = event.params.sender.toHexString();
@@ -33,6 +39,9 @@ export function wethWrapperHandleEnterPosition(event: EnterPositionEvent): void 
 export function wethWrapperHandleExitPosition(event: ExitPositionEvent): void {
   let id = event.transaction.hash.toHexString() + "-" + event.logIndex.minus(BigInt.fromI32(1)).toString();
   let exitPosition = ExitPosition.load(id);
+  if(exitPosition == null) {
+    return;
+  }
 
   getOrCreateUser(event.params.sender.toHexString());
   exitPosition.sender = event.params.sender.toHexString();
@@ -42,6 +51,9 @@ export function wethWrapperHandleExitPosition(event: ExitPositionEvent): void {
 export function wethWrapperHandleRemoveLiquidity(event: ExitPositionEvent): void {
   let id = event.transaction.hash.toHexString() + "-" + event.logIndex.minus(BigInt.fromI32(1)).toString();
   let removeLiquidity = RemoveLiquidity.load(id);
+  if(removeLiquidity == null) {
+    return;
+  }
 
   getOrCreateUser(event.params.sender.toHexString());
   removeLiquidity.sender = event.params.sender.toHexString();
