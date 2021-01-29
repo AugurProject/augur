c0ef4901fe86652385c46fdc8aa141e83b966199
Justin <328965+justinbarry@users.noreply.github.com>
2021-01-28 19:25:50 -0800

    Add tradingProceedsClaimed event array to ParaShareToken graph entity (#10420)
    ref #10308

---
+++ packages/augur-subgraph/src/mappings/paraAugur.ts
@@ -0,0 +1,26 @@
+import { TradingProceedsClaimed } from '../../generated/schema';
+import {
+  TradingProceedsClaimed as TradingProceedsClaimedEvent,
+  ParaAugur
+} from '../../generated/templates/ParaAugur/ParaAugur';
+
+export function handleTradingProceedsClaimed(
+  event: TradingProceedsClaimedEvent
+): void {
+  let paraAugur = ParaAugur.bind(event.address);
+
+  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let tradingProceedsClaimed = new TradingProceedsClaimed(id);
+
+  tradingProceedsClaimed.fees = event.params.fees;
+  tradingProceedsClaimed.market = event.params.market.toHexString();
+  tradingProceedsClaimed.numPayoutTokens = event.params.numPayoutTokens;
+  tradingProceedsClaimed.numShares = event.params.numShares;
+  tradingProceedsClaimed.outcome = event.params.outcome;
+  tradingProceedsClaimed.sender = event.params.sender.toHexString();
+  tradingProceedsClaimed.shareToken = paraAugur.shareToken().toHexString();
+  tradingProceedsClaimed.timestamp = event.params.timestamp;
+  tradingProceedsClaimed.universe = event.params.universe.toHexString();
+
+  tradingProceedsClaimed.save();
+}
