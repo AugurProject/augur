4e8c25f6b2b5ea7a485004a22f67ff27eda2b806
phoebemirman <phoebe.mirman@gmail.com>
2021-02-01 13:29:11 -0800

    add blur on enter

--- packages/augur-simplified/src/modules/common/inputs.tsx
+++ packages/augur-simplified/src/modules/common/inputs.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { XIcon } from './icons';
 import Styles from './inputs.styles.less';
 
-export const SearchInput = ({ value, onChange, clearValue }) => (
+export const SearchInput = ({ value, onChange, clearValue }) => {
+  const input = useRef();
+  const keypressHandler = e => {
+    if (e.code === "Enter") {
+      console.log('hi')
+      input.current && input.current.blur();
+    }
+  };
+
+  return (
   <div className={Styles.SearchInput}>
     <input
+      ref={input}
       placeholder="Search for a market"
       value={value}
       onChange={onChange}
+      onKeyPress={event => keypressHandler(event)}
     />
     <div onClick={clearValue}>{XIcon}</div>
   </div>
 );
+}
