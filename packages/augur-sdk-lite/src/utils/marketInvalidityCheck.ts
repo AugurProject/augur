39f8aa0acd5b4d39f532db6d72a4833371421a42
Tom Haile <bradford.haile@gmail.com>
2021-02-19 10:17:13 -0600

    missed checking in file change and updated to not need a ref

--- packages/augur-sdk-lite/src/utils/marketInvalidityCheck.ts
+++ packages/augur-sdk-lite/src/utils/marketInvalidityCheck.ts
@@ -1,13 +1,12 @@
 import { BigNumber } from 'bignumber.js';
 import { GasStation } from '..';
 import { CLAIM_GAS_COST, DEFAULT_GAS_PRICE_IN_GWEI, EULERS_NUMBER, INVALID_SWAP_GAS_COST, MINIMUM_INVALID_ORDER_VALUE_IN_ATTO_DAI, SECONDS_IN_A_YEAR } from '../constants';
-import { MarketData } from '../logs';
 
 // A Market is marked as True in the invalidFilter if the any bid for Invalid on the book would be profitable to take were the market Valid
 const recalcInvalidFilter = (
   invalidOutcomeWeight: BigNumber,
   invalidOutcomeLiquidity: BigNumber,
-  marketData: MarketData,
+  marketData: { endTime: number, numTicks: number},
   feeMultiplier: BigNumber,
   gasLevels: GasStation,
   ETHInAttoCash: BigNumber, // use 1e18 for ETH, need to pass in for USDC
