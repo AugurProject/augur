13950a8d44890bc95ec489443e2c5c5081e03329
phoebemirman <phoebe.mirman@gmail.com>
2021-02-23 11:47:01 -0800

    fix styles

--- packages/augur-comps/src/App.js
+++ packages/augur-comps/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import Styles from './App.styles.less';
+import './assets/styles/shared.less';
 
 import { LoadingMarketCard, MarketCard } from './components/market-card/market-card';
 
 function App() {
   return (
-    <div className="App">
+    <div className={Styles.App}>
       Market card:
       <MarketCard marketId={'0x0cc49229b93f87f97f657931b50c67af3f9b7845'} />
     </div>
