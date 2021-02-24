3a03138748b7d60a0a5cd2fdb81d70bad518396c
justin <328965+justinbarry@users.noreply.github.com>
2021-02-23 16:56:37 -0800

    Get ts building

--- packages/augur-comps/dist/externals.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.less' {
-    const resource: {[key: string]: string};
-    export = resource;
-  }
--- packages/augur-comps/dist/logo.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export const Logo = () => {
-    //const { isMobile } = useAppStatusStore();
-  return <div>logo</div>;
-}
