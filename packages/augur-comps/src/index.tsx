546bca83a6f321e3ecfb0b95695392affa8a8c49
JohnDanz <John.Danz87@gmail.com>
2021-02-25 10:03:47 -0600

    added category icons and the map for them but not sure why they worked, commented for now as to not break something.

--- packages/augur-comps/src/index.tsx
+++ packages/augur-comps/src/index.tsx
@@ -11,6 +11,7 @@ export * from './utils/get-number-precision';
 export * from './utils/window-ref';
 export * from './utils/format-number';
 export * from './utils/date-utils';
-
+// TODO: determine why this even works since we haven't included augur-sdk-lite
+// export { CATEGORIES_ICON_MAP } from './components/common/category-icons-map';
 
 // render(<App />, document.getElementById('root'));
