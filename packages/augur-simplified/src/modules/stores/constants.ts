f4874144a495ab5212b9c509855438ce927e9a29
JohnDanz <John.Danz87@gmail.com>
2021-03-16 13:15:32 -0500

    added hooks for loading user saved settings, deleted app status in simplified

--- packages/augur-simplified/src/modules/stores/constants.ts
+++ packages/augur-simplified/src/modules/stores/constants.ts
@@ -1,5 +1,5 @@
 import { DEFAULT_MARKET_VIEW_SETTINGS, SETTINGS_SLIPPAGE } from '../constants';
-import { AppStatusState, ParaDeploys } from '../types';
+import { ParaDeploys } from '../types';
 
 // @ts-ignore
 export const PARA_CONFIG: ParaDeploys =
@@ -35,56 +35,6 @@ export const SIMPLIFIED_ACTIONS = {
   SET_SHOW_TRADING_FORM: 'SET_SHOW_TRADING_FORM',
   UPDATE_MARKETS_VIEW_SETTINGS: 'UPDATE_MARKETS_VIEW_SETTINGS',
   UPDATE_SETTINGS: 'UPDATE_SETTINGS',
-};
-
-export const STUBBED_APP_STATUS_ACTIONS = {
-  setIsMobile: (isMobile) => {},
-  setSidebar: (sidebarType) => {},
-  setShowTradingForm: (showTradingForm) => {},
-  updateMarketsViewSettings: (settings) => {},
-  updateSettings: (settings, account = null) => {},
-  setModal: (modal) => {},
-  closeModal: () => {},
-  setIsLogged: (account) => {},
-};
-
-export const DEFAULT_APP_STATUS_STATE: AppStatusState = {
-  isMobile: false,
-  sidebarType: null,
-  isLogged: false,
-  modal: {},
-  showTradingForm: false,
-  marketsViewSettings: DEFAULT_MARKET_VIEW_SETTINGS,
-  settings: {
-    slippage: SETTINGS_SLIPPAGE,
-    showInvalidMarkets: false,
-    showLiquidMarkets: false,
-  },
-};
-
-export const APP_STATE_KEYS = {
-  IS_MOBILE: 'isMobile',
-  SIDEBAR_TYPE: 'sidebarType',
-  MARKETS_VIEW_SETTINGS: 'marketsViewSettings',
-  SETTINGS: 'settings',
-  MODAL: 'modal',
-  IS_LOGGED: 'isLogged',
-  SHOW_TRADING_FORM: 'showTradingForm',
-};
-
-export const APP_STATUS_ACTIONS = {
-  SET_IS_MOBILE: 'SET_IS_MOBILE',
-  SET_SIDEBAR: 'SET_SIDEBAR',
-  SET_SHOW_TRADING_FORM: 'SET_SHOW_TRADING_FORM',
-  UPDATE_MARKETS_VIEW_SETTINGS: 'UPDATE_MARKETS_VIEW_SETTINGS',
-  UPDATE_SETTINGS: 'UPDATE_SETTINGS',
-  SET_MODAL: 'SET_MODAL',
-  CLOSE_MODAL: 'CLOSE_MODAL',
-  SET_IS_LOGGED: 'SET_IS_LOGGED',
-};
-
-export const MOCK_APP_STATUS_STATE = {
-  ...DEFAULT_APP_STATUS_STATE,
 };
 
 export const NETWORK_NAMES = {
