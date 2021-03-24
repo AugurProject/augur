98b9ee2daf57419ea0cbf22030d5b13a27542ff1
JohnDanz <John.Danz87@gmail.com>
2021-03-24 16:18:50 -0500

    added a useEffect to app to fix our weird scroll jump issue

--- packages/augur-simplified/src/modules/App.tsx
+++ packages/augur-simplified/src/modules/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router';
 import { HashRouter } from 'react-router-dom';
 import Styles from './App.styles.less';
@@ -18,6 +18,7 @@ import {
   useUserBalances,
   PathUtils,
   Constants,
+  windowRef,
 } from '@augurproject/augur-comps';
 const { MARKETS } = Constants;
 const { parsePath } = PathUtils;
@@ -35,6 +36,21 @@ const AppBody = () => {
   useFinalizeUserTransactions(blocknumber);
   usePageView();
 
+  useEffect(() => {
+    const html = windowRef.document.firstElementChild;
+    const isHeightUnset = html.style.height === '';
+    const eitherOr = (modalShowing || showTradingForm);
+    if (eitherOr && isHeightUnset) {
+      console.log(1);
+      html.style.height = '100%';
+      html.style.overflow = 'hidden';
+    } else if (!eitherOr && !isHeightUnset) {
+      console.log(2);
+      html.style.height = '';
+      html.style.overflow = '';
+    }
+  }, [modalShowing, showTradingForm]);
+  
   return (
     <div
       id="mainContent"
