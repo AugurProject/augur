c48537dad775362dc3c0002631e16fcfc5de5919
Paul Gebheim <pgebheim@gmail.com>
2021-11-02 20:56:12 +0000

    Use SUMMARY

--- augur.sh/sidebars.js
+++ augur.sh/sidebars.js
@@ -4,7 +4,7 @@ module.exports = {
       type: 'category',
       label: 'Overview',
       items: [
-        'overview/whatisaugur',
+        'SUMMARY',
         'contracts/overview'
       ],
     },
